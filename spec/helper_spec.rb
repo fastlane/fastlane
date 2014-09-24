286c9623ff01830d6642edf04af6e740246c89fa
Felix Krause <krausefx@gmail.com>
2014-09-24 12:27:11 +0100

    Disabled Xcode specific unit tests for non Mac environments

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,12 +1,16 @@
 describe IosDeployKit do
   describe IosDeployKit::Helper do
-    it "#xcode_path" do
-      IosDeployKit::Helper.xcode_path[-1].should eq('/')
-      IosDeployKit::Helper.xcode_path.should eq("/Applications/Xcode.app/Contents/Developer/")
-    end
 
-    it "#transporter_path" do
-      IosDeployKit::Helper.transporter_path.should eq("/Applications/Xcode.app/Contents/Developer/../Applications/Application\\ Loader.app/Contents/MacOS/itms/bin/iTMSTransporter")
+    if OS.mac?
+      it "#xcode_path" do
+        IosDeployKit::Helper.xcode_path[-1].should eq('/')
+        IosDeployKit::Helper.xcode_path.should eq("/Applications/Xcode.app/Contents/Developer/")
+      end
+
+      it "#transporter_path" do
+        IosDeployKit::Helper.transporter_path.should eq("/Applications/Xcode.app/Contents/Developer/../Applications/Application\\ Loader.app/Contents/MacOS/itms/bin/iTMSTransporter")
+      end
     end
+    
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,7 @@
 require 'ios_deploy_kit'
+
+module OS
+  def self.mac?
+    (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
+end
