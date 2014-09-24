97425a4b145c735fb4009fe79a30b632252ed7f1
Felix Krause <krausefx@gmail.com>
2014-09-24 22:54:10 +0100

    Added asking and storing of login information when none was given

--- lib/ios_deploy_kit/password_manager.rb
+++ lib/ios_deploy_kit/password_manager.rb
@@ -1,4 +1,5 @@
 require 'security'
+require 'highline/import' # to hide the entered password
 
 module IosDeployKit
   class PasswordManager
@@ -11,8 +12,33 @@ module IosDeployKit
       self.username ||= ENV["IOS_DEPLOY_KIT_USER"] || self.load_from_keychain[0]
       self.password ||= ENV["IOS_DEPLOY_KIT_PASSWORD"] || self.load_from_keychain[1]
 
+      if (self.username || '').length == 0 or (self.password || '').length == 0
+        ask_for_login
+      end
     end
 
+    def ask_for_login
+      puts "No username or password given. You can use environment variables"
+      puts "IOS_DEPLOY_KIT_USER, IOS_DEPLOY_KIT_PASSWORD"
+      puts "The login information will be stored in your keychain"
+
+      while (self.username || '').length == 0
+        self.username = ask("Username: ")
+      end
+
+      while (self.password || '').length == 0
+        self.password = ask("Password: ") { |q| q.echo = "*" }
+      end
+
+      # Now we store this information in the keychain
+      # Example usage taken from https://github.com/nomad/cupertino/blob/master/lib/cupertino/provisioning_portal/commands/login.rb
+      if Security::InternetPassword.add(IosDeployKit::PasswordManager::HOST, self.username, self.password)
+        return true
+      else
+        Helper.log.error "Could not store password in keychain"
+        return false
+      end
+    end
   
     def load_from_keychain
       pass = Security::InternetPassword.find(:server => IosDeployKit::PasswordManager::HOST)
