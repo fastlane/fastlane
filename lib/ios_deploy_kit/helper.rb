5950485056bdb7b039664ee27bbf3bcdb5e8fe67
Felix Krause <krausefx@gmail.com>
2014-09-24 10:47:44 +0100

    Added Xcode and Instruments path to Helper
    Added unit tests for that

--- lib/ios_deploy_kit/helper.rb
+++ lib/ios_deploy_kit/helper.rb
@@ -8,6 +8,14 @@ module IosDeployKit
 
       @@log
     end
+
+    def self.xcode_path
+      `xcode-select -p`.gsub("\n", '') + "/"
+    end
+
+    def self.transporter_path
+      self.xcode_path + '../Applications/Application\ Loader.app/Contents/MacOS/itms/bin/iTMSTransporter'
+    end
     
   end
 end
