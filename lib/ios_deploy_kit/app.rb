ef64bd12d977ae81e25ab40ef67b851e56ec1cfc
Felix Krause <krausefx@gmail.com>
2014-09-28 16:31:25 +0100

    Added first batch of documentation for app class

--- lib/ios_deploy_kit/app.rb
+++ lib/ios_deploy_kit/app.rb
@@ -25,6 +25,9 @@ module IosDeployKit
     end
 
 
+    # @param apple_id The Apple ID of the app you want to modify or update. This ID has usually 9 digits
+    # @param app_identifier If you don't pass this, it will automatically be fetched from the Apple API
+    #   which means it takes longer. If you **can** pass the app_identifier (e.g. com.facebook.Facebook) do it
     def initialize(apple_id = nil, app_identifier = nil)
       self.apple_id = apple_id
       self.app_identifier = app_identifier
@@ -43,6 +46,9 @@ module IosDeployKit
       itc.open_app_page(self)
     end
 
+    # This method fetches the current app status from iTunesConnect.
+    # This method may take some time to execute, since it uses frontend scripting under the hood.
+    # @return the current App Status defined at {IosDeployKit::App::AppStatus}, like "Waiting For Review"
     def get_app_status
       itc.get_app_status(self)
     end
@@ -57,10 +63,18 @@ module IosDeployKit
       self.metadata_dir = dir
     end
 
+    # @return the path to the directy in which the itmsp files will be downloaded
     def get_metadata_directory
       metadata_dir || "./"
     end
 
+    # Access to update the metadata of this app
+    # 
+    # The first time accessing this, will take some time, since it's downloading
+    # the latest version from iTC.
+    # 
+    # Don't forget to call {#upload_metadata!} once you are finished
+    # @return [IosDeployKit::AppMetadata] the latest metadata of this app
     def metadata
       @metadata ||= IosDeployKit::AppMetadata.new(self, get_metadata_directory)
     end
@@ -74,8 +88,12 @@ module IosDeployKit
       itc.create_new_version!(self, version_number)
     end
 
-    # This method has to be called, after modifying the values of .metadata
-    # It will take care of uploading all changes to Apple
+    # This method has to be called, after modifying the values of .metadata.
+    # It will take care of uploading all changes to Apple.
+    # This method might take a few minutes to run
+    # @return [bool] true on success
+    # @raise [IosDeployKit::TransporterTransferError]
+    # @raise [IosDeployKit::TransporterInputError]
     def upload_metadata!
       raise "You first have to modify the metadata using app.metadata.setDescription" unless @metadata
       
