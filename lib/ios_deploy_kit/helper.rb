6986548e1be20f830f1823a1c3f65f0249e9fe32
Felix Krause <krausefx@gmail.com>
2014-09-28 17:34:53 +0100

    Added documentation to Helper class

--- lib/ios_deploy_kit/helper.rb
+++ lib/ios_deploy_kit/helper.rb
@@ -3,6 +3,7 @@ require 'logger'
 module IosDeployKit
   module Helper
 
+    # Logging happens using this method
     def self.log
       if is_test?
         @@log ||= Logger.new(STDOUT).tap { |l| l.level = Logger::FATAL }
@@ -13,14 +14,18 @@ module IosDeployKit
       @@log
     end
 
+    # @return true if the currently running program is a unit test
     def self.is_test?
       defined?SpecHelper
     end
 
+    # @return the full path to the Xcode developer tools of the currently
+    #  running system
     def self.xcode_path
       `xcode-select -p`.gsub("\n", '') + "/"
     end
 
+    # @return the full path to the iTMSTransporter executable
     def self.transporter_path
       self.xcode_path + '../Applications/Application\ Loader.app/Contents/MacOS/itms/bin/iTMSTransporter'
     end
