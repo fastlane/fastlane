904230c6c646ad810135181010e9e7dbce8655c8
Felix Krause <krausefx@gmail.com>
2014-11-08 15:38:13 +0000

    Fixed web mocking running unused loops

--- spec/mocking/webmocking.rb
+++ spec/mocking/webmocking.rb
@@ -9,6 +9,7 @@ RSpec.configure do |config|
       stub_request(:get, "https://itunes.apple.com/lookup?id=#{current.to_s}").
            with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
            to_return(:status => 200, :body => File.read("spec/responses/itunesLookup-#{current.to_s}.json"), :headers => {})
+    end
 
     # iTunes Lookup API by App Identifier
     stub_request(:get, "https://itunes.apple.com/lookup?bundleId=com.facebook.Facebook").
@@ -26,6 +27,5 @@ RSpec.configure do |config|
     stub_request(:get, "https://itunes.apple.com/lookup?bundleId=net.sunapps.invalid").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => File.read("spec/responses/itunesLookup-net.sunapps.invalid.json"), :headers => {})
-    end
   end
 end
