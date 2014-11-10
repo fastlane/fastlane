9aa9637f2fe5f439e21a30bee6bacb3400a3ca49
Felix Krause <krausefx@gmail.com>
2014-11-10 16:18:31 +0000

    Added example project

---
+++ example/Example/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Example
+//
+//  Created by Felix Krause on 10.11.14.
+//  Copyright (c) 2014 Felix Krause. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ example/Example/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Example
+//
+//  Created by Felix Krause on 10.11.14.
+//  Copyright (c) 2014 Felix Krause. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
