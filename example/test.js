33f806a8dffb67dd1769699952a64e23683d3267
Felix Krause <krausefx@gmail.com>
2014-11-10 16:22:41 +0000

    Extend example script to show orientation and taps

--- example/test.js
+++ example/test.js
@@ -6,4 +6,12 @@ var window = app.mainWindow();
 
 
 target.delay(3)
-captureLocalizedScreenshot("LandingScreen")
+captureLocalizedScreenshot("0-LandingScreen")
+
+target.frontMostApp().tabBar().buttons()[1].tap();
+target.delay(1)
+captureLocalizedScreenshot("1-SecondScreen")
+
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_LANDSCAPELEFT);
+target.delay(1)
+captureLocalizedScreenshot("3-RotatedScreen")
