fe3cd095386d6991e78e4806effb38fb83ca246b
Felix Krause <krausefx@gmail.com>
2014-11-10 19:13:17 +0000

    Updated example UI Automation script

--- example/test.js
+++ example/test.js
@@ -11,7 +11,3 @@ captureLocalizedScreenshot("0-LandingScreen")
 target.frontMostApp().tabBar().buttons()[1].tap();
 target.delay(1)
 captureLocalizedScreenshot("1-SecondScreen")
-
-target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_LANDSCAPELEFT);
-target.delay(1)
-captureLocalizedScreenshot("3-RotatedScreen")
