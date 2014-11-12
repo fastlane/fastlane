e3e733beb039b2c4083aa1568061f242747e6606
Felix Krause <krausefx@gmail.com>
2014-11-12 17:14:26 +0000

    Added setup for downloading and converting templates

--- lib/frameit/frame_converter.rb
+++ lib/frameit/frame_converter.rb
@@ -1,11 +1,63 @@
 module Frameit
   class FrameConverter
-    TEMPLATES_PATH = './devices'
+    DOWNLOAD_URL = 'https://developer.apple.com/app-store/marketing/guidelines/#images'
+    FRAME_PATH = '.frameit/devices_frames'
+    
+    def run
+      @templates_path = "#{ENV['HOME']}/#{FRAME_PATH}"
+
+      self.setup_frames
+    end
+
+    def templates_path
+      @templates_path
+    end
+
+    def setup_frames
+      unless frames_exist?
+        puts "----------------------------------------------------".green
+        puts "Looks like you have no device templates installed".green
+        puts "The images can not be pre-installed due to licensing".green
+        puts "Press Enter to get started".green
+        puts "----------------------------------------------------".green
+        STDIN.gets
+        
+        system("open '#{DOWNLOAD_URL}'")
+        puts "----------------------------------------------------".green
+        puts "Download the zip files for the following devices".green
+        puts "iPhone 6, iPhone 6 Plus, iPhone 5s and iPad Air".green
+        puts "You only need to download the devices you want to use".green
+        puts "Press Enter when you downloaded the zip files".green
+        puts "----------------------------------------------------".green
+        STDIN.gets
+
+        while not frames_exist?
+          system("mkdir -p '#{@templates_path}' && open '#{@templates_path}'")
+          puts "----------------------------------------------------".green
+          puts "Extract the downloaded files into the folder".green
+          puts "'#{@templates_path}, which should be open in your Finder'".green
+          puts "You can just copy the whole content into it.'".green
+          puts "The structure doesn't matter.'".green
+          puts "Press Enter when you downloaded the zip files".green
+          puts "----------------------------------------------------".green
+          STDIN.gets
+
+          if not frames_exist?
+            puts "Sorry, I can't find the PSD files. Make sure you unzipped them into '#{@templates_path}'".red
+          end
+        end
+      end
+
+      convert_frames
+    end
+
+    def frames_exist?
+      Dir["#{@templates_path}/**/*.psd"].count > 0
+    end
 
     # Converts all the PSD files to trimmed PNG files
-    def run
-
-      Dir["#{TEMPLATES_PATH}/**/*.psd"].each do |psd|
+    def convert_frames
+      Dir["#{@templates_path}/**/*.psd"].each do |psd|
         resulting_path = psd.gsub('.psd', '.png')
         unless File.exists?resulting_path
           Helper.log.debug "Converting PSD file '#{psd}'".yellow
