336b80ad51bb2f887639c24bc999ea7130150a8b
Felix Krause <krausefx@gmail.com>
2014-11-12 17:44:36 +0000

    Improved setup guide

--- lib/frameit/frame_converter.rb
+++ lib/frameit/frame_converter.rb
@@ -35,9 +35,9 @@ module Frameit
           system("mkdir -p '#{@templates_path}' && open '#{@templates_path}'")
           puts "----------------------------------------------------".green
           puts "Extract the downloaded files into the folder".green
-          puts "'#{@templates_path}, which should be open in your Finder'".green
+          puts "'#{@templates_path}', which should be open in your Finder'".green
           puts "You can just copy the whole content into it.'".green
-          puts "The structure doesn't matter.'".green
+          puts "The structure doesn't matter.".green
           puts "Press Enter when you downloaded the zip files".green
           puts "----------------------------------------------------".green
           STDIN.gets
