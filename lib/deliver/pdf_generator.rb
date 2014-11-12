982c729c47aa0665215dbb706d61198db2466911
James Moore <james@panic.com>
2014-11-12 13:37:23 -0800

    sort by size then filename so images are lined up visually

--- lib/deliver/pdf_generator.rb
+++ lib/deliver/pdf_generator.rb
@@ -101,7 +101,8 @@ module Deliver
           move_cursor_to top
           
           if (content[:screenshots] || []).count > 0
-            content[:screenshots].sort{ |a, b| a.screen_size <=> b.screen_size }.each do |screenshot|
+
+            content[:screenshots].sort{ |a, b| [a.screen_size, a.path] <=> [b.screen_size, b.path] }.each do |screenshot|
               
               if last_size and last_size != screenshot.screen_size
                 # Next row (other simulator size)
