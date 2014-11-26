518da5eb2bc51dda7ac0d11dd68e6aa25ed64fda
Felix Krause <krausefx@gmail.com>
2014-11-26 11:50:03 +0000

    Added unit tests

---
+++ spec/developer_spec.rb
@@ -0,0 +1,16 @@
+require 'sigh'
+describe "Something" do 
+  it 'works' do
+    system("rm -rf /tmp/sigh/")
+    ENV["DELIVER_USER"] = "felix@sunapps.net"
+    
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::APPSTORE)
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::DEVELOPMENT)
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::ADHOC)
+
+    path = "/tmp/sigh"
+    expect(File.exists?(File.join(path, "AdHoc_net.sunapps.7.mobileprovision"))).to equal(true)
+    expect(File.exists?(File.join(path, "AppStore_net.sunapps.7.mobileprovision"))).to equal(true)
+    expect(File.exists?(File.join(path, "Development_net.sunapps.7.mobileprovision"))).to equal(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+# Code climate
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'sigh'
+
+# This module is only used to check the environment is currently a testing env
+module SpecHelper
+  
+end
+
+
+module OS
+  def self.mac?
+    (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
+end
