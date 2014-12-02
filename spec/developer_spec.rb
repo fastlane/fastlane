1384351fb534400e990871fb0b2bfb715432389b
Felix Krause <krausefx@gmail.com>
2014-12-02 16:55:25 +0000

    Added tests for enterprise

--- spec/developer_spec.rb
+++ spec/developer_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "Create certificates" do 
   it 'AppStore, Development and Ad Hoc' do
     system("rm -rf /tmp/sigh/")
+    # Regular
     ENV["DELIVER_USER"] = "felix@sunapps.net"
     
     Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::APPSTORE)
@@ -13,5 +14,12 @@ describe "Create certificates" do
     expect(File.exists?(File.join(path, "AdHoc_net.sunapps.7.mobileprovision"))).to equal(true)
     expect(File.exists?(File.join(path, "AppStore_net.sunapps.7.mobileprovision"))).to equal(true)
     expect(File.exists?(File.join(path, "Development_net.sunapps.7.mobileprovision"))).to equal(true)
+
+
+    # Enterprise
+    ENV["DELIVER_USER"] = "felix.krause@sunapps.net"
+    Sigh::DeveloperCenter.new.run('net.sunapps.*', Sigh::DeveloperCenter::APPSTORE)
+    path = "/tmp/sigh"
+    expect(File.exists?(File.join(path, "Distribution_net.sunapps.*.mobileprovision"))).to equal(true)
   end
 end
