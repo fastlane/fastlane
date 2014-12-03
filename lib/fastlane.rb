7bb45a1c2f8f0e1ce22be9c8451f8eafcca75fef
Felix Krause <krausefx@gmail.com>
2014-12-03 01:15:32 +0000

    Added internals for update check, dependencies, etc.

---
+++ lib/fastlane.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'fastlane/version'
+require 'fastlane/fast_file'
+require 'fastlane/helper'
+require 'fastlane/dependency_checker'
+require 'fastlane/runner'
+
+# Third Party code
+require 'colored'
+
+module Sigh
+  TMP_FOLDER = "/tmp/fastlane/"
+end
---
+++ lib/fastlane/version.rb
@@ -0,0 +1,3 @@
+module FastLane
+  VERSION = "0.0.1"
+end
