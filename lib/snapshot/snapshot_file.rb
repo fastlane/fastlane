c1923994cbc10c0ad288357381caf0ed5b37600c
Felix Krause <krausefx@gmail.com>
2014-12-03 22:40:22 +0000

    Switched to absolute paths in Snapfile config

--- lib/snapshot/snapshot_file.rb
+++ lib/snapshot/snapshot_file.rb
@@ -36,10 +36,10 @@ module Snapshot
         when :js_file
           raise "js_file has to be an String".red unless value.kind_of?String
           raise "js_file at path '#{value}' not found".red unless File.exists?value
-          @config.manual_js_file = value.gsub("~", ENV['HOME'])
+          @config.manual_js_file = File.expand_path(value)
         when :screenshots_path
           raise "screenshots_path has to be an String".red unless value.kind_of?String
-          @config.screenshots_path = value.gsub("~", ENV['HOME'])
+          @config.screenshots_path = File.expand_path(value)
         when :build_command
           raise "build_command has to be an String".red unless value.kind_of?String
           @config.build_command = value
@@ -48,10 +48,11 @@ module Snapshot
         when :project_path
           raise "project_path has to be an String".red unless value.kind_of?String
 
-          if File.exists?value and (value.end_with?".xcworkspace" or value.end_with?".xcodeproj")
-            @config.project_path = value.gsub("~", ENV['HOME'])
+          path = File.expand_path(value)
+          if File.exists?path and (path.end_with?".xcworkspace" or path.end_with?".xcodeproj")
+            @config.project_path = path
           else
-            raise "The given project_path '#{value}' could not be found. Make sure to include the extension as well.".red
+            raise "The given project_path '#{path}' could not be found. Make sure to include the extension as well.".red
           end
 
         # Blocks
