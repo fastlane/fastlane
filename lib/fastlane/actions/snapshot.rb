2e8ffc737b6a787665928276f03a99e20702a302
Felix Krause <krausefx@gmail.com>
2014-12-12 17:46:17 +0000

    Added :noclean flag for snapshot integration

--- lib/fastlane/actions/snapshot.rb
+++ lib/fastlane/actions/snapshot.rb
@@ -5,8 +5,12 @@ module Fastlane
 
       require 'snapshot'
       ENV['SNAPSHOT_SCREENSHOTS_PATH'] = self.snapshot_screenshots_folder
+
+      clean = true
+      clean = false if params.first == :noclean
+
       Snapshot::SnapshotConfig.shared_instance
-      Snapshot::Runner.new.work(clean: false)
+      Snapshot::Runner.new.work(clean: clean)
     end
 
     def self.snapshot_screenshots_folder
