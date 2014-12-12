58bab9e66b3b0ecf77e3061b3c80a20b9f182a1d
Felix Krause <krausefx@gmail.com>
2014-12-12 17:25:49 +0000

    Switched to using ruby objects for snapshot

--- lib/fastlane/actions/snapshot.rb
+++ lib/fastlane/actions/snapshot.rb
@@ -1,8 +1,12 @@
 module Fastlane
   module Actions
     def self.snapshot(params)
+      need_gem!'snapshot'
+
+      require 'snapshot'
       ENV['SNAPSHOT_SCREENSHOTS_PATH'] = self.snapshot_screenshots_folder
-      sh "snapshot"
+      Snapshot::SnapshotConfig.shared_instance
+      Snapshot::Runner.new.work(clean: false)
     end
 
     def self.snapshot_screenshots_folder
