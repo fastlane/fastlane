febc3ba31bd7f333703276c44a34f15c21462a18
Felix Krause <KrauseFx@gmail.com>
2015-01-06 16:57:45 +0000

    Build Number is now stored in shared hash

--- lib/fastlane/actions/increment_build_number.rb
+++ lib/fastlane/actions/increment_build_number.rb
@@ -1,5 +1,9 @@
 module Fastlane
   module Actions
+    module SharedValues
+      BUILD_NUMBER = :BUILD_NUMBER
+    end
+
     def self.increment_build_number(params)
       # More information about how to set up your project and how it works: 
       # https://developer.apple.com/library/ios/qa/qa1827/_index.html
@@ -10,10 +14,14 @@ module Fastlane
           Dir.chdir("..") do
             custom_number = (params.first rescue nil)
             if custom_number
-              return sh_no_action "agvtool new-version -all #{custom_number}"
+              sh_no_action "agvtool new-version -all #{custom_number}"
             else
-              return sh_no_action "agvtool next-version -all"
+              sh_no_action "agvtool next-version -all"
             end
+
+            # Store the new number in the shared hash
+            build_number = `agvtool what-version`.split("\n").last.to_i
+            self.shared_hash[SharedValues::BUILD_NUMBER] = build_number if build_number > 0
           end
         end
       rescue => ex
