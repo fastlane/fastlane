e185626b028bcdddb8534e81bff584e674ef276f
Felix Krause <KrauseFx@gmail.com>
2015-01-06 16:23:36 +0000

    Snapshot now uses the default location to store screenshots and uses the shared hash
    Updated README to match that

--- lib/fastlane/actions/snapshot.rb
+++ lib/fastlane/actions/snapshot.rb
@@ -1,10 +1,13 @@
 module Fastlane
   module Actions
+    module SharedValues
+      SNAPSHOT_SCREENSHOTS_PATH = :SNAPSHOT_SCREENSHOTS_PATH
+    end
+
+
     def self.snapshot(params)
       execute_action("snapshot") do
         require 'snapshot'
-        
-        ENV['SNAPSHOT_SCREENSHOTS_PATH'] = self.snapshot_screenshots_folder
 
         clean = true
         clean = false if params.first == :noclean
@@ -13,11 +16,11 @@ module Fastlane
 
         Snapshot::SnapshotConfig.shared_instance
         Snapshot::Runner.new.work(clean: clean)
-      end
-    end
 
-    def self.snapshot_screenshots_folder
-      './screenshots'
+        results_path = Snapshot::SnapshotConfig.shared_instance.screenshots_path
+
+        self.shared_hash[SharedValues::SNAPSHOT_SCREENSHOTS_PATH] = results_path
+      end
     end
   end
 end
