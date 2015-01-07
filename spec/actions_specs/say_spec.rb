11f0b38b8d522bd57bee0494950ef38ca4cfd77a
Felix Krause <KrauseFx@gmail.com>
2015-01-07 14:42:21 +0000

    Added say test

---
+++ spec/actions_specs/say_spec.rb
@@ -0,0 +1,13 @@
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "Say Integration" do
+      it "works" do
+        result = Fastlane::FastFile.new.parse("lane :test do 
+          say ['Hi Felix', 'Good Job']
+        end").runner.execute(:test)
+
+        expect(result).to eq('say \'Hi Felix Good Job\'')
+      end
+    end
+  end
+end
