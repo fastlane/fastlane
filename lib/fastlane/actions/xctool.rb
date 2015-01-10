e6053e0d8e39fffa767fd57721626d8cfd83e1cb
Felix Krause <KrauseFx@gmail.com>
2015-01-10 20:55:19 +0000

    Added error message if xctool is not installed

--- lib/fastlane/actions/xctool.rb
+++ lib/fastlane/actions/xctool.rb
@@ -2,6 +2,7 @@ module Fastlane
   module Actions
     class XctoolAction
       def self.run(params)
+        raise "xctool not installed, please install using `brew install xctool`".red if `which xctool`.length == 0
         Actions.sh("xctool " + params.join(" "))
       end
     end
