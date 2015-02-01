84fcb8b4669b4d77d63e79116e66ab87fa926db3
Felix Krause <KrauseFx@users.noreply.github.com>
2015-02-01 14:19:12 +0000

    do not ask for unnecessary config when skipping itc

--- lib/produce/config.rb
+++ lib/produce/config.rb
@@ -12,25 +12,28 @@ module Produce
         :app_name => ENV['PRODUCE_APP_NAME'],
         :primary_language => ENV['PRODUCE_LANGUAGE'],
         :version => ENV['PRODUCE_VERSION'],
-        :sku => ENV['PRODUCE_SKU']
+        :sku => ENV['PRODUCE_SKU'],
+        :skip_itc => ENV['PRODUCE_SKIP_ITC']
       }
 
       @config[:bundle_identifier] ||= CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
       @config[:bundle_identifier] ||= ask("App Identifier (Bundle ID, e.g. com.krausefx.app): ")
       @config[:app_name] ||= ask("App Name: ")
       
-      while @config[:primary_language].to_s.length == 0
-        input = ask("Primary Language (e.g. 'English', 'German'): ")
-        input = input.split.map(&:capitalize).join(' ')
-        if not AvailableDefaultLanguages.all_langauges.include?(input)
-          Helper.log.error "Could not find langauge #{input} - available languages: #{AvailableDefaultLanguages.all_langauges}"
-        else
-          @config[:primary_language] = input
+      if @config[:skip_itc].to_s.length == 0
+        while @config[:primary_language].to_s.length == 0
+          input = ask("Primary Language (e.g. 'English', 'German'): ")
+          input = input.split.map(&:capitalize).join(' ')
+          if not AvailableDefaultLanguages.all_langauges.include?(input)
+            Helper.log.error "Could not find langauge #{input} - available languages: #{AvailableDefaultLanguages.all_langauges}"
+          else
+            @config[:primary_language] = input
+          end
         end
-      end
 
-      @config[:version] ||= ask("Initial version number (e.g. '1.0'): ")
-      @config[:sku] ||= ask("SKU Number (e.g. '1234'): ")
+        @config[:version] ||= ask("Initial version number (e.g. '1.0'): ")
+        @config[:sku] ||= ask("SKU Number (e.g. '1234'): ")
+      end
     end
 
     def self.val(key)
