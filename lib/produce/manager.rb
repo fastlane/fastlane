baa7a405543e4ec69c384c1e7d090f1ea483fa38
Felix Krause <KrauseFx@users.noreply.github.com>
2015-02-01 14:19:12 +0000

    Added option to skip iTunes Connect creation
    Fixes https://github.com/KrauseFx/produce/issues/1

--- lib/produce/manager.rb
+++ lib/produce/manager.rb
@@ -2,7 +2,7 @@ module Produce
   class Manager
     def self.start_producing
       DeveloperCenter.new.run
-      return ItunesConnect.new.run
+      return ItunesConnect.new.run unless (ENV["PRODUCE_SKIP_ITC"].to_s.length > 0)
     end
   end
 end
