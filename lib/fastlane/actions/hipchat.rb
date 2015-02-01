6244b4b17dd459618f3fd25af65089c8ebc1bd97
jsc <jsc@scireum.de>
2015-02-01 20:29:25 +0100

    Updated README + added the ability to send private HipChat Messages

--- lib/fastlane/actions/hipchat.rb
+++ lib/fastlane/actions/hipchat.rb
@@ -8,7 +8,7 @@ module Fastlane
       def self.run(params)
         options = { message: '',
                     success: true,
-                    room: nil
+                    channel: nil
                   }.merge(params.first || {})
 
         require 'hipchat'
@@ -21,8 +21,19 @@ module Fastlane
         end
 
         client = HipChat::Client.new(api_token, :api_version => 'v2')
+        channel = options[:channel]
         color = (options[:success] ? 'green' : 'red')
-        client[options[:room]].send('fastlane',options[:message], :message_format => 'text', :color => color)
+
+        if channel.to_s.start_with?('@')
+          #private message
+          #currently hipchat-rb release wrapper doesn´t allow to send private html message we have to send the raw message
+          channel.slice!(0)
+          client.user(channel).send(options[:message])
+        else
+          #room message
+          message = "<table><tr><td><img src=\"https://s3-eu-west-1.amazonaws.com/fastlane.tools/fastlane.png\" style=\"width:50px;height:auto\"></td><td>" + options[:message] + "</td></tr></table>"
+          client[channel].send('fastlane',message, :message_format => 'html', :color => color)
+        end
 
       end
     end
