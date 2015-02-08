fd8b2a59b38ffa36a48464e77570954da1b1e563
Felix Krause <KrauseFx@gmail.com>
2015-02-08 16:35:53 +0000

    Fixed spelling mistake

--- lib/fastlane/actions/ipa.rb
+++ lib/fastlane/actions/ipa.rb
@@ -63,7 +63,7 @@ module Fastlane
 
         if Helper.is_test?
           Actions.lane_context[SharedValues::IPA_OUTPUT_PATH] = File.join(absolute_dest_directory, "test.ipa")
-          Actions.lane_context[SharedValues::DSYM_OUTPUT_PATH] = File.join(absolute_dest_directory, "test.app.dSTM.zip")
+          Actions.lane_context[SharedValues::DSYM_OUTPUT_PATH] = File.join(absolute_dest_directory, "test.app.dSYM.zip")
           return build_args 
         end
 
