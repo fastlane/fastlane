cdd841129974cdf23aa1543eb005b359f92820d3
Felix Krause <KrauseFx@gmail.com>
2015-02-12 23:12:46 +0000

    Added `team_id` action

---
+++ lib/fastlane/actions/team_id.rb
@@ -0,0 +1,20 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      
+    end
+
+    class TeamIdAction
+      def self.run(params)
+        team = params.first
+        raise "Please pass your Team ID (e.g. team_id 'Q2CBPK58CA')".red unless team.to_s.length > 0
+        
+        Helper.log.info "Setting Team ID to '#{team}' for all build steps"
+
+        [:CERT_TEAM_ID, :SIGH_TEAM_ID, :PEM_TEAM_ID, :PRODUCE_TEAM_ID, :SIGH_TEAM_ID].each do |current|
+          ENV[current.to_s] = team
+        end
+      end
+    end
+  end
+end
