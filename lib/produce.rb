fe9deed1a4aea432f3aa152c09642ef219adc0d5
Felix Krause <KrauseFx@gmail.com>
2015-02-17 21:26:25 +0000

    Removed some old includes

--- lib/produce.rb
+++ lib/produce.rb
@@ -9,10 +9,6 @@ require 'produce/itunes_connect'
 require 'produce/update_checker'
 require 'produce/available_default_languages'
 
-# Third Party code
-require 'phantomjs/poltergeist'
-require 'colored'
-
 module Produce
   TMP_FOLDER = "/tmp/produce/"
 
