601f6a134bd0031b001f18a0a322c9dc772cea10
Felix Krause <KrauseFx@gmail.com>
2015-02-17 15:59:49 +0000

    Added available languages

--- lib/fastlane_core.rb
+++ lib/fastlane_core.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'fastlane_core/version'
 require 'fastlane_core/helper'
 require 'fastlane_core/update_checker'
+require 'fastlane_core/languages'
 
 # Third Party code
 require 'colored'
