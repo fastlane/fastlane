d27318b1538199c37c500c9c2df6f982ddc080e8
Felix Krause <KrauseFx@gmail.com>
2015-02-17 16:00:18 +0000

    Moved out available languages to fastlane core

--- lib/deliver/languages.rb
+++
@@ -1,6 +0,0 @@
-module Deliver
-  module Languages
-    # These are all the languages which are available to use to upload app metadata and screenshots
-    ALL_LANGUAGES = ["da-DK", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-US", "es-ES", "es-MX", "fi-FI", "fr-CA", "fr-FR", "id-ID", "it-IT", "ja-JP", "ko-KR", "ms-MY", "nl-NL", "no-NO", "pt-BR", "pt-PT", "ru-RU", "sv-SE", "th-TH", "tr-TR", "vi-VI", "cmn-Hans", "zh_CN", "cmn-Hant"]
-  end
-end
--- spec/languages_spec.rb
+++
@@ -1,7 +0,0 @@
-describe Deliver do
-  describe Deliver::Languages do
-    it "all languages are available" do
-      expect(Deliver::Languages::ALL_LANGUAGES.count).to be >= 28
-    end
-  end
-end
