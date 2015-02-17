5d12dde3bc11bdae6f55bcd3005009ed66a6ade3
Felix Krause <KrauseFx@gmail.com>
2015-02-17 21:22:18 +0000

    Moved out lot of code to fastlane_core gem

--- lib/fastlane.rb
+++ lib/fastlane.rb
@@ -1,15 +1,15 @@
 require 'json'
 require 'fastlane/version'
 require 'fastlane/fast_file'
-require 'fastlane/helper'
 require 'fastlane/dependency_checker'
 require 'fastlane/runner'
 require 'fastlane/setup'
 require 'fastlane/fastlane_folder'
-require 'fastlane/update_checker'
 require 'fastlane/junit_generator'
 require 'fastlane/lane_manager'
 require 'fastlane/actions/actions_helper'
+
+require 'fastlane_core'
 
 # Third Party code
 require 'colored'
@@ -17,12 +17,21 @@ require 'colored'
 module Fastlane
   TMP_FOLDER = '/tmp/fastlane/'
 
-  UpdateChecker.verify_latest_version
+  Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
+
+  FastlaneCore::UpdateChecker.verify_latest_version('fastlane', Fastlane::VERSION)
 
   Fastlane::Actions.load_default_actions
 
   if Fastlane::FastlaneFolder.path
     actions_path = File.join(Fastlane::FastlaneFolder.path, 'actions')
     Fastlane::Actions.load_external_actions(actions_path) if File.directory?(actions_path)
+  end
+end
+
+
+class String
+  def classify
+    split('_').collect!(&:capitalize).join
   end
 end
