507419e8a13caec29f2f13f8507b4f7e0ab1051c
Felix Krause <KrauseFx@gmail.com>
2015-02-17 16:36:53 +0000

    Moved out Apple Dev Portal and update checker and helper class to fastlane core

--- lib/sigh.rb
+++ lib/sigh.rb
@@ -1,10 +1,10 @@
 require 'json'
 require 'sigh/version'
-require 'sigh/helper'
 require 'sigh/dependency_checker'
 require 'sigh/developer_center'
-require 'sigh/update_checker'
 require 'sigh/resign'
+
+require 'fastlane_core'
 
 # Third Party code
 require 'colored'
@@ -13,6 +13,8 @@ require 'phantomjs/poltergeist'
 module Sigh
   TMP_FOLDER = "/tmp/sigh/"
 
-  Sigh::UpdateChecker.verify_latest_version
+  Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
+
+  FastlaneCore::UpdateChecker.verify_latest_version('sigh', Sigh::VERSION)
   DependencyChecker.check_dependencies
 end
--- spec/developer_spec.rb
+++ spec/developer_spec.rb
@@ -6,9 +6,9 @@ describe "Create certificates" do
     # Regular
     ENV["DELIVER_USER"] = "felix@sunapps.net"
     
-    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::APPSTORE)
-    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::DEVELOPMENT)
-    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::ADHOC)
+    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::APPSTORE)
+    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::DEVELOPMENT)
+    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::ADHOC)
 
     path = "/tmp/sigh"
     expect(File.exists?(File.join(path, "AdHoc_net.sunapps.7.mobileprovision"))).to equal(true)
@@ -18,7 +18,7 @@ describe "Create certificates" do
 
     # Enterprise
     ENV["DELIVER_USER"] = "felix.krause@sunapps.net"
-    Sigh::DeveloperCenter.new.run('net.sunapps.*', Sigh::DeveloperCenter::APPSTORE)
+    FastlaneCore::DeveloperCenter.new.run('net.sunapps.*', FastlaneCore::DeveloperCenter::APPSTORE)
     path = "/tmp/sigh"
     expect(File.exists?(File.join(path, "Distribution_net.sunapps.*.mobileprovision"))).to equal(true)
   end
