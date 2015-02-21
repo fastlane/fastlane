c28d2da93b578161acebfe81944b532c8ed688f0
Felix Krause <KrauseFx@gmail.com>
2015-02-21 17:03:04 +0000

    Added new action to set the provisioning profile UDID as the new project setting before buildinig the app

---
+++ lib/fastlane/actions/update_project_code_signing.rb
@@ -0,0 +1,24 @@
+module Fastlane
+  module Actions
+    module SharedValues
+    end
+
+    class UpdateProjectCodeSigningAction
+      def self.run(params)
+        path = params.first
+        path = File.join(path, "project.pbxproj")
+        raise "Could not find path to project config '#{path}'. Pass the path to your project (not workspace)!".red unless File.exists?(path)
+
+        udid = (params[1] rescue nil)
+        udid ||= ENV["SIGH_UDID"]
+
+        Helper.log.info("Updating provisioning profile UDID (#{udid}) for the given project '#{path}'")
+
+        p = File.read(path)
+        File.write(path, p.gsub(/PROVISIONING_PROFILE = ".*";/, "PROVISIONING_PROFILE = \"#{udid}\";"))
+
+        Helper.log.info("Successfully updated project settings to use UDID '#{udid}'".green)
+      end
+    end
+  end
+end
