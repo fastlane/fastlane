4a6d904b05ed53373f74ab4ef577cfb9d9bf73cd
Felix Krause <KrauseFx@gmail.com>
2015-03-05 13:30:15 +0000

    Updated tests

--- spec/developer_spec.rb
+++ spec/developer_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe "Create certificates" do 
   it 'AppStore, Development and Ad Hoc' do
-    system("rm -rf /tmp/sigh/")
+    system("rm -rf /tmp/fastlane_core/")
     # Regular
     ENV["DELIVER_USER"] = "felix@sunapps.net"
     
-    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::APPSTORE)
-    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::DEVELOPMENT)
-    FastlaneCore::DeveloperCenter.new.run('net.sunapps.7', FastlaneCore::DeveloperCenter::ADHOC)
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::APPSTORE)
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::DEVELOPMENT)
+    Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::ADHOC)
 
-    path = "/tmp/sigh"
+    path = "/tmp/fastlane_core"
     expect(File.exists?(File.join(path, "AdHoc_net.sunapps.7.mobileprovision"))).to equal(true)
     expect(File.exists?(File.join(path, "Distribution_net.sunapps.7.mobileprovision"))).to equal(true)
     expect(File.exists?(File.join(path, "Development_net.sunapps.7.mobileprovision"))).to equal(true)
@@ -18,8 +18,7 @@ describe "Create certificates" do
 
     # Enterprise
     ENV["DELIVER_USER"] = "felix.krause@sunapps.net"
-    FastlaneCore::DeveloperCenter.new.run('net.sunapps.*', FastlaneCore::DeveloperCenter::APPSTORE)
-    path = "/tmp/sigh"
+    Sigh::DeveloperCenter.new.run('net.sunapps.*', Sigh::DeveloperCenter::APPSTORE)
     expect(File.exists?(File.join(path, "Distribution_net.sunapps.*.mobileprovision"))).to equal(true)
   end
 end
