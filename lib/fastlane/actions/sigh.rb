e36c56ce2b022f95c84ff278f0f50b5d4aa7bf70
Felix Krause <KrauseFx@gmail.com>
2015-03-06 17:07:25 +0000

    Updated sigh integration

--- lib/fastlane/actions/sigh.rb
+++ lib/fastlane/actions/sigh.rb
@@ -8,26 +8,29 @@ module Fastlane
     class SighAction
       def self.run(params)
         require 'sigh'
+        require 'sigh/options'
         require 'credentials_manager/appfile_config'
 
-        type = FastlaneCore::DeveloperCenter::APPSTORE
-        type = FastlaneCore::DeveloperCenter::ADHOC if params.include? :adhoc
-        type = FastlaneCore::DeveloperCenter::DEVELOPMENT if params.include? :development
-        force = params.include? :force
+        Sigh.config = FastlaneCore::Configuration.create(Sigh::Options.available_options, (params.first || {}))
+        path = Sigh::DeveloperCenter.new.run
 
-        return type if Helper.test?
 
-        app = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
-        raise 'No app_identifier definied in `./fastlane/Appfile`'.red unless app
 
-        CredentialsManager::PasswordManager.shared_manager(ENV['SIGH_USERNAME']) if ENV['SIGH_USERNAME']
-        path = FastlaneCore::DeveloperCenter.new.run(app, type, nil, force)
-        output_path = File.expand_path(File.join('.', File.basename(path)))
-        FileUtils.mv(path, output_path)
-        Helper.log.info "Exported provisioning profile to '#{output_path}'".green
-        Actions.sh "open '#{output_path}'" unless params.include? :skip_install
+        path = File.expand_path(File.join('.', File.basename(path)))
+        if path
+          if Sigh.config[:filename]
+            file_name = Sigh.config[:filename]
+          else
+            file_name = File.basename(path)
+          end
+          
+          output = File.join(Sigh.config[:output_path].gsub("~", ENV["HOME"]), file_name)
+          (FileUtils.mv(path, output) rescue nil) # in case it already exists
+          system("open -g '#{output}'") unless Sigh.config[:skip_install]
+          puts output.green
+        end
 
-        Actions.lane_context[SharedValues::SIGH_PROFILE_PATH] = output_path # absolute path
+        Actions.lane_context[SharedValues::SIGH_PROFILE_PATH] = path # absolute path
         Actions.lane_context[SharedValues::SIGH_UDID] = ENV["SIGH_UDID"] if ENV["SIGH_UDID"] # The UDID of the new profile
       end
     end
