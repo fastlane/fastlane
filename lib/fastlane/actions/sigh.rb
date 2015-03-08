b1af54bf4c2cd7f5ec4c4ca0e39ce82132051fb7
Felix Krause <KrauseFx@gmail.com>
2015-03-08 22:17:07 +0000

    Switched to using the new sigh profile manager

--- lib/fastlane/actions/sigh.rb
+++ lib/fastlane/actions/sigh.rb
@@ -9,27 +9,12 @@ module Fastlane
       def self.run(params)
         require 'sigh'
         require 'sigh/options'
+        require 'sigh/manager'
         require 'credentials_manager/appfile_config'
 
         Sigh.config = FastlaneCore::Configuration.create(Sigh::Options.available_options, (params.first || {}))
-        path = Sigh::DeveloperCenter.new.run
-
-
-
-        path = File.expand_path(File.join('.', File.basename(path)))
-        if path
-          if Sigh.config[:filename]
-            file_name = Sigh.config[:filename]
-          else
-            file_name = File.basename(path)
-          end
-          
-          output = File.join(Sigh.config[:output_path].gsub("~", ENV["HOME"]), file_name)
-          (FileUtils.mv(path, output) rescue nil) # in case it already exists
-          system("open -g '#{output}'") unless Sigh.config[:skip_install]
-          puts output.green
-          path = output
-        end
+        
+        path = Sigh::Manager.start
 
         Actions.lane_context[SharedValues::SIGH_PROFILE_PATH] = path # absolute path
         Actions.lane_context[SharedValues::SIGH_UDID] = ENV["SIGH_UDID"] if ENV["SIGH_UDID"] # The UDID of the new profile
