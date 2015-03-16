fdacd67a705c5bc8d8b6a85ef230af4de8ad5122
Dan Trenz <dtrenz@gmail.com>
2015-03-16 11:56:22 -0400

    [Resolves #58] Installs profile in Xcode profile dir

--- spec/developer_spec.rb
+++ spec/developer_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe "Create certificates" do 
-  it 'AppStore, Development and Ad Hoc' do
+describe "Create certificates" do
+  regular_apple_id = "felix@sunapps.net"
+  enterprise_apple_id = "felix.krause@sunapps.net"
+
+  before :each do
     system("rm -rf /tmp/fastlane_core/")
+  end
+
+  it 'AppStore, Development and Ad Hoc' do
+    # system("rm -rf /tmp/fastlane_core/")
     # Regular
-    ENV["DELIVER_USER"] = "felix@sunapps.net"
-    
+    ENV["DELIVER_USER"] = regular_apple_id
+
     Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::APPSTORE)
     Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::DEVELOPMENT)
     Sigh::DeveloperCenter.new.run('net.sunapps.7', Sigh::DeveloperCenter::ADHOC)
@@ -17,8 +24,16 @@ describe "Create certificates" do
 
 
     # Enterprise
-    ENV["DELIVER_USER"] = "felix.krause@sunapps.net"
+    ENV["DELIVER_USER"] = enterprise_apple_id
     Sigh::DeveloperCenter.new.run('net.sunapps.*', Sigh::DeveloperCenter::APPSTORE)
     expect(File.exists?(File.join(path, "Distribution_net.sunapps.*.mobileprovision"))).to equal(true)
+  end
+
+  it "Installs the provisioning profile in the Xcode profile path" do
+    # Enterprise
+    ENV["DELIVER_USER"] = enterprise_apple_id
+    install_path = "~/Library/MobileDevice/Provisioning Profiles/#{ENV["SIGH_UDID"]}.mobileprovision"
+    Sigh::DeveloperCenter.new.run('net.sunapps.*', Sigh::DeveloperCenter::APPSTORE)
+    expect(File.exists?(install_path)).to equal(true)
   end
 end
