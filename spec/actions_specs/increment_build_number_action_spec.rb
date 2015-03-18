6f98a3a6a4262284f8ab76acffd6ac387b68cb69
Luka Mirosevic <luka@goonbee.com>
2015-03-18 09:48:58 +0100

    Fixed test

--- spec/actions_specs/increment_build_number_action_spec.rb
+++ spec/actions_specs/increment_build_number_action_spec.rb
@@ -1,21 +1,22 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "Increment Build Number Integration" do
+      require 'shellwords'
 
       it "increments the build number of the Xcode project" do
         Fastlane::FastFile.new.parse("lane :test do 
           increment_build_number
         end").runner.execute(:test)
-        
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq("agvtool next-version -all")
+
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq("cd #{File.expand_path('.').shellescape} && agvtool next-version -all")
       end
 
       it "pass a custom build number to the tool" do
         result = Fastlane::FastFile.new.parse("lane :test do 
           increment_build_number 24
         end").runner.execute(:test)
-        
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq("agvtool new-version -all 24")
+
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq("cd #{File.expand_path('.').shellescape} && agvtool new-version -all 24")
       end
     end
   end
