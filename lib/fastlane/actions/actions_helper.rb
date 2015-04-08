5bb90f95156dacf13e010bd6ce8b80ad9fa6170e
Francis Robichaud <frobichaud@petalmd.com>
2015-04-08 19:36:58 -0400

    Fix #183: only strip out remote when using the push_to_git_remote action

--- lib/fastlane/actions/actions_helper.rb
+++ lib/fastlane/actions/actions_helper.rb
@@ -79,7 +79,7 @@ module Fastlane
 
     # Returns the current git branch - can be replaced using the environment variable `GIT_BRANCH`
     def self.git_branch
-      return ENV['GIT_BRANCH'].gsub /origin\//, '' if ENV['GIT_BRANCH'].to_s.length > 0 # set by Jenkins
+      return ENV['GIT_BRANCH'] if ENV['GIT_BRANCH'].to_s.length > 0 # set by Jenkins
       s = `git rev-parse --abbrev-ref HEAD`
       return s.to_s.strip if s.to_s.length > 0
       nil
