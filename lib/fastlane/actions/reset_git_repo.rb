a5fe905ad9fd16ab81d984155682c0ab23306400
Felix Krause <KrauseFx@gmail.com>
2015-04-08 12:33:47 +0200

    Added :files option for reset_git action to only reset certain files

--- lib/fastlane/actions/reset_git_repo.rb
+++ lib/fastlane/actions/reset_git_repo.rb
@@ -3,10 +3,21 @@ module Fastlane
     # Does a hard reset and clean on the repo
     class ResetGitRepoAction
       def self.run(params)
-        if params.include?(:force) || Actions.lane_context[SharedValues::GIT_REPO_WAS_CLEAN_ON_START]
-          Actions.sh('git reset --hard HEAD')
-          Actions.sh('git clean -qfdx')
-          Helper.log.info 'Git repo was reset and cleaned back to a pristine state.'.green
+        hash = params.first
+        if params.include?(:force) || hash[:force] || Actions.lane_context[SharedValues::GIT_REPO_WAS_CLEAN_ON_START]
+          paths = hash[:files]
+
+          if (paths || []).count == 0
+            Actions.sh('git reset --hard HEAD')
+            Actions.sh('git clean -qfdx')
+            Helper.log.info 'Git repo was reset and cleaned back to a pristine state.'.green
+          else
+            paths.each do |path|
+              Helper.log.warn("Couldn't find file at path '#{path}'") unless File.exists?(path)
+              Actions.sh("git checkout -- '#{path}'")
+            end
+            Helper.log.info "Git cleaned up #{paths.count} files.".green
+          end
         else
           raise 'This is a destructive and potentially dangerous action. To protect from data loss, please add the `ensure_git_status_clean` action to the beginning of your lane, or if you\'re absolutely sure of what you\'re doing then call this action with the :force option.'.red
         end
