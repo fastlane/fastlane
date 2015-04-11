f1150f851820afe02b25f92451333473c189713e
Felix Krause <KrauseFx@gmail.com>
2015-04-11 20:17:12 +0100

    Added documentation for notify action

--- lib/fastlane/actions/notify.rb
+++ lib/fastlane/actions/notify.rb
@@ -7,6 +7,10 @@ module Fastlane
         text = params.join(' ')
         TerminalNotifier.notify(text, title: 'fastlane')
       end
+
+      def self.description
+        "Shows a Mac OS X notification"
+      end
     end
   end
 end
