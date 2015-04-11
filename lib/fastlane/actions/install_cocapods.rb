c49674915607bc87ced5ed515834907fe27b177c
Felix Krause <KrauseFx@gmail.com>
2015-04-11 20:17:17 +0100

    Added documentation for CocoaPods action

--- lib/fastlane/actions/install_cocapods.rb
+++ lib/fastlane/actions/install_cocapods.rb
@@ -4,6 +4,10 @@ module Fastlane
       def self.run(_params)
         Actions.sh('pod install')
       end
+
+      def self.description
+        "Runs `pod install` for the project"
+      end
     end
   end
 end
