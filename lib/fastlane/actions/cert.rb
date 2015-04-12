7a4dc1bce4f2cf6a00613c2a707ef0c2e3de1386
Felix Krause <KrauseFx@gmail.com>
2015-04-12 17:48:06 +0100

    Switced cert update check to async

--- lib/fastlane/actions/cert.rb
+++ lib/fastlane/actions/cert.rb
@@ -12,29 +12,35 @@ module Fastlane
 
         return if Helper.test?
 
-        Dir.chdir(FastlaneFolder.path || Dir.pwd) do
-          # This should be executed in the fastlane folder
+        FastlaneCore::UpdateChecker.start_looking_for_update('cert')
 
-          values = params.first
-          unless values.kind_of?Hash
-            # Old syntax
-            values = {}
-            params.each do |val|
-              values[val] = true
+        begin
+          Dir.chdir(FastlaneFolder.path || Dir.pwd) do
+            # This should be executed in the fastlane folder
+
+            values = params.first
+            unless values.kind_of?Hash
+              # Old syntax
+              values = {}
+              params.each do |val|
+                values[val] = true
+              end
             end
+
+            Cert.config = FastlaneCore::Configuration.create(Cert::Options.available_options, (values || {}))
+
+            Cert::CertRunner.run
+            cert_file_path = ENV["CER_FILE_PATH"]
+            certificate_id = ENV["CER_CERTIFICATE_ID"]
+            Actions.lane_context[SharedValues::CERT_FILE_PATH] = cert_file_path
+            Actions.lane_context[SharedValues::CERT_CERTIFICATE_ID] = certificate_id
+
+            Helper.log.info("Use signing certificate '#{certificate_id}' from now on!".green)
+
+            ENV["SIGH_CERTIFICATE_ID"] = certificate_id
           end
-
-          Cert.config = FastlaneCore::Configuration.create(Cert::Options.available_options, (values || {}))
-
-          Cert::CertRunner.run
-          cert_file_path = ENV["CER_FILE_PATH"]
-          certificate_id = ENV["CER_CERTIFICATE_ID"]
-          Actions.lane_context[SharedValues::CERT_FILE_PATH] = cert_file_path
-          Actions.lane_context[SharedValues::CERT_CERTIFICATE_ID] = certificate_id
-
-          Helper.log.info("Use signing certificate '#{certificate_id}' from now on!".green)
-
-          ENV["SIGH_CERTIFICATE_ID"] = certificate_id
+        ensure
+          FastlaneCore::UpdateChecker.show_update_status('cert', Cert::VERSION)
         end
       end
 
