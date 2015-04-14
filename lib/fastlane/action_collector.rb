b5996161c92f3d0cfa6f220b0ed6411ef4445371
Felix Krause <KrauseFx@gmail.com>
2015-04-14 22:09:55 +0100

    Showing the opt out message only once per machine

--- lib/fastlane/action_collector.rb
+++ lib/fastlane/action_collector.rb
@@ -16,11 +16,14 @@ module Fastlane
     def did_finish
       Thread.new do
         unless ENV["FASTLANE_OPT_OUT_USAGE"]
-          Helper.log.debug("Sending Crash/Success information. More information on: https://github.com/fastlane/enhancer")
-          Helper.log.debug(launches)
-          Helper.log.debug(@error) if @error
-          Helper.log.debug("This information is used to fix failing actions and improve actions that are often used.")
-          Helper.log.debug("You can disable this by adding `opt_out_usage` to your Fastfile")
+          
+          unless did_show_message?
+            Helper.log.debug("Sending Crash/Success information. More information on: https://github.com/fastlane/enhancer")
+            Helper.log.debug(launches)
+            Helper.log.debug(@error) if @error
+            Helper.log.debug("This information is used to fix failing actions and improve integrations that are often used.")
+            Helper.log.debug("You can disable this by adding `opt_out_usage` to your Fastfile")
+          end
 
           require 'excon'
           url = HOST_URL + '/did_launch?'
@@ -36,6 +39,14 @@ module Fastlane
 
     def launches
       @launches ||= {}
+    end
+
+    def did_show_message?
+      path = File.join(File.expand_path('~'), '.did_show_opt_info')
+      
+      did_show = File.exists?path
+      File.write(path, '1')
+      did_show
     end
   end
 end
