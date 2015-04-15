2bb634903cd225338e6d846aa48764b2f78b1618
Felix Krause <KrauseFx@gmail.com>
2015-04-15 20:43:31 +0100

    Added link to fastlane in documentation files

--- lib/fastlane/docs_generator.rb
+++ lib/fastlane/docs_generator.rb
@@ -11,7 +11,11 @@ module Fastlane
         output += "```\n\n"
         output += description + "\n"
       end
-      
+
+      output += "\n\n----\n"
+      output += "More information about fastlane can be found on [https://fastlane.tools](https://fastlane.tools).\n\n"
+      output += "The documentation of fastlane can be found on [GitHub](https://github.com/KrauseFx/fastlane)"
+
       File.write(output_path, output)
       Helper.log.info "Successfully generated documentation to path '#{File.expand_path(output_path)}'".green
     end
