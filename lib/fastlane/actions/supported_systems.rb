b3627dfa62d5f73677d03e47c156ec996bcad9b1
Felix Krause <KrauseFx@gmail.com>
2015-04-17 00:59:40 +0100

    Updated and documented `supported_systems` action

--- lib/fastlane/actions/supported_systems.rb
+++ lib/fastlane/actions/supported_systems.rb
@@ -1,18 +1,22 @@
 module Fastlane
   module Actions
     module SharedValues
-      FASTLANE_OPERATING_SYSTEMS = :FASTLANE_OPERATING_SYSTEMS
+      OPERATING_SYSTEMS = :OPERATING_SYSTEMS
     end
 
-    class SupportedSystemsAction
+    class SupportedSystemsAction < Action
       
       def self.is_supported?(type)
         true
       end
 
+      def self.description
+        "Define a list of operating systems this Fastfile supports"
+      end
+
       def self.run(params)
         raise "Please pass valid parameters to supported_systems".red unless params.kind_of?Array
-        Actions.lane_context[SharedValues::FASTLANE_OPERATING_SYSTEMS] = params
+        Actions.lane_context[SharedValues::OPERATING_SYSTEMS] = params
       end
     end
   end
