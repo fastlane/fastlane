c2513ef9204c49540baf38732ce98b9741272af3
Felix Krause <KrauseFx@gmail.com>
2015-04-18 12:35:04 +0100

    Updated increment_version_number action and adapted tests

--- spec/actions_specs/increment_version_number_action_spec.rb
+++ spec/actions_specs/increment_version_number_action_spec.rb
@@ -13,7 +13,7 @@ describe Fastlane do
 
       it "it increments all targets minor version number" do
         Fastlane::FastFile.new.parse("lane :test do
-          increment_version_number 'minor'
+          increment_version_number(bump_type: 'minor')
         end").runner.execute(:test)
 
         expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
@@ -21,7 +21,7 @@ describe Fastlane do
 
       it "it increments all targets minor version major" do
         Fastlane::FastFile.new.parse("lane :test do
-          increment_version_number 'major'
+          increment_version_number(bump_type: 'major')
         end").runner.execute(:test)
 
         expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 2.0.0/)
@@ -29,10 +29,34 @@ describe Fastlane do
 
       it "pass a custom version number" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          increment_version_number '1.4.3'
+          increment_version_number(version_number: '1.4.3')
         end").runner.execute(:test)
 
         expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.4.3/)
+      end
+
+      it "raises an exception when xcode project path wasn't found" do
+        expect {
+          Fastlane::FastFile.new.parse("lane :test do
+            increment_version_number(xcodeproj: '/nothere')
+          end").runner.execute(:test)
+        }.to raise_error("Could not find Xcode project".red)
+      end
+
+      it "raises an exception when use passes workspace" do
+        expect {
+          Fastlane::FastFile.new.parse("lane :test do
+            increment_version_number(xcodeproj: 'project.xcworkspace')
+          end").runner.execute(:test)
+        }.to raise_error("Please pass the path to the project, not the workspace".red)
+      end
+
+      it "raises an exception if given version number is invalid" do
+        expect {
+          Fastlane::FastFile.new.parse("lane :test do
+            increment_version_number(version_number: '3')
+          end").runner.execute(:test)
+        }.to raise_error("Invalid version '3' given. Must be x.y.z".red)
       end
     end
   end
