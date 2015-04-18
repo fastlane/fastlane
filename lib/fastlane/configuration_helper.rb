bf77ae95160a44168f13ed800dfd5c138e6d22ce
Felix Krause <KrauseFx@gmail.com>
2015-04-18 18:49:04 +0100

    Added legacy mode for unpopular actions

--- lib/fastlane/configuration_helper.rb
+++ lib/fastlane/configuration_helper.rb
@@ -2,7 +2,13 @@ module Fastlane
   class ConfigurationHelper
     def self.parse(action, params)
       begin
-        options = FastlaneCore::Configuration.create(action.available_options, params)
+        if action.available_options.first.kind_of?FastlaneCore::ConfigItem
+          return FastlaneCore::Configuration.create(action.available_options, params)
+        else
+          Helper.log.error "Action '#{action}' uses the old configuration format."
+          raise "Old configuration format".red if Helper.is_test? # only fail in tests - this might be removed in the future, once all actions are migrated
+          return params
+        end
       rescue => ex
         Helper.log.fatal "You provided an option to action #{action.action_name} which is not supported.".red
         Helper.log.fatal "Check out the available options below or run `fastlane action #{action.action_name}`".red
