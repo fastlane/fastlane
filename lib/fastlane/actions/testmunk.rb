9df9fc3961f1da993d7d82be9b4e3f553ecd6f08
Felix Krause <KrauseFx@gmail.com>
2015-04-18 18:43:32 +0100

    Upgraded testmunk action

--- lib/fastlane/actions/testmunk.rb
+++ lib/fastlane/actions/testmunk.rb
@@ -12,20 +12,13 @@
 module Fastlane
   module Actions
     class TestmunkAction < Action
-      def self.run(_params)
-        raise "Please pass your Testmunk email address using `ENV['TESTMUNK_EMAIL'] = 'value'`" unless ENV['TESTMUNK_EMAIL']
-        raise "Please pass your Testmunk API Key using `ENV['TESTMUNK_API'] = 'value'`" unless ENV['TESTMUNK_API']
-        raise "Please pass your Testmunk app name using `ENV['TESTMUNK_APP'] = 'value'`" unless ENV['TESTMUNK_APP']
-
-        ipa_path = ENV['TESTMUNK_IPA'] || ENV[Actions::SharedValues::IPA_OUTPUT_PATH.to_s]
-        raise "Please pass a path to your ipa file using `ENV['TESTMUNK_IPA'] = 'value'`" unless ipa_path
-
+      def self.run(config)
         Helper.log.info 'Testmunk: Uploading the .ipa and starting your tests'.green
 
         response = system("#{"curl -H 'Accept: application/vnd.testmunk.v1+json'" +
-            " -F 'file=@#{ipa_path}' -F 'autoStart=true'" +
-            " -F 'email=#{ENV['TESTMUNK_EMAIL']}'" +
-            " https://#{ENV['TESTMUNK_API']}@api.testmunk.com/apps/#{ENV['TESTMUNK_APP']}/testruns"}")
+            " -F 'file=@#{config[:ipa]}' -F 'autoStart=true'" +
+            " -F 'email=#{config[:email]}'" +
+            " https://#{config[:api]}@api.testmunk.com/apps/#{config[:app]}/testruns"}")
 
         if response
           Helper.log.info 'Your tests are being executed right now. Please wait for the mail with results and decide if you want to continue.'.green
@@ -40,9 +33,30 @@ module Fastlane
 
       def self.available_options
         [
-          ['', 'Your email address', 'TESTMUNK_EMAIL'],
-          ['', 'Testmunk API Key', 'TESTMUNK_API'],
-          ['', 'Testmunk App Name', 'TESTMUNK_APP']
+          FastlaneCore::ConfigItem.new(key: :ipa,
+                                       env_name: "TESTMUNK_IPA",
+                                       description: "Path to IPA",
+                                       verify_block: Proc.new do |value|
+                                        raise "Please pass to existing ipa" unless File.exists?value
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :email,
+                                       env_name: "TESTMUNK_EMAIL",
+                                       description: "Your email address",
+                                       verify_block: Proc.new do |value|
+                                        raise "Please pass your Testmunk email address using `ENV['TESTMUNK_EMAIL'] = 'value'`" unless value
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :api,
+                                       env_name: "TESTMUNK_API",
+                                       description: "Testmunk API Key",
+                                       verify_block: Proc.new do |value|
+                                          raise "Please pass your Testmunk API Key using `ENV['TESTMUNK_API'] = 'value'`" unless value
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :app,
+                                       env_name: "TESTMUNK_APP",
+                                       description: "Testmunk App Name",
+                                       verify_block: Proc.new do |value|
+                                        raise "Please pass your Testmunk app name using `ENV['TESTMUNK_APP'] = 'value'`" unless value
+                                       end),
         ]
       end
 
