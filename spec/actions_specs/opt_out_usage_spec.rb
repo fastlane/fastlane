41cc6e3ac650d9d1bb97b52a7e5b5cb149bea439
Felix Krause <KrauseFx@gmail.com>
2015-04-18 14:27:59 +0100

    Added tests for opt_out_usage action

---
+++ spec/actions_specs/opt_out_usage_spec.rb
@@ -0,0 +1,12 @@
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "Opt Out Usage" do
+      it "works as expected" do
+        Fastlane::FastFile.new.parse("lane :test do 
+          opt_out_usage
+        end").runner.execute(:test)
+        expect(ENV['FASTLANE_OPT_OUT_USAGE']).to eq("YES")
+      end
+    end
+  end
+end
