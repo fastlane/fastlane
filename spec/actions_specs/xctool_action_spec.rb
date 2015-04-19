e9e27245fd007b4657db0b3872f516b6b46a2115
Felix Krause <KrauseFx@gmail.com>
2015-04-19 08:41:30 +0100

    Added new test for xctool action

--- spec/actions_specs/xctool_action_spec.rb
+++ spec/actions_specs/xctool_action_spec.rb
@@ -9,6 +9,20 @@ describe Fastlane do
 
         expect(result).to eq("xctool build test")
       end
+
+      it "works with default setting" do
+        result = Fastlane::FastFile.new.parse('lane :test do 
+          xctool :test, [
+            "--workspace", "\'AwesomeApp.xcworkspace\'",
+            "--scheme", "\'Schema Name\'",
+            "--configuration", "Debug",
+            "--sdk", "iphonesimulator",
+            "--arch", "i386"
+          ].join(" ")
+        end').runner.execute(:test)
+
+        expect(result).to eq("xctool test --workspace 'AwesomeApp.xcworkspace' --scheme 'Schema Name' --configuration Debug --sdk iphonesimulator --arch i386")
+      end
     end
   end
 end
