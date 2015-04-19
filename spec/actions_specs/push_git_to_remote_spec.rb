d0adf9df8acbf70e72449bbf33034ce42fe25810
Felix Krause <KrauseFx@gmail.com>
2015-04-19 08:49:47 +0100

    Migrated push_to_git_remote and added tests

---
+++ spec/actions_specs/push_git_to_remote_spec.rb
@@ -0,0 +1,27 @@
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "Push To Git Remote Action" do
+
+      it "works without passing any options" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_to_git_remote
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push origin HEAD:HEAD --tags")
+      end
+
+      it "works with options specified" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_to_git_remote(
+            remote: 'origin',
+            local_branch: 'develop',
+            remote_branch: 'remote_branch',
+            force: true
+          )
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push origin develop:remote_branch --tags --force")
+      end
+    end
+  end
+end
