10ac4140426b76639ce2e1c6b8bb33bd89f6fa82
Petr Korolev <sky4winder@gmail.com>
2015-04-21 10:15:59 +0300

    Apple ID clarification

--- lib/fastlane/setup.rb
+++ lib/fastlane/setup.rb
@@ -56,7 +56,7 @@ module Fastlane
       Helper.log.info '------------------------------'
       Helper.log.info 'To not re-enter your username and app identifier every time you run one of the fastlane tools or fastlane, these will be stored from now on.'.green
       app_identifier = ask('App Identifier (com.krausefx.app): '.yellow)
-      apple_id = ask('Your Apple ID: '.yellow)
+      apple_id = ask('Your Apple ID (fastlane@krausefx.com): '.yellow)
       template = File.read("#{Helper.gem_path('fastlane')}/lib/assets/AppfileTemplate")
       template.gsub!('[[APP_IDENTIFIER]]', app_identifier)
       template.gsub!('[[APPLE_ID]]', apple_id)
