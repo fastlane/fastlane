d32dda95f4e7fdeb8b9f7f3ecaa1f0c057552778
Felix Krause <KrauseFx@gmail.com>
2015-05-03 20:57:14 +0100

    Added support for multiple platforms in Appfile
    Fixes https://github.com/KrauseFx/fastlane/issues/229

--- spec/app_file_config_spec.rb
+++ spec/app_file_config_spec.rb
@@ -18,6 +18,16 @@ describe CredentialsManager do
       end
     end
 
+    describe "Multiple Platforms" do
+      it "works as expected" do
+        ENV["FASTLANE_LANE_NAME"] = "ios something".to_s
+
+        expect(CredentialsManager::AppfileConfig.new('spec/fixtures/Appfile1').data[:app_identifier]).to eq('platform.com')
+        expect(CredentialsManager::AppfileConfig.new('spec/fixtures/Appfile1').data[:apple_id]).to eq('felix@sunapps.net')
+        expect(CredentialsManager::AppfileConfig.new('spec/fixtures/Appfile1').data[:team_id]).to eq('Q2CBPJ58CC')
+      end
+    end
+
     describe "#load_default_configuration_no_lane_found" do
       it "loads Appfile default values for current driven lane if no override is found" do
         ENV["FASTLANE_LANE_NAME"] = :this_is_not_something_you_find_in_the_app_file.to_s
