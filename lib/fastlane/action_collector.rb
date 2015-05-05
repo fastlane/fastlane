f929778d407026b4d1517b81ef9c9797977b9ef1
Bart Kockx <bkockx@gmail.com>
2015-05-05 08:42:37 +0200

    Bugfix 240 handle bad behavior of action collector
    When the action collector fails to connect or in any other way fails.
    Ignore the errors.
    
    Added timeout to join to prevent "No live threads left Deadlock?" error.

--- lib/fastlane/action_collector.rb
+++ lib/fastlane/action_collector.rb
@@ -16,25 +16,28 @@ module Fastlane
     def did_finish
       Thread.new do
         unless ENV["FASTLANE_OPT_OUT_USAGE"]
-          
-          unless did_show_message?
-            Helper.log.debug("Sending Crash/Success information. More information on: https://github.com/fastlane/enhancer")
-            Helper.log.debug("No personal/sensitive data is sent. Only sharing the following:")
-            Helper.log.debug(launches)
-            Helper.log.debug(@error) if @error
-            Helper.log.debug("This information is used to fix failing actions and improve integrations that are often used.")
-            Helper.log.debug("You can disable this by adding `opt_out_usage` to your Fastfile")
-          end
+          begin
+            unless did_show_message?
+              Helper.log.debug("Sending Crash/Success information. More information on: https://github.com/fastlane/enhancer")
+              Helper.log.debug("No personal/sensitive data is sent. Only sharing the following:")
+              Helper.log.debug(launches)
+              Helper.log.debug(@error) if @error
+              Helper.log.debug("This information is used to fix failing actions and improve integrations that are often used.")
+              Helper.log.debug("You can disable this by adding `opt_out_usage` to your Fastfile")
+            end
 
-          require 'excon'
-          url = HOST_URL + '/did_launch?'
-          url += URI.encode_www_form(
-                  steps: launches.to_json,
-                  error: @error
-                )
+            require 'excon'
+            url = HOST_URL + '/did_launch?'
+            url += URI.encode_www_form(
+                    steps: launches.to_json,
+                    error: @error
+                  )
 
-          unless Helper.is_test? # don't send test data
-            Excon.post(url)
+            unless Helper.is_test? # don't send test data
+              Excon.post(url)
+            end
+          rescue
+            Helper.log.info("Failed sending usage data")
           end
         end
       end
@@ -46,7 +49,7 @@ module Fastlane
 
     def did_show_message?
       path = File.join(File.expand_path('~'), '.did_show_opt_info')
-      
+
       did_show = File.exists?path
       File.write(path, '1')
       did_show
