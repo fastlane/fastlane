aa78206b07e0f03feae3d7428d0b8dadb7790f0f
Felix Krause <KrauseFx@gmail.com>
2015-05-07 18:23:16 +0100

    Fixed template converter and added iPad Mini support

--- lib/frameit/template_finder.rb
+++ lib/frameit/template_finder.rb
@@ -9,15 +9,16 @@ module Frameit
         screenshot.orientation_name,
         screenshot.color
       ]
+      parts << "sRGB" if screenshot.device_name == 'iPad_mini'
 
       templates_path = [ENV['HOME'], FrameConverter::FRAME_PATH].join('/')
       templates = Dir["#{templates_path}/**/#{parts.join('_')}*.png"]
 
       if templates.count == 0
-        if screen_size(path) == Deliver::AppScreenshot::ScreenSize::IOS_35
-          Helper.log.warn "Unfortunately 3.5\" device frames were discontinued. Skipping screen '#{path}'".yellow
+        if screenshot.screen_size == Deliver::AppScreenshot::ScreenSize::IOS_35
+          Helper.log.warn "Unfortunately 3.5\" device frames were discontinued. Skipping screen '#{screenshot.path}'".yellow
         else
-          Helper.log.error "Could not find a valid template for screenshot '#{path}'".red
+          Helper.log.error "Could not find a valid template for screenshot '#{screenshot.path}'".red
           Helper.log.error "You can download new templates from '#{FrameConverter::DOWNLOAD_URL}'"
           Helper.log.error "and store them in '#{templates_path}'"
           Helper.log.error "Missing file: '#{parts.join('_')}.psd'".red
