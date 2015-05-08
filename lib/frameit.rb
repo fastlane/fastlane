51614afa457fa31ec03001d5c3651a34d84f52df
Felix Krause <KrauseFx@gmail.com>
2015-05-08 18:01:48 +0100

    Fixed dependency checking on tests on linux machines

--- lib/frameit.rb
+++ lib/frameit.rb
@@ -13,8 +13,6 @@ require 'fastlane_core'
 
 module Frameit
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
-
-  Frameit::DependencyChecker.check_dependencies
 end
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ require 'frameit'
 
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
+  
 end
