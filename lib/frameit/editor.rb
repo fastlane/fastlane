12c6f733a117cf388782d178071d8479a8c4e25a
Felix Krause <KrauseFx@gmail.com>
2015-05-09 15:59:01 +0100

    Changed the way everythings gets resized resulting in much higher quality
    Refactored code to make it more readable

--- lib/frameit/editor.rb
+++ lib/frameit/editor.rb
@@ -1,27 +1,24 @@
 module Frameit
   class Editor
-    attr_accessor :screenshot
+    attr_accessor :screenshot # reference to the screenshot object to fetch the path, title, etc.
+    attr_accessor :frame # the frame of the device
+    attr_accessor :top_space_above_device
 
     def frame!(screenshot)
-      @screenshot = screenshot
+      self.screenshot = screenshot
 
-      template_path = TemplateFinder.get_template(screenshot)
-      if template_path
-        template = MiniMagick::Image.open(template_path)
+      device_path = TemplateFinder.get_template(screenshot)
+      if device_path
+        self.frame = MiniMagick::Image.open(device_path)
         image = MiniMagick::Image.open(screenshot.path)
-
-        offset_information = Offsets.image_offset(screenshot)
-        raise "Could not find offset_information for '#{screenshot}'" unless (offset_information and offset_information[:width])
-        width = offset_information[:width]
-        image.resize width
-
-        image = template.composite(image, "png") do |c|
-          c.compose "Over"
-          c.geometry offset_information[:offset]
-        end
         
-        if fetch_config['background'] and fetch_config['title'] and fetch_config['keyword']
-          image = add_title(image)
+        if should_add_title?
+          image = complex_framing(image) 
+        else
+          # easy mode from 1.0 - no title or background
+          width = offset[:width]
+          image.resize width # resize the image to fit the frame
+          image = put_into_frame(image) # put it in the frame
         end
 
         output_path = screenshot.path.gsub('.png', '_framed.png').gsub('.PNG', '_framed.png')
@@ -33,49 +30,115 @@ module Frameit
 
 
     private
+      # puts the screenshot into the frame
+      def put_into_frame(content)
+        content = frame.composite(content, "png") do |c|
+          c.compose "Over"
+          c.geometry offset[:offset]
+        end
+        content
+      end
+
+      def offset
+        return @offset_information if @offset_information
+
+        @offset_information = Offsets.image_offset(screenshot)
+        raise "Could not find offset_information for '#{screenshot}'" unless (@offset_information and @offset_information[:width])
+        @offset_information
+      end
+
+      #########################################################################################
+      # Everything below is related to title, background, etc. and is not used in the easy mode
+      #########################################################################################
+
+      # this is used to correct the 1:1 offset information
+      # the offset information is stored to work for the template images
+      # since we resize the template images to have higher quality screenshots
+      # we need to modify the offset information by a certain factor
+      def modify_offset(multiplicator)
+        # Format: "+133+50"
+        hash = offset[:offset]
+        x = hash.split("+")[1].to_f * multiplicator
+        y = hash.split("+")[2].to_f * multiplicator
+        new_offset = "+#{x.round}+#{y.round}"
+        puts new_offset
+        @offset_information[:offset] = new_offset
+      end
+
+      # Do we add a background and title as well?
+      def should_add_title?
+        (fetch_config['background'] and fetch_config['title'] and fetch_config['keyword'])
+      end
+
+      # more complex mode: background, frame and title
+      def complex_framing(image)
+        background = generate_background
+        resize_frame!
+        image = put_into_frame(image)
+
+        # Decrease the size of the framed screenshot to fit into the defined padding + background
+        frame_width = background.width - fetch_config['padding'] * 2
+        image.resize "#{frame_width}x"
+
+        image = put_device_into_background(image, background)
+
+        if fetch_config['title']
+          image = add_title(image)
+        end
+
+        image
+      end
+
+      # Returns a correctly sized background image
+      def generate_background
+        background = MiniMagick::Image.open(fetch_config['background'])
+        if background.height != screenshot.size[1]
+          background.resize "#{screenshot.size[0]}x#{screenshot.size[1]}!" # `!` says it should ignore the ratio
+        end
+        background
+      end
+
+      def put_device_into_background(image, background)
+        left_space = (background.width / 2.0 - image.width / 2.0).round
+        bottom_space = -(image.height / 10).round # to be just a bit below the image bottom
+        self.top_space_above_device = background.height - image.height - bottom_space
+        # require 'pry'; binding.pry
+
+        image = background.composite(image, "png") do |c|
+          c.compose "Over"
+          c.geometry "+#{left_space}+#{top_space_above_device}"
+        end
+      end
+
+      # Resize the frame as it's too low quality by default
+      def resize_frame!
+        multiplicator = (screenshot.size[0].to_f / offset[:width].to_f) # by how much do we have to change this?
+        new_frame_width = multiplicator * frame.width # the new width for the frame
+        frame.resize "#{new_frame_width.round}x" # resize it to the calculated witdth
+        modify_offset(multiplicator) # modify the offset to properly insert the screenshot into the frame later
+      end
+
+      # Add the title above the device
       def add_title(image)
-        # If the user defined a background + title, here we go
-        if fetch_config['background']
-          background = MiniMagick::Image.open(fetch_config['background'])
-          if background.height != screenshot.size[1]
-            background.resize "#{screenshot.size[0]}x#{screenshot.size[1]}!" # `!` says it should ignore the ratio
-          end
+        title_images = build_title_images(image.width)
+        keyword = title_images[:keyword]
+        title = title_images[:title]
 
-          # First off, change the size of `image` to match the background + padding
-          frame_width = background.width - fetch_config['padding'] * 2
-          image.resize "#{frame_width}x"
-
-          left_space = (background.width / 2.0 - image.width / 2.0).round
-          bottom_space = -(image.height / 10).round # to be just a bit below the image bottom
-          device_top = background.height - image.height - bottom_space
-
-          image = background.composite(image, "png") do |c|
+        sum_width = (keyword.width rescue 0) + title.width + keyword_padding
+        top_space = (top_space_above_device / 2.0 - actual_font_size / 2.0).round # centered
+        
+        left_space = (image.width / 2.0 - sum_width / 2.0).round
+        if keyword
+          image = image.composite(keyword, "png") do |c|
             c.compose "Over"
-            c.geometry "+#{left_space}+#{device_top}"
+            c.geometry "+#{left_space}+#{top_space}"
           end
+        end
 
-          if fetch_config['title']
-            title_images = build_title_images(image.width)
-            keyword = title_images[:keyword]
-            title = title_images[:title]
-
-            sum_width = (keyword.width rescue 0) + title.width + keyword_padding
-            top_space = (device_top / 2.0 - actual_font_size / 2.0).round # centered
-            
-            left_space = (image.width / 2.0 - sum_width / 2.0).round
-            if keyword
-              image = image.composite(keyword, "png") do |c|
-                c.compose "Over"
-                c.geometry "+#{left_space}+#{top_space}"
-              end
-            end
-
-            left_space += (keyword.width rescue 0) + keyword_padding
-            image = image.composite(title, "png") do |c|
-              c.compose "Over"
-              c.geometry "+#{left_space}+#{top_space}"
-            end
-          end
+        left_space += (keyword.width rescue 0) + keyword_padding
+        image = image.composite(title, "png") do |c|
+          c.compose "Over"
+          c.geometry "+#{left_space}+#{top_space}"
         end
         image
       end
