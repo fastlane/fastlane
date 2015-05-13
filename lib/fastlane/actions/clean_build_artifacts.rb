18bb7d6e2a0fdaf7bf9e58152cf727b6c8b632af
Felix Krause <KrauseFx@gmail.com>
2015-05-13 10:11:18 +0100

    Added 'exclude_pattern' option to clean_build_artifacts action

--- lib/fastlane/actions/clean_build_artifacts.rb
+++ lib/fastlane/actions/clean_build_artifacts.rb
@@ -1,14 +1,33 @@
 module Fastlane
   module Actions
     class CleanBuildArtifactsAction < Action
-      def self.run(params)
+      def self.run(options)
         [
           Actions.lane_context[Actions::SharedValues::IPA_OUTPUT_PATH],
           Actions.lane_context[Actions::SharedValues::SIGH_PROFILE_PATH],
           Actions.lane_context[Actions::SharedValues::DSYM_OUTPUT_PATH],
-        ].reject { |file| file.nil? || !File.exist?(file) }.each { |file| File.delete(file) }
+        ].reject { |file| file.nil? || !File.exist?(file) }.each do |file| 
+
+          if options[:exclude_pattern]
+            next if file.match(options[:exclude_pattern])
+          end
+
+          Helper.log.debug "Cleaning up '#{file}'".yellow
+          File.delete(file) 
+          
+        end
 
         Helper.log.info 'Cleaned up build artifacts 🐙'.green
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :exclude_pattern,
+                                       env_name: "FL_CLEAN_BUILD_ARTIFACTS_EXCLUDE_PATTERN",
+                                       description: "Exclude all files from clearing that match the given pattern: e.g. '.*\.mobileprovision'",
+                                       default_value: nil,
+                                       optional: true)
+        ]
       end
 
       def self.description
