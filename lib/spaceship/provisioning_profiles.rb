c212980c62341e5502ff787e46272bda741e10c8
Stefan Natchev <stefan@natchev.com>
2015-05-13 14:23:44 -0400

    add comments

--- lib/spaceship/provisioning_profiles.rb
+++ lib/spaceship/provisioning_profiles.rb
@@ -30,6 +30,11 @@ module Spaceship
       end
     end
 
+    ##
+    # Helper method for instantiating structs from API responses
+    # @params attrs [Hash] attributes returned from API to be mapped onto a struct
+    #
+    # @return [Spaceship::ProvisioningProfiles::Profile] subclass of Profile that matches the distribution method of the profile
     def self.factory(attrs)
       values = attrs.values_at('provisioningProfileId', 'UUID', 'dateExpire', 'distributionMethod', 'name', 'status', 'type', 'version', 'proProPlatfrom')
       method = attrs['distributionMethod']
@@ -54,17 +59,34 @@ module Spaceship
       end
     end
 
+    ##
+    # Creates a new provisioning profile
+    #
+    # @param klass [Spaceship::ProvisioningProfiles::Profile] the class of the profile to create. Must be a `Development`, `AppStore`, or `AdHoc`
+    # @param name [String] the name of the provisioning profile
+    # @param bundle_id [String] the bundle id of the app associated with this provisioning profile
+    # @param certificate [Spaceship::Certificates::Certificate] an instance of a certificate used for the provisioning profile. Either `Production` or `Development`
+    #
+    # @return [Spaceship::ProvisioningProfiles::Profile] the newly created provisioning profile
     def create(klass, name, bundle_id, certificate, devices = nil)
       app = Spaceship.apps.find(bundle_id)
       profile = client.create_provisioning_profile(name, klass.type, app.app_id, [certificate.id], devices)
       @profiles << self.class.factory(profile)
     end
 
+    ##
+    # download the provisioning profile associated with a bundle_id
+    #
+    # @param bundle_id [String] the bundle_id of the app associated with the provisioning profile
+    #
+    # @return [String] the contents of the provisioning profile
     def download(bundle_id)
       profile = find_by_bundle_id(bundle_id)
       client.download_provisioning_profile(profile.id)
     end
 
+    ##
+    # Helper method to find a provisioning profile that matches a bundle_id of the associated app
     def find_by_bundle_id(bundle_id)
       @profiles.find do |profile|
         if profile.app.nil?
