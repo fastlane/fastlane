a417347bc41e4e9c46c84ad0159b35780ddad84f
Felix Krause <KrauseFx@gmail.com>
2015-05-15 21:09:05 +0100

    Version bump

--- lib/fastlane_core/version.rb
+++ lib/fastlane_core/version.rb
@@ -1,3 +1,3 @@
 module FastlaneCore
-  VERSION = "0.7.2"
+  VERSION = "0.7.3"
 end
--- spec/languages_spec.rb
+++ spec/languages_spec.rb
@@ -1,7 +1,7 @@
 describe FastlaneCore do
   describe FastlaneCore::Languages do
     it "all languages are available" do
-      expect(FastlaneCore::Languages::ALL_LANGUAGES.count).to be >= 28
+      expect(FastlaneCore::Languages::ALL_LANGUAGES.count).to be >= 27
     end
   end
 end
