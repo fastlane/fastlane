8aeb338280c7aca99b5201ab4d2c233ce403ea5e
Felix Krause <KrauseFx@gmail.com>
2015-05-15 21:08:46 +0100

    Updated the list of available languages

--- lib/fastlane_core/languages.rb
+++ lib/fastlane_core/languages.rb
@@ -1,6 +1,6 @@
 module FastlaneCore
   module Languages
     # These are all the languages which are available to use to upload app metadata and screenshots
-    ALL_LANGUAGES = ["da-DK", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-US", "es-ES", "es-MX", "fi-FI", "fr-CA", "fr-FR", "id-ID", "it-IT", "ja-JP", "ko-KR", "ms-MY", "nl-NL", "no-NO", "pt-BR", "pt-PT", "ru-RU", "sv-SE", "th-TH", "tr-TR", "vi-VI", "cmn-Hans", "zh_CN", "cmn-Hant"]
+    ALL_LANGUAGES = ["da-DK", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-US", "es-ES", "es-MX", "fi-FI", "fr-CA", "fr-FR", "id-ID", "it-IT", "ja-JP", "ko-KR", "ms-MY", "nl-NL", "no-NO", "pt-BR", "pt-PT", "ru-RU", "sv-SE", "th-TH", "tr-TR", "vi-VI", "cmn-Hans", "cmn-Hant"]
   end
 end
