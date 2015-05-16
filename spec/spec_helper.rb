2c4cd1ef1fa7c73bf7706cc8f2152809cef2804c
Felix Krause <KrauseFx@gmail.com>
2015-05-16 17:46:23 +0100

    Fixed coveralls integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
+require 'coveralls'
+Coveralls.wear! unless ENV["FASTLANE_SKIP_UPDATE_CHECK"]
+
 require 'spaceship'
 require 'spaceship_stubbing'
 require 'plist'
 require 'pry'
-
-require 'coveralls'
-Coveralls.wear! unless ENV["FASTLANE_SKIP_UPDATE_CHECK"]
 
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
