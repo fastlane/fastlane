37d431433e390c7b5b502790afb7a6247b585c7e
Felix Krause <KrauseFx@gmail.com>
2015-05-21 11:12:27 +0100

    Improved detection if fastlane is already set up

--- lib/fastlane/fastlane_folder.rb
+++ lib/fastlane/fastlane_folder.rb
@@ -2,6 +2,7 @@ module Fastlane
   class FastlaneFolder
     FOLDER_NAME = 'fastlane'
 
+    # Path to the fastlane folder containing the Fastfile and other configuration files
     def self.path
       return "./#{FOLDER_NAME}/" if File.directory?("./#{FOLDER_NAME}/")
       return "./.#{FOLDER_NAME}/" if File.directory?("./.#{FOLDER_NAME}/") # hidden folder
@@ -10,9 +11,10 @@ module Fastlane
       nil
     end
 
+    # Does a fastlane configuration already exist? 
     def self.setup?
       return false unless path
-      File.exist?(path)
+      File.exist?(File.join(path, "Fastfile"))
     end
 
     def self.create_folder!(path = nil)
