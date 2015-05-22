b68f27feee9f03818bc25e2d8c096fac2f58ba6f
Felix Krause <KrauseFx@gmail.com>
2015-05-22 14:03:49 +0100

    Updated reset_git_repo specs

--- spec/actions_specs/reset_git_repo_spec.rb
+++ spec/actions_specs/reset_git_repo_spec.rb
@@ -2,10 +2,19 @@ describe Fastlane do
   describe Fastlane::FastFile do
     describe "reset_git_repo" do
       it "works as expected inside a Fastfile" do
-        Fastlane::FastFile.new.parse("lane :test do 
-          reset_git_repo :ios
+        paths = Fastlane::FastFile.new.parse("lane :test do 
+          reset_git_repo(force: true, files: ['.'])
         end").runner.execute(:test)
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::DEFAULT_PLATFORM]).to eq(:ios)
+
+        expect(paths).to eq(['.'])
+      end
+
+      it "works as expected inside a Fastfile" do
+        expect {
+          ff = Fastlane::FastFile.new.parse("lane :test do 
+            reset_git_repo
+          end").runner.execute(:test)
+        }.to raise_exception "This is a destructive and potentially dangerous action. To protect from data loss, please add the `ensure_git_status_clean` action to the beginning of your lane, or if you're absolutely sure of what you're doing then call this action with the :force option.".red
       end
     end
   end
