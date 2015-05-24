5527f69daee31d9e33d45d4a9fca66b34f711a5d
Kiran Panesar <kiran@mobilexlabs.com>
2015-05-24 17:09:11 -0500

    Added methods to find a device by the UDID or name. Implemented these checks when creating a device.

--- lib/spaceship/device.rb
+++ lib/spaceship/device.rb
@@ -24,21 +24,36 @@ module Spaceship
         end
       end
 
-      def create!(device_udid, device_name)
+      def find_by_udid(device_udid)
+        all.find do |device|
+          device.udid == device_udid
+        end
+      end
+
+      def find_by_name(device_name)
+        all.find do |device|
+          device.name == device_name
+        end
+      end
+
+      def create!(device_name, device_udid)
 
         # Check whether the user has passed in a UDID and a name
         if not device_udid or not device_name
           raise "You cannot create a device without a device_id (UDID) and device_name"
         end
 
-        # Attempt to find the device if already existing
-        device = self.find(device_udid)
-
-        # If the device is nil, create it
-        if not device
-          device = client.create_device(device_name, device_udid)
-          puts device.body
+        # Find the device by UDID, raise an exception if it already exists
+        if self.find_by_udid(device_udid)
+          raise "The device UDID '#{device_udid}' already exists on this team."
         end
+
+        # Find the device by name, raise an exception if it already exists
+        if self.find_by_name(device_name)
+          raise "The device name '#{device_name}' already exists on this team, use different one."
+        end
+
+        device = client.create_device(device_name, device_udid)
 
         # Update self with the new device
         self.new(device)
