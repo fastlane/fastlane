bea8836c37dbe0a1de31890c1075f3e1a87219bb
KrauseFx <KrauseFx@gmail.com>
2015-05-26 00:11:38 +0100

    Switched to new syntax for shared spaceship

--- lib/spaceship.rb
+++ lib/spaceship.rb
@@ -16,5 +16,27 @@ module Spaceship
     def login(username = nil, password = nil)
       @client = Client.login(username, password)
     end
+
+    # Helper methods for managing multiple instances of spaceship
+
+    # @return (Class) Access the apps for the spaceship
+    def app
+      Spaceship::App.set_client(@client)
+    end
+
+    # @return (Class) Access the devices for the spaceship
+    def device
+      Spaceship::Device.set_client(@client)
+    end
+
+    # @return (Class) Access the certificates for the spaceship
+    def certificate
+      Spaceship::Certificate.set_client(@client)
+    end
+
+    # @return (Class) Access the provisioning profiles for the spaceship
+    def provisioning_profile
+      Spaceship::ProvisioningProfile.set_client(@client)
+    end
   end
 end
