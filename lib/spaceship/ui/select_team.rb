f235e489741eff1805f721e10323f21e021c4f15
KrauseFx <KrauseFx@gmail.com>
2015-05-27 11:08:11 +0100

    Fixed and improved team selection

--- lib/spaceship/ui/select_team.rb
+++ lib/spaceship/ui/select_team.rb
@@ -35,13 +35,27 @@ module Spaceship
         teams = client.teams
 
         raise "Your account is in no teams" if teams.count == 0
+
+        user_team = ENV['FASTLANE_TEAM_ID'].strip
+        if user_team
+          # User provided a value, let's see if it's valid
+          puts "Looking for team with ID '#{user_team}'"
+          teams.each_with_index do |team, i|
+            return user_team if (team['teamId'].strip == user_team)
+          end
+          puts "Couldn't find team with ID '#{user_team}'"
+        end
+
+
         return teams[0]['teamId'] if teams.count == 1 # user is just in one team
 
+
+        # User Selection
         loop do
           # Multiple teams, user has to select
-          puts "Multiple teams found, please enter the number of the team you want to use: ".yellow
+          puts "Multiple teams found, please enter the number of the team you want to use: "
           teams.each_with_index do |team, i|
-            puts "#{i + 1}) #{team['teamId']} #{team['name']} (#{team['type']})".green
+            puts "#{i + 1}) #{team['teamId']} #{team['name']} (#{team['type']})"
           end
 
           selected = $stdin.gets.strip.to_i - 1
