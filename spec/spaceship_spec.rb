958e5b9c75b060301aa2f3ba59649f05f3429077
KrauseFx <KrauseFx@gmail.com>
2015-06-07 14:33:43 +0800

    Added more tests for spaceship class

--- spec/spaceship_spec.rb
+++ spec/spaceship_spec.rb
@@ -7,6 +7,22 @@ describe Spaceship do
     expect(Spaceship.client).to be_instance_of(Spaceship::Client)
   end
 
+  it "Device" do
+    expect(Spaceship.device.all.count).to eq(4)
+  end
+
+  it "Certificate" do
+    expect(Spaceship.certificate.all.count).to eq(3)
+  end
+
+  it "ProvisioningProfile" do
+    expect(Spaceship.provisioning_profile.all.count).to eq(33)
+  end
+
+  it "App" do
+    expect(Spaceship.app.all.count).to eq(5)
+  end
+
   describe Spaceship::Launcher do
     it 'has a client' do
       expect(subject.client).to be_instance_of(Spaceship::Client)
