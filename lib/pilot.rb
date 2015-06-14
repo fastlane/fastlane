0f715e5786ed595258bb76813f8b786ecaeb5654
KrauseFx <KrauseFx@gmail.com>
2015-06-14 13:04:30 +0700

    Added boilerplate

---
+++ lib/pilot.rb
@@ -0,0 +1,8 @@
+require 'json'
+require 'pilot/version'
+
+require 'fastlane_core'
+
+module Pilot
+  Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
+end
---
+++ lib/pilot/version.rb
@@ -0,0 +1,3 @@
+module Pilot
+  VERSION = "0.1.1"
+end
