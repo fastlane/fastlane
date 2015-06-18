64a7b3fdbe442fec5507bc1153d7e441ff0c233a
Simon Manning <simon@ecksd.com>
2015-06-18 10:31:56 +0800

    Mercurial support (ensuring clean status, version bumping, tagging, pushing)

---
+++ lib/fastlane/actions/hg_add_tag.rb
@@ -0,0 +1,34 @@
+module Fastlane
+  module Actions
+    # Adds a hg tag to the current commit
+    class HgAddTagAction < Action
+      def self.run(options)
+        tag = options[:tag]
+
+        Helper.log.info "Adding mercurial tag '#{tag}' 🎯."
+        Actions.sh("hg tag \"#{tag}\"")
+      end
+
+      def self.description
+        "This will add a hg tag to the current branch"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :tag,
+                                       env_name: "FL_HG_TAG_TAG",
+                                       description: "Tag to create",
+                                       optional: false)
+        ]
+      end
+
+      def self.author
+        "sjrmanning"
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
