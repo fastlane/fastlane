091eec71161933dfcb314c625eca6b76b5017ad6
Boris Bügling <boris@icculus.org>
2015-06-25 13:48:51 +0200

    Remove unnecessary `puts`

--- lib/spaceship/client.rb
+++ lib/spaceship/client.rb
@@ -93,7 +93,6 @@ module Spaceship
         else
           # Log to file by default
           path = "/tmp/spaceship.log"
-          puts "Logging spaceship web requests to '#{path}'"
           @logger = Logger.new(path)
         end
 
