7ba9e34b08a7a822b8121971025dca4ff3f695d1
gin0606 <kkgn06@gmail.com>
2015-06-27 13:32:19 +0900

    Add `restore_file` action

---
+++ lib/fastlane/actions/restore_file.rb
@@ -0,0 +1,34 @@
+module Fastlane
+  module Actions
+    class RestoreFileAction < Action
+      def self.run params
+        path = params[:path]
+        backup_path = "#{path}.back"
+        raise "not exist #{backup_path}" unless File.exist? backup_path
+        FileUtils.cp(backup_path, path, {:preserve => true})
+        FileUtils.rm(backup_path)
+      end
+
+      def self.description
+        'This action restore your file that backuped in `backup_file` action.'
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+
+      def self.author
+        "gin0606"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :path,
+                                       env_name: "",
+                                       description: "Original file name you want to restore",
+                                       optional: false),
+        ]
+      end
+    end
+  end
+end
