522263613e8d417dd35775acba5b1f86046157d9
Thiago Lioy <lioy@Thiagos-MacBook-Air-2.local>
2015-06-29 13:58:13 -0300

    refactoring

--- lib/fastlane/actions/lcov.rb
+++ lib/fastlane/actions/lcov.rb
@@ -1,7 +1,7 @@
 module Fastlane
   module Actions
     class LcovAction < Action
-      @@derived_data_path = "#{File.expand_path('~')}/Library/Developer/Xcode/DerivedData/"
+      @@derived_data_path = "#{Dir.home}/Library/Developer/Xcode/DerivedData/"
       @@out_cov_file = "/tmp/coverage.info"
       @@exclude_dirs = ["/Applications/*","/Frameworks/*"]
 
@@ -48,35 +48,43 @@ module Fastlane
       end
 
       private
-      def self.handle_exceptions(options)
-          unless (options[:project_name] rescue nil)
-            Helper.log.fatal "Please add 'ENV[\"PROJECT_NAME\"] = \"a_valid_project_name\"' to your Fastfile's `before_all` section.".red
-            raise 'No PROJECT_NAME given.'.red
-          end
+        def self.handle_exceptions(options)
+            unless (options[:project_name] rescue nil)
+              Helper.log.fatal "Please add 'ENV[\"PROJECT_NAME\"] = \"a_valid_project_name\"' to your Fastfile's `before_all` section.".red
+              raise 'No PROJECT_NAME given.'.red
+            end
 
-          unless (options[:scheme] rescue nil)
-            Helper.log.fatal "Please add 'ENV[\"SCHEME\"] = \"a_valid_scheme\"' to your Fastfile's `before_all` section.".red
-            raise 'No SCHEME given.'.red
-          end
-      end
-
-      def self.gen_cov(options)
-        system("lcov --capture --directory \"#{derived_data_dir(options)}\" --output-file #{@@out_cov_file}")
-        cmd = "lcov "
-        @@exclude_dirs.each do |e|
-          cmd << "--remove #{@@out_cov_file} \"#{e}\" "
+            unless (options[:scheme] rescue nil)
+              Helper.log.fatal "Please add 'ENV[\"SCHEME\"] = \"a_valid_scheme\"' to your Fastfile's `before_all` section.".red
+              raise 'No SCHEME given.'.red
+            end
         end
-        cmd << "--output #{@@out_cov_file} "
-        system(cmd)
-        system("genhtml #{@@out_cov_file} --output-directory #{options[:output_dir]}")
-      end
+
+        def self.gen_cov(options)
+          system("lcov --capture --directory \"#{derived_data_dir(options)}\" --output-file #{@@out_cov_file}")
+          cmd = "lcov "
+          @@exclude_dirs.each do |e|
+            cmd << "--remove #{@@out_cov_file} \"#{e}\" "
+          end
+          cmd << "--output #{@@out_cov_file} "
+          system(cmd)
+          system("genhtml #{@@out_cov_file} --output-directory #{options[:output_dir]}")
+        end
 
 
-      def self.derived_data_dir(options)
-         match = `ls -t #{@@derived_data_path}| grep #{options[:project_name]} | head -1`.to_s.gsub(/\n/, "")
-         derived_data_end_path = "/Build/Intermediates/#{options[:project_name]}.build/Debug-iphonesimulator/#{options[:scheme]}.build/Objects-normal/i386/"
-         "#{@@derived_data_path}#{match}#{derived_data_end_path}"
-      end
+        def self.derived_data_dir(options)
+           pn = options[:project_name]
+           sc = options[:scheme]
+
+           match = find_project_dir(pn)
+
+           derived_data_end_path = "/Build/Intermediates/#{pn}.build/Debug-iphonesimulator/#{sc}.build/Objects-normal/i386/"
+           "#{@@derived_data_path}#{match}#{derived_data_end_path}"
+        end
+
+        def self.find_project_dir(project_name)
+          `ls -t #{@@derived_data_path}| grep #{project_name} | head -1`.to_s.gsub(/\n/, "")
+        end
 
     end
   end
