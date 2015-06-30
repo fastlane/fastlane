cd0f9715b9a4b2b1ea1e7059ff652b749abd00bf
Eric <eric@9degrees.com.ar>
2015-06-30 08:50:29 -0300

    comments

--- lib/sigh/spaceship/runner.rb
+++ lib/sigh/spaceship/runner.rb
@@ -61,6 +61,7 @@ module Sigh
 
     # Create a new profile and return it
     def create_profile!
+      Helper.log.info "Creating profile..."
       cert = certificate_to_use
       bundle_id = Sigh.config[:app_identifier]
       name = Sigh.config[:provisioning_name] || [bundle_id, profile_type.pretty_type].join(' ')
