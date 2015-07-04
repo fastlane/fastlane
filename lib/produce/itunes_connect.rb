e098025c51fe3bace8ce7a059ffc63fab070e61d
Mathias Carignani <mathcarignani@gmail.com>
2015-07-04 00:37:56 -0300

    Added language option

--- lib/produce/itunes_connect.rb
+++ lib/produce/itunes_connect.rb
@@ -15,26 +15,27 @@ module Produce
       else
         Helper.log.info "Creating new app '#{Produce.config[:app_name]}' on iTunes Connect".green
 
-        app_name = Produce.config[:app_name]
-        version = Produce.config[:version]
-        sku = Produce.config[:sku]
-        bundle_id = Produce.config[:bundle_identifier]
-
-        # Invoque spaceship
-        Spaceship::Tunes::Application.create!(name: app_name, 
-                                              version: version, 
-                                              sku: sku, 
-                                              bundle_id: bundle_id)
+        Spaceship::Tunes::Application.create!(name: Produce.config[:app_name], 
+                                              primary_language: Produce.config[:primary_language],
+                                              version: Produce.config[:version], 
+                                              sku: Produce.config[:sku], 
+                                              bundle_id: Produce.config[:bundle_identifier])
 
         raise "Something went wrong when creating the new app - it's not listed in the App's list" unless app_exists?
+
+        Helper.log.info "Successfully created new app '#{Produce.config[:app_name]}' on iTC. Setting up the initial information now.".green
 
         Helper.log.info "Finished creating new app '#{Produce.config[:app_name]}' on iTunes Connect".green
       end
 
-      return true
+      return fetch_app.apple_id
     end
 
     private
+
+      def fetch_app
+        Spaceship::Application.find(Produce.config[:bundle_identifier])
+      end
 
       def app_exists?
         Spaceship::Application.find(Produce.config[:bundle_identifier]) != nil
