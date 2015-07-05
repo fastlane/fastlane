fc14d467170603d8790a40a355faf212d7bb5f94
Mathias Carignani <mathcarignani@gmail.com>
2015-07-04 22:03:17 -0300

    Added option for wildcard bundle

--- lib/produce/itunes_connect.rb
+++ lib/produce/itunes_connect.rb
@@ -4,6 +4,8 @@ module Produce
   class ItunesConnect
     
     def run
+      @full_bundle_identifier = wildcard_bundle? ? Produce.config[:bundle_identifier].gsub('*', Produce.config[:bundle_identifier_suffix].to_s) : Produce.config[:bundle_identifier]
+
       login
       create_new_app
     end
@@ -15,11 +17,15 @@ module Produce
       else
         Helper.log.info "Creating new app '#{Produce.config[:app_name]}' on iTunes Connect".green
 
+        # Hack: Not requiring app_identifier_suffix if not is wildcard
+        Produce.config[:bundle_identifier_suffix] = '' unless wildcard_bundle?
+
         Spaceship::Tunes::Application.create!(name: Produce.config[:app_name], 
                                               primary_language: Produce.config[:primary_language],
                                               version: Produce.config[:version], 
                                               sku: Produce.config[:sku], 
-                                              bundle_id: Produce.config[:bundle_identifier])
+                                              bundle_id: Produce.config[:bundle_identifier], 
+                                              bundle_id_suffix: Produce.config[:bundle_identifier_suffix])
 
         raise "Something went wrong when creating the new app - it's not listed in the App's list" unless app_exists?
 
@@ -34,11 +40,11 @@ module Produce
     private
 
       def fetch_app
-        Spaceship::Application.find(Produce.config[:bundle_identifier])
+        Spaceship::Application.find(@full_bundle_identifier)
       end
 
       def app_exists?
-        Spaceship::Application.find(Produce.config[:bundle_identifier]) != nil
+        Spaceship::Application.find(@full_bundle_identifier) != nil
       end
 
       def wildcard_bundle?
