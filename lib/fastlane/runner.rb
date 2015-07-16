792d56111b31e14bcc45679e768ac63fb56a88cb
Felix Krause <KrauseFx@gmail.com>
2015-07-16 14:04:13 +0200

    Fixed tests

--- lib/fastlane/runner.rb
+++ lib/fastlane/runner.rb
@@ -91,7 +91,7 @@ module Fastlane
         original_full = full_lane_name
         original_lane = current_lane
 
-        raise "Parameters for a lane must always be a hash".red unless parameters.kind_of?Hash
+        raise "Parameters for a lane must always be a hash".red unless (parameters.first || {}).kind_of?Hash
 
         pretty = [new_lane]
         pretty = [current_platform, new_lane] unless platform_nil
