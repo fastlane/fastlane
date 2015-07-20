c2d4125dc987d3ddeae8290edf86f65c576e5dc7
KrauseFx <KrauseFx@gmail.com>
2015-07-20 12:32:55 +0200

    Added waiting and submitting of builds

--- lib/pilot/build_manager.rb
+++ lib/pilot/build_manager.rb
@@ -13,34 +13,49 @@ module Pilot
       if result
         Helper.log.info "Successfully uploaded the new binary to iTunes Connect"
 
-        wait_for_processing_build unless config[:skip_submission]
+        unless config[:skip_submission]
+          upload_date = wait_for_processing_build
+          distribute_build(upload_date)
+
+          Helper.log.info "Successfully distribute build to beta testers 🚀"
+        end
       else
         raise "Error uploading ipa file, more information see above".red
       end
     end
 
-    # This method will takes care of checking for the processing builds every few seconds
-    def wait_for_processing_build
-      loop do
-        Helper.log.info "Waiting for build to be processed by iTunes Connect"
-        sleep 5
-        builds = fetch_processing_builds
-        break if builds == 0
-      end
+    private
+      # This method will takes care of checking for the processing builds every few seconds
+      # @return [Integer] The upload date
+      def wait_for_processing_build
+        # the upload date of the new buid
+        # we use it to identify the build
+        upload_date = nil
+        loop do
+          Helper.log.info "Waiting for iTunes Connect to process the new build"
+          sleep 5
+          builds = app.all_processing_builds
+          break if builds.count == 0
+          upload_date = builds.last.upload_date
+        end
 
-      require 'pry'; binding.pry
-    end
-
-    # @return [Array] A list of all processing builds from all build trains
-    def fetch_processing_builds
-      builds = []
-      app.build_trains.each do |version_number, train|
-        train.processing_builds.each do |build|
-          builds << build
+        if upload_date
+          Helper.log.info "Build successfully processed by iTunes Connect".green
+          return upload_date
+        else
+          raise "Error: Seems like iTunes Connect didn't properly pre-process the binary".red
         end
       end
 
-      return builds
-    end
+      def distribute_build(upload_date)
+        Helper.log.info "Distributing new build to testers"
+        
+        current_build = app.builds.find do |build|
+          build.upload_date == upload_date
+        end
+
+        # First, enable TestFlight beta testing for this train
+        current_build.build_train.update_testing_status!(true)
+      end
   end
 end
