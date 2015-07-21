a1b654f3e5ff453de1912d09047528833a6513dd
KrauseFx <KrauseFx@gmail.com>
2015-07-21 19:38:49 +0200

    Automatic error handling when short option is already taken

--- lib/fastlane_core/configuration/commander_generator.rb
+++ lib/fastlane_core/configuration/commander_generator.rb
@@ -5,10 +5,16 @@ module FastlaneCore
     include Commander::Methods
 
     def generate(options)
+      short_codes = []
       options.each do |option|
         appendix = (option.is_string ? "STRING" : "")
         type = (option.is_string ? String : nil)
         short_option = option.short_option || "-#{option.key.to_s[0]}"
+
+        raise "Short option #{short_option} already taken for key #{option.key}".red if short_codes.include?short_option
+        raise "-v is already used for the version (key #{option.key})".red if short_option == "-v"
+        
+        short_codes << short_option
         global_option short_option, "--#{option.key} #{appendix}", type, (option.description + " (#{option.env_name})")
       end
     end
