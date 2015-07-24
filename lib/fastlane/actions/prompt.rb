5de9688f23c3bc5ccc0725853a5d13d6aad4fcb3
Felix Krause <KrauseFx@gmail.com>
2015-07-24 17:05:00 +0200

    Added multi-line support for prompt action

--- lib/fastlane/actions/prompt.rb
+++ lib/fastlane/actions/prompt.rb
@@ -5,11 +5,18 @@ module Fastlane
         params[:text] += " (y/n)" if params[:boolean]
         Helper.log.info params[:text]
 
-        user_input = params[:ci_input] if Helper.is_ci?
-        user_input ||= STDIN.gets.chomp.strip
+        return params[:ci_input] if Helper.is_ci?
+
+        unless params[:multi_line_end_keyword]
+          # Standard one line input
+          user_input = STDIN.gets.chomp.strip
+        else
+          # Multi line
+          end_tag = paramsp[:multi_line_end_keyword]
+          user_input = STDIN.gets(end_tag).chomp.gsub(end_tag, "").strip
+        end
 
         user_input = (user_input.downcase == 'y') if params[:boolean]
-
         return user_input
       end
 
@@ -24,21 +31,27 @@ module Fastlane
       def self.details
         [
           "You can use `prompt` to ask the user for a value or to just let the user confirm the next step",
-          "When this is executed on a CI service, the passed `ci_input` value will be returned"
+          "When this is executed on a CI service, the passed `ci_input` value will be returned",
+          "This action also supports multi-line inputs using the `multi_line_end_keyword` option."
         ].join("\n")
       end
 
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(key: :text,
-                                       description: "The text that will be displayed to the user"),
+                                       description: "The text that will be displayed to the user",
+                                       default_value: "Please enter a text: "),
           FastlaneCore::ConfigItem.new(key: :ci_input,
                                        description: "The default text that will be used when being executed on a CI service",
                                        default_value: ''),
           FastlaneCore::ConfigItem.new(key: :boolean,
                                        description: "Is that a boolean question (yes/no)? This will add (y/n) at the end",
                                        default_value: false,
-                                       is_string: false)
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :multi_line_end_keyword,
+                                       description: "Enable multi-line inputs by providing an end text (e.g. 'END') which will stop the user input",
+                                       optional: true,
+                                       is_string: true),
         ]
       end
 
