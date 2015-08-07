d5ea66bfda9f774fc1ede772fb321864c4d4cf08
Henri Normak <henri.normak@gmail.com>
2015-08-07 09:57:42 +0300

    Slightly tweak the service commands and their output

--- lib/produce/service.rb
+++ lib/produce/service.rb
@@ -1,5 +1,13 @@
 module Produce
   class Service
+    def self.enable(options, args)
+      self.new.enable(options, args)
+    end
+
+    def self.disable(options, args)
+      self.new.disable(options, args)
+    end
+
     def enable(options, args)
       Helper.log.info "Starting login"
       Spaceship.login(Produce.config[:username], nil)
@@ -16,13 +24,17 @@ module Produce
       Helper.log.info "[DevCenter] App found '#{app.name}'".green
       Helper.log.info "Proceeding with enabling services"
 
+      enabled = 0
+
       if options.app_group
         app.update_service(Spaceship.app_service.app_group.on)
+        enabled += 1
         Helper.log.info "Enabled App Groups"
       end
 
       if options.associated_domains
         app.update_service(Spaceship.app_service.associated_domains.on)
+        enabled += 1
         Helper.log.info "Enable Associated Domains"
       end
 
@@ -30,28 +42,34 @@ module Produce
         case options.data_protection
           when "complete"
             app.update_service(Spaceship.app_service.data_protection.complete)
+            enabled += 1
             Helper.log.info "Enabled Data Protection: Complete"
           when "unlessopen"
             app.update_service(Spaceship.app_service.data_proection.unless_open)
+            enabled += 1
             Helper.log.info "Enabled Data Protection: Unless Open"
           when "untilfirstauth"
             app.update_service(Spaceship.app_service.data_protection.until_first_auth)
+            enabled += 1
             Helper.log.info "Enabled Data Protection: Until First Authentication"
         end
       end
 
       if options.healthkit
         app.update_service(Spaceship.app_service.health_kit.on)
+        enabled += 1
         Helper.log.info "Enabled HealthKit"
       end
 
       if options.homekit
         app.update_service(Spaceship.app_service.home_kit.on)
+        enabled += 1
         Helper.log.info "Enabled HomeKit"
       end
 
       if options.wireless_conf
         app.update_service(Spaceship.app_service.wireless_accessory.on)
+        enabled += 1
         Helper.log.info "Enable Wireless Accessory Configuration"
       end
 
@@ -60,33 +78,41 @@ module Produce
         when "legacy"
           app.update_service(Spaceship.app_service.icloud.on)
           app.update_service(Spaceship.app_service.cloud_kit.xcode5_compatible)
+          enabled += 1
           Helper.log.info "Enabled iCloud: CloudKit"
         when "cloudkit"
           app.update_service(Spaceship.app_service.icloud.on)
           app.update_service(Spaceship.app_service.cloud_kit.cloud_kit)
+          enabled += 1
           Helper.log.info "Enabled iCloud: CloudKit"
         end
       end
 
       if options.inter_app_audio
         app.update_service(Spaceship.app_service.inter_app_audio.on)
+        enabled += 1
         Helper.log.info "Enabled Inter-App Audio"
       end
 
       if options.passbook
         app.update_service(Spaceship.app_service.passbook.on)
+        enabled += 1
         Helper.log.info "Enabled Passbook"
       end
 
       if options.push_notification
         app.update_service(Spaceship.app_service.push_notification.on)
+        enabled += 1
         Helper.log.info "Enabled Push Notifications"
       end
 
       if options.vpn_conf
         app.update_service(Spaceship.app_service.vpn_configuration.on)
+        enabled += 1
         Helper.log.info "Enabled VPN Configuration"
       end
+
+      Helper.log.info "Done! Enabled #{enabled} services.".green
     end
 
     def disable(options, args)
@@ -105,60 +131,75 @@ module Produce
       Helper.log.info "[DevCenter] App found '#{app.name}'".green
       Helper.log.info "Proceeding with disabling services"
 
+      disabled = 0
+
       if options.app_group
         app.update_service(Spaceship.app_service.app_group.off)
+        disabled += 1
         Helper.log.info "Disabled App Groups"
       end
 
       if options.associated_domains
         app.update_service(Spaceship.app_service.associated_domains.off)
+        disabled += 1
         Helper.log.info "Disabled Associated Domains"
       end
 
       if options.data_protection
         app.update_service(Spaceship.app_service.data_protection.off)
+        disabled += 1
         Helper.log.info "Disabled Data Protection"
       end
 
       if options.healthkit
         app.update_service(Spaceship.app_service.health_kit.off)
+        disabled += 1
         Helper.log.info "Disabled HealthKit"
       end
 
       if options.homekit
         app.update_service(Spaceship.app_service.home_kit.off)
+        disabled += 1
         Helper.log.info "Disabled HomeKit"
       end
 
       if options.wireless_conf
         app.update_service(Spaceship.app_service.wireless_accessory.off)
+        disabled += 1
         Helper.log.info "Disabled Wireless Accessory Configuration"
       end
 
       if options.icloud
         app.update_service(Spaceship.app_service.icloud.off)
+        disabled += 1
         Helper.log.info "Disabled iCloud"
       end
 
       if options.inter_app_audio
         app.update_service(Spaceship.app_service.inter_app_audio.off)
+        disabled += 1
         Helper.log.info "Disabled Inter-App Audio"
       end
 
       if options.passbook
         app.update_service(Spaceship.app_service.passbook.off)
+        disabled += 1
         Helper.log.info "Disabled Passbook"
       end
 
       if options.push_notification
         app.update_service(Spaceship.app_service.push_notification.off)
+        disabled += 1
         Helper.log.info "Disabled Push Notifications"
       end
 
       if options.vpn_conf
         app.update_service(Spaceship.app_service.vpn_configuration.off)
+        disabled += 1
         Helper.log.info "Disabled VPN Configuration"
       end
+
+      Helper.log.info "Done! Disabled #{disabled} services.".green
     end
   end
 end
