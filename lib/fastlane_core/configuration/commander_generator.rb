0324730368c060786ef4979ae0f4d200eecce9cc
KrauseFx <KrauseFx@gmail.com>
2015-08-11 16:33:39 +0200

    Added verification that -h is not taken

--- lib/fastlane_core/configuration/commander_generator.rb
+++ lib/fastlane_core/configuration/commander_generator.rb
@@ -13,6 +13,7 @@ module FastlaneCore
 
         raise "Short option #{short_option} already taken for key #{option.key}".red if short_codes.include?short_option
         raise "-v is already used for the version (key #{option.key})".red if short_option == "-v"
+        raise "-h is already used for the help screen (key #{option.key})".red if short_option == "-h"
         
         short_codes << short_option
         global_option short_option, "--#{option.key} #{appendix}", type, (option.description + " (#{option.env_name})")
