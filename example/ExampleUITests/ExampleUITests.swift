43fc969e08c2e0df58ae1e98cdadcad444e5b970
KrauseFx <KrauseFx@gmail.com>
2015-08-17 22:40:31 +0200

    Updated swift tests

--- example/ExampleUITests/ExampleUITests.swift
+++ example/ExampleUITests/ExampleUITests.swift
@@ -14,12 +14,17 @@ class ExampleUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
+        let app = XCUIApplication()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
+//        app.launchArguments = ["AppleLanguages '(de-DE})'", "AppleLocale 'de-DE'"]
+        app.launchArguments = ["-AppleLanguages \"(de)\""]
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+
+        app.launch()
     }
     
     override func tearDown() {
