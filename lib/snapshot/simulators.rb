440593159a81219e7e56c7c4b75336b27ca82c73
KrauseFx <KrauseFx@gmail.com>
2015-08-17 22:44:48 +0200

    Updated simulators.rb to work with the new beta version of Xcode

--- lib/snapshot/simulators.rb
+++ lib/snapshot/simulators.rb
@@ -1,14 +1,29 @@
+require 'open3'
+
 module Snapshot
   class Simulators
-    def self.available_devices
-      if not @result
-        @result = []
-        `instruments -s`.split("\n").each do |current|
-          # Example: "iPhone 5 (8.1 Simulator) [C49ECC4A-5A3D-44B6-B9BF-4E25BC326400]"
-          @result << current.split(' [').first if current.include?"Simulator"
+    def self.available_devices(name_only = false)
+      @cached ||= {}
+      return @cached[name_only] if @cached[name_only]
+
+      Helper.log.info "Fetching available devices" if $verbose
+      result = []
+      # we do it using open since ` just randomly hangs with instruments -s
+      output = ''
+      Open3.popen3('instruments -s') do |stdin, stdout, stderr, wait_thr|
+        output = stdout.read
+      end
+
+      output.split("\n").each do |current|
+        # Example: "iPhone 5 (8.1 Simulator) [C49ECC4A-5A3D-44B6-B9BF-4E25BC326400]"
+        if name_only
+          result << current.split(' (').first if current.include?"Simulator"
+        else
+          result << current.split(' [').first if current.include?"Simulator"
         end
       end
-      return @result
+      @cached[name_only] = result
+      return result
     end
   end
 end
