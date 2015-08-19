986c2903a3fe61b41a75fabb1ed299a929cbb844
KrauseFx <KrauseFx@gmail.com>
2015-08-19 10:27:37 +0200

    Added WatchKit example

---
+++ examples/WatchKit/WatchKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchKit
+//
+//  Created by Felix Krause on 19/08/15.
+//  Copyright © 2015 Felix Krause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
