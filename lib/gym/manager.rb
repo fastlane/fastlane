7f8f2034d0e5f0b856f7ecf034a77803156dcae9
KrauseFx <KrauseFx@gmail.com>
2015-08-20 12:37:58 +0200

    Improved code style

--- lib/gym/manager.rb
+++ lib/gym/manager.rb
@@ -17,7 +17,7 @@ module Gym
       rows << ["Workspace", config[:workspace]] if config[:workspace]
       rows << ["Scheme", config[:scheme]] if config[:scheme]
       rows << ["Configuration", config[:configuration]] if config[:configuration]
-      rows << ["Platform", Gym.project.ios?? "iOS" : "Mac"]
+      rows << ["Platform", Gym.project.ios? ? "iOS" : "Mac"]
       rows << ["Xcode Path", Gym.xcode_path.gsub("/Contents/Developer", "")]
 
       puts ""
