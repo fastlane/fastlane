0ae975ceebf7870f0501d7489ea6a45838f0c7fb
Felix Krause <KrauseFx@gmail.com>
2015-08-20 13:22:06 +0200

    Improved code style

--- lib/fastlane/actions/import_from_git.rb
+++ lib/fastlane/actions/import_from_git.rb
@@ -24,18 +24,12 @@ module Fastlane
         [
           FastlaneCore::ConfigItem.new(key: :url,
                                        description: "The url of the repository to import the Fastfile from",
-                                       default_value: nil
-                                       ),
+                                       default_value: nil)
           FastlaneCore::ConfigItem.new(key: :path,
                                        description: "The path of the Fastfile in the repository",
                                        default_value: 'fastlane/Fastfile',
-                                       optional: true,
-                                       ),
+                                       optional: true)
       ]
-      end
-
-      def self.output
-        []
       end
 
       def self.authors
