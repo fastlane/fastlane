72582821abb7d09c09142b770cc799fed94d5bd0
Masashi Beheim <github@beheim.eu>
2015-08-21 15:58:45 +0200

    Test to make sure the path is shell escaped.

--- spec/package_command_generator_spec.rb
+++ spec/package_command_generator_spec.rb
@@ -13,5 +13,22 @@ describe Gym do
         ""
       ])
     end
+
+    it "works with the example project with no additional parameters and an apostrophe/single quote in the product name" do
+      options = { project: "./examples/standard/Example.xcodeproj" }
+      Gym.config = FastlaneCore::Configuration.create(Gym::Options.available_options, options)
+
+      allow(Gym::PackageCommandGenerator).to receive(:appfile_path).and_return("Krause's App")
+
+      result = Gym::PackageCommandGenerator.generate
+      expect(result).to eq([
+        "/usr/bin/xcrun /tmp/PackageApplication4Gym -v",
+        Shellwords.escape("Krause's App"),
+        "-o '#{Gym::PackageCommandGenerator.ipa_path}'",
+        "exportFormat ipa",
+        ""
+      ])
+    end
+
   end
 end
