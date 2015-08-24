82cb79817a5b882ea1ddf62895c3f700003b3d52
okohtenko <kohtenko@gmail.com>
2015-08-24 15:25:35 +0300

    get_info_plist_value and set_info_plist_value actions

---
+++ lib/fastlane/actions/get_info_plist_value.rb
@@ -0,0 +1,53 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      GET_INFO_PLIST_VALUE_CUSTOM_VALUE = :GET_INFO_PLIST_VALUE_CUSTOM_VALUE
+    end
+
+    class GetInfoPlistValueAction < Action
+      def self.run(params)
+      require "plist"
+      begin
+	     path = File.expand_path(params[:plist_path])
+	     plist = Plist::parse_xml(path)
+ 	     Actions.lane_context[SharedValues::INFO_PLIST_VALUE] = plist[params[:param_name]]
+ 	     rescue
+	     	Helper.log.info "Unable to find plist file at #{path}".red
+ 	     end
+      end
+
+      def self.description
+        "returns value from Info.plist of your project as native Ruby data structures"
+      end
+
+      def self.available_options
+        [
+        
+		  FastlaneCore::ConfigItem.new(key: :param_name,
+                                       env_name: "FL_GET_INFO_PLIST_PARAM_NAME",
+                                       description: "Name of parameter",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :plist_path,
+                                       env_name: "FL_GET_INFO_PLIST_PATH",
+                                       description: "Path to Info.plist",
+                                       optional: false)
+
+        ]
+      end
+
+      def self.output
+        [
+          ['GET_INFO_PLIST_VALUE_CUSTOM_VALUE', 'A description of what this value contains']
+        ]
+      end
+
+      def self.authors
+        ["kohtenko"]
+      end
+
+      def self.is_supported?(platform)
+         platform == :ios
+      end
+    end
+  end
+end
---
+++ lib/fastlane/actions/set_info_plist_value.rb
@@ -0,0 +1,58 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      SET_INFO_PLIST_VALUE_CUSTOM_VALUE = :SET_INFO_PLIST_VALUE_CUSTOM_VALUE
+    end
+
+    class SetInfoPlistValueAction < Action
+      def self.run(params)
+      require "plist"
+       begin 
+         path = File.expand_path(params[:plist_path])
+	     plist = Plist::parse_xml(path)
+	     plist[params[:param_name]] = params[:param_value]
+	     new_plist = plist.to_plist
+	     File.open(path, 'w') { |file| file.write(new_plist) }
+	     
+ 	     rescue
+	     	Helper.log.info "Unable to set value to plist file at #{path}".red
+ 	     end
+	  end
+
+      def self.description
+        "Sets value to Info.plist of your project as native Ruby data structures"
+      end
+
+      def self.available_options
+
+        [
+ 		  FastlaneCore::ConfigItem.new(key: :param_name,
+                                       env_name: "FL_SET_INFO_PLIST_PARAM_NAME",
+                                       description: "Name of parameter",
+                                       optional: false),
+ 		  FastlaneCore::ConfigItem.new(key: :param_value,
+                                       env_name: "FL_SET_INFO_PLIST_PARAM_VALUE",
+                                       description: "Name of parameter",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :plist_path,
+                                       env_name: "FL_SET_INFO_PLIST_PATH",
+                                       description: "Path to Info.plist",
+                                       optional: false)
+
+        ]
+      end
+
+      def self.output
+        [ ]
+      end
+
+      def self.authors
+        ["kohtenko"]
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+      end
+    end
+  end
+end
