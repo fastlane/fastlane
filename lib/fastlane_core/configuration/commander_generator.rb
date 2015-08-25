596cd7d5eb9f1a9ee754e05afead874f096ceb02
KrauseFx <KrauseFx@gmail.com>
2015-08-25 09:14:34 +0200

    Revert "Should we prevent -t reuse?"
    This reverts commit 54b02a4da0ff10c58c776b3b99a699ceb816633b.

--- lib/fastlane_core/configuration/commander_generator.rb
+++ lib/fastlane_core/configuration/commander_generator.rb
@@ -12,12 +12,10 @@ module FastlaneCore
         type = (option.is_string ? String : nil)
         short_option = option.short_option || "-#{option.key.to_s[0]}"
 
-        # https://github.com/commander-rb/commander/blob/master/lib/commander/runner.rb
         raise "Short option #{short_option} already taken for key #{option.key}".red if short_codes.include?short_option
         raise "-v is already used for the version (key #{option.key})".red if short_option == "-v"
         raise "-h is already used for the help screen (key #{option.key})".red if short_option == "-h"
-        raise "-t is already used for the trace mode (key #{option.key})".red if short_option == "-t"
-
+        
         short_codes << short_option
 
         # Example Call
