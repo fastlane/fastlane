d1e12500034fd834a687a23defc7592e902f44ba
KrauseFx <KrauseFx@gmail.com>
2015-08-25 09:29:13 +0200

    Added detection of -t trace conflict

--- lib/fastlane_core/configuration/commander_generator.rb
+++ lib/fastlane_core/configuration/commander_generator.rb
@@ -15,6 +15,7 @@ module FastlaneCore
         raise "Short option #{short_option} already taken for key #{option.key}".red if short_codes.include?short_option
         raise "-v is already used for the version (key #{option.key})".red if short_option == "-v"
         raise "-h is already used for the help screen (key #{option.key})".red if short_option == "-h"
+        raise "-t is already used for the trace screen (key #{option.key})".red if short_option == "-t"
         
         short_codes << short_option
 
