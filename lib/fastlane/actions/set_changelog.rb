23b4160263c3e1a975d9a83d42e872bb4d11a40f
Felix Krause <KrauseFx@gmail.com>
2015-09-07 13:13:03 -0700

    Updated to the new AppfileConfig

--- lib/fastlane/actions/set_changelog.rb
+++ lib/fastlane/actions/set_changelog.rb
@@ -84,10 +84,7 @@ module Fastlane
                                      short_option: "-u",
                                      env_name: "FASTLANE_USERNAME",
                                      description: "Your Apple ID Username",
-                                     default_value: ENV["DELIVER_USER"] || CredentialsManager::AppfileConfig.try_fetch_value(:apple_id),
-                                     verify_block: proc do |value|
-                                       CredentialsManager::PasswordManager.shared_manager(value)
-                                     end),
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)),
           FastlaneCore::ConfigItem.new(key: :version,
                                        env_name: "FL_SET_CHANGELOG_VERSION",
                                        description: "The version number to create/update",
