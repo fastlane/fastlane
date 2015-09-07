499e52e60057ee3f6f0c8facee19b7a54a273084
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-07 21:39:35 +0300

    Fixed Travis CI tests issue

--- spec/actions_specs/unlock_keychain_spec.rb
+++ spec/actions_specs/unlock_keychain_spec.rb
@@ -3,18 +3,18 @@ describe Fastlane do
     describe "Unlock keychain Integration" do
 
       it "works with path and password and existing keychain" do
-        expanded_path = File.expand_path("~/Library/Keychains/login.keychain")
+        keychain_path = Tempfile.new('foo').path
 
         result = Fastlane::FastFile.new.parse("lane :test do
           unlock_keychain ({
-            path: '#{expanded_path}',
+            path: '#{keychain_path}',
             password: 'testpassword',
           })
         end").runner.execute(:test)
 
         expect(result.size).to eq 2
-        expect(result[0]).to eq "security unlock-keychain -p testpassword #{expanded_path}"
-        expect(result[1]).to eq "security set-keychain-settings #{expanded_path}"
+        expect(result[0]).to eq "security unlock-keychain -p testpassword #{keychain_path}"
+        expect(result[1]).to eq "security set-keychain-settings #{keychain_path}"
       end
     end
   end
