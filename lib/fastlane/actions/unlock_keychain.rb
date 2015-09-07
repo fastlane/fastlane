d76aa815cdef18582b2f7f6753c5027e1ce801d9
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-07 18:22:53 +0300

    Added unlock_keychain action

---
+++ lib/fastlane/actions/unlock_keychain.rb
@@ -0,0 +1,98 @@
+module Fastlane
+  module Actions
+    class UnlockKeychainAction < Action
+      def self.run(params)
+        @keychain_path = params[:path]
+        add_to_search_list = params[:add_to_search_list]
+
+        if !keychainfile_exists?
+          raise "Could not find the keychain file: #{@keychain_path}".red
+        end
+
+        # add to search list if not already added
+        if add_to_search_list
+          add_keychain_to_search_list
+        end
+
+        escaped_path = @keychain_path.shellescape
+        escaped_password = params[:password].shellescape
+
+        commands = []
+        # unlock given keychain and disable lock and timeout
+        commands << Fastlane::Actions.sh("security unlock-keychain -p #{escaped_password} #{escaped_path}", log: false)
+        commands << Fastlane::Actions.sh("security set-keychain-settings #{escaped_path}", log: false)
+        commands
+      end
+
+      def self.add_keychain_to_search_list
+        escaped_path = @keychain_path.shellescape
+
+        result = Fastlane::Actions.sh("security list-keychains", log: false)
+
+        # add the keychain to the keychains list
+        # the basic strategy is to open the keychain file it with Keychain Access
+        if !result.include?(@keychain_path)
+          commands = []
+          commands << Fastlane::Actions.sh("open #{escaped_path}")
+          commands
+        end
+      end
+
+      def self.keychainfile_exists?
+        possible_locations = []
+        possible_locations << @keychain_path
+        possible_locations << "~/Library/Keychains/#{@keychain_path}"
+        possible_locations << "~/Library/Keychains/#{@keychain_path}.keychain"
+
+        for location in possible_locations
+          expaded_location = File.expand_path(location)
+          if File.exist?(expaded_location)
+            @keychain_path = expaded_location
+            return true
+          end
+        end
+
+        return false
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Unlock a keychain"
+      end
+
+      def self.details
+        "Unlocks the give keychain file and it adds it to the keychain search list."
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :path,
+                                       env_name: "FL_UNLOCK_KEYCHAIN_PATH", # The name of the environment variable
+                                       description: "Path to the Keychain file", # a short description of this parameter
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: "FL_UNLOCK_KEYCHAIN_PASSWORD",
+                                       description: "Keychain password",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
+                                       env_name: "FL_UNLOCK_KEYCHAIN_ADD_TO_SEARCH_LIST",
+                                       description: "Add to keychain search list",
+                                       is_string: false, # true: verifies the input is a string, false: every kind of value
+                                       default_value: true) # the default value if the user didn't provide one
+
+        ]
+      end
+
+      def self.authors
+        ["xfreebird"]
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+      end
+    end
+  end
+end
