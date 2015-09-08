83a0cc59fdc7d323f6fc5f02be734a384df4c01d
KrauseFx <KrauseFx@gmail.com>
2015-09-07 17:53:15 -0700

    Improved Ruby code style

--- lib/snapshot/latest_ios_version.rb
+++ lib/snapshot/latest_ios_version.rb
@@ -1,15 +1,15 @@
 module Snapshot
   class LatestIosVersion
-    @@version = nil
+    @version = nil
     def self.version
       return ENV["SNAPSHOT_IOS_VERSION"] if ENV["SNAPSHOT_IOS_VERSION"]
-      return @@version if @@version
+      return @version if @version
 
       output = `xcodebuild -version -sdk`.split("Mac").last # don't care about the Mac Part
       matched = output.match(/iPhoneSimulator([\d\.]+)\.sdk/)
       
       if matched.length > 1
-        return @@version ||= matched[1]
+        return @version ||= matched[1]
       else
         raise "Could not determine installed iOS SDK version. Please pass it via the environment variable 'SNAPSHOT_IOS_VERSION'".red
       end
