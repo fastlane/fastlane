ea953789b353ffceb538f63de8afa0d4432ef73f
Felix Krause <krausefx@gmail.com>
2015-09-08 16:36:07 -0700

    Improved design of docuemntation of increment_version_number

--- lib/fastlane/actions/increment_version_number.rb
+++ lib/fastlane/actions/increment_version_number.rb
@@ -83,7 +83,7 @@ module Fastlane
           "This action will increment the version number. ",
           "You first have to set up your Xcode project, if you haven't done it already:",
           "https://developer.apple.com/library/ios/qa/qa1827/_index.html"
-        ].join(' ')
+        ].join("\n")
       end
 
       def self.available_options
