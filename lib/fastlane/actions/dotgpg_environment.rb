e8b20d6fb50b7540c441c7046424f9068a6bd300
Simon Levy <simon@vouch.com>
2015-09-11 11:12:11 -0700

    Adding dotgpg environment action
    Action to support loading dotgpg encrypted variables into the ruby ENV.
    This has two gem dependencies: ‘dotgpg’ and ‘dotgpg-environment’

---
+++ lib/fastlane/actions/dotgpg_environment.rb
@@ -0,0 +1,46 @@
+module Fastlane
+  module Actions
+    module SharedValues
+    end
+
+    class DotgpgEnvironmentAction < Action
+      def self.run(options)
+        require 'dotgpg/environment'
+        
+        if options[:dotgpg_file]
+          dotgpg_file = options[:dotgpg_file]
+        end
+
+        raise "Dotgpg file '#{File.expand_path(dotgpg_file)}' not found".red if dotgpg_file && !File.exist?(dotgpg_file)
+
+        Helper.log.info "Reading secrets from #{dotgpg_file}"
+        Dotgpg::Environment.new(dotgpg_file).apply
+      end
+
+      def self.description
+        "Reads in production secrets set in a dotgpg file and puts them in ENV. More information at https://github.com/ConradIrwin/dotgpg"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :dotgpg_file,
+                                       env_name: "DOTGPG_FILE",
+                                       description: "Path to your DSYM file",
+                                       default_value: "dotgpg/secrets.gpg",
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                          # validation is done in the action
+                                       end)
+        ]
+      end
+
+      def self.authors
+        ["simonlevy5"]
+      end
+
+      def self.is_supported?(platform)
+        [:ios, :mac].include? platform
+      end
+    end
+  end
+end
