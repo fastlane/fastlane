86a780c9b97106206f5d02b01ba305ebb0a2b106
Jerome Lacoste <jerome.lacoste@gmail.com>
2015-09-12 11:45:16 +0200

    Code style compatibility with 2.0.0

--- lib/gym.rb
+++ lib/gym.rb
@@ -35,12 +35,6 @@ module Gym
       @version ||= parse_version
     end
 
-    private def parse_version
-      output = `DEVELOPER_DIR='' "#{xcode_path}/usr/bin/xcodebuild" -version`
-      return '0.0' if output.nil?
-      output.split("\n").first.split(' ')[1]
-    end
-
     def pre_7?
       v = xcode_version
       is_pre = v.split('.')[0].to_i < 7
@@ -53,6 +47,14 @@ module Gym
       require 'gym/xcodebuild_fixes/swift_fix'
       require 'gym/xcodebuild_fixes/watchkit_fix'
       require 'gym/xcodebuild_fixes/package_application_fix'
+    end
+
+    private
+
+    def parse_version
+      output = `DEVELOPER_DIR='' "#{xcode_path}/usr/bin/xcodebuild" -version`
+      return '0.0' if output.nil?
+      output.split("\n").first.split(' ')[1]
     end
   end
 
