be8bf875cc6cdb3c7b1bd1e6ae7f7c500b96cd27
Felix Krause <krausefx@gmail.com>
2015-09-23 15:01:40 -0700

    Don't set env in lane context if there is no value

--- lib/fastlane/lane_manager.rb
+++ lib/fastlane/lane_manager.rb
@@ -127,7 +127,7 @@ module Fastlane
     def self.load_dot_env(env)
       require 'dotenv'
 
-      Actions.lane_context[Actions::SharedValues::ENVIRONMENT] = env
+      Actions.lane_context[Actions::SharedValues::ENVIRONMENT] = env if env
 
       # Making sure the default '.env' and '.env.default' get loaded
       env_file = File.join(Fastlane::FastlaneFolder.path || "", '.env')
