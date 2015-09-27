ec9d4ba48dd20d81cbc8a037fe9522551705c4f3
KrauseFx <KrauseFx@gmail.com>
2015-09-26 21:41:17 -0700

    Improved screenshot handling

--- lib/deliver/upload_screenshots.rb
+++ lib/deliver/upload_screenshots.rb
@@ -9,6 +9,16 @@ module Deliver
 
       Helper.log.info "Starting with the upload of screenshots..."
 
+      # First, clear all previously uploaded screenshots, but only where we have new ones
+      # screenshots.each do |screenshot|
+      #   to_remove = v.screenshots[screenshot.language].find_all do |current|
+      #     current.device_type == screenshot.device_type
+      #   end
+      #   to_remove.each { |t| t.reset! }
+      # end
+      # This part is not working yet...
+
+      # Now, fill in the new ones
       indized = {} # per language and device type
       screenshots.each do |screenshot|
         indized[screenshot.language] ||= {}
@@ -16,6 +26,11 @@ module Deliver
         indized[screenshot.language][screenshot.device_type] += 1 # we actually start with 1... wtf iTC
 
         index = indized[screenshot.language][screenshot.device_type]
+
+        if index > 5
+          Helper.log.error "Too many screenshots found for device '#{screenshot.device_type}' in '#{screenshot.language}'"
+          next
+        end
 
         Helper.log.info "Uploading '#{screenshot.path}'..."
         v.upload_screenshot!(screenshot.path,
@@ -31,7 +46,7 @@ module Deliver
 
     def collect_screenshots(options)
       screenshots = []
-      Dir.glob(File.join(options[:screenshots_path], "*")) do |lng_folder|
+      Dir.glob(File.join(options[:screenshots_path], "*"), File::FNM_CASEFOLD).sort.each do |lng_folder|
         language = File.basename(lng_folder)
 
         files = Dir.glob(File.join(lng_folder, '*.png'))
