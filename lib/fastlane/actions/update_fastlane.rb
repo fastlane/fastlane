ea989afd060350efb148e98ce8dad50cb268d713
Felix Krause <krausefx@gmail.com>
2015-10-05 10:47:12 -0400

    Added gym, spaceship and pilot to `update_fastlane` action

--- lib/fastlane/actions/update_fastlane.rb
+++ lib/fastlane/actions/update_fastlane.rb
@@ -18,7 +18,10 @@ module Fastlane
         "produce",
         "cert",
         "codes",
-        "credentials_manager"
+        "credentials_manager",
+        "gym",
+        "spaceship",
+        "pilot"
       ]
 
       def self.run(options)
