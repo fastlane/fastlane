dd3e0ef546fb269587ce91f88f36b28597f8a5cb
Paul Williamson <squarefrog@gmail.com>
2015-10-06 20:56:13 +0100

    Support for multiple paths in git_commit action
    Fixes #660

--- spec/actions_specs/git_commit_spec.rb
+++ spec/actions_specs/git_commit_spec.rb
@@ -8,6 +8,14 @@ describe Fastlane do
 
         expect(result).to eq("git commit -m 'message' './fastlane/README.md'")
       end
+
+      it "generates the correct git command with an array of paths" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          git_commit(path: ['./fastlane/README.md', './fastlane/LICENSE'], message: 'message')
+        end").runner.execute(:test)
+
+        expect(result).to eq("git commit -m 'message' ./fastlane/README.md ./fastlane/LICENSE")
+      end
     end
   end
 end
