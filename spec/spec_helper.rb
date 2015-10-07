6b9057f9aaa69d4934e9e7ce58c0ca301ab7087c
Felix Krause <krausefx@gmail.com>
2015-10-06 22:54:27 -0400

    Enabled coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
-# Code climate
+require 'coveralls'
+Coveralls.wear! unless ENV["FASTLANE_SKIP_UPDATE_CHECK"]
+
 require 'credentials_manager'
 
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
 end
+
+WebMock.disable_net_connect!(allow: 'coveralls.io')
