7ce04e7656f00f58588eade2a27e813f6e78505b
Felix Krause <krausefx@gmail.com>
2015-10-08 12:05:15 -0400

    Added webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "coveralls"
 Coveralls.wear! unless ENV["FASTLANE_SKIP_UPDATE_CHECK"]
 
+require 'webmock'
+
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
 end
