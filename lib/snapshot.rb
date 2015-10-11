a0cb18894dc3e4c1b888e918cb1d2aa05a42c046
Felix Krause <krausefx@gmail.com>
2015-10-10 20:57:05 -0400

    Added initial tests

--- lib/snapshot.rb
+++ lib/snapshot.rb
@@ -10,6 +10,7 @@ require 'snapshot/latest_ios_version'
 require 'snapshot/test_command_generator'
 require 'snapshot/error_handler'
 require 'snapshot/collector'
+require 'snapshot/options'
 
 require 'fastlane_core'
 
---
+++ spec/test_command_generator_spec.rb
@@ -0,0 +1,24 @@
+describe Snapshot do
+  describe Snapshot::TestCommandGenerator do
+    describe "Valid Configuration" do
+      before do
+        options = { project: "./example/Example.xcodeproj" }
+        Snapshot.config = FastlaneCore::Configuration.create(Snapshot::Options.available_options, options)
+      end
+
+      it "uses the default parameters" do
+        command = Snapshot::TestCommandGenerator.generate(device_type: "Something")
+        expect(command).to eq([
+          "set -o pipefail &&", 
+          "xcodebuild", 
+          "-scheme 'ExampleUITests'", 
+          "-project './example/Example.xcodeproj'", 
+          "-derivedDataPath '/tmp/snapshot_derived/'", 
+          "-destination 'platform=iOS Simulator,id=,OS=9.0'", 
+          :test, 
+          "| tee '/Users/fkrause/Library/Logs/snapshot/Example-ExampleUITests.log' | xcpretty"
+        ])
+      end
+    end
+  end
+end
