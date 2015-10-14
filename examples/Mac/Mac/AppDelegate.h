ab8c8a7ef22c82438ddb9cbe74628615728db9a5
Felix Krause <krausefx@gmail.com>
2015-10-14 13:53:07 -0700

    Added mac example project

---
+++ examples/Mac/Mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mac
+//
+//  Created by Felix Krause on 10/14/15.
+//  Copyright © 2015 Felix Krause. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
