176f5e028feb3adc925c93f858cd3268ff185602
Felix Krause <krausefx@gmail.com>
2015-10-14 15:48:11 -0700

    Improved table of option summary

--- lib/scan/manager.rb
+++ lib/scan/manager.rb
@@ -3,7 +3,9 @@ module Scan
     def work(options)
       Scan.config = options
 
-      FastlaneCore::PrintTable.print_values(config: options, hide_keys: [:destination], title: "Summary")
+      FastlaneCore::PrintTable.print_values(config: options, 
+                                         hide_keys: [:destination, :slack_url], 
+                                             title: "Summary for scan #{Scan::VERSION}")
 
       return Runner.new.run
     end
