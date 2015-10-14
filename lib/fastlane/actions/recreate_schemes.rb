7da17a8ce897dd369e431c05910524f0c7168f2c
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2015-10-15 00:23:59 +0200

    Create new action to recreate/regenerate Xcode project schemes. This solves an issue when no scheme is commited into the SCM or the Xcode project is generated (in Apache Cordova for example). This could be fixed with a `recreate_schemes` call within `before_all` for example.

---
+++ lib/fastlane/actions/recreate_schemes.rb
@@ -0,0 +1,37 @@
+module Fastlane
+  module Actions
+    class RecreateSchemesAction < Action
+      def self.run(params)
+        require 'xcodeproj'
+
+        Helper.log.info "Recreate schemes for project: #{params[:project]}"
+
+        project = Xcodeproj::Project.open(params[:project])
+        project.recreate_user_schemes
+        #project.save
+      end
+
+      def self.description
+        "Recreate not shared Xcode project schemes."
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(
+            key: :project,
+            env_name: "XCODE_PROJECT",
+            description: "The project to use"
+          )
+        ]
+      end
+
+      def self.authors
+        "jerolimov"
+      end
+
+      def self.is_supported?(platform)
+        [:ios, :mac].include? platform
+      end
+    end
+  end
+end
