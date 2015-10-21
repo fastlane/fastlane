97912dc4464b2b1163529a61f04462bdc74a68db
Felix Krause <krausefx@gmail.com>
2015-10-20 17:03:30 -0700

    Updated test

--- spec/actions_specs/gradle_spec.rb
+++ spec/actions_specs/gradle_spec.rb
@@ -1,6 +1,6 @@
 describe Fastlane do
   describe Fastlane::FastFile do
-    describe "gradle", now: true do
+    describe "gradle" do
       it "generates a valid command" do
         result = Fastlane::FastFile.new.parse("lane :test do
           gradle(task: 'test', gradle_path: './README.md')
