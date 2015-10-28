ea7e6d8615ca419c72be4feec193b8bfe3fe7ab0
Felix Krause <krausefx@gmail.com>
2015-10-28 10:47:31 -0700

    Improved error message to say iTC

--- lib/deliver/detect_values.rb
+++ lib/deliver/detect_values.rb
@@ -25,7 +25,7 @@ module Deliver
       if app
         options[:app] = app
       else
-        raise "Could not find app with app identifier '#{options[:app_identifier]}' in your account".red
+        raise "Could not find app with app identifier '#{options[:app_identifier]}' in your iTunes Connect account".red
       end
     end
 
