8882cf952723b45a22fb64fa82e393bbcb79e2e0
Brian King <brian.king@raizlabs.com>
2015-10-29 21:37:16 -0400

    Added tag matching

--- lib/fastlane/actions/set_build_number_repository.rb
+++ lib/fastlane/actions/set_build_number_repository.rb
@@ -36,18 +36,26 @@ module Fastlane
         return false
       end
 
-      def self.git_tag_count
-        tags = Actions.sh('git log --simplify-by-decoration --decorate --pretty=oneline HEAD | grep "tag:"').shellsplit
-        tags.count
-      rescue
-        0
+      def self.git_initial_commit
+        Actions.sh('git rev-list --max-parents=0 --abbrev-commit HEAD').chomp
       end
 
-      def self.git_commit_count_reference_point
-        Actions.sh('git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1 HEAD)').chomp
-      rescue
-        # No tags, return first sha
-        Actions.sh('git rev-list --max-parents=0 --abbrev-commit HEAD').chomp
+      def self.git_tags_matching(matching)
+        tags = Actions.sh('git show-ref --tags').split("\n")
+        tags = tags.map { |tag| tag.split(" ") }
+        if matching
+          tags = tags.select { |shaTag| shaTag[1].start_with? "refs/tags/#{matching}" }
+        end
+        tags.map { |shaTag| shaTag[0] }
+      end
+
+      def self.git_diverging_commit_matching(matching)
+        list = git_tags_matching(matching)
+        if list.empty?
+          git_initial_commit
+        else
+          list.last.split(" ").first
+        end
       end
 
       def self.git_commit_count_between(commitA, commitB = "HEAD")
@@ -67,9 +75,10 @@ module Fastlane
           command = 'git svn info | grep Revision | egrep -o "[0-9]+"'
         elsif is_git?
           Helper.log.info "Detected repo: git"
-          if params[:use_git_counts_from_tag]
-            major = git_tag_count
-            minor = git_commit_count_between(git_commit_count_reference_point)
+          tagPrefix = params[:use_git_counts_matching_tag]
+          if tagPrefix
+            major = git_tags_matching(tagPrefix).count
+            minor = git_commit_count_between(git_diverging_commit_matching(tagPrefix))
             patch = git_abbrev_last_commit.to_i(16)
             build_number = "#{major}.#{minor}.#{patch}"
           else
@@ -108,12 +117,12 @@ module Fastlane
                                        optional: true,
                                        is_string: false,
                                        default_value: false),
-          FastlaneCore::ConfigItem.new(key: :use_git_counts_from_tag,
-                                       env_name: "USE_GIT_COUNTS_FROM_TAG",
-                                       description: "Use the number of tags as the major version, number of commits since last tag as minor version, and a numeric SHA as the patch version",
+          FastlaneCore::ConfigItem.new(key: :use_git_counts_matching_tag,
+                                       env_name: "USE_GIT_COUNTS_MATCHING_TAG",
+                                       description: "Use the number of matching tags as the major version, number of commits since last matching tag as minor version, and a numeric SHA as the patch version",
                                        optional: true,
-                                       is_string: false,
-                                       default_value: false)
+                                       is_string: true,
+                                       default_value: nil)
         ]
       end
 
