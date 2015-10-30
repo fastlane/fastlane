2cc993cd4c2a4b6868262ca8fcef560f5804c3f3
James Campbell <james@supmenow.com>
2015-10-30 11:08:06 +0000

    Make codesign have access.

--- lib/cert/keychain_importer.rb
+++ lib/cert/keychain_importer.rb
@@ -4,7 +4,7 @@ module Cert
       raise "Could not find file '#{path}'".red unless File.exist?(path)
       keychain = File.expand_path(Cert.config[:keychain_path]) || "#{Dir.home}/Library/Keychains/login.keychain"
 
-      puts `security import '#{path}' -k '#{keychain}'`
+      puts `security import '#{path}' -k '#{keychain}' -T /usr/bin/codesign`
     end
   end
 end
