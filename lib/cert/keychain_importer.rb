b8c49db56121532b7570bea68fee872e361ddb38
James Campbell <james@supmenow.com>
2015-11-07 21:59:39 +0000

    Auto trust certificate and expands keychain path.

--- lib/cert/keychain_importer.rb
+++ lib/cert/keychain_importer.rb
@@ -2,9 +2,9 @@ module Cert
   class KeychainImporter
     def self.import_file(path)
       raise "Could not find file '#{path}'".red unless File.exist?(path)
-      keychain = Cert.config[:keychain_path] || "#{Dir.home}/Library/Keychains/login.keychain"
+      keychain = File.expand_path(Cert.config[:keychain_path]) || "#{Dir.home}/Library/Keychains/login.keychain"
 
-      puts `security import '#{path}' -k '#{keychain}'`
+      puts `security import '#{path}' -k '#{keychain}' -T /usr/bin/codesign`
     end
   end
 end
