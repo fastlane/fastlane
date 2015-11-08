44ee515d0ff60b471a2a914afb646b753d138ca2
Felix Krause <krausefx@gmail.com>
2015-11-08 22:02:35 +0100

    Changed default output name of p12

--- lib/cert/runner.rb
+++ lib/cert/runner.rb
@@ -79,7 +79,7 @@ module Cert
       # Store all that onto the filesystem
       request_path = File.expand_path(File.join(Cert.config[:output_path], 'CertCertificateSigningRequest.certSigningRequest'))
       File.write(request_path, csr.to_pem)
-      private_key_path = File.expand_path(File.join(Cert.config[:output_path], 'private_key.p12'))
+      private_key_path = File.expand_path(File.join(Cert.config[:output_path], "#{certificate.id}.p12"))
       File.write(private_key_path, pkey)
       cert_path = store_certificate(certificate)
 
