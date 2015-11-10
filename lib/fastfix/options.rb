849181102cdbb67e0d562541c1a4ac05ac006c94
Felix Krause <krausefx@gmail.com>
2015-11-11 00:53:52 +0100

    Fixed code style

--- lib/fastfix/options.rb
+++ lib/fastfix/options.rb
@@ -5,46 +5,46 @@ module Fastfix
   class Options
     def self.available_options
       [
-          FastlaneCore::ConfigItem.new(key: :path,
-                                       env_name: "FASTFIX_PATH",
-                                       description: "Path to the certificates directory",
-                                       default_value: File.join('fastlane', 'certificates'),
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :git_url,
-                                       env_name: "FASTFIX_GIT_URl",
-                                       description: "URL to the git repo containing all the certificates",
-                                       optional: true,
-                                       short_option: "-r",
-                                       verify_block: proc do |value|
-                                         # TODO
-                                       end),
-          FastlaneCore::ConfigItem.new(key: :type,
-                                       env_name: "FASTFIX_TYPE",
-                                       description: "Create a development certificate instead of a distribution one",
-                                       is_string: true,
-                                       short_option: "-y",
-                                       default_value: 'appstore',
-                                       verify_block: proc do |value|
-                                         supported = %w{appstore adhoc development enterprise}
-                                         unless supported.include?(value)
-                                           raise "Unsupported environment #{value}, must be in #{supported.join(', ')}".red
-                                         end
-                                       end),
-          FastlaneCore::ConfigItem.new(key: :app_identifier,
-                                     short_option: "-a",
-                                     env_name: "FASTFIX_APP_IDENTIFIER",
-                                     description: "The bundle identifier of your app",
-                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
-          FastlaneCore::ConfigItem.new(key: :username,
-                                       short_option: "-u",
-                                       env_name: "FASTFIX_USERNAME",
-                                       description: "Your Apple ID Username",
-                                       default_value: CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)),
-          FastlaneCore::ConfigItem.new(key: :keychain_name,
-                                       env_name: "FASTFIX_KEYCHAIN_NAME",
-                                       description: "Keychain the items should be imported to",
-                                       default_value: "login.keychain")
-        ]
+        FastlaneCore::ConfigItem.new(key: :path,
+                                     env_name: "FASTFIX_PATH",
+                                     description: "Path to the certificates directory",
+                                     default_value: File.join('fastlane', 'certificates'),
+                                     optional: true),
+        FastlaneCore::ConfigItem.new(key: :git_url,
+                                     env_name: "FASTFIX_GIT_URl",
+                                     description: "URL to the git repo containing all the certificates",
+                                     optional: true,
+                                     short_option: "-r",
+                                     verify_block: proc do |value|
+                                       # TODO
+                                     end),
+        FastlaneCore::ConfigItem.new(key: :type,
+                                     env_name: "FASTFIX_TYPE",
+                                     description: "Create a development certificate instead of a distribution one",
+                                     is_string: true,
+                                     short_option: "-y",
+                                     default_value: 'appstore',
+                                     verify_block: proc do |value|
+                                       supported = %w(appstore adhoc development enterprise)
+                                       unless supported.include?(value)
+                                         raise "Unsupported environment #{value}, must be in #{supported.join(', ')}".red
+                                       end
+                                     end),
+        FastlaneCore::ConfigItem.new(key: :app_identifier,
+                                   short_option: "-a",
+                                   env_name: "FASTFIX_APP_IDENTIFIER",
+                                   description: "The bundle identifier of your app",
+                                   default_value: CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
+        FastlaneCore::ConfigItem.new(key: :username,
+                                     short_option: "-u",
+                                     env_name: "FASTFIX_USERNAME",
+                                     description: "Your Apple ID Username",
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)),
+        FastlaneCore::ConfigItem.new(key: :keychain_name,
+                                     env_name: "FASTFIX_KEYCHAIN_NAME",
+                                     description: "Keychain the items should be imported to",
+                                     default_value: "login.keychain")
+      ]
     end
   end
 end
