5ae5c629534c557038a972faa46c0f2879c24c4e
Felix Krause <krausefx@gmail.com>
2015-11-16 10:51:12 +0100

    Improved code style

--- lib/fastlane_core/configuration/config_item.rb
+++ lib/fastlane_core/configuration/config_item.rb
@@ -20,7 +20,7 @@ module FastlaneCore
         raise "short_option must be a String of length 1" unless short_option.kind_of? String and short_option.delete('-').length == 1
       end
       if description
-        raise "Do not let descriptions end with a '.', since it's used for user inputs as well".red if (description[-1] == '.')
+        raise "Do not let descriptions end with a '.', since it's used for user inputs as well".red if description[-1] == '.'
       end
 
       @key = key
