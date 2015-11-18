19751ea995e69199fc32d68e9ac417515a653694
Felix Krause <krausefx@gmail.com>
2015-11-18 12:00:45 -0800

    Fixed not working cert when not specifying a keychain path

--- lib/cert/keychain_importer.rb
+++ lib/cert/keychain_importer.rb
@@ -2,7 +2,7 @@ module Cert
   class KeychainImporter
     def self.import_file(path)
       raise "Could not find file '#{path}'".red unless File.exist?(path)
-      keychain = File.expand_path(Cert.config[:keychain_path]) || "#{Dir.home}/Library/Keychains/login.keychain"
+      keychain = File.expand_path(Cert.config[:keychain_path] || "#{Dir.home}/Library/Keychains/login.keychain")
 
       command = "security import #{path.shellescape} -k '#{keychain}'"
       command << " -T /usr/bin/codesign" # to not be asked for permission when running a tool like `gym`
