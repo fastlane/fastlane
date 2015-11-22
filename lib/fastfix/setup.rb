170c77fd2c4146feb45a27acc4de3a3cb55e4558
KrauseFx <KrauseFx@gmail.com>
2015-11-22 09:44:36 -0800

    Improved wording

--- lib/fastfix/setup.rb
+++ lib/fastfix/setup.rb
@@ -11,7 +11,7 @@ module Fastfix
       File.write(path, template)
       puts "Successfully created '#{path}'. Open the file using a code editor.".green
 
-      puts "From now on you can run `fastfix development`, `fastfix adhoc` and `fastlane appstore`"
+      puts "You can now run `fastfix development`, `fastfix adhoc` and `fastlane appstore`"
       puts "On the first run for each environment it will create the provisioning profiles and"
       puts "certificates for you. From then on, it will automatically import the existing profiles."
     end
