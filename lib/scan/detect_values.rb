7e4fd1959fb25c7d028452fc84176f9b03770f45
Felix Krause <krausefx@gmail.com>
2015-11-24 11:17:08 -0800

    Improved iOS detection

--- lib/scan/detect_values.rb
+++ lib/scan/detect_values.rb
@@ -26,7 +26,6 @@ module Scan
       config = Scan.config
 
       if config[:device] # make sure it actually exists
-
         device = config[:device].to_s.strip.tr('()', '') # Remove parenthesis
 
         found = FastlaneCore::Simulator.all.find { |d| (d.name + " " + d.ios_version).include? device }
@@ -39,11 +38,15 @@ module Scan
         end
       end
 
-      min_target = Scan.project.build_settings(key: "IPHONEOS_DEPLOYMENT_TARGET").split(".").first.to_i
+      sims = FastlaneCore::Simulator.all
+      xcode_target = Scan.project.build_settings(key: "IPHONEOS_DEPLOYMENT_TARGET")
 
       # Filter out any simulators that are not the same major version of our deployment target
-      sims = FastlaneCore::Simulator.all.select { |s| s.ios_version.to_i >= min_target }
-      
+      if xcode_target.to_s.length > 0
+        min_target = xcode_target.split(".").first.to_i
+        sims = sims.select { |s| s.ios_version.to_i >= min_target }
+      end
+
       # An iPhone 5s is reasonable small and useful for tests
       found = sims.find { |d| d.name == "iPhone 5s" }
       found ||= sims.first # anything is better than nothing
