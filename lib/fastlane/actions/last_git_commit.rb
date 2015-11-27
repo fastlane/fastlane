14452a63b106d88368c1fa0f17026d7a2f9c56ec
ngutman <nimrod@billguard.com>
2015-11-27 17:36:23 +0200

    add last_git_commit action
    return information about the last git commit, we use it to create a
    custom message when releasing a Crashlytics build

---
+++ lib/fastlane/actions/last_git_commit.rb
@@ -0,0 +1,32 @@
+module Fastlane
+  module Actions
+    class LastGitCommitAction < Action
+      def self.run(params)
+        message = sh "git log -1 --pretty=%B"
+        author = sh "git log -1 --pretty=%an"
+
+        {author: author, message: message}
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Return last git commit message and author"
+      end
+
+      def self.return_value
+        "Returns the following dict: {author: \"Author\", message: \"commit message\"}"
+      end
+
+      def self.author
+        "ngutman"
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+      end
+    end
+  end
+end
