213716b6c58491f1c021a1b7688d0f566b2fd98b
joshrlesch <josh.r.lesch@gmail.com>
2015-11-30 15:10:11 -0600

    Added an option to pass a custom derived data path

--- lib/fastlane/actions/clear_derived_data.rb
+++ lib/fastlane/actions/clear_derived_data.rb
@@ -1,8 +1,14 @@
 module Fastlane
   module Actions
     class ClearDerivedDataAction < Action
-      def self.run(params)
-        path = File.expand_path("~/Library/Developer/Xcode/DerivedData")
+      def self.run(options)
+        if options[:derived_data_path]
+          path = File.expand_path(options[:derived_data_path])
+        else
+          path = File.expand_path("~/Library/Developer/Xcode/DerivedData")
+        end
+
+        Helper.log.info "Derived Data path located at: #{path}"
         FileUtils.rm_rf(path) if File.directory?(path)
         Helper.log.info "Successfully cleared Derived Data ♻️".green
       end
@@ -16,11 +22,16 @@ module Fastlane
       end
 
       def self.details
-        "Deletes the Derived Data from '~/Library/Developer/Xcode/DerivedData'"
+        "Deletes the Derived Data from '~/Library/Developer/Xcode/DerivedData' or a supplied path"
       end
 
       def self.available_options
-        []
+        [
+          FastlaneCore::ConfigItem.new(key: :derived_data_path,
+                                       env_name: "DERIVED_DATA_PATH",
+                                       description: "Custom path for derivedData",
+                                       default_value: nil, optional: true)
+        ]
       end
 
       def self.output
