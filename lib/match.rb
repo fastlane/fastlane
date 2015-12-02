aae56d04ac1760f87e1b3c8aa38ff40758c15b08
Felix Krause <krausefx@gmail.com>
2015-12-02 14:18:27 -0500

    Fixed type of environments

--- lib/match.rb
+++ lib/match.rb
@@ -15,8 +15,8 @@ module Match
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
 
   def self.environments
-    envs = [:appstore, :adhoc, :development]
-    envs << :enterprise if ENV["MATCH_FORCE_ENTERPRISE"]
+    envs = %w{appstore adhoc development}
+    envs << "enterprise" if ENV["MATCH_FORCE_ENTERPRISE"]
     return envs
   end
 end
