230c3cb40a00d0c27f7a9ea24b5922ebfb9e046f
Felix Krause <krausefx@gmail.com>
2015-12-09 15:52:53 -0800

    Added verification of the provisioning profile on the dev portal

--- lib/match/spaceship_ensure.rb
+++ lib/match/spaceship_ensure.rb
@@ -1,13 +1,21 @@
 module Match
   # Ensures the certificate and profiles are also available on iTunes Connect
   class SpaceshipEnsure
-    def self.certificate_exists?(certificate_id)
-      Spaceship.login
+    def initialize(user)
+      Spaceship.login(user)
       Spaceship.select_team
-      Spaceship.certificate.all.each do |cert|
-        return cert if cert.id == certificate_id
+    end
+
+    def certificate_exists?(certificate_id)
+      Spaceship.certificate.all.find do |cert|
+        cert.id == certificate_id
       end
-      nil
+    end
+
+    def profile_exists?(uuid)
+      Spaceship.provisioning_profile.all.find do |profile|
+        profile.uuid == uuid
+      end
     end
   end
 end
