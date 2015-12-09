519482cad4895f274d359ade7bee72bca926eeb3
Felix Krause <krausefx@gmail.com>
2015-12-09 15:58:08 -0800

    Moved error message to SpaceshipEnsure

--- lib/match/runner.rb
+++ lib/match/runner.rb
@@ -8,29 +8,18 @@ module Match
                                              title: "Summary for match #{Match::VERSION}")
 
       params[:workspace] = GitHelper.clone(params[:git_url])
-
-      cert_id = certificate(params: params)
       spaceship = SpaceshipEnsure.new(params[:username])
 
-      unless spaceship.certificate_exists?(cert_id)
-        UI.error("Certificate '#{cert_id}' (stored in your git repo) is not available on the Developer Portal")
-        UI.error("for the user #{params[:username]}")
-        UI.error("Make sure to use the same user and team every time you run 'match' for this")
-        UI.error("Git repository. This might be caused by revoking the certificate on the Dev Portal")
-        UI.user_error!("To reset the certificates of your Apple account, you can use the `match nuke` feature, more information on https://github.com/fastlane/match")
-      end
+      # Certificate
+      cert_id = certificate(params: params)
+      spaceship.certificate_exists(params, cert_id)
 
+      # Provisioning Profile
       uuid = profile(params: params, 
                      certificate_id: cert_id)
-      
-      unless spaceship.profile_exists?(uuid)
-        UI.error("Provisioning profile '#{uuid}' is not available on the Developer Portal")
-        UI.error("for the user #{params[:username]}")
-        UI.error("Make sure to use the same user and team every time you run 'match' for this")
-        UI.error("Git repository. This might be caused by deleting the provisioning profile on the Dev Portal")
-        UI.user_error!("To reset the provisioning profiles of your Apple account, you can use the `match nuke` feature, more information on https://github.com/fastlane/match")
-      end
+      spaceship.profile_exists(params, uuid)
 
+      # Done
       if self.changes_to_commit
         message = GitHelper.generate_commit_message(params)
         GitHelper.commit_changes(params[:workspace], message, params[:git_url])
