0676459bfd58f1b6a2d6142c752e6335574cc502
KrauseFx <KrauseFx@gmail.com>
2015-12-11 19:46:33 -0800

    Added dynamic unfolding of parameters

--- lib/fastlane_core/ui/ui.rb
+++ lib/fastlane_core/ui/ui.rb
@@ -7,13 +7,11 @@ module FastlaneCore
     end
 
     def self.method_missing(method_sym, *args, &_block)
-      raise "Only pass exactly one parameter to UI".red if args.length != 1
-
       # not using `responds` beacuse we don't care about methods like .to_s and so on
       interface_methods = Interface.instance_methods - Object.instance_methods
       raise "Unknown method '#{method_sym}', supported #{interface_methods}" unless interface_methods.include?(method_sym)
 
-      self.current.send(method_sym, args.first)
+      self.current.send(method_sym, *args)
     end
   end
 end
