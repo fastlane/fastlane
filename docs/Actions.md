d55f28744f12788e4ccdf5b781e2dfa23a5f3923
Juan Ignacio Donoso <jidonoso@gmail.com>
2015-12-13 21:56:51 -0500

    fixup! add action to get values from config.xml file on cordova projects

--- docs/Actions.md
+++ docs/Actions.md
@@ -23,7 +23,6 @@ import './path/to/other/Fastfile'
 - [Using git](#using-git)
 - [Using mercurial](#using-mercurial)
 - [Notifications](#notifications)
-- [Cordova](#cordova)
 - [Misc](#misc)
 
 ## Building
@@ -1493,24 +1492,6 @@ before_all do
 end
 ```
 
-## Cordova
-
-### cordova_get_config_value
-
-Get a value from a config file, which can be used to fetch the app identifier
-and more information about your app
-
-```ruby
-identifier = cordova_get_config_value(key: 'id')
-puts identifier # => tools.fastlane.cordova
-
-# path defaults to
-name = cordova_get_config_value(
-  path: 'cordova/config.xml',
-  key: 'name').first
-puts name.first # => Cordova App
-```
-
 ## Misc
 
 ### appledoc
@@ -1754,4 +1735,20 @@ Install an Xcode plugin for the current user
 
 ```ruby
 install_xcode_plugin(url: 'https://github.com/contentful/ContentfulXcodePlugin/releases/download/0.5/ContentfulPlugin.xcplugin.zip')
+```
+
+### cordova_get_config_value
+
+Get a value from a config file, which can be used to fetch the app identifier
+and more information about your app
+
+```ruby
+identifier = cordova_get_config_value(key: 'id')
+puts identifier # => tools.fastlane.cordova
+
+# path defaults to
+name = cordova_get_config_value(
+  path: 'cordova/config.xml',
+  key: 'name').first
+puts name.first # => Cordova App
 ```
--- lib/fastlane/actions/cordova_get_config_value.rb
+++ lib/fastlane/actions/cordova_get_config_value.rb
@@ -8,18 +8,14 @@ module Fastlane
       def self.run(params)
         Actions.verify_gem!('xml-simple')
         require 'xmlsimple'
-        begin
-          config_file = File.open(File.expand_path(params[:path]))
-          config = XmlSimple.xml_in(config_file)
 
-          value = config[params[:key]]
-          Actions.lane_context[SharedValues::CORDOVA_GET_CONFIG_VALUE_CUSTOM_VALUE] = value
+        config_file = File.open(File.expand_path(params[:path]))
+        config = XmlSimple.xml_in(config_file)
 
-          return value
-        rescue => ex
-          Helper.log.error ex
-          Helper.log.error "Unable to find config file at '#{path}'".red
-        end
+        value = config[params[:key]]
+        Actions.lane_context[SharedValues::CORDOVA_GET_CONFIG_VALUE_CUSTOM_VALUE] = value
+
+        return value
       end
 
       #####################################################
@@ -31,15 +27,10 @@ module Fastlane
       end
 
       def self.details
-        # Optional:
-        # this is your change to provide a more detailed description of this action
-        "You can use this action to do cool things..."
+        "This action let to get any value from the config.xml file. It will return a ruby object you can use."
       end
 
       def self.available_options
-        # Define all options your action supports.
-
-        # Below a few examples
         [
           FastlaneCore::ConfigItem.new(key: :key,
                                        env_name: "CORDOVA_GET_CONFIG_PARAM_NAME",
@@ -58,10 +49,8 @@ module Fastlane
       end
 
       def self.output
-        # Define the shared values you are going to provide
-        # Example
         [
-          ['CORDOVA_GET_CONFIG_VALUE_CUSTOM_VALUE', 'A description of what this value contains']
+          ['CORDOVA_GET_CONFIG_VALUE_CUSTOM_VALUE', 'Value for the key required from the config.xml file']
         ]
       end
 
@@ -70,21 +59,11 @@ module Fastlane
       end
 
       def self.authors
-        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
         ["platanus", "blackjid"]
       end
 
       def self.is_supported?(platform)
-        # you can do things like
-        #
-        #  true
-        #
-        #  platform == :ios
-        #
-        #  [:ios, :mac].include?(platform)
-        #
-
-        platform == :ios
+        [:ios, :android].include?(platform)
       end
     end
   end
