ff99cab9245e882e5e750af11746cdd99b4988a1
Felix Krause <krausefx@gmail.com>
2015-12-17 13:25:57 -0800

    Enabled UI layer

--- lib/gym.rb
+++ lib/gym.rb
@@ -40,6 +40,7 @@ module Gym
   end
 
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
+  UI = FastlaneCore::UI
 
   Gym.init_libs
 end
