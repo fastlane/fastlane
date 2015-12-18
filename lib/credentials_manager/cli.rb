cefaafef6929b5c8061cd2bb0298a5f004c4ce81
Andrew Page <andrew@andrewpage.me>
2015-12-18 09:59:57 -0800

    Capture command

--- lib/credentials_manager/cli.rb
+++ lib/credentials_manager/cli.rb
@@ -2,25 +2,34 @@ require 'credentials_manager/version'
 
 module CredentialsManager
   class CLI
-    REQUIRED_PARAMS = %i(username password)
+    REQUIRED_PARAMS = {
+      add: %i(username password),
+      remove: %i(username)
+    }
 
     def initialize(args)
       @args = args
       @options = {}
 
-      parse
-      validate
+      begin
+        parse
+        validate
+      rescue OptionParser::MissingArgument => ex
+        # If we have a missing argument, print out the help
+        puts ex.message.capitalize
+        puts parser
+      end
     end
 
     def execute
-      puts @options
+      # puts @options
     end
 
     private
 
-    # Parse the option list
-    def parse
-      OptionParser.new do |opts|
+    # Generate the options parser
+    def parser
+      @parser ||= OptionParser.new do |opts|
         opts.banner = 'Usage: fastlane-credentials [command] [-options]'
 
         opts.on('-u', '--username [username]', 'Set a username') do |username|
@@ -41,13 +50,26 @@ module CredentialsManager
           puts ::CredentialsManager::VERSION
           exit
         end
-      end.parse!(@args)
+      end
+    end
+
+    # Parse the option list
+    def parse
+      parser.parse!(@args)
+
+      # First non-option argument
+      command = @args.pop
+      if command
+        @command = command.to_sym
+      else
+        raise OptionParser::MissingArgument, 'command'
+      end
     end
 
     # Ensure that all required paramters are present
     def validate
       # Required parameters that were not specified
-      diff = REQUIRED_PARAMS - @options.keys
+      diff = REQUIRED_PARAMS[@command] - @options.keys
 
       unless diff.empty?
         diff.each { |key| raise OptionParser::MissingArgument, key.to_s }
