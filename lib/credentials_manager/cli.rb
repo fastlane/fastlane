2934103f759cf6bb1e7ea90f7754a1dac02bf076
Andrew Page <andrew@andrewpage.me>
2015-12-18 12:52:24 -0800

    Convert CLI to use commander

--- lib/credentials_manager/cli.rb
+++ lib/credentials_manager/cli.rb
@@ -1,107 +1,76 @@
 require 'credentials_manager/version'
+require 'commander'
 
 module CredentialsManager
   class CLI
-    VALID_COMMANDS = %i(add remove)
+    include Commander::Methods
 
-    REQUIRED_PARAMS = {
-      add: %i(username password),
-      remove: %i(username)
-    }
+    # Parses command options and executes actions
+    def run
+      program :name, 'CredentialsManager'
+      program :version, ::CredentialsManager::VERSION
+      program :description, 'Manage credentials for fastlane tools.'
 
-    def initialize(args)
-      @args = args
-      @options = {}
+      # Command to add entry to Keychain
+      command :add do |c|
+        c.syntax = 'fastlane-credentials add'
+        c.description = 'Adds a fastlane credential to the keychain.'
 
-      begin
-        parse
-        validate
-      rescue => ex
-        # If we have a missing argument, print out the help
-        puts ex.message.capitalize
-        puts parser
+        c.option '--username username', String, 'Username to add.'
+        c.option '--password password', String, 'Password to add.'
+
+        c.action do |args, options|
+          username, password = options.username, options.password
+
+          if username.nil?
+            raise 'You must specify a username'
+          elsif password.nil?
+            raise 'You must specify a password'
+          else
+            add(username, password)
+
+            puts "Credential #{username}:#{'*' * password.length} added to keychain."
+          end
+
+        end
       end
-    end
 
-    # Perform actions based on data
-    def execute
-      case @command
-      when :add
-        CredentialsManager::AccountManager.new(
-          user: @options[:username],
-          password: @options[:password]
-        ).add_to_keychain
+      # Command to remove credential from Keychain
+      command :remove do |c|
+        c.syntax = 'fastlane-credentials remove'
+        c.description = 'Removes a fastlane credential from the keychain.'
 
-        puts "Credential #{@options[:username]}:#{'*' * @options[:password].length} added to keychain."
-      when :remove
-        CredentialsManager::AccountManager.new(
-          user: @options[:username]
-        ).remove_from_keychain
+        c.option '--username username', String, 'Username to remove.'
+
+        c.action do |args, options|
+          username = options.username
+
+          if username.nil?
+            raise 'You must specify a username'
+          else
+            remove(username)
+          end
+        end
       end
+
+      run!
     end
 
     private
 
-    # Generate the options parser
-    def parser
-      @parser ||= OptionParser.new do |opts|
-        opts.banner = 'Usage: fastlane-credentials [command] [-options]'
-
-        opts.on('-u', '--username [username]', 'Set a username') do |username|
-          @options[:username] = username
-        end
-
-        opts.on('-p', '--password [password]', 'Set a password') do |password|
-          @options[:password] = password
-        end
-
-        opts.on_tail("-h", "--help", 'See program help.') do
-          puts opts
-          exit
-        end
-
-        # Another typical switch to print the version.
-        opts.on_tail("-v", "--version", 'Show version.') do
-          puts ::CredentialsManager::VERSION
-          exit
-        end
-      end
+    # Add entry to Apple Keychain using AccountManager
+    def add(username, password)
+      CredentialsManager::AccountManager.new(
+        user: username,
+        password: password
+      ).add_to_keychain
     end
 
-    # Parse the option list
-    def parse
-      parser.parse!(@args)
-
-      # First non-option argument
-      command = @args.pop
-      if command
-        @command = command.to_sym
-      else
-        raise OptionParser::MissingArgument, 'command'
-      end
-    end
-
-    # Perform all necessary validations
-    def validate
-      validate_command
-      validate_options
-    end
-
-    # Ensure that the entered command is valid
-    def validate_command
-      unless VALID_COMMANDS.include? @command
-        raise "#{@command} is not a valid command. Must be one of #{VALID_COMMANDS.join(', ')}"
-      end
-    end
-
-    # Ensure that all required paramters are present
-    def validate_options
-      # Required parameters that were not specified
-      diff = REQUIRED_PARAMS[@command] - @options.keys
-
-      unless diff.empty?
-        diff.each { |key| raise OptionParser::MissingArgument, key.to_s }
-      end
+    # Remove entry from Apple Keychain using AccountManager
+    def remove(username)
+      CredentialsManager::AccountManager.new(
+        user: username
+      ).remove_from_keychain
     end
   end
 end
