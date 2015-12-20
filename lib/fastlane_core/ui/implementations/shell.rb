fbcdbccd573c58b20c4c502d9a0629895035a708
KrauseFx <KrauseFx@gmail.com>
2015-12-20 09:40:58 +0100

    Improved shell output

--- lib/fastlane_core/ui/implementations/shell.rb
+++ lib/fastlane_core/ui/implementations/shell.rb
@@ -52,8 +52,8 @@ module FastlaneCore
     def command_output(message)
       actual = (message.split("\r").last || "") # as clearing the line will remove the `>` and the time stamp
       actual.split("\n").each do |msg|
-        prefix = msg.include?("▸") ? "" : "▸"
-        log.info(prefix + " " + msg.magenta)
+        prefix = msg.include?("▸") ? "" : "▸ "
+        log.info(prefix + "" + msg.magenta)
       end
     end
 
