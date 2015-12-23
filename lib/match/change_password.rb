83a76f6fba80ba799418f982c249f259f0a430be
Jerome Lacoste <jerome.lacoste@gmail.com>
2015-12-23 13:06:58 +0100

    Also ask twice under change_password command

--- lib/match/change_password.rb
+++ lib/match/change_password.rb
@@ -1,14 +1,27 @@
 module Match
   class ChangePassword
     def self.update(params: nil, from: nil, to: nil)
-      to ||= UI.password("New password: ")
+      to ||= ChangePassword.ask_password("New passphrase for Git Repo: ")
       GitHelper.clear_changes
       workspace = GitHelper.clone(params[:git_url], params[:shallow_clone], manual_password: from)
       Encrypt.new.clear_password(params[:git_url])
       Encrypt.new.store_password(params[:git_url], to)
 
-      message = "[fastlane] Changed password"
+      message = "[fastlane] Changed passphrase"
       GitHelper.commit_changes(workspace, message, params[:git_url])
+    end
+
+    def self.ask_password(msg="Passphrase for Git Repo: ")
+      password = ""
+      while password.to_s.length == 0
+        password = ask(msg.yellow) { |q| q.echo = "*" }
+        password2 = ask("Type passphrase again: ".yellow) { |q| q.echo = "*" }
+        if password != password2
+          UI.error("Passhprases differ. Try again")
+          password = ""
+        end
+      end
+      password
     end
   end
 end
