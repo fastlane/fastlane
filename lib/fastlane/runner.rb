84b27edb69b983c57c4c50df99451923cbad47d7
Felix Krause <KrauseFx@gmail.com>
2015-12-30 00:33:05 +0400

    Fixed code style

--- lib/fastlane/runner.rb
+++ lib/fastlane/runner.rb
@@ -18,7 +18,6 @@ module Fastlane
     # @param lane_name The name of the lane to execute
     # @param platform The name of the platform to execute
     # @param parameters [Hash] The parameters passed from the command line to the lane
-    # rubocop:disable Metrics/CyclomaticComplexity
     # rubocop:disable Metrics/AbcSize
     def execute(lane, platform = nil, parameters = nil)
       raise "No lane given" unless lane
@@ -70,7 +69,6 @@ module Fastlane
         raise ex
       end
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
     # rubocop:enable Metrics/AbcSize
 
     # @param filter_platform: Filter, to only show the lanes of a given platform
