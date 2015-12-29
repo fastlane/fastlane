f8e7ca39af80356ecfe0dbb212f82bfd4f1b0d33
KrauseFx <KrauseFx@gmail.com>
2015-12-30 00:45:56 +0400

    Added tvOS example project

---
+++ examples/tvapp/tvapp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  tvapp
+//
+//  Created by Felix Krause on 30/12/15.
+//  Copyright © 2015 Felix Krause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ examples/tvapp/tvapp/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  tvapp
+//
+//  Created by Felix Krause on 30/12/15.
+//  Copyright © 2015 Felix Krause. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
