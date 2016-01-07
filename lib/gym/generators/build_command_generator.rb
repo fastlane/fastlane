7577fad25250ca2aa245a9f843389f521764af76
KrauseFx <KrauseFx@gmail.com>
2016-01-07 15:28:26 +0700

    Improved color disabled detection

--- lib/gym/generators/build_command_generator.rb
+++ lib/gym/generators/build_command_generator.rb
@@ -60,7 +60,7 @@ module Gym
       def pipe
         pipe = []
         pipe << "| tee '#{xcodebuild_log_path}' | xcpretty"
-        pipe << "--no-color" if ENV["FASTLANE_DISABLE_COLORS"]
+        pipe << "--no-color" if Helper.colors_disabled?
         pipe << "> /dev/null" if Gym.config[:suppress_xcode_output]
 
         pipe
