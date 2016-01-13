69c6959c69c504a54379844f66adcae610933d4d
Michael Furtak <mfurtak@twitter.com>
2016-01-12 19:01:33 -0500

    Update DependencyChecker to use CommandExecutor.which

--- lib/chiizu/dependency_checker.rb
+++ lib/chiizu/dependency_checker.rb
@@ -3,15 +3,14 @@ module Chiizu
     def self.check_dependencies
       return if Helper.test?
 
-      self.check_adb
+      check_adb
     end
 
     def self.check_adb
-      unless `adb version`.include? "Android Debug Bridge"
-        Helper.log.fatal '#############################################################'
-        Helper.log.fatal '# TODO Helpful error message'
-        Helper.log.fatal '#############################################################'
-        raise 'TODO Helpful error message'
+      unless FastlaneCore::CommandExecutor.which('adb')
+        UI.error 'The `adb` command could not be found on your PATH'
+        UI.error "Please ensure that the Android tools are installed and the platform-tools directory is present and on your PATH"
+        Ui.user_error! 'adb command not found'
       end
     end
   end
