5a83b356d420d4d55a82d7fd7fac5c4ad5142b00
André Filipe Maciel da Silva <andre_fms_@live.com.pt>
2016-01-13 15:42:33 +0000

    Update hipchat.rb
    For some reason i can't send a private message if "@" is removed because the api can't find the user. Maybe the api have changed?

--- lib/fastlane/actions/hipchat.rb
+++ lib/fastlane/actions/hipchat.rb
@@ -43,7 +43,6 @@ module Fastlane
         else
           ########## running on V2 ##########
           if user?(channel)
-            channel.slice!(0)
             params = { 'message' => message, 'message_format' => message_format }
             json_headers = { 'Content-Type' => 'application/json',
                              'Accept' => 'application/json', 'Authorization' => "Bearer #{api_token}" }
