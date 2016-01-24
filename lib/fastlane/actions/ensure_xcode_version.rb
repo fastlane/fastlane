1b327419f44a0b832b9727e19a6c10ac19b63f7c
Javier Soto <javiers@twitter.com>
2016-01-23 17:40:39 -0800

    Add ensure_xcode_version action to verify that the selected Xcode version that would be used by gym matches your expectation

---
+++ lib/fastlane/actions/ensure_xcode_version.rb
@@ -0,0 +1,59 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      ENSURE_XCODE_VERSION_CUSTOM_VALUE = :ENSURE_XCODE_VERSION_CUSTOM_VALUE
+    end
+
+    class EnsureXcodeVersionAction < Action
+      def self.run(params)
+        required_version = params[:version]
+
+        selected_version = sh "xcversion selected | head -1 | xargs echo -n"
+
+        versions_match = selected_version == "Xcode #{required_version}"
+
+        raise "Selected Xcode version doesn't match your requirement.\nExpected: Xcode #{required_version}\nActual: #{selected_version}" unless versions_match
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Ensure the selected Xcode version with xcode-select matches a value"
+      end
+
+      def self.details
+        "If building your app requires a specific version of Xcode, you can invoke this command before using gym.\n
+        For example, to ensure that a beta version is not accidentally selected to build, which would make uploading to TestFlight fail."
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :version,
+                                       env_name: "FL_ENSURE_XCODE_VERSION",
+                                       description: "Xcode version to verify that is selected",
+                                       is_string: true,
+                                       default_value: false)
+        ]
+      end
+
+      def self.output
+        [
+          ['FL_ENSURE_XCODE_VERSION', 'Xcode version to verify that is selected']
+        ]
+      end
+
+      def self.return_value
+      end
+
+      def self.authors
+        ["@Javi"]
+      end
+
+      def self.is_supported?(platform)
+        [:ios, :mac].include?(platform)
+      end
+    end
+  end
+end
