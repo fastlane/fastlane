9f16bfd04c87455df7878da8387924e9f43f9ee0
Javier Soto <javiers@twitter.com>
2016-01-23 17:55:52 -0800

    Fix style

--- lib/fastlane/actions/ensure_xcode_version.rb
+++ lib/fastlane/actions/ensure_xcode_version.rb
@@ -48,11 +48,11 @@ module Fastlane
       end
 
       def self.authors
-        ["@Javi"]
+        ["Javier Soto"]
       end
 
       def self.is_supported?(platform)
-        [:ios, :mac].include?(platform)
+         [:ios, :mac].include?(platform)
       end
     end
   end
