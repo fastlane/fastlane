c242535092df2d4d4406d8a79c547a2b0899019e
Ortwin Gentz <gentz@futuretap.com>
2016-03-16 10:11:32 +0100

    Support padding as a percentage of the screen size
    Makes more sense because iPad Pro shots need more padding than iPhone 5 shots

--- frameit/lib/frameit/config_parser.rb
+++ frameit/lib/frameit/config_parser.rb
@@ -85,7 +85,7 @@ module Frameit
           end
 
           if key == 'padding'
-            UI.user_error! "padding must be type integer or pair of integers of format 'AxB'" unless value.kind_of?(Integer) || value.split('x').length == 2
+            UI.user_error! "padding must be type integer or pair of integers of format 'AxB' or a percentage" unless value.kind_of?(Integer) || value.split('x').length == 2 || (value.end_with?('%') && value.to_f > 0)
           end
         end
       end
