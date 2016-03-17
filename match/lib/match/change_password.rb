06fb50b648d7bab3daaf97bb3d614621991531fa
Michael Furtak <mfurtak@twitter.com>
2016-03-17 15:40:22 -0400

    Add skip_docs option to match
    The skip_docs option can be used to ask match to avoid creating a README.md
    for the generated git repo.

--- match/lib/match/change_password.rb
+++ match/lib/match/change_password.rb
@@ -4,7 +4,7 @@ module Match
       to ||= ChangePassword.ask_password(message: "New passphrase for Git Repo: ", confirm: false)
       from ||= ChangePassword.ask_password(message: "Old passphrase for Git Repo: ", confirm: true)
       GitHelper.clear_changes
-      workspace = GitHelper.clone(params[:git_url], params[:shallow_clone], manual_password: from)
+      workspace = GitHelper.clone(params[:git_url], params[:shallow_clone], manual_password: from, skip_docs: params[:skip_docs])
       Encrypt.new.clear_password(params[:git_url])
       Encrypt.new.store_password(params[:git_url], to)
 
