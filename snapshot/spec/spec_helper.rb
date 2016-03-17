c300ab2fffde72e0073ededbfc973cc82b59e1bd
Sam Phillips <samp@twitter.com>
2016-03-17 10:48:36 -0400

    Update clearing derived data path and add tests

--- snapshot/spec/spec_helper.rb
+++ snapshot/spec/spec_helper.rb
@@ -10,3 +10,22 @@ end
 require 'snapshot'
 
 WebMock.disable_net_connect!(allow: 'coveralls.io')
+
+# Loading the XCode Project is really slow (>1 second)
+# fake it out for tests
+def fake_out_xcode_project_loading
+  fake_result = <<-EOS
+Information about project "Example":
+    Targets:
+        Example
+        ExampleUITests
+    Build Configurations:
+        Debug
+        Release
+    If no build configuration is specified and -scheme is not passed then "Release" is used.
+    Schemes:
+        Example
+        ExampleUITests
+EOS
+  allow_any_instance_of(FastlaneCore::Project).to receive(:raw_info).and_return fake_result
+end
