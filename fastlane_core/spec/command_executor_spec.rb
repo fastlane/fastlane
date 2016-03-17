95633643e9e634308e4844e0e75c2df29e211b17
Sam Phillips <samp@twitter.com>
2016-03-17 17:06:20 -0400

    Revert "Fixes for flaky test"

--- fastlane_core/spec/command_executor_spec.rb
+++ fastlane_core/spec/command_executor_spec.rb
@@ -9,17 +9,16 @@ describe FastlaneCore do
         # we raise when the line is cleaned up with `strip` afterward
         expect(explodes_on_strip).to receive(:strip).and_raise Errno::EIO
 
-        # Make a fake child process so we have a valid $?
+        # Make a fake child process so we have a valid PID and $? is set correctly
+        child_process_id = nil
         PTY.spawn('echo') do |stdin, stdout, pid|
-          Process.wait(pid)
+          child_process_id = pid
         end
 
         expect(PTY).to receive(:spawn) do |command, &block|
           expect(command).to eq('ls')
-          block.yield fake_std_in, 'not_really_std_out', 'fake_process_id'
+          block.yield fake_std_in, 'not_really_std_out', child_process_id
         end
-
-        expect(Process).to receive(:wait).with('fake_process_id')
 
         result = FastlaneCore::CommandExecutor.execute(command: 'ls')
 
