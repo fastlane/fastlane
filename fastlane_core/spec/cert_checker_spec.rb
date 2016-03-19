94563992325f441343d7f917ff39432a39a0e0fd
Michael Furtak <michael.furtak@gmail.com>
2016-03-19 11:39:39 -0400

    [fastlane_core] Alias installed_identies to the correct spelling

--- fastlane_core/spec/cert_checker_spec.rb
+++ fastlane_core/spec/cert_checker_spec.rb
@@ -2,18 +2,28 @@ require 'spec_helper'
 
 describe FastlaneCore do
   describe FastlaneCore::CertChecker do
-    describe '#installed_identies' do
+    describe '#installed_identities' do
       it 'should print an error when no local code signing identities are found' do
         allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
-        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return("     0 valid identities found\n")
+        expect(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return("     0 valid identities found\n")
         expect(FastlaneCore::UI).to receive(:error).with(/There are no local code signing identities found/)
 
-        FastlaneCore::CertChecker.installed_identies
+        FastlaneCore::CertChecker.installed_identities
       end
 
       it 'should not be fooled by 10 local code signing identities available' do
         allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
-        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return("     10 valid identities found\n")
+        expect(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return("     10 valid identities found\n")
+        expect(FastlaneCore::UI).not_to receive(:error)
+
+        FastlaneCore::CertChecker.installed_identities
+      end
+    end
+
+    describe '#installed_identies' do
+      it 'should be aliased to the correct spelling' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        expect(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return("     10 valid identities found\n")
         expect(FastlaneCore::UI).not_to receive(:error)
 
         FastlaneCore::CertChecker.installed_identies
