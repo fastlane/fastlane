e7d053c8e2c994bfaf92367a44ad529cc07a76bd
Nikolas Falco <nikolas.falco@finantix.com>
2016-03-22 20:05:20 +0100

    Fix #1942 define a different default based on report type (e.g. junit wants xml file) otherwise fallback on previous behaviour.

--- scan/spec/report_collector_spec.rb
+++ scan/spec/report_collector_spec.rb
@@ -3,11 +3,12 @@ describe Scan do
     let (:path) { "./spec/fixtures/boring.log" }
 
     it "ignores invalid report types" do
-      commands = Scan::ReportCollector.new(false, "invalid, html", "/tmp").generate_commands(path)
+      commands = Scan::ReportCollector.new(false, "invalid, html, junit", "/tmp").generate_commands(path)
 
-      expect(commands.count).to eq(1)
+      expect(commands.count).to eq(2)
       expect(commands).to eq({
-        "/tmp/report.html" => "cat './spec/fixtures/boring.log' |  xcpretty --report html --output '/tmp/report.html' &> /dev/null "
+        "/tmp/report.html" => "cat './spec/fixtures/boring.log' |  xcpretty --report html --output '/tmp/report.html' &> /dev/null ",
+		"/tmp/TEST-report.xml" => "cat './spec/fixtures/boring.log' |  xcpretty --report junit --output '/tmp/TEST-report.xml' &> /dev/null "
       })
     end
   end
