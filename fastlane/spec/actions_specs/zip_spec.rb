0c0c1a13b3bc0e268c156d4708815f93a868815b
Felix Krause <krausefx@gmail.com>
2016-04-06 10:28:27 -0700

    Fixed tests

--- fastlane/spec/actions_specs/zip_spec.rb
+++ fastlane/spec/actions_specs/zip_spec.rb
@@ -3,7 +3,7 @@ describe Fastlane do
     describe "zip" do
       it "generates a valid zip command" do
         path = "./fastlane/spec/fixtures/actions/archive.rb"
-        expect(Fastlane::Actions).to receive(:sh).with("zip -r #{path}.zip #{path}")
+        expect(Fastlane::Actions).to receive(:sh).with("zip -r #{path}.zip #{File.basename(path)}")
 
         result = Fastlane::FastFile.new.parse("lane :test do
           zip(path: '#{path}')
