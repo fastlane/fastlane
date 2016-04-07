2258e267ec77b0894c9628ec4dcdc753811258ed
Jerome Lacoste <jerome.lacoste@gmail.com>
2016-04-07 23:18:49 +0200

    A ruby based autocomplete. Supports imported Fastfile and .fastlane folders

--- fastlane/lib/assets/completions/completion.zsh
+++ fastlane/lib/assets/completions/completion.zsh
@@ -1,18 +1,5 @@
 _fastlane_complete() {
-  local word completions
-  word="$1"
-
-  # look for Fastfile either in this directory or fastlane/ then grab the lane names
-  if [[ -e "Fastfile" ]] then
-    file="Fastfile"
-  elif [[ -e "fastlane/Fastfile" ]] then
-    file="fastlane/Fastfile"
-  fi
-
-  # parse 'beta' out of 'lane :beta do', etc
-  completions=`cat $file | grep "^\s*lane \:" | awk -F ':' '{print $2}' | awk -F ' ' '{print $1}'`
-
-  reply=( "${(ps:\n:)completions}" )
+  read -l; local cl="$REPLY"; read -ln; local cp="$REPLY"; reply=(`COMP_SHELL=zsh COMP_LINE="$cl" COMP_POINT="$cp" ~/.fastlane/completions/_fastlane_complete.rb`)
 }
 
 compctl -K _fastlane_complete fastlane
