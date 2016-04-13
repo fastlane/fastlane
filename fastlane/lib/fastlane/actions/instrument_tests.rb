931fe3dd9bfe655552b908634dac7b8594a31559
joshrlesch <josh.r.lesch@gmail.com>
2016-04-13 09:22:22 -0500

    finish up the workflow for deleting emulator

---
+++ fastlane/lib/fastlane/actions/instrument_tests.rb
@@ -0,0 +1,130 @@
+require 'open3'
+
+module Fastlane
+  module Actions
+    module SharedValues
+    end
+
+    class InstrumentedTestsAction < Action
+      def self.run(params)
+        adb = Helper::AdbHelper.new(adb_path: "#{params[:sdk_path]}/platform-tools/adb")
+        gradle = Helper::GradleHelper.new(gradle_path: Dir["./gradlew"].last)
+
+        avd_name = "--name \"#{params[:avd_name]}\""
+        target_id = "--target #{params[:target_id]}"
+        avd_options = params[:avd_options] if not params[:avd_options].nil? # Make this better!!
+        avd_abi = "--abi #{params[:avd_abi]}" if not params[:avd_abi].nil?
+        avd_tag = "--tag #{params[:avd_tag]}" if not params[:avd_tag].nil?
+        create_avd = ["#{params[:sdk_path]}/tools/android", "create avd", avd_name, target_id, avd_abi, avd_tag, avd_options].join(" ")
+        start_avd = ["#{params[:sdk_path]}/tools/emulator", "-avd #{params[:avd_name]}", "-gpu on"]
+        devices = `#{params[:sdk_path]}/tools/android list avd`.chomp
+
+        # Delete avd if one already exists
+        if not devices.match(/#{params[:avd_name]}/).nil?
+          Action.sh("#{params[:sdk_path]}/tools/android delete avd -n #{params[:avd_name]}")
+        end
+
+        Helper.log.info('Creating AVD...'.yellow)
+        Action.sh(create_avd)
+
+        Helper.log.info('Starting AVD....'.yellow)
+        pid = fork do
+          exec Action.sh(start_avd)
+        end
+
+        # Wait for device to be fully booted
+        Helper.log.info("Waiting for emulator to finish booting.....".yellow)
+        while true
+          stdout, stdeerr, status = Open3.capture3("#{params[:sdk_path]}/platform-tools/adb shell getprop sys.boot_completed")
+
+          if stdout.strip == "1"
+            Helper.log.info("Emulator Booted!".green)
+            break
+          end
+        end
+
+        Helper.log.info('Executing Instrumented Tests!'.green)
+        begin
+          gradle.trigger(task: params[:task], flags: params[:flags], serial: nil)
+        ensure
+          Helper.log.info('Shutting down emulator...'.green)
+          adb.trigger(command: 'emu kill', serial: 'emulator-5554')
+
+          Helper.log.info('Deleting emulator....'.green)
+          `#{params[:sdk_path]}/tools/android delete avd -n #{params[:avd_name]}`.chomp
+        end
+      end
+
+      def self.description
+        'Run android instrumented test againts a newly created avd.'
+      end
+
+      def self.details
+        [
+            'Instrumented tests need a emulator or real device to execute against.',
+           ' "This will run against a local avd which will be created/booted/deleted on each run,"'
+        ].join("\n")
+      end
+
+      def self.available_options
+        [
+            FastlaneCore::ConfigItem.new(key: :avd_name,
+                                         env_name: 'AVD_NAME',
+                                         description: 'Name of the avd to be created',
+                                         is_string: true,
+                                         optional: true,
+                                         default: 'Nexus_6'),
+            FastlaneCore::ConfigItem.new(key: :target_id,
+                                         env_name: 'TARGET_ID',
+                                         description: "Target id of the avd to be created, get list of installed target by running command 'android list targets'",
+                                         is_string: true,
+                                         optional: false),
+            FastlaneCore::ConfigItem.new(key: :avd_options,
+                                         env_name: 'AVD_OPTIONS',
+                                         description: "Other avd options in the form of a <option>=<value> list, i.e \"--scale 96dpi --dpi-device 160\"",
+                                         is_string: true,
+                                         optional: true),
+            FastlaneCore::ConfigItem.new(key: :avd_abi,
+                                         env_name: 'AVD_ABI',
+                                         description: 'The ABI to use for the AVD. The default is to auto-select the ABI if the platform has only one ABI for its system images',
+                                         is_string: true,
+                                         optional: true),
+            FastlaneCore::ConfigItem.new(key: :avd_tag,
+                                         env_name: 'AVD_TAG',
+                                         description: 'The sys-img tag to use for the AVD. The default is to auto-select if the platform has only one tag for its system images',
+                                         is_string: true,
+                                         optional: true),
+            FastlaneCore::ConfigItem.new(key: :sdk_path,
+                                         env_name: 'SDK_PATH',
+                                         description: 'The path to your android sdk directory',
+                                         is_string: true,
+                                         optional: true),
+            FastlaneCore::ConfigItem.new(key: :flags,
+                                         env_name: 'GRADLE_FLAGS',
+                                         description: "All parameter flags you want to pass to the gradle command, e.g. `--exitcode --xml file.xml`",
+                                         optional: true,
+                                         is_string: true),
+            FastlaneCore::ConfigItem.new(key: :task,
+                                         env_name: 'GRADLE_TASK',
+                                         description: 'The gradle task you want to execute',
+                                         is_string: true)
+        ]
+      end
+
+      def self.output
+      end
+
+      def self.return_value
+        "Console output from gradle command"
+      end
+
+      def self.authors
+        ["joshrlesch"]
+      end
+
+      def self.is_supported?(platform)
+        platform == :android
+      end
+    end
+  end
+end
