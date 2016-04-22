b7e9a9c8d8bf2dd7ed92594eb2892054560d36cd
Josh Lesch <josh.r.lesch@gmail.com>
2016-04-22 04:19:30 -0500

    fix hound violations

--- fastlane/lib/fastlane/actions/instrumented_tests.rb
+++ fastlane/lib/fastlane/actions/instrumented_tests.rb
@@ -15,15 +15,15 @@ module Fastlane
         # Set up params
         avd_name = "--name \"#{params[:avd_name]}\""
         target_id = "--target #{params[:target_id]}"
-        avd_options = params[:avd_options] if not params[:avd_options].nil? # Make this better!!
-        avd_abi = "--abi #{params[:avd_abi]}" if not params[:avd_abi].nil?
-        avd_tag = "--tag #{params[:avd_tag]}" if not params[:avd_tag].nil?
+        avd_options = params[:avd_options] unless params[:avd_options].nil?
+        avd_abi = "--abi #{params[:avd_abi]}" unless params[:avd_abi].nil?
+        avd_tag = "--tag #{params[:avd_tag]}" unless params[:avd_tag].nil?
         create_avd = ["#{params[:sdk_path]}/tools/android", "create avd", avd_name, target_id, avd_abi, avd_tag, avd_options].join(" ")
         start_avd = ["#{params[:sdk_path]}/tools/emulator", "-avd #{params[:avd_name]}", "-gpu on -no-boot-anim &>#{file.path} &"]
         devices = `#{params[:sdk_path]}/tools/android list avd`.chomp
 
         # Delete avd if one already exists for clean state.
-        if not devices.match(/#{params[:avd_name]}/).nil?
+        unless devices.match(/#{params[:avd_name]}/).nil?
           Action.sh("#{params[:sdk_path]}/tools/android delete avd -n #{params[:avd_name]}")
         end
 
@@ -36,8 +36,8 @@ module Fastlane
 
           # Wait for device to be fully
           Helper.log.info("Waiting for emulator to finish booting.....".yellow)
-          while true
-            stdout, stdeerr, status = Open3.capture3("#{params[:sdk_path]}/platform-tools/adb shell getprop sys.boot_completed")
+          loop do
+            stdout, _stdeerr, _status = Open3.capture3("#{params[:sdk_path]}/platform-tools/adb shell getprop sys.boot_completed")
 
             if stdout.strip == "1"
               Helper.log.info("Emulator Booted!".green)
@@ -77,54 +77,54 @@ module Fastlane
 
       def self.details
         [
-            "Instrumented tests need a emulator or real device to execute against.",
-            "This action will check for a specific avd and created, wait for full boot,",
-            "run gradle command, then deleted that avd on each run."
+          "Instrumented tests need a emulator or real device to execute against.",
+          "This action will check for a specific avd and created, wait for full boot,",
+          "run gradle command, then deleted that avd on each run."
         ].join("\n")
       end
 
       def self.available_options
         [
-            FastlaneCore::ConfigItem.new(key: :avd_name,
-                                         env_name: "AVD_NAME",
-                                         description: "Name of the avd to be created",
-                                         is_string: true,
-                                         optional: false),
-            FastlaneCore::ConfigItem.new(key: :target_id,
-                                         env_name: "TARGET_ID",
-                                         description: "Target id of the avd to be created, get list of installed target by running command 'android list targets'",
-                                         is_string: true,
-                                         optional: false),
-            FastlaneCore::ConfigItem.new(key: :avd_options,
-                                         env_name: "AVD_OPTIONS",
-                                         description: "Other avd options in the form of a <option>=<value> list, i.e \"--scale 96dpi --dpi-device 160\"",
-                                         is_string: true,
-                                         optional: true),
-            FastlaneCore::ConfigItem.new(key: :avd_abi,
-                                         env_name: "AVD_ABI",
-                                         description: "The ABI to use for the AVD. The default is to auto-select the ABI if the platform has only one ABI for its system images",
-                                         is_string: true,
-                                         optional: true),
-            FastlaneCore::ConfigItem.new(key: :avd_tag,
-                                         env_name: "AVD_TAG",
-                                         description: "The sys-img tag to use for the AVD. The default is to auto-select if the platform has only one tag for its system images",
-                                         is_string: true,
-                                         optional: true),
-            FastlaneCore::ConfigItem.new(key: :sdk_path,
-                                         env_name: "SDK_PATH",
-                                         description: "The path to your android sdk directory",
-                                         is_string: true,
-                                         default_value: ENV['SDK_PATH'],
-                                         optional: true),
-            FastlaneCore::ConfigItem.new(key: :flags,
-                                         env_name: "GRADLE_FLAGS",
-                                         description: "All parameter flags you want to pass to the gradle command, e.g. `--exitcode --xml file.xml`",
-                                         optional: true,
-                                         is_string: true),
-            FastlaneCore::ConfigItem.new(key: :task,
-                                         env_name: "GRADLE_TASK",
-                                         description: "The gradle task you want to execute",
-                                         is_string: true)
+          FastlaneCore::ConfigItem.new(key: :avd_name,
+                                       env_name: "AVD_NAME",
+                                       description: "Name of the avd to be created",
+                                       is_string: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :target_id,
+                                       env_name: "TARGET_ID",
+                                       description: "Target id of the avd to be created, get list of installed target by running command 'android list targets'",
+                                       is_string: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :avd_options,
+                                       env_name: "AVD_OPTIONS",
+                                       description: "Other avd options in the form of a <option>=<value> list, i.e \"--scale 96dpi --dpi-device 160\"",
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :avd_abi,
+                                       env_name: "AVD_ABI",
+                                       description: "The ABI to use for the AVD. The default is to auto-select the ABI if the platform has only one ABI for its system images",
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :avd_tag,
+                                       env_name: "AVD_TAG",
+                                       description: "The sys-img tag to use for the AVD. The default is to auto-select if the platform has only one tag for its system images",
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :sdk_path,
+                                       env_name: "SDK_PATH",
+                                       description: "The path to your android sdk directory",
+                                       is_string: true,
+                                       default_value: ENV['SDK_PATH'],
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :flags,
+                                       env_name: "GRADLE_FLAGS",
+                                       description: "All parameter flags you want to pass to the gradle command, e.g. `--exitcode --xml file.xml`",
+                                       optional: true,
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :task,
+                                       env_name: "GRADLE_TASK",
+                                       description: "The gradle task you want to execute",
+                                       is_string: true)
         ]
       end
 
