45086d8248432108e2fd1673d0cf79036721356f
Michael Furtak <mfurtak@twitter.com>
2016-04-25 14:03:17 -0400

    Explicitly pass the team_id given to match through to cert and sigh

--- match/spec/spec_helper.rb
+++ match/spec/spec_helper.rb
@@ -9,3 +9,9 @@ module SpecHelper
 end
 
 WebMock.disable_net_connect!(allow: 'coveralls.io')
+
+RSpec::Matchers.define :a_configuration_matching do |expected|
+  match do |actual|
+    actual._values == expected._values
+  end
+end
