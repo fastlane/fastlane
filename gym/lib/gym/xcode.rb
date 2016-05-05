043479808c0abd8085795b82bdfa1c0d3014978a
Felix Krause <KrauseFx@users.noreply.github.com>
2016-05-05 16:45:39 +0200

    Raise an exception if xcode_version is nil (#4518)
    Taken from https://github.com/fastlane/fastlane/pull/4138
    
    > Came across this during testing on a machine without Xcode installed.
    
    via @ohwutup

--- gym/lib/gym/xcode.rb
+++ gym/lib/gym/xcode.rb
@@ -11,6 +11,7 @@ module Gym
 
       # Below Xcode 7 (which offers a new nice API to sign the app)
       def pre_7?
+        UI.user_error!("Unable to locate Xcode. Please make sure to have Xcode installed on your machine") if xcode_version.nil?
         v = xcode_version
         is_pre = v.split('.')[0].to_i < 7
         is_pre
