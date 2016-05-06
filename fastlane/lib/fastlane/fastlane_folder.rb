e430bee7fd2fe143a5591264dbbafac13f8d14b2
Felix Krause <krausefx@gmail.com>
2016-05-06 15:39:46 +0200

    [fastlane] Improved error handling when Fastfile is not available
    Improved wording

--- fastlane/lib/fastlane/fastlane_folder.rb
+++ fastlane/lib/fastlane/fastlane_folder.rb
@@ -13,14 +13,16 @@ module Fastlane
       return value
     end
 
+    # Path to the Fastfile inside the fastlane folder. This is nil when none is available
     def self.fastfile_path
-      return nil if path.nil?
-      File.join(path, "Fastfile")
+      path = File.join(self.path || '.', 'Fastfile')
+      return path if File.exist?(path)
+      return nil
     end
 
     # Does a fastlane configuration already exist?
     def self.setup?
-      return false unless path
+      return false unless self.fastfile_path
       File.exist?(self.fastfile_path)
     end
 
