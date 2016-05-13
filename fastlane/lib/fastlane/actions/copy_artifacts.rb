5a12ade5ccb2fd3b3a63c2c374dfae29030ed0be
Julian Nadeau <julian@jnadeau.ca>
2016-05-13 14:43:12 -0400

    Write tests for copy_artifacts, comment less obvious parts, make escaping better

--- fastlane/lib/fastlane/actions/copy_artifacts.rb
+++ fastlane/lib/fastlane/actions/copy_artifacts.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'shellwords'
 
 module Fastlane
   module Actions
@@ -7,18 +8,24 @@ module Fastlane
         # we want to make sure that our target folder exist already
         FileUtils.mkdir_p(params[:target_path])
 
-        # Replace any spaces in any of the artifact paths with '\ '
-        artifacts = params[:artifacts].map { |e| e.tr(' ', '\ ') }
+        # Ensure that artifacts is an array
+        artifacts_to_search = [params[:artifacts]].flatten
 
-        if params[:verbose]
-          UI.message("Copying artifacts #{artifacts.join(', ')} to #{params[:target_path]}")
-          UI.message(params[:keep_original] ? "Keeping originals files" : "Not keeping original files")
-        end
+        # If any of the paths include "*", we assume that we are referring to the Unix entries
+        # e.g /tmp/fastlane/* refers to all the files in /tmp/fastlane
+        # We use Dir.glob to expand all those paths, this would create an array of arrays though, so flatten
+        # Lastly, we shell escape everything in case they contain incompatible symbols (e.g. spaces)
+        artifacts = artifacts_to_search.map { |f| f.include?("*") ? Dir.glob(f) : f }.flatten.map(&:shellescape)
+
+        UI.verbose("Copying artifacts #{artifacts.join(', ')} to #{params[:target_path]}")
+        UI.verbose(params[:keep_original] ? "Keeping original files" : "Not keeping original files")
 
         if params[:fail_on_missing]
           missing = artifacts.select { |a| !File.exist?(a) }
-          UI.error "Not all files were present in copy artifacts. \
-                      Missing #{missing.join(', ')}" unless missing.empty?
+          UI.user_error! "Not all files were present in copy artifacts. Missing #{missing.join(', ')}" unless missing.empty?
+        else
+          # If we don't fail on non-existant files, don't try to copy non-existant files
+          artifacts.reject! { |artifact| !File.exist?(artifact) }
         end
 
         if params[:keep_original]
@@ -57,11 +64,6 @@ module Fastlane
                                        default_value: []),
           FastlaneCore::ConfigItem.new(key: :fail_on_missing,
                                        description: "Fail when a source file isn't found",
-                                       is_string: false,
-                                       optional: true,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :verbose,
-                                       description: "Print out additional logs that are useful for debugging or tracking the action",
                                        is_string: false,
                                        optional: true,
                                        default_value: false)
