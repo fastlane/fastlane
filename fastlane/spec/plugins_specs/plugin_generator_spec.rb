146d8ea0d129d620e05e697dfe3253d37109202a
Mark Pirri <mpirri@twitter.com>
2016-05-20 16:28:13 -0400

    One-time initialization in plugin generator spec

--- fastlane/spec/plugins_specs/plugin_generator_spec.rb
+++ fastlane/spec/plugins_specs/plugin_generator_spec.rb
@@ -1,47 +1,62 @@
+initialized = false
+test_ui = nil
+generator = nil
+tmp_dir = nil
+oldwd = nil
+
 describe Fastlane::PluginGenerator do
   describe '#generate' do
-    let(:test_ui) do
-      ui = Fastlane::PluginGeneratorUI.new
-      allow(ui).to receive(:message)
-      allow(ui).to receive(:input)
-      allow(ui).to receive(:confirm)
-      ui
-    end
-    let(:generator) { Fastlane::PluginGenerator.new(test_ui) }
     let(:plugin_name) { "plugin" }
     let(:gem_name) { "fastlane_#{plugin_name}" }
     let(:author) { "Fabricio Devtoolio" }
 
     before(:each) do
-      @tmp_dir = Dir.mktmpdir
-      @oldwd = Dir.pwd
-      Dir.chdir(@tmp_dir)
+      unless initialized
+        test_ui = Fastlane::PluginGeneratorUI.new
+        allow(test_ui).to receive(:message)
+        allow(test_ui).to receive(:input)
+        allow(test_ui).to receive(:confirm)
 
-      expect(test_ui).to receive(:input).and_return(plugin_name)
-      expect(test_ui).to receive(:input).and_return(author)
+        generator = Fastlane::PluginGenerator.new(test_ui)
 
-      generator.generate
+        tmp_dir = Dir.mktmpdir
+        oldwd = Dir.pwd
+        Dir.chdir(tmp_dir)
+
+        expect(test_ui).to receive(:input).and_return(plugin_name)
+        expect(test_ui).to receive(:input).and_return(author)
+
+        generator.generate
+
+        initialized = true
+      end
     end
 
-    after(:each) do
-      Dir.chdir(@oldwd) if @oldwd
-      FileUtils.remove_entry(@tmp_dir) if @tmp_dir
+    after(:all) do
+      Dir.chdir(oldwd) if oldwd
+      FileUtils.remove_entry(tmp_dir) if tmp_dir
+
+      test_ui = nil
+      generator = nil
+      tmp_dir = nil
+      oldwd = nil
+      initialized = false
     end
 
     it "creates gem root directory" do
-      expect(File.directory?(File.join(@tmp_dir, gem_name))).to be(true)
+      expect(File.directory?(File.join(tmp_dir, gem_name))).to be(true)
     end
 
     it "creates a README" do
-      expect(File.exists?(File.join(@tmp_dir, 'README.md'))).to be(true)
+      expect(File.exists?(File.join(tmp_dir, 'README.md'))).to be(true)
     end
 
     it "README contains gem name" do
-      expect(File.read(File.join(@tmp_dir, 'README.md')).include?(gem_name))
+      expect(File.read(File.join(tmp_dir, 'README.md')).include?(gem_name))
     end
 
     it "creates a LICENSE" do
-      expect(File.exists?(File.join(@tmp_dir, 'LICENSE'))).to be(true)
+      expect(File.exists?(File.join(tmp_dir, 'LICENSE'))).to be(true)
     end
 
     it "creates a gemspec" do
