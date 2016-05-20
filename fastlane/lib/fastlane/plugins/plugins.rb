6bcddc9c043dfbe434458cc48f3558a114ad38e5
Michael Furtak <mfurtak@twitter.com>
2016-05-20 13:20:23 -0400

    Add more structure and tests around plugin generation

---
+++ fastlane/lib/fastlane/plugins/plugins.rb
@@ -0,0 +1,5 @@
+require 'fastlane/plugins/plugin_info'
+require 'fastlane/plugins/plugin_generator'
+require 'fastlane/plugins/plugin_generator_ui'
+require 'fastlane/plugins/plugin_info_collector'
+require 'fastlane/plugins/plugin_manager'
