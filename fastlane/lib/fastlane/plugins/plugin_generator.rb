64acbbb858f4ce7ce79e6716ff50d80e0804e939
Mark Pirri <mpirri@twitter.com>
2016-05-20 15:08:58 -0400

    Minimal spec for PluginGeneratoer

--- fastlane/lib/fastlane/plugins/plugin_generator.rb
+++ fastlane/lib/fastlane/plugins/plugin_generator.rb
@@ -4,15 +4,24 @@ module Fastlane
 
     def initialize(ui = PluginGeneratorUI.new)
       @ui = ui
+      @plugin_info_collector = PluginInfoCollector.new(ui)
     end
 
     # entry point
     def generate
-      collect_info
+      plugin_info = @plugin_info_collector.collect_info
+      generate_plugin(plugin_info)
     end
 
   private
 
+    def generate_plugin(plugin_info)
+      Dir.mkdir(plugin_info.gem_name)
+
+      require 'fileutils'
+      File.write('README.md', plugin_info.gem_name)
+      FileUtils.touch('LICENSE')
+    end
 
   end
 end
