e8707a2f559a56368591f151296c856afce8b6f0
KrauseFx <KrauseFx@gmail.com>
2016-05-20 20:55:02 -0700

    Added generating of available plugins based on RubyGems API ❤️

---
+++ fastlane/docs/AvailablePlugins.md
@@ -0,0 +1,7 @@
+### Available Plugins
+
+| Plugin Name | Description | Downloads
+--------------|-------------|----------|----------
+[fastlane-qyer](http://github.com/icyleaf/fastlane-qyer) | qyer mobile app actions(test, build, upload) for fastlane | 2516
+`fastlane-actions-xcode_get_product_name` | Fastlane action that helps getting your xcode's product name | 1402
+[fastlane-yaml-init](http://github.com/JaniJegoroff/fastlane-yaml-init) | Inits fastlane folder structure and enables metadata configuration in YAML format | 705
---
+++ fastlane/lib/fastlane/plugin_fetcher.rb
@@ -0,0 +1,45 @@
+module Fastlane
+  # Use the RubyGems API to get all fastlane plugins
+  class PluginFetcher
+    require 'fastlane_core'
+    require 'fastlane/plugin_manager'
+
+    def run
+      require 'json'
+      require 'open-uri'
+      url = "https://rubygems.org/api/v1/search.json?query=fastlane-" # TODO: change to `PluginManager.plugin_prefix`
+      results = JSON.parse(open(url).read)
+      @plugins = results.collect do |current|
+        FastlanePlugin.new(current)
+      end
+
+      lib_path = FastlaneCore::Helper.gem_path('fastlane')
+      template_path = File.join(lib_path, "lib/assets/Plugins.md.erb")
+      md = ERB.new(File.read(template_path), nil, '<>').result(binding) # http://www.rrn.dk/rubys-erb-templating-system
+      
+      puts md
+      File.write("docs/AvailablePlugins.md", md)
+    end
+  end
+
+  class FastlanePlugin
+    attr_accessor :full_name
+    attr_accessor :name
+    attr_accessor :downloads
+    attr_accessor :info
+    attr_accessor :homepage
+
+    def initialize(hash)
+      self.full_name = hash["name"]
+      self.name = self.full_name.gsub(PluginManager.plugin_prefix, '')
+      self.downloads = hash["downloads"]
+      self.info = hash["info"]
+      self.homepage = hash["homepage_uri"]
+    end
+
+    def linked_title
+      return "`#{name}`" if homepage.to_s.length == 0
+      return "[#{name}](#{homepage})"
+    end
+  end
+end
