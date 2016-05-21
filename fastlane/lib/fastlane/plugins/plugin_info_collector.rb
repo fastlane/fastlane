532a9a6fe97ec3038deacb16b2160abcf5ca27d0
Michael Furtak <mfurtak@twitter.com>
2016-05-21 12:57:13 -0400

    Address feedback, adapt to new prefix, fix paths
    * Improve ui object mocking to prevent test hangs if an
      interactive method is not properly mocked
    * Fix file generation paths to be correctly start inside
      the gem name directory
    * Fix gemspec evaluation during tests to account for its
      new location relative to the temp directory
    * Fix plugin info collection to be aware of the changed
      fastlane plugin prefix value
    * PluginInfo can generate the require_path from the gem_name
    * Command name changed to 'new_plugin'

--- fastlane/lib/fastlane/plugins/plugin_info_collector.rb
+++ fastlane/lib/fastlane/plugins/plugin_info_collector.rb
@@ -17,30 +17,49 @@ module Fastlane
 
     def collect_plugin_name
       plugin_name = nil
+
       loop do
         plugin_name = @ui.input("What would you like to be the name of your plugin?")
+
         unless plugin_name_valid?(plugin_name)
           fixed_name = fix_plugin_name(plugin_name)
+
           if plugin_name_valid?(fixed_name)
-            plugin_name = fixed_name if @ui.confirm("Is '#{fixed_name}' OK?")
+            plugin_name = fixed_name if @ui.confirm("Is '#{fixed_name}' okay?")
           end
         end
 
         break if plugin_name_valid?(plugin_name)
 
-        @ui.message("Plugin names can only contain lower case letters, numbers, and underscores.")
+        @ui.message("Plugin names can only contain lower case letters, numbers, and underscores")
+        @ui.message("and should not contain 'fastlane' or 'plugin'.")
       end
 
       plugin_name
     end
 
-    # Consider testing this more directly?
     def plugin_name_valid?(name)
-      /^[a-z0-9_]+$/ =~ name && !name.downcase.start_with?('fastlane_')
+      # Only lower case letters, numbers and underscores allowed
+      /^[a-z0-9_]+$/ =~ name &&
+        # Does not contain the words 'fastlane' or 'plugin' since those will become
+        # part of the gem name
+        [/fastlane/, /plugin/].none? { |regex| regex =~ name }
     end
 
+    # Applies a series of replacement rules to turn the requested plugin name into one
+    # that is acceptable, returning that suggestion
     def fix_plugin_name(name)
-      name.to_s.downcase.gsub(/[\- ]/, '_').gsub(/[^a-z0-9_]/, '').gsub(/^fastlane_/, '')
+      name = name.to_s.downcase
+      fixes = {
+        /[\- ]/        => '_', # dashes and spaces become underscores
+        /[^a-z0-9_]/   => '',  # anything other than lower case letters, numbers and underscores is removed
+        /fastlane[_]?/ => '',  # 'fastlane' or 'fastlane_' is removed
+        /plugin[_]?/ => ''   # 'plugin' or 'plugin_' is removed
+      }
+      fixes.each do |regex, replacement|
+        name = name.gsub(regex, replacement)
+      end
+      name
     end
 
     #
@@ -50,7 +69,7 @@ module Fastlane
     def collect_author
       author = nil
       loop do
-        author = @ui.input("What is the name of the plugin author?")
+        author = @ui.input("What is the name of the plugin's author?")
         break if author_valid?(author)
 
         @ui.message('An author name is required.')
@@ -62,6 +81,5 @@ module Fastlane
     def author_valid?(author)
       !author.to_s.strip.empty?
     end
-
   end
 end
