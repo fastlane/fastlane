9c1e7e218916239069984486bffbf527cb079118
Michael Furtak <mfurtak@twitter.com>
2016-05-20 21:03:49 -0400

    Begin templating files as part of plugin generation
    Test the correctness of the gemspec by evaluating it and
    checking its values

--- fastlane/lib/fastlane/plugins/plugin_generator.rb
+++ fastlane/lib/fastlane/plugins/plugin_generator.rb
@@ -1,27 +1,52 @@
+require 'fileutils'
+require 'erb'
+
 module Fastlane
   class PluginGenerator
-    attr_reader :ui
-
-    def initialize(ui = PluginGeneratorUI.new)
+    def initialize(ui             = PluginGeneratorUI.new,
+                   info_collector = PluginInfoCollector.new(ui))
       @ui = ui
-      @plugin_info_collector = PluginInfoCollector.new(ui)
+      @info_collector = info_collector
     end
 
     # entry point
     def generate
-      plugin_info = @plugin_info_collector.collect_info
-      generate_plugin(plugin_info)
+      plugin_info = @info_collector.collect_info
+
+      generate_paths(plugin_info)
+
+      generate_gemspec(plugin_info)
+      generate_readme(plugin_info)
+      generate_version(plugin_info)
+      generate_license(plugin_info)
     end
 
-  private
-
-    def generate_plugin(plugin_info)
+    def generate_paths(plugin_info)
       Dir.mkdir(plugin_info.gem_name)
+      FileUtils.mkdir_p(File.join('lib', plugin_info.gem_name))
+    end
 
-      require 'fileutils'
-      File.write('README.md', plugin_info.gem_name)
+    def generate_gemspec(plugin_info)
+      write_template(plugin_info, 'plugin.gemspec.erb', "#{plugin_info.gem_name}.gemspec")
+    end
+
+    def generate_version(plugin_info)
+      write_template(plugin_info, 'version.rb.erb', File.join('lib', plugin_info.gem_name, 'version.rb'))
+    end
+
+    def generate_readme(plugin_info)
+      write_template(plugin_info, 'README.md.erb', 'README.md')
+    end
+
+    def generate_license(plugin_info)
       FileUtils.touch('LICENSE')
     end
 
+    def write_template(plugin_info, template_name, dest_path)
+      template = File.join(File.dirname(__FILE__), 'templates', template_name)
+      erb = ERB.new(File.read(template))
+      result = erb.result(plugin_info.get_binding)
+      File.write(dest_path, result)
+    end
   end
 end
--- fastlane/spec/plugins_specs/plugin_generator_spec.rb
+++ fastlane/spec/plugins_specs/plugin_generator_spec.rb
@@ -1,3 +1,5 @@
+require 'rubygems'
+
 initialized = false
 test_ui = nil
 generator = nil
@@ -47,26 +49,39 @@ describe Fastlane::PluginGenerator do
       expect(File.directory?(File.join(tmp_dir, gem_name))).to be(true)
     end
 
-    it "creates a README" do
+    it "creates a README that contains the gem name" do
       expect(File.exists?(File.join(tmp_dir, 'README.md'))).to be(true)
+
+      readme_contents = File.read(File.join(tmp_dir, 'README.md'))
+
+      expect(readme_contents).to include(gem_name)
+      expect(readme_contents.length).to be > 100
     end
 
-    it "README contains gem name" do
-      expect(File.read(File.join(tmp_dir, 'README.md')).include?(gem_name))
+    it "creates a module for the VERSION" do
+      # We'll be asserting that this file is valid Ruby when we check
+      # the value of the version as evaluated by the gemspec!
+      expect(File.exists?(File.join('lib', gem_name, 'version.rb'))).to be(true)
     end
 
     it "creates a LICENSE" do
       expect(File.exists?(File.join(tmp_dir, 'LICENSE'))).to be(true)
     end
 
-    it "creates a gemspec" do
+    it "creates a complete, valid gemspec" do
+      gemspec_file = File.join(tmp_dir, "#{gem_name}.gemspec")
 
+      expect(File.exists?(gemspec_file)).to be(true)
+
+      # If we evaluate the contents of the generated gemspec file,
+      # we'll get the Gem Specification object back out, which
+      # ensures that the syntax is valid, and lets us interrogate
+      # the values!
+      gemspec = eval(File.read(gemspec_file))
+
+      expect(gemspec.name).to eq(gem_name)
+      expect(gemspec.author).to eq(author)
+      expect(gemspec.version).to eq(Gem::Version.new('0.1.0'))
     end
-
-    it "creates a lib directory" do
-
-    end
-
-
   end
 end
