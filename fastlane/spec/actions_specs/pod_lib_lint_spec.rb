a9d79e82ad398f4daaf9498c5f2936f23d1d23d7
Marcelo Fabri <marcelofabri@users.noreply.github.com>
2016-05-22 21:58:34 -0300

    Adding more options to pod_lib_lint action (#4771)

--- fastlane/spec/actions_specs/pod_lib_lint_spec.rb
+++ fastlane/spec/actions_specs/pod_lib_lint_spec.rb
@@ -24,6 +24,38 @@ describe Fastlane do
 
         expect(result).to eq("bundle exec pod lib lint --allow-warnings")
       end
+
+      it "generates the correct pod lib lint command with quick parameter" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          pod_lib_lint(quick: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("bundle exec pod lib lint --quick")
+      end
+
+      it "generates the correct pod lib lint command with private parameter" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          pod_lib_lint(private: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("bundle exec pod lib lint --private")
+      end
+
+      it "generates the correct pod lib lint command with fail-fast parameter" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          pod_lib_lint(fail_fast: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("bundle exec pod lib lint --fail-fast")
+      end
+
+      it "generates the correct pod lib lint command with use-libraries parameter" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          pod_lib_lint(use_libraries: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("bundle exec pod lib lint --use-libraries")
+      end
     end
   end
 end
