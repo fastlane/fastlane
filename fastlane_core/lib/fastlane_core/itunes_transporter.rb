e58177f5422b9d8a96cca9844a51df5a843d9e28
znerol <lo+github@znerol.ch>
2016-05-24 08:49:15 +0200

    Only query for xcode version on macs (#4631)

--- fastlane_core/lib/fastlane_core/itunes_transporter.rb
+++ fastlane_core/lib/fastlane_core/itunes_transporter.rb
@@ -280,7 +280,7 @@ module FastlaneCore
     def initialize(user = nil, password = nil, use_shell_script = false, team_id = nil)
       # Xcode 6.x doesn't have the same iTMSTransporter Java setup as later Xcode versions, so
       # we can't default to using the better direct Java invocation strategy for those versions.
-      use_shell_script ||= Helper.xcode_version.start_with?('6.')
+      use_shell_script ||= Helper.is_mac? && Helper.xcode_version.start_with?('6.')
       use_shell_script ||= !ENV['FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT'].nil?
 
       # First, see if we have an application specific password
