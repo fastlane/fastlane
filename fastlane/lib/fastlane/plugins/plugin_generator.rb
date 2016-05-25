214a9edf42284d4dcc980b4bed6da6c4ac19f18f
Michael Furtak <mfurtak@twitter.com>
2016-05-25 16:01:12 -0400

    Add helper file generation to the plugin generator

--- fastlane/lib/fastlane/plugins/plugin_generator.rb
+++ fastlane/lib/fastlane/plugins/plugin_generator.rb
@@ -20,6 +20,7 @@ module Fastlane
       generate_version(plugin_info)
       generate_license(plugin_info)
       generate_action(plugin_info)
+      generate_helper(plugin_info)
     end
 
     def generate_paths(plugin_info)
@@ -46,6 +47,10 @@ module Fastlane
 
     def generate_action(plugin_info)
       write_template(plugin_info, 'action.rb.erb', plugin_path(plugin_info, 'lib', plugin_info.actions_path, "#{plugin_info.plugin_name}_action.rb"))
+    end
+
+    def generate_helper(plugin_info)
+      write_template(plugin_info, 'helper.rb.erb', plugin_path(plugin_info, 'lib', plugin_info.helper_path, "#{plugin_info.plugin_name}_helper.rb"))
     end
 
     def write_template(plugin_info, template_name, dest_path)
