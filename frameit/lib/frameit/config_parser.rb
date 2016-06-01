e8cd346df29f8b15b21286c8f29cb653829a9b5d
Felix Krause <KrauseFx@users.noreply.github.com>
2016-05-31 19:43:51 -0700

    [frameit] Show full device (#4929)
    * Add option to show complete device
    
    For some apps, the bottom of the screen is important. This allows the
    developer to specify that this is the case.
    Frameit will then shrink the screenshot (if necessary) to ensure that
    the entire device and frame is visible in the framed output.
    
    * [frameit] Code style improvements

--- frameit/lib/frameit/config_parser.rb
+++ frameit/lib/frameit/config_parser.rb
@@ -59,33 +59,35 @@ module Frameit
     # Make sure the paths/colors are valid
     def validate_values(values)
       values.each do |key, value|
-        if value.kind_of? Hash
+        if value.kind_of?(Hash)
           validate_values(value) # recursive call
         else
           if key == 'font'
-            UI.user_error! "Could not find font at path '#{File.expand_path(value)}'" unless File.exist? value
+            UI.user_error!("Could not find font at path '#{File.expand_path(value)}'") unless File.exist?(value)
           end
 
           if key == 'fonts'
-            UI.user_error! "`fonts` must be an array" unless value.kind_of? Array
+            UI.user_error!("`fonts` must be an array") unless value.kind_of?(Array)
 
             value.each do |current|
-              UI.user_error! "You must specify a font path" if current.fetch('font', '').length == 0
-              UI.user_error! "Could not find font at path '#{File.expand_path(current.fetch('font'))}'" unless File.exist? current.fetch('font')
-              UI.user_error! "`supported` must be an array" unless current.fetch('supported', []).kind_of? Array
+              UI.user_error!("You must specify a font path") if current.fetch('font', '').length == 0
+              UI.user_error!("Could not find font at path '#{File.expand_path(current.fetch('font'))}'") unless File.exist?(current.fetch('font'))
+              UI.user_error!("`supported` must be an array") unless current.fetch('supported', []).kind_of? Array
             end
           end
 
           if key == 'background'
-            UI.user_error! "Could not find background image at path '#{File.expand_path(value)}'" unless File.exist? value
+            UI.user_error!("Could not find background image at path '#{File.expand_path(value)}'") unless File.exist? value
           end
 
           if key == 'color'
-            UI.user_error! "Invalid color '#{value}'. Must be valid Hex #123123" unless value.include? "#"
+            UI.user_error!("Invalid color '#{value}'. Must be valid Hex #123123") unless value.include?("#")
           end
 
           if key == 'padding'
-            UI.user_error! "padding must be type integer or pair of integers of format 'AxB'" unless value.kind_of?(Integer) || value.split('x').length == 2
+            unless value.kind_of?(Integer) || value.split('x').length == 2
+              UI.user_error!("padding must be type integer or pair of integers of format 'AxB'")
+            end
           end
         end
       end
