70f3bbe4f9c261879bbf00f83c163376633f3c1f
Valerio Mazzeo <valerio.mazzeo@workangel.com>
2016-06-01 18:00:29 +0100

    implemented jira transition

--- fastlane/lib/fastlane/actions/jira_transition.rb
+++ fastlane/lib/fastlane/actions/jira_transition.rb
@@ -6,13 +6,14 @@ module Fastlane
         Actions.verify_gem!('jira-ruby')
         require 'jira'
 
-        site         = params[:url]
-        context_path = ""
-        auth_type    = :basic
-        username     = params[:username]
-        password     = params[:password]
-        project_key  = params[:project_key]
-        transition   = params[:transition]
+        site          = params[:url]
+        context_path  = ""
+        auth_type     = :basic
+        username      = params[:username]
+        password      = params[:password]
+        project_key   = params[:project_key]
+        transition_id = params[:transition_id]
+        comment_text  = params[:comment]
 
         options = {
                     site: site,
@@ -23,11 +24,28 @@ module Fastlane
                   }
 
         client = JIRA::Client.new(options)
-        project = client.Project.find(project_key)
-        puts Actions.lane_context[SharedValues::FL_CHANGELOG]
-        #issue = client.Issue.find(ticket_id)
-        #comment = issue.comments.build
-        #comment.save({ 'body' => comment_text })
+
+        if Actions.lane_context[SharedValues::FL_CHANGELOG].nil?
+          changelog_configuration = FastlaneCore::Configuration.create(Actions::ChangelogFromGitCommitsAction.available_options, {})
+          Actions::ChangelogFromGitCommitsAction.run(changelog_configuration)
+        end
+
+        issue_ids = Actions.lane_context[SharedValues::FL_CHANGELOG].scan(/#{project_key}-\d+/i).uniq
+
+        issue_ids.each do |issue_id|
+          begin
+            issue = client.Issue.find(issue_id)
+            transition = issue.transitions.build
+            transition.save!("transition" => { "id" => transition_id })
+
+            if !comment_text.nil?
+              comment = issue.comments.build
+              comment.save({"body" => comment_text})
+            end
+          rescue Exception => e
+            "Skipping issue #{issue_id}"
+          end
+        end
       end
 
       #####################################################
@@ -59,17 +77,24 @@ module Fastlane
                                          UI.user_error!("No password") if value.to_s.length == 0
                                        end),
           FastlaneCore::ConfigItem.new(key: :project_key,
-                                       env_name: "FL_JIRA_PROJECT_KEY",
+                                       env_name: "FL_JIRA_TRANSITION_PROJECT_KEY",
                                        description: "Project Key for Jira, i.e. IOS",
                                        verify_block: proc do |value|
                                          UI.user_error!("No Project Key specified") if value.to_s.length == 0
                                        end),
-          FastlaneCore::ConfigItem.new(key: :transition,
-                                       env_name: "FL_JIRA_TRANSITION",
+          FastlaneCore::ConfigItem.new(key: :transition_id,
+                                       env_name: "FL_JIRA_TRANSITION_TRANSITION_ID",
                                        description: "Transition to apply to the tickets referenced in the changelog",
                                        verify_block: proc do |value|
-                                         UI.user_error!("No transition specified") if value.to_s.length == 0
-                                       end)
+                                         UI.user_error!("No transition id specified") if value.to_s.length == 0
+                                       end),
+        FastlaneCore::ConfigItem.new(key: :comment,
+                                     env_name: "FL_JIRA_TRANSITION_COMMENT",
+                                     description: "Comment to add if the transition is applied",
+                                     optional: true,
+                                     verify_block: proc do |value|
+                                       UI.user_error!("No transition id specified") if value.to_s.length == 0
+                                     end)
         ]
       end
 
