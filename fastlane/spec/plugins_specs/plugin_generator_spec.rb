84aa8cbb7dc314b3ea02b0b6bdfd25045f978b79
KrauseFx <KrauseFx@gmail.com>
2016-05-31 19:42:45 -0700

    Added detection if plugin is already taken on RubyGems

--- fastlane/spec/plugins_specs/plugin_generator_spec.rb
+++ fastlane/spec/plugins_specs/plugin_generator_spec.rb
@@ -17,6 +17,10 @@ describe Fastlane::PluginGenerator do
     let(:summary) { plugin_info.summary }
 
     before(:each) do
+      stub_request(:get, "https://rubygems.org/api/v1/gems/tester_thing.json").
+        with(headers: {'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent' => 'Ruby'}).
+        to_return(status: 200, body: nil, headers: {})
+
       unless initialized
         test_ui = Fastlane::PluginGeneratorUI.new
         allow(test_ui).to receive(:message)
