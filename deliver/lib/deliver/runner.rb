5d651ecbe5383dc09c8a731ffa6c165618ed8689
Michael Furtak <mfurtak@twitter.com>
2016-06-02 14:10:37 -0400

    Add :itc_provider option for deliver and pilot
    This provides an explicit option for controlling use of the
    -itc_provider option in the iTMSTransporter

--- deliver/lib/deliver/runner.rb
+++ deliver/lib/deliver/runner.rb
@@ -78,7 +78,7 @@ module Deliver
         )
       end
 
-      transporter = FastlaneCore::ItunesTransporter.new(options[:username])
+      transporter = FastlaneCore::ItunesTransporter.new(options[:username], nil, false, options[:itc_provider])
       result = transporter.upload(options[:app].apple_id, package_path)
       UI.user_error!("Could not upload binary to iTunes Connect. Check out the error above") unless result
     end
