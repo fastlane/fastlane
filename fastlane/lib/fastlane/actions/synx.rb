b5da247dd045e0af579e49c83375523386ce4d61
Afonso Graça <afonso.graca@me.com>
2016-06-09 17:36:06 +0100

    fix: houndci-bot format violations

--- fastlane/lib/fastlane/actions/synx.rb
+++ fastlane/lib/fastlane/actions/synx.rb
@@ -4,11 +4,10 @@ module Fastlane
     end
 
     class SynxAction < Action
-
       def self.is_supported?(platform)
         [:ios, :mac].include?(platform)
       end
-      
+
       def self.run(params)
         Actions.verify_gem!("synx")
 
@@ -43,37 +42,37 @@ module Fastlane
                                        env_name: "FL_SYNX_PRUNE",
                                        description: "Remove source files and image resources that are not referenced by the the xcode project",
                                        optional: true,
-                                       is_string: false, 
+                                       is_string: false,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :no_color,
                                        env_name: "FL_SYNX_NO_COLOR",
                                        description: "Remove all color from the output",
                                        optional: true,
-                                       is_string: false, 
+                                       is_string: false,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :no_default_exclusions,
                                        env_name: "FL_SYNX_NO_DEFAULT_EXCLUSIONS",
                                        description: "Do not use the default exclusions of /Libraries, /Frameworks, and /Products",
                                        optional: true,
-                                       is_string: false, 
+                                       is_string: false,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :no_sort_by_name,
                                        env_name: "FL_SYNX_NO_SORT_BY_NAME",
                                        description: "Disable sorting groups by name",
                                        optional: true,
-                                       is_string: false, 
+                                       is_string: false,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :quiet,
                                        env_name: "FL_SYNX_QUIET",
                                        description: "Silence all output",
                                        optional: true,
-                                       is_string: false, 
+                                       is_string: false,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :exclusion,
                                        env_name: "FL_SYNX_EXCLUSION",
                                        description: "Ignore an Xcode group while syncing",
                                        optional: true,
-                                       is_string: true, 
+                                       is_string: true,
                                        default_value: nil)
         ]
       end
