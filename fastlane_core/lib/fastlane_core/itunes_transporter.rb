5638ca1765f6d5a812e982a8848496f02ebf685f
David Ohayon <ohayon.1@gmail.com>
2016-06-10 11:31:22 -0400

    add feature manager and refactor itms transporter

--- fastlane_core/lib/fastlane_core/itunes_transporter.rb
+++ fastlane_core/lib/fastlane_core/itunes_transporter.rb
@@ -284,7 +284,7 @@ module FastlaneCore
       # Xcode 6.x doesn't have the same iTMSTransporter Java setup as later Xcode versions, so
       # we can't default to using the better direct Java invocation strategy for those versions.
       use_shell_script ||= Helper.is_mac? && Helper.xcode_version.start_with?('6.')
-      use_shell_script ||= !ENV['FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT'].nil?
+      use_shell_script ||= Feature.enabled?('FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT')
 
       # First, see if we have an application specific password
       data = CredentialsManager::AccountManager.new(user: user,
@@ -300,7 +300,6 @@ module FastlaneCore
         @user = data.user
         @password ||= data.password
       end
-
       @transporter_executor = use_shell_script ? ShellScriptTransporterExecutor.new : JavaTransporterExecutor.new
       @provider_short_name = provider_short_name
     end
