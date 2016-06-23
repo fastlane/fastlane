14ea820d33ee8d4bc18bb5680b518fe3f6ea5221
David Ohayon <ohayon.1@gmail.com>
2016-06-23 10:59:06 -0400

    start to crash beta

---
+++ fastlane/lib/fastlane/features.rb
@@ -0,0 +1,2 @@
+FastlaneCore::Feature.register(env_var: 'FASTLANE_ENABLE_CRASHLYTICS_BETA_INITIALIZATION',
+                           description: 'Allow beta_init command to setup Beta by Crashlytics')
