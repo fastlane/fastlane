1dc393b2bf273836a08dd495d7f7691773629716
Massimo Oliviero <massimo.oliviero@gmail.com>
2016-06-28 14:47:44 +0200

    Fixed spec

--- fastlane/spec/actions_specs/unzip_spec.rb
+++ fastlane/spec/actions_specs/unzip_spec.rb
@@ -4,7 +4,7 @@ describe Fastlane do
       it "generates a valid unzip command" do
         path = "./fastlane/spec/fixtures/actions/example_action.rb.zip"
 
-        expect(Fastlane::Actions).to receive(:sh).with("unzip -o ./fastlane/spec/fixtures/actions/example_action.rb.zip")
+        expect(Fastlane::Actions).to receive(:sh).with("unzip -o ./fastlane/spec/fixtures/actions/example_action.rb.zip", log: false)
 
         result = Fastlane::FastFile.new.parse("lane :test do
           unzip(file: '#{path}')
