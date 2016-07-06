da0bab00764446d9d597a7d01f08120f113fe2a0
Mark Pirri <mpirri@twitter.com>
2016-07-06 13:31:22 -0400

    Add pilot feature to wait for a build train to appear on iTunes Connect if not present

--- pilot/lib/pilot.rb
+++ pilot/lib/pilot.rb
@@ -1,5 +1,9 @@
+# fastlane_core must be required before 'pilot/features' which depends on it for FastlaneCore::Feature
+require "fastlane_core"
+
 require "json"
 require "pilot/version"
+require 'pilot/features'
 require "pilot/options"
 require "pilot/manager"
 require "pilot/build_manager"
@@ -7,7 +11,6 @@ require "pilot/tester_manager"
 require "pilot/tester_importer"
 require "pilot/tester_exporter"
 
-require "fastlane_core"
 require "spaceship"
 require "terminal-table"
 
