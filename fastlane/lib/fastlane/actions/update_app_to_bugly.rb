ee1032812d11802363db6c7ac1d25cd37c81b29f
pudgeli <402265710@qq.com>
2016-07-22 14:55:42 +0800

    *add api to upload/update apk to bugly

--- fastlane/lib/fastlane/actions/update_app_to_bugly.rb
+++ fastlane/lib/fastlane/actions/update_app_to_bugly.rb
@@ -15,40 +15,45 @@ module Fastlane
         require 'json'
         # fastlane will take care of reading in the parameter and fetching the environment variable:
         UI.message "file path: #{params[:file_path]}"
-        result_file = File.new("update_app_to_bugly_result.json", "w+")
+        result_file = File.new('update_app_to_bugly_result.json', 'w+')
         result_file.close
         json_file = 'update_app_to_bugly_result.json'
-
-        if ( !"#{params[:secret]}".to_s.nil? and !"#{params[:secret]}".to_s.empty?)
+        secret = ''
+        if !params[:secret].nil? && !params[:secret].empty?
           secret = " -F \"secret=#{params[:secret]}\" "
           UI.message "secret:#{secret}"
         end
-        if ( !"#{params[:title]}".to_s.nil? and  !"#{params[:title]}".to_s.empty?)
+        title = ''
+        if !params[:title].nil? && !params[:title].to_s.empty?
           title = " -F \"title=#{params[:title]}\" "
           UI.message "title:#{title}"
         end
-        if ( !"#{params[:desc]}".to_s.nil? and  !"#{params[:desc]}".to_s.empty?)
+        desc = ''
+        if !params[:desc].nil? && !params[:desc].to_s.empty?
           desc = " -F \"description=#{params[:desc]}\" "
           UI.message "desc:#{desc}"
         end
-        if ( !"#{params[:users]}".to_s.nil? and  !"#{params[:users]}".to_s.empty?)
+        users = ''
+        if !params[:users].nil? && !params[:users].empty?
           users = " -F \"users=#{params[:users]}\" "
           UI.message "users:#{users}"
         end
-        if ( !"#{params[:password]}".to_s.nil? and  !"#{params[:password]}".to_s.empty?)
+        password = ''
+        if !params[:password].nil? && !params[:password].empty?
           password = " -F \"password=#{params[:password]}\" "
           UI.message "password:#{password}"
         end
-        if ( !"#{params[:download_limit]}".to_s.nil? and "#{params[:download_limit]}".to_i > 0)
+        download_limit = ''
+        if !params[:download_limit].nil? && params[:download_limit] > 0
           download_limit = " -F \"download_limit=#{params[:download_limit]}\" "
           UI.message "download_limit:#{download_limit}"
         end
-        cmd = "curl --insecure -X \"PUT\" -F \"file=@#{params[:file_path]}\" -F \"exp_id=#{params[:exp_id]}\"" + "#{title}".to_s + "#{desc}".to_s + "#{secret}".to_s + "#{users}".to_s + "#{password}".to_s + "#{download_limit}".to_s + "https://api.bugly.qq.com/beta/apiv1/exp?app_key=#{params[:app_key]} -o #{json_file}"
+        cmd = "curl --insecure -X \"PUT\" -F \"file=@#{params[:file_path]}\" -F \"exp_id=#{params[:exp_id]}\"" + title + desc + secret + users + password + download_limit + "https://api.bugly.qq.com/beta/apiv1/exp?app_key=#{params[:app_key]} -o " + json_file
         sh(cmd)
         obj = JSON.parse(File.read(json_file))
-        ret = obj["rtcode"]
+        ret = obj['rtcode']
         if ret == 0
-          url = obj["data"]["url"]
+          url = obj['data']['url']
           Actions.lane_context[SharedValues::UPDATE_APP_TO_BUGLY_DOWNLOAD_URL] = url
         end
       end
@@ -58,80 +63,71 @@ module Fastlane
       #####################################################
 
       def self.description
-        "A short description with <= 80 characters of what this action does"
+        'A short description with <= 80 characters of what this action does'
       end
 
       def self.details
         # Optional:
         # this is your chance to provide a more detailed description of this action
-        "You can use this action to do cool things..."
+        'You can use this action to do cool things...'
       end
 
       def self.available_options
-        # Define all options your action supports.       
+        # Define all options your action supports.
         # Below a few examples
         [
           FastlaneCore::ConfigItem.new(key: :file_path,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_FILE_PATH", 
-                                       description: "file path for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_FILE_PATH',
+                                       description: 'file path for UploadAppToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                        UI.user_error!("No file path for UploadAppToBuglyAction given, pass using `file_path: 'path'`") unless value and !value.empty?
-                                        UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
-                                       end
-                                       ),
+                                         UI.user_error!("No file path for UploadAppToBuglyAction given, pass using `file_path: 'path'`") unless value && !value.empty?
+                                         UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :app_key,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_APP_KEY",
-                                       description: "app key for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_APP_KEY',
+                                       description: 'app key for UploadAppToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                        UI.user_error!("NO app_key for UploadAppToBuglyAction given, pass using `app_key: 'app_key'`") unless value and !value.empty?
-                                       end
-                                       ),
+                                         UI.user_error!("NO app_key for UploadAppToBuglyAction given, pass using `app_key: 'app_key'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :exp_id,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_EXP_ID",
-                                       description: "exp id for UploadAppToBuglyAction",
-                                       is_string: true, 
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_EXP_ID',
+                                       description: 'exp id for UploadAppToBuglyAction',
+                                       is_string: true,
                                        verify_block: proc do |value|
-                                        UI.user_error!("No exp_id for UploadAppToBuglyAction given, pass using `exp_id: 'exp_id'`") unless value and !value.empty?
-                                       end
-                                       ),
+                                         UI.user_error!("No exp_id for UploadAppToBuglyAction given, pass using `exp_id: 'exp_id'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :title,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_TITLE",
-                                       description: "title for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_TITLE',
+                                       description: 'title for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                      ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :desc,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESC",
-                                       description: "desc for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESC',
+                                       description: 'desc for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: "description"
-                                       ),
+                                       default_value: 'description'),
           FastlaneCore::ConfigItem.new(key: :secret,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "secret for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'secret for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :users,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "users for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'users for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "password for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'password for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :download_limit,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "download_limit for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'download_limit for UploadAppToBuglyAction',
                                        is_string: false,
-                                       default_value: 10000
-                                       )
+                                       default_value: 10_000)
         ]
       end
 
@@ -149,18 +145,18 @@ module Fastlane
 
       def self.authors
         # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        "sexiong306"
+        ['sexiong306']
       end
 
       def self.is_supported?(platform)
         # you can do things like
-        # 
+        #
         #  true
-        # 
+        #
         #  platform == :ios
-        # 
+        #
         #  [:ios, :mac].include?(platform)
-        # 
+        #
 
         platform == :ios
       end
--- fastlane/lib/fastlane/actions/upload_app_to_bugly.rb
+++ fastlane/lib/fastlane/actions/upload_app_to_bugly.rb
@@ -15,32 +15,35 @@ module Fastlane
         require 'json'
         # fastlane will take care of reading in the parameter and fetching the environment variable:
         UI.message "file path: #{params[:file_path]}"
-        result_file = File.new("upload_result.json", "w+")
+        result_file = File.new('upload_result.json', 'w+')
         result_file.close
         json_file = 'upload_app_to_bugly_result.json'
-
-        if !"#{params[:secret]}".to_s.nil? and !"#{params[:secret]}".to_s.empty?
+        secret = ''
+        if !params[:secret].to_s.nil? && !params[:secret].empty?
           secret = " -F \"secret=#{params[:secret]}\" "
           UI.message "secret:#{secret}"
         end
-        if !"#{params[:users]}".to_s.nil? and !"#{params[:users]}".to_s.empty?
+        users = ''
+        if !params[:users].nil? && !params[:users].empty?
           users = " -F \"users=#{params[:users]}\" "
           UI.message "users:#{users}"
         end
-        if !"#{params[:password]}".to_s.nil? and !"#{params[:password]}".to_s.empty?
+        password = ''
+        if !params[:password].nil? && !params[:password].empty?
           password = " -F \"password=#{params[:password]}\" "
           UI.message "password:#{password}"
         end
-        if !"#{params[:download_limit]}".to_s.nil?  and "#{params[:download_limit]}".to_i > 0
+        download_limit = ''
+        if !params[:download_limit].nil? && params[:download_limit] > 0
           download_limit = " -F \"download_limit=#{params[:download_limit]}\" "
           UI.message "download_limit:#{download_limit}"
         end
-        cmd = "curl --insecure -F \"file=@#{params[:file_path]}\" -F \"app_id=#{params[:app_id]}\" -F \"pid=#{params[:pid]}\" -F \"title=#{params[:title]}\" -F \"description=#{params[:desc]}\"" + "#{secret}".to_s + "#{users}".to_s + "#{password}".to_s + "#{download_limit}".to_s + " https://api.bugly.qq.com/beta/apiv1/exp?app_key=#{params[:app_key]} -o #{json_file}"
+        cmd = "curl --insecure -F \"file=@#{params[:file_path]}\" -F \"app_id=#{params[:app_id]}\" -F \"pid=#{params[:pid]}\" -F \"title=#{params[:title]}\" -F \"description=#{params[:desc]}\"" + secret + users + password + download_limit + " https://api.bugly.qq.com/beta/apiv1/exp?app_key=#{params[:app_key]} -o " + json_file
         sh(cmd)
         obj = JSON.parse(File.read(json_file))
-        ret = obj["rtcode"]  
+        ret = obj['rtcode']
         if ret == 0
-          url = obj["data"]["url"]
+          url = obj['data']['url']
           Actions.lane_context[SharedValues::UPLOAD_APP_TO_BUGLY_DOWNLOAD_URL] = url
         end
       end
@@ -50,92 +53,82 @@ module Fastlane
       #####################################################
 
       def self.description
-        "A short description with <= 80 characters of what this action does"
+        'A short description with <= 80 characters of what this action does'
       end
 
       def self.details
         # Optional:
         # this is your chance to provide a more detailed description of this action
-        "You can use this action to do cool things..."
+        'You can use this action to do cool things...'
       end
 
       def self.available_options
-        # Define all options your action supports. 
-        
+        # Define all options your action supports.
+
         # Below a few examples
         [
           FastlaneCore::ConfigItem.new(key: :file_path,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_FILE_PATH",
-                                       description: "file path for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_FILE_PATH',
+                                       description: 'file path for UploadAppToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                          UI.user_error!("No file path for UploadAppToBuglyAction given, pass using `file_path: 'path'`") unless value and !value.empty?
-                                          UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
-                                       end
-                                       ),
+                                         UI.user_error!("No file path for UploadAppToBuglyAction given, pass using `file_path: 'path'`") unless value && !value.empty?
+                                         UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :app_key,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_APP_KEY",
-                                       description: "app key for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_APP_KEY',
+                                       description: 'app key for UploadAppToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                          UI.user_error!("NO app_key for UploadAppToBuglyAction given, pass using `app_key: 'app_key'`") unless value and !value.empty?
-                                       end
-                                       ),
+                                         UI.user_error!("NO app_key for UploadAppToBuglyAction given, pass using `app_key: 'app_key'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :app_id,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_APP_ID",
-                                       description: "app id for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_APP_ID',
+                                       description: 'app id for UploadAppToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                          UI.user_error!("No app_id for UploadAppToBuglyAction given, pass using `app_id: 'app_id'`") unless value and !value.empty?
-                                       end
-                                       ),
+                                         UI.user_error!("No app_id for UploadAppToBuglyAction given, pass using `app_id: 'app_id'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :pid,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_PID",
-                                       description: "pid for UploadToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_PID',
+                                       description: 'pid for UploadToBuglyAction',
                                        is_string: true,
                                        verify_block: proc do |value|
-                                          UI.user_error!("No pid for UploadAppToBuglyAction given, pass using `pid: 'pid'`") unless value and !value.empty?
-                                       end
-                                       ),
+                                         UI.user_error!("No pid for UploadAppToBuglyAction given, pass using `pid: 'pid'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :title,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_TITLE",
-                                       description: "title for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_TITLE',
+                                       description: 'title for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: "title",
+                                       default_value: 'title',
                                        verify_block: proc do |value|
-                                          UI.user_error!("No title for UploadAppToBuglyAction given, pass using `title: 'title'`") unless value and !value.empty?
-                                       end
-                                      ),
+                                         UI.user_error!("No title for UploadAppToBuglyAction given, pass using `title: 'title'`") unless value && !value.empty?
+                                       end),
           FastlaneCore::ConfigItem.new(key: :desc,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESC",
-                                       description: "desc for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESC',
+                                       description: 'desc for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: "desc"
-                                       ),
+                                       default_value: 'desc'),
           FastlaneCore::ConfigItem.new(key: :secret,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "secret for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'secret for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :users,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "users for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'users for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "password for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'password for UploadAppToBuglyAction',
                                        is_string: true,
-                                       default_value: ""
-                                       ),
+                                       default_value: ''),
           FastlaneCore::ConfigItem.new(key: :download_limit,
-                                       env_name: "FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION",
-                                       description: "download_limit for UploadAppToBuglyAction",
+                                       env_name: 'FL_UPLOAD_APP_TO_BUGLY_DESCRIPTION',
+                                       description: 'download_limit for UploadAppToBuglyAction',
                                        is_string: false, # true: verifies the input is a string, false: every kind of value
-                                       default_value: 10000
-                                       )
+                                       default_value: 10_000)
         ]
       end
 
@@ -152,7 +145,7 @@ module Fastlane
 
       def self.authors
         # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        "sexiong306"
+        ['sexiong306']
       end
 
       def self.is_supported?(platform)
