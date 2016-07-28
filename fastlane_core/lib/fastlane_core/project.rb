53b2b361c498195635e9d0b0997530d5b9d28c28
Felipe Cypriano <felipe@cypriano.me>
2016-07-27 18:04:38 -0700

    Print -showBuildSettings command
    Omitting it causes confusion on what command is hanging.

--- fastlane_core/lib/fastlane_core/project.rb
+++ fastlane_core/lib/fastlane_core/project.rb
@@ -216,6 +216,7 @@ module FastlaneCore
     def build_settings(key: nil, optional: true)
       unless @build_settings
         command = build_xcodebuild_showbuildsettings_command
+        UI.important(command) unless silent
 
         # xcode might hang here and retrying fixes the problem, see fastlane#4059
         begin
