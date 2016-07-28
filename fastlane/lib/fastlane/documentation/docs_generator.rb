4a4913c19f6cad08b92ef85a67ab066364d0c776
ILI4S K4RIM <ILI4S@users.noreply.github.com>
2016-07-28 14:16:05 -0700

    Fix typo (#5532)
    convert to passive voice

--- fastlane/lib/fastlane/documentation/docs_generator.rb
+++ fastlane/lib/fastlane/documentation/docs_generator.rb
@@ -35,7 +35,7 @@ module Fastlane
         output << ""
       end
 
-      output << "This README.md is auto-generated and will be re-generated every time to run [fastlane](https://fastlane.tools)."
+      output << "This README.md is auto-generated and will be re-generated every time [fastlane](https://fastlane.tools) is run."
       output << "More information about fastlane can be found on [https://fastlane.tools](https://fastlane.tools)."
       output << "The documentation of fastlane can be found on [GitHub](https://github.com/fastlane/fastlane/tree/master/fastlane)."
 
