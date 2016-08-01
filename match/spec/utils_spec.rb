f1267fa5b5dff06b7ef82dd8735efbe20e3a7eca
Michael Furtak <mfurtak@twitter.com>
2016-08-01 10:23:52 -0400

    Refactor for PR feedback
    * Raise user_error! if the specified keychain path cannot be found
    * Stub Helper.backticks instead of short-circuiting execution during tests

--- match/spec/utils_spec.rb
+++ match/spec/utils_spec.rb
@@ -3,12 +3,31 @@ describe Match do
     describe 'import' do
       it 'finds a normal keychain name relative to ~/Library/Keychains' do
         expected_command = "security import item.path -k #{Dir.home}/Library/Keychains/login.keychain -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
-        expect(Match::Utils.import('item.path', 'login.keychain')).to eq(expected_command)
+
+        expect(File).to receive(:exist?).with("#{Dir.home}/Library/Keychains/login.keychain").and_return(true)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: $verbose)
+
+        Match::Utils.import('item.path', 'login.keychain')
       end
 
       it 'treats a keychain name it cannot find in ~/Library/Keychains as the full keychain path' do
         expected_command = "security import item.path -k /my/special.keychain -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
-        expect(Match::Utils.import('item.path', '/my/special.keychain')).to eq(expected_command)
+
+        expect(File).to receive(:exist?).with("#{Dir.home}/Library/Keychains/my/special.keychain").and_return(false)
+        expect(File).to receive(:exist?).with('/my/special.keychain').and_return(true)
+
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: $verbose)
+
+        Match::Utils.import('item.path', '/my/special.keychain')
+      end
+
+      it 'shows a user error if the keychain path cannot be resolved' do
+        expect(File).to receive(:exist?).with("#{Dir.home}/Library/Keychains/my/special.keychain").and_return(false)
+        expect(File).to receive(:exist?).with('/my/special.keychain').and_return(false)
+
+        expect do
+          Match::Utils.import('item.path', '/my/special.keychain')
+        end.to raise_error(/Could not locate the provided keychain/)
       end
     end
 
