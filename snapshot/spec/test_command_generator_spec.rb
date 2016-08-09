c8430c528cdb505d47dc9bffc9838691ab3ab30e
Aman Gupta <aman@tmm1.net>
2016-08-09 12:25:44 -0700

    add test for tvos command

--- snapshot/spec/test_command_generator_spec.rb
+++ snapshot/spec/test_command_generator_spec.rb
@@ -10,12 +10,10 @@ describe Snapshot do
       def configure(options)
         Snapshot.config = FastlaneCore::Configuration.create(Snapshot::Options.available_options, options)
       end
-      context 'default options' do
-        before do
-          configure options
-        end
 
+      context 'default options' do
         it "uses the default parameters" do
+          configure options
           expect(Dir).to receive(:mktmpdir).with("snapshot_derived").and_return("/tmp/path/to/snapshot_derived")
           command = Snapshot::TestCommandGenerator.generate(device_type: "iPhone 6")
           id = command.join('').match(/id=(.+?),/)[1]
@@ -28,6 +26,28 @@ describe Snapshot do
               "-project ./example/Example.xcodeproj",
               "-derivedDataPath '/tmp/path/to/snapshot_derived'",
               "-destination 'platform=iOS Simulator,id=#{id},OS=#{ios}'",
+              "FASTLANE_SNAPSHOT=YES",
+              :build,
+              :test,
+              "| tee #{File.expand_path('~/Library/Logs/snapshot/Example-ExampleUITests.log')} | xcpretty "
+            ]
+          )
+        end
+
+        it "uses the default parameters on tvOS too" do
+          configure options.merge(devices: ["Apple TV 1080p"])
+          expect(Dir).to receive(:mktmpdir).with("snapshot_derived").and_return("/tmp/path/to/snapshot_derived")
+          command = Snapshot::TestCommandGenerator.generate(device_type: "Apple TV 1080p")
+          id = command.join('').match(/id=(.+?),/)[1]
+          os = command.join('').match(/OS=(\d+.\d+)/)[1]
+          expect(command).to eq(
+            [
+              "set -o pipefail &&",
+              "xcodebuild",
+              "-scheme ExampleUITests",
+              "-project ./example/Example.xcodeproj",
+              "-derivedDataPath '/tmp/path/to/snapshot_derived'",
+              "-destination 'platform=tvOS Simulator,id=#{id},OS=#{os}'",
               "FASTLANE_SNAPSHOT=YES",
               :build,
               :test,
