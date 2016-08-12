2005bef004d3a947cd8dd4fe4a4b753e3b0910cc
Martin <mail@azgul.com>
2016-08-12 16:44:00 +0300

    Truncate all values, not just strings (#5454)
    Reason being that config options can also be arrays

--- fastlane_core/lib/fastlane_core/print_table.rb
+++ fastlane_core/lib/fastlane_core/print_table.rb
@@ -35,7 +35,7 @@ module FastlaneCore
         max_allowed_value_length = max_length - max_key_length - 7
         rows.map do |e|
           value = e[1]
-          value = value.truncate(max_allowed_value_length) if value.kind_of? String
+          value = value.to_s.truncate(max_allowed_value_length) unless [true, false].include?(value)
           [e[0], value]
         end
       end
