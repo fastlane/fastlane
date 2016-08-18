82e4541861a4a158ca7d5711eaed772239bbca67
Michael Furtak <mfurtak@twitter.com>
2016-08-18 12:00:21 -0400

    Replace use of Helper.gem_path for locating assets

--- deliver/lib/deliver.rb
+++ deliver/lib/deliver.rb
@@ -23,4 +23,8 @@ module Deliver
 
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
   UI = FastlaneCore::UI
+
+  # Constant that captures the root Pathname for the project. Should be used for building paths to assets or other
+  # resources that code needs to locate locally
+  ROOT = Pathname.new(File.expand_path('../..', __FILE__))
 end
