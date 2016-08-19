d73b3a745e2ea4be318061b96e1e76bd389de0d2
Andrea Falcone <andrea@crashlytics.com>
2016-08-19 16:16:44 -0400

    Refactor csrf_token fetching to work properly (#5815)

--- spaceship/lib/spaceship/portal/portal_client.rb
+++ spaceship/lib/spaceship/portal/portal_client.rb
@@ -171,7 +171,6 @@ module Spaceship
       params.merge!(ident_params)
 
       ensure_csrf
-
       r = request(:post, "account/#{platform_slug(mac)}/identifiers/addAppId.action", params)
       parse_response(r, 'appId')
     end
@@ -387,17 +386,21 @@ module Spaceship
     private
 
     def ensure_csrf
-      if csrf_tokens.count == 0
-        # If we directly create a new resource (e.g. app) without querying anything before
-        # we don't have a valid csrf token, that's why we have to do at least one request
-        apps
+      # It's important we store this state here
+      # as we always want to request something at least once
+      return unless @requested_csrf_token.nil?
 
-        # Update 18th August 2016
-        # For some reason, we have to query the resource twice to actually get a valid csrf_token
-        # I couldn't find out why, the first response does have a valid Set-Cookie header
-        # But it still needs this second request
-        apps
-      end
+      # If we directly create a new resource (e.g. app) without querying anything before
+      # we don't have a valid csrf token, that's why we have to do at least one request
+      Certificate::Production.all
+
+      # Update 18th August 2016
+      # For some reason, we have to query the resource twice to actually get a valid csrf_token
+      # I couldn't find out why, the first response does have a valid Set-Cookie header
+      # But it still needs this second request
+      Certificate::Production.all
+
+      @requested_csrf_token = true if csrf_tokens.count > 0
     end
   end
 end
