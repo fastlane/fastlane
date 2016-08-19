440a2e9deaa0a96af2ddf016c058d9a2e2bf7096
Felix Krause <KrauseFx@users.noreply.github.com>
2016-08-18 17:21:16 -0700

    [spaceship] Update spaceship to work with new csrf token (#5803)
    * [spaceship] Update create_app! method to work with the new Dev Portal
    
    * [spaceship] Fix csrf_token issue

--- spaceship/spec/portal/portal_stubbing.rb
+++ spaceship/spec/portal/portal_stubbing.rb
@@ -150,11 +150,11 @@ def adp_stub_apps
     to_return(status: 200, body: adp_read_fixture_file('getAppIdDetail.action.json'), headers: { 'Content-Type' => 'application/json' })
 
   stub_request(:post, "https://developer.apple.com/services-account/QH65B2/account/ios/identifiers/addAppId.action").
-    with(body: { "appIdName" => "Production App", "appIdentifierString" => "tools.fastlane.spaceship.some-explicit-app", "explicitIdentifier" => "tools.fastlane.spaceship.some-explicit-app", "gameCenter" => "on", "inAppPurchase" => "on", "push" => "on", "teamId" => "XXXXXXXXXX", "type" => "explicit" }).
+    with(body: { "name" => "Production App", "identifier" => "tools.fastlane.spaceship.some-explicit-app", "gameCenter" => "on", "inAppPurchase" => "on", "push" => "on", "teamId" => "XXXXXXXXXX", "type" => "explicit" }).
     to_return(status: 200, body: adp_read_fixture_file('addAppId.action.explicit.json'), headers: { 'Content-Type' => 'application/json' })
 
   stub_request(:post, "https://developer.apple.com/services-account/QH65B2/account/ios/identifiers/addAppId.action").
-    with(body: { "appIdName" => "Development App", "appIdentifierString" => "tools.fastlane.spaceship.*", "teamId" => "XXXXXXXXXX", "type" => "wildcard", "wildcardIdentifier" => "tools.fastlane.spaceship.*" }).
+    with(body: { "name" => "Development App", "identifier" => "tools.fastlane.spaceship.*", "teamId" => "XXXXXXXXXX", "type" => "wildcard" }).
     to_return(status: 200, body: adp_read_fixture_file('addAppId.action.wildcard.json'), headers: { 'Content-Type' => 'application/json' })
 
   stub_request(:post, "https://developer.apple.com/services-account/QH65B2/account/ios/identifiers/deleteAppId.action").
