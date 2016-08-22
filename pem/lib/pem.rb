63e769f0ae016e70f71cbfbccf208bdc98f98918
Michael Furtak <michael.furtak@gmail.com>
2016-08-22 14:16:19 -0400

    Add ROOT constant paths for projects that don't need conversion (#5839)

--- pem/lib/pem.rb
+++ pem/lib/pem.rb
@@ -18,4 +18,5 @@ module PEM
 
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
   UI = FastlaneCore::UI
+  ROOT = Pathname.new(File.expand_path('../..', __FILE__))
 end
