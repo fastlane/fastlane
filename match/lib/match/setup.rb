d696cae18e9d321a173961ea4d38408e101c51d0
Michael Furtak <mfurtak@twitter.com>
2016-08-22 15:41:34 -0400

    Add a ROOT path constant for match

--- match/lib/match/setup.rb
+++ match/lib/match/setup.rb
@@ -1,7 +1,7 @@
 module Match
   class Setup
     def run(path)
-      template = File.read("#{Helper.gem_path('match')}/lib/assets/MatchfileTemplate")
+      template = File.read("#{Match::ROOT}/lib/assets/MatchfileTemplate")
 
       UI.important "Please create a new, private git repository"
       UI.important "to store the certificates and profiles there"
