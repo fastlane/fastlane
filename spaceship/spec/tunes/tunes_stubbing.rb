386a6df167217743c2d61f3c9308f9532b4b5442
Simon Cook <simon.cook@trademe.co.nz>
2016-08-29 11:02:15 +1200

    Added tests and docs

--- spaceship/spec/tunes/tunes_stubbing.rb
+++ spaceship/spec/tunes/tunes_stubbing.rb
@@ -65,6 +65,15 @@ def itc_stub_applications
     to_return(status: 200, body: itc_read_fixture_file('app_version_states_history.json'), headers: { 'Content-Type' => 'application/json' })
 end
 
+def itc_stub_ratings
+	# Rating summary
+	stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/898536088/reviews/summary?platform=ios&versionId=").
+    	to_return(status: 200, body: itc_read_fixture_file('ratings_summary.json'), headers: { 'Content-Type' => 'application/json' })
+
+    stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/898536088/reviews?platform=ios&storefront=US&versionId=").
+    	to_return(status: 200, body: itc_read_fixture_file('review_by_storefront.json'), headers: { 'Content-Type' => 'application/json' })
+end
+
 def itc_stub_build_details
   stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/898536088/buildHistory?platform=ios").
     to_return(status: 200, body: itc_read_fixture_file('build_history.json'), headers: { 'Content-Type' => 'application/json' })
