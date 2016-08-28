c9003d88e766a8b434058ae773cf2728f57eae5d
Felix Krause <KrauseFx@users.noreply.github.com>
2016-08-28 15:08:06 -0700

    Fix spelling
    via @mfurtak

--- match/lib/match/runner.rb
+++ match/lib/match/runner.rb
@@ -32,9 +32,9 @@ module Match
 
       UI.success "All required keys, certificates and provisioning profiles are installed 🙌".green
     rescue Spaceship::Client::UnexpectedResponse, Spaceship::Client::InvalidUserCredentialsError, Spaceship::Client::NoUserCredentialsError => ex
-      UI.error("An error occured while verifying your certificates and profiles with the Apple Developer Portal")
+      UI.error("An error occured while verifying your certificates and profiles with the Apple Developer Portal.")
       UI.error("If you already have your certificates stored in git, you can run `match` in readonly mode")
-      UI.error("To just install the certificates and profiles without accessing the Dev Portal")
+      UI.error("to just install the certificates and profiles without accessing the Dev Portal.")
       UI.error("To do so, just pass `readonly: true` to your match call.")
       raise ex
     ensure
