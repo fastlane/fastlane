f5cfffc10bf88f871b26d253e58452e72d18aaf2
Simon Cook <simon.cook@trademe.co.nz>
2016-08-29 11:07:10 +1200

    Fix hound issues

--- spaceship/spec/tunes/tunes_stubbing.rb
+++ spaceship/spec/tunes/tunes_stubbing.rb
@@ -70,7 +70,7 @@ def itc_stub_ratings
     to_return(status: 200, body: itc_read_fixture_file('ratings_summary.json'), headers: { 'Content-Type' => 'application/json' })
 
   stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/898536088/reviews?platform=ios&storefront=US&versionId=").
-   	to_return(status: 200, body: itc_read_fixture_file('review_by_storefront.json'), headers: { 'Content-Type' => 'application/json' })
+    to_return(status: 200, body: itc_read_fixture_file('review_by_storefront.json'), headers: { 'Content-Type' => 'application/json' })
 end
 
 def itc_stub_build_details
