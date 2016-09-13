26b27e621a0273bd33d68c0ec7a088e8a3fb0ed2
Mark Schall <schall.mark@gmail.com>
2016-09-13 17:59:29 -0400

    Add force and remote options to push_git_tags action

--- fastlane/lib/fastlane/actions/push_git_tags.rb
+++ fastlane/lib/fastlane/actions/push_git_tags.rb
@@ -5,8 +5,12 @@ module Fastlane
         command = [
           'git',
           'push',
+          params[:remote],
           '--tags'
         ]
+
+        # optionally add the force component
+        command << '--force' if params[:force]
 
         result = Actions.sh(command.join(' '))
         UI.success('Tags pushed to remote')
@@ -23,6 +27,15 @@ module Fastlane
 
       def self.available_options
         [
+          FastlaneCore::ConfigItem.new(key: :force,
+                                       env_name: "FL_PUSH_GIT_FORCE",
+                                       description: "Force push to remote. Defaults to false",
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :remote,
+                                       env_name: "FL_GIT_PUSH_REMOTE",
+                                       description: "The remote to push to. Defaults to `origin`",
+                                       default_value: 'origin')
         ]
       end
 
--- fastlane/spec/actions_specs/push_git_tags_spec.rb
+++ fastlane/spec/actions_specs/push_git_tags_spec.rb
@@ -1,12 +1,36 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "push_git_tags" do
-      it "uses the correct comand" do
+      it "uses the correct default comand" do
         result = Fastlane::FastFile.new.parse("lane :test do
           push_git_tags
         end").runner.execute(:test)
 
-        expect(result).to eq("git push --tags")
+        expect(result).to eq("git push origin --tags")
+      end
+
+      it "uses the correct comand when forced" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(force: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push origin --tags --force")
+      end
+
+      it "uses the correct comand when remote set" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(remote: 'foo')
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push foo --tags")
+      end
+
+      it "uses the correct comand when remote set and forced" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(remote: 'foo', force: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push foo --tags --force")
       end
     end
   end
