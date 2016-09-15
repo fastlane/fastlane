699822d0633b748b9b002bcb0b69ed607df721e1
Mark Schall <mark@detroitlabs.com>
2016-09-15 19:28:24 -0400

    Add force and remote options to push_git_tags action (#6109)
    * Add force and remote options to push_git_tags action
    
    * Make remote option optional

--- fastlane/spec/actions_specs/push_git_tags_spec.rb
+++ fastlane/spec/actions_specs/push_git_tags_spec.rb
@@ -1,12 +1,36 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "push_git_tags" do
-      it "uses the correct comand" do
+      it "uses the correct default comand" do
         result = Fastlane::FastFile.new.parse("lane :test do
           push_git_tags
         end").runner.execute(:test)
 
         expect(result).to eq("git push --tags")
+      end
+
+      it "uses the correct comand when forced" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(force: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push --tags --force")
+      end
+
+      it "uses the correct comand when remote set" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(remote: 'foo')
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push --tags foo")
+      end
+
+      it "uses the correct comand when remote set and forced" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          push_git_tags(remote: 'foo', force: true)
+        end").runner.execute(:test)
+
+        expect(result).to eq("git push --tags --force foo")
       end
     end
   end
