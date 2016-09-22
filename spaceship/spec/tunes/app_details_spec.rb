0bba3690d3d15cc4fb73e8b5f6b2ee8370d49d6f
Andrea Falcone <asfalcone@gmail.com>
2016-09-22 15:05:36 -0400

    Fix Sticker category by adding correct Apps prefix

--- spaceship/spec/tunes/app_details_spec.rb
+++ spaceship/spec/tunes/app_details_spec.rb
@@ -17,7 +17,7 @@ describe Spaceship::Tunes::AppDetails do
   end
 
   describe "Modifying the app category" do
-    it "prefixes the category with the correct value for all category types" do
+    it "prefixes the category with MZGenre for all category types" do
       details = app.details
 
       details.primary_category = "Weather"
@@ -39,11 +39,39 @@ describe Spaceship::Tunes::AppDetails do
       expect(details.secondary_second_sub_category).to eq("MZGenre.Weather")
     end
 
+    it "prefixes the category with MZGenre.Apps for Stickers types" do
+      details = app.details
+
+      details.primary_category = "Stickers"
+      expect(details.primary_category).to eq("MZGenre.Apps.Stickers")
+
+      details.primary_first_sub_category = "Stickers.Art"
+      expect(details.primary_first_sub_category).to eq("MZGenre.Apps.Stickers.Art")
+
+      details.primary_second_sub_category = "Stickers.Art"
+      expect(details.primary_second_sub_category).to eq("MZGenre.Apps.Stickers.Art")
+
+      details.secondary_category = "Stickers"
+      expect(details.secondary_category).to eq("MZGenre.Apps.Stickers")
+
+      details.secondary_first_sub_category = "Stickers.Art"
+      expect(details.secondary_first_sub_category).to eq("MZGenre.Apps.Stickers.Art")
+
+      details.secondary_second_sub_category = "Stickers.Art"
+      expect(details.secondary_second_sub_category).to eq("MZGenre.Apps.Stickers.Art")
+
+      details.primary_category = "Apps.Stickers"
+      expect(details.primary_category).to eq("MZGenre.Apps.Stickers")
+    end
+
     it "doesn't prefix if the prefix is already there" do
       details = app.details
 
       details.primary_category = "MZGenre.Weather"
       expect(details.primary_category).to eq("MZGenre.Weather")
+
+      details.primary_category = "MZGenre.Apps.Stickers"
+      expect(details.primary_category).to eq("MZGenre.Apps.Stickers")
     end
   end
 end
