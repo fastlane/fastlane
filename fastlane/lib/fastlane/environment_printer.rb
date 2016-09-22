07df06e74e6134f579a35244ea088f42f26fc6cd
Felix Krause <krausefx@gmail.com>
2016-09-22 17:21:58 -0400

    Fix rubocop issues

--- fastlane/lib/fastlane/environment_printer.rb
+++ fastlane/lib/fastlane/environment_printer.rb
@@ -1,6 +1,5 @@
 module Fastlane
   class EnvironmentPrinter
-
     def self.print
       puts "My fastlane environment"
       print_system_environment
@@ -18,15 +17,16 @@ module Fastlane
     end
 
     def self.print_fastlane_properties
-      #prints the fastfile
+      # prints the fastfile
       fastlane_path = FastlaneFolder.fastfile_path
       puts "fastfile located at: #{fastlane_path}"
       puts "```"
       puts File.read(fastlane_path)
       puts "```"
     end
+
     def self.print_appfile
-      #prints the fastfile
+      # prints the fastfile
       appfile_path = CredentialsManager::AppfileConfig.default_path
       puts "appfile located at: #{appfile_path}"
       puts "```"
