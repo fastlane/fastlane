b8a66d797cba1bc1129b0472d09a8e22ac447a53
Jack Flintermann <jack@stripe.com>
2016-09-22 13:32:07 -0400

    initial tests in place

--- spaceship/spec/tunes/tunes_stubbing.rb
+++ spaceship/spec/tunes/tunes_stubbing.rb
@@ -224,6 +224,20 @@ def itc_stub_user_detail
               headers: { "Content-Type" => "application/json" })
 end
 
+def itc_stub_sandbox_testers
+  stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/users/iap").
+    to_return(status: 200, body: itc_read_fixture_file("sandbox_testers.json"),
+              headers: { "Content-Type" => "application/json" })
+end
+
+def itc_stub_create_sandbox_tester
+  stub_request(:post, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/users/iap/add").
+    with(itc_read_fixture_file("create_sandbox_tester_payload.json"))
+    to_return(status: 200, body: itc_read_fixture_file("create_sandbox_tester.json"),
+              headers: { "Content-Type" => "application/json" })
+    # TODO figure out the min set of params needed
+end
+
 def itc_stub_pricing_tiers
   stub_request(:get, "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/apps/pricing/matrix").
     to_return(status: 200, body: itc_read_fixture_file("pricing_tiers.json"),
@@ -267,6 +281,8 @@ RSpec.configure do |config|
     itc_stub_testflight
     itc_stub_app_version_ref
     itc_stub_user_detail
+    itc_stub_sandbox_testers
+    itc_stub_create_sandbox_tester
     itc_stub_candiate_builds
     itc_stub_pricing_tiers
     itc_stub_release_to_store
