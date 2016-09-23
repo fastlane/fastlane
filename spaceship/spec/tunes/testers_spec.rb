b53b82ef22427ef0f6aa227a0dc1d6c07be41f81
Jack Flintermann <jack@stripe.com>
2016-09-23 10:22:46 -0600

    Add support for iCloud Sandbox testers (#6258)
    * initial tests in place
    
    * implement list + create sandbox tester
    
    * add documentation
    
    * rubocop fixups

--- spaceship/spec/tunes/testers_spec.rb
+++ spaceship/spec/tunes/testers_spec.rb
@@ -71,6 +71,36 @@ describe Spaceship::Tunes::Tester do
     end
   end
 
+  describe "Sandbox testers" do
+    it 'loads sandbox testers correctly' do
+      testers = Spaceship::Tunes::SandboxTester.all
+      expect(testers.count).to eq(1)
+      t = testers[0]
+      expect(t.class).to eq(Spaceship::Tunes::SandboxTester)
+
+      expect(t.email).to eq("test@test.com")
+      expect(t.first_name).to eq("Test")
+      expect(t.last_name).to eq("McTestington")
+      expect(t.country).to eq("GB")
+    end
+
+    it 'creates sandbox testers correctly' do
+      t = Spaceship::Tunes::SandboxTester.create!(
+        email: 'test2@test.com',
+        password: 'Passwordtest1',
+        country: 'US',
+        first_name: 'Steve',
+        last_name: 'Brule'
+      )
+      expect(t.class).to eq(Spaceship::Tunes::SandboxTester)
+
+      expect(t.email).to eq("test2@test.com")
+      expect(t.first_name).to eq("Steve")
+      expect(t.last_name).to eq("Brule")
+      expect(t.country).to eq("US")
+    end
+  end
+
   # describe "invite testers to an existing app" do
   #   it "invite all users to an app" do
   #     app.add_all_testers!
