79297d83ddbc4ba4e8ae668d073946cc98ff1b2b
Felix Krause <krausefx@gmail.com>
2016-10-05 14:47:02 -0700

    Fix code style

--- match/lib/match/git_helper.rb
+++ match/lib/match/git_helper.rb
@@ -14,7 +14,7 @@ module Match
         FastlaneCore::CommandExecutor.execute(command: "GIT_TERMINAL_PROMPT=0 #{command}",
                                             print_all: $verbose,
                                         print_command: $verbose)
-      rescue => ex
+      rescue
         UI.error("Error cloning certificates repo, please make sure you have read access to the repository you want to use")
         UI.error("Run the following command manually to make sure you're properly authenticated:")
         UI.command(command)
