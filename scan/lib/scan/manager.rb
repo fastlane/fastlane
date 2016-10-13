a0e50fb94e18071643dd1cd0842b5e75bfc3ac2f
Felix Krause <KrauseFx@users.noreply.github.com>
2016-10-13 15:39:37 -0700

    [scan] Show Xcode path in summary table (#6470)

--- scan/lib/scan/manager.rb
+++ scan/lib/scan/manager.rb
@@ -3,7 +3,11 @@ module Scan
     def work(options)
       Scan.config = options
 
-      FastlaneCore::PrintTable.print_values(config: options,
+      # Also print out the path to the used Xcode installation
+      # We go 2 folders up, to not show "Contents/Developer/"
+      values = Scan.config.values(ask: false)
+      values[:xcode_path] = File.expand_path("../..", FastlaneCore::Helper.xcode_path)
+      FastlaneCore::PrintTable.print_values(config: values,
                                          hide_keys: [:destination, :slack_url],
                                              title: "Summary for scan #{Scan::VERSION}")
 
