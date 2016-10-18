c5abfba910b875aeb8a946f69b98cb91b9a4ff32
KrauseFx <KrauseFx@gmail.com>
2016-10-17 20:35:40 -0700

    Remove old offset information

--- frameit/lib/frameit/offsets.rb
+++ frameit/lib/frameit/offsets.rb
@@ -14,51 +14,6 @@ module Frameit
       offset_value = @offsets["portrait"][screenshot.device_name]
       UI.error("Tried looking for offset information for 'portrait', #{screenshot.device_name}") unless offset_value
       return offset_value
-
-      # TODO: Remove all that
-      size = Deliver::AppScreenshot::ScreenSize
-      case screenshot.orientation_name
-      when Orientation::PORTRAIT
-        case screenshot.screen_size
-        when size::IOS_55
-          return {
-            'offset' => '+41+146',
-            'width' => 541
-          }
-        when size::IOS_47
-          return {
-            'offset' => "+43+154",
-            'width' => 530
-          }
-        when size::IOS_40
-          if Frameit.config[:use_legacy_iphone5s]
-            return {
-              'offset' => "+54+197",
-              'width' => 544
-            }
-          else
-            return {
-             'offset' => "+48+178",
-             'width' => 485
-            }
-          end
-        when size::IOS_35
-          return {
-            'offset' => "+59+260",
-            'width' => 647
-          }
-        when size::IOS_IPAD
-          return {
-            'offset' => '+47+135',
-            'width' => 737
-          }
-        when size::IOS_IPAD_PRO
-          return {
-            'offset' => '+48+90',
-            'width' => 805
-          }
-        end
-      end
     end
   end
 end
