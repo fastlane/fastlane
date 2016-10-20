9680746d2863b830bac17eb6560d933ed5e7a73d
KrauseFx <KrauseFx@gmail.com>
2016-10-20 11:03:02 -0700

    Smaller style improvements and foldable Fastfile

--- fastlane/lib/fastlane/environment_printer.rb
+++ fastlane/lib/fastlane/environment_printer.rb
@@ -1,9 +1,10 @@
 module Fastlane
   class EnvironmentPrinter
     def self.get
+      UI.important("Generating fastlane environment output, this might take a few seconds...")
       require "fastlane/markdown_table_formatter"
       env_output = ""
-      env_output << "### fastlane Environment\n"
+      env_output << "### fastlane environment\n"
       env_output << "\n"
       env_output << print_system_environment
       env_output << print_fastlane_files
@@ -15,18 +16,18 @@ module Fastlane
     end
 
     def self.print_date
-      date = Time.now.strftime("%d/%m/%Y %H:%M")
-      "\n *generated at:* **#{date}** \n"
+      date = Time.now.strftime("%Y-%m-%d")
+      "\n*generated on:* **#{date}**\n"
     end
 
     def self.print_loaded_plugins
       ENV["FASTLANE_ENV_PRINTER"] = "enabled"
-      env_output =  "### Loaded fastlane Plugins:\n"
+      env_output =  "### Loaded fastlane plugins:\n"
       env_output << "\n"
       plugin_manager = Fastlane::PluginManager.new
       plugin_manager.load_plugins
       if plugin_manager.available_plugins.length <= 0
-        env_output << "**No Plugins Loaded***\n"
+        env_output << "**No plugins Loaded***\n"
       else
         table = ""
         table << "| Plugin | Version | Update-Status |\n"
@@ -56,7 +57,7 @@ module Fastlane
     end
 
     def self.print_loaded_fastlane_gems
-      # fastlanes own gems
+      # fastlanes internal gems
       env_output = "### fastlane gems\n\n"
       table = ""
       table << "| Gem | Version | Update-Status |\n"
@@ -116,7 +117,7 @@ module Fastlane
       table << "| fastlane | #{Fastlane::VERSION} |\n"
       table << "| OS  | #{`sw_vers -productVersion`.strip}|\n"
       table << "| Ruby  | #{RUBY_VERSION}|\n"
-      table << "| Xcode | #{`xcode-select -p`.strip.tr("\n", ' ')}|\n"
+      table << "| Xcode Path | #{`xcode-select -p`.strip.tr("\n", ' ')}|\n"
       table << "| Xcode Version | #{`xcodebuild -version`.strip.tr("\n", ' ')}|\n"
       table << "| Git | #{`git --version`.strip.split("\n").first} |\n"
       table << "| Installation Source | #{$PROGRAM_NAME} |\n"
@@ -132,16 +133,18 @@ module Fastlane
     end
 
     def self.print_fastlane_files
-      env_output = "### fastlane Files:\n\n"
+      env_output = "### fastlane files:\n\n"
 
       fastlane_path = FastlaneFolder.fastfile_path
 
       if fastlane_path && File.exist?(fastlane_path)
-        env_output << "`Fastfile` located in #{fastlane_path}\n"
+        env_output << "<details>"
+        env_output << "<summary>`#{fastlane_path}`</summary>\n"
         env_output << "\n"
-        env_output << "```\n"
+        env_output << "```ruby\n"
         env_output <<  File.read(fastlane_path)
         env_output <<  "```\n"
+        env_output << "</details>"
       else
         env_output << "**No Fastfile found**\n"
       end
@@ -151,7 +154,7 @@ module Fastlane
       if appfile_path && File.exist?(appfile_path)
         env_output << "`Appfile` located in #{appfile_path}\n"
         env_output << "\n"
-        env_output << "```\n"
+        env_output << "```ruby\n"
         env_output <<  File.read(appfile_path)
         env_output <<  "```\n"
       else
