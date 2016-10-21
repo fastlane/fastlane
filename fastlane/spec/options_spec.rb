f2801bbdcc356b8558ca978f0a4c1ab60ff64eb8
Olivier Halligon <olivier.halligon@niji.fr>
2016-10-21 12:29:19 +0200

    [Env Vars] Adding tests to TDD inconsistent env vars

--- fastlane/spec/options_spec.rb
+++ fastlane/spec/options_spec.rb
@@ -1,6 +1,6 @@
 describe Fastlane do
   describe Fastlane::Action do
-    describe "No unused options" do
+    describe "Options" do
       let (:all_exceptions) { %w(pilot appstore cert deliver gym match pem produce scan sigh snapshot supply testflight mailgun testfairy ipa import_from_git hockey deploygate crashlytics artifactory appledoc slather screengrab download_dsyms notification frameit set_changelog register_devices latest_testflight_build_number) }
 
       Fastlane::ActionsList.all_actions do |action, name|
@@ -13,6 +13,17 @@ describe Fastlane do
           action.available_options.each do |option|
             unless content.include?("[:#{option.key}]")
               UI.user_error!("Action '#{name}' doesn't use the option :#{option.key}")
+            end
+          end
+        end
+
+        it "Every environment variable is prefixed in '#{name}'", :env_action => name do
+          action.available_options.map(&:env_name).reject(&:nil?).each do |env_name|
+            if name == 'fastlane'
+              env_name.should start_with("FL_")
+            else
+              prefix = name.upcase
+              expect(env_name).to start_with("#{prefix}_").or start_with("FL_#{prefix}_")
             end
           end
         end
