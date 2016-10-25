4ea601677400024802a4db4db2bad90494daaf6f
KrauseFx <KrauseFx@gmail.com>
2016-10-24 21:09:09 -0700

    Implement dsIS to iTunes Connect team selection
    Fixes https://github.com/fastlane/fastlane/issues/6711
    Fixes https://github.com/fastlane/fastlane/issues/6710
    
    Thanks @summera for providing a solution for the problem

--- spaceship/lib/spaceship/tunes/user_detail.rb
+++ spaceship/lib/spaceship/tunes/user_detail.rb
@@ -2,9 +2,11 @@ module Spaceship
   module Tunes
     class UserDetail < TunesBase
       attr_accessor :content_provider_id
+      attr_accessor :ds_id
 
       attr_mapping(
-        'contentProviderId' => :content_provider_id
+        'contentProviderId' => :content_provider_id,
+        'sessionToken.dsId' => :ds_id
       )
 
       class << self
