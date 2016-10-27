e1243a3c253d0cffca34af4e3da7828548b8605a
Sota Yokoe <kreuz45@kreuz45.com>
2016-10-27 22:17:00 +0900

    [gym] Add support for exporting pkg file.

--- gym/lib/gym/generators/package_command_generator_xcode7.rb
+++ gym/lib/gym/generators/package_command_generator_xcode7.rb
@@ -66,6 +66,10 @@ module Gym
         Gym.cache[:ipa_path]
       end
 
+      def pkg_path
+        File.join(temporary_output_path, "#{Gym.config[:output_name]}.pkg")
+      end
+
       # The path the the dsym file for this app. Might be nil
       def dsym_path
         Dir[BuildCommandGenerator.archive_path + "/**/*.app.dSYM"].last
