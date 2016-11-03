5287d27dec99076ea58f7c568251d0b693e2c093
Julien Quéré <jjy.quere@gmail.com>
2016-11-03 21:36:59 +0100

    [fastlane] Update error message when using fastlane_version with bundle, fix #6828 (#6831)
    * Update error message when using fastlane_version with bundle #6828
    
    * Update error message when using fastlane_version with bundle #6828

--- fastlane/spec/actions_specs/fastlane_version_spec.rb
+++ fastlane/spec/actions_specs/fastlane_version_spec.rb
@@ -12,7 +12,27 @@ describe Fastlane do
           Fastlane::FastFile.new.parse("lane :test do
             fastlane_version '9999'
           end").runner.execute(:test)
+        end.to raise_error
+      end
+
+      it "raises an exception if it's an old version in a non-bundler environement" do
+        expect do
+          # We have to clean ENV to be sure that Bundler environement is not defined.
+          stub_const('ENV', {})
+          Fastlane::FastFile.new.parse("lane :test do
+            fastlane_version '9999'
+          end").runner.execute(:test)
         end.to raise_error("The Fastfile requires a fastlane version of >= 9999. You are on #{Fastlane::VERSION}. Please update using `sudo gem update fastlane`.")
+      end
+
+      it "raises an exception if it's an old version in a bundler environement" do
+        expect do
+          # Let's define BUNDLE_BIN_PATH in ENV to simulate a bundler environement
+          stub_const('ENV', { 'BUNDLE_BIN_PATH' => '/fake/elsewhere' })
+          Fastlane::FastFile.new.parse("lane :test do
+            fastlane_version '9999'
+          end").runner.execute(:test)
+        end.to raise_error("The Fastfile requires a fastlane version of >= 9999. You are on #{Fastlane::VERSION}. Please update using `bundle update fastlane`.")
       end
 
       it "raises an error if no team ID is given" do
