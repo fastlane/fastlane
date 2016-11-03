ed899913d92e86d7549dc6ece651ea81d886a18c
Manu Wallner <m.wallner@xforge.at>
2016-11-03 16:19:56 -0700

    Version Bump

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '1.106.2'.freeze
+  VERSION = '1.107.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps"
 end
