756905fe35e7be7c22def06c7acf224130a39923
GoodMirek <GoodMirek@users.noreply.github.com>
2016-11-13 20:31:58 +0100

    Clarification of devices_file

--- fastlane/lib/fastlane/actions/register_devices.rb
+++ fastlane/lib/fastlane/actions/register_devices.rb
@@ -65,7 +65,7 @@ module Fastlane
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :devices_file,
                                        env_name: "FL_REGISTER_DEVICES_FILE",
-                                       description: "Provide a path to the devices to register",
+                                       description: "Provide a path to a file with the devices to register. For format see the examples above.",
                                        optional: true,
                                        verify_block: proc do |value|
                                          UI.user_error!("Could not find file '#{value}'") unless File.exist?(value)
