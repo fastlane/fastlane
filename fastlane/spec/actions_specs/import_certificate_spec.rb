986cc02012cec9140393d2134dac1f93c540a188
Mark Pirri <mpirri@twitter.com>
2016-11-17 16:10:28 -0500

    Add allow() to fix running unit tests on Sierra

--- fastlane/spec/actions_specs/import_certificate_spec.rb
+++ fastlane/spec/actions_specs/import_certificate_spec.rb
@@ -9,8 +9,12 @@ describe Fastlane do
         keychain_path = File.expand_path(File.join('~', 'Library', 'Keychains', keychain))
         expected_command = "security import #{cert_name} -k '#{keychain_path}' -P #{password} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
 
+        # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k \"\" #{keychain_path.shellescape} &> /dev/null"
+
         allow(File).to receive(:exist?).and_return(false)
         expect(File).to receive(:exist?).with(cert_name).and_return(true)
+        allow(FastlaneCore::Helper).to receive(:backticks).with(allowed_command, print: false)
         expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: false)
 
         Fastlane::FastFile.new.parse("lane :test do
@@ -30,8 +34,12 @@ describe Fastlane do
         keychain_path = File.expand_path(File.join('~', 'Library', 'Keychains', keychain))
         expected_command = "security import #{cert_name.shellescape} -k '#{keychain_path.shellescape}' -P #{password.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
 
+        # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k \"\" #{keychain_path.shellescape} &> /dev/null"
+
         allow(File).to receive(:exist?).and_return(false)
         expect(File).to receive(:exist?).with(cert_name).and_return(true)
+        allow(FastlaneCore::Helper).to receive(:backticks).with(allowed_command, print: false)
         expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: false)
 
         Fastlane::FastFile.new.parse("lane :test do
@@ -51,8 +59,12 @@ describe Fastlane do
         keychain_path = File.expand_path(File.join('~', 'Library', 'Keychains', keychain))
         expected_command = "security import #{cert_name} -k '#{keychain_path}' -P #{password} -T /usr/bin/codesign -T /usr/bin/security"
 
+        # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k \"\" #{keychain_path.shellescape}"
+
         allow(File).to receive(:exist?).and_return(false)
         expect(File).to receive(:exist?).with(cert_name).and_return(true)
+        allow(FastlaneCore::Helper).to receive(:backticks).with(allowed_command, print: true)
         expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: true)
 
         Fastlane::FastFile.new.parse("lane :test do
