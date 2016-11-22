1dbda5e92b388789b6716b2db3cd7a9b95004073
Helmut Januschka <h.januschka@krone.at>
2016-11-22 08:09:22 +0100

    add tvos sample

---
+++ gym/examples/commandline/tvos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tvos
+//
+//  Created by Januschka Helmut on 22.11.16.
+//  Copyright © 2016 hjanuschka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
