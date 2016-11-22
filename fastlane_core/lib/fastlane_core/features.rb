0ee5557f188cd7859f41edcf61950b734c99c1f1
Helmut Januschka <helmut@januschka.com>
2016-11-22 19:08:24 +0100

    add feature flag for snapshot

--- fastlane_core/lib/fastlane_core/features.rb
+++ fastlane_core/lib/fastlane_core/features.rb
@@ -1,2 +1,4 @@
+FastlaneCore::Feature.register(env_var: 'FASTLANE_SNAPSHOT_BUILD_FOR_TESTING',
+                           description: 'Use Build-For-Testing instead of build on every device used in snapshot')
 FastlaneCore::Feature.register(env_var: 'FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT',
                            description: 'Use iTunes Transporter shell script')
