c67d569b4a3c49a9b86039598323a10ac3deb254
Helmut Januschka <helmut@januschka.com>
2016-11-22 20:04:29 +0100

    feature flag round2

---
+++ snapshot/lib/snapshot/features.rb
@@ -0,0 +1,2 @@
+FastlaneCore::Feature.register(env_var: 'FASTLANE_SNAPSHOT_BUILD_FOR_TESTING',
+                               description: 'Use Build-For-Testing instead of build on every device used in snapshot')
