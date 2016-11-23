95f71bb8778ad744106a8a68eaa1a2f62662b54d
Helmut Januschka <helmut@januschka.com>
2016-11-23 21:02:35 +0100

    [fastlane_core] build_settings parsing - fix #7141 (#7144)
    * fix #7141

--- fastlane_core/spec/project_spec.rb
+++ fastlane_core/spec/project_spec.rb
@@ -294,6 +294,14 @@ describe FastlaneCore do
         expect(@project.schemes).to eq(["CrossPlatformFramework"])
       end
     end
+    describe "build_settings() can handle empty lines" do
+      it "SUPPORTED_PLATFORMS should be iphonesimulator iphoneos", now: true do
+        options = { project: "./spec/fixtures/projects/Example.xcodeproj" }
+        @project = FastlaneCore::Project.new(options, xcodebuild_list_silent: true, xcodebuild_suppress_stderr: true)
+        expect(FastlaneCore::Project).to receive(:run_command).with("xcodebuild clean -showBuildSettings -project ./spec/fixtures/projects/Example.xcodeproj 2> /dev/null", { timeout: 10, retries: 3, print: false }).and_return(File.read("./spec/fixtures/projects/build_settings_with_toolchains"))
+        expect(@project.build_settings(key: "SUPPORTED_PLATFORMS")).to eq("iphonesimulator iphoneos")
+      end
+    end
 
     describe "Build Settings with default configuration" do
       before do
