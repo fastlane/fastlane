2c097cd8b495287d398dcd3294344bd7c213f434
giginet <giginet.net@gmail.com>
2016-11-24 22:34:08 +0900

    Pass correct screenshot order
    closes #6981

--- deliver/lib/deliver/upload_screenshots.rb
+++ deliver/lib/deliver/upload_screenshots.rb
@@ -16,8 +16,12 @@ module Deliver
         UI.message("Removing all previously uploaded screenshots...")
         # First, clear all previously uploaded screenshots
         screenshots_per_language.keys.each do |language|
-          v.screenshots[language].each_with_index do |t, index|
-            v.upload_screenshot!(nil, t.sort_order, t.language, t.device_type, false)
+          v.screenshots[language].each_with_index do |screenshot, index|
+            begin
+              v.upload_screenshot!(nil, index + 1, screenshot.language, screenshot.device_type, false)
+            rescue
+              UI.error("Removing existing screenshot is failure")
+            end
           end
         end
       end
