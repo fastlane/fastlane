e0b90e871b2db8cac1cfc1ce4f296860befe4bbb
Eakawat Tantamjarik <eakawat2232@gmail.com>
2016-11-26 16:40:16 +0700

    Add test case

--- pem/spec/manager_spec.rb
+++ pem/spec/manager_spec.rb
@@ -20,6 +20,24 @@ describe PEM do
       expect(File.exist?("production_com.krausefx.app.pkey")).to eq(true)
     end
 
+    it "Successful run with development option" do
+      options = { app_identifier: "com.krausefx.app.development", generate_p12: false, development: true, voip: true }
+      PEM.config = FastlaneCore::Configuration.create(PEM::Options.available_options, options)
+      PEM::Manager.start
+
+      expect(File.exist?("development_com.krausefx.app.development.pem")).to eq(true)
+      expect(File.exist?("development_com.krausefx.app.development.pkey")).to eq(true)
+    end
+
+    it "Successful run with voip option" do
+      options = { app_identifier: "com.krausefx.app.voip", generate_p12: false, voip: true }
+      PEM.config = FastlaneCore::Configuration.create(PEM::Options.available_options, options)
+      PEM::Manager.start
+
+      expect(File.exist?("production_com.krausefx.app.voip.pem")).to eq(true)
+      expect(File.exist?("production_com.krausefx.app.voip.pkey")).to eq(true)
+    end
+
     it "Successful run with output_path" do
       options = { app_identifier: "com.krausefx.app", generate_p12: false, output_path: "tmp/" }
       PEM.config = FastlaneCore::Configuration.create(PEM::Options.available_options, options)
@@ -32,6 +50,10 @@ describe PEM do
 
     after :all do
       FileUtils.rm_r("tmp")
+      File.delete("development_com.krausefx.app.development.pem")
+      File.delete("development_com.krausefx.app.development.pkey")
+      File.delete("production_com.krausefx.app.voip.pem")
+      File.delete("production_com.krausefx.app.voip.pkey")
       File.delete("production_com.krausefx.app.pem")
       File.delete("production_com.krausefx.app.pkey")
 
--- pem/spec/stubbing.rb
+++ pem/spec/stubbing.rb
@@ -10,7 +10,9 @@ def stub_spaceship
   x509 = "x509"
 
   expect(Spaceship.certificate).to receive(:create_certificate_signing_request).and_return([csr, pkey])
-  expect(Spaceship.certificate.production_push).to receive(:create!).with(csr: csr, bundle_id: "com.krausefx.app").and_return(cert)
+  allow(Spaceship.certificate.production_push).to receive(:create!).with(csr: csr, bundle_id: "com.krausefx.app").and_return(cert)
+  allow(Spaceship.certificate.development_push).to receive(:create!).with(csr: csr, bundle_id: "com.krausefx.app.development").and_return(cert)
+  allow(Spaceship.certificate.voip_push).to receive(:create!).with(csr: csr, bundle_id: "com.krausefx.app.voip").and_return(cert)
   expect(cert).to receive(:download).and_return(x509)
   expect(pkey).to receive(:to_pem).twice.and_return("to_pem")
   expect(x509).to receive(:to_pem).and_return("to_pem")
