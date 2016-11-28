619106dc364cc58b8b82c036d307f40f6c342a65
Michael Furtak <michael.furtak@gmail.com>
2016-11-28 11:56:44 -0500

    Refactor ensure_xcode_version to do more work in Ruby (#7205)
    Supplement the tests to cover the error case from #7171

--- fastlane/spec/actions_specs/ensure_xcode_version_spec.rb
+++ fastlane/spec/actions_specs/ensure_xcode_version_spec.rb
@@ -1,7 +1,11 @@
 describe Fastlane::Actions::EnsureXcodeVersionAction do
   describe "matching versions" do
-    it "matches" do
-      expect(Fastlane::Actions::EnsureXcodeVersionAction).to receive(:sh).and_return("Xcode 8.0")
+    let(:different_response) { "Xcode 7.3\nBuild version 34a893" }
+    let(:matching_response) { "Xcode 8.0\nBuild version 8A218a" }
+    let(:matching_response_extra_output) { "Couldn't verify that spaceship is up to date\nXcode 8.0\nBuild version 8A218a" }
+
+    it "is successful when the version matches" do
+      expect(Fastlane::Actions::EnsureXcodeVersionAction).to receive(:sh).and_return(matching_response)
       expect(UI).to receive(:success).with(/Driving the lane/)
       expect(UI).to receive(:success).with(/Selected Xcode version is correct/)
 
@@ -10,8 +14,18 @@ describe Fastlane::Actions::EnsureXcodeVersionAction do
       end").runner.execute(:test)
     end
 
-    it "doesn't match" do
-      expect(Fastlane::Actions::EnsureXcodeVersionAction).to receive(:sh).and_return("Xcode 7.3")
+    it "matches even when there is extra output" do
+      expect(Fastlane::Actions::EnsureXcodeVersionAction).to receive(:sh).and_return(matching_response_extra_output)
+      expect(UI).to receive(:success).with(/Driving the lane/)
+      expect(UI).to receive(:success).with(/Selected Xcode version is correct/)
+
+      result = Fastlane::FastFile.new.parse("lane :test do
+        ensure_xcode_version(version: '8.0')
+      end").runner.execute(:test)
+    end
+
+    it "presents an error when the version does not match" do
+      expect(Fastlane::Actions::EnsureXcodeVersionAction).to receive(:sh).and_return(different_response)
       expect(UI).to receive(:user_error!)
 
       result = Fastlane::FastFile.new.parse("lane :test do
