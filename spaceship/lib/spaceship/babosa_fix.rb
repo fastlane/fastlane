9e85de4c9d79ac79d17018b8383f96c5a1d4e27f
sebastianludwig <sebastian@lurado.de>
2016-12-02 00:50:02 +0100

    Added comment to keep track when the babosa fix can be removed again. (#7277)

--- spaceship/lib/spaceship/babosa_fix.rb
+++ spaceship/lib/spaceship/babosa_fix.rb
@@ -7,6 +7,8 @@
 # This works around the possibility that the unicode-string_width gem may already be
 # loaded by temporarily undefining the `Unicode` constant while we load Babosa,
 # then restoring it to its previous state if necessary.
+#
+# Can be removed once https://github.com/norman/babosa/pull/42 is merged and released
 class BabosaFix
   def apply
     unicode_removed = false
