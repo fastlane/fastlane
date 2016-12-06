ef58a31fc429e9ab53dbe61d205d1099b1b68de8
Helmut Januschka <h.januschka@krone.at>
2016-12-06 13:28:45 +0100

    fix typo

--- fastlane_core/lib/fastlane_core/env.rb
+++ fastlane_core/lib/fastlane_core/env.rb
@@ -3,7 +3,7 @@ module FastlaneCore
     def self.enabled?(env)
       return false unless ENV[env]
       unless ENV["SKIP_ENV_HANDLER"]
-        return false if ["no", "false", "off"].include(ENV[env].to_s)
+        return false if ["no", "false", "off"].include?(ENV[env].to_s)
       end
       # if SKIP_ENV_HANDLER is set this should fallback to previous behaviour
       return ENV[env]
