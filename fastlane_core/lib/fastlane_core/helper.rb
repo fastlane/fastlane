f81e7de977b9a76ead470068771f2d5ac97702b5
Manu Wallner <manu@supermil.ch>
2016-12-06 13:33:35 -0800

    Try more combinations of keychain paths (#7276)
    * Try more combinations of keychain paths
    
    * Make the keychain_paths array easier to modify

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -165,12 +165,22 @@ module FastlaneCore
       # has changed for some users: https://github.com/fastlane/fastlane/issues/5649
       #
 
-      keychain_paths = [
+      # Remove the ".keychain" at the end of the name
+      name.sub!(/\.keychain$/, "")
+
+      possible_locations = [
         File.join(Dir.home, 'Library', 'Keychains', name),
-        File.join(Dir.home, 'Library', 'Keychains', "#{name}-db"),
-        name,
-        "#{name}-db"
+        name
       ].map { |path| File.expand_path(path) }
+
+      # Transforms ["thing"] to ["thing", "thing-db", "thing.keychain", "thing-db.keychain"]
+      keychain_paths = []
+      possible_locations.each do |location|
+        keychain_paths << location
+        keychain_paths << "#{location}-db"
+        keychain_paths << "#{location}.keychain"
+        keychain_paths << "#{location}-db.keychain"
+      end
 
       keychain_path = keychain_paths.find { |path| File.exist?(path) }
       UI.user_error!("Could not locate the provided keychain. Tried:\n\t#{keychain_paths.join("\n\t")}") unless keychain_path
