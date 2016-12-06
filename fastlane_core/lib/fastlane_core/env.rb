e737c705f3eff0245547f520ae70770dd1f5b68c
Helmut Januschka <h.januschka@krone.at>
2016-12-06 12:16:55 +0100

    refactor feature flag

--- fastlane_core/lib/fastlane_core/env.rb
+++ fastlane_core/lib/fastlane_core/env.rb
@@ -5,7 +5,8 @@ module FastlaneCore
       unless ENV["SKIP_ENV_HANDLER"]
         return false if ["no", "false", "off"].include(ENV[env].to_s)
       end
-      return true
+      # if SKIP_ENV_HANDLER is set this should fallback to previous behaviour
+      return ENV[env]
     end
   end
 end
