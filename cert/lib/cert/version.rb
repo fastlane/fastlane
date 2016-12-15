e09b619c340a80809322f3c55d64d5ceaec778ee
Felix Krause <KrauseFx@users.noreply.github.com>
2016-12-15 13:10:10 -0800

    Fix fastlane version (#7514)
    * Include fastlane version in core
    
    * Hotfix for receiving the fastlane version
    
    This also includes patch version bumps

--- cert/lib/cert/version.rb
+++ cert/lib/cert/version.rb
@@ -1,3 +1,3 @@
 module Cert
-  VERSION = "1.5.0"
+  VERSION = "1.5.1"
 end
--- snapshot/lib/snapshot/version.rb
+++ snapshot/lib/snapshot/version.rb
@@ -1,4 +1,4 @@
 module Snapshot
-  VERSION = "1.18.0".freeze
+  VERSION = "1.18.1".freeze
   DESCRIPTION = "Automate taking localized screenshots of your iOS and tvOS apps on every device"
 end
