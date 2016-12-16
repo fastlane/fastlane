7fb2777c782a3dd647c6a7b4de7f3f11ebebbce6
Felix Krause <KrauseFx@users.noreply.github.com>
2016-12-15 19:33:21 -0800

    Version bump (#7532)
    In particular to fix gym https://github.com/fastlane/fastlane/pull/7526

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '2.0.3'.freeze
+  VERSION = '2.0.4'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
 end
