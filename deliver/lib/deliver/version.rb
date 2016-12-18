47deb83d2c6ff0b27f309eb6e0595129fef7034c
Manu Wallner <m.wallner@xforge.at>
2016-12-18 15:50:06 -0800

    Fix up versions that couldn't be released before

--- deliver/lib/deliver/version.rb
+++ deliver/lib/deliver/version.rb
@@ -1,4 +1,4 @@
 module Deliver
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
   DESCRIPTION = 'Upload screenshots, metadata and your app to the App Store using a single command'
 end
--- sigh/lib/sigh/version.rb
+++ sigh/lib/sigh/version.rb
@@ -1,3 +1,3 @@
 module Sigh
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
 end
