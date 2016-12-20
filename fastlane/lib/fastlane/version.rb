501529551301732d5aec49f6779656217a35760d
KrauseFx <KrauseFx@gmail.com>
2016-12-20 16:14:26 +0100

    Version bump to 2.1.2

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '2.1.1'.freeze
+  VERSION = '2.1.2'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
 end
