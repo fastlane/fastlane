fddf7dba4a3c98a50ae5d3f14b8168aeecd42ee5
mpirri <mnpirri@gmail.com>
2016-12-20 14:53:51 -0500

    Version bump to 2.1.3 (#7603)

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '2.1.2'.freeze
+  VERSION = '2.1.3'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
 end
