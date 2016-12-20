a38d70cd811aaf8d1ebca51263423d2fd0bdf1cb
Helmut Januschka <helmut@januschka.com>
2016-12-20 16:13:53 +0100

    [gym] temp. disable `produces_archives?` to fix user issues. (#7591)
    * [gym] temp. disable `produces_archives?` to fix user issues.
    
    * remove FIXME notes
    
    * address feedback
    
    * revert marker too

--- gym/lib/gym/runner.rb
+++ gym/lib/gym/runner.rb
@@ -11,15 +11,15 @@ module Gym
         clear_old_files
         build_app
       end
-      verify_archive if Gym.project.produces_archive?
+      verify_archive
       FileUtils.mkdir_p(File.expand_path(Gym.config[:output_directory]))
 
       if Gym.project.ios? || Gym.project.tvos?
         fix_generic_archive # See https://github.com/fastlane/fastlane/pull/4325
-        package_app if Gym.project.produces_archive?
+        package_app
         fix_package
         compress_and_move_dsym
-        path = move_ipa if Gym.project.produces_archive?
+        path = move_ipa
         move_manifest
         move_app_thinning
         move_app_thinning_size_report
@@ -32,14 +32,6 @@ module Gym
           return path
         end
         copy_files_from_path(File.join(BuildCommandGenerator.archive_path, "Products/usr/local/bin/*")) if Gym.project.command_line_tool?
-      end
-
-      if Gym.project.library? || Gym.project.framework?
-        base_framework_path = Gym.project.build_settings(key: "BUILD_ROOT")
-        base_framework_path = Gym.config[:derived_data_path] + "/Build/Products/" if Gym.config[:derived_data_path]
-
-        copy_files_from_path("#{base_framework_path}/*/*")
-        path = base_framework_path
       end
       path
     end
@@ -97,7 +89,6 @@ module Gym
     end
 
     def mark_archive_as_built_by_gym(archive_path)
-      return if Gym.project.library? || Gym.project.framework?
       escaped_archive_path = archive_path.shellescape
       system("xattr -w info.fastlane.generated_by_gym 1 #{escaped_archive_path}")
     end
