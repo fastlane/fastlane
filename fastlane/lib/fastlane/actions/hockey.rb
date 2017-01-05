35eae35de9242e7d3ba47b7c3e2d649d49097e5c
Mike <overtakeful@gmail.com>
2017-01-05 18:12:59 +0300

    Update hockey.rb
    added optional dsa_signature parameter for sparkle (macOS) updates.

--- fastlane/lib/fastlane/actions/hockey.rb
+++ fastlane/lib/fastlane/actions/hockey.rb
@@ -217,7 +217,11 @@ module Fastlane
                                       env_name: "FL_HOCKEY_BYPASS_CDN",
                                       description: "Flag to bypass Hockey CDN when it uploads successfully but reports error",
                                       is_string: false,
-                                      default_value: false)
+                                      default_value: false),
+          FastlaneCore::ConfigItem.new(key: :dsa_signature,
+                                      env_name: "FL_HOCKEY_DSA_SIGNATURE",
+                                      description: "dsa signature for sparkle updates",
+                                      optional: true)
         ]
       end
 
