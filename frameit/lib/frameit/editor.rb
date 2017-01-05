b68ddc635573eb631b274ef8421a6b12df5a9978
Michael Furtak <michael.furtak@gmail.com>
2017-01-05 16:12:33 -0500

    Fix frameit 'easy mode' problem in landscape (#7759)

--- frameit/lib/frameit/editor.rb
+++ frameit/lib/frameit/editor.rb
@@ -22,8 +22,6 @@ module Frameit
         @image = complex_framing
       else
         # easy mode from 1.0 - no title or background
-        width = offset['width']
-        image.resize width # resize the image to fit the frame
         put_into_frame # put it in the frame
       end
 
