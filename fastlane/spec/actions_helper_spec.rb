9d7cdb28f39555623f2f5273ff439627594636be
Josh Holtz <josh@rokkincat.com>
2017-01-09 10:02:03 -0600

    Rubocop changes

--- fastlane/spec/actions_helper_spec.rb
+++ fastlane/spec/actions_helper_spec.rb
@@ -52,36 +52,36 @@ describe Fastlane do
       it "is class action" do
         Fastlane::Actions.load_external_actions("./fastlane/spec/fixtures/actions")
         require_relative './fixtures/broken_actions/broken_action.rb'
-        
+
         # An action
         example_action_ref = Fastlane::Actions.action_class_ref("example_action")
         expect(Fastlane::Actions.is_class_action?(example_action_ref)).to eq(true)
-        
+
         # Not an action
         broken_action_ref = Fastlane::Actions::BrokenAction
         expect(Fastlane::Actions.is_class_action?(broken_action_ref)).to eq(false)
-        
+
         # Nil
         expect(Fastlane::Actions.is_class_action?(nil)).to eq(false)
       end
-      
+
       it "is action deprecated" do
         Fastlane::Actions.load_external_actions("./fastlane/spec/fixtures/actions")
         require_relative './fixtures/broken_actions/broken_action.rb'
         require_relative './fixtures/deprecated_actions/deprecated_action.rb'
-        
+
         # An action (not deprecated)
         example_action_ref = Fastlane::Actions.action_class_ref("example_action")
         expect(Fastlane::Actions.is_deprecated?(example_action_ref)).to eq(false)
-        
+
         # An action (depreated)
         deprecated_action_ref = Fastlane::Actions.action_class_ref("deprecated_action")
         expect(Fastlane::Actions.is_deprecated?(deprecated_action_ref)).to eq(true)
-        
+
         # Not an action
         broken_action_ref = Fastlane::Actions::BrokenAction
         expect(Fastlane::Actions.is_deprecated?(broken_action_ref)).to eq(false)
-      
+
         # Nil
         expect(Fastlane::Actions.is_deprecated?(nil)).to eq(false)
       end
