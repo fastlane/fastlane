6b2f373332526c3d33e0b1c75286a006de2de7a7
rbaumbach <rbaumbach@users.noreply.github.com>
2017-01-09 07:25:49 -0800

    Add verbose flag to zip action with a default value of 'true' (#7777)

--- fastlane/spec/actions_specs/zip_spec.rb
+++ fastlane/spec/actions_specs/zip_spec.rb
@@ -1,13 +1,23 @@
 describe Fastlane do
   describe Fastlane::FastFile do
+    before do
+      @path = "./fastlane/spec/fixtures/actions/archive.rb"
+    end
+
     describe "zip" do
       it "generates a valid zip command" do
-        path = "./fastlane/spec/fixtures/actions/archive.rb"
-
-        expect(Fastlane::Actions).to receive(:sh).with("zip -r ./fastlane/spec/fixtures/actions/archive.rb.zip archive.rb")
+        expect(Fastlane::Actions).to receive(:sh).with("zip -r #{@path}.zip archive.rb")
 
         result = Fastlane::FastFile.new.parse("lane :test do
-          zip(path: '#{path}')
+          zip(path: '#{@path}')
+        end").runner.execute(:test)
+      end
+
+      it "generates a valid zip command without verbose output" do
+        expect(Fastlane::Actions).to receive(:sh).with("zip -rq #{@path}.zip archive.rb")
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          zip(path: '#{@path}', verbose: 'false')
         end").runner.execute(:test)
       end
     end
