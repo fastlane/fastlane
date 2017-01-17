edbb19f7098056d3669cae1a4d5400d5122457e9
Helmut Januschka <helmut@januschka.com>
2017-01-17 16:36:34 +0100

    add `json_file?` helper (#7878)

--- supply/lib/supply/options.rb
+++ supply/lib/supply/options.rb
@@ -63,6 +63,7 @@ module Supply
                                      description: "The service account json file used to authenticate with Google",
                                      default_value: CredentialsManager::AppfileConfig.try_fetch_value(:json_key_file),
                                      verify_block: proc do |value|
+                                       UI.user_error! "'#{value}' doesn't seem to be a JSON file" unless FastlaneCore::Helper.json_file?(File.expand_path(value))
                                        UI.user_error! "Could not find service account json file at path '#{File.expand_path(value)}'" unless File.exist?(File.expand_path(value))
                                      end),
         FastlaneCore::ConfigItem.new(key: :json_key_data,
