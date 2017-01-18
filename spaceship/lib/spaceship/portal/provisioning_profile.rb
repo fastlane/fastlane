a00b7aad17b174bdbf804912ca19880618c02d4f
Ashton Williams <Ashton-W@users.noreply.github.com>
2017-01-18 18:02:15 +1100

    document parameters for ProvisioningProfile.all

--- spaceship/lib/spaceship/portal/provisioning_profile.rb
+++ spaceship/lib/spaceship/portal/provisioning_profile.rb
@@ -249,6 +249,8 @@ module Spaceship
         # @return (Array) Returns all profiles registered for this account
         #  If you're calling this from a subclass (like AdHoc), this will
         #  only return the profiles that are of this type
+        # @param mac (Bool) (optional): Pass true to get all Mac provisioning profiles
+        # @param xcode (Bool) (optional): Pass true to include Xcode managed provisioning profiles
         def all(mac: false, xcode: false)
           profiles = client.provisioning_profiles(mac: mac).map do |profile|
             self.factory(profile)
