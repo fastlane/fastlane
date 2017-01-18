3e27b378854ce88151fce0a6292e3c9a68f18c7a
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2017-01-18 17:23:37 -0500

    Add the option to export simulator device logs

--- snapshot/spec/runner_spec.rb
+++ snapshot/spec/runner_spec.rb
@@ -7,5 +7,25 @@ describe Snapshot do
         expect(helper_version).to match(/^SnapshotHelperVersion \[\d.\d\]$/)
       end
     end
+
+    describe 'output_simulator_logs' do
+      before(:each) do
+        allow(FileUtils).to receive(:cp).with(anything, anything)
+        allow(File).to receive(:exist?).and_return(true)
+      end
+
+      it 'copies all device logs to the output directory' do
+        Snapshot.config = FastlaneCore::Configuration.create(Snapshot::Options.available_options, {
+          output_directory: '/tmp/scan_results',
+          output_simulator_logs: true,
+          devices: ['iPhone 6s', 'iPad Air'],
+          project: './snapshot/example/Example.xcodeproj',
+          scheme: 'ExampleUITests'
+        })
+        expect(FileUtils).to receive(:cp).with(/.*/, %r{#{Snapshot.config[:output_directory]}/iPhone .*_iOS_.*system.log})
+        expect(FileUtils).to receive(:cp).with(/.*/, %r{#{Snapshot.config[:output_directory]}/iPad Air_iOS_.*system.log})
+        runner.output_simulator_logs
+      end
+    end
   end
 end
