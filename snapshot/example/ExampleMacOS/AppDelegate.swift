e36b1a779037d6166b73891744ebe54ef84d6f27
Alexander Semenov <alexs@2kgroup.com>
2017-01-19 14:23:15 +0200

    Added example app for OSX

---
+++ snapshot/example/ExampleMacOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ExampleMacOS
+//
+//  Created by Alexander Semenov on 1/19/17.
+//  Copyright © 2017 Felix Krause. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ snapshot/example/ExampleMacOSUITests/ExampleMacOSUITests.swift
@@ -0,0 +1,25 @@
+//
+//  ExampleMacOSUITests.swift
+//  ExampleMacOSUITests
+//
+//  Created by Alexander Semenov on 1/19/17.
+//  Copyright © 2017 Felix Krause. All rights reserved.
+//
+
+import XCTest
+
+class ExampleMacOSUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+    
+    func testExample() {
+        snapshot("0Launch")
+    }
+    
+}
