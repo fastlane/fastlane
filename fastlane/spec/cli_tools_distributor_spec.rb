ecb6029f9ad34be3bfbd09fc4b69090b6933eaeb
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-01-20 23:38:28 +0100

    Set ARGV for good instead of temporarily set it as it causes weird test failures. To investigate later

--- fastlane/spec/cli_tools_distributor_spec.rb
+++ fastlane/spec/cli_tools_distributor_spec.rb
@@ -2,21 +2,19 @@ require 'fastlane/cli_tools_distributor'
 
 describe Fastlane::CLIToolsDistributor do
   it "runs the lane instead of the tool when there is a conflict" do
-    FastlaneSpec::Env.with_ARGV(["sigh"]) do
-      require 'fastlane/commands_generator'
-      expect(FastlaneCore::FastlaneFolder).to receive(:fastfile_path).and_return("./fastlane/spec/fixtures/fastfiles/FastfileUseToolNameAsLane").at_least(:once)
-      expect(Fastlane::CommandsGenerator).to receive(:start).and_return(nil)
-      Fastlane::CLIToolsDistributor.take_off
-    end
+    FastlaneSpec::Env.with_ARGV(["sigh"])
+    require 'fastlane/commands_generator'
+    expect(FastlaneCore::FastlaneFolder).to receive(:fastfile_path).and_return("./fastlane/spec/fixtures/fastfiles/FastfileUseToolNameAsLane").at_least(:once)
+    expect(Fastlane::CommandsGenerator).to receive(:start).and_return(nil)
+    Fastlane::CLIToolsDistributor.take_off
   end
 
   it "runs a separate tool when the tool is available and the name is not used in a lane" do
-    FastlaneSpec::Env.with_ARGV(["gym"]) do
-      require 'gym/options'
-      require 'gym/commands_generator'
-      expect(FastlaneCore::FastlaneFolder).to receive(:fastfile_path).and_return("./fastlane/spec/fixtures/fastfiles/FastfileUseToolNameAsLane").at_least(:once)
-      expect(Gym::CommandsGenerator).to receive(:start).and_return(nil)
-      Fastlane::CLIToolsDistributor.take_off
-    end
+    FastlaneSpec::Env.with_ARGV(["gym"])
+    require 'gym/options'
+    require 'gym/commands_generator'
+    expect(FastlaneCore::FastlaneFolder).to receive(:fastfile_path).and_return("./fastlane/spec/fixtures/fastfiles/FastfileUseToolNameAsLane").at_least(:once)
+    expect(Gym::CommandsGenerator).to receive(:start).and_return(nil)
+    Fastlane::CLIToolsDistributor.take_off
   end
 end
