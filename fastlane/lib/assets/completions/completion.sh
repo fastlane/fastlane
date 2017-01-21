adf57f0cd3d699ee99a34a6fe6fe67874fcab9af
Maksym Grebenets <maksym.grebenets@cba.com.au>
2017-01-21 20:58:27 +1100

    Disable SC2039 for use of source command

--- fastlane/lib/assets/completions/completion.sh
+++ fastlane/lib/assets/completions/completion.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # shellcheck disable=SC2155
 # shellcheck disable=SC1090
+# shellcheck disable=SC2039
 
 if [ -n "$BASH_VERSION" ]; then
-  . ~/.fastlane/completions/completion.bash
+  source ~/.fastlane/completions/completion.bash
 elif [ -n "$ZSH_VERSION" ]; then
-  . ~/.fastlane/completions/completion.zsh
+  source ~/.fastlane/completions/completion.zsh
 fi
 
 # Do not remove v0.0.1
