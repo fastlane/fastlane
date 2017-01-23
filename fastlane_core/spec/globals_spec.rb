37c0b37c7943ed638cacfbfb733c4adf61b26d0a
Helmut Januschka <helmut@januschka.com>
2017-01-23 16:21:05 +0100

    Update globals_spec.rb

--- fastlane_core/spec/globals_spec.rb
+++ fastlane_core/spec/globals_spec.rb
@@ -6,14 +6,14 @@ describe FastlaneCore do
       FastlaneCore::Globals.verbose = true
       expect(FastlaneCore::Globals.verbose?).to eq(true)
       FastlaneCore::Globals.verbose = false
-      expect(FastlaneCore::Globals.verbose?).to eq(false)
+      expect(FastlaneCore::Globals.verbose?).to eq(nil)
     end
 
     it "Toggle capture_mode " do
       FastlaneCore::Globals.capture_output = true
       expect(FastlaneCore::Globals.capture_output?).to eq(true)
       FastlaneCore::Globals.capture_output = false
-      expect(FastlaneCore::Globals.capture_output?).to eq(false)
+      expect(FastlaneCore::Globals.capture_output?).to eq(nil)
     end
   end
 end
