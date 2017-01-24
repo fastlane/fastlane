df7a42482439785a7ae6efe3f3154afbf9122200
Helmut Januschka <h.januschka@krone.at>
2017-01-24 09:46:48 +0100

    feedback

--- produce/spec/manager_spec.rb
+++ produce/spec/manager_spec.rb
@@ -1,22 +1,24 @@
 describe Produce do
   describe "Manager" do
     it "should auto convert string hash keys to symbol keys" do
-      Produce.config = {
-        username: "helmut@januschka.com",
-        enabled_features: { "data_protection" => "complete" },
-        skip_itc: true
-      }
+      Produce.config = FastlaneCore::Configuration.create(Produce::Options.available_options, {
+          username: "helmut@januschka.com",
+          enabled_features: { "data_protection" => "complete" },
+          skip_itc: true
+      })
+
       instance = Produce::DeveloperCenter.new
       features = instance.enabled_features
       expect(features["dataProtection"].value).to eq("complete")
     end
 
     it "accepts symbol'd hash" do
-      Produce.config = {
+      Produce.config = FastlaneCore::Configuration.create(Produce::Options.available_options, {
         username: "helmut@januschka.com",
         enabled_features: { data_protection: "complete" },
         skip_itc: true
-      }
+      })
+
       instance = Produce::DeveloperCenter.new
       features = instance.enabled_features
       expect(features["dataProtection"].value).to eq("complete")
