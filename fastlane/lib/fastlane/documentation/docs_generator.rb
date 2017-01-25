75d55a1180a15100dabbb61730f3f564b7a50531
Felix Krause <KrauseFx@users.noreply.github.com>
2017-01-25 10:19:20 -0800

    Update URL

--- fastlane/lib/fastlane/documentation/docs_generator.rb
+++ fastlane/lib/fastlane/documentation/docs_generator.rb
@@ -35,7 +35,7 @@ module Fastlane
         output << ""
       end
 
-      output << "This README.md is auto-generated and will be re-generated every time [fastlane](fastlane.tools) is run."
+      output << "This README.md is auto-generated and will be re-generated every time [fastlane](https://fastlane.tools) is run."
       output << "More information about fastlane can be found on [fastlane.tools](https://fastlane.tools)."
       output << "The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools)."
       output << ""
