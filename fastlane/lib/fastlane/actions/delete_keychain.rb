df3896998f8c117496c5f6f09a5946e0e61ca26d
Koen Punt <koen@koenpunt.nl>
2017-01-25 22:04:16 +0100

    Always provide keychain as absolute path

--- fastlane/lib/fastlane/actions/delete_keychain.rb
+++ fastlane/lib/fastlane/actions/delete_keychain.rb
@@ -5,8 +5,19 @@ module Fastlane
     class DeleteKeychainAction < Action
       def self.run(params)
         original = Actions.lane_context[Actions::SharedValues::ORIGINAL_DEFAULT_KEYCHAIN]
+
+        if params[:name]
+          keychain_path = File.expand_path(File.join("~", "Library", "Keychains", params[:name]))
+        else
+          keychain_path = params[:path]
+        end
+
+        if keychain_path.nil?
+          UI.user_error!("You either have to set :name or :path")
+        end
+
         Fastlane::Actions.sh("security default-keychain -s #{original}", log: false) unless original.nil?
-        Fastlane::Actions.sh "security delete-keychain #{params[:name].shellescape}", log: false
+        Fastlane::Actions.sh "security delete-keychain #{keychain_path.shellescape}", log: false
       end
 
       def self.details
@@ -22,13 +33,20 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :name,
                                        env_name: "KEYCHAIN_NAME",
                                        description: "Keychain name",
-                                       optional: false)
+                                       conflicting_options: [:path],
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :path,
+                                       env_name: "KEYCHAIN_PATH",
+                                       description: "Keychain path",
+                                       conflicting_options: [:name],
+                                       optional: true)
         ]
       end
 
       def self.example_code
         [
-          'delete_keychain(name: "KeychainName")'
+          'delete_keychain(name: "KeychainName")',
+          'delete_keychain(path: "/keychains/project.keychain")'
         ]
       end
 
@@ -37,7 +55,7 @@ module Fastlane
       end
 
       def self.authors
-        ["gin0606"]
+        ["gin0606", "koenpunt"]
       end
 
       def self.is_supported?(platform)
