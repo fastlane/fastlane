c7bf787a080f1abeb086847ad6dfdbe923fb2cfc
Koen Punt <koen@koenpunt.nl>
2017-01-26 09:59:26 +0100

    update specs

--- fastlane/spec/actions_specs/delete_keychain_spec.rb
+++ fastlane/spec/actions_specs/delete_keychain_spec.rb
@@ -8,17 +8,31 @@ describe Fastlane do
           })
         end").runner.execute(:test)
 
-        expect(result).to eq("security delete-keychain test.keychain")
+        keychain = File.expand_path('~/Library/Keychains/test.keychain')
+
+        expect(result).to eq("security delete-keychain #{keychain}")
       end
 
-      it "works with keychain name that contain spaces or `\"`" do
+      it "works with keychain name that contain spaces and `\"`" do
         result = Fastlane::FastFile.new.parse("lane :test do
           delete_keychain ({
             name: '\" test \".keychain'
           })
         end").runner.execute(:test)
 
-        expect(result).to eq %(security delete-keychain \\\"\\ test\\ \\\".keychain)
+        keychain = File.expand_path(%(~/Library/Keychains/\\\"\\ test\\ \\\".keychain))
+
+        expect(result).to eq %(security delete-keychain #{keychain})
+      end
+
+      it "works with absolute keychain path" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          delete_keychain ({
+            path: '/projects/test.keychain'
+          })
+        end").runner.execute(:test)
+
+        expect(result).to eq("security delete-keychain /projects/test.keychain")
       end
     end
   end
