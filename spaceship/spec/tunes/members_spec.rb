f6e8cc23f031a782dff4643ea12709b7707b1e50
Helmut Januschka <helmut@januschka.com>
2017-01-27 20:59:33 +0100

    @krausefx feedback

--- spaceship/spec/tunes/members_spec.rb
+++ spaceship/spec/tunes/members_spec.rb
@@ -7,18 +7,22 @@ describe Spaceship::Tunes::Members do
       members = Spaceship::Members.all
       expect(members.length).to eq(3)
     end
+
     it "finds one member by email" do
       member = Spaceship::Members.find("helmut@januschka.com")
       expect(member.class).to eq(Spaceship::Tunes::Member)
       expect(member.email_address).to eq("helmut@januschka.com")
     end
+
     describe "creates a new member" do
       it "role: admin, apps: all" do
         Spaceship::Members.create!(firstname: "Helmut", lastname: "Januschka", email_address: "helmut@januschka.com")
       end
+
       it "role: developer apps: all" do
         Spaceship::Members.create!(firstname: "Helmut", lastname: "Januschka", email_address: "helmut@januschka.com", roles: ["developer"])
       end
+
       it "role: appmanager, apps: 898536088" do
         Spaceship::Members.create!(firstname: "Helmut", lastname: "Januschka", email_address: "helmut@januschka.com", roles: ["appmanager"], apps: ["898536088"])
       end
