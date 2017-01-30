d722d975ec9c2bf81b88cdb94e0c0096a9466d83
Felix Krause <KrauseFx@users.noreply.github.com>
2017-01-30 09:12:32 -0800

    Version bump to 2.13.0 (#8050)

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '2.12.0'.freeze
+  VERSION = '2.13.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
 end
