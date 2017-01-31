4b8dd60950f34d75de0f22e023bdf4f73f24fc66
Andrew Nesbitt <andrewnez@gmail.com>
2017-01-31 02:46:12 +0000

    Switch from unmaintained colored to maintained colored2 (#8028)

--- fastlane_core/lib/fastlane_core/ui/disable_colors.rb
+++ fastlane_core/lib/fastlane_core/ui/disable_colors.rb
@@ -1,15 +1,15 @@
 # This code overwrites the methods from the colored gem
-# via https://github.com/defunkt/colored/blob/master/lib/colored.rb
+# via https://github.com/kigster/colored2/blob/aa274018906641ffb07aaa3015081a174d169dfe/lib/colored2.rb
 
-require 'colored'
+require 'colored2'
 
 class String
-  Colored::COLORS.keys.each do |color|
+  Colored2::COLORS.keys.each do |color|
     define_method(color) do
       self # do nothing with the string, but return it
     end
   end
-  Colored::EXTRAS.keys.each do |extra|
+  Colored2::EXTRAS.keys.each do |extra|
     define_method(extra) do
       self # do nothing with the string, but return it
     end
