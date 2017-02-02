d3ca31a69ed04a2780cfefad7e4ba59ea29407dd
Laurent Gaches <laurent@binimo.com>
2017-02-02 22:44:22 +0100

    [Sigh] download_all name file using profile UUID instead of application bundle identifier (#8082)

--- sigh/lib/sigh/download_all.rb
+++ sigh/lib/sigh/download_all.rb
@@ -23,7 +23,7 @@ module Sigh
       type_name = profile.class.pretty_type
       type_name = "AdHoc" if profile.is_adhoc?
 
-      profile_name = "#{type_name}_#{profile.app.bundle_id}.mobileprovision" # default name
+      profile_name = "#{type_name}_#{profile.uuid}_#{profile.app.bundle_id}.mobileprovision" # default name
 
       output_path = File.join(Sigh.config[:output_path], profile_name)
       File.open(output_path, "wb") do |f|
