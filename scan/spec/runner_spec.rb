1ced81f8107fcbd6b2f6fc9346e4bfe660c49f22
Michael Furtak <michael.furtak@gmail.com>
2017-02-04 14:16:22 -0500

    Fix the broken scan test for include_simulator_logs (#8130)
    Circle CI is setting the env var for this option
    which is making our test fail. This makes the test
    more explicit.
    
    * Fix the broken scan test for include_simulator_logs
    * Add a positive test case for include_simulator_logs

--- scan/spec/runner_spec.rb
+++ scan/spec/runner_spec.rb
@@ -11,13 +11,30 @@ describe Scan do
         @scan = Scan::Runner.new
       end
 
-      describe "without Scan option :include_simulator_logs" do
+      describe "with scan option :include_simulator_logs set to false" do
         it "does not copy any device logs to the output directory" do
+          # Circle CI is setting the SCAN_INCLUDE_SIMULATOR_LOGS env var, so just leaving
+          # the include_simulator_logs option out does not let it default to false
           Scan.config = FastlaneCore::Configuration.create(Scan::Options.available_options, {
             output_directory: '/tmp/scan_results',
-            project: './scan/examples/standard/app.xcodeproj'
+            project: './scan/examples/standard/app.xcodeproj',
+            include_simulator_logs: false
           })
-          expect(FileUtils).not_to receive(:cp)
+          expect(FastlaneCore::Simulator).not_to receive(:copy_logarchive)
+          @scan.handle_results(0)
+        end
+      end
+
+      describe "with scan option :include_simulator_logs set to true" do
+        it "copies any device logs to the output directory" do
+          # Circle CI is setting the SCAN_INCLUDE_SIMULATOR_LOGS env var, so just leaving
+          # the include_simulator_logs option out does not let it default to false
+          Scan.config = FastlaneCore::Configuration.create(Scan::Options.available_options, {
+            output_directory: '/tmp/scan_results',
+            project: './scan/examples/standard/app.xcodeproj',
+            include_simulator_logs: true
+          })
+          expect(FastlaneCore::Simulator).to receive(:copy_logarchive)
           @scan.handle_results(0)
         end
       end
