1c8c970747a29ced27f9437da731016da2ada339
Michael Furtak <michael.furtak@gmail.com>
2017-02-04 14:05:11 -0500

    Add a positive test case for include_simulator_logs

--- scan/spec/runner_spec.rb
+++ scan/spec/runner_spec.rb
@@ -24,6 +24,20 @@ describe Scan do
           @scan.handle_results(0)
         end
       end
+
+      describe "with scan option :include_simulator_logs set to true" do
+        it "copies any device logs to the output directory" do
+          # Circle CI is setting the SCAN_INCLUDE_SIMULATOR_LOGS env var, so just leaving
+          # the include_simulator_logs option out does not let it default to false
+          Scan.config = FastlaneCore::Configuration.create(Scan::Options.available_options, {
+            output_directory: '/tmp/scan_results',
+            project: './scan/examples/standard/app.xcodeproj',
+            include_simulator_logs: true
+          })
+          expect(FastlaneCore::Simulator).to receive(:copy_logarchive)
+          @scan.handle_results(0)
+        end
+      end
     end
   end
 end
