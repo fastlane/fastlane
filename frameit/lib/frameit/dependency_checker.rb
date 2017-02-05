eb2423f44eb686cf76d6d5b56998793fce2ae523
Mokhlas Hussein <mokhleshussien@aol.com>
2017-02-05 09:56:14 +0100

    added bidi dependency check

--- frameit/lib/frameit/dependency_checker.rb
+++ frameit/lib/frameit/dependency_checker.rb
@@ -4,8 +4,20 @@ module Frameit
       return if Helper.test?
 
       self.check_image_magick
+      self.check_bidi
     end
-
+    
+    def self.check_bidi
+      begin
+        Gem::Specification.find_by_name("bidi")
+      rescue Gem::LoadError
+        UI.error '#############################################################'
+        UI.error "# You have to install the bidi to use FrameIt"
+        UI.error "# Install it using 'gem install bidi'"
+        UI.error '#############################################################'
+        UI.user_error! "Install bidi and start frameit again!"
+      end
+    end
     def self.check_image_magick
       unless `which convert`.include? "convert"
         UI.error '#############################################################'
