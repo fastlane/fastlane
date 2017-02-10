bb6b1223f7d8f943a71f223b3fd5d9502acdf020
giginet <giginet.net@gmail.com>
2017-02-11 00:04:42 +0900

    Add spec for Deliver::Setup

--- deliver/spec/setup_spec.rb
+++ deliver/spec/setup_spec.rb
@@ -16,5 +16,41 @@ describe Deliver do
       # Deliver::Runner.new(options) # to login
       # Deliver::Setup.new.run(options)
     end
+
+    describe '#generate_metadata_files' do
+      context 'with review_information' do
+        let(:version) do
+          double(
+              'version',
+              review_first_name: 'John',
+              review_last_name: 'Smith',
+              review_phone_number: '+819012345678',
+              review_email: 'deliver@example.com',
+              review_demo_user: 'user',
+              review_demo_password: 'password',
+              review_notes: 'This is a note',
+          )
+        end
+        let(:application) { double('application') }
+        let(:setup) { Deliver::Setup.new }
+        let(:tempdir) { Dir.mktmpdir }
+        before do
+          allow(version).to receive_message_chain('application.details').and_return(application)
+          allow(version).to receive_message_chain('description.languages').and_return([])
+          allow(version).to receive_message_chain('large_app_icon.asset_token').and_return(nil)
+          allow(version).to receive_message_chain('watch_app_icon.asset_token').and_return(nil)
+          stub_const('Deliver::UploadMetadata::NON_LOCALISED_VERSION_VALUES', [])
+          stub_const('Deliver::UploadMetadata::NON_LOCALISED_APP_VALUES', [])
+        end
+
+        it 'generates review information' do
+          setup.generate_metadata_files(version, tempdir)
+          base_dir = File.join(tempdir, 'review_information')
+          %w(first_name last_name phone_number email_address demo_user demo_password notes).each do |filename|
+            expect(File.exist?(File.join(base_dir, "#{filename}.txt"))).to be_truthy
+          end
+        end
+      end
+    end
   end
 end
