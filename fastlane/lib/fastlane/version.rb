8e9a0c79a138de098da835d1a07bfda03b89f5ef
Andrea Falcone <asfalcone@asfalcone-macbookpro.roam.corp.google.com>
2017-02-13 15:45:36 -0500

    Version bump to 2.15.0

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,4 @@
 module Fastlane
-  VERSION = '2.14.2'.freeze
+  VERSION = '2.15.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
 end
