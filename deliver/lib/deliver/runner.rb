fastlane/fastlane

commit 788f457d72082ed93f0bbcc68235989e52708f91

[deliver] respect file extension based on users uploaded one. (#7868)

deliver/lib/deliver/runner.rb
@@ -72,10 +72,10 @@ module Deliver
     def prepare_app_icons(options = {})
       return unless options[:metadata_path]
 
-      default_app_icon_path = File.join(options[:metadata_path], "app_icon.png")
+      default_app_icon_path = Dir[File.join(options[:metadata_path], "app_icon.{png,jpg}")].first
       options[:app_icon] ||= default_app_icon_path if File.exist?(default_app_icon_path)
 
-      default_watch_icon_path = File.join(options[:metadata_path], "watch_icon.png")
+      default_watch_icon_path = Dir[File.join(options[:metadata_path], "watch_icon.{png,jpg}")].first
       options[:app_icon] ||= default_watch_icon_path if File.exist?(default_watch_icon_path)
     end
 

deliver/lib/deliver/setup.rb
@@ -70,12 +70,14 @@ module Deliver
 
       # get App icon + watch icon
       if v.large_app_icon.asset_token
-        app_icon_path = File.join(path, "app_icon.png")
+        app_icon_extension = File.extname(v.large_app_icon.url)
+        app_icon_path = File.join(path, "app_icon#{app_icon_extension}")
         File.write(app_icon_path, open(v.large_app_icon.url).read)
         UI.success("Successfully downloaded large app icon")
       end
       if v.watch_app_icon.asset_token
-        watch_icon_path = File.join(path, "watch_icon.png")
+        watch_app_icon_extension = File.extname(v.watch_app_icon.url)
+        watch_icon_path = File.join(path, "watch_icon#{watch_app_icon_extension}")
         File.write(watch_icon_path, open(v.watch_app_icon.url).read)
         UI.success("Successfully downloaded watch icon")
       end

commit 0c930f06c913e60883d4cb84d12265d4213d218f

[deliver] Fix uncaught nil value + watch icon option name (#8225)
* [deliver] Add nil check when finding app icons

Fixes issue introduced by #7868

* [deliver] Set correct option when a watch icon is found

deliver/lib/deliver/runner.rb
@@ -73,10 +73,10 @@ module Deliver
       return unless options[:metadata_path]
 
       default_app_icon_path = Dir[File.join(options[:metadata_path], "app_icon.{png,jpg}")].first
-      options[:app_icon] ||= default_app_icon_path if File.exist?(default_app_icon_path)
+      options[:app_icon] ||= default_app_icon_path if default_app_icon_path && File.exist?(default_app_icon_path)
 
       default_watch_icon_path = Dir[File.join(options[:metadata_path], "watch_icon.{png,jpg}")].first
-      options[:app_icon] ||= default_watch_icon_path if File.exist?(default_watch_icon_path)
+      options[:apple_watch_app_icon] ||= default_watch_icon_path if default_watch_icon_path && File.exist?(default_watch_icon_path)
     end
 
     # Upload the binary to iTunes Connect