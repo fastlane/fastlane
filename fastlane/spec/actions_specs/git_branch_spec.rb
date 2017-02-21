3d4669c2c1e16dc9899726b089c19c478eb54e76
Michael Furtak <michael.furtak@gmail.com>
2017-02-21 14:52:36 -0500

    Fix git-branch crash if no ENV vars present (#8321)
    Add specs to test the behavior

---
+++ fastlane/spec/actions_specs/git_branch_spec.rb
@@ -0,0 +1,29 @@
+describe Fastlane::Actions::GitBranchAction do
+  describe "CI set ENV values" do
+    Fastlane::Actions::SharedValues::GIT_BRANCH_ENV_VARS.each do |env_var|
+      it "can control the output of the action with #{env_var}" do
+        with_env_values(env_var => "#{env_var}-branch-name") do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            git_branch
+          end").runner.execute(:test)
+
+          expect(result).to eq("#{env_var}-branch-name")
+        end
+      end
+    end
+  end
+
+  describe "with no CI set ENV values" do
+    it "gets the value from Git directly" do
+      expect(Fastlane::Actions::GitBranchAction).to receive(:`)
+        .with('git symbolic-ref HEAD --short 2>/dev/null')
+        .and_return('branch-name')
+
+      result = Fastlane::FastFile.new.parse("lane :test do
+        git_branch
+      end").runner.execute(:test)
+
+      expect(result).to eq("branch-name")
+    end
+  end
+end
