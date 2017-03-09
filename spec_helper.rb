b6aebfeac003767abf6d93885b99da2ef49cd05f
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2017-03-09 22:07:56 +0900

    Mock FastlaneCore::FastlaneFolder.path in spec_helper

--- spec_helper.rb
+++ spec_helper.rb
@@ -30,6 +30,10 @@ end
 my_main = self
 RSpec.configure do |config|
   config.before(:each) do |current_test|
+    if current_test.id.match(/\/fastlane\//)
+      allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
+    end
+
     tool_name = current_test.id.match(%r{\.\/(\w+)\/})[1]
     method_name = "before_each_#{tool_name}".to_sym
     begin
