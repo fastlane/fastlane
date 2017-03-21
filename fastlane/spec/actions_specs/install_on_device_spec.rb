c62b14b28e010536a29a50c446f6303871729008
Rafael Sachetto <rafael.sachetto@gmail.com>
2017-03-21 15:21:23 -0300

    Fix actions install on device (#8582)
    * FIX escape ipa path to compile using ios-deploy
    
    * Testing escape ipa path to compile using ios-deploy
    
    * Replace from gsub to shellescape

--- fastlane/spec/actions_specs/install_on_device_spec.rb
+++ fastlane/spec/actions_specs/install_on_device_spec.rb
@@ -7,6 +7,13 @@ describe Fastlane do
         end").runner.execute(:test)
         expect(result).to eq("ios-deploy  --bundle demo.ipa")
       end
+
+      it "generates a valid command using app name with space" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          install_on_device(ipa: 'demo app.ipa')
+        end").runner.execute(:test)
+        expect(result).to eq("ios-deploy  --bundle demo\\ app.ipa")
+      end
     end
   end
 end
