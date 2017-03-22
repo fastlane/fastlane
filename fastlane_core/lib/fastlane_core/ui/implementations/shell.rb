d46b8177806e192ba4c8b30ff0534f39e0c2181d
Felix Krause <krausefx@gmail.com>
2017-03-21 18:02:41 -0700

    Prefix all user questions with time stamp just like the rest of the output

--- fastlane_core/lib/fastlane_core/ui/implementations/shell.rb
+++ fastlane_core/lib/fastlane_core/ui/implementations/shell.rb
@@ -23,7 +23,7 @@ module FastlaneCore
       @log
     end
 
-    def format_string(datetime, severity)
+    def format_string(datetime = Time.now, severity = "")
       if FastlaneCore::Globals.verbose?
         return "#{severity} [#{datetime.strftime('%Y-%m-%d %H:%M:%S.%2N')}]: "
       elsif FastlaneCore::Env.truthy?("FASTLANE_HIDE_TIMESTAMP")
@@ -74,7 +74,7 @@ module FastlaneCore
     end
 
     def header(message)
-      format = format_string(Time.now, "")
+      format = format_string
       if message.length + 8 < TTY::Screen.width - format.length
         message = "--- #{message} ---"
         i = message.length
@@ -99,12 +99,12 @@ module FastlaneCore
 
     def input(message)
       verify_interactive!(message)
-      ask(message.to_s.yellow).to_s.strip
+      ask("#{format_string}#{message.to_s.yellow}").to_s.strip
     end
 
     def confirm(message)
       verify_interactive!(message)
-      agree("#{message} (y/n)".yellow, true)
+      agree("#{format_string}#{message.to_s.yellow} (y/n)", true)
     end
 
     def select(message, options)
@@ -117,7 +117,7 @@ module FastlaneCore
     def password(message)
       verify_interactive!(message)
 
-      ask(message.yellow) { |q| q.echo = "*" }
+      ask("#{format_string}#{message.to_s.yellow}") { |q| q.echo = "*" }
     end
 
     private
