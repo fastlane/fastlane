0b2070ff5aaedc0ed7c07bb2d6b36bd7bb1bce2d
giginet <giginet.net@gmail.com>
2017-03-26 23:26:12 +0900

    Apply rubocop -a

--- fastlane/lib/fastlane/actions/deploygate.rb
+++ fastlane/lib/fastlane/actions/deploygate.rb
@@ -32,7 +32,6 @@ module Fastlane
         })
 
         connection.post("/api/users/#{user_name}/apps", options)
-
       rescue Faraday::Error::TimeoutError
         UI.crash! "Timed out while uploading build. Check https://deploygate.com/ to see if the upload was completed."
       end
