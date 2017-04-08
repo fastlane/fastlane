b0646cb58d216ace4eadaf78adcaec0ecddfe54f
Felix Krause <krausefx@gmail.com>
2017-04-07 17:56:39 -0700

    Fix code style

--- fastlane_core/lib/fastlane_core/print_table.rb
+++ fastlane_core/lib/fastlane_core/print_table.rb
@@ -52,7 +52,9 @@ module FastlaneCore
       end
 
       def should_transform?
-        return false if (FastlaneCore::Helper.ci? && !FastlaneCore::Helper.test?)
+        if FastlaneCore::Helper.ci? && !FastlaneCore::Helper.test?
+          return false
+        end
         return !FastlaneCore::Env.truthy?("FL_SKIP_TABLE_TRANSFORM")
       end
 
