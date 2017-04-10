78150e80c528bbf303b0647d5b80cbbb58ae2dbb
Lyndsey Ferguson <lyndsey-ferguson@users.noreply.github.com>
2017-04-10 15:18:50 -0500

    Update 'OtherAction' error message to indicate 'other_action' instead (#8840)

--- fastlane/lib/fastlane/action.rb
+++ fastlane/lib/fastlane/action.rb
@@ -115,7 +115,7 @@ module Fastlane
     # Allows the user to call an action from an action
     def self.method_missing(method_sym, *arguments, &_block)
       UI.error("Unknown method '#{method_sym}'")
-      UI.user_error!("To call another action from an action use `OtherAction.#{method_sym}` instead")
+      UI.user_error!("To call another action from an action use `other_action.#{method_sym}` instead")
     end
 
     # When shelling out from the actoin, should we use `bundle exec`?
