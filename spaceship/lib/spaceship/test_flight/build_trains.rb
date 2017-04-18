23d4db0bfa3951d386924ec9fd0ad6b3840d348b
David Ohayon <ohayon.1@gmail.com>
2017-04-18 17:19:56 -0400

    add comment to BuildTrains

--- spaceship/lib/spaceship/test_flight/build_trains.rb
+++ spaceship/lib/spaceship/test_flight/build_trains.rb
@@ -3,6 +3,9 @@ module TestFlight
 
     include Enumerable
 
+    # This returns hashes that are partial versions of TestFlight::Build objects
+    # We are using hashes here for now because we dont want
+    # Them to be misinterpreted as Build objects.
     def self.all(provider_id: nil, app_id: nil, platform: nil)
       data = client.get_build_trains(provider_id: provider_id, app_id: app_id, platform: platform)
       trains = {}
