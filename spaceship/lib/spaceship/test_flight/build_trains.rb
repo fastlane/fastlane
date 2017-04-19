0a05b11bc2360deb6d8da14ab79a7eaf590e8b5d
Helmut Januschka <helmut@januschka.com>
2017-04-19 08:17:42 +0200

    get rid again of provider_id

--- spaceship/lib/spaceship/test_flight/build_trains.rb
+++ spaceship/lib/spaceship/test_flight/build_trains.rb
@@ -5,11 +5,11 @@ module TestFlight
     # This returns hashes that are partial versions of TestFlight::Build objects
     # We are using hashes here for now because we dont want
     # Them to be misinterpreted as Build objects.
-    def self.all(provider_id: nil, app_id: nil, platform: nil)
-      data = client.get_build_trains(provider_id: provider_id, app_id: app_id, platform: platform)
+    def self.all(app_id: nil, platform: nil)
+      data = client.get_build_trains(app_id: app_id, platform: platform)
       trains = {}
       data.each do |train_version|
-        trains[train_version] = client.get_builds_for_train(provider_id: provider_id, app_id: app_id, platform: platform, train_version: train_version)
+        trains[train_version] = client.get_builds_for_train(app_id: app_id, platform: platform, train_version: train_version)
       end
 
       self.new(trains)
