39c1f27dbbe67182f603ae7102c57888d3032bc4
Helmut Januschka <helmut@januschka.com>
2017-05-02 07:46:50 +0200

    remove write ops for dev. response

--- spaceship/lib/spaceship/tunes/tunes_client.rb
+++ spaceship/lib/spaceship/tunes/tunes_client.rb
@@ -323,38 +323,6 @@ module Spaceship
       parse_response(r, 'data')
     end
 
-    def create_developer_response!(app_id: nil, platform: "ios", review_id: nil, response: nil)
-      raise "app_id is required" unless app_id
-      raise "review_id is required" unless review_id
-      raise "response is required" unless response
-
-      data = {
-        responseText: response,
-        reviewId: review_id
-      }
-      request(:post) do |req|
-        req.url "ra/apps/#{app_id}/platforms/#{platform}/reviews/#{review_id}/responses"
-        req.body = data.to_json
-        req.headers['Content-Type'] = 'application/json'
-      end
-    end
-
-    def update_developer_response!(app_id: nil, platform: "ios", review_id: nil, response_id: nil, response: nil)
-      raise "app_id is required" unless app_id
-      raise "review_id is required" unless review_id
-      raise "response_id is required" unless response_id
-      raise "response is required" unless response
-
-      data = {
-        responseText: response
-      }
-      request(:put) do |req|
-        req.url "ra/apps/#{app_id}/platforms/#{platform}/reviews/#{review_id}/responses/#{response_id}"
-        req.body = data.to_json
-        req.headers['Content-Type'] = 'application/json'
-      end
-    end
-
     def get_reviews(app_id, platform, storefront, versionId = '')
       index = 0
       per_page = 100 # apple default
