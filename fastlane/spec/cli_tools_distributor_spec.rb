4339ac28f742a2b7c7b4aaa987be1e52981b1edd
David Ohayon <ohayon.1@gmail.com>
2017-05-11 15:45:53 -0400

    Set the service field to the action name (#9145)

--- fastlane/spec/cli_tools_distributor_spec.rb
+++ fastlane/spec/cli_tools_distributor_spec.rb
@@ -21,10 +21,6 @@ describe Fastlane::CLIToolsDistributor do
   end
 
   describe "update checking" do
-    before do
-      allow(FastlaneCore::CrashReporter).to receive(:report_crash)
-    end
-
     it "checks for updates when running a lane" do
       FastlaneSpec::Env.with_ARGV(["sigh"])
       require 'fastlane/commands_generator'
--- fastlane_core/spec/crash_reporter/crash_reporter_spec.rb
+++ fastlane_core/spec/crash_reporter/crash_reporter_spec.rb
@@ -1,5 +1,16 @@
 describe FastlaneCore::CrashReporter do
   context 'crash reporting' do
+    before do
+      ENV['FASTLANE_OPT_OUT_CRASH_REPORTING'] = nil
+      FastlaneCore::CrashReporter.reset_crash_reporter_for_testing
+      FastlaneCore::CrashReporter.enable_for_testing
+    end
+
+    after do
+      ENV['FASTLANE_OPT_OUT_CRASH_REPORTING'] = '1'
+      FastlaneCore::CrashReporter.disable_for_testing
+    end
+
     let(:exception) { double('Exception') }
 
     let(:stub_body) do
@@ -22,9 +33,25 @@ describe FastlaneCore::CrashReporter do
       end
 
       it 'posts a report to Stackdriver with specified type' do
-        stub_stackdriver_request(type: :crash)
+        stub_stackdriver_request
         setup_crash_report_generator_expectation(type: :crash)
         FastlaneCore::CrashReporter.report_crash(type: :crash, exception: exception)
+      end
+
+      it 'posts a report to Stackdriver with specified service' do
+        stub_stackdriver_request
+        setup_crash_report_generator_expectation(action: 'test_action')
+        FastlaneCore::CrashReporter.report_crash(action: 'test_action', exception: exception)
+      end
+
+      it 'only posts one report' do
+        stub_stackdriver_request
+        setup_crash_report_generator_expectation
+        FastlaneCore::CrashReporter.report_crash(exception: exception)
+
+        # The expectation we set up above is only for one invocation of the
+        # report generator, so if this calls it again, it will fail
+        FastlaneCore::CrashReporter.report_crash(exception: exception)
       end
     end
 
@@ -38,10 +65,6 @@ describe FastlaneCore::CrashReporter do
       it 'does not post a report to Stackdriver if opted out' do
         ENV['FASTLANE_OPT_OUT_CRASH_REPORTING'] = '1'
         assert_not_requested(stub_stackdriver_request)
-      end
-
-      after do
-        ENV['FASTLANE_OPT_OUT_CRASH_REPORTING'] = nil
       end
     end
 
@@ -78,14 +101,15 @@ def supress_stackdriver_reporting
   stub_stackdriver_request
 end
 
-def setup_crash_report_generator_expectation(type: :unknown)
+def setup_crash_report_generator_expectation(type: :unknown, action: nil)
   expect(FastlaneCore::CrashReportGenerator).to receive(:generate).with(
     type: type,
-    exception: exception
+    exception: exception,
+    action: action
   ).and_return(stub_body.to_json)
 end
 
-def stub_stackdriver_request(type: :unknown)
+def stub_stackdriver_request
   stub_request(:post, %r{https:\/\/clouderrorreporting.googleapis.com\/v1beta1\/projects\/fastlane-166414\/events:report\?key=.*}).with do |request|
     request.body == stub_body.to_json
   end
