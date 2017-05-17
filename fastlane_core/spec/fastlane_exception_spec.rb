6de583f346b949aa2f82408a6fcc289666deddfe
David Ohayon <ohayon.1@gmail.com>
2017-05-17 14:41:14 -0400

    rubocop

--- fastlane_core/spec/fastlane_exception_spec.rb
+++ fastlane_core/spec/fastlane_exception_spec.rb
@@ -18,7 +18,7 @@ describe FastlaneCore::Interface::FastlaneException do
 
     it 'properly determines that the exception was raised explicitly' do
       begin
-        raise FastlaneCore::Interface::FastlaneError.new
+        raise FastlaneCore::Interface::FastlaneError.new, 'EXPLICITLY RAISED ERROR'
       rescue => e
         expect(e.caused_by_calling_ui_method?(method_name: 'user_error!')). to be false
       end
@@ -37,7 +37,7 @@ describe FastlaneCore::Interface::FastlaneException do
 
     it 'does not trim backtrace if raised explicitly' do
       begin
-        raise FastlaneCore::Interface::FastlaneError.new
+        raise FastlaneCore::Interface::FastlaneError.new, 'EXPLICITLY RAISED ERROR'
       rescue => e
         expect(e).to respond_to(:trimmed_backtrace)
         expect(e.trimmed_backtrace.count).to eq(e.backtrace.count)
@@ -57,7 +57,7 @@ describe FastlaneCore::Interface::FastlaneException do
 
     it 'returns the original exception message if the message does not contain PII' do
       begin
-        raise FastlaneCore::Interface::FastlaneError.new
+        raise FastlaneCore::Interface::FastlaneError.new, 'EXPLICITLY RAISED ERROR'
       rescue => e
         expect(e).to respond_to(:crash_report_message)
         expect(e.crash_report_message).to eq(e.message)
