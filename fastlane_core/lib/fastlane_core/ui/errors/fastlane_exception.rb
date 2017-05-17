08ac56e544587fe8d72fe0dcbf435a4c681b5b5b
David Ohayon <ohayon.1@gmail.com>
2017-05-17 16:01:25 -0400

    Move error types into their own files

---
+++ fastlane_core/lib/fastlane_core/ui/errors/fastlane_exception.rb
@@ -0,0 +1,42 @@
+module FastlaneCore
+  class Interface
+    class FastlaneException < StandardError
+      def prefix
+        '[FASTLANE_EXCEPTION]'
+      end
+
+      def caused_by_calling_ui_method?(method_name: nil)
+        return false if backtrace.nil? || backtrace[0].nil? || method_name.nil?
+        first_frame = backtrace[0]
+        if first_frame.include?(method_name) && first_frame.include?('interface.rb')
+          true
+        else
+          false
+        end
+      end
+
+      def trim_backtrace(method_name: nil)
+        if caused_by_calling_ui_method?(method_name: method_name)
+          backtrace.drop(2)
+        else
+          backtrace
+        end
+      end
+
+      def could_contain_pii?
+        caused_by_calling_ui_method?
+      end
+
+      def crash_report_message
+        return '' if could_contain_pii?
+        exception.message
+      end
+    end
+  end
+
+  class FastlaneBuildFailure < Interface::FastlaneException
+  end
+
+  class FastlaneTestFailure < Interface::FastlaneException
+  end
+end
