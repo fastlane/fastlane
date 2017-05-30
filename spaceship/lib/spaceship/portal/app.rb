456e569b37b161529e3f5ce17d77fcbb26f9d7d8
Josh Holtz <josh@rokkincat.com>
2017-05-30 09:10:48 -0500

    List produce enable_services options in config item (deprecating enabled_features) (#9223)
    * List produce enabled_features options in config item
    
    * Moved features to constant and option to show/hide in shell
    
    * Changed allow_shell to displayed_in_shell
    
    * Renamed enabled_features to enable_services to be consistent
    
    * Made code a little prettier
    
    * Rubocop changes
    
    * Made things easier to read
    
    * Updated produce readme
    
    * Fix for breaking all of action optional params
    
    * rubocop wins again
    
    * Change display_in_shell param to default to true

--- spaceship/lib/spaceship/portal/app.rb
+++ spaceship/lib/spaceship/portal/app.rb
@@ -33,8 +33,8 @@ module Spaceship
       # @return (Hash) Feature details
       attr_accessor :features
 
-      # @return (Array) List of enabled features
-      attr_accessor :enabled_features
+      # @return (Array) List of enabled services
+      attr_accessor :enable_services
 
       # @return (Bool) Development Push Enabled?
       attr_accessor :dev_push_enabled
@@ -62,7 +62,7 @@ module Spaceship
         'identifier' => :bundle_id,
         'isWildCard' => :is_wildcard,
         'features' => :features,
-        'enabledFeatures' => :enabled_features,
+        'enabledFeatures' => :enable_services,
         'isDevPushEnabled' => :dev_push_enabled,
         'isProdPushEnabled' => :prod_push_enabled,
         'associatedApplicationGroupsCount' => :app_groups_count,
@@ -92,14 +92,14 @@ module Spaceship
         # @param name [String] the name of the App
         # @param mac [Bool] is this a Mac app?
         # @return (App) The app you just created
-        def create!(bundle_id: nil, name: nil, mac: false, enabled_features: {})
+        def create!(bundle_id: nil, name: nil, mac: false, enable_services: {})
           if bundle_id.end_with?('*')
             type = :wildcard
           else
             type = :explicit
           end
 
-          new_app = client.create_app!(type, name, bundle_id, mac: mac, enabled_features: enabled_features)
+          new_app = client.create_app!(type, name, bundle_id, mac: mac, enable_services: enable_services)
           self.new(new_app)
         end
 
