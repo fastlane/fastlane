2e4e1dfb34925d26f2cc9ef19d14e07fddd65d31
Joshua Liebowitz <taquitos@google.com>
2017-05-31 12:21:06 -0600

    Initial layout of review tool

---
+++ review/lib/review.rb
@@ -0,0 +1,18 @@
+require 'fastlane_core'
+require 'review/runner'
+require 'review/options'
+
+module Review
+  # Use this to just setup the configuration attribute and set it later somewhere else
+  class << self
+    attr_accessor :config
+  end
+
+  Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
+  UI = FastlaneCore::UI
+  ROOT = Pathname.new(File.expand_path('../..', __FILE__))
+
+  ENV['FASTLANE_TEAM_ID'] ||= ENV["REVIEW_TEAM_ID"]
+  ENV['DELIVER_USER'] ||= ENV["REVIEW_USERNAME"]
+  ENV['APP_IDENTIFIER'] ||= ENV["REVIEW_APP_IDENTIFIER"]
+end
---
+++ review/lib/review/options.rb
@@ -0,0 +1,42 @@
+require 'fastlane_core'
+require 'credentials_manager'
+
+module Review
+  class Options
+    def self.available_options
+      user = CredentialsManager::AppfileConfig.try_fetch_value(:apple_dev_portal_id)
+      user ||= CredentialsManager::AppfileConfig.try_fetch_value(:apple_id)
+
+      [
+        FastlaneCore::ConfigItem.new(key: :app_identifier,
+                                     short_option: "-a",
+                                     env_name: "REVIEW_APP_IDENTIFIER",
+                                     description: "The bundle identifier of your app",
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
+        FastlaneCore::ConfigItem.new(key: :username,
+                                     short_option: "-u",
+                                     env_name: "REVIEW_USERNAME",
+                                     description: "Your Apple ID Username",
+                                     default_value: user),
+        FastlaneCore::ConfigItem.new(key: :team_id,
+                                     short_option: "-b",
+                                     env_name: "REVIEW_TEAM_ID",
+                                     description: "The ID of your Developer Portal team if you're in multiple teams",
+                                     optional: true,
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:team_id),
+                                     verify_block: proc do |value|
+                                       ENV["FASTLANE_TEAM_ID"] = value.to_s
+                                     end),
+        FastlaneCore::ConfigItem.new(key: :team_name,
+                                     short_option: "-l",
+                                     env_name: "REVIEW_TEAM_NAME",
+                                     description: "The name of your Developer Portal team if you're in multiple teams",
+                                     optional: true,
+                                     default_value: CredentialsManager::AppfileConfig.try_fetch_value(:team_name),
+                                     verify_block: proc do |value|
+                                       ENV["FASTLANE_TEAM_NAME"] = value.to_s
+                                     end)
+      ]
+    end
+  end
+end
