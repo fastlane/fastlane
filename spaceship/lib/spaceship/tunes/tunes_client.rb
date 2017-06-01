883ceac399525fd2925d5664d2317c460bfe49ba
Helmut Januschka <helmut@januschka.com>
2017-06-01 21:07:42 +0200

    refactor for newest version of rating endpoint (#9334)
    * refactor for newest version of rating endpoint
    
    * address feedback
    
    * rename fixtures

--- spaceship/lib/spaceship/tunes/tunes_client.rb
+++ spaceship/lib/spaceship/tunes/tunes_client.rb
@@ -258,8 +258,13 @@ module Spaceship
       parse_response(r, 'data')
     end
 
-    def get_rating_summary(app_id, platform, versionId = '')
-      r = request(:get, "ra/apps/#{app_id}/reviews/summary?platform=#{platform}&versionId=#{versionId}")
+    def get_ratings(app_id, platform, versionId = '', storefront = '')
+      # if storefront or versionId is empty api fails
+      rating_url = "ra/apps/#{app_id}/platforms/#{platform}/reviews/summary?"
+      rating_url << "storefront=#{storefront}" unless storefront.empty?
+      rating_url << "versionId=#{versionId}" unless versionId.empty?
+
+      r = request(:get, rating_url)
       parse_response(r, 'data')
     end
 
