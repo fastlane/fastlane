4b5082f381c1d2276cb720424b182b2e73e40060
Felix Krause <KrauseFx@users.noreply.github.com>
2017-06-05 15:46:54 -0700

    Add full support for new promotional text support in iTC (#9370)

--- deliver/lib/deliver/options.rb
+++ deliver/lib/deliver/options.rb
@@ -259,6 +259,13 @@ module Deliver
                                          UI.user_error!(":keywords must be a hash with all values being strings") unless keywords.kind_of?(String)
                                        end
                                      end),
+        FastlaneCore::ConfigItem.new(key: :promotional_text,
+                                     description: "Metadata: An array of localised promotional texts",
+                                     optional: true,
+                                     is_string: false,
+                                     verify_block: proc do |value|
+                                       UI.user_error!(":keywords must be a Hash, with the language being the key") unless value.kind_of?(Hash)
+                                     end),
         FastlaneCore::ConfigItem.new(key: :release_notes,
                                      description: "Metadata: Localised release notes for this version",
                                      optional: true,
