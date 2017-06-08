06579a03ffee723e6c8c43230b34bee56f21cc26
Jaehong Kang <sinoru@me.com>
2017-06-08 18:48:10 +0900

    Add a spec for BuildNumber & Clean ups

--- fastlane/spec/actions_specs/app_store_build_number_spec.rb
+++ fastlane/spec/actions_specs/app_store_build_number_spec.rb
@@ -1,64 +1,82 @@
-require 'spec_helper'
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe Fastlane::Actions::AppStoreBuildNumberAction::BuildNumber do
+      it "Should sorted fine in mixed types" do
+        build_number1 = Fastlane::Actions::AppStoreBuildNumberAction::BuildNumber.new("3")
+        build_number2 = Fastlane::Actions::AppStoreBuildNumberAction::BuildNumber.new("3.9")
+        build_number3 = Fastlane::Actions::AppStoreBuildNumberAction::BuildNumber.new("3.9.1")
+        build_number4 = Fastlane::Actions::AppStoreBuildNumberAction::BuildNumber.new("39")
 
-describe Fastlane::Actions::AppStoreBuildNumberAction do
-  it "Should return latest build in integer" do
-    # Mock Builds
-    build1 = OpenStruct.new
-    build1.build_version = "3"
+        build_numbers = [build_number1, build_number2, build_number3, build_number4]
 
-    build2 = OpenStruct.new
-    build2.build_version = "2"
+        shuffled_build_numbers = build_numbers.shuffle
+        result = shuffled_build_numbers.sort
 
-    builds = OpenStruct.new
-    builds.builds = [build1, build2]
+        expect(result).to eq(build_numbers)
+      end
+    end
 
-    # Mock App with Buildtrain
-    spec_app = OpenStruct.new
-    spec_app.build_trains = {
-      "1.0" => builds
-    }
+    describe Fastlane::Actions::AppStoreBuildNumberAction do
+      it "Should return latest build in integer" do
+        # Mock Builds
+        build1 = OpenStruct.new
+        build1.build_version = "3"
 
-    # Mock Spaceship
-    expect(Spaceship::Tunes).to receive(:login).and_return(true)
-    expect(Spaceship::Tunes).to receive(:select_team).and_return(true)
-    expect(Spaceship::Application).to receive(:find).and_return(spec_app)
+        build2 = OpenStruct.new
+        build2.build_version = "2"
 
-    result = Fastlane::FastFile.new.parse("lane :test do
-      app_store_build_number(username:'demo@demo.com', app_identifier: 'demo.app', live: false, version: '1.0')
-    end").runner.execute(:test)
+        builds = OpenStruct.new
+        builds.builds = [build1, build2]
 
-    expect(result).to eq(3)
-  end
+        # Mock App with Buildtrain
+        spec_app = OpenStruct.new
+        spec_app.build_trains = {
+          "1.0" => builds
+        }
 
-  it "Should return latest build in mixed type" do
-    # Mock Builds
-    build1 = OpenStruct.new
-    build1.build_version = "3"
+        # Mock Spaceship
+        expect(Spaceship::Tunes).to receive(:login).and_return(true)
+        expect(Spaceship::Tunes).to receive(:select_team).and_return(true)
+        expect(Spaceship::Application).to receive(:find).and_return(spec_app)
 
-    build2 = OpenStruct.new
-    build2.build_version = "2"
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_build_number(username:'demo@demo.com', app_identifier: 'demo.app', live: false, version: '1.0')
+        end").runner.execute(:test)
 
-    build3 = OpenStruct.new
-    build3.build_version = "3.9"
+        expect(result).to eq(3)
+      end
 
-    builds = OpenStruct.new
-    builds.builds = [build1, build2, build3]
+      it "Should return latest build in mixed type" do
+        # Mock Builds
+        build1 = OpenStruct.new
+        build1.build_version = "3"
 
-    # Mock App with Buildtrain
-    spec_app = OpenStruct.new
-    spec_app.build_trains = {
-      "1.0" => builds
-    }
+        build2 = OpenStruct.new
+        build2.build_version = "2"
 
-    # Mock Spaceship
-    expect(Spaceship::Tunes).to receive(:login).and_return(true)
-    expect(Spaceship::Tunes).to receive(:select_team).and_return(true)
-    expect(Spaceship::Application).to receive(:find).and_return(spec_app)
+        build3 = OpenStruct.new
+        build3.build_version = "3.9"
 
-    result = Fastlane::FastFile.new.parse("lane :test do
-      app_store_build_number(username:'demo@demo.com', app_identifier: 'demo.app', live: false, version: '1.0')
-    end").runner.execute(:test)
+        builds = OpenStruct.new
+        builds.builds = [build1, build2, build3]
 
-    expect(result).to eq(3.9)
+        # Mock App with Buildtrain
+        spec_app = OpenStruct.new
+        spec_app.build_trains = {
+          "1.0" => builds
+        }
+
+        # Mock Spaceship
+        expect(Spaceship::Tunes).to receive(:login).and_return(true)
+        expect(Spaceship::Tunes).to receive(:select_team).and_return(true)
+        expect(Spaceship::Application).to receive(:find).and_return(spec_app)
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_build_number(username:'demo@demo.com', app_identifier: 'demo.app', live: false, version: '1.0')
+        end").runner.execute(:test)
+
+        expect(result).to eq(3.9)
+      end
+    end
   end
 end
