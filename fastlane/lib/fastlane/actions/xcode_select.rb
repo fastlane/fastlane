1baa34123da35616a47296748f9505350fc9891c
Felix Krause <KrauseFx@users.noreply.github.com>
2017-06-11 20:17:49 -0700

    Update xcode_select sample code to follow naming conventions

--- fastlane/lib/fastlane/actions/xcode_select.rb
+++ fastlane/lib/fastlane/actions/xcode_select.rb
@@ -51,7 +51,7 @@ module Fastlane
 
       def self.example_code
         [
-          'xcode_select "/Applications/Xcode6.1.app"'
+          'xcode_select "/Applications/Xcode-8.3.2.app"'
         ]
       end
 
