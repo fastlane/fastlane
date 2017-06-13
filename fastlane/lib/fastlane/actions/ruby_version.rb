5d42ea759a72696e0ebc80fe3294c33808e6d5db
David Ohayon <ohayon.1@gmail.com>
2017-06-13 16:37:03 -0400

    Create ruby_version.rb

--- fastlane/lib/fastlane/actions/ruby_version.rb
+++ fastlane/lib/fastlane/actions/ruby_version.rb
@@ -34,6 +34,13 @@ module Fastlane
       def self.example_code
         ['ruby_version "2.4.0"']
       end
+      
+      def self.details		
+        [		
+          "Add this to your `Fastfile` to require a certain version of _ruby_.",		
+          "Put it at the top of your `Fastfile to ensure that _fastlane_ is executed appropriately."		
+        ].join("\n")		
+      end
 
       def self.category
         :misc
