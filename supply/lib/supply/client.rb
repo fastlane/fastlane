5e33a7727c12a22b29ad11c8b98f645c9a88c115
Tom Wilson <tom.wilson@blueapron.com>
2017-06-13 13:55:36 -0700

    Update Supply to use 0.12.0 of google-api-client (#9445)

--- supply/lib/supply/client.rb
+++ supply/lib/supply/client.rb
@@ -130,7 +130,7 @@ module Supply
     def listings
       ensure_active_edit!
 
-      result = call_google_api { android_publisher.list_listings(current_package_name, current_edit.id) }
+      result = call_google_api { android_publisher.list_edit_listings(current_package_name, current_edit.id) }
 
       return result.listings.map do |row|
         Listing.new(self, row.language, row)
@@ -142,7 +142,7 @@ module Supply
       ensure_active_edit!
 
       begin
-        result = android_publisher.get_listing(
+        result = android_publisher.get_edit_listing(
           current_package_name,
           current_edit.id,
           language
@@ -159,7 +159,7 @@ module Supply
     def apks_version_codes
       ensure_active_edit!
 
-      result = call_google_api { android_publisher.list_apks(current_package_name, current_edit.id) }
+      result = call_google_api { android_publisher.list_edit_apks(current_package_name, current_edit.id) }
 
       return result.apks.map(&:version_code)
     end
@@ -169,7 +169,7 @@ module Supply
       ensure_active_edit!
 
       result = call_google_api do
-        android_publisher.list_apk_listings(
+        android_publisher.list_edit_apklistings(
           current_package_name,
           current_edit.id,
           apk_version_code
@@ -198,7 +198,7 @@ module Supply
       })
 
       call_google_api do
-        android_publisher.update_listing(
+        android_publisher.update_edit_listing(
           current_package_name,
           current_edit.id,
           language,
@@ -211,7 +211,7 @@ module Supply
       ensure_active_edit!
 
       result_upload = call_google_api do
-        android_publisher.upload_apk(
+        android_publisher.upload_edit_apk(
           current_package_name,
           current_edit.id,
           upload_source: path_to_apk
@@ -249,7 +249,7 @@ module Supply
       })
 
       call_google_api do
-        android_publisher.update_track(
+        android_publisher.update_edit_track(
           current_package_name,
           current_edit.id,
           track,
@@ -262,8 +262,8 @@ module Supply
     def track_version_codes(track)
       ensure_active_edit!
 
-      result = call_google_api do
-        android_publisher.get_track(
+      begin
+        result = android_publisher.get_edit_track(
           current_package_name,
           current_edit.id,
           track
@@ -282,7 +282,7 @@ module Supply
       })
 
       call_google_api do
-        android_publisher.update_apk_listing(
+        android_publisher.update_edit_apklisting(
           current_package_name,
           current_edit.id,
           apk_listing.apk_version_code,
@@ -300,7 +300,7 @@ module Supply
       ensure_active_edit!
 
       result = call_google_api do
-        android_publisher.list_images(
+        android_publisher.list_edit_images(
           current_package_name,
           current_edit.id,
           language,
@@ -316,7 +316,7 @@ module Supply
       ensure_active_edit!
 
       call_google_api do
-        android_publisher.upload_image(
+        android_publisher.upload_edit_image(
           current_package_name,
           current_edit.id,
           language,
@@ -331,7 +331,7 @@ module Supply
       ensure_active_edit!
 
       call_google_api do
-        android_publisher.delete_all_images(
+        android_publisher.deleteall_edit_image(
           current_package_name,
           current_edit.id,
           language,
@@ -344,7 +344,7 @@ module Supply
       ensure_active_edit!
 
       call_google_api do
-        android_publisher.upload_expansion_file(
+        android_publisher.upload_edit_expansionfile(
           current_package_name,
           current_edit.id,
           apk_version_code,
