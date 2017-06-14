d69a493b34948e89f05d31a145f9e7f029566580
Jimmy Dee <jgvdthree@gmail.com>
2017-06-14 13:24:25 -0700

    Revert "Added test for :include parameter"
    This reverts commit bc8ce5f24fd12b53fdf9761670fb9e05134f1f19.

--- fastlane/spec/actions_specs/commit_version_bump_action_spec.rb
+++ fastlane/spec/actions_specs/commit_version_bump_action_spec.rb
@@ -8,36 +8,13 @@ describe Fastlane::Actions::CommitVersionBumpAction do
     end
 
     it 'returns the param if an Array' do
-      settings_plists = action.settings_plists_from_param %w{About.plist Root.plist}
-      expect(settings_plists).to eq %w{About.plist Root.plist}
+      settings_plists = action.settings_plists_from_param ["About.plist", "Root.plist"]
+      expect(settings_plists).to eq ["About.plist", "Root.plist"]
     end
 
     it 'returns ["Root.plist"] for any other input' do
       settings_plists = action.settings_plists_from_param true
       expect(settings_plists).to eq ["Root.plist"]
-    end
-  end
-
-  describe 'include_list_from_param' do
-    it 'returns an empty array for a nil argument' do
-      include_list = action.include_list_from_param nil
-      expect(include_list).to be_an Array
-      expect(include_list).to be_empty
-    end
-
-    it 'splits a comma-separated String into an Array' do
-      include_list = action.include_list_from_param "relative/path1,relative/path2"
-      expect(include_list).to eq %w{relative/path1 relative/path2}
-    end
-
-    it 'returns the param if an Array' do
-      include_list = action.include_list_from_param %w{relative/path1 relative/path2}
-      expect(include_list).to eq %w{relative/path1 relative/path2}
-    end
-
-    it 'returns nil for any other type' do
-      include_list = action.include_list_from_param true
-      expect(include_list).to be_nil
     end
   end
 
