b5b4c80999563c4921455c8b293ab5a01a989aec
Joseph <josephp90@gmail.com>
2017-06-14 19:26:04 +0200

    Add default path for automatic code signing actions (#9465)

--- fastlane/lib/fastlane/actions/automatic_code_signing.rb
+++ fastlane/lib/fastlane/actions/automatic_code_signing.rb
@@ -75,6 +75,7 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :path,
                                        env_name: "FL_PROJECT_SIGNING_PROJECT_PATH",
                                        description: "Path to your Xcode project",
+                                       default_value: Dir['*.xcodeproj'].first,
                                        verify_block: proc do |value|
                                          UI.user_error!("Path is invalid") unless File.exist?(File.expand_path(value))
                                        end),
