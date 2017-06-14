3174813cc56f7bf5585a558402dcf7fa70be9062
Nick Fox <nick@livesafemobile.com>
2017-06-14 17:32:28 -0400

    added path expansion to additional methods in IpaFileAnalyser

--- fastlane_core/lib/fastlane_core/ipa_file_analyser.rb
+++ fastlane_core/lib/fastlane_core/ipa_file_analyser.rb
@@ -5,6 +5,7 @@ module FastlaneCore
   class IpaFileAnalyser
     # Fetches the app identifier (e.g. com.facebook.Facebook) from the given ipa file.
     def self.fetch_app_identifier(path)
+      path = File.expand_path(path)
       plist = self.fetch_info_plist_file(path)
       return plist['CFBundleIdentifier'] if plist
       return nil
@@ -12,6 +13,7 @@ module FastlaneCore
 
     # Fetches the app version from the given ipa file.
     def self.fetch_app_version(path)
+      path = File.expand_path(path)
       plist = self.fetch_info_plist_file(path)
       return plist['CFBundleShortVersionString'] if plist
       return nil
@@ -28,6 +30,7 @@ module FastlaneCore
     end
 
     def self.fetch_info_plist_file(path)
+      path = File.expand_path(path)
       UI.user_error!("Could not find file at path '#{path}'") unless File.exist?(path)
       Zip::File.open(path, "rb") do |zipfile|
         file = zipfile.glob('**/Payload/*.app/Info.plist').first
