fastlane/fastlane

commit 3c597616d1d5b170d2e2b2d54446079b32e1b8de

Add support for Xcode 9 by automatically using the right profile for each bundle identifier (#9432)
* Add support for Xcode 9 by automatically using the right profile for each bundle identifier

If you're using _match_ and _gym_ together with _fastlane_, you can already use Xcode 9 out of the box 🚀

I still want a fallback for this, if the user just uses gym (why would you do that even....), because right now we're failing (see https://github.com/fastlane/fastlane/issues/9380), instead we should try to automatically determine the profile to use based on the Xcode project settings. Using match + fastlane is the recommended way as we can be 100% certain the profiles are correct, but we should still support other ways too

Kind of fixes https://github.com/fastlane/fastlane/issues/9380

* Create gym.rb

Comment update

fastlane/lib/fastlane/actions/gym.rb
@@ -12,11 +12,19 @@ module Fastlane
         should_use_legacy_api = values[:use_legacy_build_api] || Gym::Xcode.pre_7?
 
         if values[:provisioning_profile_path].to_s.length.zero? && should_use_legacy_api
-          sigh_path = Actions.lane_context[Actions::SharedValues::SIGH_PROFILE_PATH] || ENV["SIGH_PROFILE_PATH"]
+          sigh_path = Actions.lane_context[SharedValues::SIGH_PROFILE_PATH] || ENV["SIGH_PROFILE_PATH"]
           values[:provisioning_profile_path] = File.expand_path(sigh_path) if sigh_path
         end
 
-        values[:export_method] ||= Actions.lane_context[Actions::SharedValues::SIGH_PROFILE_TYPE]
+        values[:export_method] ||= Actions.lane_context[SharedValues::SIGH_PROFILE_TYPE]
+
+        if Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING]
+          # Since Xcode 9 you need to explicitly provide the provisioning profile per app target
+          # If the user is smart and uses match and gym together with fastlane, we can do all
+          # the heavy lifting for them
+          values[:export_options] ||= {}
+          values[:export_options][:provisioningProfiles] = Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING]
+        end
 
         absolute_ipa_path = File.expand_path(Gym::Manager.new.work(values))
         absolute_dsym_path = absolute_ipa_path.gsub(".ipa", ".app.dSYM.zip")

fastlane/lib/fastlane/actions/match.rb
@@ -1,5 +1,9 @@
 module Fastlane
   module Actions
+    module SharedValues
+      MATCH_PROVISIONING_PROFILE_MAPPING = :MATCH_PROVISIONING_PROFILE_MAPPING
+    end
+
     class MatchAction < Action
       def self.run(params)
         require 'match'
@@ -8,17 +12,36 @@ module Fastlane
         Match::Runner.new.run(params)
 
         define_profile_type(params)
+        define_provisioning_profile_mapping(params)
       end
 
-      def self.define_profile_type(values)
+      def self.define_profile_type(params)
         profile_type = "app-store"
-        profile_type = "ad-hoc" if values[:type] == 'adhoc'
-        profile_type = "development" if values[:type] == 'development'
-        profile_type = "enterprise" if values[:type] == 'enterprise'
+        profile_type = "ad-hoc" if params[:type] == 'adhoc'
+        profile_type = "development" if params[:type] == 'development'
+        profile_type = "enterprise" if params[:type] == 'enterprise'
 
         UI.message("Setting Provisioning Profile type to '#{profile_type}'")
 
         Actions.lane_context[SharedValues::SIGH_PROFILE_TYPE] = profile_type
+      end
+
+      # Maps the bundle identifier to the appropriate provisioning profile
+      # This is used in the _gym_ action as part of the export options
+      # e.g.
+      #
+      #   export_options: {
+      #     provisioningProfiles: { "me.themoji.app.beta": "match AppStore me.themoji.app.beta" }
+      #   }
+      #
+      def self.define_provisioning_profile_mapping(params)
+        env_variable_name = Match::Utils.environment_variable_name_profile_name(app_identifier: params[:app_identifier],
+                                                                                          type: Match.profile_type_sym(params[:type]),
+                                                                                      platform: params[:platform])
+
+        mapping = Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING] || {}
+        mapping[params[:app_identifier]] = ENV[env_variable_name]
+        Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING] = mapping
       end
 
       #####################################################

commit d303c64525dde360d7c3fd1a570995de0c44a13c

Fix gym action failure if user passed a custom export_options plist file path
Fixes https://github.com/fastlane/fastlane/issues/9490
Introduced by https://github.com/fastlane/fastlane/pull/9432

fastlane/lib/fastlane/actions/gym.rb
@@ -23,7 +23,12 @@ module Fastlane
           # If the user is smart and uses match and gym together with fastlane, we can do all
           # the heavy lifting for them
           values[:export_options] ||= {}
-          values[:export_options][:provisioningProfiles] = Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING]
+          # It's not always a hash, because the user might have passed a string path to a ready plist file
+          # If that's the case, we won't set the provisioning profiles
+          # see https://github.com/fastlane/fastlane/issues/9490
+          if values[:export_options].kind_of?(Hash)
+            values[:export_options][:provisioningProfiles] = Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING]
+          end
         end
 
         absolute_ipa_path = File.expand_path(Gym::Manager.new.work(values))