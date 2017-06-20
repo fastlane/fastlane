be14c5d33c927ab31bfefad7436bdd0bac748416
Sam Dean <sam.dean@net-a-porter.com>
2017-06-20 18:46:00 +0100

    Duplicate the executable reference, so it doesn't get corrupted

--- fastlane/lib/fastlane/actions/swiftlint.rb
+++ fastlane/lib/fastlane/actions/swiftlint.rb
@@ -11,7 +11,7 @@ module Fastlane
           UI.user_error!("Your version of swiftlint (#{version}) does not support autocorrect mode.\nUpdate swiftlint using `brew update && brew upgrade swiftlint`")
         end
 
-        command = params[:executable].nil? ? "swiftlint" : params[:executable]
+        command = params[:executable].nil? ? "swiftlint" : params[:executable].dup
         command << " #{params[:mode]}"
         command << supported_option_switch(params, :strict, "0.9.2", true)
         command << " --config #{params[:config_file].shellescape}" if params[:config_file]
