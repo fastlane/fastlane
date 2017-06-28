3eb02f75868a2e211ae98d84ab79e7ed9c866bc3
Felix Krause <krausefx@gmail.com>
2017-06-28 15:41:57 -0700

    Add verification of Xcode version when running fastlane
    - This PR will skip verification if no Xcode can be found at all
    - This PR will show a nice error message if the user uses an old version of Xcode

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,4 +1,5 @@
 module Fastlane
   VERSION = '2.42.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
+  MINIMUM_XCODE_VERSION = "7.0".freeze
 end
