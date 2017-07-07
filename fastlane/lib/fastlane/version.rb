ef87462997e064c57b3eac6ccd7fbe2ad9a5dc70
Felix Krause <krausefx@gmail.com>
2017-07-07 11:10:54 +0200

    Version bump to 2.46.0

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
-  VERSION = '2.45.0'.freeze
+  VERSION = '2.46.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
   MINIMUM_XCODE_RELEASE = "7.0".freeze
 end
