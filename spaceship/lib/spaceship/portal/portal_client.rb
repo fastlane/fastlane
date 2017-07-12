6f3544980a83af6dc1ea0a14a7f5544537fe36ce
Stefan Natchev <stefan.natchev@gmail.com>
2017-07-12 17:51:10 -0400

    Manage JWT with Spaceship (#9560)
    * initial pass of JWT/Key API
    
    * rubocop fixes
    
    * add tests and refactor
    
    * add spaceship client tests for Portal::Keys requests
    
    * rubocop fixes
    
    * accidentally added a file

--- spaceship/lib/spaceship/portal/portal_client.rb
+++ spaceship/lib/spaceship/portal/portal_client.rb
@@ -560,6 +560,54 @@ module Spaceship
       parse_response(r, 'provisioningProfile')
     end
 
+    #####################################################
+    # @!group Keys
+    #####################################################
+
+    def list_keys
+      paging do |page_number|
+        response = request(:post, 'account/auth/key/list', {
+          teamId: team_id,
+          pageNumber: page_number,
+          pageSize: page_size,
+          sort: 'name=asc'
+        })
+        parse_response(response, 'keys')
+      end
+    end
+
+    def get_key(id: nil)
+      response = request(:post, 'account/auth/key/get', { teamId: team_id, keyId: id })
+      # response contains a list of keys with 1 item
+      parse_response(response, 'keys').first
+    end
+
+    def download_key(id: nil)
+      response = request(:get, 'account/auth/key/download', { teamId: team_id, keyId: id })
+      parse_response(response)
+    end
+
+    def create_key!(name: nil, service_configs: nil)
+      params = {
+        name: name,
+        serviceConfigurations: service_configs,
+        teamId: team_id
+      }
+
+      response = request(:post, 'account/auth/key/create') do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.body = params.to_json
+      end
+
+      # response contains a list of keys with 1 item
+      parse_response(response, 'keys').first
+    end
+
+    def revoke_key!(id: nil)
+      response = request(:post, 'account/auth/key/revoke', { teamId: team_id, keyId: id })
+      parse_response(response)
+    end
+
     private
 
     # This is a cache of entity type (App, AppGroup, Certificate, Device) to csrf_tokens
