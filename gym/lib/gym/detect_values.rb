80583541122c35596c437f5bdd19364177f04b80
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2017-07-14 17:04:35 +0200

    Resolve PRODUCT_BUNDLE_IDENTIFIER and PROVISIONING_PROFILE_SPECIFIER (#9380)
    If a user project uses xcconfig files to set up these build settings, XCBuildConfiguration#resolve_build_setting properly resolves the value in case of variable substitution

--- gym/lib/gym/detect_values.rb
+++ gym/lib/gym/detect_values.rb
@@ -94,10 +94,8 @@ module Gym
           project = Xcodeproj::Project.open(project_path)
           project.targets.each do |target|
             target.build_configuration_list.build_configurations.each do |build_configuration|
-              current = build_configuration.build_settings
-
-              bundle_identifier = current["PRODUCT_BUNDLE_IDENTIFIER"]
-              provisioning_profile_specifier = current["PROVISIONING_PROFILE_SPECIFIER"]
+              bundle_identifier = build_configuration.resolve_build_setting("PRODUCT_BUNDLE_IDENTIFIER")
+              provisioning_profile_specifier = build_configuration.resolve_build_setting("PROVISIONING_PROFILE_SPECIFIER")
               next if provisioning_profile_specifier.to_s.length == 0
 
               provisioning_profile_mapping[bundle_identifier] = provisioning_profile_specifier
