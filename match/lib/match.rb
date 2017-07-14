8d89cd0cf6c4b291bb1cc5038520bd6f89015ac4
Vlad Bokov <razum2um@mail.ru>
2017-07-14 09:27:27 +0700

    Match import certificate

--- match/lib/match.rb
+++ match/lib/match.rb
@@ -34,4 +34,10 @@ module Match
     return :distribution if ["adhoc", "appstore", "distribution"].include?(type)
     raise "Unknown cert type: '#{type}'"
   end
+
+  def self.cert_type_sym_from_cert(cert)
+    return :enterprise if cert.kind_of?(Spaceship::Portal::Certificate::InHouse)
+    return :development if cert.kind_of?(Spaceship::Portal::Certificate::Development)
+    return :distribution
+  end
 end
