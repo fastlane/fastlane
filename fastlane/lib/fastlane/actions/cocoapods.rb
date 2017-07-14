00afb6336cd368891883b3430088fbce043ebbd5
Felix Krause <krausefx@gmail.com>
2017-07-14 13:25:24 +0200

    Add warning if fastlane is executed without `bundle exec`
    This PR also updates the links to our new docs

--- fastlane/lib/fastlane/actions/cocoapods.rb
+++ fastlane/lib/fastlane/actions/cocoapods.rb
@@ -84,7 +84,7 @@ module Fastlane
         ]
         # Please don't add a version parameter to the `cocoapods` action. If you need to specify a version when running
         # `cocoapods`, please start using a Gemfile and lock the version there
-        # More information https://guides.cocoapods.org/using/a-gemfile.html
+        # More information https://docs.fastlane.tools/getting-started/ios/setup/#use-a-gemfile
       end
 
       def self.is_supported?(platform)
