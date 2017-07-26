a4c284e5f3236cf178bf96e6b1b4f032c586829d
Vincent Isambart <vincent.isambart@gmail.com>
2017-07-26 23:58:13 +0900

    Detect selected provisioning profile UUIDs for Xcode 9 (#9843)
    * Detect selected provisioning profile UUIDs for Xcode 9
    
    The Xcode 9 provisioningProfiles export option allows to specify either
    provisioning profile names (specifiers) or UUIDs.
    When automatically generating provisioningProfiles from the project file
    content, fastlane was only using specifiers.
    If no specifier is present but a UUID is, this makes it use the UUID.
    
    * Make emptiness check a bit simpler

--- gym/lib/gym/detect_values.rb
+++ gym/lib/gym/detect_values.rb
@@ -98,9 +98,12 @@ module Gym
 
               bundle_identifier = current["PRODUCT_BUNDLE_IDENTIFIER"]
               provisioning_profile_specifier = current["PROVISIONING_PROFILE_SPECIFIER"]
-              next if provisioning_profile_specifier.to_s.length == 0
-
-              provisioning_profile_mapping[bundle_identifier] = provisioning_profile_specifier
+              provisioning_profile_uuid = current["PROVISIONING_PROFILE"]
+              if provisioning_profile_specifier.to_s.length > 0
+                provisioning_profile_mapping[bundle_identifier] = provisioning_profile_specifier
+              elsif provisioning_profile_uuid.to_s.length > 0
+                provisioning_profile_mapping[bundle_identifier] = provisioning_profile_uuid
+              end
             end
           end
         rescue => ex
