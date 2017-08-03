646168e47ffad58f1ea5384a1253ffbdbab6e01d
Henry Howeson <howeson.h@gmail.com>
2017-08-03 23:54:16 +0100

    Added default store_front value in order to support user not specifying a store_front

--- spaceship/lib/spaceship/tunes/app_ratings.rb
+++ spaceship/lib/spaceship/tunes/app_ratings.rb
@@ -44,7 +44,7 @@ module Spaceship
       end
 
       # @return (Array) of Review Objects
-      def reviews(store_front, versionId = '')
+      def reviews(store_front = '', versionId = '')
         raw_reviews = client.get_reviews(application.apple_id, application.platform, store_front, versionId)
         raw_reviews.map do |review|
           review["value"]["application"] = self.application
