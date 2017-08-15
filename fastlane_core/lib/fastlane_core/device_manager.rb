a0fbe68db06da4ee2bb46533ef1e154ebbffc054
Joshua Liebowitz <taquitos@google.com>
2017-08-15 11:40:47 -0700

    Delete device logs if they exist, then recreate the folder up to but excluding device log folder

--- fastlane_core/lib/fastlane_core/device_manager.rb
+++ fastlane_core/lib/fastlane_core/device_manager.rb
@@ -254,8 +254,8 @@ module FastlaneCore
 
         logarchive_dst = Shellwords.escape(File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive"))
         FileUtils.rm_rf(logarchive_dst)
-        FileUtils.mkdir_p(logarchive_dst)
-        command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst} 2>/dev/null"
+        FileUtils.mkdir_p(File.expand_path("..", logarchive_dst))
+        command = "xcrun -v simctl spawn #{device.udid} log collect --output #{logarchive_dst} 2>/dev/null"
         FastlaneCore::CommandExecutor.execute(command: command, print_all: false, print_command: true)
       end
     end
