ac0a5e43e6c3a046238ead7b9129a99227ecbf89
Josh Holtz <josh@rokkincat.com>
2017-08-21 12:34:46 -0500

    MOSTLY fixes gym auto selects provisioning profile mapping wrong in an Xcode project with multiple configs (#10085)
    * Bump xcodeproj to 1.5.0 to resolve build settings using xcconfigs if they exist in CodeSigningMapping
    
    * Warns user if CodeSigningMapping cannot safely create a mapping due to duplicates
    
    * Fix some code styles and fixed workspace that was causing errors when resolving build setting
    
    * Check against a configuration
    
    * Always rubocop stuff
    
    * Spelling is hard

--- gym/lib/gym/code_signing_mapping.rb
+++ gym/lib/gym/code_signing_mapping.rb
@@ -103,26 +103,50 @@ module Gym
 
     def detect_project_profile_mapping
       provisioning_profile_mapping = {}
+      specified_configuration = Gym.config[:configuration] || Gym.project.default_build_settings(key: "CONFIGURATION")
 
       self.project_paths.each do |project_path|
         UI.verbose("Parsing project file '#{project_path}' to find selected provisioning profiles")
+        UI.verbose("Finding provision profiles for '#{specified_configuration}'") if specified_configuration
 
         begin
+          # Storing bundle identifiers with duplicate profiles
+          # for informing user later on
+          bundle_identifiers_with_duplicates = []
+
           project = Xcodeproj::Project.open(project_path)
           project.targets.each do |target|
             target.build_configuration_list.build_configurations.each do |build_configuration|
               current = build_configuration.build_settings
               next if test_target?(current)
+              next unless specified_configuration == build_configuration.name
 
-              bundle_identifier = current["PRODUCT_BUNDLE_IDENTIFIER"]
-              provisioning_profile_specifier = current["PROVISIONING_PROFILE_SPECIFIER"]
-              provisioning_profile_uuid = current["PROVISIONING_PROFILE"]
-              if provisioning_profile_specifier.to_s.length > 0
+              bundle_identifier = build_configuration.resolve_build_setting("PRODUCT_BUNDLE_IDENTIFIER")
+              provisioning_profile_specifier = build_configuration.resolve_build_setting("PROVISIONING_PROFILE_SPECIFIER")
+              provisioning_profile_uuid = build_configuration.resolve_build_setting("PROVISIONING_PROFILE")
+
+              has_profile_specifier = provisioning_profile_specifier.to_s.length > 0
+              has_profile_uuid = provisioning_profile_uuid.to_s.length > 0
+
+              # Stores bundle identifiers that have already been mapped to inform user
+              if provisioning_profile_mapping[bundle_identifier] && (has_profile_specifier || has_profile_uuid)
+                bundle_identifiers_with_duplicates << bundle_identifier
+              end
+
+              # Creates the mapping for a bundle identifier and profile specifier/uuid
+              if has_profile_specifier
                 provisioning_profile_mapping[bundle_identifier] = provisioning_profile_specifier
-              elsif provisioning_profile_uuid.to_s.length > 0
+              elsif has_profile_uuid
                 provisioning_profile_mapping[bundle_identifier] = provisioning_profile_uuid
               end
             end
+
+            # Alerting user to explicitly specify a mapping if cannot be determined
+            next if bundle_identifiers_with_duplicates.empty?
+            UI.error("Couldn't automatically detect the provisioning profile mapping")
+            UI.error("There were multiple profiles for bundle identifier(s): #{bundle_identifiers_with_duplicates.uniq.join(', ')}")
+            UI.error("You need to provide an explicit mapping of what provisioning")
+            UI.error("profile to use for each bundle identifier of your app")
           end
         rescue => ex
           # We catch errors here, as we might run into an exception on one included project
