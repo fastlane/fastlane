9afee1dd5c896afe2e7465e3c6f1534f16b82150
Joshua Liebowitz <taquitos@users.noreply.github.com>
2017-08-22 11:49:36 -0700

    Use new TestFlight API to find testers by full-text search (#10100)
    * Use new TestFlight API to find testers by full-text search
    
    * Added backwards compat, and added tests for backwards compat
    
    * Add client spec
    
    * MockAPI doesn't like query params
    
    * Force search to look for exact matches when removing a tester

--- spaceship/spec/test_flight/tester_spec.rb
+++ spaceship/spec/test_flight/tester_spec.rb
@@ -35,13 +35,53 @@ describe Spaceship::TestFlight::Tester do
           }
         ]
       end
+
+      mock_client_response(:search_for_tester_in_app, with: { app_id: 'app-id', text: 'email_1@domain.com' }) do
+        [
+          {
+            id: 1,
+            email: "email_1@domain.com"
+          }
+        ]
+      end
+
+      mock_client_response(:search_for_tester_in_app, with: { app_id: 'app-id', text: 'EmAiL_3@domain.com' }) do
+        [
+          {
+            id: 3,
+            email: "EmAiL_3@domain.com"
+          },
+          {
+            id: 4,
+            email: "tacos_email_3@domain.com"
+          }
+        ]
+      end
+
+      mock_client_response(:search_for_tester_in_app, with: { app_id: 'app-id', text: 'taquito' }) do
+        [
+          {
+            id: 1,
+            email: "taquito@domain.com"
+          },
+          {
+            id: 2,
+            email: "taquitos@domain.com"
+          }
+
+        ]
+      end
+
+      mock_client_response(:search_for_tester_in_app, with: { app_id: 'app-id', text: 'NaN@domain.com' }) do
+        []
+      end
     end
 
     context '.all' do
       it 'returns all of the testers' do
         groups = Spaceship::TestFlight::Tester.all(app_id: 'app-id')
         expect(groups.size).to eq(2)
-        expect(groups.first).to be_instance_of(Spaceship::TestFlight::Tester)
+        expect(groups).to all(be_instance_of(Spaceship::TestFlight::Tester))
       end
     end
 
@@ -55,6 +95,34 @@ describe Spaceship::TestFlight::Tester do
       it 'returns nil if no Tester matches' do
         tester = Spaceship::TestFlight::Tester.find(app_id: 'app-id', email: 'NaN@domain.com')
         expect(tester).to be_nil
+      end
+    end
+
+    context '.search' do
+      it 'returns a Tester by email address' do
+        testers = Spaceship::TestFlight::Tester.search(app_id: 'app-id', text: 'email_1@domain.com')
+        expect(testers.length).to be(1)
+        expect(testers.first).to be_instance_of(Spaceship::TestFlight::Tester)
+        expect(testers.first.tester_id).to be(1)
+      end
+
+      it 'returns a Tester by email address if exact match case-insensitive' do
+        testers = Spaceship::TestFlight::Tester.search(app_id: 'app-id', text: 'EmAiL_3@domain.com', is_email_exact_match: true)
+        expect(testers.length).to be(1)
+        expect(testers.first).to be_instance_of(Spaceship::TestFlight::Tester)
+        expect(testers.first.tester_id).to be(3)
+        expect(testers.first.email).to eq("EmAiL_3@domain.com")
+      end
+
+      it 'returns empty array if no Tester matches' do
+        testers = Spaceship::TestFlight::Tester.search(app_id: 'app-id', text: 'NaN@domain.com')
+        expect(testers.length).to be(0)
+      end
+
+      it 'returns two testers if two testers match full-text search' do
+        testers = Spaceship::TestFlight::Tester.search(app_id: 'app-id', text: 'taquito')
+        expect(testers.length).to be(2)
+        expect(testers).to all(be_instance_of(Spaceship::TestFlight::Tester))
       end
     end
   end
