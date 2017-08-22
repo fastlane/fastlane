090c26be20fdba7b40e7360ff37d715944453c51
Nicolas Braun <nicolas.braun@applidium.com>
2017-08-22 17:30:54 +0200

    Fix #10027. Filter match/sigh returned profiles based on the platform ios/tvos (#10029)
    * Add sub_platform option to find_by_bundle_id method
    
    * [Sigh] Pass sub_platform option to call to spaceship find_by_bundle_id method
    
    * Remove useless step
    
    * Exract profiles filtering based on sigh configuration to pass rubocop validation
    
    * Update tests
    
    * Make code more readable

--- spaceship/lib/spaceship/portal/provisioning_profile.rb
+++ spaceship/lib/spaceship/portal/provisioning_profile.rb
@@ -316,9 +316,12 @@ module Spaceship
         #   profiles matching the bundle identifier
         #   Returns [] if no profiles were found
         #   This may also contain invalid or expired profiles
-        def find_by_bundle_id(bundle_id, mac: false)
+        def find_by_bundle_id(bundle_id: nil, mac: false, sub_platform: nil)
+          raise "Missing required parameter 'bundle_id'" if bundle_id.to_s.empty?
+          raise "Invalid sub_platform #{sub_platform}, valid values are tvOS" if !sub_platform.nil? and sub_platform != 'tvOS'
+          find_tvos_profiles = sub_platform == 'tvOS'
           all(mac: mac).find_all do |profile|
-            profile.app.bundle_id == bundle_id
+            profile.app.bundle_id == bundle_id && profile.tvos? == find_tvos_profiles
           end
         end
       end
