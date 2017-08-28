ff1559d9999976701573e7354878ca0780884139
Joshua Liebowitz <taquitos@google.com>
2017-08-28 17:46:06 -0400

    Added basics for calling actions from generated swift

---
+++ fastlane/swift/SocketClientDelegateProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  SocketClientDelegateProtocol.swift
+//  SwiftRubyRPC
+//
+//  Created by Joshua Liebowitz on 8/12/17.
+//  Copyright © 2017 Joshua Liebowitz. All rights reserved.
+//
+
+import Foundation
+
+protocol SocketClientDelegateProtocol: class {
+    func connectionsOpened()
+    func connectionsClosed()
+    func commandExecuted(error: SocketClientError?)
+}
