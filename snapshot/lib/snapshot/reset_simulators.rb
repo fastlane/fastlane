35774361d30445c6d55fa4de74c123b3ec9d89f9
Joseph Petersen <josephp90@gmail.com>
2017-09-14 20:33:58 +0200

    Loop over specified versions

--- snapshot/lib/snapshot/reset_simulators.rb
+++ snapshot/lib/snapshot/reset_simulators.rb
@@ -16,7 +16,9 @@ module Snapshot
       UI.abort_with_message!("User cancelled action") unless sure
 
       if ios_versions
-        FastlaneCore::Simulator.delete_all_by_version(os_version: ios_versions)
+        ios_versions.each do |version|
+          FastlaneCore::Simulator.delete_all_by_version(os_version: version)
+        end
       else
         FastlaneCore::Simulator.delete_all
       end
