1d17633e55012bfb0d9d1a2f27bd2a688455f60c
Joshua Liebowitz <taquitos@google.com>
2017-09-15 15:08:31 -0700

    Default implementations of each <Toolname>file.swift

--- fastlane/swift/DefaultFileImplementations.swift
+++
@@ -1,7 +0,0 @@
-class Deliverfile: DeliverfileProtocol {}
-class Gymfile: GymfileProtocol {}
-class Matchfile: MatchfileProtocol {}
-class Precheckfile: PrecheckfileProtocol {}
-class Scanfile: ScanfileProtocol {}
-class Screengrabfile: ScreengrabfileProtocol {}
-class Snapshotfile: SnapshotfileProtocol {}
