64c2ec932497ce5738178736b76fda555e7ff173
Joshua Liebowitz <taquitos@google.com>
2017-09-25 16:49:52 -0700

    Added special handling for 'sh' action to match the fast_file.rb Updated fast_file.rb so self.sh can be reused Added method run_action_requiring_special_handling for actions that will require special handling due to overrides in fast_file.rb that can't be inferred from the action source code

--- fastlane/lib/fastlane/fast_file.rb
+++ fastlane/lib/fastlane/fast_file.rb
@@ -175,7 +175,10 @@ module Fastlane
 
     # Execute shell command
     def sh(command, log: true, error_callback: nil)
-      command = Shellwords.join(command) if command.kind_of?(Array)
+      FastFile.sh(command, log: log, error_callback: error_callback)
+    end
+
+    def self.sh(command, log: true, error_callback: nil)
       command_header = log ? command : "shell command"
       Actions.execute_action(command_header) do
         Actions.sh_no_action(command, log: log, error_callback: error_callback)
