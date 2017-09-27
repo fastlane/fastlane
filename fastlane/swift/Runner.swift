1b86a5634227c04858c047e9e3363883a3faa10a
Joshua Liebowitz <taquitos@google.com>
2017-09-27 16:48:45 -0700

    Integrated building and testing of generated Swift into release process Updated FastlaneRunner project to include a default Fastfile.swift and Appfile.swift
    Organized FastlaneRunner project so it's easier to understand what is going on
    Updated Swift templates to strip out Ruby mentions
    Improved fatalError() messages for runner since fastlane hides log output
    Added `timeoutSeconds` arg to runner in case 1 hour isn't enough time
    Added messaging to `fastlane init swift` so you know where to edit the Fastfile.swift
    
    Squash me

--- fastlane/swift/Runner.swift
+++ fastlane/swift/Runner.swift
@@ -21,12 +21,14 @@ class Runner {
     fileprivate var socketClient: SocketClient!
     fileprivate let dispatchGroup: DispatchGroup = DispatchGroup()
     fileprivate var returnValue: String? // lol, so safe
+    fileprivate var currentlyExecutingCommand: RubyCommandable? = nil
     fileprivate var shouldLeaveDispatchGroupDuringDisconnect = false
-
+    
     func executeCommand(_ command: RubyCommandable) -> String {
         self.dispatchGroup.enter()
+        currentlyExecutingCommand = command
         socketClient.send(rubyCommand: command)
-
+        
         let secondsToWait = DispatchTimeInterval.seconds(SocketClient.connectTimeoutSeconds)
         let connectTimeout = DispatchTime.now() + secondsToWait
         let timeoutResult = self.dispatchGroup.wait(timeout: connectTimeout)
@@ -36,7 +38,7 @@ class Runner {
             log(message: "command timeout")
             fatalError()
         }
-
+        
         if let returnValue = self.returnValue {
             return returnValue
         } else {
@@ -49,43 +51,43 @@ class Runner {
 extension Runner {
     func startSocketThread() {
         let secondsToWait = DispatchTimeInterval.seconds(SocketClient.connectTimeoutSeconds)
-
+        
         self.dispatchGroup.enter()
-
-        self.socketClient = SocketClient(socketDelegate: self)
+        
+        self.socketClient = SocketClient(commandTimeoutSeconds:timeout, socketDelegate: self)
         self.thread = Thread(target: self, selector: #selector(startSocketComs), object: nil)
         self.thread!.name = "socket thread"
         self.thread!.start()
-
+        
         let connectTimeout = DispatchTime.now() + secondsToWait
         let timeoutResult = self.dispatchGroup.wait(timeout: connectTimeout)
-
-        let failureMessage = "command start socket thread in: \(SocketClient.connectTimeoutSeconds) seconds"
+        
+        let failureMessage = "couldn't start socket thread in: \(SocketClient.connectTimeoutSeconds) seconds"
         let success = testDispatchTimeoutResult(timeoutResult, failureMessage: failureMessage, timeToWait: secondsToWait)
         guard success else {
             log(message: "socket thread timeout")
             fatalError()
         }
     }
-
+    
     func disconnectFromFastlaneProcess() {
         self.shouldLeaveDispatchGroupDuringDisconnect = true
         self.dispatchGroup.enter()
         socketClient.sendComplete()
-
+        
         let connectTimeout = DispatchTime.now() + 2
         _ = self.dispatchGroup.wait(timeout: connectTimeout)
     }
-
+    
     @objc func startSocketComs() {
         guard let socketClient = self.socketClient else {
             return
         }
-
+        
         socketClient.connectAndOpenStreams()
         self.dispatchGroup.leave()
     }
-
+    
     fileprivate func testDispatchTimeoutResult(_ timeoutResult: DispatchTimeoutResult, failureMessage: String, timeToWait: DispatchTimeInterval) -> Bool {
         switch timeoutResult {
         case .success:
@@ -100,26 +102,31 @@ extension Runner {
 extension Runner : SocketClientDelegateProtocol {
     func commandExecuted(serverResponse: SocketClientResponse) {
         switch serverResponse {
-        case .success(let returnedObject):
+        case .success(let returnedObject, let closureArgumentValue):
             verbose(message: "command executed")
             self.returnValue = returnedObject
-
+            if let command = self.currentlyExecutingCommand as? RubyCommand {
+                if let closureArgumentValue = closureArgumentValue {
+                    command.performCallback(callbackArg: closureArgumentValue)
+                }
+            }
+            
         case .alreadyClosedSockets, .connectionFailure, .malformedRequest, .malformedResponse, .serverError:
             log(message: "error encountered while executing command:\n\(serverResponse)")
-
+            
         case .commandTimeout(let timeout):
             log(message: "Runner timed out after \(timeout) second(s)")
         }
-
+        
         self.dispatchGroup.leave()
     }
-
+    
     func connectionsOpened() {
         DispatchQueue.main.async {
             verbose(message: "connected!")
         }
     }
-
+    
     func connectionsClosed() {
         DispatchQueue.main.async {
             self.thread?.cancel()
@@ -149,14 +156,14 @@ class Logger {
         case normal
         case verbose
     }
-
+    
     public static var logMode: LogMode = .normal
-
+    
     func log(message: String) {
         let timestamp = NSDate().timeIntervalSince1970
         print("[\(timestamp)]: \(message)")
     }
-
+    
     func verbose(message: String) {
         if Logger.logMode == .verbose {
             let timestamp = NSDate().timeIntervalSince1970
