f1e2b9663df57e014d325e83b1f11def60c8abf5
Emilio Pavia <emilio.pavia@gmail.com>
2017-09-28 15:09:23 +0200

    Use GEM_HOME in update_fastlane
    Until now the action used Gem.home to check for sudo requirement
    when updating fastlane, but it fails if fastlane is installed with
    the --user-install option.
    
    With this commit the action first check for the GEM_HOME env var
    and fallback to Gem.home if missing.

--- fastlane/lib/fastlane/actions/update_fastlane.rb
+++ fastlane/lib/fastlane/actions/update_fastlane.rb
@@ -24,7 +24,11 @@ module Fastlane
         updater.options[:prerelease] = true if options[:nightly]
         cleaner = Gem::CommandManager.instance[:cleanup]
 
-        sudo_needed = !File.writable?(Gem.dir)
+        gem_dir = ENV['GEM_HOME']
+        unless gem_dir
+          gem_dir = Gem.dir
+        end
+        sudo_needed = !File.writable?(gem_dir)
 
         if sudo_needed
           UI.important("It seems that your Gem directory is not writable by your current User.")
