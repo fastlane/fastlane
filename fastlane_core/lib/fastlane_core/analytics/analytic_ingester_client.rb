aa79df04eb65b987b15991aee2b5d6079f00806d
David Ohayon <ohayon.1@gmail.com>
2017-10-03 16:44:33 -0400

    client api

--- fastlane_core/lib/fastlane_core/analytics/analytic_ingester_client.rb
+++ fastlane_core/lib/fastlane_core/analytics/analytic_ingester_client.rb
@@ -1,4 +1,9 @@
 module FastlaneCore
   class AnalyticIngesterClient
+    def launched_event(session: nil, launch_context: nil)
+    end
+
+    def completed_event(session: nil, launch_context: nil)
+    end
   end
 end
