3f32ac7ab8c1b0411b67992e32b2f24793eff83c
Takashi Hasegawa <thase51@gmail.com>
2017-10-04 00:25:53 +0900

    add unittest

--- fastlane/lib/fastlane/actions/verify_two_step_session.rb
+++ fastlane/lib/fastlane/actions/verify_two_step_session.rb
@@ -7,7 +7,8 @@ module Fastlane
         begin
           Spaceship::Tunes.login(user)
         rescue Spaceship::Client::InvalidUserCredentialsError => e
-          UI.user_error!(e.preferred_error_info)
+          # Invalid username and password combination
+          UI.user_error!(e.message)
         rescue
           UI.user_error!('Your session cookie has been expired.')
         end
@@ -37,25 +38,24 @@ module Fastlane
       end
 
       def self.check_expiration_time(cookie)
-        return unless cookie
         cookie.each do |content|
           next unless content.domain == 'idmsa.apple.com' && content.max_age.to_s.length > 0
           next unless content.created_at.to_s =~ /([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/
-          year = Regexp.last_match(1)
-          month = Regexp.last_match(2)
-          day = Regexp.last_match(3)
-          hour = Regexp.last_match(4)
-          min = Regexp.last_match(5)
-          sec = Regexp.last_match(6)
+          year = Regexp.last_match(1).to_i
+          month = Regexp.last_match(2).to_i
+          day = Regexp.last_match(3).to_i
+          hour = Regexp.last_match(4).to_i
+          min = Regexp.last_match(5).to_i
+          sec = Regexp.last_match(6).to_i
 
-          created_date = DateTime.new(year.to_i, month.to_i, day.to_i, hour.to_i, min.to_i, sec.to_i)
-          expire_date = created_date + Rational(content.max_age, 24 * 60 * 60)
-          remain = (expire_date - DateTime.now).to_i
+          created_date = DateTime.new(year, month, day, hour, min, sec)
+          expiration_date = created_date + Rational(content.max_age, 24 * 60 * 60)
+          remaining_days = (expiration_date - DateTime.now).to_i
 
-          if remain > 0
-            UI.important("Your session cookie will expire at #{expire_date.strftime('%Y-%m-%d %H:%M:%S')} (#{remain} days left).")
+          if remaining_days > 0
+            UI.important("Your session cookie will expire at #{expiration_date.strftime('%Y-%m-%d %H:%M:%S')} (#{remaining_days} days left).")
           else
-            UI.error("Your session cookie is due to expire today!") if remain == 0
+            UI.error("Your session cookie is due to expire today!")
           end
           break
         end
@@ -70,7 +70,10 @@ module Fastlane
       end
 
       def self.details
-        "Verifies the session cookie for 'Two-Step verification for Apple ID'"
+        [
+          "This action will validate the session cookie for 'Two-Step verification for Apple ID'"
+          "and display the remaining days by expiration date."
+        ].join(' ')
       end
 
       def self.available_options
--- fastlane/spec/actions_specs/verify_two_step_session_spec.rb
+++ fastlane/spec/actions_specs/verify_two_step_session_spec.rb
@@ -1,53 +1,74 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe 'VerifyTwoStepSessionAction' do
-      before do
-        allow(Spaceship::Tunes).to receive(:login).and_return(true)
-        allow(FastlaneCore::UI).to receive(:success)
-        date = DateTime.new(2017, 10, 3, 1, 23, 45)
-        allow(DateTime).to receive(:now).and_return(date)
-      end
-
-      context 'when loading cookie from environment variables' do
-        it 'displays the expiration date and the remaining day correctly.' do
-          cookie = YAML.safe_load(
-            File.read('fastlane/spec/fixtures/actions/cookie_env').gsub("\\n", "\n"),
-            [HTTP::Cookie, Time],
-            [],
-            true
-          )
-          expect(FastlaneCore::UI).to receive(:important).with(/expire at 2017-10-21 09:35:31 \(18 days left\)./)
-          Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
+      describe '#run' do
+        context 'when InvalidUserCredentialsError has occurred' do
+          before do
+            allow(Spaceship::Tunes).to receive(:login).and_raise(Spaceship::Client::InvalidUserCredentialsError, "test message")
+          end
+          it 'raises InvalidUserCredentialsError' do
+            expect { Fastlane::Actions::VerifyTwoStepSessionAction.run({ user: 'foo@example.com' }) }.to raise_error(FastlaneCore::Interface::FastlaneError, "test message")
+          end
+        end
+        context 'when InvalidUserCredentialsError has occurred' do
+          before do
+            allow(Spaceship::Tunes).to receive(:login).and_raise(Spaceship::Client::NoUserCredentialsError)
+          end
+          it 'raises InvalidUserCredentialsError' do
+            expect { Fastlane::Actions::VerifyTwoStepSessionAction.run({ user: 'foo@example.com' }) }.to raise_error(FastlaneCore::Interface::FastlaneError, "Your session cookie has been expired.")
+          end
         end
       end
 
-      context 'when loading cookie from file' do
-        it 'displays the expiration date and the remaining day correctly.' do
-          cookie = YAML.safe_load(
-            File.read('fastlane/spec/fixtures/actions/cookie'),
-            [HTTP::Cookie, Time],
-            [],
-            true
-          )
-          expect(FastlaneCore::UI).to receive(:important).with(/expire at 2017-11-02 13:14:27 \(30 days left\)./)
-          Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
-        end
-      end
-
-      context 'when the expiration date is today' do
+      describe '#check_expiration_time' do
         before do
-          date = DateTime.new(2017, 11, 2, 1, 23, 45)
+          allow(Spaceship::Tunes).to receive(:login).and_return(true)
+          allow(FastlaneCore::UI).to receive(:success)
+          date = DateTime.new(2017, 10, 3, 1, 23, 45)
           allow(DateTime).to receive(:now).and_return(date)
         end
-        it 'displays the expiration date and the remaining day correctly.' do
-          cookie = YAML.safe_load(
-            File.read('fastlane/spec/fixtures/actions/cookie'),
-            [HTTP::Cookie, Time],
-            [],
-            true
-          )
-          expect(FastlaneCore::UI).to receive(:error).with("Your session cookie is due to expire today!")
-          Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
+
+        context 'when loading cookie from environment variables' do
+          it 'displays the expiration date and the remaining day correctly.' do
+            cookie = YAML.safe_load(
+              File.read('fastlane/spec/fixtures/actions/cookie_env').gsub("\\n", "\n"),
+              [HTTP::Cookie, Time],
+              [],
+              true
+            )
+            expect(FastlaneCore::UI).to receive(:important).with(/expire at 2017-10-21 09:35:31 \(18 days left\)./)
+            Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
+          end
+        end
+
+        context 'when loading cookie from file' do
+          it 'displays the expiration date and the remaining day correctly.' do
+            cookie = YAML.safe_load(
+              File.read('fastlane/spec/fixtures/actions/cookie'),
+              [HTTP::Cookie, Time],
+              [],
+              true
+            )
+            expect(FastlaneCore::UI).to receive(:important).with(/expire at 2017-11-02 13:14:27 \(30 days left\)./)
+            Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
+          end
+        end
+
+        context 'when the expiration date is today' do
+          before do
+            date = DateTime.new(2017, 11, 2, 1, 23, 45)
+            allow(DateTime).to receive(:now).and_return(date)
+          end
+          it 'displays the expiration date and the remaining day correctly.' do
+            cookie = YAML.safe_load(
+              File.read('fastlane/spec/fixtures/actions/cookie'),
+              [HTTP::Cookie, Time],
+              [],
+              true
+            )
+            expect(FastlaneCore::UI).to receive(:error).with("Your session cookie is due to expire today!")
+            Fastlane::Actions::VerifyTwoStepSessionAction.check_expiration_time(cookie)
+          end
         end
       end
     end
