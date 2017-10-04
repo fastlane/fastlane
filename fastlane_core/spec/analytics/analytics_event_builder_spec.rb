29cbf922e1d94f518f246f6f19dd9a558d70ac29
David Ohayon <ohayon.1@gmail.com>
2017-10-04 16:05:33 -0400

    spec for creating a launched event with two targets

--- fastlane_core/spec/analytics/analytics_event_builder_spec.rb
+++ fastlane_core/spec/analytics/analytics_event_builder_spec.rb
@@ -43,5 +43,51 @@ describe FastlaneCore::AnalyticsEventBuilder do
         }
       )
     end
+
+    it 'creates a new launched event with primary and secondary targets' do
+      builder = FastlaneCore::AnalyticsEventBuilder.new(
+        oauth_app_name: oauth_app_name,
+        p_hash: p_hash,
+        session_id: session_id,
+        action_name: action_name,
+        timestamp_millis: timestamp_millis
+      )
+      event = builder.launched_event(
+        primary_target_hash: {
+          name: 'primary target name',
+          detail: 'primary target detail'
+        },
+        secondary_target_hash: {
+          name: 'secondary target name',
+          detail: 'secondary target detail'
+        }
+      )
+      expect(event).to eq(
+        {
+          event_source: {
+            oauth_app_name: oauth_app_name,
+            product: 'fastlane'
+          },
+          actor: {
+            name: p_hash,
+            detail: session_id
+          },
+          action: {
+            name: 'launched',
+            detail: action_name
+          },
+          primary_target: {
+            name: 'primary target name',
+            detail: 'primary target detail'
+          },
+          secondary_target: {
+            name: 'secondary target name',
+            detail: 'secondary target detail'
+          },
+          timestamp_millis: timestamp_millis,
+          version: 1
+        }
+      )
+    end
   end
 end
