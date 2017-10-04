b759f3669308e3fff8e3807b7b7c3c94e7848796
David Ohayon <ohayon.1@gmail.com>
2017-10-04 14:02:15 -0400

    remove unnecessary methods from client

--- fastlane_core/lib/fastlane_core/analytics/analytics_ingester_client.rb
+++ fastlane_core/lib/fastlane_core/analytics/analytics_ingester_client.rb
@@ -1,23 +1,5 @@
 module FastlaneCore
   class AnalyticsIngesterClient
-    def launched_event(session: nil, action_launched_context: nil)
-      ingester_events = action_launched_events(
-        action_launched_context: action_launched_context,
-        session: session
-      )
-
-      post_events(events: ingester_events)
-    end
-
-    def completed_event(session: nil, action_completed_context: nil)
-      ingester_events = action_completed_events(
-        action_completed_context: action_completed_context,
-        session: session
-      )
-
-      post_events(events: ingester_events)
-    end
-
     def post_events(events)
       unless Helper.test?
         fork do
