9446807891d582f8838ce90bc0a7287e42f1f1b3
David Ohayon <ohayon.1@gmail.com>
2017-10-04 11:11:27 -0400

    add code to post to ana-ing and retry if necessary

--- fastlane_core/lib/fastlane_core/analytics/analytics_ingester_client.rb
+++ fastlane_core/lib/fastlane_core/analytics/analytics_ingester_client.rb
@@ -19,7 +19,25 @@ module FastlaneCore
     end
 
     def post_events(events: nil)
-      # post the events to the endpoint
+      unless Helper.test?
+        fork do
+          send_request(events: events)
+        end
+      end
+      return true
+    end
+
+    def send_request(events: nil, retries: 2)
+      require 'excon'
+      url = ENV["FASTLANE_METRICS_URL"] || "https://fastlane-metrics.fabric.io/public"
+      Excon.post(
+        url,
+        body: events,
+        headers: { "Content-Type" => 'application/json' }
+      )
+    rescue
+      retries -= 1
+      retry if retries >= 0
     end
 
     def action_completed_events(action_completed_context: nil, session: nil)
