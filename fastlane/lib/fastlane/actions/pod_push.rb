1bf67acfba8e4ffe6b1d5a43f5f56c9c29e028eb
Marcus Wu <mwu@digitaltorque.com>
2017-10-12 11:14:59 -0400

    Add swift_version option to pod_push action (#10570)
    * add swift_version option to pod_push action
    
    * pod_push action test for swift_version parameter

--- fastlane/lib/fastlane/actions/pod_push.rb
+++ fastlane/lib/fastlane/actions/pod_push.rb
@@ -18,6 +18,11 @@ module Fastlane
           command << " --sources='#{sources}'"
         end
 
+        if params[:swift_version]
+          swift_version = params[:swift_version]
+          command << " --swift-version=#{swift_version}"
+        end
+
         if params[:allow_warnings]
           command << " --allow-warnings"
         end
@@ -74,6 +79,10 @@ module Fastlane
                                        verify_block: proc do |value|
                                          UI.user_error!("Sources must be an array.") unless value.kind_of?(Array)
                                        end),
+          FastlaneCore::ConfigItem.new(key: :swift_version,
+                                       description: "The SWIFT_VERSION that should be used to lint the spec. This takes precedence over a .swift-version file",
+                                       optional: true,
+                                       is_string: false),
           FastlaneCore::ConfigItem.new(key: :verbose,
                                        description: "Show more debugging information",
                                        optional: true,
