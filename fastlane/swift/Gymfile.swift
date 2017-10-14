7148b30ed1fbb169a6da7d5f9d4b0d50d05e6c63
Joshua Liebowitz <taquitos@google.com>
2017-10-13 19:23:16 -0700

    Updated API generation to add @discardableResult to all methods that return data

--- fastlane/swift/Gymfile.swift
+++ fastlane/swift/Gymfile.swift
@@ -2,6 +2,6 @@
 // If you have a custom Gymfile.swift, this file will be replaced by it
 // Don't modify this file unless you are familiar with how fastlane's swift code generation works
 // *** This file will be overwritten or replaced during build time ***
-// Generated at 16:58:08 on 09-28-2017 with fastlane 2.59.0
+// Generated at 19:20:15 on 10-13-2017 with fastlane 2.61.0
 
 class Gymfile: GymfileProtocol {}
