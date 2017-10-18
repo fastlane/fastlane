e54412b8603962520e8e4d0b88cd3f448000d917
David Cordero <dcorderoramirez@gmail.com>
2017-10-18 16:31:55 +0200

    Fix swift syntax

--- snapshot/lib/assets/SnapshotHelperXcode8.swift
+++ snapshot/lib/assets/SnapshotHelperXcode8.swift
@@ -86,7 +86,7 @@ open class Snapshot: NSObject {
         do {
             let launchArguments = try String(contentsOf: path, encoding: String.Encoding.utf8)
             let regex = try NSRegularExpression(pattern: "(\\\".+?\\\"|\\S+)", options: [])
-            let matches = regex.matches(in: launchArguments, options: [], range: NSRange(location:0, length:launchArguments.characters.count))
+            let matches = regex.matches(in: launchArguments, options: [], range: NSRange(location: 0, length: launchArguments.characters.count))
             let results = matches.map { result -> String in
                 (launchArguments as NSString).substring(with: result.range)
             }
