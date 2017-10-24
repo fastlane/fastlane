fastlane/fastlane

commit 3e2b43acdcd36df9bf3234db0d2b3b5a69b8fd93

Support manual signingStyle (#10508)
* support -allowProvisioningUpdate and manual signing

* fix mistake in logic expressions

* fix typo

* remove allowProvisioningUpdates flag

* move check to other place

* correct indentation

gym/lib/gym/generators/package_command_generator_xcode7.rb
@@ -166,6 +166,13 @@ module Gym
           hash[:uploadSymbols] = (Gym.config[:include_symbols] ? true : false) unless Gym.config[:include_symbols].nil?
           hash[:uploadBitcode] = (Gym.config[:include_bitcode] ? true : false) unless Gym.config[:include_bitcode].nil?
         end
+
+        # xcodebuild will not use provisioning profiles
+        # if we doens't specify signingStyle as manual
+        if Helper.xcode_at_least?("9.0") && hash[:provisioningProfiles]
+          hash[:signingStyle] = 'manual'
+        end
+
         hash[:teamID] = Gym.config[:export_team_id] if Gym.config[:export_team_id]
 
         UI.important("Generated plist file with the following values:")

commit 218cf45aab1befe234afbd5a3e9d3df5b2de02f2

Fix typo in comment
Introduced by #10508.

gym/lib/gym/generators/package_command_generator_xcode7.rb
@@ -168,7 +168,7 @@ module Gym
         end
 
         # xcodebuild will not use provisioning profiles
-        # if we doens't specify signingStyle as manual
+        # if we don't specify signingStyle as manual
         if Helper.xcode_at_least?("9.0") && hash[:provisioningProfiles]
           hash[:signingStyle] = 'manual'
         end