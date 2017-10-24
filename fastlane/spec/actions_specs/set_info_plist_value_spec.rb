25386833e88e858e685172f455dcd29528e45c53
Joseph Petersen <josephp90@gmail.com>
2017-10-24 19:55:08 +0200

    [set_info_plist_value] update test

--- fastlane/spec/actions_specs/set_info_plist_value_spec.rb
+++ fastlane/spec/actions_specs/set_info_plist_value_spec.rb
@@ -1,10 +1,16 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "set_info_plist" do
+      require 'plist'
+
       let (:plist_path) { "./fastlane/spec/fixtures/plist/Info.plist" }
+      let (:expected_plist_path) { './fastlane/spec/fixtures/plist/Info-expected.plist' }
       let (:test_path) { "/tmp/fastlane/tests/fastlane" }
       let (:output_path) { "Folder/output.plist" }
       let (:new_value) { "NewValue#{Time.now.to_i}" }
+      let (:output_file) { File.join(test_path, output_path) }
+      let (:old_plist) { Plist.parse_xml(plist_path) }
+      let (:expected_plist) { Plist.parse_xml(expected_plist_path) }
 
       it "stores changes in the plist file" do
         allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
@@ -35,7 +41,7 @@ describe Fastlane do
         end").runner.execute(:test)
 
         Fastlane::FastFile.new.parse("lane :test do
-          set_info_plist_value(path: '#{plist_path}', key: 'CFBundleIdentifier', value: '#{new_value}', output_file_name:'#{File.join(test_path, output_path)}')
+          set_info_plist_value(path: '#{plist_path}', key: 'CFBundleIdentifier', value: '#{new_value}', output_file_name:'#{output_file}')
         end").runner.execute(:test)
 
         value = Fastlane::FastFile.new.parse("lane :test do
@@ -45,12 +51,44 @@ describe Fastlane do
         expect(value).to eq(old_value)
 
         value = Fastlane::FastFile.new.parse("lane :test do
-          get_info_plist_value(path: '#{File.join(test_path, output_path)}', key: 'CFBundleIdentifier')
+          get_info_plist_value(path: '#{output_file}', key: 'CFBundleIdentifier')
         end").runner.execute(:test)
 
         expect(value).to eq(new_value)
 
-        File.delete(File.join(test_path, output_path))
+        File.delete(output_file)
+      end
+
+      it "stores changes in the plist file when using map" do
+        allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
+        ret = Fastlane::FastFile.new.parse("lane :test do
+          set_info_plist_value(
+            path: '#{plist_path}',
+            map: {
+              CFBundleIdentifier: 'com.krausefx.app123',
+              CFBundleShortVersionString: '9.9.9',
+              UIRequiredDeviceCapabilities: ['arm64'],
+              CFBundleURLTypes: [{
+                'CFBundleURLName'=>'com.krausefx.app123',
+              }]
+            },
+            output_file_name: '#{output_file}'
+          )
+        end").runner.execute(:test)
+
+        expect(ret).to eq(expected_plist)
+
+        ret = Fastlane::FastFile.new.parse("lane :test do
+          set_info_plist_value(
+            path: '#{plist_path}',
+            map: #{old_plist},
+            replace: true,
+            output_file_name: '#{output_file}'
+          )
+        end").runner.execute(:test)
+
+        expect(ret).to eq(old_plist)
+        File.delete(output_file)
       end
     end
   end
