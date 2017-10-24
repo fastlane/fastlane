06aefd798cd3a6dc23255d5df6cb49ab6c60f6b1
Joseph Petersen <josephp90@gmail.com>
2017-10-24 22:14:14 +0200

    [set_info_plist_value] remove fixture and expect some objects

--- fastlane/spec/actions_specs/set_info_plist_value_spec.rb
+++ fastlane/spec/actions_specs/set_info_plist_value_spec.rb
@@ -10,7 +10,22 @@ describe Fastlane do
       let (:new_value) { "NewValue#{Time.now.to_i}" }
       let (:output_file) { File.join(test_path, output_path) }
       let (:old_plist) { Plist.parse_xml(plist_path) }
-      let (:expected_plist) { Plist.parse_xml(expected_plist_path) }
+      let (:expected_name) { 'App Name' }
+      let (:expected_modes) { ['remote-notification'] }
+      let (:identifier) { 'com.krausefx.app123' }
+      let (:version) { '9.9.9' }
+      let (:capabilities) { ['arm64'] }
+      let (:expected_capabilities) { ['armv7', 'arm64'] }
+      let (:types) { 
+        [{
+          CFBundleURLName: 'com.krausefx.app123',
+        }]
+      }
+      let (:expected_types) {
+        [{"CFBundleTypeRole"=>"Viewer",
+        "CFBundleURLName"=>"com.krausefx.app123",
+        "CFBundleURLSchemes"=>["com.krausefx.app"]}]
+      }
 
       it "stores changes in the plist file" do
         allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
@@ -65,18 +80,43 @@ describe Fastlane do
           set_info_plist_value(
             path: '#{plist_path}',
             map: {
-              CFBundleIdentifier: 'com.krausefx.app123',
-              CFBundleShortVersionString: '9.9.9',
-              UIRequiredDeviceCapabilities: ['arm64'],
-              CFBundleURLTypes: [{
-                'CFBundleURLName'=>'com.krausefx.app123',
-              }]
+              CFBundleIdentifier: '#{identifier}',
+              CFBundleShortVersionString: '#{version}',
+              UIRequiredDeviceCapabilities: #{capabilities},
+              CFBundleURLTypes: #{types}
             },
             output_file_name: '#{output_file}'
           )
         end").runner.execute(:test)
 
-        expect(ret).to eq(expected_plist)
+        new_name = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'CFBundleName')
+        end").runner.execute(:test)
+        new_modes = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'UIBackgroundModes')
+        end").runner.execute(:test)
+        new_identifier = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'CFBundleIdentifier')
+        end").runner.execute(:test)
+        new_version = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'CFBundleShortVersionString')
+        end").runner.execute(:test)
+        new_capabilities = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'UIRequiredDeviceCapabilities')
+        end").runner.execute(:test)
+        new_types = Fastlane::FastFile.new.parse("lane :test do
+          get_info_plist_value(path: '#{output_file}', key: 'CFBundleURLTypes')
+        end").runner.execute(:test)
+
+        # check unchanged values
+        expect(new_name).to eq(expected_name)
+        expect(new_modes).to eq(expected_modes)
+        
+        # check changed values
+        expect(new_identifier).to eq(identifier)
+        expect(new_version).to eq(version)
+        expect(new_capabilities).to eq(expected_capabilities)
+        expect(new_types).to eq(expected_types)
 
         ret = Fastlane::FastFile.new.parse("lane :test do
           set_info_plist_value(
