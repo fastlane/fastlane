35e50a87f0c605161a3b0f3376ec0e9ad881c7f4
Joshua Liebowitz <taquitos@google.com>
2017-11-01 19:44:59 +0100

    Add auto_release_date option to deliver

--- deliver/lib/deliver/options.rb
+++ deliver/lib/deliver/options.rb
@@ -111,6 +111,15 @@ module Deliver
                                      description: "Should the app be automatically released once it's approved?",
                                      is_string: false,
                                      default_value: false),
+        FastlaneCore::ConfigItem.new(key: :auto_release_date,
+                                     env_name: "DELIVER_AUTO_RELEASE_DATE",
+                                     description: "Time to release when release on approval is used in milliseconds GMT",
+                                     is_string: false,
+                                     optional: true,
+                                     conflicting_options: [:automatic_release],
+                                     conflict_block: proc do |value|
+                                       UI.user_error!("You can't use 'auto_release_date' and '#{value.key}' options together.")
+                                     end),
         FastlaneCore::ConfigItem.new(key: :phased_release,
                                      description: "Enable the phased release feature of iTC",
                                      optional: true,
