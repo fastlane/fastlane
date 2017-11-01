6ff0c06aa304897d8963ccb873b4752e8a17a2dd
Rex Feng <rexfeng@gmail.com>
2017-11-01 08:07:57 -0700

    increment SnapshotHelperVersion

--- snapshot/lib/assets/SnapshotHelper.swift
+++ snapshot/lib/assets/SnapshotHelper.swift
@@ -242,4 +242,4 @@ private extension CGFloat {
 
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
-// SnapshotHelperVersion [1.6]
+// SnapshotHelperVersion [1.7]
