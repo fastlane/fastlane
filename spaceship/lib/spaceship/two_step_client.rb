6098b9e6811095784ea78ff6eb5277fdadf746f6
Joshua Liebowitz <taquitos@google.com>
2017-11-08 08:21:18 -0800

    Further 2FA refinements

--- spaceship/lib/spaceship/two_step_client.rb
+++ spaceship/lib/spaceship/two_step_client.rb
@@ -20,7 +20,9 @@ module Spaceship
           selected_index += selected_index
         end
 
-        select_device(response, selected_index)
+        scnt_regex = %r{<input type="hidden" id="scnt" name="scnt" value="(.*?)" .*?\/>}
+        scnt = response.body.scan(scnt_regex)[0][0]
+        select_device(scnt, selected_index)
       else
         raise "Invalid 2 step response #{response.body}"
       end
@@ -61,53 +63,47 @@ module Spaceship
       ENV["FASTLANE_SESSION"] || ENV["SPACESHIP_SESSION"]
     end
 
-    def select_device(r, device_id)
+    def select_device(scnt, device_id)
       # Request Token
-      # Not quite there yet
-      scnt_regex = %r{<input type="hidden" id="scnt" name="scnt" value="(.*?)" .*?\/>}
-      scnt = r.body.scan(scnt_regex)[0][0]
-      r = request(:post) do |req|
+      response = request(:post) do |req|
         req.url "https://idmsa.apple.com/IDMSWebAuth/generateSecurityCode"
         req.body = "deviceIndex=#{device_id}&scnt=#{scnt}"
         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
         req.headers['Accept'] = 'application/json, text/javascript'
       end
 
-      raise "Too many codes sent, enter the last code you received, use a different device, or try again later" if r.body.include?("Too many codes sent")
-
-      # we use `Spaceship::TunesClient.new.handle_itc_response`
-      # since this might be from the Dev Portal, but for 2 step
-      Spaceship::TunesClient.new.handle_itc_response(r.body)
+      # if the hidden rate limit message isn't there, that means it is being shown, so we have an error
+      hidden_rate_limit_regex = /<div\s*id="tooManyCodesSentError"\s*style="display:\snone">/
+      hidden_rate_limit_error_matches = response.body.scan(hidden_rate_limit_regex)[0]
+      raise "Too many codes sent, enter the last code you received, use a different device, or try again later" if hidden_rate_limit_error_matches.nil?
 
       puts "Successfully requested notification"
       code = ask("Please enter the 4 digit code: ")
       puts "Requesting session..."
 
       # Send code back to server to get a valid session
-      r = request(:post) do |req|
+      response = request(:post) do |req|
         req.url "https://idmsa.apple.com/IDMSWebAuth/validateSecurityCode"
         req.body = "digit1=#{code[0]}&digit2=#{code[1]}&digit3=#{code[2]}&digit4=#{code[3]}&scnt=#{scnt}"
         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
         req.headers['Accept'] = 'application/json, text/javascript'
       end
 
-      begin
-        Spaceship::TunesClient.new.handle_itc_response(r.body) # this will fail if the code is invalid
-      rescue => ex
-        if ex.to_s.include?("verification code") # to have a nicer output
-          puts "Error: Incorrect verification code"
-          return select_device(r, device_id)
-        end
-
-        raise ex
+      case response.status
+      when 200
+        # When 2 factor fails, it's a 200 with error instead of a 302 redirect
+        puts "Error: Incorrect verification code"
+        return select_device(scnt, device_id)
+      when 302
+        store_session
+        return true
+      else
+        raise "Unable to valid your verification code: #{response.body}"
       end
-
-      store_session
-      return true
     end
 
     def store_session
-      # If the request was successful, r.body is actually nil
+      # If the request was successful, response.body is actually nil
       # The previous request will fail if the user isn't on a team
       # on iTunes Connect, but it still works, so we're good
 
