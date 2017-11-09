960a44614cb1f08db49341a6e5a01ea1c643b869
Mark Pirri <mpirri@google.com>
2017-11-09 17:10:42 -0500

    Simplify appstore and testflight aliases via the magic of subclassing.

--- fastlane/lib/fastlane/actions/appstore.rb
+++ fastlane/lib/fastlane/actions/appstore.rb
@@ -1,42 +1,12 @@
 module Fastlane
   module Actions
-    class AppstoreAction < Action
-      def self.run(params)
-        Actions::DeliverAction.run(params)
-      end
-
+    class AppstoreAction < DeliverAction
       #####################################################
       # @!group Documentation
       #####################################################
 
       def self.description
         "Alias for the deliver action"
-      end
-
-      def self.available_options
-        require "deliver"
-        require "deliver/options"
-        FastlaneCore::CommanderGenerator.new.generate(Deliver::Options.available_options)
-      end
-
-      def self.output
-        []
-      end
-
-      def self.author
-        'KrauseFx'
-      end
-
-      def self.is_supported?(platform)
-        Actions::DeliverAction.is_supported?(platform)
-      end
-
-      def self.category
-        Actions::DeliverAction.category
-      end
-
-      def self.example_code
-        Actions::DeliverAction.example_code
       end
     end
   end
--- fastlane/lib/fastlane/actions/testflight.rb
+++ fastlane/lib/fastlane/actions/testflight.rb
@@ -1,9 +1,6 @@
 module Fastlane
   module Actions
-    class TestflightAction < Action
-      def self.run(params)
-        Actions::PilotAction.run(params)
-      end
+    class TestflightAction < PilotAction
 
       #####################################################
       # @!group Documentation
@@ -11,32 +8,6 @@ module Fastlane
 
       def self.description
         "Alias for the pilot action"
-      end
-
-      def self.available_options
-        require "pilot"
-        require "pilot/options"
-        FastlaneCore::CommanderGenerator.new.generate(Pilot::Options.available_options)
-      end
-
-      def self.output
-        []
-      end
-
-      def self.author
-        'KrauseFx'
-      end
-
-      def self.example_code
-        Actions::PilotAction.example_code
-      end
-
-      def self.category
-        Actions::PilotAction.category
-      end
-
-      def self.is_supported?(platform)
-        Actions::PilotAction.is_supported?(platform)
       end
     end
   end
