edd455ae2472e66dbd8074d7f6c726884b189803
Felix Krause <krausefx@gmail.com>
2017-11-13 12:57:36 -0800

    Add run_as_build_phase? method

--- precheck/lib/precheck/xcode_env.rb
+++ precheck/lib/precheck/xcode_env.rb
@@ -3,6 +3,10 @@ module Precheck
   # Xcode project settings
   class XcodeEnv
     class << self
+      def run_as_build_phase?
+        return true if ENV["PROJECT_FILE_PATH"].to_s.length > 0 && ENV["INFOPLIST_PATH"].to_s.length > 0
+      end
+
       def project_path
         ENV["PROJECT_FILE_PATH"]
       end
