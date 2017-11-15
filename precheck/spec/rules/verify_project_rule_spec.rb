8053727edc870af013aea956e8e65faa7dfcb7e7
David Airapetyan <davidair@google.com>
2017-11-15 14:17:07 -0500

    Adding a passing test

--- precheck/spec/rules/verify_project_rule_spec.rb
+++ precheck/spec/rules/verify_project_rule_spec.rb
@@ -6,6 +6,7 @@ module Precheck
       let(:rule) { VerifyProjectRule.new }
       let(:minimal_xcode_project) {XcodeProjectItemToCheck.new("./spec/fixtures/MinimalApp/MinimalApp.xcodeproj", "MinimalApp", "Release", :xcode_project, "XcodeProject")}
       let(:mismatched_xcode_project) {XcodeProjectItemToCheck.new("./spec/fixtures/MismatchedApp/MismatchedApp.xcodeproj", "MismatchedApp", "Release", :xcode_project, "XcodeProject")}
+      let(:good_xcode_project) {XcodeProjectItemToCheck.new("./spec/fixtures/GoodApp/GoodApp.xcodeproj", "GoodApp", "Release", :xcode_project, "XcodeProject")}
 
       it "fails for missing GoogleServices-Info.plist" do
         result = rule.check_item(minimal_xcode_project)
@@ -17,6 +18,11 @@ module Precheck
         result = rule.check_item(mismatched_xcode_project)
         expect(result.status).to eq(VALIDATION_STATES[:failed])
         expect(result.failure_data).to eq("The project bundle id com.google.firebase.test.MismatchedApp does not match the GoogleService-Info.plist bundle id com.google.firebase.test.MinimalApp.")
+      end
+
+      it "passes for properly configured app" do
+        result = rule.check_item(good_xcode_project)
+        expect(result.status).to eq(VALIDATION_STATES[:passed])
       end
 
     end
