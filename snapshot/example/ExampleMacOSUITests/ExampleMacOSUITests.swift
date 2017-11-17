34cb15c57d0a9b1fab7ca384d183383e302d9a90
Aaron Brager <getaaron@gmail.com>
2017-11-17 14:28:18 -0600

    Fix swiftlint complaints in example snapshot tests (#10940)

--- snapshot/example/ExampleMacOSUITests/ExampleMacOSUITests.swift
+++ snapshot/example/ExampleMacOSUITests/ExampleMacOSUITests.swift
@@ -9,17 +9,17 @@
 import XCTest
 
 class ExampleMacOSUITests: XCTestCase {
-        
+
     override func setUp() {
         super.setUp()
-        
+
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
     }
-    
+
     func testExample() {
         snapshot("0Launch")
     }
-    
+
 }
