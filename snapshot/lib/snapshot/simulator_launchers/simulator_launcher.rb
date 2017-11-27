bda024ed73a06f55ce9259d0aff98232010ce541
Manu Wallner <manu@supermil.ch>
2017-11-27 22:01:52 +0100

    Separate concurrent simulators by OS version (#10994)
    * Separate concurrent simulators by OS version
    
    * Fix small issue with how patch-level simulators are parsed
    
    * I fixed them! I fixed them all! They're fixed, every single one of them. And not just the testmen, but the testwomen and the testchildren, too!

--- snapshot/lib/snapshot/simulator_launchers/simulator_launcher.rb
+++ snapshot/lib/snapshot/simulator_launchers/simulator_launcher.rb
@@ -31,7 +31,10 @@ module Snapshot
           # Break up the array of devices into chunks that can
           # be run simultaneously.
           if launcher_config.concurrent_simulators?
-            device_batches = launcher_config.devices.each_slice(default_number_of_simultaneous_simulators).to_a
+            all_devices = launcher_config.devices
+            # We have to break up the concurrent simulators by device version too, otherwise there is an error (see #10969)
+            by_simulator_version = all_devices.group_by { |d| FastlaneCore::DeviceManager.latest_simulator_version_for_device(d) }.values
+            device_batches = by_simulator_version.flat_map { |a| a.each_slice(default_number_of_simultaneous_simulators).to_a }
           else
             # Put each device in it's own array to run tests one at a time
             device_batches = launcher_config.devices.map { |d| [d] }
