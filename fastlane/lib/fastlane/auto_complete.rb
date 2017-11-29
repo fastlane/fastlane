8db0592b58cef2127ead7357cc25f616bf520aec
Iulian Onofrei <6d0847b9@opayq.com>
2017-11-29 02:37:16 +0200

    Extended tab auto complete to work with bundler and custom commands (#11004)
    * Enable auto complete when using bundler
    
    This enables typing "bundle exec fastlane lane_nam" and pressing
    tab to auto complete with "lane_name".
    
    The trade-off is that it also works when typing
    "bundle exec lane_nam" or even "bundle lane_nam", but I think it's
    an acceptable trade-off, as the chances of something thinking it
    can work this way seems low.
    
    * Allow enable_auto_complete command to be called non-interactively
    
    * Add option to enable_auto_complete to specify custom commands
    
    Tab auto complete will work for the specified custom commands as
    it does for "fastlane" and "bundle exec fastlane".
    
    You use it by passing them to the "custom" option like:
    bundle exec fastlane enable_auto_complete --custom fl,bundle_fastlane
    
    * Remove superfluous auto complete lines
    
    This enables auto complete for "fastlane" if running
    "enable_auto_complete" directly with fastlane and for "bundle exec
    fastlane" if running via bundler

--- fastlane/lib/fastlane/auto_complete.rb
+++ fastlane/lib/fastlane/auto_complete.rb
@@ -3,13 +3,19 @@ require 'fileutils'
 module Fastlane
   # Enable tab auto completion
   class AutoComplete
-    def self.execute
+    # This method copies the tab auto completion scripts to the user's home folder,
+    # while optionally adding custom commands for which to enable auto complete
+    # @param [Array] options An array of all options (e.g. --custom fl)
+    def self.execute(args, options)
       shell = ENV['SHELL']
 
       if shell.end_with? "fish"
         fish_completions_dir = "~/.config/fish/completions"
-        confirm = UI.confirm "This will copy a fish script into #{fish_completions_dir} that provides the command tab completion. If the directory does not exist it will be created. Sound good?"
-        return unless confirm
+
+        if UI.interactive?
+          confirm = UI.confirm "This will copy a fish script into #{fish_completions_dir} that provides the command tab completion. If the directory does not exist it will be created. Sound good?"
+          return unless confirm
+        end
 
         fish_completions_dir = File.expand_path fish_completions_dir
         FileUtils.mkdir_p fish_completions_dir
@@ -22,8 +28,11 @@ module Fastlane
         UI.success "Copied! You can now use tab completion for lanes"
       else
         fastlane_conf_dir = "~/.fastlane"
-        confirm = UI.confirm "This will copy a shell script into #{fastlane_conf_dir} that provides the command tab completion. Sound good?"
-        return unless confirm
+
+        if UI.interactive?
+          confirm = UI.confirm "This will copy a shell script into #{fastlane_conf_dir} that provides the command tab completion. Sound good?"
+          return unless confirm
+        end
 
         # create the ~/.fastlane directory
         fastlane_conf_dir = File.expand_path fastlane_conf_dir
@@ -33,10 +42,41 @@ module Fastlane
         completion_script_path = File.join(Fastlane::ROOT, 'lib', 'assets', 'completions')
         FileUtils.cp_r completion_script_path, fastlane_conf_dir
 
+        custom_commands = options.custom.to_s.split(',')
+
+        Fastlane::SHELLS.each do |shell_name|
+          open("#{fastlane_conf_dir}/completions/completion.#{shell_name}", 'a') do |file|
+            default_line_prefix = Helper.bundler? ? "bundle exec " : ""
+
+            file.puts self.get_auto_complete_line(shell_name, "#{default_line_prefix}fastlane")
+
+            custom_commands.each do |command|
+              auto_complete_line = self.get_auto_complete_line(shell_name, command)
+
+              next if auto_complete_line.nil?
+
+              file.puts auto_complete_line
+            end
+          end
+        end
+
         UI.success "Copied! To use auto complete for fastlane, add the following line to your favorite rc file (e.g. ~/.bashrc)"
         UI.important "  . ~/.fastlane/completions/completion.sh"
         UI.success "Don't forget to source that file in your current shell! 🐚"
       end
+    end
+
+    # Helper to get the auto complete register script line
+    def self.get_auto_complete_line(shell, command)
+      if shell == :bash
+        prefix = "complete -F"
+      elsif shell == :zsh
+        prefix = "compctl -K"
+      else
+        return nil
+      end
+
+      return "#{prefix} _fastlane_complete #{command}"
     end
   end
 end
