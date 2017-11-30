eb5908a58ad4f7e0a26f757946855afd89e6dec0
Jimmy Dee <jgvdthree@gmail.com>
2017-11-29 17:41:24 -0800

    Allow shell conversion when type is :shell_string (#10946)
    * Allow shell conversion when type is :shell_string. Allow arrays to be passed to FastFile.sh
    
    * Added shell escaping to Actions.sh as well.
    
    * Refactored sh method to use Open3 for better testability. Added tests.
    
    * Use Shellwords.join
    
    * Simplify tests by opening an empty file. Removed #close call

--- fastlane/spec/helper/sh_helper_spec.rb
+++ fastlane/spec/helper/sh_helper_spec.rb
@@ -1,5 +1,11 @@
 describe Fastlane::Actions do
   describe "#sh" do
+    let (:mock_input) { double :input }
+    let (:mock_status) { double :status }
+    let (:mock_thread) { double :thread, value: mock_status }
+    # Just open an empty file to mock command output
+    let (:mock_output) { File.open(File.expand_path(File.join("..", "..", "fixtures", "appfiles", "Appfile_empty"), __FILE__)) }
+
     before do
       allow(FastlaneCore::Helper).to receive(:sh_enabled?).and_return(true)
     end
@@ -9,6 +15,7 @@ describe Fastlane::Actions do
         it "doesn't raise shell_error" do
           allow(FastlaneCore::UI).to receive(:error)
           called = false
+          expect_command "exit 1", 1
           Fastlane::Actions.sh("exit 1", error_callback: ->(_) { called = true })
 
           expect(called).to be true
@@ -19,11 +26,37 @@ describe Fastlane::Actions do
       context "without error_callback" do
         it "raise shell_error" do
           allow(FastlaneCore::UI).to receive(:shell_error!)
+          expect_command "exit 1", 1
           Fastlane::Actions.sh("exit 1")
 
           expect(UI).to have_received(:shell_error!).with("Exit status of command 'exit 1' was 1 instead of 0.\n")
         end
       end
     end
+
+    context "handling of array arguments" do
+      it "joins arrays into a single string" do
+        expect_command "git commit"
+        Fastlane::Actions.sh(%w(git commit))
+      end
+
+      it "shell escapes array elements" do
+        expect_command 'git commit -m a\ message'
+        Fastlane::Actions.sh(["git", "commit", "-m", "a message"])
+      end
+
+      it "converts array elements to strings" do
+        pathname = Pathname.new "."
+        expect_command 'git commit . -m a\ message'
+        Fastlane::Actions.sh(["git", "commit", pathname, "-m", "a message"])
+      end
+    end
   end
+end
+
+def expect_command(command, exitstatus = 0)
+  require "open3"
+
+  allow(mock_status).to receive(:exitstatus) { exitstatus }
+  expect(Open3).to receive(:popen2e).with(command).and_yield mock_input, mock_output, mock_thread
 end
