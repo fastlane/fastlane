e979c0f6627695b2ba045f3d80ef70eb86426551
Mark Pirri <mnpirri@gmail.com>
2017-11-30 12:02:56 -0500

    Change gym reference to be surrounded by underscores (#11077)

--- fastlane/lib/fastlane/actions/copy_artifacts.rb
+++ fastlane/lib/fastlane/actions/copy_artifacts.rb
@@ -97,7 +97,7 @@ module Fastlane
           reset_git_repo(
             exclude: "artifacts"
           )',
-          '# Copy the .ipa created by `gym` if it was successfully created
+          '# Copy the .ipa created by _gym_ if it was successfully created
           artifacts = []
           artifacts << lane_context[SharedValues::IPA_OUTPUT_PATH] if lane_context[SharedValues::IPA_OUTPUT_PATH]
           copy_artifacts(
