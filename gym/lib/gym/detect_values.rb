941de9f45ee70d45017a6a9af886a4ba7de086bf
Jimmy Dee <jgvdthree@gmail.com>
2017-12-09 12:13:48 -0800

    May work better with initial require

--- gym/lib/gym/detect_values.rb
+++ gym/lib/gym/detect_values.rb
@@ -1,3 +1,5 @@
+require "gym/cfpropertylist"
+
 module Gym
   # This class detects all kinds of default values
   class DetectValues
@@ -55,7 +57,6 @@ module Gym
     end
 
     def self.xcode_preferences_dictionary(path = xcode_preference_plist_path)
-      require "gym/cfpropertylist"
       CFPropertyList.native_types(CFPropertyList::List.new(file: path).value)
     end
 
