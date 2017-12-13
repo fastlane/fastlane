625146142999dcc1e603ffb704dd11dc94b1f798
Mark Pirri <mnpirri@gmail.com>
2017-12-13 12:28:20 -0500

    Revert "Change sync_code_signing example to use an array type" (#11211)
    This reverts commit 6f810ba351592585d2746e6774926c77a4916dca.

--- fastlane/lib/fastlane/actions/sync_code_signing.rb
+++ fastlane/lib/fastlane/actions/sync_code_signing.rb
@@ -84,7 +84,7 @@ module Fastlane
 
       def self.example_code
         [
-          'sync_code_signing(type: "appstore", app_identifier: ["tools.fastlane.app"])',
+          'sync_code_signing(type: "appstore", app_identifier: "tools.fastlane.app")',
           'sync_code_signing(type: "development", readonly: true)',
           'sync_code_signing(app_identifier: ["tools.fastlane.app", "tools.fastlane.sleepy"])',
           'match   # alias for "sync_code_signing"'
