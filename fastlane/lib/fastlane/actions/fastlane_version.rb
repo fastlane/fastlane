a8d5ce7118c935d46df2c8cc581a0c1411192a5f
Jimmy Dee <jgvdthree@gmail.com>
2017-12-15 08:22:39 -0800

    Added min_fastlane_version action and fastlane_version alias. (#11209)
    * Added min_fastlane_version. Made fastlane_version an alias.
    
    * Finish off min_fastlane_version. This includes changes to the default Fastfile templates for iOS, Android and Swift.
    
    * Revert change to default Swift Fastfile template. This change does not break Swift. The new action name and default Fastfile can be addressed for Swift in a separate PR.

--- fastlane/lib/fastlane/actions/fastlane_version.rb
+++ fastlane/lib/fastlane/actions/fastlane_version.rb
@@ -1,54 +1,14 @@
+require "fastlane/actions/min_fastlane_version"
+
 module Fastlane
   module Actions
-    module SharedValues
-    end
-
-    class FastlaneVersionAction < Action
-      def self.run(params)
-        params = nil unless params.kind_of?(Array)
-        value = (params || []).first
-        defined_version = Gem::Version.new(value) if value
-
-        UI.user_error!("Please pass minimum fastlane version as parameter to fastlane_version") unless defined_version
-
-        if Gem::Version.new(Fastlane::VERSION) < defined_version
-          FastlaneCore::UpdateChecker.show_update_message('fastlane', Fastlane::VERSION)
-          error_message = "The Fastfile requires a fastlane version of >= #{defined_version}. You are on #{Fastlane::VERSION}."
-          UI.user_error!(error_message)
-        end
-
-        UI.message("Your fastlane version #{Fastlane::VERSION} matches the minimum requirement of #{defined_version}  ✅")
-      end
-
-      def self.step_text
-        "Verifying required fastlane version"
-      end
-
-      def self.author
-        "KrauseFx"
-      end
+    class FastlaneVersionAction < MinFastlaneVersionAction
+      #####################################################
+      # @!group Documentation
+      #####################################################
 
       def self.description
-        "Verifies the minimum fastlane version required"
-      end
-
-      def self.example_code
-        ['fastlane_version "1.50.0"']
-      end
-
-      def self.details
-        [
-          "Add this to your `Fastfile` to require a certain version of _fastlane_.",
-          "Use it if you use an action that just recently came out and you need it"
-        ].join("\n")
-      end
-
-      def self.category
-        :misc
-      end
-
-      def self.is_supported?(platform)
-        true
+        "Alias for the `min_fastlane_version` action"
       end
     end
   end
