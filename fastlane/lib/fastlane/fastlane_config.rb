f13a4bca63f77fcab2a519a3de0507d611d5b56a
Jimmy Dee <jgvdthree@gmail.com>
2017-12-14 20:17:34 -0800

    Allow support for more actions

--- fastlane/lib/fastlane/fastlane_config.rb
+++ fastlane/lib/fastlane/fastlane_config.rb
@@ -1,11 +1,28 @@
 module Fastlane
   class FastlaneConfig
-    def min_fastlane_version=(version)
-      Actions::MinFastlaneVersionAction.run([version])
-    end
+    #
+    # Limited to actions that take a single argument, not params
+    #
+    SUPPORTED_ACTIONS = %w(
+      min_fastlane_version
+      default_platform
+      team_id
+    )
 
-    def default_platform=(platform)
-      Actions::DefaultPlatformAction.run([platform])
+    def method_missing(method_sym, *args, &block)
+      method_str = method_sym.to_s
+      return super unless method_str =~ /\=$/
+
+      root = method_str.chomp("=")
+      return super unless SUPPORTED_ACTIONS.include?(root)
+
+      # It was a setter. Should have a single argument.
+      raise ArgumentError, "#{method_sym} requires one argument." unless args.count == 1
+
+      action_class = Actions.const_get("#{root.split("_").map(&:capitalize).join}Action")
+      return super unless action_class
+
+      action_class.run([args.first])
     end
   end
 end
