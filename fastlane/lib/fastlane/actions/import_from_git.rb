1c6826e61c7138c18d127c524c93156a71f86eca
Jimmy Dee <jgvdthree@gmail.com>
2017-12-18 17:56:36 -0800

    Fix version selection with import_from_git

--- fastlane/lib/fastlane/actions/import_from_git.rb
+++ fastlane/lib/fastlane/actions/import_from_git.rb
@@ -54,7 +54,13 @@ module Fastlane
             url: "git@github.com:fastlane/fastlane.git", # The URL of the repository to import the Fastfile from.
             branch: "HEAD", # The branch to checkout on the repository. Defaults to `HEAD`.
             path: "fastlane/Fastfile" # The path of the Fastfile in the repository. Defaults to `fastlane/Fastfile`.
-            version: "~> 1.0.0" #The version to checkout on the respository. Optimistic operator can be used to select the latest version within constraints.
+            version: "~> 1.0.0" # The version to checkout on the repository. Optimistic operator can be used to select the latest version within constraints.
+          )',
+          'import_from_git(
+            url: "git@github.com:fastlane/fastlane.git", # The URL of the repository to import the Fastfile from.
+            branch: "HEAD", # The branch to checkout on the repository. Defaults to `HEAD`.
+            path: "fastlane/Fastfile" # The path of the Fastfile in the repository. Defaults to `fastlane/Fastfile`.
+            version: [">= 1.1.0", "< 2.0.0"] # The version to checkout on the repository. Multiple conditions can be used to select the latest version within constraints.
           )'
         ]
       end
