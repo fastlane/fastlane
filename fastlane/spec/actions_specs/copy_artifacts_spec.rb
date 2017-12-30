18651d717540e309d1143f8605d9059965efc21f
Jan Piotrowski <piotrowski@gmail.com>
2017-12-30 18:06:29 +0100

    [Waiting for PR to get merged] hard coded test paths (2)

--- fastlane/spec/actions_specs/copy_artifacts_spec.rb
+++ fastlane/spec/actions_specs/copy_artifacts_spec.rb
@@ -2,51 +2,47 @@ describe Fastlane do
   describe Fastlane::FastFile do
     describe "Copy Artifacts Integration" do
       before do
-        # Reset directories
-        FileUtils.rm_rf('/tmp/fastlane')
-        FileUtils.rm_rf('/tmp/fastlane2')
-
         # Create base test directory
-        FileUtils.mkdir_p('/tmp/fastlane')
+        @tmp_path = Dir.mktmpdir
+        FileUtils.mkdir_p("#{@tmp_path}/source")
       end
 
       it "Copies a file to target path" do
-        FileUtils.touch('/tmp/fastlane/copy_artifacts')
+        FileUtils.touch("#{@tmp_path}/source/copy_artifacts")
 
         Fastlane::FastFile.new.parse("lane :test do
-          copy_artifacts(artifacts: '/tmp/fastlane/copy_artifacts', target_path: '/tmp/fastlane2')
+          copy_artifacts(artifacts: '#{@tmp_path}/source/copy_artifacts', target_path: '#{@tmp_path}/target')
         end").runner.execute(:test)
 
-        expect(File.exist?('/tmp/fastlane2/copy_artifacts')).to eq(true)
+        expect(File.exist?("#{@tmp_path}/target/copy_artifacts")).to eq(true)
       end
 
       it "Copies a directory and subfiles to target path" do
-        FileUtils.mkdir_p('/tmp/fastlane/dir')
-        FileUtils.touch('/tmp/fastlane/dir/copy_artifacts')
+        FileUtils.mkdir_p("#{@tmp_path}/source/dir")
+        FileUtils.touch("#{@tmp_path}/source/dir/copy_artifacts")
 
         Fastlane::FastFile.new.parse("lane :test do
-          copy_artifacts(artifacts: '/tmp/fastlane/*', target_path: '/tmp/fastlane2')
+          copy_artifacts(artifacts: '#{@tmp_path}/source/*', target_path: '#{@tmp_path}/target/')
         end").runner.execute(:test)
-        expect(Dir.exist?('/tmp/fastlane2/dir')).to eq(true)
-        expect(File.exist?('/tmp/fastlane2/dir/copy_artifacts')).to eq(true)
+
+        expect(Dir.exist?("#{@tmp_path}/target/dir")).to eq(true)
+        expect(File.exist?("#{@tmp_path}/target/dir/copy_artifacts")).to eq(true)
       end
 
       it "Copies a file with a space" do
-        FileUtils.touch("/tmp/fastlane/copy_artifacts with a space")
+        FileUtils.touch("#{@tmp_path}/source/copy_artifacts\ with\ a\ space")
 
         Fastlane::FastFile.new.parse("lane :test do
-          copy_artifacts(artifacts: '/tmp/fastlane/copy_artifacts with a space', target_path: '/tmp/fastlane2')
+          copy_artifacts(artifacts: '#{@tmp_path}/source/copy_artifacts\ with\ a\ space', target_path: '#{@tmp_path}/target')
         end").runner.execute(:test)
-        expect(File.exist?('/tmp/fastlane/copy_artifacts with a space')).to eq(true)
+
+        expect(File.exist?("#{@tmp_path}/target/copy_artifacts\ with\ a\ space")).to eq(true)
       end
 
       it "Copies a file with verbose" do
-        tmp_path1 = Dir.mktmpdir
-        source_path = "#{tmp_path1}/source"
+        source_path = "#{@tmp_path}/source"
         FileUtils.touch(source_path)
-
-        tmp_path2 = Dir.mktmpdir
-        target_path = "#{tmp_path2}/target"
+        target_path = "#{@tmp_path}/target738"
 
         expect(UI).to receive(:verbose).once.ordered.with("Copying artifacts #{source_path} to #{target_path}")
         expect(UI).to receive(:verbose).once.ordered.with('Keeping original files')
@@ -62,12 +58,9 @@ describe Fastlane do
       end
 
       it "Copies a file without keeping original and verbose" do
-        tmp_path1 = Dir.mktmpdir
-        source_path = "#{tmp_path1}/source"
+        source_path = "#{@tmp_path}/source"
         FileUtils.touch(source_path)
-
-        tmp_path2 = Dir.mktmpdir
-        target_path = "#{tmp_path2}/target"
+        target_path = "#{@tmp_path}/target987"
 
         expect(UI).to receive(:verbose).once.ordered.with("Copying artifacts #{source_path} to #{target_path}")
         expect(UI).to receive(:verbose).once.ordered.with('Not keeping original files')
@@ -83,21 +76,27 @@ describe Fastlane do
       end
 
       it "Copies a file with file missing does not fail (no flag set)" do
+        source_path = "#{@tmp_path}/source/missing"
+        target_path = "#{@tmp_path}/target123"
+
         Fastlane::FastFile.new.parse("lane :test do
-          copy_artifacts(artifacts: '/tmp/fastlane/not_going_to_be_there', target_path: '/tmp/fastlane2')
+          copy_artifacts(artifacts: '#{source_path}', target_path: '#{target_path}')
         end").runner.execute(:test)
 
-        expect(File.exist?('/tmp/fastlane2/not_going_to_be_there')).to eq(false)
+        expect(File.exist?(source_path)).to eq(false)
       end
 
       it "Copies a file with file missing fails (flag set)" do
+        source_path = "#{@tmp_path}/source/not_going_to_be_there"
+        target_path = "#{@tmp_path}/target456"
+
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            copy_artifacts(artifacts: '/tmp/fastlane/not_going_to_be_there', target_path: '/tmp/fastlane2', fail_on_missing: true)
+            copy_artifacts(artifacts: '#{source_path}', target_path: '#{target_path}', fail_on_missing: true)
           end").runner.execute(:test)
-        end.to raise_error "Not all files were present in copy artifacts. Missing /tmp/fastlane/not_going_to_be_there"
+        end.to raise_error "Not all files were present in copy artifacts. Missing #{source_path}"
 
-        expect(File.exist?('/tmp/fastlane2/not_going_to_be_there')).to eq(false)
+        expect(File.exist?(source_path)).to eq(false)
       end
     end
   end
