0b07878c173dab6439d27c52aa8e44d5a0c686cf
Jan Piotrowski <piotrowski@gmail.com>
2018-01-04 19:41:49 +0100

    [Waiting for PR to get merged] hard coded test paths (4)

--- fastlane/spec/actions_specs/restore_file_spec.rb
+++ fastlane/spec/actions_specs/restore_file_spec.rb
@@ -2,10 +2,10 @@ describe Fastlane do
   describe Fastlane::FastFile do
     describe "Backup file Integration" do
       tmp_path = Dir.mktmpdir
-      let(:test_path) { "#{tmp_path}/tests/fastlane" }
-      let(:file_path) { "file.txt" }
-      let(:backup_path) { "#{file_path}.back" }
-      let(:file_content) { Time.now.to_s }
+      let (:test_path) { "#{tmp_path}/tests/fastlane" }
+      let (:file_path) { "file.txt" }
+      let (:backup_path) { "#{file_path}.back" }
+      let (:file_content) { Time.now.to_s }
 
       before do
         FileUtils.mkdir_p(test_path)
@@ -26,8 +26,8 @@ describe Fastlane do
           end").runner.execute(:test)
           restored_file = File.read(File.join(test_path, file_path))
 
-          expect(restored_file).to include(file_content)
-          expect(File).not_to(exist(File.join(test_path, backup_path)))
+          expect(restored_file).to include file_content
+          expect(File).not_to exist(File.join(test_path, backup_path))
         end
       end
 
@@ -42,7 +42,7 @@ describe Fastlane do
       end
 
       after do
-        File.delete(File.join(test_path, backup_path)) if File.exist?(File.join(test_path, backup_path))
+        File.delete(File.join(test_path, backup_path)) if File.exist? File.join(test_path, backup_path)
         File.delete(File.join(test_path, file_path))
       end
     end
