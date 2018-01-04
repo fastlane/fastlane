78d2e030943fd4c89a730b06e0e9cc4b10d68bbc
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-01-04 15:15:39 -0800

    Add another check for DISABLE_ANIMATION

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -275,6 +275,7 @@ module FastlaneCore
 
     # Show/Hide loading indicator
     def self.show_loading_indicator(text = "🚀")
+      return if FastlaneCore::Env.truthy?("FASTLANE_DISABLE_ANIMATION")
       @require_fastlane_spinner = TTY::Spinner.new("[:spinner] #{text} ", format: :dots)
       @require_fastlane_spinner.auto_spin
     end
