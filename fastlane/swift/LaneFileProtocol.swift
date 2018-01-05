4a4d69baa9e6cb25254cc35684c85a2eaa631917
Joshua Liebowitz <taquitos@google.com>
2018-01-04 16:19:11 -0800

    Cleaner

--- fastlane/swift/LaneFileProtocol.swift
+++ fastlane/swift/LaneFileProtocol.swift
@@ -11,7 +11,7 @@ import Foundation
 public protocol LaneFileProtocol: class {
     var fastlaneVersion: String { get }
     static func runLane(named: String, parameters: [String : String]) -> Bool
-    
+
     func recordLaneDescriptions()
     func beforeAll()
     func afterAll(currentLane: String)
@@ -29,35 +29,54 @@ public extension LaneFileProtocol {
 @objcMembers
 public class LaneFile: NSObject, LaneFileProtocol {
     private(set) static var fastfileInstance: Fastfile?
-    
+
     // Called before any lane is executed.
     private func setupAllTheThings() {
         LaneFile.fastfileInstance!.beforeAll()
     }
-    
-    public static var lanes: [String : String] {
-        var laneToMethodName: [String : String] = [:]
+
+    private static func trimLaneFromName(laneName: String) -> String {
+        return String(laneName.prefix(laneName.count - 4))
+    }
+
+    private static func trimLaneWithOptionsFromName(laneName: String) -> String {
+        return String(laneName.prefix(laneName.count - 12))
+    }
+
+    private static var laneFunctionNames: [String] {
+        var lanes: [String] = []
         var methodCount: UInt32 = 0
         let methodList = class_copyMethodList(self, &methodCount)
         for i in 0..<Int(methodCount) {
             let selName = sel_getName(method_getName(methodList![i]))
             let name = String(cString: selName)
             let lowercasedName = name.lowercased()
+            if lowercasedName.hasSuffix("lane") || lowercasedName.hasSuffix("lanewithoptions:") {
+                lanes.append(name)
+            }
+        }
+        return lanes
+    }
+
+    public static var lanes: [String : String] {
+        var laneToMethodName: [String : String] = [:]
+        self.laneFunctionNames.forEach { name in
+            let lowercasedName = name.lowercased()
             if lowercasedName.hasSuffix("lane") {
                 laneToMethodName[lowercasedName] = name
-                let lowercasedNameNoLane = String(lowercasedName.prefix(lowercasedName.count - 4))
+                let lowercasedNameNoLane = trimLaneFromName(laneName: lowercasedName)
                 laneToMethodName[lowercasedNameNoLane] = name
             } else if lowercasedName.hasSuffix("lanewithoptions:") {
-                let lowercasedNameNoOptions = String(lowercasedName.prefix(lowercasedName.count - 12))
+                let lowercasedNameNoOptions = trimLaneWithOptionsFromName(laneName: lowercasedName)
                 laneToMethodName[lowercasedNameNoOptions] = name
-                let lowercasedNameNoLane = String(lowercasedNameNoOptions.prefix(lowercasedNameNoOptions.count - 4))
+                let lowercasedNameNoLane = trimLaneFromName(laneName: lowercasedNameNoOptions)
                 laneToMethodName[lowercasedNameNoLane] = name
             }
         }
 
         return laneToMethodName
     }
-    
+
     public static func loadFastfile() {
         if self.fastfileInstance == nil {
             let fastfileType: AnyObject.Type = NSClassFromString(self.className())!
@@ -66,11 +85,11 @@ public class LaneFile: NSObject, LaneFileProtocol {
             self.fastfileInstance = currentFastfileInstance
         }
     }
-    
+
     public static func runLane(named: String, parameters: [String : String]) -> Bool {
         log(message: "Running lane: \(named)")
         self.loadFastfile()
-        
+
         guard let fastfileInstance: Fastfile = self.fastfileInstance else {
             let message = "Unable to instantiate class named: \(self.className())"
             log(message: message)
@@ -79,10 +98,16 @@ public class LaneFile: NSObject, LaneFileProtocol {
 
         let currentLanes = self.lanes
         let lowerCasedLaneRequested = named.lowercased()
-        
+
         guard let laneMethod = currentLanes[lowerCasedLaneRequested] else {
-            let currentLaneNames = laneFunctions.map { String($0.prefix($0.count - 4)) }
-            let laneNames = currentLaneNames.joined(separator: ", ")
+            let laneNames = self.laneFunctionNames.map { laneFuctionName in
+                if laneFuctionName.hasSuffix("lanewithoptions:") {
+                    return trimLaneWithOptionsFromName(laneName: laneFuctionName)
+                } else {
+                    return trimLaneFromName(laneName: laneFuctionName)
+                }
+            }.joined(separator: ", ")
+
             let message = "[!] Could not find lane '\(named)'. Available lanes: \(laneNames)"
             log(message: message)
 
@@ -93,10 +118,10 @@ public class LaneFile: NSObject, LaneFileProtocol {
 
         // call all methods that need to be called before we start calling lanes
         fastfileInstance.setupAllTheThings()
-        
+
         // We need to catch all possible errors here and display a nice message
         _ = fastfileInstance.perform(NSSelectorFromString(laneMethod), with: parameters)
-        
+
         // only call on success
         fastfileInstance.afterAll(currentLane: named)
         log(message: "Done running lane: \(named) 🚀")
