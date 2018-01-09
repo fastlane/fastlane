07065d5754da193a8061f618f88e69a658d833e5
Manu Wallner <manu@supermil.ch>
2018-01-09 21:58:47 +0100

    Refactor to separate file

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -6,18 +6,7 @@ require 'pathname'
 require_relative 'fastlane_folder'
 require_relative 'ui/ui'
 require_relative 'env'
-
-# Allows nice relative directory require_relative
-# like `require_relative from_fastlane_core/'helper'`
-base_fastlane_dir = File.expand_path("../../../", File.dirname(__FILE__))
-tool_dirs = Dir["#{base_fastlane_dir}/**/lib/*.rb"]
-tool_dirs.map { |t| File.basename(t, ".rb") }.each do |tool|
-  define_method("from_#{tool}".to_sym) do
-    source = Pathname.new(File.dirname(caller.first.split(":").first))
-    destination = Pathname.new(File.join(base_fastlane_dir, tool, "lib", tool))
-    destination.relative_path_from(source)
-  end
-end
+require_relative 'require_relative_helper'
 
 module FastlaneCore
   module Helper
