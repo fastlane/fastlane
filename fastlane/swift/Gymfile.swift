c249c548993e52a96ddf77bc450a304448968d37
Joshua Liebowitz <taquitos@users.noreply.github.com>
2018-01-18 12:42:50 -0800

    Version bump to 2.77.1 (#11617)

--- fastlane/swift/Gymfile.swift
+++ fastlane/swift/Gymfile.swift
@@ -18,4 +18,4 @@ class Gymfile: GymfileProtocol {
 
 
 
-// Generated with fastlane 2.76.1
+// Generated with fastlane 2.77.0
--- fastlane/swift/Precheckfile.swift
+++ fastlane/swift/Precheckfile.swift
@@ -18,4 +18,4 @@ class Precheckfile: PrecheckfileProtocol {
 
 
 
-// Generated with fastlane 2.76.1
+// Generated with fastlane 2.77.0
