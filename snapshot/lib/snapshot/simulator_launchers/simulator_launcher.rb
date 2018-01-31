c2a4a9f61c91327635ffa7ba3a4c539203cda289
Jules2010 <julesthecoder@gmail.com>
2018-01-31 13:44:23 +0000

    Update simulator_launcher.rb

--- snapshot/lib/snapshot/simulator_launchers/simulator_launcher.rb
+++ snapshot/lib/snapshot/simulator_launchers/simulator_launcher.rb
@@ -120,7 +120,7 @@ module Snapshot
                                                 cleanup_after_failure(devices, language, locale, launch_args, return_code)
 
                                                 # no exception raised... that means we need to retry
-                                                UI.error "Caught error... #{return_code}"
+                                                UI.error("Caught error... #{return_code}")
 
                                                 self.current_number_of_retries_due_to_failing_simulator += 1
                                                 if self.current_number_of_retries_due_to_failing_simulator < 20 && return_code != 65
@@ -154,8 +154,8 @@ module Snapshot
                                                        end,
                                                 error: proc do |output, return_code|
                                                          ErrorHandler.handle_test_error(output, return_code)
-                                                         UI.error "Caught error... #{return_code}"
-                                                         UI.error "Caught output... #{output}"
+                                                         UI.error("Caught error... #{return_code}")
+                                                         UI.error("Caught output... #{output}")
                                                        end)
       end
     end
