60497b97e51400d849cff73bb193865ba01e7f36
Jules2010 <julesthecoder@gmail.com>
2018-01-31 13:00:10 +0000

    Update SnapshotHelper.swift

--- snapshot/lib/assets/SnapshotHelper.swift
+++ snapshot/lib/assets/SnapshotHelper.swift
@@ -248,29 +248,6 @@ private extension CGFloat {
     }
 }
 
-extension URLSession {
-    func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
-        var data: Data?
-        var response: URLResponse?
-        var error: Error?
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        let dataTask = self.dataTask(with: url) {
-            data = $0
-            response = $1
-            error = $2
-            
-            semaphore.signal()
-        }
-        dataTask.resume()
-        
-        _ = semaphore.wait(timeout: .distantFuture)
-        
-        return (data, response, error)
-    }
-}
-
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
 
