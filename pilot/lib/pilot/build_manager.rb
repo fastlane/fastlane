884455f2ef7558799aebbe3057a16b2983133f78
Rishabh Tayal <rtayal11@gmail.com>
2018-02-05 12:32:19 -0600

    pilot - added changelog validation (#11777)
    * added changelog check
    
    * rubocop fix
    
    * Added UI.input to ask for user input

--- pilot/lib/pilot/build_manager.rb
+++ pilot/lib/pilot/build_manager.rb
@@ -15,6 +15,14 @@ module Pilot
 
       UI.user_error!("No ipa file given") unless config[:ipa]
 
+      if options[:changelog].nil? and options[:distribute_external] == true
+        if UI.interactive?
+          options[:changelog] = UI.input("No changelog provided for new build. Please provide a changelog. You can also provide a changelog using the `changelog` option")
+        else
+          UI.user_error!("No changelog provided for new build. Please either disable `distribute_external` or provide a changelog using the `changelog` option")
+        end
+      end
+
       UI.success("Ready to upload new build to TestFlight (App: #{app.apple_id})...")
 
       dir = Dir.mktmpdir
