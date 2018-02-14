916586f3dacc2f5d6d150c2894081486225663b5
giginet <giginet.net@gmail.com>
2018-02-14 17:20:54 +0900

    resolve_path on build_settings

--- fastlane/lib/fastlane/actions/update_app_identifier.rb
+++ fastlane/lib/fastlane/actions/update_app_identifier.rb
@@ -23,7 +23,7 @@ module Fastlane
           configs = project.objects.select { |obj| obj.isa == 'XCBuildConfiguration' && !obj.build_settings[identifier_key].nil? }
           UI.user_error!("Info plist uses $(#{identifier_key}), but xcodeproj does not") unless configs.count > 0
 
-          configs = configs.select { |obj| obj.build_settings[info_plist_key] == params[:plist_path] }
+          configs = configs.select { |obj| resolve_path(obj.build_settings[info_plist_key], params[:xcodeproj]) == info_plist_path }
           UI.user_error!("Xcodeproj doesn't have configuration with info plist #{params[:plist_path]}.") unless configs.count > 0
 
           # For each of the build configurations, set app identifier
