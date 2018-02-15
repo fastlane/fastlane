824712ece84d1dd09cd3c290cbcbe03ab8f2fc48
Dunya Kirkali <dunyakirkali@gmail.com>
2018-02-15 18:19:59 +0100

    Allow the usage of a postfix for the add_git_tag action (#11794)
    * Allow the usage of a postfix for the add_git_tag action
    
    * Boyscout

--- fastlane/lib/fastlane/actions/add_git_tag.rb
+++ fastlane/lib/fastlane/actions/add_git_tag.rb
@@ -6,7 +6,7 @@ module Fastlane
         # lane name in lane_context could be nil because you can just call $fastlane add_git_tag which has no context
         lane_name = Actions.lane_context[Actions::SharedValues::LANE_NAME].to_s.delete(' ') # no spaces allowed
 
-        tag = options[:tag] || "#{options[:grouping]}/#{lane_name}/#{options[:prefix]}#{options[:build_number]}"
+        tag = options[:tag] || "#{options[:grouping]}/#{lane_name}/#{options[:prefix]}#{options[:build_number]}#{options[:postfix]}"
         message = options[:message] || "#{tag} (fastlane)"
 
         cmd = ['git tag']
@@ -31,6 +31,7 @@ module Fastlane
           "- `grouping` is just to keep your tags organised under one 'folder', defaults to 'builds'",
           "- `lane` is the name of the current fastlane lane",
           "- `prefix` is anything you want to stick in front of the version number, e.g. 'v'",
+          "- `postfix` is anything you want to stick at the end of the version number, e.g. '-RC1'",
           "- `build_number` is the build number, which defaults to the value emitted by the `increment_build_number` action",
           "",
           "For example for build 1234 in the 'appstore' lane it will tag the commit with `builds/appstore/1234`"
@@ -50,6 +51,10 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :prefix,
                                        env_name: "FL_GIT_TAG_PREFIX",
                                        description: "Anything you want to put in front of the version number (e.g. 'v')",
+                                       default_value: ''),
+          FastlaneCore::ConfigItem.new(key: :postfix,
+                                       env_name: "FL_GIT_TAG_POSTFIX",
+                                       description: "Anything you want to put at the end of the version number (e.g. '-RC1')",
                                        default_value: ''),
           FastlaneCore::ConfigItem.new(key: :build_number,
                                        env_name: "FL_GIT_TAG_BUILD_NUMBER",
@@ -85,6 +90,7 @@ module Fastlane
           'add_git_tag(
             grouping: "fastlane-builds",
             prefix: "v",
+            postfix: "-RC1",
             build_number: 123
           )',
           '# Alternatively, you can specify your own tag. Note that if you do specify a tag, all other arguments are ignored.
