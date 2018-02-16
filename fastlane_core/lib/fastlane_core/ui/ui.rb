9a437ba6c7c1fee28fcb821fab7fb6b0effd7ba7
Felix Krause <krausefx@gmail.com>
2018-02-15 16:26:24 -0800

    Update fastlane UI handler to support FastlaneCI rendering
    Related to https://github.com/fastlane/ci/pull/139

--- fastlane_core/lib/fastlane_core/ui/ui.rb
+++ fastlane_core/lib/fastlane_core/ui/ui.rb
@@ -1,19 +1,21 @@
 module FastlaneCore
   class UI
     class << self
-      def current
+      def ui_object
         require_relative 'implementations/shell'
-        @current ||= Shell.new
+        @ui_object ||= Shell.new
       end
-    end
 
-    def self.method_missing(method_sym, *args, &_block)
-      # not using `responds` because we don't care about methods like .to_s and so on
-      require_relative 'interface'
-      interface_methods = FastlaneCore::Interface.instance_methods - Object.instance_methods
-      UI.user_error!("Unknown method '#{method_sym}', supported #{interface_methods}") unless interface_methods.include?(method_sym)
+      def method_missing(method_sym, *args, &_block)
+        # not using `responds` because we don't care about methods like .to_s and so on
+        require_relative 'interface'
+        interface_methods = FastlaneCore::Interface.instance_methods - Object.instance_methods
+        UI.user_error!("Unknown method '#{method_sym}', supported #{interface_methods}") unless interface_methods.include?(method_sym)
 
-      self.current.send(method_sym, *args)
+        @ui_object.send(method_sym, *args)
+      end
+
+      attr_writer(:ui_object)
     end
   end
 end
