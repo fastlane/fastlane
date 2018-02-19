daad5f45c78c91a58eeeeffae3fbfa6cdccd0181
Matteo Martinelli <matteomartinelli1992@gmail.com>
2018-02-19 09:08:58 +0100

    Remove documentation

--- spaceship/lib/spaceship/tunes/iap_subscription_pricing_intro_offer_type.rb
+++ spaceship/lib/spaceship/tunes/iap_subscription_pricing_intro_offer_type.rb
@@ -1,8 +1,6 @@
 module Spaceship
   module Tunes
     # Defines the different in-app purchase intro offer types
-    #
-    # As specified by Apple: https://developer.apple.com/library/content/documentation/LanguagesUtilities/Conceptual/iTunesConnectInAppPurchase_Guide/Chapters/CreatingInAppPurchaseProducts.html
     module IAPSubscriptionPricingIntroOfferType
       FREE_TRIAL = "FreeTrial"
       PAY_AS_YOU_GO = "PayAsYouGo"
