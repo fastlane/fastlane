c74dcc4c7d1915f4b6dba4dc1e73eea47aa7f031
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-02-24 23:19:25 +0200

    Rename dynamic default value legend link
    Making it more specific decreases the chances of causing issues
    with other links.

--- fastlane_core/lib/fastlane_core/configuration/config_item.rb
+++ fastlane_core/lib/fastlane_core/configuration/config_item.rb
@@ -279,7 +279,7 @@ module FastlaneCore
     end
 
     def doc_default_value
-      return "[*](#dynamic)" if self.default_value_dynamic
+      return "[*](#parameters-legend-dynamic)" if self.default_value_dynamic
       return "" if self.default_value.nil?
       return "`''`" if self.default_value.instance_of?(String) && self.default_value.empty?
 
