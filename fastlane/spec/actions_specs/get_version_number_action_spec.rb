5e76d2f87496a0cd252088f7728ca676d2591a38
Josh Holtz <josh@rokkincat.com>
2018-03-12 07:54:15 -0500

    Tests now using actual xcode project

--- fastlane/spec/actions_specs/get_version_number_action_spec.rb
+++ fastlane/spec/actions_specs/get_version_number_action_spec.rb
@@ -3,58 +3,60 @@ describe Fastlane do
     describe "Get Version Number Integration" do
       require 'shellwords'
 
+      path = File.absolute_path("./fastlane/spec/fixtures/actions/get_version_number/get_version_number/")
+
       it "gets the correct version number for 'TargetA'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetA')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetA')
         end").runner.execute(:test)
         expect(result).to eq("4.3.2")
       end
 
       it "gets the correct version number for 'TargetATests'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetATests')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetATests')
         end").runner.execute(:test)
         expect(result).to eq("4.3.2")
       end
 
       it "gets the correct version number for 'TargetB'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetB')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetB')
         end").runner.execute(:test)
         expect(result).to eq("5.4.3")
       end
 
       it "gets the correct version number for 'TargetBTests'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetBTests')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetBTests')
         end").runner.execute(:test)
         expect(result).to eq("5.4.3")
       end
 
       it "gets the correct version number for 'TargetC_internal'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetC_internal')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetC_internal')
         end").runner.execute(:test)
         expect(result).to eq("7.5.2")
       end
 
       it "gets the correct version number for 'TargetC_production'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetC_production')
+          get_version_number(xcodeproj: '#{path}', target: 'TargetC_production')
         end").runner.execute(:test)
         expect(result).to eq("6.4.9")
       end
 
       it "gets the correct version number for 'SampleProject_tests'" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'SampleProject_tests')
+          get_version_number(xcodeproj: '#{path}', target: 'SampleProject_tests')
         end").runner.execute(:test)
         expect(result).to eq("1.0")
       end
 
       it "gets the correct version number with no target specified" do
         result = Fastlane::FastFile.new.parse("lane :test do
-          get_version_number(xcodeproj: '.xcproject')
+          get_version_number(xcodeproj: '#{path}')
         end").runner.execute(:test)
         expect(result).to eq("4.3.2")
       end
