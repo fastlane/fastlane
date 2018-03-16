2d2298ff7a46615701eec837234b185217360ce2
Josh Holtz <josh@rokkincat.com>
2018-03-16 14:31:24 -0500

    get_version_number uses xcodeproj as fallback when can't find "target" (#12037)
    * Initial commit for xcodeproj fallback
    
    * Tests now using actual xcode project
    
    * Everything works now, woot woot
    
    * Rubo and test fix
    
    * Cleaned up some code
    
    * Rubo stuff...
    
    * Hardcoded agvtool resutls for test
    
    * Pass ubuntu tests hopefully
    
    * whooops
    
    * Added xcodeproj dep on one test
    
    * Now only uses xcodeproj cause agvtool was a bit sketch
    
    * Rubo-ed
    
    * rubo man

---
+++ fastlane/spec/fixtures/actions/get_version_number/get_version_number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  get_version_number
+//
+//  Created by Josh Holtz on 3/12/18.
+//  Copyright © 2018 fastlane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ fastlane/spec/fixtures/actions/get_version_number/get_version_numberTests/get_version_numberTests.swift
@@ -0,0 +1,36 @@
+//
+//  get_version_numberTests.swift
+//  get_version_numberTests
+//
+//  Created by Josh Holtz on 3/12/18.
+//  Copyright © 2018 fastlane. All rights reserved.
+//
+
+import XCTest
+@testable import get_version_number
+
+class get_version_numberTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
