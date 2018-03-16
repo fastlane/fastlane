7178dc67a0f86f8f8e73040e42d85a5c7bd647ca
Josh Holtz <josh@rokkincat.com>
2018-03-16 13:57:52 -0500

    Adding link to issue

--- snapshot/lib/snapshot/test_command_generator.rb
+++ snapshot/lib/snapshot/test_command_generator.rb
@@ -83,6 +83,8 @@ module Snapshot
       # Creates an alias for require to prevent `rubocop/require_tools`
       # from failing. This eventually change it when `simctl` doesn't
       # execute shell commands at the top-level anymore
+      #
+      # Issue: https://github.com/fastlane/fastlane/issues/12071
       alias silence_the_horrible_require_checker require
 
       def get_device_type_with_simctl(device_names)
