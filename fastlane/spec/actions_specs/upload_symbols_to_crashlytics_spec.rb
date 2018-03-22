f8f83124e082aacffc13beb5b8dac0b10e956494
Takeru Chuganji <chu@ganzy.jp>
2018-03-22 08:17:22 -0700

    [Action] Fix a bug that upload_symbols_to_crashlytics fails in specific cases (#12098)
    * Shell escape path
    
    * Fix test to use plist which includes space
    
    and adds test to the case when given gsp_path is not found

--- fastlane/spec/actions_specs/upload_symbols_to_crashlytics_spec.rb
+++ fastlane/spec/actions_specs/upload_symbols_to_crashlytics_spec.rb
@@ -22,7 +22,7 @@ describe Fastlane do
       it "uploads dSYM files" do
         binary_path = './spec/fixtures/screenshots/screenshot1.png'
         dsym_path = './spec/fixtures/dSYM/Themoji.dSYM'
-        gsp_path = './spec/fixtures/plist/Info.plist'
+        gsp_path = './spec/fixtures/plist/With Space.plist'
 
         command = []
         command << File.expand_path(File.join("fastlane", binary_path)).shellescape
@@ -51,6 +51,17 @@ describe Fastlane do
             upload_symbols_to_crashlytics(
               dsym_path: 'fastlane/#{dsym_path}',
               binary_path: 'fastlane/#{binary_path}')
+          end").runner.execute(:test)
+        end.to raise_error(FastlaneCore::Interface::FastlaneError)
+      end
+
+      it "raises exception if given gsp_path is not found" do
+        gsp_path = './spec/fixtures/plist/_Not Exist_.plist'
+        expect do
+          Fastlane::FastFile.new.parse("lane :test do
+            upload_symbols_to_crashlytics(
+              gsp_path: 'fastlane/#{gsp_path}',
+              api_token: 'something123')
           end").runner.execute(:test)
         end.to raise_error(FastlaneCore::Interface::FastlaneError)
       end
