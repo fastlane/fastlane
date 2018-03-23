b5203c51c01cc50dc5fe1ded4659f4757f762952
Josh Holtz <josh@rokkincat.com>
2018-03-23 09:59:24 -0500

    app_store_build_number converts return to int if possible (#12148)

--- fastlane/spec/actions_specs/app_store_build_number_spec.rb
+++ fastlane/spec/actions_specs/app_store_build_number_spec.rb
@@ -21,6 +21,26 @@ describe Fastlane do
 
         expect(result).to eq(['1', '2.3', '3', '5.6', '6.5.4', '9', '11.4.6'])
       end
+
+      it "returns value as string (with build number as version string)" do
+        allow(Fastlane::Actions::AppStoreBuildNumberAction).to receive(:get_build_number).and_return("1.2.3")
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_build_number(username: 'name@example.com', app_identifier: 'x.y.z')
+        end").runner.execute(:test)
+
+        expect(result).to eq("1.2.3")
+      end
+
+      it "returns value as integer (with build number as version number)" do
+        allow(Fastlane::Actions::AppStoreBuildNumberAction).to receive(:get_build_number).and_return("3")
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_build_number(username: 'name@example.com', app_identifier: 'x.y.z')
+        end").runner.execute(:test)
+
+        expect(result).to eq(3)
+      end
     end
   end
 end
