5d5d60f357d92d4bf498444bbf273b760ba6f176
Josh Holtz <josh@rokkincat.com>
2018-04-06 07:23:00 -0500

    Added tests for special character and emojis and tests for tests

--- fastlane_core/spec/ipa_upload_package_builder_spec.rb
+++ fastlane_core/spec/ipa_upload_package_builder_spec.rb
@@ -5,13 +5,35 @@ describe FastlaneCore do
     let(:uploader) { FastlaneCore::IpaUploadPackageBuilder.new }
     let(:unique_path) { uploader.unique_ipa_path(path) }
 
-    context 'unique IPA file name' do
-      it 'does not contain any special characters' do
+    def special_chars?(string)
+      string =~ /^[A-Za-z0-9_\.]+$/ ? false : true
+    end
+
+    context 'special_chars?' do
+      it 'returns false for and zero special characters and emoji' do
+        is_valid = special_chars?("something_IPA_1234567890.ipa")
+        expect(is_valid).to be(false)
+      end
+
+      it 'returns true for all special characters' do
         special_chars = %w[! @ # $ % ^ & * ( ) + = [ ] " ' ; : < > ? / \ | { } , ~ `]
 
-        special_chars.each do |special_char|
-          expect(unique_path).not_to(include(special_char))
+        special_chars.each do |c|
+          is_valid = special_chars?("something_#{c}.ipa")
+          expect(is_valid).to be(true)
         end
+      end
+
+      it 'returns true for emoji' do
+        is_valid = special_chars?("something_😝_🚀.ipa")
+        expect(is_valid).to be(true)
+      end
+    end
+
+    context 'unique IPA file name' do
+      it 'does not contain any special characters' do
+        is_valid = !special_chars?(unique_path)
+        expect(is_valid).to be(true)
       end
 
       it 'does not start with allowed special characters' do
