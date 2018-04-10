cb174573f913c2839dd7934b07c86eb47e424013
Adam Cohen-Rose <github.com@cohen-rose.org>
2018-04-10 23:13:25 +0100

    [adb] Pick up ANDROID_SDK_ROOT environment variable (#12198)
    * Use standard Android SDK environment variable for adb path
    
    According to https://developer.android.com/studio/command-line/variables.html
    the ANDROID_SDK_ROOT environment variable can contain the path to the
    sdk root. `adb` is then in the `platform-tools` sub-directory.
    
    * rubocop fix
    
    * Moved environment variable detection to AdbHelper
    
    * rubocop fix

--- fastlane/spec/actions_specs/adb_spec.rb
+++ fastlane/spec/actions_specs/adb_spec.rb
@@ -8,6 +8,15 @@ describe Fastlane do
 
         expect(result).to eq(" ./fastlane/README.md test")
       end
+
+      it "picks up path from ANDROID_SDK_ROOT environment variable" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          ENV['ANDROID_SDK_ROOT'] = '/usr/local/android-sdk'
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq(" /usr/local/android-sdk/platform-tools/adb test")
+      end
     end
   end
 end
