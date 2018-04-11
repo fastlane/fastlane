1325cd83e1c7fd7d96d2756175565b321411c1e9
Rishabh Tayal <rtayal11@gmail.com>
2018-04-11 08:58:27 -0500

    some test case changes

--- fastlane/spec/actions_specs/update_plist_spec.rb
+++ fastlane/spec/actions_specs/update_plist_spec.rb
@@ -20,11 +20,9 @@ describe Fastlane do
       end
 
       it "updates the plist based on the given block" do
-        full_path = [test_path, proj_file, '..', "NOEXIST-#{plist_path}"].join(File::SEPARATOR)
-
         result = Fastlane::FastFile.new.parse("lane :test do
           update_plist ({
-            plist_path: '#{full_path}',
+            plist_path: '#{plist_path}',
             block: lambda { |plist|
               plist['CFBundleIdentifier'] = '#{app_identifier}'
               plist['CFBundleDisplayName'] = '#{display_name}'
