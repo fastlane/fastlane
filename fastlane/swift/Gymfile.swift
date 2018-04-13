22dd9e84c3481a8e75d9741e8c356b2a887d5495
Josh Holtz <josh@rokkincat.com>
2018-04-13 06:56:13 -0500

    Version bump to 2.91.0 (#12299)

--- fastlane/swift/Gymfile.swift
+++ fastlane/swift/Gymfile.swift
@@ -18,4 +18,4 @@ class Gymfile: GymfileProtocol {
 
 
 
-// Generated with fastlane 2.90.0
+// Generated with fastlane 2.91.0
--- fastlane/swift/Precheckfile.swift
+++ fastlane/swift/Precheckfile.swift
@@ -18,4 +18,4 @@ class Precheckfile: PrecheckfileProtocol {
 
 
 
-// Generated with fastlane 2.90.0
+// Generated with fastlane 2.91.0
