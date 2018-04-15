94d34a02f3d0209698eab74399d37c2e7116c92c
Josh Holtz <josh@rokkincat.com>
2018-04-15 15:38:51 -0500

    [spaceship] add preorder to availability (#12311)
    * Get and set preorder on availability
    
    * Almost there
    
    * Tests work great now
    
    * Fix using attr_mapping

--- spaceship/lib/spaceship/tunes/availability.rb
+++ spaceship/lib/spaceship/tunes/availability.rb
@@ -9,15 +9,38 @@ module Spaceship
       # @return (Array of Spaceship::Tunes::Territory objects) A list of the territories
       attr_accessor :territories
 
+      # @return (Bool) Cleared for preorder
+      attr_accessor :cleared_for_preorder
+
+      # @return (String) App available date in format of "YYYY-MM-DD"
+      attr_accessor :app_available_date
+
       attr_mapping(
-        'theWorld' => :include_future_territories
+        'theWorld' => :include_future_territories,
+        'preOrder.clearedForPreOrder.value' => :cleared_for_preorder,
+        'preOrder.appAvailableDate.value' => :app_available_date
       )
 
       # Create a new object based on a set of territories.
       # @param territories (Array of String or Spaceship::Tunes::Territory objects): A list of the territories
       # @param params (Hash): Optional parameters (include_future_territories (Bool, default: true) Are future territories included?)
       def self.from_territories(territories = [], params = {})
-        obj = self.new
+        # Initializes the DataHash with our preOrder structure so values
+        # that are being modified will be saved
+        #
+        # Note: A better solution for this in the future might be to improve how
+        # Base::DataHash sets values for paths that don't exist
+        obj = self.new(
+          'preOrder' => {
+            'clearedForPreOrder' => {
+              'value' => false
+            },
+            'appAvailableDate' => {
+              'value' => nil
+            }
+          }
+        )
+
         # Detect if the territories attribute is an array of Strings and convert to Territories
         obj.territories =
           if territories[0].kind_of?(String)
@@ -26,11 +49,17 @@ module Spaceship
             territories
           end
         obj.include_future_territories = params.fetch(:include_future_territories, true)
+        obj.cleared_for_preorder = params.fetch(:cleared_for_preorder, false)
+        obj.app_available_date = params.fetch(:app_available_date, nil)
         return obj
       end
 
       def territories
         @territories ||= raw_data['countries'].map { |info| Territory.new(info) }
+      end
+
+      def cleared_for_preorder
+        super || false
       end
     end
   end
