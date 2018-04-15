03c36aaeb6534a8ab0bbdcd991e87374bdfae31e
Josh Holtz <josh@rokkincat.com>
2018-04-15 12:27:27 -0500

    Fix using attr_mapping

--- spaceship/lib/spaceship/tunes/availability.rb
+++ spaceship/lib/spaceship/tunes/availability.rb
@@ -16,14 +16,31 @@ module Spaceship
       attr_accessor :app_available_date
 
       attr_mapping(
-        'theWorld' => :include_future_territories
+        'theWorld' => :include_future_territories,
+        'preOrder.clearedForPreOrder.value' => :cleared_for_preorder,
+        'preOrder.appAvailableDate.value' => :app_available_date
       )
 
       # Create a new object based on a set of territories.
       # @param territories (Array of String or Spaceship::Tunes::Territory objects): A list of the territories
       # @param params (Hash): Optional parameters (include_future_territories (Bool, default: true) Are future territories included?)
       def self.from_territories(territories = [], params = {})
-        obj = self.new
+        # Initializes the DataHash with our preOrder structure so values
+        # that are being modified will be saved
+        #
+        # Note: A better solution for this in the future might be to improve how
+        # Base::DataHash sets values for paths that don't exist
+        obj = self.new(
+          'preOrder' => {
+            'clearedForPreOrder' => {
+              'value' => false
+            },
+            'appAvailableDate' => {
+              'value' => nil
+            }
+          }
+        )
+
         # Detect if the territories attribute is an array of Strings and convert to Territories
         obj.territories =
           if territories[0].kind_of?(String)
@@ -42,22 +59,7 @@ module Spaceship
       end
 
       def cleared_for_preorder
-        return @cleared_for_preorder unless @cleared_for_preorder.nil?
-
-        value = false
-        if (pre_order = raw_data['preOrder']) && (hash = pre_order['clearedForPreOrder'])
-          value = hash['value']
-        end
-        @cleared_for_preorder ||= value
-      end
-
-      def app_available_date
-        return @app_available_date unless @app_available_date.nil?
-
-        if (pre_order = raw_data['preOrder']) && (hash = pre_order['appAvailableDate'])
-          value = hash['value']
-        end
-        @app_available_date ||= value
+        super || false
       end
     end
   end
