91f4fe882194ad6f5c0a3bf624f795338dab034c
Josh Holtz <josh@rokkincat.com>
2018-04-16 12:50:35 -0500

    Stabilize tests by setting all android sdk path environment variables (#12312)
    * Stabalize tests by seting all android sdk path environment variables
    
    * Moved ENV setting to before(:each)

--- fastlane/spec/actions_specs/adb_spec.rb
+++ fastlane/spec/actions_specs/adb_spec.rb
@@ -1,6 +1,12 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "adb" do
+      before(:each) do
+        ENV['ANDROID_HOME'] = nil
+        ENV['ANDROID_SDK_ROOT'] = nil
+        ENV['ANDROID_SDK'] = nil
+      end
+
       it "generates a valid command" do
         result = Fastlane::FastFile.new.parse("lane :test do
           adb(command: 'test', adb_path: './fastlane/README.md')
@@ -9,9 +15,27 @@ describe Fastlane do
         expect(result).to eq(" ./fastlane/README.md test")
       end
 
+      it "picks up path from ANDROID_HOME environment variable" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          ENV['ANDROID_HOME'] = '/usr/local/android-sdk'
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq(" /usr/local/android-sdk/platform-tools/adb test")
+      end
+
       it "picks up path from ANDROID_SDK_ROOT environment variable" do
         result = Fastlane::FastFile.new.parse("lane :test do
           ENV['ANDROID_SDK_ROOT'] = '/usr/local/android-sdk'
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq(" /usr/local/android-sdk/platform-tools/adb test")
+      end
+
+      it "picks up path from ANDROID_SDK environment variable" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          ENV['ANDROID_SDK'] = '/usr/local/android-sdk'
           adb(command: 'test')
         end").runner.execute(:test)
 
