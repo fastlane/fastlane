64762c758949954eeb2a552b0f3c57265e8b095a
Josh Holtz <josh@rokkincat.com>
2018-04-16 09:19:37 -0500

    [screengrab] get and filter by device name

--- fastlane/lib/fastlane/helper/adb_helper.rb
+++ fastlane/lib/fastlane/helper/adb_helper.rb
@@ -1,7 +1,7 @@
 module Fastlane
   module Helper
     class AdbDevice
-      attr_accessor :serial
+      attr_accessor :serial, :name
 
       def initialize(serial: nil)
         self.serial = serial
@@ -30,22 +30,73 @@ module Fastlane
         Action.sh(command)
       end
 
-      def device_avalaible?(serial)
-        load_all_devices
+      def device_avalaible?(serial, load_names = false)
+        load_all_devices(load_names)
         return devices.map(&:serial).include?(serial)
       end
 
-      def load_all_devices
+      def load_all_devices(load_names = false)
         self.devices = []
 
         command = [adb_path, "devices"].join(" ")
         output = Actions.sh(command, log: false)
         output.split("\n").each do |line|
-          if (result = line.match(/(.*)\tdevice$/))
+          if (result = line.match(/^(\S+)(\s+)(?=device)/))
             self.devices << AdbDevice.new(serial: result[1])
           end
         end
-        self.devices
+
+        # Checks if telnet is intalled
+        # Display message to user to install it to get full value
+        has_telnet_installed = has_brew_tool!('telnet')
+
+        # Iterates devices to load name from telnet for emulators
+        # and adb from actual devices
+        if load_names &&
+
+           self.devices
+          self.devices.each do |device|
+            if device.serial.include?('emulator-')
+              load_name_from_telnet(device) if has_telnet_installed
+            else
+              load_name_from_adb(device)
+            end
+          end
+        end
+      end
+
+      private
+
+      def load_name_from_telnet(device)
+        # Gets port to connect to via telnet
+        port = device.serial.gsub('emulator-', '')
+
+        # Pipes `avd name` to telnet, sleeps to get output, and exits
+        # Runs empty echo after telnet to make Actions.sh happy
+        output = Actions.sh(
+          "sleep1; (echo 'avd name'; sleep 1; echo 'exit 0') | telnet localhost #{port}; echo ''",
+          log: false
+        )
+
+        # Gets emulator name from between the "OK"s
+        regex = /\sOK\s(.*?)\sOK\b/m
+        device.name = output.slice(regex, 1)
+      end
+
+      def load_name_from_adb(device)
+        device.name = trigger(command: "shell getprop ro.product.model", serial: device.serial).strip
+      end
+
+      def has_brew_tool!(tool)
+        unless `which #{tool}`.include?(tool.to_s)
+          UI.error('#############################################################')
+          UI.error("# You have to install the #{tool} to use load_name")
+          UI.error("# Install it using 'brew update && brew install #{tool}'")
+          UI.error("# If you don't have homebrew: http://brew.sh")
+          UI.error('#############################################################')
+          return false
+        end
+        return true
       end
     end
   end
