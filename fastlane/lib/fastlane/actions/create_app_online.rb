d1638ec4dbabae0231b237e3fa89f0339e23b4e5
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-04-17 22:45:05 +0300

    Replace single quotes with double quotes

--- fastlane/lib/fastlane/actions/create_app_online.rb
+++ fastlane/lib/fastlane/actions/create_app_online.rb
@@ -27,9 +27,9 @@ module Fastlane
 
       def self.details
         [
-          'Create new apps on iTunes Connect and Apple Developer Portal via _produce_.',
-          'If the app already exists, `create_app_online` will not do anything.',
-          'For more information about _produce_, visit its documentation page: [https://docs.fastlane.tools/actions/produce/](https://docs.fastlane.tools/actions/produce/).'
+          "Create new apps on iTunes Connect and Apple Developer Portal via _produce_.",
+          "If the app already exists, `create_app_online` will not do anything.",
+          "For more information about _produce_, visit its documentation page: [https://docs.fastlane.tools/actions/produce/](https://docs.fastlane.tools/actions/produce/)."
         ].join(' ')
       end
 
