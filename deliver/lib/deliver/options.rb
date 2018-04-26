32fbc16d6eb11ffb8217d06a57ebf9e81fa683d6
arakawa_yusuke <mofneko@users.noreply.github.com>
2018-04-26 15:55:00 +0900

    Fix fastlane docs

--- deliver/lib/deliver/options.rb
+++ deliver/lib/deliver/options.rb
@@ -221,7 +221,13 @@ module Deliver
         # rubocop:disable Metrics/LineLength
         FastlaneCore::ConfigItem.new(key: :itc_provider,
                                      env_name: "DELIVER_ITC_PROVIDER",
-                                     description: "The provider short name to be used with the iTMSTransporter to identify your team. To get provider short name run `pathToXcode.app/Contents/Applications/Application\\ Loader.app/Contents/itms/bin/iTMSTransporter -m provider -u 'USERNAME' -p 'PASSWORD' -account_type itunes_connect -v off`. The short names of providers should be listed in the second column",
+                                     description:
+                                     [
+                                               "The provider short name to be used with the iTMSTransporter to identify your team. To get provider short name run",
+                                               "`pathToXcode.app/Contents/Applications/Application\\ Loader.app/Contents/itms/bin/iTMSTransporter -m provider \\`",
+                                               "`-u 'USERNAME' -p 'PASSWORD' -account_type itunes_connect -v off`",
+                                               "The short names of providers should be listed in the second column",
+                                     ].join("\n"),
                                      optional: true),
         # rubocop:enable Metrics/LineLength
         FastlaneCore::ConfigItem.new(key: :overwrite_screenshots,
