ad47cd1206201c2fd895db46d1277f42e1691a65
Felix Krause <KrauseFx@gmail.com>
2018-05-02 10:23:52 +0200

    Update spelling of user

--- fastlane/lib/fastlane/actions/update_fastlane.rb
+++ fastlane/lib/fastlane/actions/update_fastlane.rb
@@ -28,7 +28,7 @@ module Fastlane
         sudo_needed = !File.writable?(gem_dir)
 
         if sudo_needed
-          UI.important("It seems that your Gem directory is not writable by your current User.")
+          UI.important("It seems that your Gem directory is not writable by your current user.")
           UI.important("fastlane would need sudo rights to update itself, however, running 'sudo fastlane' is not recommended.")
           UI.important("If you still want to use this action, please read the documentation on a guide how to set this up:")
           UI.important("https://docs.fastlane.tools/actions/#update_fastlane")
