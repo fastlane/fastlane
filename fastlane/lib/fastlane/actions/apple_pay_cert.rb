db08d80c3518c61b47aa65771aab0197c8b71956
RishabhTayal <rtayal11@gmail.com>
2018-05-07 20:11:15 -0500

    saves certificates locally

--- fastlane/lib/fastlane/actions/apple_pay_cert.rb
+++ fastlane/lib/fastlane/actions/apple_pay_cert.rb
@@ -29,6 +29,31 @@ module Fastlane
             raise ex
           end
         end
+
+        x509_certificate = cert.download
+        filename_base = params[:merchant_bundle_id] # PEM.config[:pem_name] || "#{PEM.config[:app_identifier]}"
+        filename_base = File.basename(filename_base, ".pem") # strip off the .pem if it was provided.
+
+        output_path = File.expand_path('.')
+        FileUtils.mkdir_p(output_path)
+
+        if params[:save_private_key]
+          private_key_path = File.join(output_path, "#{filename_base}.pkey")
+          File.write(private_key_path, pkey.to_pem)
+          UI.message("Private key: ".green + Pathname.new(private_key_path).realpath.to_s)
+        end
+
+        if params[:generate_p12]
+          p12_cert_path = File.join(output_path, "#{filename_base}.p12")
+          p12 = OpenSSL::PKCS12.create(params[:p12_password], 'production', pkey, x509_certificate)
+          File.write(p12_cert_path, p12.to_der)
+          UI.message("p12 certificate: ".green + Pathname.new(p12_cert_path).realpath.to_s)
+        end
+
+        x509_cert_path = File.join(output_path, "#{filename_base}.pem")
+        File.write(x509_cert_path, x509_certificate.to_pem + pkey.to_pem)
+        UI.message("PEM: ".green + Pathname.new(x509_cert_path).realpath.to_s)
+        return x509_cert_path
       end
 
       def self.certificate
@@ -70,6 +95,12 @@ module Fastlane
                                  verify_block: proc do |value|
                                    ENV["FASTLANE_TEAM_ID"] = value.to_s
                                  end),
+          FastlaneCore::ConfigItem.new(key: :p12_password,
+                                     short_option: "-p",
+                                     env_name: "APPLE_PAY_CERT_P12_PASSWORD",
+                                     sensitive: true,
+                                     description: "The password that is used for your p12 file",
+                                     default_value: ""),
           FastlaneCore::ConfigItem.new(key: :team_name,
                                        short_option: "-l",
                                        env_name: "APPLE_PAY_CERT_TEAM_NAME",
@@ -80,7 +111,18 @@ module Fastlane
                                        default_value_dynamic: true,
                                        verify_block: proc do |value|
                                          ENV["FASTLANE_TEAM_NAME"] = value.to_s
-                                       end)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :generate_p12,
+                                     env_name: "APPLE_PAY_CERT_GENERATE_P12_FILE",
+                                     description: "Generate a p12 file additionally to a PEM file",
+                                     is_string: false,
+                                     default_value: true),
+          FastlaneCore::ConfigItem.new(key: :save_private_key,
+                                    short_option: "-s",
+                                    env_name: "APPLE_PAY_CERT_SAVE_PRIVATEKEY",
+                                    description: "Set to save the private RSA key",
+                                    is_string: false,
+                                    default_value: true)
         ]
       end
 
