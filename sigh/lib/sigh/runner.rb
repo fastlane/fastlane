1eedced3db6ec86642c37a2e1845ca962e413f5b
Brendan Conron <conronb@gmail.com>
2018-05-07 17:59:32 -0700

    added comment

--- sigh/lib/sigh/runner.rb
+++ sigh/lib/sigh/runner.rb
@@ -176,6 +176,9 @@ module Sigh
           certificates = Spaceship.certificate.development.all
         elsif profile_type == Spaceship.provisioning_profile.InHouse
           certificates = Spaceship.certificate.in_house.all
+        # handles case where the desired certificate type is adhoc but the account is an enterprise account
+        # the apple dev portal api has a weird quirk in it where if you query for distribution certificates
+        # for enterprise accounts, you get nothing back even if they exist.
         elsif profile_type == Spaceship.provisioning_profile.AdHoc && Spaceship.client.in_house?
           certificates = Spaceship.certificate.in_house.all
         else
