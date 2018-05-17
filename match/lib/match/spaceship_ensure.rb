c6fdadacb6563c15455837909477cea8e047f323
Josh Holtz <josh@rokkincat.com>
2018-05-16 20:42:35 -0500

    [match] add new option to recreate deleted profiles in dev portal

--- match/lib/match/spaceship_ensure.rb
+++ match/lib/match/spaceship_ensure.rb
@@ -53,7 +53,7 @@ module Match
       UI.user_error!("To reset the certificates of your Apple account, you can use the `fastlane match nuke` feature, more information on https://docs.fastlane.tools/actions/match/")
     end
 
-    def profile_exists(username: nil, uuid: nil)
+    def profile_exists(username: nil, uuid: nil, return_if_missing: false)
       found = Spaceship.provisioning_profile.all.find do |profile|
         profile.uuid == uuid
       end
@@ -61,6 +61,9 @@ module Match
       unless found
         UI.error("Provisioning profile '#{uuid}' is not available on the Developer Portal")
         UI.error("for the user #{username}")
+
+        return false if return_if_missing
+
         UI.error("Make sure to use the same user and team every time you run 'match' for this")
         UI.error("Git repository. This might be caused by deleting the provisioning profile on the Dev Portal")
         UI.user_error!("To reset the provisioning profiles of your Apple account, you can use the `fastlane match nuke` feature, more information on https://docs.fastlane.tools/actions/match/")
