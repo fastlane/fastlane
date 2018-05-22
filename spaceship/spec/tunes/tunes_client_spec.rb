8464339e1318ba9f3cbe64bd94d76ac781ed1e87
Josh Holtz <josh@rokkincat.com>
2018-05-22 09:28:50 -0500

    [spaceship] display apple id and privacy statement instructions to hsa2 accounts (#12591)

--- spaceship/spec/tunes/tunes_client_spec.rb
+++ spaceship/spec/tunes/tunes_client_spec.rb
@@ -54,6 +54,17 @@ describe Spaceship::TunesClient do
         Spaceship::Tunes.login(username, password)
       end.to raise_exception(Spaceship::AppleIDAndPrivacyAcknowledgementNeeded, "Need to acknowledge to Apple's Apple ID and Privacy statement. Please manually log into https://appleid.apple.com (or https://itunesconnect.apple.com) to acknowledge the statement.")
     end
+
+    it 'has authType of hsa2' do
+      response = double
+      allow(response).to receive(:status).and_return(412)
+      allow(response).to receive(:body).and_return({ "authType" => "hsa2" })
+      allow_any_instance_of(Spaceship::Client).to receive(:request).and_return(response)
+
+      expect do
+        Spaceship::Tunes.login(username, password)
+      end.to raise_exception(Spaceship::AppleIDAndPrivacyAcknowledgementNeeded, "Need to acknowledge to Apple's Apple ID and Privacy statement. Please manually log into https://appleid.apple.com (or https://itunesconnect.apple.com) to acknowledge the statement.")
+    end
   end
 
   describe "Logged in" do
