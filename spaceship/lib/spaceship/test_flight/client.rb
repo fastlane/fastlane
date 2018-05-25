707d5cda8bb19ea7e7bd45581abb093b7c08b017
Jerome Lacoste <jerome.lacoste@gmail.com>
2018-05-25 06:10:48 +0200

    spaceship: allow to remove tester(s) from testflight

--- spaceship/lib/spaceship/test_flight/client.rb
+++ spaceship/lib/spaceship/test_flight/client.rb
@@ -202,6 +202,17 @@ module Spaceship
         handle_response(response)
       end
 
+      def remove_testers_from_testflight(app_id: nil, tester_ids: nil)
+        assert_required_params(__method__, binding)
+        url = "providers/#{team_id}/apps/#{app_id}/deleteTesters"
+        response = request(:post) do |req|
+          req.url(url)
+          req.body = tester_ids.map { |i| { "id" => i } }.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+        handle_response(response)
+      end
+
       def search_for_tester_in_app(app_id: nil, text: nil)
         assert_required_params(__method__, binding)
         text = CGI.escape(text)
