09a7530682c76336b6cf10f7f8a398a3753e96ad
Josh Holtz <josh@rokkincat.com>
2018-06-01 17:16:11 -0500

    increment_build_number informs user what to do when detecting SRCROOT (#12540)
    * increment_build_number warns user what to do when detecting SRCROOT
    
    * Whoops, tests

--- fastlane/spec/actions_specs/set_build_number_repository_spec.rb
+++ fastlane/spec/actions_specs/set_build_number_repository_spec.rb
@@ -2,35 +2,60 @@ describe Fastlane do
   describe Fastlane::FastFile do
     context "set build number repository" do
       before do
+        allow(Fastlane::Actions::IncrementBuildNumberAction).to receive(:system).with(/agvtool/).and_return(true)
         expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:run).and_return("asd123")
       end
 
       it "set build number without xcodeproj" do
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool new[-]version [-]all asd123 && cd [-]/)
+          .and_return("")
+
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool what[-]version/, log: false)
+          .and_return("Current version of project Test is:\nasd123")
+
         result = Fastlane::FastFile.new.parse("lane :test do
             set_build_number_repository
         end").runner.execute(:test)
 
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all asd123/)
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq('asd123')
       end
 
       it "set build number with use_hg_revision_number" do
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool new[-]version [-]all asd123 && cd [-]/)
+          .and_return("")
+
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool what[-]version/, log: false)
+          .and_return("Current version of project Test is:\nasd123")
+
         result = Fastlane::FastFile.new.parse("lane :test do
             set_build_number_repository(
               use_hg_revision_number: true
             )
         end").runner.execute(:test)
 
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all asd123/)
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq('asd123')
       end
 
       it "set build number with explicit xcodeproj" do
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool new[-]version [-]all asd123 && cd [-]/)
+          .and_return("")
+
+        expect(Fastlane::Actions).to receive(:sh)
+          .with(/agvtool what[-]version/, log: false)
+          .and_return("Current version of project Test is:\nasd123")
+
         result = Fastlane::FastFile.new.parse("lane :test do
             set_build_number_repository(
               xcodeproj: 'asd123/project.xcodeproj'
             )
         end").runner.execute(:test)
 
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all asd123/)
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to eq('asd123')
       end
     end
   end
