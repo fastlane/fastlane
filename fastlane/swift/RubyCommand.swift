d4be64b5980932ce1566cb07d52ac9876eaec1eb
JD Hall <jhall@nextit.com>
2018-06-28 16:21:53 -0700

    Remove flattening of array in ruby command

--- fastlane/swift/RubyCommand.swift
+++ fastlane/swift/RubyCommand.swift
@@ -55,8 +55,6 @@ struct RubyCommand: RubyCommandable {
 
                     if type == .stringClosure  {
                         return "{\"name\" : \"\(name)\", \"value\" : \"ignored_for_closure\"\(typeJson)}"
-                    } else if let array = someValue as? [String] {
-                        return "{\"name\" : \"\(name)\", \"value\" : \"\(array.joined(separator: ","))\"\(typeJson)}"
                     } else if let hash = someValue as? [String : Any] {
                         let jsonData = try! JSONSerialization.data(withJSONObject: hash, options: [])
                         let jsonString = String(data: jsonData, encoding: .utf8)!
