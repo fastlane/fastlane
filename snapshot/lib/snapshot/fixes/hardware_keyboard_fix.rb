52f29d392d378dbcd2dbaf72823e730590eb0830
Stephen Williams <onato@users.noreply.github.com>
2018-07-12 23:45:10 +1200

    Set ConnectHardwareKeyboard to false for all simulators. Issue #12325. (#12829)

--- snapshot/lib/snapshot/fixes/hardware_keyboard_fix.rb
+++ snapshot/lib/snapshot/fixes/hardware_keyboard_fix.rb
@@ -10,6 +10,17 @@ module Snapshot
         UI.verbose("Patching simulator to work with secure text fields")
 
         Helper.backticks("defaults write com.apple.iphonesimulator ConnectHardwareKeyboard 0", print: FastlaneCore::Globals.verbose?)
+
+        # For > Xcode 9
+        # https://stackoverflow.com/questions/38010494/is-it-possible-to-toggle-software-keyboard-via-the-code-in-ui-test/47820883#47820883
+        Helper.backticks("/usr/libexec/PlistBuddy "\
+                         "-c \"Print :DevicePreferences\" ~/Library/Preferences/com.apple.iphonesimulator.plist | "\
+                         "perl -lne 'print $1 if /^    (\\S*) =/' | while read -r a; do /usr/libexec/PlistBuddy "\
+                         "-c \"Set :DevicePreferences:$a:ConnectHardwareKeyboard false\" "\
+                         "~/Library/Preferences/com.apple.iphonesimulator.plist "\
+                         "|| /usr/libexec/PlistBuddy "\
+                         "-c \"Add :DevicePreferences:$a:ConnectHardwareKeyboard bool false\" "\
+                         "~/Library/Preferences/com.apple.iphonesimulator.plist; done", print: FastlaneCore::Globals.verbose?)
       end
     end
   end
