1666cc296efd54c28139f78f492baf618c2746ce
Josh Holtz <josh@rokkincat.com>
2018-07-13 07:15:56 -0500

    Added one more test

--- fastlane/spec/helper/podspec_helper_spec.rb
+++ fastlane/spec/helper/podspec_helper_spec.rb
@@ -47,6 +47,17 @@ describe Fastlane::Actions do
         expect(@version_podspec_file.version_match[:appendix]).to eq('.4')
       end
 
+      it "returns the current version once parsed with longer appendix" do
+        test_content = 'spec.version = "1.3.2.4.5"'
+        result = @version_podspec_file.parse(test_content)
+        expect(result).to eq('1.3.2.4.5')
+        expect(@version_podspec_file.version_value).to eq('1.3.2.4.5')
+        expect(@version_podspec_file.version_match[:major]).to eq('1')
+        expect(@version_podspec_file.version_match[:minor]).to eq('3')
+        expect(@version_podspec_file.version_match[:patch]).to eq('2')
+        expect(@version_podspec_file.version_match[:appendix]).to eq('.4.5')
+      end
+
       it "returns the current version once parsed with prerelease" do
         test_content = 'spec.version = "1.3.2-SNAPSHOT"'
         result = @version_podspec_file.parse(test_content)
