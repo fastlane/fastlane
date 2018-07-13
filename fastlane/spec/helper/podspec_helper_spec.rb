6d1d2ad312d3d27017696a6e9b03461693df2e24
Josh Holtz <josh@rokkincat.com>
2018-07-13 07:21:50 -0500

    Wrapped appendix in context

--- fastlane/spec/helper/podspec_helper_spec.rb
+++ fastlane/spec/helper/podspec_helper_spec.rb
@@ -36,26 +36,28 @@ describe Fastlane::Actions do
         expect(@version_podspec_file.version_match[:patch]).to eq('2')
       end
 
-      it "returns the current version once parsed with appendix" do
-        test_content = 'spec.version = "1.3.2.4"'
-        result = @version_podspec_file.parse(test_content)
-        expect(result).to eq('1.3.2.4')
-        expect(@version_podspec_file.version_value).to eq('1.3.2.4')
-        expect(@version_podspec_file.version_match[:major]).to eq('1')
-        expect(@version_podspec_file.version_match[:minor]).to eq('3')
-        expect(@version_podspec_file.version_match[:patch]).to eq('2')
-        expect(@version_podspec_file.version_match[:appendix]).to eq('.4')
-      end
+      context "with appendix" do
+        it "returns the current version once parsed with appendix" do
+          test_content = 'spec.version = "1.3.2.4"'
+          result = @version_podspec_file.parse(test_content)
+          expect(result).to eq('1.3.2.4')
+          expect(@version_podspec_file.version_value).to eq('1.3.2.4')
+          expect(@version_podspec_file.version_match[:major]).to eq('1')
+          expect(@version_podspec_file.version_match[:minor]).to eq('3')
+          expect(@version_podspec_file.version_match[:patch]).to eq('2')
+          expect(@version_podspec_file.version_match[:appendix]).to eq('.4')
+        end
 
-      it "returns the current version once parsed with longer appendix" do
-        test_content = 'spec.version = "1.3.2.4.5"'
-        result = @version_podspec_file.parse(test_content)
-        expect(result).to eq('1.3.2.4.5')
-        expect(@version_podspec_file.version_value).to eq('1.3.2.4.5')
-        expect(@version_podspec_file.version_match[:major]).to eq('1')
-        expect(@version_podspec_file.version_match[:minor]).to eq('3')
-        expect(@version_podspec_file.version_match[:patch]).to eq('2')
-        expect(@version_podspec_file.version_match[:appendix]).to eq('.4.5')
+        it "returns the current version once parsed with longer appendix" do
+          test_content = 'spec.version = "1.3.2.4.5"'
+          result = @version_podspec_file.parse(test_content)
+          expect(result).to eq('1.3.2.4.5')
+          expect(@version_podspec_file.version_value).to eq('1.3.2.4.5')
+          expect(@version_podspec_file.version_match[:major]).to eq('1')
+          expect(@version_podspec_file.version_match[:minor]).to eq('3')
+          expect(@version_podspec_file.version_match[:patch]).to eq('2')
+          expect(@version_podspec_file.version_match[:appendix]).to eq('.4.5')
+        end
       end
 
       it "returns the current version once parsed with prerelease" do
