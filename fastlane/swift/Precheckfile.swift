f55cfa0c7f85b9cc973a9c4bfc610507b3ee6013
Josh Holtz <josh@rokkincat.com>
2018-07-20 15:49:17 -0500

    Version bump to 2.100.1 (#12941)

--- fastlane/swift/Precheckfile.swift
+++ fastlane/swift/Precheckfile.swift
@@ -18,4 +18,4 @@ class Precheckfile: PrecheckfileProtocol {
 
 
 
-// Generated with fastlane 2.100.0
+// Generated with fastlane 2.100.1
--- fastlane/swift/Scanfile.swift
+++ fastlane/swift/Scanfile.swift
@@ -18,4 +18,4 @@ class Scanfile: ScanfileProtocol {
 
 
 
-// Generated with fastlane 2.100.0
+// Generated with fastlane 2.100.1
