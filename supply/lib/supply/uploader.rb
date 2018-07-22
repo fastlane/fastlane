7ae87c899788ee03cf30be30d5aa65de90365ab0
Takao Chiba <chibatching@users.noreply.github.com>
2018-07-23 08:41:02 +0900

    Fix issue that uploading aab with mapping file (#12856)
    * Fix issue that uploading aab with mapping file
    
    * Rename upload_binaries to upload_mapping
    
    * Move update_track method to outer upload_mapping

--- supply/lib/supply/uploader.rb
+++ supply/lib/supply/uploader.rb
@@ -23,8 +23,14 @@ module Supply
         end
       end
 
-      upload_binaries unless Supply.config[:skip_upload_apk]
-      upload_bundles unless Supply.config[:skip_upload_aab]
+      apk_version_codes = []
+      apk_version_codes.concat(upload_apks) unless Supply.config[:skip_upload_apk]
+      apk_version_codes.concat(upload_bundles) unless Supply.config[:skip_upload_aab]
+      upload_mapping(apk_version_codes)
+
+      # Only update tracks if we have version codes
+      # Updating a track with empty version codes can completely clear out a track
+      update_track(apk_version_codes) unless apk_version_codes.empty?
 
       promote_track if Supply.config[:track_promote_to]
 
@@ -123,7 +129,7 @@ module Supply
       end
     end
 
-    def upload_binaries
+    def upload_apks
       apk_paths = [Supply.config[:apk]] unless (apk_paths = Supply.config[:apk_paths])
       apk_paths.compact!
 
@@ -133,16 +139,16 @@ module Supply
         apk_version_codes.push(upload_binary_data(apk_path))
       end
 
+      return apk_version_codes
+    end
+
+    def upload_mapping(apk_version_codes)
       mapping_paths = [Supply.config[:mapping]] unless (mapping_paths = Supply.config[:mapping_paths])
       mapping_paths.zip(apk_version_codes).each do |mapping_path, version_code|
         if mapping_path
           client.upload_mapping(mapping_path, version_code)
         end
       end
-
-      # Only update tracks if we have version codes
-      # Updating a track with empty version codes can completely clear out a track
-      update_track(apk_version_codes) unless apk_version_codes.empty?
     end
 
     def upload_bundles
@@ -150,11 +156,7 @@ module Supply
       return unless aab_path
 
       UI.message("Preparing aab at path '#{aab_path}' for upload...")
-      apk_version_codes = [client.upload_bundle(aab_path)]
-
-      # Only update tracks if we have version codes
-      # Updating a track with empty version codes can completely clear out a track
-      update_track(apk_version_codes) unless apk_version_codes.empty?
+      return [client.upload_bundle(aab_path)]
     end
 
     private
