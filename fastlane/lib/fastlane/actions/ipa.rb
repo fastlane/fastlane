32509cf307f38fa8f747c7fe2549fa9513c9ef26
Jan Piotrowski <piotrowski@gmail.com>
2018-07-31 11:50:28 +0200

    add trailing slash to docs link

--- fastlane/lib/fastlane/actions/ipa.rb
+++ fastlane/lib/fastlane/actions/ipa.rb
@@ -232,7 +232,7 @@ module Fastlane
           "You are using legacy `shenzhen` to build your app, which will be removed soon!",
           "It is recommended to upgrade to _gym_.",
           "To do so, just replace `ipa(...)` with `gym(...)` in your Fastfile.",
-          "To make code signing work, follow [https://docs.fastlane.tools/codesigning/xcode-project](https://docs.fastlane.tools/codesigning/xcode-project)."
+          "To make code signing work, follow [https://docs.fastlane.tools/codesigning/xcode-project/](https://docs.fastlane.tools/codesigning/xcode-project/)."
         ].join("\n")
       end
     end
