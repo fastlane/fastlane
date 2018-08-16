9ae9c1c35b74134ecd0f6b7289a3939cb309aefb
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-16 20:53:48 +0200

    fix is_supported for get_build_nuber_repository

--- fastlane/lib/fastlane/actions/get_build_number_repository.rb
+++ fastlane/lib/fastlane/actions/get_build_number_repository.rb
@@ -103,7 +103,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        [:ios, :mac].include?(platform)
+        [:ios, :mac, :android].include?(platform)
       end
 
       def self.example_code
