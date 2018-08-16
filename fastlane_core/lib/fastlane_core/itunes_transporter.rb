7b324de33b5edead2fa9fad613aef7dd89381a87
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-17 00:54:07 +0200

    fix logic error in previous

--- fastlane_core/lib/fastlane_core/itunes_transporter.rb
+++ fastlane_core/lib/fastlane_core/itunes_transporter.rb
@@ -166,7 +166,7 @@ module FastlaneCore
         ENV["DELIVER_ITMSTRANSPORTER_ADDITIONAL_UPLOAD_PARAMETERS"], # that's here, because the user might overwrite the -t option
         "-t #{single_quotify('Signiant')}",
         "-k 100000",
-        ("-WONoPause true" unless Helper.windows?), # Windows only: process returns instead of waiting for key press
+        ("-WONoPause true" if Helper.windows?), # Windows only: process returns instead of waiting for key press
         ("-itc_provider #{provider_short_name}" unless provider_short_name.to_s.empty?)
       ].compact.join(' ')
     end
