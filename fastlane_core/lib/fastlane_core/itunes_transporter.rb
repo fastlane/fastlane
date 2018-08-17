8810751dd0de04f174b134b938f6bd773f16e4c3
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-17 12:57:52 +0200

    simplify initialize code

--- fastlane_core/lib/fastlane_core/itunes_transporter.rb
+++ fastlane_core/lib/fastlane_core/itunes_transporter.rb
@@ -316,12 +316,10 @@ module FastlaneCore
     #                            for more information about how to use the iTMSTransporter to list your provider
     #                            short names
     def initialize(user = nil, password = nil, use_shell_script = false, provider_short_name = nil)
+      # Xcode 6.x doesn't have the same iTMSTransporter Java setup as later Xcode versions, so
+      # we can't default to using the newer direct Java invocation strategy for those versions.
+      use_shell_script ||= Helper.is_mac? && Helper.xcode_version.start_with?('6.') 
       use_shell_script ||= Helper.windows?
-      if Helper.mac?
-        # Xcode 6.x doesn't have the same iTMSTransporter Java setup as later Xcode versions, so
-        # we can't default to using the better direct Java invocation strategy for those versions.
-        use_shell_script ||= Helper.xcode_version.start_with?('6.')
-      end
       use_shell_script ||= Feature.enabled?('FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT')
 
       @user = user
