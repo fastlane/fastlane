37d511edfe031a764a0eb71badd6d266c1bd7d90
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-28 00:33:57 +0200

    rubocop

--- fastlane_core/lib/fastlane_core/core_ext/shellwords.rb
+++ fastlane_core/lib/fastlane_core/core_ext/shellwords.rb
@@ -1,16 +1,16 @@
 class String
-    # CrossplatformShellwords
-    def shellescape
+  # CrossplatformShellwords
+  def shellescape
     CrossplatformShellwords.shellescape(self)
   end
 end
-  
+
 class Array
   def shelljoin
     CrossplatformShellwords.shelljoin(self)
   end
 end
-  
+
 module CrossplatformShellwords
   def shellescape(str)
     if FastlaneCore::Helper.windows?
@@ -20,23 +20,23 @@ module CrossplatformShellwords
     end
   end
   module_function :shellescape
-  
+
   def shelljoin(array)
     array.map { |arg| shellescape(arg) }.join(' ')
   end
   module_function :shelljoin
 end
-  
+
 module WindowsShellwords
   def shellescape(str)
     str = str.to_s
-  
+
     # An empty argument will be skipped, so return empty quotes.
     # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
     return '""'.dup if str.empty?
-  
+
     str = str.dup
-  
+
     # wrap in double quotes if contains space
     # then return (and skip Shellwords.escape)
     if str =~ /\s/
