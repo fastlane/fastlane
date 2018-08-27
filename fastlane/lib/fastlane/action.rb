a15df4f53159aa0de7d62ade1432d9a2f470eaa7
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-27 19:04:13 +0200

    also implement/monkey patch Array.shelljoin

--- fastlane/lib/fastlane/action.rb
+++ fastlane/lib/fastlane/action.rb
@@ -188,15 +188,33 @@ class String
     self.gsub!(/^#{first_line_indent}/, "")
   end
 
+  # FastlaneShellwords
+
+  #def shellsplit
+  #  FastlaneShellwords.split(self)
+  #end
+
   def shellescape
+    FastlaneShellwords.shellescape(self)
+  end
+end
+
+class Array
+  def shelljoin
+    FastlaneShellwords.shelljoin(self)
+  end
+end
+
+module FastlaneShellwords
+  def shellescape(str)
     if FastlaneCore::Helper.windows?
-      str = self.to_s
+      str = str.to_s
 
       # An empty argument will be skipped, so return empty quotes.
       # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
       return '""'.dup if str.empty?
 
-      str = self.dup
+      str = str.dup
 
       # wrap in double quotes if contains space
       # then return (and skip Shellwords.escape)
@@ -208,7 +226,23 @@ class String
         return str
       end
     else
-      return Shellwords.escape(self)
+      return Shellwords.escape(str)
     end
+  end
+
+  module_function :shellescape
+
+  class << self
+    alias escape shellescape
+  end
+
+  def shelljoin(array)
+    array.map { |arg| shellescape(arg) }.join(' ')
+  end
+
+  module_function :shelljoin
+
+  class << self
+    alias join shelljoin
   end
 end
