1e100f6f5e6eeb51301722c13932d262e5e67e69
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-28 15:49:38 +0200

    more and better tests

--- fastlane_core/spec/core_ext/shellwords_ext_spec.rb
+++ fastlane_core/spec/core_ext/shellwords_ext_spec.rb
@@ -4,15 +4,14 @@
 # https://github.com/ruby/ruby/blob/trunk/test/test_shellwords.rb
 # https://github.com/ruby/ruby/blob/trunk/spec/ruby/library/shellwords/shellwords_spec.rb
 
-# used to confirm that the escaped command that is generated actually
+# confirms that the escaped string that is generated actually
 # gets turned back into the source string by the actual shell.
 # abuses a `grep` error message because that should be cross platform
+# (I'm so sorry, but this actually works)
 def confirm_shell_unescapes_string_correctly(string, escaped)
+  string = simulate_normal_shell_unwrapping(string) unless FastlaneCore::Helper.windows?
   string = simulate_windows_shell_unwrapping(string) if FastlaneCore::Helper.windows?
-  string = simulate_other_shell_unwrapping(string) unless FastlaneCore::Helper.windows?
-
   compare_command = "grep 'foo' #{escaped}"
-  puts('execute command: ' + compare_command)
 
   # https://stackoverflow.com/a/18623297/252627, last variant
   require 'open3'
@@ -23,25 +22,26 @@ def confirm_shell_unescapes_string_correctly(string, escaped)
   end
 end
 
+# remove double quote pair
+# un-double-double quote resulting string
 def simulate_windows_shell_unwrapping(string)
   regex = /^"(([^"])(\S*)([^"]))"$/
   unless string.match(regex).nil?
-    puts('string before mod: ' + string)
     string = string.match(regex)[1] # get only part in quotes
     string.gsub!('""', '"') # remove double double quotes
-    puts('string after mod: ' + string)
   end
   return string
 end
 
-def simulate_other_shell_unwrapping(string)
+# remove all double quotes
+def simulate_normal_shell_unwrapping(string)
   string.gsub!('"', '')
   return string
 end
 
 # test Windows implementation directly
 describe "WindowsShellwords#shellescape" do
-  it "doesn't touch a simple string" do
+  it "on simple string: doesn't change it" do
     str = 'normal_string_without_spaces'
     escaped = WindowsShellwords.shellescape(str)
 
@@ -49,7 +49,7 @@ describe "WindowsShellwords#shellescape" do
     confirm_shell_unescapes_string_correctly(str, escaped)
   end
 
-  it "double quotes an empty string" do
+  it "on empty string: wraps it in double quotes" do
     str = ''
     escaped = WindowsShellwords.shellescape(str)
 
@@ -57,7 +57,7 @@ describe "WindowsShellwords#shellescape" do
     confirm_shell_unescapes_string_correctly(str, escaped)
   end
 
-  it "double quotes a string with spaces" do
+  it "on string with spaces: wraps it in double quotes" do
     str = 'string with spaces'
     escaped = WindowsShellwords.shellescape(str)
 
@@ -65,15 +65,7 @@ describe "WindowsShellwords#shellescape" do
     confirm_shell_unescapes_string_correctly(str, escaped)
   end
 
-  it "wraps in double quotes and double-double quotes a string with spaces and double quotes" do
-    str = 'string with spaces and "double" quotes'
-    escaped = WindowsShellwords.shellescape(str)
-
-    expect(escaped).to eq('"string with spaces and ""double"" quotes"')
-    confirm_shell_unescapes_string_correctly(str, escaped)
-  end
-
-  it "doesn't touch a simple string that starts and ends with double quotes" do
+  it "on simple string that is already wrapped in double quotes: doesn't touch it" do
     str = '"normal_string_without_spaces"'
     escaped = WindowsShellwords.shellescape(str)
 
@@ -81,19 +73,118 @@ describe "WindowsShellwords#shellescape" do
     confirm_shell_unescapes_string_correctly(str, escaped)
   end
 
-  it "wraps in double quotes and double-double quotes a string with spaces that is already wrapped in quotes" do
+  it "on string with spaces that is already wrapped in double quotes: wraps in double quotes and duplicates existing double quotes" do
     str = '"string with spaces already wrapped in double quotes"'
     escaped = WindowsShellwords.shellescape(str)
 
     expect(escaped).to eq('"""string with spaces already wrapped in double quotes"""')
     confirm_shell_unescapes_string_correctly(str, escaped)
   end
+
+  it "on string with spaces and double quotes: wraps in double quotes and duplicates existing double quotes" do
+    str = 'string with spaces and "double" quotes'
+    escaped = WindowsShellwords.shellescape(str)
+
+    expect(escaped).to eq('"string with spaces and ""double"" quotes"')
+    confirm_shell_unescapes_string_correctly(str, escaped)
+  end
 end
 
 describe "monkey patch of String.shellescape (via CrossplatformShellwords)" do
+  describe "on Windows" do
+    before(:each) do
+      allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+    end
+
+    it "on simple string: doesn't change it" do
+      str = 'normal_string_without_spaces'
+      escaped = str.shellescape
+      expect(escaped).to eq(str)
+    end
+
+    it "on empty string: wraps it in double quotes" do
+      str = ''
+      escaped = str.shellescape
+      expect(escaped).to eq('""')
+    end
+
+    it "on string with spaces: wraps it in double quotes" do
+      str = 'string with spaces'
+      escaped = str.shellescape
+      expect(escaped).to eq('"string with spaces"')
+    end
+
+    it "on simple string that is already wrapped in double quotes: doesn't touch it" do
+      str = '"normal_string_without_spaces"'
+      escaped = str.shellescape
+      expect(escaped).to eq('"normal_string_without_spaces"')
+    end
+
+    it "on string with spaces that is already wrapped in double quotes: wraps in double quotes and duplicates existing double quotes" do
+      str = '"string with spaces already wrapped in double quotes"'
+      escaped = str.shellescape
+      expect(escaped).to eq('"""string with spaces already wrapped in double quotes"""')
+    end
+
+    it "on string with spaces and double quotes: wraps in double quotes and duplicates existing double quotes" do
+      str = 'string with spaces and "double" quotes'
+      escaped = str.shellescape
+      expect(escaped).to eq('"string with spaces and ""double"" quotes"')
+    end
+  end
+
+  describe "on other OSs (macOS, Linux)" do
+    before(:each) do
+      allow(FastlaneCore::Helper).to receive(:windows?).and_return(false)
+    end
+
+    it "on simple string: doesn't change it" do
+      str = 'normal_string_without_spaces'
+      escaped = str.shellescape
+      expect(escaped).to eq(str)
+    end
+
+    it "on empty string: wraps it in single quotes" do
+      str = ''
+      escaped = str.shellescape 
+      expect(escaped).to eq('\'\'')
+    end
+
+    it "on string with spaces: escapes spaces with <backslash>" do
+      str = 'string with spaces'
+      escaped = str.shellescape
+      expect(escaped).to eq('string\ with\ spaces')
+    end
+
+    it "on simple string that is already wrapped in double quotes: escapes the double quotes with <backslash>" do
+      str = '"normal_string_without_spaces"'
+      escaped = str.shellescape
+      expect(escaped).to eq('\"normal_string_without_spaces\"')
+    end
+
+    it "on string with spaces that is already wrapped in double quotes: escapes the double quotes and spaces with <backslash>" do
+      str = '"string with spaces already wrapped in double quotes"'
+      escaped = str.shellescape
+      expect(escaped).to eq('\"string\ with\ spaces\ already\ wrapped\ in\ double\ quotes\"')
+    end
+
+    it "on string with spaces and double quotes: escapes the double quotes and spaces with <backslash>" do
+      str = 'string with spaces and "double" quotes'
+      escaped = str.shellescape
+      expect(escaped).to eq('string\ with\ spaces\ and\ \"double\"\ quotes')
+    end
+  end
 end
 
+# TODO: 
+# single quotes in string
+# \ in string
+# / in string
+# special characters in string
+# multi byte characters in string
+
 describe "monkey patch of Array.shelljoin (via CrossplatformShellwords)" do
+  # TODO
 end
 
 describe "monkey patch of Shellwords.shellescape" do
