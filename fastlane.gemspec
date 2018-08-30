d2ab429fb0cc3b1ecb367a6ce82a726edf73fba1
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-30 16:17:46 +0200

    update xcodeproj gem to 1.6.0 (#13253)
    * update xcodeproj to minimum 1.6.0
    
    * improve description of xcodeproj gem
    
    * undo #11897

--- fastlane.gemspec
+++ fastlane.gemspec
@@ -54,7 +54,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = Dir["*/lib"]
 
   spec.add_dependency('slack-notifier', '>= 2.0.0', '< 3.0.0') # Slack notifications
-  spec.add_dependency('xcodeproj', '>= 1.5.7', '< 2.0.0') # Needed for commit_version_bump action and gym code_signing_mapping
+  spec.add_dependency('xcodeproj', '>= 1.6.0', '< 2.0.0') # Modify Xcode projects
   spec.add_dependency('xcpretty', '~> 0.2.8') # prettify xcodebuild output
   spec.add_dependency('terminal-notifier', '>= 1.6.2', '< 2.0.0') # macOS notifications
   spec.add_dependency('terminal-table', '>= 1.4.5', '< 2.0.0') # Actions documentation
--- spec_helper.rb
+++ spec_helper.rb
@@ -89,9 +89,6 @@ RSpec.configure do |config|
     config.define_derived_metadata(:requires_security) do |meta|
       meta[:skip] = "Skipped: Requires `security` to be installed (which is not possible on this platform and no workaround has been implemented)"
     end
-    config.define_derived_metadata(:requires_xcodeproj) do |meta|
-      meta[:skip] = "Skipped: Requires `xcodeproj` which is unfortunately currently broken on Windows: https://github.com/CocoaPods/Xcodeproj/issues/549"
-    end
 
     # also skip `before()` for test groups that are skipped because of their tags.
     # only works for `describe` groups (that are parents of the `before`, not if the tag is set on `it`.
