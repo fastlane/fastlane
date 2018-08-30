18f317042543065fe5c8b3ef7e6598fc051533cd
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-30 16:18:10 +0200

    iTMSTransporter support on Windows (#13148)
    * define path of itms on Windows
    
    * define itms_path for Windows (C:/Program Files (x86)/itms)
    
    * clean up usage of FastlanePty.spawn
    
    * always use shell script on Windows
    
    * remove single quotes around the password
    
    *  fix ipa checksum generation on Windows
    
    * fix (single) quote usage on Windows
    also adapt tests
    
    * itms should return directly instead of waiting for key press (on Windows)
    
    * don't run itms tests that depend on xcode on platforms without Xcode
    
    * new tests with FASTLANE_ITUNES_TRANSPORTER_USE_SHELL_SCRIPT
    
    * new test that simulate being on Windows (even when not)
    
    * relax password regex as app specific password contain `-` by default
    
    * improve error output in case of 2 step verification problems
    
    * manual rubocop -a
    
    * fix broken exit_status checks
    
    * fix broken password check + make rubocop happy
    
    * rework tests to work on all OS
    
    * move return '' into else for itms_path
    
    * rubocop
    
    * exit_status == 0 => exit_status.zero?
    
    * remove trailing dot in UI output

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -193,22 +193,34 @@ module FastlaneCore
 
     # @return the full path to the iTMSTransporter executable
     def self.transporter_path
-      return File.join(self.itms_path, 'bin', 'iTMSTransporter')
+      return File.join(self.itms_path, 'bin', 'iTMSTransporter') unless Helper.windows?
+      return File.join(self.itms_path, 'iTMSTransporter')
     end
 
     # @return the full path to the iTMSTransporter executable
     def self.itms_path
       return ENV["FASTLANE_ITUNES_TRANSPORTER_PATH"] if FastlaneCore::Env.truthy?("FASTLANE_ITUNES_TRANSPORTER_PATH")
-      return '' unless self.mac? # so tests work on Linux and Windows too
 
-      [
-        "../Applications/Application Loader.app/Contents/MacOS/itms",
-        "../Applications/Application Loader.app/Contents/itms"
-      ].each do |path|
-        result = File.expand_path(File.join(self.xcode_path, path))
-        return result if File.exist?(result)
+      if self.mac?
+        [
+          "../Applications/Application Loader.app/Contents/MacOS/itms",
+          "../Applications/Application Loader.app/Contents/itms"
+        ].each do |path|
+          result = File.expand_path(File.join(self.xcode_path, path))
+          return result if File.exist?(result)
+        end
+        UI.user_error!("Could not find transporter at #{self.xcode_path}. Please make sure you set the correct path to your Xcode installation.")
+      elsif self.windows?
+        [
+          "C:/Program Files (x86)/itms"
+        ].each do |path|
+          return path if File.exist?(path)
+        end
+        UI.user_error!("Could not find transporter at usual locations. Please use environment variable `FASTLANE_ITUNES_TRANSPORTER_PATH` to specify your installation path.")
+      else
+        # not Mac or Windows
+        return ''
       end
-      UI.user_error!("Could not find transporter at #{self.xcode_path}. Please make sure you set the correct path to your Xcode installation.")
     end
 
     # keychain
