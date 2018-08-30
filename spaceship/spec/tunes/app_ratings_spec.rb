2b014cb21218697d3990b8913bcd6c634466c0e8
Josh Holtz <josh@rokkincat.com>
2018-08-30 11:10:25 -0500

    Added tests for new upto_date parameter

--- spaceship/spec/tunes/app_ratings_spec.rb
+++ spaceship/spec/tunes/app_ratings_spec.rb
@@ -42,6 +42,29 @@ describe Spaceship::Tunes::AppRatings do
       expect(reviews.first.last_modified).to eq(1_463_887_020_000)
       expect(reviews.first.nickname).to eq("Reviewer1")
     end
+
+    it "contains the right information with upto_date" do
+      TunesStubbing.itc_stub_ratings
+      ratings = app.ratings
+      reviews = ratings.reviews("US", "", "2016-03-27")
+
+      expect(reviews.count).to eq(2)
+      expect(reviews.first.store_front).to eq("NZ")
+      expect(reviews.first.id).to eq(1_000_000_000)
+      expect(reviews.first.rating).to eq(2)
+      expect(reviews.first.title).to eq("Title 1")
+      expect(reviews.first.review).to eq("Review 1")
+      expect(reviews.first.last_modified).to eq(1_463_887_020_000)
+      expect(reviews.first.nickname).to eq("Reviewer1")
+      
+      expect(reviews.last.store_front).to eq("NZ")
+      expect(reviews.last.id).to eq(1_000_000_001)
+      expect(reviews.last.rating).to eq(2)
+      expect(reviews.last.title).to eq("Title 2")
+      expect(reviews.last.review).to eq("Review 2")
+      expect(reviews.last.last_modified).to eq(1_459_152_540_000)
+      expect(reviews.last.nickname).to eq("Reviewer2")
+    end
   end
 
   describe "Manages Developer Response" do
