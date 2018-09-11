e8787caf698667922ee061a0053b8f622b693057
Joshua Liebowitz <taquitos@users.noreply.github.com>
2018-09-11 10:18:24 -0400

    Report fastlane.swift usage vs normal fastlane (ruby) usage (#13333)
    * Report fastlane.swift usage vs normal fastlane (ruby) usage
    
    We want to understand how many folks are using fastlane.swift, so we need to report execution of fastlane.swift vs normal fastlane.
    Also includes a tiny bit of cleanup.
    
    * Make category more friendly
    
    as described in best practices for Category: https://support.google.com/analytics/answer/1033068?hl=en

--- fastlane_core/lib/fastlane_core/analytics/analytics_event_builder.rb
+++ fastlane_core/lib/fastlane_core/analytics/analytics_event_builder.rb
@@ -2,16 +2,18 @@ module FastlaneCore
   class AnalyticsEventBuilder
     attr_accessor :action_name
 
-    def initialize(p_hash: nil, session_id: nil, action_name: nil)
+    # fastlane_client_language valid options are :ruby or :swift
+    def initialize(p_hash: nil, session_id: nil, action_name: nil, fastlane_client_language: :ruby)
       @p_hash = p_hash
       @session_id = session_id
       @action_name = action_name
+      @fastlane_client_language = fastlane_client_language
     end
 
     def new_event(action_stage)
       {
         client_id: @p_hash,
-        category: :undefined,
+        category: "fastlane Client Langauge - #{@fastlane_client_language}",
         action: action_stage,
         label: action_name,
         value: nil
--- fastlane_core/spec/analytics/analytics_event_builder_spec.rb
+++ fastlane_core/spec/analytics/analytics_event_builder_spec.rb
@@ -11,13 +11,48 @@ describe FastlaneCore::AnalyticsEventBuilder do
     )
   end
 
+  let(:swift_builder) do
+    FastlaneCore::AnalyticsEventBuilder.new(
+      p_hash: p_hash,
+      session_id: session_id,
+      action_name: action_name,
+      fastlane_client_language: :swift
+    )
+  end
+
   context '#launch_event' do
-    it 'creates a launch event' do
+    it 'creates a default launch event' do
       event = builder.new_event(:launch)
       expect(event).to eq(
         {
           client_id: p_hash,
-          category: :undefined,
+          category: 'fastlane Client Langauge - ruby',
+          action: :launch,
+          label: action_name,
+          value: nil
+        }
+      )
+    end
+
+    it 'creates a ruby launch event' do
+      event = builder.new_event(:launch)
+      expect(event).to eq(
+        {
+          client_id: p_hash,
+          category: 'fastlane Client Langauge - ruby',
+          action: :launch,
+          label: action_name,
+          value: nil
+        }
+      )
+    end
+
+    it 'creates a swift launch event' do
+      event = swift_builder.new_event(:launch)
+      expect(event).to eq(
+        {
+          client_id: p_hash,
+          category: 'fastlane Client Langauge - swift',
           action: :launch,
           label: action_name,
           value: nil
