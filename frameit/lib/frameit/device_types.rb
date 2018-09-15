4a50c7c3645a446205d99781da036e8d1d5373b3
Mirko Tomic <mirko@m-quart.com>
2018-09-15 15:48:18 +0200

    Add XR color variations

--- frameit/lib/frameit/device_types.rb
+++ frameit/lib/frameit/device_types.rb
@@ -4,6 +4,11 @@ module Frameit
     SILVER = "Silver"
     ROSE_GOLD = "Rose Gold"
     GOLD = "Gold"
+    RED = "Red"
+    YELLOW = "Yellow"
+    WHITE = "White"
+    CORAL = "Coral"
+    BLUE = "Blue"
   end
 
   module Orientation
