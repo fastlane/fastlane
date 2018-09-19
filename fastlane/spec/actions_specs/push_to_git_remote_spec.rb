9b9deea181cebd63b14edd0db9a59c2d6e7e5b49
Prabhuanand Sivashanmugam <prabhu@mindstronghealth.com>
2018-09-19 03:26:45 -0700

    [action] Add force-with-lease to push_to_git_remote (#13098)
    * Adding support for --force-with-lease
    
    * Correcting whitespace error for force-with-lease
    
    * Replaced is_string:false with type:Boolean and added tests

---
+++ fastlane/spec/actions_specs/push_to_git_remote_spec.rb
@@ -0,0 +1,78 @@
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "Git Push to Remote Action" do
+      before(:each) do
+        allow(Fastlane::Actions).to receive(:git_branch).and_return("master")
+      end
+
+      it "runs git push with defaults" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin master:master --tags")
+      end
+
+      it "run git push with local_branch" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              local_branch: 'staging'
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin staging:staging --tags")
+      end
+
+      it "run git push with local_branch and remote_branch" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              local_branch: 'staging',
+              remote_branch: 'production'
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin staging:production --tags")
+      end
+
+      it "runs git push with tags:false" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              tags: false
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin master:master")
+      end
+
+      it "runs git push with force:true" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              force: true
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin master:master --tags --force")
+      end
+
+      it "runs git push with force_with_lease:true" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              force_with_lease: true
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push origin master:master --tags --force-with-lease")
+      end
+
+      it "runs git push with remote" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+            push_to_git_remote(
+              remote: 'not_github'
+            )
+          end").runner.execute(:test)
+
+        expect(result).to eq("git push not_github master:master --tags")
+      end
+    end
+  end
+end
