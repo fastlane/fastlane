f5b9170ff7f85638c08f54a4df6c081fa99e94c6
Jan Piotrowski <piotrowski+github@gmail.com>
2018-09-28 19:47:40 +0200

    Replace Shellwords.*(var) with var.* (#13467)
    <!-- Thanks for contributing to _fastlane_! Before you submit your pull request, please make sure to check the following boxes by putting an x in the [ ] (don't: [x ], [ x], do: [x]) -->
    
    ### Checklist
    - [x] I've run `bundle exec rspec` from the root directory to see all new and existing tests pass
    - [x] I've followed the _fastlane_ code style and run `bundle exec rubocop -a` to ensure the code style is valid
    - [x] I've read the [Contribution Guidelines](https://github.com/fastlane/fastlane/blob/master/CONTRIBUTING.md)
    - [x] I've updated the documentation if necessary.
    
    ### Motivation and Context
    Some places still used `Shellwords.*(var)` instead of the usual `var.*` we are using.
    
    ### Description
    Fixed those.

--- fastlane_core/lib/fastlane_core/device_manager.rb
+++ fastlane_core/lib/fastlane_core/device_manager.rb
@@ -292,7 +292,7 @@ module FastlaneCore
       def copy_logarchive(device, log_identity, logs_destination_dir)
         require 'shellwords'
 
-        logarchive_dst = Shellwords.escape(File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive"))
+        logarchive_dst = File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive").shellescape
         FileUtils.rm_rf(logarchive_dst)
         FileUtils.mkdir_p(File.expand_path("..", logarchive_dst))
         command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst} 2>/dev/null"
