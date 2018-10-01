cb624d5c29eab83a348461b7436a62b3d98ceeb5
Jan Piotrowski <piotrowski+github@gmail.com>
2018-10-01 11:01:29 +0200

    Fix ROOT path after moving code from fastlane.rb to fastlane/module.rb

--- fastlane/lib/fastlane/module.rb
+++ fastlane/lib/fastlane/module.rb
@@ -7,7 +7,7 @@ require_relative internal('fastlane_core/fastlane_folder')
 module Fastlane
   Helper = FastlaneCore::Helper # you gotta love Ruby: Helper.* should use the Helper class contained in FastlaneCore
   UI = FastlaneCore::UI
-  ROOT = Pathname.new(File.expand_path('../..', __FILE__))
+  ROOT = Pathname.new(File.expand_path('../../..', __FILE__))
 
   class << self
     def load_actions
