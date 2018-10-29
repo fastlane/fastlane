1bb7925952debc460d8a0f41afe807ed6e308e1c
Jan Piotrowski <piotrowski+github@gmail.com>
2018-10-29 13:19:17 +0100

    [action] Integrate Managed Google Play with get_managed_play_store_publishing_rights and create_app_on_managed_play_store (#13421)
    * Move over actions from plugin with minimal changes
    
    * create empty spec files for actions
    
    * remove unneeded/broken require
    
    * update author
    
    * return URI in get_managedplay_store_publishing_rights
    
    * make clear the details will be filled
    
    * add action category
    
    * make clear that URL will change
    
    * example_code for get_managed_play_store_publishing_rights
    
    * include documentation from README into details of get_managed_play_store_publishing_rights
    
    * add description and example_code for create_app_on_managed_play_store
    
    * better example
    
    * fix code example
    
    * remove root_url and timeout for now
    
    * remove TODOs that were added to PR
    
    * replace callback_uri with probable future URL
    
    * rubocop
    
    * rubocop 2
    
    * Refactor Supply::Client to be usable by create_app_on_managed_play_store (#13431)
    
    * fix order in verify_block for Supply's json_key:
    check if file exists first, then if it is JSON
    
    * Extract reusable parts of Client into GoogleServiceClient (self.make_from_config, initialize, call_google_api)
    
    * Modify create_app_on_managed_play_store to implement and use Supply::GoogleServiceClient
    
    * remove leftover code and TODOs
    rubocop
    
    * skip fastlane/spec/unused_options_spec.rb for new action
    
    * remove TODO
    
    * Rename Supply::GoogleServiceClient to Supply::AbstractGoogleServiceClient
    
    * replce @scope/@service with SCOPE/SERVICE and use self.class::SCOPE/SERVICE to read it
    https://stackoverflow.com/questions/19382626/how-do-i-override-a-variable-in-a-ruby-subclass-without-affecting-the-superclass/19382692#19382692
    
    * workaround for `warning: already initialized constant Supply::UI`
    
    * get rid of more code by moving client/android_publisher to AbstractGoogleServiceClient as client
    (and also get rid of empty method implementations in PlaycustomappClient)
    
    * move .new into initialize()
    
    * Comment and environment variable name cleanup
    
    * Moved some requires around
    
    * Cleaned up some options on create_app_on_managed_play_store
    
    * get_managed_play_store_publishing_rights works with json_key_data
    
    * Rubo
    
    * Added tests for get_managed_play_store_publishing_rights
    
    * Rubo
    
    * Removed unsured before in test
    
    * Added some testing to create_app_on_managed_play_store
    
    * Recommit to run tests maybe
    
    * Moved languages into supply
    
    * Remove commented code

--- supply/lib/supply/options.rb
+++ supply/lib/supply/options.rb
@@ -69,8 +69,8 @@ module Supply
                                      default_value: CredentialsManager::AppfileConfig.try_fetch_value(:json_key_file),
                                      default_value_dynamic: true,
                                      verify_block: proc do |value|
-                                       UI.user_error!("'#{value}' doesn't seem to be a JSON file") unless FastlaneCore::Helper.json_file?(File.expand_path(value))
                                        UI.user_error!("Could not find service account json file at path '#{File.expand_path(value)}'") unless File.exist?(File.expand_path(value))
+                                       UI.user_error!("'#{value}' doesn't seem to be a JSON file") unless FastlaneCore::Helper.json_file?(File.expand_path(value))
                                      end),
         FastlaneCore::ConfigItem.new(key: :json_key_data,
                                      env_name: "SUPPLY_JSON_KEY_DATA",
