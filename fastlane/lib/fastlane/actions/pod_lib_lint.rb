1e7b57dabda3390f6a9820d00880883592b0da13
Josh Holtz <josh@rokkincat.com>
2018-11-02 10:46:00 -0500

    Updated is_string to true for pod_lib_lint swift_version
    Co-Authored-By: bdrelling <bdrelling@users.noreply.github.com>

--- fastlane/lib/fastlane/actions/pod_lib_lint.rb
+++ fastlane/lib/fastlane/actions/pod_lib_lint.rb
@@ -73,7 +73,7 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :swift_version,
                                        description: "The SWIFT_VERSION that should be used to lint the spec. This takes precedence over a .swift-version file",
                                        optional: true,
-                                       is_string: false),
+                                       is_string: true),
           FastlaneCore::ConfigItem.new(key: :use_libraries,
                                        description: "Lint uses static libraries to install the spec",
                                        is_string: false,
