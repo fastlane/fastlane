0999b8a99943d355fb1a013319bec3b696fb623f
Manu Wallner <wallman@amazon.com>
2018-11-05 14:29:55 -0800

    Refactor to storage/encryption interfaces
    - Make it possible to dynamically change backends, e.g. so that plugins can provide their own
    - Refactor storage interface so common operations on file upload aren’t repeated

--- match/lib/match/storage/interface.rb
+++ match/lib/match/storage/interface.rb
@@ -1,6 +1,8 @@
 module Match
   module Storage
     class Interface
+      MATCH_VERSION_FILE_NAME = "match_version.txt"
+
       # The working directory in which we download all the profiles
       # and decrypt/encrypt them
       attr_accessor :working_directory
@@ -37,7 +39,41 @@ module Match
       #   that should be committed to the storage provider
       # @parameter custom_message: [String] Custom change message
       #           that's optional, is used for commit title
-      def save_changes!(files_to_commit: [], custom_message: nil)
+      def save_changes!(files_to_commit: [], files_to_delete: [], custom_message: nil)
+        Dir.chdir(File.expand_path(self.working_directory)) do
+          if files_to_commit.count > 0 # everything that isn't `match nuke`
+            UI.user_error!("You can't provide both `files_to_delete` and `files_to_commit` right now") if files_to_delete.count > 0
+
+            if !File.exist?(MATCH_VERSION_FILE_NAME) || File.read(MATCH_VERSION_FILE_NAME) != Fastlane::VERSION.to_s
+              files_to_commit << MATCH_VERSION_FILE_NAME
+              File.write(MATCH_VERSION_FILE_NAME, Fastlane::VERSION) # stored unencrypted
+            end
+
+            template = File.read("#{Match::ROOT}/lib/assets/READMETemplate.md")
+            readme_path = "README.md"
+            if (!File.exist?(readme_path) || File.read(readme_path) != template) && !self.skip_docs
+              files_to_commit << readme_path
+              File.write(readme_path, template)
+            end
+
+            self.upload_files(files_to_upload: files_to_commit, custom_message: custom_message)
+          elsif files_to_delete.count > 0
+            self.delete_files(files_to_delete: files_to_delete, custom_message: custom_message)
+          else
+            UI.user_error!("Neither `files_to_commit` nor `files_to_delete` were provided to the `save_changes!` method call")
+          end
+        end
+      end
+
+      def upload_files(files_to_upload: [], custom_message: nil)
+        not_implemented(__method__)
+      end
+
+      def delete_files(files_to_delete: [], custom_message: nil)
+        not_implemented(__method__)
+      end
+
+      def skip_docs
         not_implemented(__method__)
       end
 
