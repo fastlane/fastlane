e6057d37cf11fc6ff09c26f49880c27a51220e8d
Leonardo Formaggio <leoformaggio@gmail.com>
2018-11-07 19:05:34 -0800

    Fixing format

--- fastlane/spec/actions_specs/request_reviewers_spec.rb
+++ fastlane/spec/actions_specs/request_reviewers_spec.rb
@@ -2,12 +2,12 @@ describe Fastlane do
   describe Fastlane::FastFile do
     describe 'request_reviewers' do
       let(:response_body) { File.read('./fastlane/spec/fixtures/requests/github_request_reviewers_response.json') }
-      
+
       context 'required data is sent' do
         before do
-          stub_request(:post, 'https://api.github.com/repos/octocat/Hello-World/pulls/1347/requested_reviewers').
-          with(body: '{"reviewers":["octocat","hubot","other_user"],"team_reviewers":["justice-league"]}').
-          to_return(status: 201, body: response_body)
+          stub_request(:post, 'https://api.github.com/repos/octocat/Hello-World/pulls/1347/requested_reviewers')
+            .with(body: '{"reviewers":["octocat","hubot","other_user"],"team_reviewers":["justice-league"]}')
+            .to_return(status: 201, body: response_body)
         end
 
         context 'passing all parameters' do
@@ -20,13 +20,13 @@ describe Fastlane do
               team_reviewers: ['justice-league']
             )
           end"
-  
+
           it 'correctly submits to github' do
             result = Fastlane::FastFile.new.parse(data).runner.execute(:test)
             expect(result).to eq('https://github.com/octocat/Hello-World/pull/1347')
           end
         end
-  
+
         context 'from pull request page' do
           data = "lane :test do
             lane_context[SharedValues::CREATE_PULL_REQUEST_HTML_URL] = 'https://github.com/octocat/Hello-World/pull/1347'
@@ -36,7 +36,7 @@ describe Fastlane do
               team_reviewers: ['justice-league']
             )
           end"
-  
+
           it 'correctly submits to github' do
             result = Fastlane::FastFile.new.parse(data).runner.execute(:test)
             expect(result).to eq('https://github.com/octocat/Hello-World/pull/1347')
@@ -47,9 +47,9 @@ describe Fastlane do
       context 'required data is not sent' do
         context 'passing the parameters' do
           before do
-            stub_request(:post, 'https://api.github.com/repos/octocat/Hello-World/pulls/1347/requested_reviewers').
-            with(body: '{"reviewers":[],"team_reviewers":[]}').
-            to_return(status: 422)
+            stub_request(:post, 'https://api.github.com/repos/octocat/Hello-World/pulls/1347/requested_reviewers')
+              .with(body: '{"reviewers":[],"team_reviewers":[]}')
+              .to_return(status: 422)
           end
 
           data = "lane :test do
@@ -59,7 +59,7 @@ describe Fastlane do
               number: 1347
             )
           end"
-  
+
           it 'fails to submit to github' do
             result = Fastlane::FastFile.new.parse(data).runner.execute(:test)
             expect(result).to be(nil)
@@ -68,9 +68,9 @@ describe Fastlane do
 
         context 'from pull request page' do
           before do
-            stub_request(:post, 'https://api.github.com/repos//pulls//requested_reviewers').
-            with(body: '{"reviewers":[],"team_reviewers":[]}').
-            to_return(status: 404)
+            stub_request(:post, 'https://api.github.com/repos//pulls//requested_reviewers')
+              .with(body: '{"reviewers":[],"team_reviewers":[]}')
+              .to_return(status: 404)
           end
 
           data = "lane :test do
@@ -78,7 +78,7 @@ describe Fastlane do
               api_token: '123456789'
             )
           end"
-  
+
           it 'fails to submit to github' do
             result = Fastlane::FastFile.new.parse(data).runner.execute(:test)
             expect(result).to be(nil)
