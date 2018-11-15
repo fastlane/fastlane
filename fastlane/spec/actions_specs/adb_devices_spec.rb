da7a0e5e1cd0947ea906ee1fbbaadeb4b74eb8c8
Jan Piotrowski <piotrowski+git@gmail.com>
2018-11-15 22:31:05 +0100

    fix test description of adb_devices

--- fastlane/spec/actions_specs/adb_devices_spec.rb
+++ fastlane/spec/actions_specs/adb_devices_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "adb_devices" do
-      it "generates a valid command" do
+      it "generates an empty list of devices" do
         result = Fastlane::FastFile.new.parse("lane :test do
           adb_devices(adb_path: '/some/path/to/adb')
         end").runner.execute(:test)
@@ -9,7 +9,7 @@ describe Fastlane do
         expect(result).to match_array([])
       end
 
-      it "generates a valid command when ANDROID_SDK_ROOT is set" do
+      it "generates an empty list of devices when ANDROID_SDK_ROOT is set" do
         result = Fastlane::FastFile.new.parse("lane :test do
           ENV['ANDROID_SDK_ROOT'] = '/usr/local/android-sdk'
           adb_devices()
