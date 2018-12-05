7fb17dfbab6c1801c6caba9329f2bf041f5c6f77
Josh Holtz <josh@rokkincat.com>
2018-12-04 23:25:20 -0600

    Fix produce tests

--- produce/spec/options_spec.rb
+++ produce/spec/options_spec.rb
@@ -1,5 +1,13 @@
 describe Produce do
   describe Produce::Options do
+    before(:each) do
+      ENV.delete('FASTLANE_TEAM_ID')
+    end
+
+    after(:each) do
+      ENV.delete('FASTLANE_TEAM_ID')
+    end
+
     describe ":itc_team_id" do
       it "accepts String" do
         options = { itc_team_id: "1234" }
