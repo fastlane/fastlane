af172a7ee07097813e270868d81f3ce8cfda4cf0
Jan Piotrowski <piotrowski+github@gmail.com>
2018-12-07 21:17:48 +0100

    Update fastlane_folder.rb

--- fastlane_core/lib/fastlane_core/fastlane_folder.rb
+++ fastlane_core/lib/fastlane_core/fastlane_folder.rb
@@ -28,19 +28,19 @@ module FastlaneCore
       fastfile_exists_in_fastlane_folder = File.exist?('./#{FOLDER_NAME}/Fastfile')
       puts("3 path: Dir.getwd = '#{Dir.getwd}' - fastfile_exists_in_fastlane_folder =              '#{fastfile_exists_in_fastlane_folder}' (File.exist?(./FOLDER_NAME/Fastfile))")
       
-      puts("3 path: 1: Dir.getwd = '#{Dir.getwd}' - ''")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 1: ''")
       value ||= "./#{FOLDER_NAME}/" if fastlane_folder_is_directory
-      puts("3 path: 2: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 2: '#{value}'")
       value ||= "./.#{FOLDER_NAME}/" if File.directory?("./.#{FOLDER_NAME}/") # hidden folder
-      puts("3 path: 3: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 3: '#{value}'")
       value ||= "./" if File.basename(Dir.getwd) == FOLDER_NAME && File.exist?('Fastfile.swift') # inside the folder
-      puts("3 path: 4: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 4: '#{value}'")
       value ||= "./" if File.basename(Dir.getwd) == ".#{FOLDER_NAME}" && File.exist?('Fastfile.swift') # inside the folder and hidden
-      puts("3 path: 5: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 5: '#{value}'")
       value ||= "./" if File.basename(Dir.getwd) == FOLDER_NAME && File.exist?('Fastfile') # inside the folder
-      puts("3 path: 6: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 6: '#{value}'")
       value ||= "./" if File.basename(Dir.getwd) == ".#{FOLDER_NAME}" && File.exist?('Fastfile') # inside the folder and hidden
-      puts("3 path: 7: Dir.getwd = '#{Dir.getwd}' - '#{value}'")
+      puts("3 path: Dir.getwd = '#{Dir.getwd}' - 7: '#{value}'")
       
       puts("3 path: Dir.getwd = '#{Dir.getwd}' - END value = '#{value}'")
       puts("")
