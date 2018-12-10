214ad2cf721d38ecda9d2181b5f5557b02a538d5
RishabhTayal <rtayal11@gmail.com>
2018-12-10 11:39:55 -0600

    Update ensure_bundle_exec.rb

--- fastlane/lib/fastlane/actions/ensure_bundle_exec.rb
+++ fastlane/lib/fastlane/actions/ensure_bundle_exec.rb
@@ -6,7 +6,24 @@ module Fastlane
     # Raises an exception and stop the lane execution if the repo is not on a specific branch
     class EnsureBundleExecAction < Action
       def self.run(params)
-       
+
+        # gemfile_path = PluginManager.new.gemfile_path
+        # if gemfile_path
+
+          if !FastlaneCore::Helper.bundler?
+            UI.user_error!("Not using bundled fastlane")
+          end
+        
+        #   # The user has a Gemfile, but forgot to use `bundle exec`
+        #   # Let's tell the user how to use `bundle exec`
+        #   # We show this warning no matter if the command is slow or not
+        #   UI.important("fastlane detected a Gemfile in the current directory")
+        #   UI.important("however it seems like you don't use `bundle exec`")
+        #   UI.important("to launch fastlane faster, please use")
+        #   UI.message("")
+        #   UI.command "bundle exec fastlane #{ARGV.join(' ')}"
+        #   UI.message("")
+        # end
       end
 
       #####################################################
