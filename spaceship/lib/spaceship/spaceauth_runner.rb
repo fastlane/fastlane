ebbcd86615a790546033e2d2b8bb3104633fce2c
RishabhTayal <rtayal11@gmail.com>
2018-12-10 14:27:27 -0600

    :cop:

--- spaceship/lib/spaceship/spaceauth_runner.rb
+++ spaceship/lib/spaceship/spaceauth_runner.rb
@@ -49,11 +49,10 @@ module Spaceship
 
       yaml = cookies.to_yaml.gsub("\n", "\\n")
 
-
       if FastlaneCore::Helper.mac? && Spaceship::Client::UserInterface.interactive? && agree("🙄 should fastlane put cookie into your clipboard, so you can easily paste it? (y/n)", true)
         require 'open3'
         Open3.popen3('pbcopy') { |input, _, _| input << yaml }
-        puts("Successfully copied text into your clipboard 🎨".green) 
+        puts("Successfully copied text into your clipboard 🎨".green)
       end
 
       puts("---")
