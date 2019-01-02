4915175c89d097ae20ef8e7fbb1b2a324d93f675
Josh Holtz <josh@rokkincat.com>
2019-01-02 13:35:27 -0600

    [match] fix change password to add files and fix breaking change on cert import (#13983)
    * set the partition thing to only the imported keys
    
    * add more descriptive error message, pointing the user to a possible solutin
    
    * [match] fix change password to add files and fix breaking change on cert import
    
    * Updated some comment words
    
    * Added new line for easier reading
    
    * Fix tests
    
    * Fixed tests agin for real

--- fastlane_core/lib/fastlane_core/keychain_importer.rb
+++ fastlane_core/lib/fastlane_core/keychain_importer.rb
@@ -8,7 +8,7 @@ module FastlaneCore
 
       command = "security import #{path.shellescape} -k '#{keychain_path.shellescape}'"
       command << " -P #{certificate_password.shellescape}"
-      command << " -T /usr/bin/codesign" # to not be asked for permission when running a tool like `gym`
+      command << " -T /usr/bin/codesign" # to not be asked for permission when running a tool like `gym` (before Sierra)
       command << " -T /usr/bin/security"
       command << " &> /dev/null" unless output
 
@@ -19,17 +19,22 @@ module FastlaneCore
       if Helper.backticks('security -h | grep set-key-partition-list', print: false).length > 0
         command = "security set-key-partition-list"
         command << " -S apple-tool:,apple:"
+        command << " -l 'Imported Private Key'"
         command << " -k #{keychain_password.to_s.shellescape}"
         command << " #{keychain_path.shellescape}"
+        command << " &> /dev/null" # always disable stdout. This can be very verbose, and leak potentially sensitive info
 
+        UI.command(command) if output
         Open3.popen3(command) do |stdin, stdout, stderr, thrd|
-          if output
-            UI.command(command)
-            UI.command_output(stdout.read)
-          end
-
           unless thrd.value.success?
-            UI.user_error!("Could not configure key to bypass permission popup:\n#{stderr.read}")
+            UI.error("")
+            UI.error("Could not configure imported keychain item (certificate) to prevent UI permission popup when code signing\n" \
+                     "Check if you supplied the correct `keychain_password` for keychain: `#{keychain_path}`\n" \
+                     "#{stderr.read.to_s.strip}")
+            UI.error("")
+            UI.error("Please look at the following docs to see how to set a keychain password:")
+            UI.error(" - https://docs.fastlane.tools/actions/sync_code_signing")
+            UI.error(" - https://docs.fastlane.tools/actions/get_certificates")
           end
         end
       end
