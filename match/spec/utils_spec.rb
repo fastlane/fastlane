adda8881139637f5d3cb15ce8af5ddbadf08dc91
Josh Holtz <josh@rokkincat.com>
2019-01-02 11:24:26 -0600

    Fixed tests agin for real

--- match/spec/utils_spec.rb
+++ match/spec/utils_spec.rb
@@ -9,7 +9,7 @@ describe Match do
         expected_command = "security import item.path -k '#{Dir.home}/Library/Keychains/login.keychain' -P #{''.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
 
         # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
-        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k #{''.shellescape} #{Dir.home}/Library/Keychains/login.keychain"
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -l 'Imported Private Key' -k #{''.shellescape} #{Dir.home}/Library/Keychains/login.keychain &> /dev/null"
 
         allow(File).to receive(:file?).and_return(false)
         expect(File).to receive(:file?).with("#{Dir.home}/Library/Keychains/login.keychain").and_return(true)
@@ -28,7 +28,7 @@ describe Match do
         expected_command = "security import item.path -k '#{keychain}' -P #{''.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
 
         # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
-        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k #{''.shellescape} #{keychain}"
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -l 'Imported Private Key' -k #{''.shellescape} #{keychain} &> /dev/null"
 
         allow(File).to receive(:file?).and_return(false)
         expect(File).to receive(:file?).with(keychain).and_return(true)
@@ -53,7 +53,7 @@ describe Match do
         expected_command = "security import item.path -k '#{Dir.home}/Library/Keychains/login.keychain-db' -P #{''.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null"
 
         # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
-        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -k #{''.shellescape} #{Dir.home}/Library/Keychains/login.keychain-db"
+        allowed_command = "security set-key-partition-list -S apple-tool:,apple: -l 'Imported Private Key' -k #{''.shellescape} #{Dir.home}/Library/Keychains/login.keychain-db &> /dev/null"
 
         allow(File).to receive(:file?).and_return(false)
         expect(File).to receive(:file?).with("#{Dir.home}/Library/Keychains/login.keychain-db").and_return(true)
