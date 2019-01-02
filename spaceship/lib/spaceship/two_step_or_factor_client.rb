bb60ac0581fd70fce2df6061bf01c7c50fdea827
Jan Piotrowski <piotrowski+git@gmail.com>
2019-01-02 15:04:07 +0100

    rework 2fa method to also work with invalid code and ask again already adds methods to handle sms, but doesn't call them yet

--- spaceship/lib/spaceship/two_step_or_factor_client.rb
+++ spaceship/lib/spaceship/two_step_or_factor_client.rb
@@ -115,12 +115,16 @@ module Spaceship
       return true
     end
 
-    def handle_two_factor(response)
-      two_factor_url = "https://github.com/fastlane/fastlane/tree/master/spaceship#2-step-verification"
-      puts("Two-factor Authentication (6 digits code) is enabled for account '#{self.user}'")
+    def handle_two_factor(response, depth = 0)
+      if depth == 0
+        puts("Two-factor Authentication (6 digits code) is enabled for account '#{self.user}'")
 
-      puts("If you're running this in a non-interactive session (e.g. server or CI)")
-      puts("check out #{two_factor_url}")
+        two_factor_url = "https://github.com/fastlane/fastlane/tree/master/spaceship#2-step-verification"
+        puts("If you're running this in a non-interactive session (e.g. server or CI)")
+        puts("check out #{two_factor_url}")
+      end
+
+      # "verification code" has already be pushed to devices
 
       security_code = response.body["securityCode"]
       # "securityCode": {
@@ -130,7 +134,8 @@ module Spaceship
       # 	"securityCodeLocked": false
       # },
       code_length = security_code["length"]
-      code = ask("Please enter the #{code_length} digit code: ")
+      puts("")
+      code = ask("Please enter the #{code_length} digit code:")
       puts("Requesting session...")
 
       # Send "verification code" back to server to get a valid session
@@ -138,7 +143,61 @@ module Spaceship
         req.url("https://idmsa.apple.com/appleauth/auth/verify/trusteddevice/securitycode")
         req.headers['Content-Type'] = 'application/json'
         req.body = { "securityCode" => { "code" => code.to_s } }.to_json
+        update_request_headers(req)
+      end
 
+      begin
+        puts r.body
+        # we use `Spaceship::TunesClient.new.handle_itc_response`
+        # since this might be from the Dev Portal, but for 2 factor
+        Spaceship::TunesClient.new.handle_itc_response(r.body) # this will fail if the code is invalid
+      rescue => ex
+        # If the code was entered wrong
+        # {
+        #   "service_errors": [{
+        #     "code": "-21669",
+        #     "title": "Incorrect Verification Code",
+        #     "message": "Incorrect verification code."
+        #   }],
+        #   "hasError": true
+        # }
+
+        if ex.to_s.include?("verification code") # to have a nicer output
+          puts("Error: Incorrect verification code")
+          depth = depth+1 # TODO check if works
+          return handle_two_factor(response, depth)
+        end
+
+        raise ex
+      end
+
+      store_session
+
+      return true
+    end
+
+    def get_id_for_number(trustedPhoneNumbers, result)
+      trustedPhoneNumbers.each do |phone|
+        phone_id = phone['id']
+        return phone_id if phone['numberWithDialCode'] == result 
+      end     
+    end
+
+    def request_two_factor_code_from_phone(trustedPhoneNumbers, code_length)
+      puts("Please select a trusted phone number to send code to:")
+
+      available = trustedPhoneNumbers.collect do |current|
+        current['numberWithDialCode']
+      end
+      result = choose(*available)
+
+      phone_id = get_id_for_number(trustedPhoneNumbers, result)
+
+      # Request code
+      r = request(:put) do |req|
+        req.url("https://idmsa.apple.com/appleauth/auth/verify/phone")
+        req.headers['Content-Type'] = 'application/json'
+        req.body = { "phoneNumber" => { "id" => phone_id }, "mode" => "sms" }.to_json
         update_request_headers(req)
       end
 
@@ -146,9 +205,9 @@ module Spaceship
       # since this might be from the Dev Portal, but for 2 step
       Spaceship::TunesClient.new.handle_itc_response(r.body)
 
-      store_session
+      puts("Successfully requested text message")
 
-      return true
+      ask("Please enter the #{code_length} digit code you received at #{result}:")
     end
 
     def store_session
