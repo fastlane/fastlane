9d120bf92ccdcd4b0ea8ec633960e31cc2e9a505
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-01-09 15:09:46 +0100

    [screengrab] Update Android Library to API level 28 (#13231)
    ### Checklist
    - [x] I've run `bundle exec rspec` from the root directory to see all new and existing tests pass
    - [x] I've followed the _fastlane_ code style and run `bundle exec rubocop -a` to ensure the code style is valid
    - [x] I've read the [Contribution Guidelines](https://github.com/fastlane/fastlane/blob/master/CONTRIBUTING.md)
    - [x] I've updated the documentation if necessary.
    
    ### Motivation and Context
    
    It's best to target the last version. I've also got bitten by incompatible issues when testing with one of my apps which should be resolved with these changes. I could not test it out since `uploadArchives` didn't publish the library to my local maven repository - is there any other task?
    
    ### Description
    
    Updated the entire Android screengrab library to the latest versions.

--- screengrab/screengrab-lib/src/test/java/tools/fastlane/screengrab/locale/LocaleUtilTest.java
+++ screengrab/screengrab-lib/src/test/java/tools/fastlane/screengrab/locale/LocaleUtilTest.java
@@ -1,12 +1,12 @@
-package screengrab;
+package tools.fastlane.screengrab.locale;
 
 import org.junit.Test;
 
 import java.util.Locale;
 
-import screengrab.locale.LocaleUtil;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -14,47 +14,47 @@ import static org.junit.Assert.*;
 public class LocaleUtilTest {
 
     @Test
-    public void testLocaleFromParts_ignores4thPart() throws Exception {
+    public void testLocaleFromParts_ignores4thPart() {
         assertEquals(new Locale("ja", "JP", "JP"), LocaleUtil.localeFromParts(new String[]{"ja", "JP", "JP", "junk"}));
     }
 
     @Test
-    public void testLocaleFromParts_threeParts() throws Exception {
+    public void testLocaleFromParts_threeParts() {
         assertEquals(new Locale ("ja", "JP", "JP"), LocaleUtil.localeFromParts(new String[]{"ja", "JP", "JP"}));
     }
 
     @Test
-    public void testLocaleFromParts_twoParts() throws Exception {
+    public void testLocaleFromParts_twoParts() {
         assertEquals(Locale.CANADA_FRENCH, LocaleUtil.localeFromParts(new String[]{"fr", "CA"}));
     }
 
     @Test
-    public void testLocaleFromParts_onePart() throws Exception {
+    public void testLocaleFromParts_onePart() {
         assertEquals(Locale.FRENCH, LocaleUtil.localeFromParts(new String[]{"fr"}));
     }
 
     @Test
-    public void testLocaleFromParts_nullNoParts() throws Exception {
+    public void testLocaleFromParts_nullNoParts() {
         assertNull(LocaleUtil.localeFromParts(new String[]{}));
     }
 
     @Test
-    public void testLocaleFromParts_nullFromNull() throws Exception {
+    public void testLocaleFromParts_nullFromNull() {
         assertNull(LocaleUtil.localeFromParts(null));
     }
 
     @Test
-    public void testlocalePartsFrom_twoParts() throws Exception {
+    public void testlocalePartsFrom_twoParts() {
         assertArrayEquals(new String[]{"fr", "CA"}, LocaleUtil.localePartsFrom("fr_CA"));
     }
 
     @Test
-    public void testlocalePartsFrom_onePart() throws Exception {
+    public void testlocalePartsFrom_onePart() {
         assertArrayEquals(new String[]{"fr"}, LocaleUtil.localePartsFrom("fr"));
     }
 
     @Test
-    public void testlocalePartsFrom_fromNull() throws Exception {
+    public void testlocalePartsFrom_fromNull() {
         assertNull(LocaleUtil.localePartsFrom(null));
     }
 
