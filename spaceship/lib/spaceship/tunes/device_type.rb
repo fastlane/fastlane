f2832d162f3dcacf971cf90a7fd4eebfd3b5ca30
Jan Piotrowski <piotrowski+git@gmail.com>
2019-03-22 23:42:50 +0100

    rubocop

--- spaceship/lib/spaceship/tunes/device_type.rb
+++ spaceship/lib/spaceship/tunes/device_type.rb
@@ -4,8 +4,8 @@ module Spaceship
     class DeviceType
       @types = [
         # iPhone
-        'iphone35', 
-        'iphone4', 
+        'iphone35',
+        'iphone4',
         'iphone6', # 4.7-inch Display
         'iphone6Plus', # 5.5-inch Display
         'iphone58', # iPhone XS
@@ -13,19 +13,19 @@ module Spaceship
 
         # iPad
         'ipad', # 9.7-inch Display
-        'ipad105', 
-        'ipadPro', 
-        'ipadPro11', 
-        'ipadPro129', 
+        'ipad105',
+        'ipadPro',
+        'ipadPro11',
+        'ipadPro129',
 
         # Apple Watch
         'watch', # series 3
         'watchSeries4',
 
-        # Apple TV 
+        # Apple TV
         'appleTV',
 
-        # Mac 
+        # Mac
         'desktop'
       ]
       class << self
