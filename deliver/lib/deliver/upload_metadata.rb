643c1c773ed0f88ae860e64acc5fecd5afff7a85
Krish722 <krish722@gmail.com>
2019-03-24 18:38:11 +1300

    Link the app review attachment option to deliver

--- deliver/lib/deliver/upload_metadata.rb
+++ deliver/lib/deliver/upload_metadata.rb
@@ -370,7 +370,7 @@ module Deliver
     end
 
     def set_review_attachment_file(v, options)
-      v.upload_review_attachment(options[:app_review_attachment_file])
+      v.upload_review_attachment!(options[:app_review_attachment_file])
     end
 
     def set_app_rating(v, options)
