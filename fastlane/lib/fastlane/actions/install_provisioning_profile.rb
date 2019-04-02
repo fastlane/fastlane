a56d6e9a52938b304072319f82b3f57eb887c7ef
SofteqDG <dmitry.gruzd@softeq.com>
2019-04-02 17:07:08 +0300

    New action to install provisioning profile from file

---
+++ fastlane/lib/fastlane/actions/install_provisioning_profile.rb
@@ -0,0 +1,52 @@
+require 'shellwords'
+
+module Fastlane
+  module Actions
+    class InstallProvisioningProfileAction < Action
+      def self.run(params)
+        provisioning_profile_path = params[:provisioning_profile_path]
+        FastlaneCore::ProvisioningProfile.install(provisioning_profile_path)
+      end
+
+      def self.description
+        "Install provisioning profile from inputfile"
+      end
+
+      def self.details
+        "Install provisioning profile from inputfile for current user"
+      end
+
+      def self.authors
+        ["SofteqDG"]
+      end
+
+      def self.category
+        :code_signing
+      end
+
+      def self.is_supported?(platform)
+        [:ios, :mac].include?(platform)
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :provisioning_profile_path,
+                                  env_name: "FL_INSTALL_PROVISIONING_PROFILE_PATH",
+                               description: "Path to provisioning profile",
+                                  optional: false,
+                                      type: String)
+        ]
+      end
+
+      def self.return_value
+        "The absolute path to the installed provisioning profile"
+      end
+
+      def self.example_code
+        [
+          'install_provisioning_profile(provisioning_profile_path: "profiles/profile.mobileprovision")'
+        ]
+      end
+    end
+  end
+end
