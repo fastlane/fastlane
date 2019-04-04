c3a2fa6ee1eedc30c5696282ff09d2700d460d81
Josh Holtz <josh@rokkincat.com>
2019-04-03 22:27:18 -0500

    Doing some mad mapping from new api to old data format - gross

--- spaceship/lib/spaceship/test_flight/build_trains.rb
+++ spaceship/lib/spaceship/test_flight/build_trains.rb
@@ -13,12 +13,46 @@ module Spaceship::TestFlight
     # See `Spaceship::TestFlight::Build#reload`
 
     def self.all(app_id: nil, platform: nil, retry_count: 3)
-      data = client.get_build_trains(app_id: app_id, platform: platform)
-      trains = {}
+      client = Spaceship::ConnectAPI::Base.client
+      builds_resp = client.get_builds(filter: { app: app_id}, sort: "-uploadedDate", includes: "buildBetaDetail,betaBuildMetrics,preReleaseVersion,app", only_data: false)
 
-      data.each do |train_version|
-        builds_data = client.get_builds_for_train(app_id: app_id, platform: platform, train_version: train_version, retry_count: retry_count)
-        trains[train_version] = builds_data.map { |attrs| Spaceship::TestFlight::Build.new(attrs) }
+      builds = builds_resp["data"]
+      included = builds_resp["included"]
+
+      # Load with all of the data
+      builds.map do |build|
+        r = build["relationships"]["app"]["data"]
+        build["app"] = included.find { |h| h["type"] == r["type"] && h["id"] == r["id"] }
+
+        r = build["relationships"]["preReleaseVersion"]["data"]
+        build["preReleaseVersion"] = included.find { |h| h["type"] == r["type"] && h["id"] == r["id"] }
+
+        r = build["relationships"]["betaBuildMetrics"]["data"][0]
+        build["betaBuildMetrics"] = included.find { |h| h["type"] == r["type"] && h["id"] == r["id"] }
+
+        build
+      end
+
+      # Map to testflight build response????
+      train_builds = builds.map do |build|
+        h = {}
+
+        h['buildVersion'] = build["attributes"]["version"]
+        h['uploadDate'] = build["attributes"]["uploadedDate"]
+
+        h['appAdamId'] = build["app"]["id"]
+        h['bundleId'] = build["app"]["attributes"]["bundleId"]
+
+        h['trainVersion'] = build["preReleaseVersion"]["attributes"]["version"]
+        h['installCount'] = build["betaBuildMetrics"]["attributes"]["installCount"]
+
+        h
+      end
+
+      trains = {}
+      train_builds.each do |build|
+        train_version = build["trainVersion"]
+        trains[train_version] = Spaceship::TestFlight::Build.new(build)
       end
 
       self.new(trains)
