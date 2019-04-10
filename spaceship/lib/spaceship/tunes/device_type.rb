dd9f2385f31b090d30b40527096cc99134f1cc89
Jan Piotrowski <piotrowski+github@gmail.com>
2019-04-10 23:21:09 +0200

    More device type comments

--- spaceship/lib/spaceship/tunes/device_type.rb
+++ spaceship/lib/spaceship/tunes/device_type.rb
@@ -1,26 +1,26 @@
 module Spaceship
   module Tunes
-    # identifiers of devices that App Store Connect accepts screenshots for
+    # identifiers of device types that App Store Connect accepts screenshots for
     class DeviceType
       @types = [
         # iPhone
-        'iphone35',
-        'iphone4',
-        'iphone6', # 4.7-inch Display
-        'iphone6Plus', # 5.5-inch Display
-        'iphone58', # iPhone XS
-        'iphone65', # iPhone XS Max
+        'iphone35',     #  3.5-inch Display
+        'iphone4',      #    4-inch Display
+        'iphone6',      #  4.7-inch Display
+        'iphone6Plus',  #  5.5-inch Display
+        'iphone58',     #  5.8-inch Display, iPhone XS
+        'iphone65',     #  6.5-inch Display, iPhone XS Max
 
         # iPad
-        'ipad', # 9.7-inch Display
-        'ipad105',
-        'ipadPro',
-        'ipadPro11',
-        'ipadPro129',
+        'ipad',         #  9.7-inch Display
+        'ipad105',      # 10.5-inch Display
+        'ipadPro',      # 12.9-inch Display, iPad Pro (2nd Generation)
+        'ipadPro11',    #   11-inch Display
+        'ipadPro129',   # 12.9-inch Display, iPad Pro (3rd Generation)
 
         # Apple Watch
-        'watch', # series 3
-        'watchSeries4',
+        'watch',
+        'watchSeries4', # Series 4
 
         # Apple TV
         'appleTV',
