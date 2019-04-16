464f3dcbaed18c5f007610ab9eb8d720709387a6
Raymond Hoagland <raymond.hoagland@gmail.com>
2019-04-15 21:23:11 -0400

    address rubocop linting issues

--- fastlane/lib/fastlane/actions/pod_lib_lint.rb
+++ fastlane/lib/fastlane/actions/pod_lib_lint.rb
@@ -42,26 +42,22 @@ module Fastlane
                                        description: "Use bundle exec when there is a Gemfile presented",
                                        type: Boolean,
                                        default_value: true,
-                                       env_name: "FL_POD_LIB_LINT_USE_BUNDLE" 
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_USE_BUNDLE"),
           FastlaneCore::ConfigItem.new(key: :podspec,
                                        description: "Path of spec to lint",
                                        type: String,
                                        optional: true,
-                                       env_name: "FL_POD_LIB_LINT_PODSPEC"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_PODSPEC"),
           FastlaneCore::ConfigItem.new(key: :verbose,
                                        description: "Allow output detail in console",
                                        type: Boolean,
                                        optional: true,
-                                       env_name: "FL_POD_LIB_LINT_VERBOSE"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_VERBOSE"),
           FastlaneCore::ConfigItem.new(key: :allow_warnings,
                                        description: "Allow warnings during pod lint",
                                        type: Boolean,
                                        optional: true,
-                                       env_name: "FL_POD_LIB_LINT_ALLOW_WARNINGS"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_ALLOW_WARNINGS"),
           FastlaneCore::ConfigItem.new(key: :sources,
                                        description: "The sources of repos you want the pod spec to lint with, separated by commas",
                                        type: Array,
@@ -74,44 +70,37 @@ module Fastlane
                                        description: "A specific subspec to lint instead of the entire spec",
                                        type: String,
                                        optional: true,
-                                       env_name: "FL_POD_LIB_LINT_SUBSPEC"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_SUBSPEC"),
           FastlaneCore::ConfigItem.new(key: :swift_version,
                                        description: "The SWIFT_VERSION that should be used to lint the spec. This takes precedence over a .swift-version file",
                                        type: String,
                                        optional: true,
-                                       env_name: "FL_POD_LIB_LINT_SWIFT_VERSION"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_SWIFT_VERSION"),
           FastlaneCore::ConfigItem.new(key: :use_libraries,
                                        description: "Lint uses static libraries to install the spec",
                                        type: Boolean,
                                        default_value: false,
-                                       env_name: "FL_POD_LIB_LINT_USE_LIBRARIES"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_USE_LIBRARIES"),
           FastlaneCore::ConfigItem.new(key: :use_modular_headers,
                                        description: "Lint using modular libraries",
                                        type: Boolean,
                                        default_value: false,
-                                       env_name: "FL_POD_LIB_LINT_USE_MODULAR_HEADERS"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_USE_MODULAR_HEADERS"),
           FastlaneCore::ConfigItem.new(key: :fail_fast,
                                        description: "Lint stops on the first failing platform or subspec",
                                        type: Boolean,
                                        default_value: false,
-                                       env_name: "FL_POD_LIB_LINT_FAIL_FAST"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_FAIL_FAST"),
           FastlaneCore::ConfigItem.new(key: :private,
                                        description: "Lint skips checks that apply only to public specs",
                                        type: Boolean,
                                        default_value: false,
-                                       env_name: "FL_POD_LIB_LINT_PRIVATE"
-                                      ),
+                                       env_name: "FL_POD_LIB_LINT_PRIVATE"),
           FastlaneCore::ConfigItem.new(key: :quick,
                                        description: "Lint skips checks that would require to download and build the spec",
                                        type: Boolean,
                                        default_value: false,
-                                       env_name: "FL_POD_LIB_LINT_QUICK"
-                                      )
+                                       env_name: "FL_POD_LIB_LINT_QUICK"),
         ]
       end
 
