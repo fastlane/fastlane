ce9a28418138fdb90e60331852d1b74413a96f50
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2019-04-24 19:15:45 +0200

    Install gem before tests exec

--- test_modules/run_tests.sh
+++ test_modules/run_tests.sh
@@ -4,4 +4,9 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
+# installing fastlane gem
+gem build fastlane.gemspec
+gem install fastlane-2.121.1.gem
+
+# running tests
 ruby test_modules/test_spaceship.rb
