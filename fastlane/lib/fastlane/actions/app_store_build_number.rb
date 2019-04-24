d9e7691f0933762c971d1d8684a7c01fe61a85b0
Marius Davidsen <marius.davidsen@gmail.com>
2019-04-24 20:14:07 +0200

    Change sorting of requested builds
    * Changes from `-uploadedDate` to `-version`
    
    Fixes #14669

--- fastlane/lib/fastlane/actions/app_store_build_number.rb
+++ fastlane/lib/fastlane/actions/app_store_build_number.rb
@@ -49,7 +49,7 @@ module Fastlane
             UI.message("Fetching the latest build number for version #{version_number}")
 
             # Get latest build for version number
-            build = client.get_builds(filter: { app: app.apple_id, "preReleaseVersion" => pre_release_version_id }, sort: "-uploadedDate", limit: 1).first
+            build = client.get_builds(filter: { app: app.apple_id, "preReleaseVersion" => pre_release_version_id }, sort: "-version", limit: 1).first
             if build
               build_nr = build["attributes"]["version"]
               build_nr
