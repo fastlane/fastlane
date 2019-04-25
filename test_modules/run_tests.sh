ce4531619cc73474bd0312d3991b03466286195d
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2019-04-25 10:38:29 +0200

    Gem install as build setup step

--- test_modules/run_tests.sh
+++ test_modules/run_tests.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# installing current version of fastlane gem
-BUILD_LOG=$(eval gem build fastlane.gemspec)
-if [ $? -ne 0 ]
-then
-  exit 1
-fi
-GEM_FILE=$(eval echo $BUILD_LOG | sed 's/.*File: //')
-gem install $GEM_FILE
-
 # running tests
 TEST_FILES=(
   "test_modules/test_spaceship.rb"
