13b28c0064498ef68df408ce604452262d626356
Daniel Jankowski <daniell.jankowskii@gmail.com>
2019-04-25 17:29:10 +0200

    Fix sigh uuid description

--- fastlane/lib/fastlane/actions/get_provisioning_profile.rb
+++ fastlane/lib/fastlane/actions/get_provisioning_profile.rb
@@ -57,7 +57,7 @@ module Fastlane
         [
           ['SIGH_PROFILE_PATH', 'A path in which certificates, key and profile are exported'],
           ['SIGH_PROFILE_PATHS', 'Paths in which certificates, key and profile are exported'],
-          ['SIGH_UUID', 'UUID (Universally Unique IDentifier) of an application on a device'],
+          ['SIGH_UUID', 'UUID (Universally Unique IDentifier) of a provisioning profile'],
           ['SIGH_NAME', 'The name of the profile'],
           ['SIGH_PROFILE_TYPE', 'The profile type, can be appstore, adhoc, development, enterprise']
         ]
