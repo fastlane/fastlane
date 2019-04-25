58c79b605f6b10114b6bd328aa30cb151de1ac36
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2019-04-25 10:00:51 +0200

    Fix syntax

--- test_modules/run_tests.sh
+++ test_modules/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-installing current version of fastlane gem
+# installing current version of fastlane gem
 BUILD_LOG=$(eval gem build fastlane.gemspec)
 if [ $? -ne 0 ]
 then
@@ -10,7 +10,7 @@ GEM_FILE=$(eval echo $BUILD_LOG | sed 's/.*File: //')
 gem install $GEM_FILE
 
 # running tests
-TEST_FILES=( "test_modules/test_spaceship.rb" "test_modules/test_scan.rb" )
+TEST_FILES=("test_modules/test_spaceship.rb" "test_modules/test_scan.rb")
 for TEST_FILE in ${TEST_FILES[*]} 
 do
   ruby $TEST_FILE
