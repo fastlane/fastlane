51ad4849874f42a9b2ae616967ceeb2899249cab
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2019-04-25 10:16:10 +0200

    Exit with failure if at least one test fails

--- test_modules/run_tests.sh
+++ test_modules/run_tests.sh
@@ -14,7 +14,14 @@ TEST_FILES=(
   "test_modules/test_spaceship.rb"
   "test_modules/test_scan.rb"
 )
+EXIT_CODE=0
 for TEST_FILE in ${TEST_FILES[*]} 
 do
   ruby $TEST_FILE
+  if [ $? -ne 0 ]
+  then
+    EXIT_CODE=2
+  fi
 done
+
+exit $EXIT_CODE
