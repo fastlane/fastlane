76d6ac49af7c7ff0b1f46bd3de2966dd7522e675
Josh Holtz <josh@rokkincat.com>
2019-05-21 22:59:04 -0500

    Reworked client spec to have all the things

--- spaceship/lib/spaceship/connect_api/client.rb
+++ spaceship/lib/spaceship/connect_api/client.rb
@@ -38,7 +38,8 @@ module Spaceship
       end
 
       def get(url_or_path, params = nil)
-        response = request(:get, url_or_path) do |req|
+        response = request(:get) do |req|
+          req.url(url_or_path)
           req.options.params_encoder = Faraday::NestedParamsEncoder
           req.params = params if params
         end
@@ -64,8 +65,9 @@ module Spaceship
       end
 
       def delete(url_or_path, params = nil, body = nil)
-        response = request(:delete, url_or_path) do |req|
-          req.options.params_encoder = Faraday::NestedParamsEncoder
+        response = request(:delete) do |req|
+          req.url(url_or_path)
+          req.options.params_encoder = Faraday::NestedParamsEncoder if params
           req.params = params if params
           req.body = body.to_json if body
           req.headers['Content-Type'] = 'application/json' if body
@@ -89,33 +91,6 @@ module Spaceship
         # https://appstoreconnect.apple.com/iris/v1/apps/<app_id>
         params = build_params(filter: nil, includes: includes, limit: nil, sort: nil)
         get("apps/#{app_id}", params)
-      end
-
-      #
-      # betaAppReviewDetails
-      #
-
-      def get_beta_app_review_detail(filter: {}, includes: nil, limit: nil, sort: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewDetails?filter[app]=<app_id>
-        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
-        get("betaAppReviewDetails", params)
-      end
-
-      def patch_beta_app_review_detail(app_id: nil, attributes: {})
-        # PATCH
-        # https://appstoreconnect.apple.com/iris/v1/apps/<app_id>/betaAppReviewDetails
-        path = "betaAppReviewDetails/#{app_id}"
-
-        body = {
-          data: {
-            attributes: attributes,
-            id: app_id,
-            type: "betaAppReviewDetails"
-          }
-        }
-
-        patch(path, body)
       end
 
       #
@@ -166,6 +141,72 @@ module Spaceship
         }
 
         patch(path, body)
+      end
+
+      #
+      # betaAppReviewDetails
+      #
+
+      def get_beta_app_review_detail(filter: {}, includes: nil, limit: nil, sort: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewDetails?filter[app]=<app_id>
+        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
+        get("betaAppReviewDetails", params)
+      end
+
+      def patch_beta_app_review_detail(app_id: nil, attributes: {})
+        # PATCH
+        # https://appstoreconnect.apple.com/iris/v1/apps/<app_id>/betaAppReviewDetails
+        path = "betaAppReviewDetails/#{app_id}"
+
+        body = {
+          data: {
+            attributes: attributes,
+            id: app_id,
+            type: "betaAppReviewDetails"
+          }
+        }
+
+        patch(path, body)
+      end
+
+      #
+      # betaAppReviewSubmissions
+      #
+
+      def get_beta_app_review_submissions(filter: {}, includes: nil, limit: nil, sort: nil, cursor: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions
+        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort, cursor: cursor)
+        get("betaAppReviewSubmissions", params)
+      end
+
+      def post_beta_app_review_submissions(build_id: nil)
+        # POST
+        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions
+        path = "betaAppReviewSubmissions"
+        body = {
+          data: {
+            type: "betaAppReviewSubmissions",
+            relationships: {
+              build: {
+                data: {
+                  type: "builds",
+                  id: build_id
+                }
+              }
+            }
+          }
+        }
+
+        post(path, body)
+      end
+
+      def delete_beta_app_review_submission(beta_app_review_submission_id: nil)
+        # DELETE
+        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions/<beta_app_review_submission_id>
+        params = build_params(filter: nil, includes: nil, limit: nil, sort: nil, cursor: nil)
+        delete("betaAppReviewSubmissions/#{beta_app_review_submission_id}", params)
       end
 
       #
@@ -220,6 +261,140 @@ module Spaceship
       end
 
       #
+      # betaGroups
+      #
+
+      def get_beta_groups(filter: {}, includes: nil, limit: nil, sort: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/betaGroups
+        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
+        get("betaGroups", params)
+      end
+
+      def add_beta_groups_to_build(build_id: nil, beta_group_ids: [])
+        # POST
+        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>/relationships/betaGroups
+        path = "builds/#{build_id}/relationships/betaGroups"
+        body = {
+          data: beta_group_ids.map do |id|
+            {
+              type: "betaGroups",
+              id: id
+            }
+          end
+        }
+
+        post(path, body)
+      end
+
+      #
+      # betaTesters
+      #
+
+      def get_beta_testers(filter: {}, includes: nil, limit: nil, sort: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/betaTesters
+        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
+        get("betaTesters", params)
+      end
+
+      # beta_testers - [{email: "", firstName: "", lastName: ""}]
+      def post_bulk_beta_tester_assignments(beta_group_id: nil, beta_testers: nil)
+        # POST
+        # https://appstoreconnect.apple.com/iris/v1/bulkBetaTesterAssignments
+        beta_testers || []
+
+        beta_testers.map do |tester|
+          tester[:errors] = []
+        end
+
+        body = {
+          data: {
+            attributes: {
+              betaTesters: beta_testers
+            },
+            relationships: {
+              betaGroup: {
+                data: {
+                  type: "betaGroups",
+                  id: beta_group_id
+                }
+              }
+            },
+            type: "bulkBetaTesterAssignments"
+          }
+        }
+
+        post("bulkBetaTesterAssignments", body)
+      end
+
+      def delete_beta_tester_from_apps(beta_tester_id: nil, app_ids: [])
+        # DELETE
+        # https://appstoreconnect.apple.com/iris/v1/betaTesters/<beta_tester_id>/relationships/apps
+        path = "betaTesters/#{beta_tester_id}/relationships/apps"
+        body = {
+          data: app_ids.map do |id|
+            {
+              type: "apps",
+              id: id
+            }
+          end
+        }
+
+        delete(path, nil, body)
+      end
+
+      def delete_beta_tester_from_beta_groups(beta_tester_id: nil, beta_group_ids: [])
+        # DELETE
+        # https://appstoreconnect.apple.com/iris/v1/betaTesters/<beta_tester_id>/relationships/betaGroups
+        path = "betaTesters/#{beta_tester_id}/relationships/betaGroups"
+        body = {
+          data: beta_group_ids.map do |id|
+            {
+              type: "betaGroups",
+              id: id
+            }
+          end
+        }
+
+        delete(path, nil, body)
+      end
+
+      #
+      # builds
+      #
+
+      def get_builds(filter: {}, includes: "buildBetaDetail,betaBuildMetrics", limit: 10, sort: "uploadedDate", cursor: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/builds
+        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort, cursor: cursor)
+        get("builds", params)
+      end
+
+      def get_build(build_id: nil, includes: nil)
+        # GET
+        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>?
+        params = build_params(filter: nil, includes: includes, limit: nil, sort: nil, cursor: nil)
+        get("builds/#{build_id}", params)
+      end
+
+      def patch_builds(build_id: nil, attributes: {})
+        # PATCH
+        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>
+        path = "builds/#{build_id}"
+
+        body = {
+          data: {
+            attributes: attributes,
+            id: build_id,
+            type: "builds"
+          }
+        }
+
+        patch(path, body)
+      end
+
+      #
       # buildBetaDetails
       #
 
@@ -258,79 +433,6 @@ module Spaceship
       end
 
       #
-      # builds
-      #
-
-      def get_builds(filter: {}, includes: "buildBetaDetail,betaBuildMetrics", limit: 10, sort: "uploadedDate", cursor: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/builds
-        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort, cursor: cursor)
-        get("builds", params)
-      end
-
-      def get_build(build_id: nil, includes: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>?
-        params = build_params(filter: nil, includes: includes, limit: nil, sort: nil, cursor: nil)
-        get("builds/#{build_id}", params)
-      end
-
-      def patch_builds(build_id: nil, attributes: {})
-        # PATCH
-        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>
-        path = "builds/#{build_id}"
-
-        body = {
-          data: {
-            attributes: attributes,
-            id: build_id,
-            type: "builds"
-          }
-        }
-
-        patch(path, body)
-      end
-
-      #
-      # betaAppReviewSubmissions
-      #
-
-      def get_beta_app_review_submissions(filter: {}, includes: nil, limit: nil, sort: nil, cursor: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions
-        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort, cursor: cursor)
-        get("betaAppReviewSubmissions", params)
-      end
-
-      def post_beta_app_review_submissions(build_id: nil)
-        # POST
-        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions
-        path = "betaAppReviewSubmissions"
-        body = {
-          data: {
-            type: "betaAppReviewSubmissions",
-            relationships: {
-              build: {
-                data: {
-                  type: "builds",
-                  id: build_id
-                }
-              }
-            }
-          }
-        }
-
-        post(path, body)
-      end
-
-      def delete_beta_app_review_submission(beta_app_review_submission_id: nil)
-        # DELETE
-        # https://appstoreconnect.apple.com/iris/v1/betaAppReviewSubmissions/<beta_app_review_submission_id>
-        params = build_params(filter: nil, includes: nil, limit: nil, sort: nil, cursor: nil)
-        delete("betaAppReviewSubmissions/#{beta_app_review_submission_id}", params)
-      end
-
-      #
       # preReleaseVersions
       #
 
@@ -345,111 +447,11 @@ module Spaceship
       # users
       #
 
-      def get_users(filter: {}, includes: nil, limit: 40, sort: nil)
+      def get_users(filter: {}, includes: nil, limit: nil, sort: nil)
         # GET
         # https://appstoreconnect.apple.com/iris/v1/users
         params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
         get("users", params)
-      end
-
-      #
-      # betaTesters
-      #
-
-      def get_beta_testers(filter: {}, includes: nil, limit: 40, sort: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/betaTesters
-        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
-        get("betaTesters", params)
-      end
-
-      def delete_beta_tester_from_apps(beta_tester_id: nil, app_ids: [])
-        # DELETE
-        # https://appstoreconnect.apple.com/iris/v1/betaTesters/<beta_tester_id>/relationships/apps
-        path = "betaTesters/#{beta_tester_id}/relationships/apps"
-        body = {
-          data: app_ids.map do |id|
-            {
-              type: "apps",
-              id: id
-            }
-          end
-        }
-
-        delete(path, nil, body)
-      end
-
-      def delete_beta_tester_from_beta_groups(beta_tester_id: nil, beta_group_ids: [])
-        # DELETE
-        # https://appstoreconnect.apple.com/iris/v1/betaTesters/<beta_tester_id>/relationships/betaGroups
-        path = "betaTesters/#{beta_tester_id}/relationships/betaGroups"
-        body = {
-          data: beta_group_ids.map do |id|
-            {
-              type: "betaGroups",
-              id: id
-            }
-          end
-        }
-
-        delete(path, nil, body)
-      end
-
-      #
-      # betaGroups
-      #
-
-      def get_beta_groups(filter: {}, includes: nil, limit: nil, sort: nil)
-        # GET
-        # https://appstoreconnect.apple.com/iris/v1/betaGroups
-        params = build_params(filter: filter, includes: includes, limit: limit, sort: sort)
-        get("betaGroups", params)
-      end
-
-      def add_beta_groups_to_build(build_id: nil, beta_group_ids: [])
-        # POST
-        # https://appstoreconnect.apple.com/iris/v1/builds/<build_id>/relationships/betaGroups
-        path = "builds/#{build_id}/relationships/betaGroups"
-        body = {
-          data: beta_group_ids.map do |id|
-            {
-              type: "betaGroups",
-              id: id
-            }
-          end
-        }
-
-        post(path, body)
-      end
-
-      # beta_testers - [{email: "", firstName: "", lastName: ""}]
-      def post_bulk_beta_tester_assignments(beta_group_id: nil, beta_testers: nil)
-        # POST
-        # https://appstoreconnect.apple.com/iris/v1/bulkBetaTesterAssignments
-        beta_testers || []
-
-        beta_testers.map do |tester|
-          tester[:errors] = []
-        end
-
-        body = {
-          data: {
-            attributes: {
-              betaTesters: beta_testers
-            },
-            relationships: {
-              betaGroup: {
-                data: {
-                  id: beta_group_id,
-                  type: "betaGroups"
-                }
-              }
-            },
-            type: "bulkBetaTesterAssignments"
-          }
-        }
-
-        post("bulkBetaTesterAssignments", body)
       end
 
       protected
