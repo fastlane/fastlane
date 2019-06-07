fd21ac62852e3354db70f94eb05e0540edbe8fcf
Vladimir Zdravkovic <v.zdravkovic@levi9.com>
2019-06-07 15:55:14 +0200

    Changed the srt value for when returning the list of builds from the Connect API for TestFlight builds

--- fastlane/lib/fastlane/actions/app_store_build_number.rb
+++ fastlane/lib/fastlane/actions/app_store_build_number.rb
@@ -50,7 +50,7 @@ module Fastlane
 
           # Get latest build for optional version number and return build number if found
           client = Spaceship::ConnectAPI::Base.client
-          build = client.get_builds(filter: filter, sort: "-version", includes: "preReleaseVersion", limit: 1).first
+          build = client.get_builds(filter: filter, sort: "-uploadedDate", includes: "preReleaseVersion", limit: 1).first
           if build
             build_nr = build.version
             UI.message("Latest upload for version #{build.app_version} is build: #{build_nr}")
