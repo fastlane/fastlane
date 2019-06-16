05c409e9f905ef316309bcf1ea6ac094494cc915
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2019-06-16 19:55:22 +0200

    Clean up + Reimplement Reset content

--- fastlane/lib/fastlane/actions/reset_simulator_contents.rb
+++ fastlane/lib/fastlane/actions/reset_simulator_contents.rb
@@ -4,35 +4,33 @@ module Fastlane
       def self.run(params)
         os_versions = params[:version]
 
-        if Helper.xcode_at_least?("9")
-          reset_xcode9_and_higher(os_versions)
-        else
-          reset_up_to_xcode8(os_versions)
-        end
+        reset_simulators(os_versions)
       end
 
-      def self.reset_xcode9_and_higher(ios_versions)
-        UI.verbose("Resetting simulator contents for Xcode 9 and later")
-        simulators = FastlaneCore::DeviceManager.simulators('iOS')
-        if ios_versions
-          simulators.select! { |s| ios_versions.include?(s.os_version) }
-        end
-        simulators.each do |simulator|
-          FastlaneCore::Simulator.reset(udid: simulator.udid)
-        end
-        UI.success('Simulators reset')
-      end
+      def self.reset_simulators(os_versions)
+        UI.verbose("Resetting simulator contents")
 
-      def self.reset_up_to_xcode8(ios_versions)
-        UI.verbose("Resetting simulator contents for Xcode 8 and earlier")
-        if ios_versions
-          ios_versions.each do |os_version|
-            FastlaneCore::Simulator.reset_all_by_version(os_version: os_version)
+        if os_versions
+          os_versions.each do |os_version|
+            reset_all_by_version(os_version)
           end
-        else
-          FastlaneCore::Simulator.reset_all
+        else 
+          reset_all
         end
-        UI.success('Simulators reset')
+
+        UI.success('Simulators reset done')
+      end
+
+      def self.reset_all_by_version(os_version)
+        FastlaneCore::Simulator.reset_all_by_version(os_version: os_version)
+        FastlaneCore::SimulatorTV.reset_all_by_version(os_version: os_version)
+        FastlaneCore::SimulatorWatch.reset_all_by_version(os_version: os_version)
+      end
+
+      def self.reset_all
+        FastlaneCore::Simulator.reset_all
+        FastlaneCore::SimulatorTV.reset_all
+        FastlaneCore::SimulatorWatch.reset_all
       end
 
       def self.description
