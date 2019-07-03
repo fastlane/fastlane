26bc18b3161d015449faa6d3b1166b60c7c4fe3f
Daniel Jankowski <daniell.jankowskii@gmail.com>
2019-07-03 12:59:27 +0200

    [pilot] Add verification to apple_id parameter (#14938)
    * [pilot] Add verification to apple_id parameter
    
    * Change parameter validation to check if apple_id is not an email or it is not a bundle identifier
    
    * Improve apple_id parameter description

--- fastlane/spec/actions_specs/pilot_spec.rb
+++ fastlane/spec/actions_specs/pilot_spec.rb
@@ -20,6 +20,37 @@ describe Fastlane do
 
         expect(values[:changelog]).to eq('custom changelog')
       end
+
+      describe "Test `apple_id` parameter" do
+        it "raises an error if `apple_id` is set to email address" do
+          expect do
+            options = {
+              username: "username@example.com",
+              apple_id: "username@example.com"
+            }
+            pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
+          end.to raise_error("`apple_id` value is incorrect. The correct value should be taken from Apple ID property in the App Information section in App Store Connect.")
+        end
+
+        it "raises an error if `apple_id` is set to bundle identifier" do
+          expect do
+            options = {
+              username: "username@example.com",
+              apple_id: "com.bundle.identifier"
+            }
+            pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
+          end.to raise_error("`apple_id` value is incorrect. The correct value should be taken from Apple ID property in the App Information section in App Store Connect.")
+        end
+
+        it "passes when `apple_id` is correct" do
+          options = {
+            username: "username@example.com",
+            apple_id: "123456789"
+          }
+          pilot_config = FastlaneCore::Configuration.create(Pilot::Options.available_options, options)
+          expect(pilot_config[:apple_id]).to eq('123456789')
+        end
+      end
     end
   end
 end
