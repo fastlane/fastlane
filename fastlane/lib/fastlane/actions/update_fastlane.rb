6537974e792690dc63fb6fe8209046e8df0a8ac4
Stefan Painhapp <stefan@painhapp.com>
2019-07-29 17:56:57 +0900

    [action] update_fastlane: Fix broken link (#15086)
    * Fix broken link for update_fastlane action
    
    * Add trailing slash to update_fastlane link
    
    Co-Authored-By: Daniel Jankowski <daniell.jankowskii@gmail.com>

--- fastlane/lib/fastlane/actions/update_fastlane.rb
+++ fastlane/lib/fastlane/actions/update_fastlane.rb
@@ -31,7 +31,7 @@ module Fastlane
           UI.important("It seems that your Gem directory is not writable by your current user.")
           UI.important("fastlane would need sudo rights to update itself, however, running 'sudo fastlane' is not recommended.")
           UI.important("If you still want to use this action, please read the documentation on how to set this up:")
-          UI.important("https://docs.fastlane.tools/actions/#update_fastlane")
+          UI.important("https://docs.fastlane.tools/actions/update_fastlane/")
           return
         end
 
