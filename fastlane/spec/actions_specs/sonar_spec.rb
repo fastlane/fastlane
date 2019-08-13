774de1ec5fc78261db487aab6a30c5c22ef8f30f
Damien R <damien.rivet@gmail.com>
2019-08-13 16:10:57 +0200

    [action] add SonarQube pull request parameters to sonar (#15069)
    * Add SonarQube pull request parameters
    
    Signed-off-by: Damien Rivet <damien.rivet@gmail.com>
    
    * Add unit test for PR parameters
    
    Signed-off-by: Damien Rivet <damien.rivet@gmail.com>
    
    * Revert fastlane.swift changes
    
    * Revert fastlane.swift changes

--- fastlane/spec/actions_specs/sonar_spec.rb
+++ fastlane/spec/actions_specs/sonar_spec.rb
@@ -1,6 +1,19 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "Sonar Integration" do
+      let(:test_path) { "/tmp/fastlane/tests/fastlane" }
+      let(:sonar_project_path) { "sonar-project.properties" }
+
+      before do
+        # Set up example sonar-project.properties file
+        FileUtils.mkdir_p(test_path)
+        File.write(File.join(test_path, sonar_project_path), '')
+      end
+
+      after(:each) do
+        File.delete(File.join(test_path, sonar_project_path)) if File.exist?(File.join(test_path, sonar_project_path))
+      end
+
       it "Should not print sonar command" do
         allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
         expected_command = "cd #{File.expand_path('.').shellescape} && sonar-scanner -Dsonar.login=\"asdf\""
@@ -9,6 +22,45 @@ describe Fastlane do
         Fastlane::FastFile.new.parse("lane :sonar_test do
           sonar(sonar_login: 'asdf')
         end").runner.execute(:sonar_test)
+      end
+
+      it "works with all parameters" do
+        expect(Fastlane::Actions::SonarAction).to receive(:verify_sonar_scanner_binary).and_return(true)
+        allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          sonar({
+            project_configuration_path: '#{test_path}/#{sonar_project_path}',
+            project_key: 'project-key',
+            project_name: 'project-name',
+            project_version: '1.0.0',
+            sources_path: '/Sources',
+            project_language: 'ruby',
+            source_encoding: 'utf-8',
+            sonar_login: 'sonar-login',
+            sonar_url: 'http://www.sonarqube.com',
+            branch_name: 'branch-name',
+            pull_request_branch: 'pull-request-branch-name',
+            pull_request_base: 'pull-request-base',
+            pull_request_key: 'pull-request-key'
+          })
+        end").runner.execute(:test)
+
+        expected = "cd #{File.expand_path('.').shellescape} && sonar-scanner
+                    -Dproject.settings=\"#{test_path}/#{sonar_project_path}\"
+                    -Dsonar.projectKey=\"project-key\"
+                    -Dsonar.projectName=\"project-name\"
+                    -Dsonar.projectVersion=\"1.0.0\"
+                    -Dsonar.sources=\"/Sources\"
+                    -Dsonar.language=\"ruby\"
+                    -Dsonar.sourceEncoding=\"utf-8\"
+                    -Dsonar.login=\"sonar-login\"
+                    -Dsonar.host.url=\"http://www.sonarqube.com\"
+                    -Dsonar.branch.name=\"branch-name\"
+                    -Dsonar.pullrequest.branch=\"pull-request-branch-name\"
+                    -Dsonar.pullrequest.base=\"pull-request-base\"
+                    -Dsonar.pullrequest.key=\"pull-request-key\"".gsub(/\s+/, ' ')
+        expect(result).to eq(expected)
       end
     end
   end
