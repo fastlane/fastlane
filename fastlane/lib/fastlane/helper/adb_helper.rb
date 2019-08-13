a5ada101fddbc7e99f36fa7feda84a9e4b2981da
Josh Holtz <josh@rokkincat.com>
2019-08-13 08:41:47 -0500

    Alias the method name change but also with a deprecated warning

--- fastlane/lib/fastlane/helper/adb_helper.rb
+++ fastlane/lib/fastlane/helper/adb_helper.rb
@@ -30,6 +30,11 @@ module Fastlane
         Action.sh(command)
       end
 
+      def device_avalaible?(serial)
+        UI.deprecated("Please use `device_available?` instead... This will be removed in a future version of fastlane")
+        device_available?(serial)
+      end
+
       def device_available?(serial)
         load_all_devices
         return devices.map(&:serial).include?(serial)
