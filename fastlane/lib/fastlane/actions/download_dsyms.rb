cc1c351f18a70c8c898f7536a08578f8261523f5
Yen-Chia Lin <buju77@gmail.com>
2019-09-13 20:52:32 +0200

    fixed circleci by removing '.' from the description

--- fastlane/lib/fastlane/actions/download_dsyms.rb
+++ fastlane/lib/fastlane/actions/download_dsyms.rb
@@ -240,7 +240,7 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :version,
                                        short_option: "-v",
                                        env_name: "DOWNLOAD_DSYMS_VERSION",
-                                       description: "The app version for dSYMs you wish to download, pass in 'latest' to download only the latest build's dSYMs. Pass in 'live' to download only the latest live build's dSYMs.",
+                                       description: "The app version for dSYMs you wish to download, pass in 'latest' to download only the latest build's dSYMs or pass in 'live' to download only the latest live verion dSYMs",
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :build_number,
                                        short_option: "-b",
