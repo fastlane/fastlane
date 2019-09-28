da6288dc98daf0b07efe36e210f75aeb5098f496
Jan Piotrowski <piotrowski+git@gmail.com>
2019-09-28 22:24:15 +0200

    simplify

--- fastlane/lib/fastlane/actions/get_apple_pay_certificate.rb
+++ fastlane/lib/fastlane/actions/get_apple_pay_certificate.rb
@@ -23,7 +23,7 @@ module Fastlane
         csr, pkey = Spaceship.certificate.create_apple_pay_certificate_signing_request
 
         begin
-          cert = certificate.create!(csr: csr, bundle_id: params[:merchant_bundle_id])
+          cert = Spaceship.certificate.apple_pay_certificate.create!(csr: csr, bundle_id: params[:merchant_bundle_id])
         rescue => ex
           if ex.to_s.include?("You already have a current")
             UI.message(ex.to_s)
@@ -57,10 +57,6 @@ module Fastlane
         File.write(x509_cert_path, x509_certificate.to_pem + pkey.to_pem)
         UI.message("PEM: ".green + Pathname.new(x509_cert_path).realpath.to_s)
         return x509_cert_path
-      end
-
-      def self.certificate
-        Spaceship.certificate.apple_pay_certificate
       end
 
       #####################################################
