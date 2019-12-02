33c1e3eb64bff0d5485866ed50d8ec9b6b938c83
Alex Trott <hello@alextrott.me>
2019-12-02 18:59:37 +0000

    #15742 Adds Config Options for slide to type

--- scan/lib/scan/runner.rb
+++ scan/lib/scan/runner.rb
@@ -26,6 +26,8 @@ module Scan
       force_quit_simulator_processes if Scan.config[:force_quit_simulator]
 
       if Scan.config[:reset_simulator]
+        puts "Testing puts"
+        UI.message("Logging")
         Scan.devices.each do |device|
           FastlaneCore::Simulator.reset(udid: device.udid)
         end
