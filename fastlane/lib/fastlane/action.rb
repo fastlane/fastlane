a95ecf481b25de43cb669b36e47d0b839d2026a0
Sven Münnich <sven.muennich@viison.com>
2019-12-27 18:52:01 +0100

    Fix parsing of action name (#15547)

--- fastlane/lib/fastlane/action.rb
+++ fastlane/lib/fastlane/action.rb
@@ -121,7 +121,7 @@ module Fastlane
 
     # instead of "AddGitAction", this will return "add_git" to print it to the user
     def self.action_name
-      self.name.split('::').last.gsub('Action', '').fastlane_underscore
+      self.name.split('::').last.gsub(/Action$/, '').fastlane_underscore
     end
 
     def self.lane_context
