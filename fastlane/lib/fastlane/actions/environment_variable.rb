e0835df1185edecd9034f20f1085998559751ebb
Josh Holtz <josh@rokkincat.com>
2020-01-06 06:56:34 -0600

    Forgot to remove an unused param

--- fastlane/lib/fastlane/actions/environment_variable.rb
+++ fastlane/lib/fastlane/actions/environment_variable.rb
@@ -57,7 +57,7 @@ module Fastlane
         "Sets/gets env vars for Fastlane.swift. Don't use in ruby, use `ENV[key] = val`"
       end
 
-      def self.step_text(params)
+      def self.step_text
         nil
       end
 
