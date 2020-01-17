bf4e65403022c154b1e109275efbc2d5478935de
Milos Cernilovsky <cernilovsky@gmail.com>
2020-01-17 15:12:20 +0100

    Added Android as a supported platform in frame_screenshots

--- fastlane/lib/fastlane/actions/frame_screenshots.rb
+++ fastlane/lib/fastlane/actions/frame_screenshots.rb
@@ -57,7 +57,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        [:ios, :mac].include?(platform)
+        [:ios, :mac, :android].include?(platform)
       end
     end
   end
