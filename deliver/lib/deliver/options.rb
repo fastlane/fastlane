595627590c523247a942ea20d9c03940ea3120cf
Wataru Sasaki <47016810+genius-munou@users.noreply.github.com>
2020-02-25 13:57:19 +0900

    Add skip_auto_detecting_app_version option

--- deliver/lib/deliver/options.rb
+++ deliver/lib/deliver/options.rb
@@ -133,6 +133,11 @@ module Deliver
                                      description: "Don't update app version for submission",
                                      is_string: false,
                                      default_value: false),
+        FastlaneCore::ConfigItem.new(key: :skip_auto_detecting_app_version,
+                                     optional: true,
+                                     default_value: false,
+                                     description: "Don't detecting app version from ipa or pkg",
+                                     is_string: false),
 
         # how to operate
         FastlaneCore::ConfigItem.new(key: :force,
