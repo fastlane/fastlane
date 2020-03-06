39c683cefc27c0ec0287f95fbf3433fee4badcd6
Jan Piotrowski <piotrowski+git@gmail.com>
2020-03-07 00:19:41 +0100

    fix rubocop + actually commit tests

--- screengrab/spec/android_environment_spec.rb
+++ screengrab/spec/android_environment_spec.rb
@@ -30,6 +30,22 @@ describe Screengrab do
       end
     end
 
+    describe "with an empty ANDROID_HOME and a complete PATH on Windows" do
+      it "finds commands on the PATH" do
+        allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+        FastlaneSpec::Env.with_env_values('PATH' => 'screengrab/spec/fixtures/empty_home_complete_path/path') do
+          android_env = Screengrab::AndroidEnvironment.new('screengrab/spec/fixtures/empty_home_complete_path/android_home', nil)
+
+          expect(android_env.android_home).to eq('screengrab/spec/fixtures/empty_home_complete_path/android_home')
+          expect(android_env.build_tools_version).to be_nil
+          expect(android_env.build_tools_path).to be_nil
+          expect(android_env.platform_tools_path).to be_nil
+          expect(android_env.adb_path).to eq('screengrab/spec/fixtures/empty_home_complete_path/path/adb.exe')
+          expect(android_env.aapt_path).to eq('screengrab/spec/fixtures/empty_home_complete_path/path/aapt.exe')
+        end
+      end
+    end
+
     describe "with a complete ANDROID_HOME and a complete PATH and no build tools version specified" do
       it "finds adb in platform-tools and aapt in the highest version build tools dir" do
         FastlaneSpec::Env.with_env_values('PATH' => 'screengrab/spec/fixtures/complete_home_complete_path/path') do
@@ -41,6 +57,22 @@ describe Screengrab do
           expect(android_env.platform_tools_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/platform-tools')
           expect(android_env.adb_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/platform-tools/adb')
           expect(android_env.aapt_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/build-tools/23.0.2/aapt')
+        end
+      end
+    end
+
+    describe "with a complete ANDROID_HOME and a complete PATH and no build tools version specified on Windows" do
+      it "finds adb.exe in platform-tools and aapt in the highest version build tools dir" do
+        allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+        FastlaneSpec::Env.with_env_values('PATH' => 'screengrab/spec/fixtures/complete_home_complete_path/path') do
+          android_env = Screengrab::AndroidEnvironment.new('screengrab/spec/fixtures/complete_home_complete_path/android_home', nil)
+
+          expect(android_env.android_home).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home')
+          expect(android_env.build_tools_version).to be_nil
+          expect(android_env.build_tools_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/build-tools/23.0.2')
+          expect(android_env.platform_tools_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/platform-tools')
+          expect(android_env.adb_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/platform-tools/adb.exe')
+          expect(android_env.aapt_path).to eq('screengrab/spec/fixtures/complete_home_complete_path/android_home/build-tools/23.0.2/aapt.exe')
         end
       end
     end
