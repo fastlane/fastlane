ef8cab122c16602f087c82024f34cfc4f832a5ff
Josh Holtz <josh@rokkincat.com>
2020-03-18 09:01:04 -0500

    Started to rework tests

--- sigh/spec/manager_spec.rb
+++ sigh/spec/manager_spec.rb
@@ -5,6 +5,15 @@ describe Sigh do
       ENV["DELIVER_PASSWORD"] = "123"
     end
 
+    let(:mock_base_client) { "fake api base client" }
+
+    before(:each) do
+      allow(mock_base_client).to receive(:login)
+      allow(mock_base_client).to receive(:team_id).and_return('')
+
+      allow(Spaceship::ConnectAPI::Provisioning::Client).to receive(:instance).and_return(mock_base_client)
+    end
+
     it "Successful run" do
       sigh_stub_spaceship
       options = { app_identifier: "com.krausefx.app", skip_install: true, skip_certificate_verification: true }
