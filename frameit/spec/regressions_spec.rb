7b6c45a5a93eb49317deff678603cfb8283a7152
Bruno Virlet <bruno.virlet@gmail.com>
2020-04-14 16:55:55 +0200

    fix(frameit|spec) Add additional regression tests

--- frameit/spec/regressions_spec.rb
+++ frameit/spec/regressions_spec.rb
@@ -1,7 +1,11 @@
 require 'imatcher'
 
 describe Frameit do
-  describe 'Regressions', :focus do
+  describe 'Regressions' do
+    before :each do
+      Frameit.config = {}
+    end
+
     let(:matcher) { Imatcher::Matcher.new }
 
     def path(path)
@@ -9,7 +13,6 @@ describe Frameit do
     end
 
     before :each do
-      #FastlaneCore::Globals.verbose = true
       allow(Frameit::FrameDownloader).to receive(:templates_path).and_return(path('devices'))
     end
 
@@ -19,8 +22,30 @@ describe Frameit do
 
         expect(matcher.compare(
           path('short-title/fr-FR/iPhone 7 Plus-screenshot_expectation_framed.png'),
-          path('short-title/fr-FR/iPhone 7 Plus-screenshot_framed.png'),
-          ).match?).to be_truthy
+          path('short-title/fr-FR/iPhone 7 Plus-screenshot_framed.png')
+        ).match?).to be_truthy
+      end
+    end
+
+    describe 'a long text' do
+      it 'generates the proper image' do
+        Frameit::Runner.new.run(path('long-title'), nil, Platform::IOS)
+
+        expect(matcher.compare(
+          path('long-title/fr-FR/iPhone 7 Plus-screenshot_expectation_framed.png'),
+          path('long-title/fr-FR/iPhone 7 Plus-screenshot_framed.png')
+        ).match?).to be_truthy
+      end
+    end
+
+    describe 'a multiline text' do
+      it 'generates the proper image' do
+        Frameit::Runner.new.run(path('multiline'), nil, Platform::IOS)
+
+        expect(matcher.compare(
+          path('multiline/fr-FR/iPhone 7 Plus-screenshot_expectation_framed.png'),
+          path('multiline/fr-FR/iPhone 7 Plus-screenshot_framed.png')
+        ).match?).to be_truthy
       end
     end
 
@@ -31,12 +56,34 @@ describe Frameit do
         expect(matcher.compare(
           path('apostrophes/fr-FR/iPhone 7 Plus-apostrophes_expectation_framed.png'),
           path('apostrophes/fr-FR/iPhone 7 Plus-apostrophes_framed.png')
-          ).match?).to be_truthy
+        ).match?).to be_truthy
 
         expect(matcher.compare(
           path('apostrophes/fr-FR/iPhone 7 Plus-apostrophes_expectation_framed.png'), # Same fixture as above
           path('apostrophes/fr-FR/iPhone 7 Plus-escaped-apostrophes_framed.png')
-          ).match?).to be_truthy
+        ).match?).to be_truthy
+      end
+    end
+
+    describe 'keywords' do
+      it 'generates the proper image' do
+        Frameit::Runner.new.run(path('keywords'), nil, Platform::IOS)
+
+        expect(matcher.compare(
+          path('keywords/fr-FR/iPhone 7 Plus-screenshot_expectation_framed.png'),
+          path('keywords/fr-FR/iPhone 7 Plus-screenshot_framed.png')
+        ).match?).to be_truthy
+      end
+    end
+
+    describe 'stacked-keywords' do
+      it 'generates the proper image' do
+        Frameit::Runner.new.run(path('stacked-keywords'), nil, Platform::IOS)
+
+        expect(matcher.compare(
+          path('stacked-keywords/fr-FR/iPhone 7 Plus-screenshot_expectation_framed.png'),
+          path('stacked-keywords/fr-FR/iPhone 7 Plus-screenshot_framed.png')
+        ).match?).to be_truthy
       end
     end
   end
