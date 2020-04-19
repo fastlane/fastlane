502744d0cabc8e523c841337d152bc3388e7ed83
Youngern Song <ysong028@gmail.com>
2020-04-19 09:25:14 -0500

    [match] Fix S3 errors for match nuke and sync (#16335)
    * add tests for s3 client helper
    
    * use correct variable for method arg
    
    * use object.key
    
    * add tests for upload, download, delete
    
    * rubocop

--- fastlane/lib/fastlane/helper/s3_client_helper.rb
+++ fastlane/lib/fastlane/helper/s3_client_helper.rb
@@ -7,16 +7,14 @@ module Fastlane
 
       def initialize(access_key: nil, secret_access_key: nil, region: nil)
         creds = Aws::Credentials.new(access_key, secret_access_key)
-        Aws.config.update({
+        Aws.config.update(
           region: region,
           credentials: creds
-        })
-
-        @client = Aws::S3::Client.new
+        )
       end
 
       def list_buckets
-        return @client.list_buckets
+        return client.list_buckets
       end
 
       def upload_file(bucket_name, file_name, file_data, acl)
@@ -40,17 +38,24 @@ module Fastlane
         obj.public_url.to_s
       end
 
-      def delete_file(bucket, file_name)
+      def delete_file(bucket_name, file_name)
         bucket = find_bucket!(bucket_name)
-        bucket.objects[file_name].delete
+        file = bucket.object(file_name)
+        file.delete
       end
 
       def find_bucket!(bucket_name)
-        bucket = Aws::S3::Bucket.new(bucket_name, client: @client)
+        bucket = Aws::S3::Bucket.new(bucket_name, client: client)
         raise "Bucket '#{bucket_name}' not found" unless bucket.exists?
 
         return bucket
       end
+    end
+
+    private
+
+    def client
+      @client ||= Aws::S3::Client.new
     end
   end
 end
---
+++ match/spec/storage/s3_storage_spec.rb
@@ -0,0 +1,73 @@
+describe Match do
+  describe Match::Storage::S3Storage do
+    subject { described_class.new(s3_region: nil, s3_access_key: nil, s3_secret_access_key: nil, s3_bucket: 'foobar') }
+    let(:working_directory) { '/var/folders/px/abcdefghijklmnop/T/d20181026-96528-1av4gge' }
+
+    before do
+      allow(subject).to receive(:working_directory).and_return(working_directory)
+      allow(subject).to receive(:s3_client).and_return(s3_client)
+    end
+
+    describe '#upload_files' do
+      let(:files_to_upload) do
+        [
+          "#{working_directory}/ABCDEFG/certs/development/ABCDEFG.cer",
+          "#{working_directory}/ABCDEFG/certs/development/ABCDEFG.p12"
+        ]
+      end
+      let(:s3_client) { double(upload_file: true) }
+      let!(:file) { class_double('File', read: 'body').as_stubbed_const }
+
+      it 'reads the correct files from local storage' do
+        files_to_upload.each do |file_name|
+          expect(file).to receive(:read).with(file_name)
+        end
+
+        subject.upload_files(files_to_upload: files_to_upload)
+      end
+
+      it 'uploads files to the correct path in remote storage' do
+        expect(s3_client).to receive(:upload_file).with('foobar', 'ABCDEFG/certs/development/ABCDEFG.cer', 'body', 'private')
+        expect(s3_client).to receive(:upload_file).with('foobar', 'ABCDEFG/certs/development/ABCDEFG.p12', 'body', 'private')
+        subject.upload_files(files_to_upload: files_to_upload)
+      end
+    end
+
+    describe '#delete_files' do
+      let(:files_to_upload) do
+        [
+          "#{working_directory}/ABCDEFG/certs/development/ABCDEFG.cer",
+          "#{working_directory}/ABCDEFG/certs/development/ABCDEFG.p12"
+        ]
+      end
+      let(:s3_client) { double(delete_file: true) }
+
+      it 'deletes files with correct paths' do
+        expect(s3_client).to receive(:delete_file).with('foobar', 'ABCDEFG/certs/development/ABCDEFG.cer')
+        expect(s3_client).to receive(:delete_file).with('foobar', 'ABCDEFG/certs/development/ABCDEFG.p12')
+
+        subject.delete_files(files_to_delete: files_to_upload)
+      end
+    end
+
+    describe '#download' do
+      let(:files_to_download) do
+        [
+          instance_double('Aws::S3::Object', key: 'ABCDEFG/certs/development/ABCDEFG.cer', download_file: true),
+          instance_double('Aws::S3::Object', key: 'ABCDEFG/certs/development/ABCDEFG.p12', download_file: true)
+        ]
+      end
+      let(:s3_client) { instance_double('Fastlane::Helper::S3ClientHelper', find_bucket!: double(objects: files_to_download)) }
+
+      before { class_double('FileUtils', mkdir_p: true).as_stubbed_const }
+
+      it 'downloads to correct working directory' do
+        files_to_download.each do |file_object|
+          expect(file_object).to receive(:download_file).with("#{working_directory}/#{file_object.key}")
+        end
+
+        subject.download
+      end
+    end
+  end
+end
