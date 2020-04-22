d15fbf9bae5a2d014f498b518d7abeaba96a4b11
Josh Holtz <josh@rokkincat.com>
2020-04-21 21:20:13 -0500

    [action] allow filtering by build configuration in update_code_sign_settings (#16353)
    * [action] allow filtering by build configuration in update_code_sign_settings
    
    * Updated tests for update_code_sign_settings
    
    * Delete rouge team id

--- fastlane/lib/fastlane/actions/update_code_signing_settings.rb
+++ fastlane/lib/fastlane/actions/update_code_signing_settings.rb
@@ -16,72 +16,62 @@ module Fastlane
           return false
         end
 
-        target_dictionary = project.targets.map { |f| { name: f.name, uuid: f.uuid, build_configuration_list: f.build_configuration_list } }
-        target_attributes = project.root_object.attributes["TargetAttributes"]
         changed_targets = []
+        changed_build_configurations = []
 
-        # make sure TargetAttributes exist for all targets
-        target_dictionary.each do |props|
-          unless target_attributes.key?(props[:uuid])
-            target_attributes[props[:uuid]] = {}
-          end
-        end
-
-        target_attributes.each do |target, sett|
-          found_target = target_dictionary.detect { |h| h[:uuid] == target }
+        project.targets.each do |target|
           if params[:targets]
-            # get target name
-            unless params[:targets].include?(found_target[:name])
-              UI.important("Skipping #{found_target[:name]} not selected (#{params[:targets].join(',')})")
+            unless params[:targets].include?(target.name)
+              UI.important("Skipping #{target.name} not selected (#{params[:targets].join(',')})")
               next
             end
           end
 
-          style_value = params[:use_automatic_signing] ? 'Automatic' : 'Manual'
-          build_configuration_list = found_target[:build_configuration_list]
-          build_configuration_list.set_setting("CODE_SIGN_STYLE", style_value)
-          sett["ProvisioningStyle"] = style_value
-
-          if params[:team_id]
-            sett["DevelopmentTeam"] = params[:team_id]
-            build_configuration_list.set_setting("DEVELOPMENT_TEAM", params[:team_id])
-            UI.important("Set Team id to: #{params[:team_id]} for target: #{found_target[:name]}")
-          end
-          if params[:code_sign_identity]
-            build_configuration_list.set_setting("CODE_SIGN_IDENTITY", params[:code_sign_identity])
-
-            # We also need to update the value if it was overridden for a specific SDK
-            build_configuration_list.build_configurations.each do |build_configuration|
-              codesign_build_settings_keys = build_configuration.build_settings.keys.select { |key| key.to_s.match(/CODE_SIGN_IDENTITY.*/) }
-              codesign_build_settings_keys.each do |setting|
-                build_configuration_list.set_setting(setting, params[:code_sign_identity])
+          target.build_configurations.each do |config|
+            if params[:build_configurations]
+              unless params[:build_configurations].include?(config.name)
+                UI.important("Skipping #{config.name} not selected (#{params[:build_configurations].join(',')})")
+                next
               end
             end
-            UI.important("Set Code Sign identity to: #{params[:code_sign_identity]} for target: #{found_target[:name]}")
-          end
-          if params[:profile_name]
-            build_configuration_list.set_setting("PROVISIONING_PROFILE_SPECIFIER", params[:profile_name])
-            UI.important("Set Provisioning Profile name to: #{params[:profile_name]} for target: #{found_target[:name]}")
-          end
-          # Since Xcode 8, this is no longer needed, you simply use PROVISIONING_PROFILE_SPECIFIER
-          if params[:profile_uuid]
-            build_configuration_list.set_setting("PROVISIONING_PROFILE", params[:profile_uuid])
-            UI.important("Set Provisioning Profile UUID to: #{params[:profile_uuid]} for target: #{found_target[:name]}")
-          end
-          if params[:bundle_identifier]
-            build_configuration_list.set_setting("PRODUCT_BUNDLE_IDENTIFIER", params[:bundle_identifier])
-            UI.important("Set Bundle identifier to: #{params[:bundle_identifier]} for target: #{found_target[:name]}")
+
+            style_value = params[:use_automatic_signing] ? 'Automatic' : 'Manual'
+            set_build_setting(config, "CODE_SIGN_STYLE", style_value)
+
+            if params[:team_id]
+              set_build_setting(config, "DEVELOPMENT_TEAM", params[:team_id])
+              UI.important("Set Team id to: #{params[:team_id]} for target: #{target.name} for build configuration: #{config.name}")
+            end
+            if params[:code_sign_identity]
+              set_build_setting(config, "CODE_SIGN_IDENTITY", params[:code_sign_identity])
+              UI.important("Set Code Sign identity to: #{params[:code_sign_identity]} for target: #{target.name} for build configuration: #{config.name}")
+            end
+            if params[:profile_name]
+              set_build_setting(config, "PROVISIONING_PROFILE_SPECIFIER", params[:profile_name])
+              UI.important("Set Provisioning Profile name to: #{params[:profile_name]} for target: #{target.name} for build configuration: #{config.name}")
+            end
+            # Since Xcode 8, this is no longer needed, you simply use PROVISIONING_PROFILE_SPECIFIER
+            if params[:profile_uuid]
+              set_build_setting(config, "PROVISIONING_PROFILE", params[:profile_uuid])
+              UI.important("Set Provisioning Profile UUID to: #{params[:profile_uuid]} for target: #{target.name} for build configuration: #{config.name}")
+            end
+            if params[:bundle_identifier]
+              set_build_setting(config, "PRODUCT_BUNDLE_IDENTIFIER", params[:bundle_identifier])
+              UI.important("Set Bundle identifier to: #{params[:bundle_identifier]} for target: #{target.name} for build configuration: #{config.name}")
+            end
+
+            changed_build_configurations << config.name
           end
 
-          changed_targets << found_target[:name]
+          changed_targets << target.name
         end
         project.save
 
         if changed_targets.empty?
           UI.important("None of the specified targets has been modified")
           UI.important("available targets:")
-          target_dictionary.each do |target|
-            UI.important("\t* #{target[:name]}")
+          project.targets.each do |target|
+            UI.important("\t* #{target.name}")
           end
         else
           UI.success("Successfully updated project settings to use Code Sign Style = '#{params[:use_automatic_signing] ? 'Automatic' : 'Manual'}'")
@@ -89,9 +79,21 @@ module Fastlane
           changed_targets.each do |target|
             UI.success("\t * #{target}")
           end
+
+          UI.success("Modified Build Configurations:")
+          changed_build_configurations.each do |name|
+            UI.success("\t * #{name}")
+          end
         end
 
         params[:use_automatic_signing]
+      end
+
+      def self.set_build_setting(configuration, name, value)
+        codesign_build_settings_keys = configuration.build_settings.keys.select { |key| key.to_s.match(/#{name}.*/) }
+        codesign_build_settings_keys.each do |key|
+          configuration.build_settings[key] = value
+        end
       end
 
       def self.description
@@ -128,6 +130,12 @@ module Fastlane
                                        optional: true,
                                        type: Array,
                                        description: "Specify targets you want to toggle the signing mech. (default to all targets)",
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :build_configurations,
+                                       env_name: "FL_PROJECT_SIGNING_BUILD_CONFIGURATIONS",
+                                       optional: true,
+                                       type: Array,
+                                       description: "Specify build_configurations you want to toggle the signing mech. (default to all targets)",
                                        is_string: false),
           FastlaneCore::ConfigItem.new(key: :code_sign_identity,
                                        env_name: "FL_CODE_SIGN_IDENTITY",
