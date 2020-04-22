a2b021d79c5cf06f67c44fbc5340b191351eaad5
Josh Holtz <josh@rokkincat.com>
2020-04-22 12:28:32 -0500

    Version bump to 2.146.1 (#16358)

--- fastlane/swift/Deliverfile.swift
+++ fastlane/swift/Deliverfile.swift
@@ -18,4 +18,4 @@ class Deliverfile: DeliverfileProtocol {
 
 
 
-// Generated with fastlane 2.146.0
+// Generated with fastlane 2.146.1
--- fastlane/swift/Precheckfile.swift
+++ fastlane/swift/Precheckfile.swift
@@ -18,4 +18,4 @@ class Precheckfile: PrecheckfileProtocol {
 
 
 
-// Generated with fastlane 2.146.0
+// Generated with fastlane 2.146.1
