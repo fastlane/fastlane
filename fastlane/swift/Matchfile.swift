4558261f07f2301da32d8597483e26f48c128193
Josh Holtz <josh@rokkincat.com>
2020-04-21 22:01:07 -0500

    Version bump to 2.146.0 (#16354)

--- fastlane/swift/Matchfile.swift
+++ fastlane/swift/Matchfile.swift
@@ -18,4 +18,4 @@ class Matchfile: MatchfileProtocol {
 
 
 
-// Generated with fastlane 2.145.0
+// Generated with fastlane 2.146.0
--- fastlane/swift/Scanfile.swift
+++ fastlane/swift/Scanfile.swift
@@ -18,4 +18,4 @@ class Scanfile: ScanfileProtocol {
 
 
 
-// Generated with fastlane 2.145.0
+// Generated with fastlane 2.146.0
