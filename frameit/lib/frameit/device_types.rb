e43807fb34a2551596d6c3ed7f81374f46e1c06d
Miloš Černilovský <milhauscz@users.noreply.github.com>
2020-05-12 03:03:38 +0200

    [frameit] Automatically determine platform from fastfile (#16428)
    * Adding Android support to frameit
    Frameit refactoring and decoupling from the deliver module
    New Device class which contains all necessary device information: id, device name, priority, resolutions, screen pixel density, default color and platform
    Added "use_platform" parameter which can be specified in Framefile.json, fastfile or passed via CLI parameters; default is iOS for backward compatibility
    Added new "android" and "ios" commands for frameit
    Updated frames_generator to include Android devices and exclude some deprecated devices
    Updated frames_generator's image magick commands to remove shadows and correctly process Android devices (specifically Samsung Galaxy S9 whose screen contains shadows representing round edges)
    Updated frameit docs with Android support
    Created unit tests for the new Device class
    Fixed force_device_type option which didn't work before, also added support to Framefile.json
    Added iPhone 8 support
    Added new option :use_legacy_iphone7 to prefer iPhone 7 over iPhone 8
    Code style fixes according to rubocop
    
    * Modified some frames_generator comments according to the discussion
    
    * Added iPad Pro (12.9-inch) (3rd generation) to ensure backward compatibility with PR #14787
    
    * Added missing "Apple" to the iPad 12.9 inch 3rd generation name
    Searching algorithm compares filename with the device's name without "Apple" for backward compatibility
    
    * Updated documentation - "Set of Android devices"
    
    Co-Authored-By: Jan Piotrowski <piotrowski+github@gmail.com>
    
    * Refactored checking frame colors in config_parser.rb according to @mollyIV's suggestion
    
    * Refactored REQUIRED_PRIORITY according to @mollyIV's suggestion
    
    * Minor if refactorings according to @mollyIV's suggestion
    
    * Removed a redundant function in editor
    
    * Reverted a comment modification
    
    * Refactoring of checking config / options
    
    * Fixes according to rubocop (added parenthesis to a method call and lowered perceived complexity of config_parser.validate_key)
    
    * frames_generator will sort devices in JSONs alphabetically according to @janpio's suggestion
    
    * Added new devices from PR #15373 as a fix for merge conflict in frameit/lib/frameit/screenshot.rb
    
    * Updated the frames generator to correctly rename iPads in the latest Facebook devices
    Updated device_spec to count with newer iPhone 11 Pro
    Added new devices: Google Pixel 4, Google Pixel 4 XL, Samsung Galaxy Note 10, Samsung Galaxy Note 10+, Samsung Galaxy S10, Samsung Galaxy S10+, Xiaomi Mi Mix Alpha, iPhone 11, iPhone 11 Pro, iPhone 11 Pro Max, iPad 10.2, iPad Air 2019, iPad Mini 2019
    
    * Added missing number for iPhone 11
    
    * Increased Pixel 4's priority to be higher than Galaxy S10
    Fixed name for iPad Pro 12.9 3rd gen.
    Fixed device name sanitization in offsets.rb
    Created legacy options for iPhone XR, XS and XS Max
    
    * Added Android as a supported platform in frame_screenshots
    
    * Failed test fix - do not use frameit for checking unsupported platform since it already supports Android; replaced by team_name
    
    * Failed test fix - do not use frameit for checking unsupported platform since it already supports Android; replaced by team_name
    
    * Adding support for automatically determining platform used in frameit for detecting device frames from fastfile's general defined platform unless configured otherwise.
    
    * Code style fixes
    
    * Added missing require statement
    
    * Move fetching platform and lane to helper with tests
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>
    Co-authored-by: Jan Piotrowski <piotrowski+git@gmail.com>
    Co-authored-by: Josh Holtz <josh@rokkincat.com>

--- frameit/lib/frameit/device_types.rb
+++ frameit/lib/frameit/device_types.rb
@@ -64,6 +64,14 @@ module Frameit
     def self.all_platforms
       Platform.constants.map { |c| Platform.const_get(c) }
     end
+
+    def self.symbol_to_constant(symbol)
+      if symbol == :android
+        ANDROID
+      else
+        IOS
+      end
+    end
   end
 
   module Devices
