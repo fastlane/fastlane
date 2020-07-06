a9e1f730e157b92ecbbd38a9ed895cc49d8902c1
Ray Deck <ray@raydeck.com>
2020-07-06 15:18:01 -0500

    [fastlane] Extend socket failure payload (#16632)
    * Adding port setter to action interface of socket_server
    
    * Trailing whitespace caused lint failure
    
    * Adding error detail for socket server
    
    * Fixing trailing whitespace
    
    * Clearing blank spaces from blank line (thank you rubocop)

--- fastlane/lib/fastlane/server/socket_server.rb
+++ fastlane/lib/fastlane/server/socket_server.rb
@@ -215,6 +215,10 @@ module Fastlane
       exception_array << "#{e.class}:"
       exception_array << e.backtrace
 
+      ec = e.class
+
+      em = e.message
+
       while e.respond_to?("cause") && (e = e.cause)
         exception_array << "cause: #{e.class}"
         exception_array << e.backtrace
@@ -223,7 +227,9 @@ module Fastlane
       payload = {
         payload: {
           status: "failure",
-          failure_information: exception_array.flatten
+          failure_information: exception_array.flatten,
+          failure_class: ec,
+          failure_message: em
         }
       }
       return JSON.generate(payload)
