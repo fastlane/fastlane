94cd231c47d4dbb899109c88feeb6168c6412f03
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-07-11 17:08:23 -0400

    [scan] Fix method for pre-deleting existing logarchive

--- fastlane_core/lib/fastlane_core/device_manager.rb
+++ fastlane_core/lib/fastlane_core/device_manager.rb
@@ -338,10 +338,10 @@ module FastlaneCore
       def copy_logarchive(device, log_identity, logs_destination_dir)
         require 'shellwords'
 
-        logarchive_dst = File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive").shellescape
+        logarchive_dst = File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive")
         FileUtils.rm_rf(logarchive_dst)
         FileUtils.mkdir_p(File.expand_path("..", logarchive_dst))
-        command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst} 2>/dev/null"
+        command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst.shellescape} 2>/dev/null"
         FastlaneCore::CommandExecutor.execute(command: command, print_all: false, print_command: true)
       end
     end
