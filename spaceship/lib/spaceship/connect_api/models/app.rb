c45e38b3e41730c18c2acfdd0a20c2aa679ab006
Josh Holtz <josh@rokkincat.com>
2020-07-20 16:24:02 -0500

    Made territories fetch all pages because it seems necessary :)

--- spaceship/lib/spaceship/connect_api/models/app.rb
+++ spaceship/lib/spaceship/connect_api/models/app.rb
@@ -101,8 +101,8 @@ module Spaceship
 
       def fetch_available_territories(filter: {}, includes: nil, limit: nil, sort: nil)
         filter ||= {}
-        resp = Spaceship::ConnectAPI.get_available_territories(app_id: id, filter: filter, includes: includes, limit: limit, sort: sort)
-        return resp.to_models
+        resps = Spaceship::ConnectAPI.get_available_territories(app_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        return resps.flat_map(&:to_models)
       end
 
       #
