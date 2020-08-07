a09ef5446fde352df5e0b443253ceadf9465423a
Josh Holtz <josh@rokkincat.com>
2020-08-07 07:27:09 -0500

    Version bump to 2.155.2 (#17020)

--- fastlane/swift/Screengrabfile.swift
+++ fastlane/swift/Screengrabfile.swift
@@ -14,4 +14,4 @@ class Screengrabfile: ScreengrabfileProtocol {
     // during the `init` process, and you won't see this message
 }
 
-// Generated with fastlane 2.155.1
+// Generated with fastlane 2.155.2
