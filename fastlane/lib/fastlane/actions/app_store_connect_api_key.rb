51816aedfdd02c837d46757592b50e2427ae0e68
Josh Holtz <josh@rokkincat.com>
2020-08-20 18:27:14 -0500

    More tests for new action

--- fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
+++ fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
@@ -6,14 +6,23 @@ module Fastlane
 
     class AppStoreConnectApiKeyAction < Action
       def self.run(options)
-        key_id = options[:id]
+        key_id = options[:key_id]
         issuer_id = options[:issuer_id]
-        filepath = options[:filepath]
+        key_content = options[:key_content]
+        filepath = options[:key_filepath]
+        duration = options[:duration]
+        in_house = options[:in_house]
+
+        if key_content.nil? && filepath.nil?
+          UI.user_error!(":key_content or :key_filepath is required")
+        end
 
         key = {
           key_id: key_id,
           issuer_id: issuer_id,
-          key: File.binread(filepath)
+          key: key_content || File.binread(filepath),
+          duration: duration,
+          in_house: in_house
         }
 
         Actions.lane_context[SharedValues::APP_STORE_CONNECT_API_KEY] = key
@@ -27,18 +36,35 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :id,
-                                       env_name: "APP_STORE_CONNECT_API_KEY_ID",
-                                       description: "App Store Connect API key ID"),
+          FastlaneCore::ConfigItem.new(key: :key_id,
+                                       env_name: "APP_STORE_CONNECT_API_KEY_KEY_ID",
+                                       description: "The key ID"),
           FastlaneCore::ConfigItem.new(key: :issuer_id,
                                        env_name: "APP_STORE_CONNECT_API_KEY_ISSUER_ID",
-                                       description: "App Store Connect API key issuer ID"),
-          FastlaneCore::ConfigItem.new(key: :filepath,
-                                       env_name: "APP_STORE_CONNECT_API_KEY_FILEPATH",
-                                       description: "Path to your App Store Connect API key p8 file",
+                                       description: "The issuer ID"),
+          FastlaneCore::ConfigItem.new(key: :key_filepath,
+                                       env_name: "APP_STORE_CONNECT_API_KEY_KEY_FILEPATH",
+                                       description: "The path to the key p8 file",
+                                       optional: true,
+                                       conflicting_options: [:key_content],
                                        verify_block: proc do |value|
                                          UI.user_error!("Couldn't find key p8 file at path '#{value}'") unless File.exist?(value)
-                                       end)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :key_content,
+                                       env_name: "APP_STORE_CONNECT_API_KEY_KEY",
+                                       description: "The content of the key p8 file",
+                                       optional: true,
+                                       conflicting_options: [:filepath]),
+          FastlaneCore::ConfigItem.new(key: :duration,
+                                       env_name: "APP_STORE_CONNECT_API_KEY_DURATION",
+                                       description: "The token session duration",
+                                       optional: true,
+                                       type: Integer),
+          FastlaneCore::ConfigItem.new(key: :in_house,
+                                       env_name: "APP_STORE_CONNECT_API_KEY_IN_HOUSE",
+                                       description: "Is App Store or Enterprise (in house) team? App Store Connect API cannot not determine this on its own (yet)",
+                                       optional: true,
+                                       type: Boolean)
         ]
       end
 
---
+++ fastlane/spec/actions_specs/app_store_connect_api_key_spec.rb
@@ -0,0 +1,67 @@
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe "App Store Connect API Key" do
+      let(:fake_api_key_p8_path) { File.absolute_path("./spaceship/spec/connect_api/fixtures/asc_key.p8") }
+      let(:fake_api_key_json_path) { "./spaceship/spec/connect_api/fixtures/asc_key.json" }
+      let(:fake_api_key_in_house_json_path) { "./spaceship/spec/connect_api/fixtures/asc_key_in_house.json" }
+
+      let(:key_id) { "D484D98393" }
+      let(:issuer_id) { "32423-234234-234324-234" }
+
+      it "with key_filepath" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_connect_api_key(
+            key_id: '#{key_id}',
+            issuer_id: '#{issuer_id}',
+            key_filepath: '#{fake_api_key_p8_path}',
+          )
+        end").runner.execute(:test)
+
+        hash = {
+          key_id: key_id,
+          issuer_id: issuer_id,
+          key: File.binread(fake_api_key_p8_path),
+          duration: nil,
+          in_house: nil
+        }
+
+        expect(result).to eq(hash)
+      end
+
+      it "with key_content" do
+        key_content = File.binread(fake_api_key_p8_path)
+
+        result = Fastlane::FastFile.new.parse("lane :test do
+          app_store_connect_api_key(
+            key_id: '#{key_id}',
+            issuer_id: '#{issuer_id}',
+            key_content: '#{key_content}',
+            duration: 200,
+            in_house: true
+          )
+        end").runner.execute(:test)
+
+        hash = {
+          key_id: key_id,
+          issuer_id: issuer_id,
+          key: key_content,
+          duration: 200,
+          in_house: true
+        }
+
+        expect(result).to eq(hash)
+      end
+
+      it "raise error when no key_filepath or key_content" do
+        expect do
+          Fastlane::FastFile.new.parse("lane :test do
+            app_store_connect_api_key(
+              key_id: '#{key_id}',
+              issuer_id: '#{issuer_id}'
+            )
+          end").runner.execute(:test)
+        end.to raise_error(/:key_content or :key_filepath is required/)
+      end
+    end
+  end
+end
