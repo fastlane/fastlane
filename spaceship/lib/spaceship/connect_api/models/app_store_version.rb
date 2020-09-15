96681aa056323435ee803595583c354aa11adf0d
Josh Holtz <josh@rokkincat.com>
2020-09-15 16:38:53 -0500

    [deliver] add support for App Store Connect API Key (#17238)
    * It mostly works
    
    * Download works
    
    * Rubo
    
    * Some cleanup
    
    * Fixed tests
    
    * Precheck mostly works with ASC API
    
    * Spelling

--- spaceship/lib/spaceship/connect_api/models/app_store_version.rb
+++ spaceship/lib/spaceship/connect_api/models/app_store_version.rb
@@ -114,11 +114,8 @@ module Spaceship
         return resp.to_models.first
       end
 
-      # appScreenshotSets,appPreviewSets
-      def get_app_store_version_localizations(filter: {}, includes: "appScreenshotSets", limit: nil, sort: nil)
-        filter ||= {}
-        filter["appStoreVersion"] = id
-        return Spaceship::ConnectAPI::AppStoreVersionLocalization.all(filter: filter, includes: includes, limit: limit, sort: sort)
+      def get_app_store_version_localizations(filter: {}, includes: nil, limit: nil, sort: nil)
+        return Spaceship::ConnectAPI::AppStoreVersionLocalization.all(app_store_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort)
       end
 
       #
