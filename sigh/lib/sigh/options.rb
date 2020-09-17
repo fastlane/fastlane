0f41a125cb9fe7a0d72cc242078002c0482c053c
Josh Holtz <josh@rokkincat.com>
2020-09-17 15:25:17 -0500

    Whoops... copy paste error

--- sigh/lib/sigh/options.rb
+++ sigh/lib/sigh/options.rb
@@ -57,7 +57,7 @@ module Sigh
 
         # App Store Connect API
         FastlaneCore::ConfigItem.new(key: :api_key_path,
-                                     env_name: "DELIVER_API_KEY_PATH",
+                                     env_name: "SIGH_API_KEY_PATH",
                                      description: "Path to your App Store Connect API Key JSON file (https://docs.fastlane.tools/app-store-connect-api/#using-fastlane-api-key-json-file)",
                                      optional: true,
                                      conflicting_options: [:username],
@@ -65,7 +65,7 @@ module Sigh
                                        UI.user_error!("Couldn't find API key JSON file at path '#{value}'") unless File.exist?(value)
                                      end),
         FastlaneCore::ConfigItem.new(key: :api_key,
-                                     env_name: "DELIVER_API_KEY",
+                                     env_name: "SIGH_API_KEY",
                                      description: "Your App Store Connect API Key information (https://docs.fastlane.tools/app-store-connect-api/#use-return-value-and-pass-in-as-an-option)",
                                      type: Hash,
                                      optional: true,
