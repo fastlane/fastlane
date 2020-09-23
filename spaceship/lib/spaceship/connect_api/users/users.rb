dcbac9ad87267f64746eacc2cc28b2266a26b3f3
Max Ott <max.ott@me.com>
2020-09-23 13:32:27 +0200

    Add ability to add and remove users

--- spaceship/lib/spaceship/connect_api/users/users.rb
+++ spaceship/lib/spaceship/connect_api/users/users.rb
@@ -14,14 +14,50 @@ module Spaceship
         end
 
         #
-        # users
+        # users and invitations
         #
 
+        # Get all accepted users
         def get_users(filter: {}, includes: nil, limit: nil, sort: nil)
           params = users_request_client.build_params(filter: filter, includes: includes, limit: limit, sort: sort)
           users_request_client.get("users", params)
         end
 
+        # Get all invited users (not yet accepted)
+        def get_invited_users(filter: {}, includes: nil, limit: nil, sort: nil)
+          params = users_request_client.build_params(filter: filter, includes: includes, limit: limit, sort: sort)
+          users_request_client.get("userInvitations", params)
+        end
+
+        # Invite new users to App Store Connect
+        def invite_user(email: nil, first_name: nil, last_name: nil, roles: [], provisioning_allowed: nil, all_apps_visible: nil)
+          body = {
+            data: {
+              type: "userInvitations",
+              attributes: {
+                email: email,
+                firstName: first_name,
+                lastName: last_name,
+                roles: roles,
+                provisioningAllowed: provisioning_allowed,
+                allAppsVisible: all_apps_visible
+              }
+            }
+          }
+          users_request_client.post("userInvitations", body)
+        end
+
+        # Remove existing user from team
+        def remove_user(user_id: nil)
+          users_request_client.delete("users/#{user_id}")
+        end
+
+        # Remove invited user from team (not yet accepted)
+        def remove_invited_user(user_id: nil)
+          users_request_client.delete("userInvitations/#{user_id}")
+        end
+
+        # Add apps to user
         def add_user_visible_apps(user_id: nil, app_ids: nil)
           body = {
             data: app_ids.map do |app_id|
