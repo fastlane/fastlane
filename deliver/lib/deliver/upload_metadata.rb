991e9f5a3244b43256dace0a1ae0d4c4f2170161
Lancer TENG <78370@qq.com>
2020-09-23 19:38:36 +0800

    fix(deliver/upload_metadata/review_info): transform symbol key to string, since hash args in options from CLI keyed by string

--- deliver/lib/deliver/upload_metadata.rb
+++ deliver/lib/deliver/upload_metadata.rb
@@ -583,7 +583,7 @@ module Deliver
 
     def set_review_information(version, options)
       return unless options[:app_review_information]
-      info = options[:app_review_information]
+      info = options[:app_review_information].transform_keys(&:to_sym)
       UI.user_error!("`app_review_information` must be a hash", show_github_issues: true) unless info.kind_of?(Hash)
 
       attributes = {}
