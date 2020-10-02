de2e530324da18857a044a796eec297165ba26d6
Pierre Felgines <pierre.felgines@gmail.com>
2020-10-02 14:20:57 +0200

    Revert auto incremented version number

--- snapshot/lib/assets/SnapshotHelper.swift
+++ snapshot/lib/assets/SnapshotHelper.swift
@@ -298,4 +298,4 @@ private extension CGFloat {
 
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
-// SnapshotHelperVersion [1.25]
+// SnapshotHelperVersion [1.24]
