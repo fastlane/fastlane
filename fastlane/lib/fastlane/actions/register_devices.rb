5370be98d8b84c80bb9db63fbbff186a8ce8a56c
Josh Holtz <josh@rokkincat.com>
2020-10-13 13:38:02 -0500

    [action] fix register_devices to compare udids without case

--- fastlane/lib/fastlane/actions/register_devices.rb
+++ fastlane/lib/fastlane/actions/register_devices.rb
@@ -53,7 +53,10 @@ module Fastlane
         existing_devices = Spaceship::ConnectAPI::Device.all
 
         device_objs = new_devices.map do |device|
-          next if existing_devices.map(&:udid).include?(device[0])
+          if existing_devices.map(&:udid).map(&:downcase).include?(device[0].downcase)
+            UI.verbose("UDID #{device[0]} already exists - Skipping...")
+            next
+          end
 
           device_platform = platform
 
