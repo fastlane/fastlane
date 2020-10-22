f342143e7d637a07e16c88844e8d9b4d8462145e
Max Ott <max.ott@me.com>
2020-10-22 16:56:24 +0200

    [cert] fixed "\x82" from ASCII-8BIT to UTF-8
    Related: https://github.com/fastlane/fastlane/pull/17446
    
    We also have this issue for `cert`, so might be worth a try. 😇

--- cert/lib/cert/runner.rb
+++ cert/lib/cert/runner.rb
@@ -232,7 +232,7 @@ module Cert
       cert_name = "#{cert_name}.cer" unless File.extname(cert_name) == ".cer"
       path = File.expand_path(File.join(Cert.config[:output_path], cert_name))
       raw_data = Base64.decode64(certificate.certificate_content)
-      File.write(path, raw_data)
+      File.write(path, raw_data.force_encoding("UTF-8"))
       return path
     end
   end
