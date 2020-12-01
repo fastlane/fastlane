9e4e40815a61220ea334affd65db99496bc538a4
Max Ott <max.ott@me.com>
2020-12-01 18:09:57 +0100

    [spaceship] fix get_live_version (#17701)
    * Fix get_live_version
    
    If the app is "DEVELOPER_REMOVED_FROM_SALE" there will be no live nor edit version. ASC treats it as live version, so this will fix it.
    
    * Add DEVELOPER_REMOVED_FROM_SALE
    
    * Add DEVELOPER_REMOVED FROM SALE for fetch_app_info
    
    * Add DEVELOPER_REMOVED_FROM_SALE to app_info

--- spaceship/lib/spaceship/connect_api/models/app.rb
+++ spaceship/lib/spaceship/connect_api/models/app.rb
@@ -117,7 +117,8 @@ module Spaceship
           Spaceship::ConnectAPI::AppInfo::AppStoreState::PENDING_APPLE_RELEASE,
           Spaceship::ConnectAPI::AppInfo::AppStoreState::PENDING_DEVELOPER_RELEASE,
           Spaceship::ConnectAPI::AppInfo::AppStoreState::PROCESSING_FOR_APP_STORE,
-          Spaceship::ConnectAPI::AppInfo::AppStoreState::IN_REVIEW
+          Spaceship::ConnectAPI::AppInfo::AppStoreState::IN_REVIEW,
+          Spaceship::ConnectAPI::AppInfo::AppStoreState::DEVELOPER_REMOVED_FROM_SALE
         ]
 
         resp = client.get_app_infos(app_id: id, includes: includes)
@@ -230,7 +231,10 @@ module Spaceship
         client ||= Spaceship::ConnectAPI
         platform ||= Spaceship::ConnectAPI::Platform::IOS
         filter = {
-          appStoreState: Spaceship::ConnectAPI::AppStoreVersion::AppStoreState::READY_FOR_SALE,
+          appStoreState: [
+            Spaceship::ConnectAPI::AppStoreVersion::AppStoreState::READY_FOR_SALE,
+            Spaceship::ConnectAPI::AppStoreVersion::AppStoreState::DEVELOPER_REMOVED_FROM_SALE
+          ].join(","),
           platform: platform
         }
         return get_app_store_versions(client: client, filter: filter, includes: includes).first
