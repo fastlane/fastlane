4e498e8d94453a1605f489f04913b118adac34bb
ainame <s.namai.09@gmail.com>
2020-12-04 02:39:53 +0000

    Use basename to restore previous behavior

--- deliver/lib/deliver/upload_metadata.rb
+++ deliver/lib/deliver/upload_metadata.rb
@@ -375,7 +375,7 @@ module Deliver
       # Check folder list (an empty folder signifies a language is required)
       ignore_validation = options[:ignore_language_directory_validation]
       Loader.language_folders(options[:metadata_path], ignore_validation).each do |lang_folder|
-        enabled_languages << lang_folder.language unless enabled_languages.include?(lang_folder.language)
+        enabled_languages << lang_folder.basename unless enabled_languages.include?(lang_folder.basename)
       end
 
       return unless enabled_languages.include?("default")
@@ -414,7 +414,7 @@ module Deliver
       # Check folder list (an empty folder signifies a language is required)
       ignore_validation = options[:ignore_language_directory_validation]
       Loader.language_folders(options[:metadata_path], ignore_validation).each do |lang_folder|
-        enabled_languages << lang_folder.language unless enabled_languages.include?(lang_folder.language)
+        enabled_languages << lang_folder.basename unless enabled_languages.include?(lang_folder.basename)
       end
 
       # Mapping to strings because :default symbol can be passed in
@@ -531,7 +531,7 @@ module Deliver
 
           UI.message("Loading '#{path}'...")
           options[key] ||= {}
-          options[key][lang_folder.language] ||= File.read(path)
+          options[key][lang_folder.basename] ||= File.read(path)
         end
       end
 
