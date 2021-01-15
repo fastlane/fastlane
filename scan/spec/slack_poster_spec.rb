ee9af639f7b55f18941e27a69f9a34bbcee29010
Roger Oba <rogerluan.oba@gmail.com>
2021-01-15 13:27:08 -0300

    [scan] fix scan regression caused by not setting `slack_default_payloads` option (#17923)
    * Fix Slack action options initialization.
    
    * Swap ConfigurationHelper with Configuration.create.
    Reason was just consistency. The ConfigurationHelper was pretty much
    only used in this file, while Configuration.create is being used
    in over 300+ occurrences - seems the better way to do it.
    
    * Remove test expectations that are unrelated to the test description.
    
    * Fix specs.
    
    * Improve unit tests by leveraging rspec-mock APIs.

--- scan/spec/slack_poster_spec.rb
+++ scan/spec/slack_poster_spec.rb
@@ -80,31 +80,29 @@ describe Scan::SlackPoster do
       end
     end
 
-    def expect_slack_posting
-      expect(Fastlane::Actions::SlackAction).to receive(:run).with(
-        hash_including({
-          message: a_string_matching(' Tests:'),
-          channel: nil,
-          slack_url: 'https://slack/hook/url',
-          username: 'fastlane',
-          icon_url: 'https://fastlane.tools/assets/img/fastlane_icon.png',
-          default_payloads: nil,
-          attachment_properties: {
-            fields: [
-              {
-                title: 'Test Failures',
-                value: '0',
-                short: true
-              },
-              {
-                title: 'Successful Tests',
-                value: '0',
-                short: true
-              }
-            ]
-          }
-        })
-      )
+    def expected_slack_poster_arguments
+      hash = {
+        message: a_string_matching(' Tests:'),
+        channel: nil,
+        slack_url: 'https://slack/hook/url',
+        username: 'fastlane',
+        icon_url: 'https://fastlane.tools/assets/img/fastlane_icon.png',
+        default_payloads: nil,
+        attachment_properties: {
+          fields: [
+            {
+              title: 'Test Failures',
+              value: '0',
+              short: true
+            },
+            {
+              title: 'Successful Tests',
+              value: '0',
+              short: true
+            }
+          ]
+        }
+      }
     end
 
     describe "with slack_url option set to a URL value" do
@@ -112,13 +110,17 @@ describe Scan::SlackPoster do
         # ensures that people's local environment variable doesn't interfere with this test
         FastlaneSpec::Env.with_env_values('SLACK_URL' => nil) do
           expect(ENV['SLACK_URL']).to eq(nil)
-          expect_slack_posting
 
           Scan.config = FastlaneCore::Configuration.create(Scan::Options.available_options, {
             project: './scan/examples/standard/app.xcodeproj',
             slack_url: 'https://slack/hook/url'
           })
 
+          expect(FastlaneCore::Configuration).to(
+            receive(:create)
+            .with(any_args, hash_including(expected_slack_poster_arguments))
+            .and_call_original
+          )
           Scan::SlackPoster.new.run({ tests: 0, failures: 0 })
         end
       end
@@ -128,12 +130,16 @@ describe Scan::SlackPoster do
       it "does Slack posting", requires_xcodebuild: true do
         FastlaneSpec::Env.with_env_values('SLACK_URL' => 'https://slack/hook/url') do
           expect(ENV['SLACK_URL']).to eq('https://slack/hook/url')
-          expect_slack_posting
 
           Scan.config = FastlaneCore::Configuration.create(Scan::Options.available_options, {
             project: './scan/examples/standard/app.xcodeproj'
           })
 
+          expect(FastlaneCore::Configuration).to(
+            receive(:create)
+            .with(any_args, hash_including(expected_slack_poster_arguments))
+            .and_call_original
+          )
           Scan::SlackPoster.new.run({ tests: 0, failures: 0 })
         end
       end
