8981055ca147177310e951d5378c8a5eab4f7c71
Roger Oba <rogerluan.oba@gmail.com>
2021-01-16 16:51:03 -0300

    Remove logic from create and add find_or_create function.

--- spaceship/lib/spaceship/connect_api/models/device.rb
+++ spaceship/lib/spaceship/connect_api/models/device.rb
@@ -64,13 +64,24 @@ module Spaceship
         end
       end
 
+      # @param client [ConnectAPI] ConnectAPI client.
+      # @param name [String] The name to be assigned to the device, if it needs to be created.
+      # @param platform [String] The platform of the device.
+      # @param include_disabled [Bool] Whether to include disable devices. false by default.
+      # @return (Device) Find a device based on the UDID of the device. If no device was found,  nil if no device was found.
+      def self.find_or_create(device_udid, client: nil, name: nil, platform: nil, include_disabled: false)
+        existing = self.find_by_udid(device_udid, client: client, platform: platform)
+        return existing if existing
+        return self.create(client: client, name: name, platform: platform, udid: device_udid)
+      end
+
+      # @param client [ConnectAPI] ConnectAPI client.
+      # @param name [String] The name to be assigned to the device.
+      # @param platform [String] The platform of the device.
+      # @param udid [String] The udid of the device being created.
+      # @return (Device) Find a device based on the UDID of the device. nil if no device was found.
       def self.create(client: nil, name: nil, platform: nil, udid: nil)
         client ||= Spaceship::ConnectAPI
-
-        # Find the device by UDID and return it in case it already exists
-        existing = self.find_by_udid(udid, client: client, platform: platform)
-        return existing if existing
-
         resp = client.post_device(name: name, platform: platform, udid: udid)
         return resp.to_models.first
       end
