5b327010ce9f7feeb7fc934491107fcbb438779b
Josh Holtz <josh@rokkincat.com>
2021-02-11 03:59:16 -0600

    [app store connect] unified ENV vars for api key with APP_STORE_CONNECT_API_KEY_PATH and APP_STORE_CONNECT_API_KEY

--- pilot/lib/pilot/options.rb
+++ pilot/lib/pilot/options.rb
@@ -11,7 +11,7 @@ module Pilot
 
       [
         FastlaneCore::ConfigItem.new(key: :api_key_path,
-                                     env_name: "PILOT_API_KEY_PATH",
+                                     env_names: ["PILOT_API_KEY_PATH", "APP_STORE_CONNECT_API_KEY_PATH"],
                                      description: "Path to your App Store Connect API Key JSON file (https://docs.fastlane.tools/app-store-connect-api/#using-fastlane-api-key-json-file)",
                                      optional: true,
                                      conflicting_options: [:username],
@@ -19,7 +19,7 @@ module Pilot
                                        UI.user_error!("Couldn't find API key JSON file at path '#{value}'") unless File.exist?(value)
                                      end),
         FastlaneCore::ConfigItem.new(key: :api_key,
-                                     env_name: "PILOT_API_KEY",
+                                     env_names: ["PILOT_API_KEY", "APP_STORE_CONNECT_API_KEY"],
                                      description: "Your App Store Connect API Key information (https://docs.fastlane.tools/app-store-connect-api/#use-return-value-and-pass-in-as-an-option)",
                                      type: Hash,
                                      optional: true,
