f6d8a729b8747c64fc37f06875d0fc2b15e85b10
Stuart McHattie <stuart.mchattie@tessella.com>
2021-03-05 15:32:02 +0000

    Fix some issues with unit test

--- fastlane_core/spec/shell_spec.rb
+++ fastlane_core/spec/shell_spec.rb
@@ -5,10 +5,9 @@ describe FastlaneCore do
         @shell = FastlaneCore::Shell.new
       end
 
-      it 'command_output handles UTF-16 and ISO-8859-1 encoded messages' do
+      it 'command_output handles encodings incorrectly tagged as UTF-8' do
         expect do
-          @shell.command_output("string_in_utf-16_with_µ\n".encode('UTF-16'))
-          @shell.command_output("string_in_iso-8859-1_with_µ\n".encode('ISO-8859-1'))
+          @shell.command_output("utf_16_string_tagged_as_utf_8\n".encode(Encoding::UTF_16).force_encoding(Encoding::UTF_8))
         end.not_to raise_error
       end
     end
