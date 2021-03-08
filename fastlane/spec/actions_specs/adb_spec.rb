caa0aace05e4cf0f0a6bdb631bfedf7a40d96618
Kartik Soneji <kartiksoneji@rocketmail.com>
2021-03-08 16:07:43 +0530

    Use correct path separator according to platform.

--- fastlane/spec/actions_specs/adb_spec.rb
+++ fastlane/spec/actions_specs/adb_spec.rb
@@ -1,6 +1,10 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "adb" do
+      before(:each) do
+        allow(FastlaneCore::Helper).to receive(:windows?).and_return(false)
+      end
+
       it "generates a valid command" do
         result = Fastlane::FastFile.new.parse("lane :test do
           adb(command: 'test', adb_path: './README.md')
@@ -68,6 +72,81 @@ describe Fastlane do
         end").runner.execute(:test)
 
         expect(result).to eq("#{File.expand_path('/usr/local/android-sdk/platform-tools/adb')} test")
+      end
+    end
+
+    describe "adb on Windows" do
+      before(:each) do
+        allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+      end
+
+      it "generates a valid command" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test', adb_path: './README.md')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('./fastlane/README.md').gsub('/', '\\')} test")
+      end
+
+      it "generates a valid command for commands with multiple parts" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test command with multiple parts', adb_path: './README.md')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('./fastlane/README.md').gsub('/', '\\')} test command with multiple parts")
+      end
+
+      it "generates a valid command when a non-empty serial is passed" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test command with non-empty serial', adb_path: './README.md', serial: 'emulator-1234')
+        end").runner.execute(:test)
+
+        expect(result).to eq("ANDROID_SERIAL=emulator-1234 #{File.expand_path('./fastlane/README.md').gsub('/', '\\')} test command with non-empty serial")
+      end
+
+      it "generates a valid command when an empty serial is passed" do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test command with empty serial', adb_path: './README.md', serial: '')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('./fastlane/README.md').gsub('/', '\\')} test command with empty serial")
+      end
+
+      it "picks up path from ANDROID_HOME environment variable" do
+        stub_const('ENV', { 'ANDROID_HOME' => '/Users\\SomeUser\\AppData\\Local\\Android\\Sdk' })
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('/Users\\SomeUser\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb').gsub('/', '\\')} test")
+      end
+
+      it "picks up path from ANDROID_HOME environment variable and handles path that has to be made safe" do
+        stub_const('ENV', { 'ANDROID_HOME' => '/Users\\Some User With Space\\AppData\\Local\\Android\\Sdk' })
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        path = File.expand_path("/Users\\Some User With Space\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb").shellescape.gsub('/', '\\')
+        expect(result).to eq("#{path} test")
+      end
+
+      it "picks up path from ANDROID_SDK_ROOT environment variable" do
+        stub_const('ENV', { 'ANDROID_SDK_ROOT' => '/Users\\SomeUser\\AppData\\Local\\Android\\Sdk' })
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('/Users\\SomeUser\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb').gsub('/', '\\')} test")
+      end
+
+      it "picks up path from ANDROID_SDK environment variable" do
+        stub_const('ENV', { 'ANDROID_SDK' => '/Users\\SomeUser\\AppData\\Local\\Android\\Sdk' })
+        result = Fastlane::FastFile.new.parse("lane :test do
+          adb(command: 'test')
+        end").runner.execute(:test)
+
+        expect(result).to eq("#{File.expand_path('/Users\\SomeUser\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb').gsub('/', '\\')} test")
       end
     end
   end
