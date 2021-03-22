42b93fde227e63857f2db49cb79c07ddcc66ed4e
Stuart McHattie <stuart.mchattie@tessella.com>
2021-03-22 08:49:54 +0000

    Add an extra test for a string containing the µ character

--- fastlane_core/spec/shell_spec.rb
+++ fastlane_core/spec/shell_spec.rb
@@ -8,6 +8,7 @@ describe FastlaneCore do
       it 'command_output handles encodings incorrectly tagged as UTF-8' do
         expect do
           @shell.command_output("utf_16_string_tagged_as_utf_8\n".encode(Encoding::UTF_16).force_encoding(Encoding::UTF_8))
+          @shell.command_output("µm\n".encode(Encoding::UTF_16).force_encoding(Encoding::UTF_8))
         end.not_to raise_error
       end
     end
