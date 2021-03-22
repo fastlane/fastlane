acf7a25b376f274aeb0058c6fcce19dcfb441f21
Josh Holtz <josh@rokkincat.com>
2021-03-22 04:48:09 -0500

    Adding back in shared values

--- fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
+++ fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
@@ -2,6 +2,10 @@ require 'base64'
 
 module Fastlane
   module Actions
+    module SharedValues
+      APP_STORE_CONNECT_API_KEY = :APP_STORE_CONNECT_API_KEY
+    end
+
     class AppStoreConnectApiKeyAction < Action
       def self.run(options)
         key_id = options[:key_id]
@@ -30,6 +34,8 @@ module Fastlane
           duration: duration,
           in_house: in_house
         }
+
+        Actions.lane_context.set_sensitive(SharedValues::APP_STORE_CONNECT_API_KEY, key)
 
         # Creates Spaceship API Key session
         # User does not need to pass the token into any actions because of this
@@ -83,6 +89,12 @@ module Fastlane
                                        description: "Is App Store or Enterprise (in house) team? App Store Connect API cannot determine this on its own (yet)",
                                        optional: true,
                                        type: Boolean)
+        ]
+      end
+
+      def self.output
+        [
+          ['APP_STORE_CONNECT_API_KEY', 'The App Store Connect API key information used for authorization requests. This hash can be passed directly into the :api_key options on other tools or into Spaceship::ConnectAPI::Token.create method']
         ]
       end
 
