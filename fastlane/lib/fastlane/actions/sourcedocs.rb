447502ba648445c9e93e47b712251046886feaa8
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-03-24 00:11:42 +0100

    add all options and test it

--- fastlane/lib/fastlane/actions/sourcedocs.rb
+++ fastlane/lib/fastlane/actions/sourcedocs.rb
@@ -5,9 +5,18 @@ module Fastlane
         UI.user_error!("You have to install sourcedocs using `brew install sourcedocs`") if `which sourcedocs`.to_s.length == 0
 
         command =  "sourcedocs generate"
-        command << " --reproducible-docs" if params[:reproducible]
-        command << " -o #{params[:output]}"
+        command << " --all-modules" if params[:all_modules]
+        command << " --spm-module #{params[:spm_module]}" unless params[:spm_module].nil?
+        command << " --module-name #{params[:module_name]}" unless params[:module_name].nil?
+        command << " --link-beginning #{params[:link_beginning]}" unless params[:link_beginning].nil?
+        command << " --link-ending #{params[:link_ending]}" unless params[:link_ending].nil?
+        command << " --output-folder #{params[:output_folder]}" unless params[:output_folder].nil?
+        command << " --min-acl #{params[:min_acl]}" unless params[:min_acl].nil?
+        command << " --module-name-path" if params[:module_name_path]
         command << " --clean" if params[:clean]
+        command << " --collapsible" if params[:collapsible]
+        command << " --table-of-contents" if params[:table_of_contents]
+        command << " --reproducible-docs" if params[:reproducible]
         unless params[:scheme].nil?
           command << " -- -scheme #{params[:scheme]}"
           command << " -sdk #{params[:sdk_platform]}" unless params[:sdk_platform].nil?
@@ -26,23 +35,86 @@ module Fastlane
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(
-            key: :reproducible,
-            env_name: 'FL_SOURCEDOCS_REPRODUCIBLE',
-            description: 'Generate documentation that is reproducible: only depends on the sources',
+            key: :all_modules,
+            env_name: 'FL_SOURCEDOCS_OUTPUT_ALL_MODULES',
+            description: 'Generate documentation for all modules in a Swift package',
             type: Boolean,
             optional: true
           ),
           FastlaneCore::ConfigItem.new(
-            key: :output,
-            env_name: 'FL_SOURCEDOCS_OUTPUT',
-            description: 'Path to write docs to',
+            key: :spm_module,
+            env_name: 'FL_SOURCEDOCS_SPM_MODULE',
+            description: 'Generate documentation for Swift Package Manager module',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :module_name,
+            env_name: 'FL_SOURCEDOCS_MODULE_NAME',
+            description: 'Generate documentation for a Swift module',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :link_beginning,
+            env_name: 'FL_SOURCEDOCS_LINK_BEGINNING',
+            description: 'The text to begin links with',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :link_ending,
+            env_name: 'FL_SOURCEDOCS_LINK_ENDING',
+            description: 'The text to end links with (default: .md)',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :output_folder,
+            env_name: 'FL_SOURCEDOCS_OUTPUT_FOLDER',
+            description: 'Output directory to clean (default: Documentation/Reference)',
             type: String,
             optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :min_acl,
+            env_name: 'FL_SOURCEDOCS_MIN_ACL',
+            description: 'Access level to include in documentation [private, fileprivate, internal, public, open] (default: public)',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :module_name_path,
+            env_name: 'FL_SOURCEDOCS_MODULE_NAME_PATH',
+            description: 'Include the module name as part of the output folder path',
+            type: Boolean,
+            optional: true
           ),
           FastlaneCore::ConfigItem.new(
             key: :clean,
             env_name: 'FL_SOURCEDOCS_CLEAN',
             description: 'Delete output folder before generating documentation',
+            type: Boolean,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :collapsible,
+            env_name: 'FL_SOURCEDOCS_COLLAPSIBLE',
+            description: 'Put methods, properties and enum cases inside collapsible blocks',
+            type: Boolean,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :table_of_contents,
+            env_name: 'FL_SOURCEDOCS_TABLE_OF_CONTENT',
+            description: 'Generate a table of contents with properties and methods for each type',
+            type: Boolean,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :reproducible,
+            env_name: 'FL_SOURCEDOCS_REPRODUCIBLE',
+            description: 'Generate documentation that is reproducible: only depends on the sources',
             type: Boolean,
             optional: true
           ),
--- fastlane/spec/actions_specs/sourcedocs_spec.rb
+++ fastlane/spec/actions_specs/sourcedocs_spec.rb
@@ -5,50 +5,269 @@ describe Fastlane do
         it "default use case" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs'
+              output_folder: 'docs'
             )
           end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs")
+          expect(result).to eq("sourcedocs generate --output-folder docs")
         end
       end
 
-      context "when specify false for clean option" do
+      context "when specify all_modules option" do
+        it "doesn't add all_modules option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              all_modules: false,
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds all_modules option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              all_modules: true
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --all-modules --output-folder docs")
+        end
+      end
+
+      context "when specify spm_module option" do
+        it "doesn't add spm_module option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds spm_module option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              spm_module: 'MySpmModule'
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --spm-module MySpmModule --output-folder docs")
+        end
+      end
+
+      context "when specify module_name option" do
+        it "doesn't add module_name option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds module_name option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              module_name: 'MyModule'
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --module-name MyModule --output-folder docs")
+        end
+      end
+
+      context "when specify link_beginning option" do
+        it "doesn't add link_beginning option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds link_beginning option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              link_beginning: 'sdk'
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --link-beginning sdk --output-folder docs")
+        end
+      end
+
+      context "when specify link_ending option" do
+        it "doesn't add link_ending option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds link_ending option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              link_ending: '.md'
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --link-ending .md --output-folder docs")
+        end
+      end
+
+      context "when specify min_acl option" do
+        it "doesn't add min_acl option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds min_acl option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              min_acl: 'internal'
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs --min-acl internal")
+        end
+      end
+
+      context "when specify module_name_path option" do
+        it "doesn't add module_name_path option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              module_name_path: false,
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+
+        it "adds module_name_path option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              module_name_path: true
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs --module-name-path")
+        end
+      end
+
+      context "when specify clean option" do
         it "doesn't add clean option" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
-              clean: false
+              output_folder: 'docs',
+              clean: false,
             )
           end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs")
+          expect(result).to eq("sourcedocs generate --output-folder docs")
         end
-      end
 
-      context "when specify true for clean option" do
         it "adds clean option" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
               clean: true
             )
           end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs --clean")
+          expect(result).to eq("sourcedocs generate --output-folder docs --clean")
         end
       end
 
-      context "when specify reproducible parameter" do
-        it "adds reproducible-docs parameter" do
+      context "when specify collapsible option" do
+        it "adds collapsile option" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
+              collapsible: true
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs --collapsible")
+        end
+
+        it "doesn't add collapsile option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              collapsible: false
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+      end
+      context "when specify table_of_contents option" do
+        it "adds table_of_contents option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              table_of_contents: true
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs --table-of-contents")
+        end
+
+        it "doesn't add table-of-contents option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              table_of_contents: false
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
+        end
+      end
+
+      context "when specify reproducible option" do
+        it "adds reproducible option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
               reproducible: true
             )
           end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate --reproducible-docs -o docs")
+          expect(result).to eq("sourcedocs generate --output-folder docs --reproducible-docs")
+        end
+
+        it "doesn't add reproducible option" do
+          result = Fastlane::FastFile.new.parse("lane :test do
+            sourcedocs(
+              output_folder: 'docs',
+              reproducible: false
+            )
+          end").runner.execute(:test)
+
+          expect(result).to eq("sourcedocs generate --output-folder docs")
         end
       end
 
@@ -56,24 +275,24 @@ describe Fastlane do
         it "adds scheme parameter" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
               scheme: 'MyApp'
             )
             end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs -- -scheme MyApp")
+          expect(result).to eq("sourcedocs generate --output-folder docs -- -scheme MyApp")
         end
 
         it "adds sdk platform parameter" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
               scheme: 'MyApp',
               sdk_platform: 'iphoneos'
             )
             end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs -- -scheme MyApp -sdk iphoneos")
+          expect(result).to eq("sourcedocs generate --output-folder docs -- -scheme MyApp -sdk iphoneos")
         end
       end
 
@@ -81,39 +300,42 @@ describe Fastlane do
         it "adds no scheme parameter" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
             )
             end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs")
+          expect(result).to eq("sourcedocs generate --output-folder docs")
         end
 
         it "adds no sdk platform parameter" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
-              output: 'docs',
+              output_folder: 'docs',
               sdk_platform: 'iphoneos'
             )
             end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate -o docs")
+          expect(result).to eq("sourcedocs generate --output-folder docs")
         end
       end
 
-      context "when specify all parameters" do
-        it "adds all parameters" do
+      context "when specify lots parameters" do
+        it "adds lots parameters" do
           result = Fastlane::FastFile.new.parse("lane :test do
             sourcedocs(
               sdk_platform: 'macosx',
-              output: 'docs',
+              output_folder: 'docs',
+              collapsible: true,
+              table_of_contents: true,
               clean: true,
               reproducible: true,
+              module_name_path: true,
               scheme: 'MyApp',
-
+              min_acl: 'internal'
             )
           end").runner.execute(:test)
 
-          expect(result).to eq("sourcedocs generate --reproducible-docs -o docs --clean -- -scheme MyApp -sdk macosx")
+          expect(result).to eq("sourcedocs generate --output-folder docs --min-acl internal --module-name-path --clean --collapsible --table-of-contents --reproducible-docs -- -scheme MyApp -sdk macosx")
         end
       end
     end
