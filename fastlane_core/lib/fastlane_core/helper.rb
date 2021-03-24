b38017696c48b5bdef5184b852988f7ecb1a3c06
Alex <Katsz@users.noreply.github.com>
2021-03-24 04:55:24 +0300

    Update helper.rb (#18429)

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -75,7 +75,7 @@ module FastlaneCore
 
       # Check for Jenkins, Travis CI, ... environment variables
       ['JENKINS_HOME', 'JENKINS_URL', 'TRAVIS', 'CI', 'APPCENTER_BUILD_ID', 'TEAMCITY_VERSION', 'GO_PIPELINE_NAME', 'bamboo_buildKey', 'GITLAB_CI', 'XCS', 'TF_BUILD', 'GITHUB_ACTION', 'GITHUB_ACTIONS', 'BITRISE_IO', 'BUDDY'].each do |current|
-        return true if ENV.key?(current)
+        return true if FastlaneCore::Env.truthy?(current)
       end
       return false
     end
