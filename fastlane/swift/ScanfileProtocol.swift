d21cda030555531b1138f1af045c7d716eb938bb
Josh Holtz <josh@rokkincat.com>
2021-03-26 05:48:46 -0500

    Version bump to 2.179.0 (#18456)
    * Version bump to 2.179.0
    
    * fix ruby 2.4 tests
    
    * fix ruby 2.4 tests
    
    * fix ruby 2.4 tests
    
    * fix ruby 2.4 tests

--- fastlane/swift/ScanfileProtocol.swift
+++ fastlane/swift/ScanfileProtocol.swift
@@ -288,4 +288,4 @@ public extension ScanfileProtocol {
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.69]
+// FastlaneRunnerAPIVersion [0.9.70]
