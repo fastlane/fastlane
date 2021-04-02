bda92f33ef72a456dc11c6284fb7a7e8857da405
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-04-02 19:25:19 +0200

    PR review fix
    Co-authored-by: Josh Holtz <josh@rokkincat.com>

--- fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
+++ fastlane/lib/fastlane/actions/app_store_connect_api_key.rb
@@ -99,7 +99,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        [:ios, :mac].include?(platform)
+        [:ios, :mac, :tvos].include?(platform)
       end
 
       def self.details
