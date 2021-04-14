bc4a564ddbe83b82f0bf065c03e8e28856b4440d
Davide <info@davidebenini.it>
2021-04-14 04:34:24 +0200

    Added iPhone 12 models to snapshot reports generator (#18535)

--- snapshot/lib/snapshot/reports_generator.rb
+++ snapshot/lib/snapshot/reports_generator.rb
@@ -84,6 +84,10 @@ module Snapshot
       {
         # snapshot in Xcode 9 saves screenshots with the SIMULATOR_DEVICE_NAME
         # which includes spaces
+        'iPhone 12 Pro Max' => "iPhone 12 Pro Max",
+        'iPhone 12 Pro' => "iPhone 12 Pro",
+        'iPhone 12 mini' => "iPhone 12 mini",
+        'iPhone 12' => "iPhone 12",
         'iPhone 11 Pro Max' => "iPhone 11 Pro Max",
         'iPhone 11 Pro' => "iPhone 11 Pro",
         'iPhone 11' => "iPhone 11",
