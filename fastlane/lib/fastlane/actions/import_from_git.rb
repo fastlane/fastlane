222386f468153fca1e9a2f80458b40438884f361
Hiroki Nagasawa <hiroki@pixyzehn.com>
2021-04-17 08:58:34 +0900

    Add punctation marks to example code of import_from_git

--- fastlane/lib/fastlane/actions/import_from_git.rb
+++ fastlane/lib/fastlane/actions/import_from_git.rb
@@ -61,14 +61,14 @@ module Fastlane
         [
           'import_from_git(
             url: "git@github.com:fastlane/fastlane.git", # The URL of the repository to import the Fastfile from.
-            branch: "HEAD", # The branch to checkout on the repository
-            path: "fastlane/Fastfile", # The path of the Fastfile in the repository
+            branch: "HEAD", # The branch to checkout on the repository.
+            path: "fastlane/Fastfile", # The path of the Fastfile in the repository.
             version: "~> 1.0.0" # The version to checkout on the repository. Optimistic match operator can be used to select the latest version within constraints.
           )',
           'import_from_git(
             url: "git@github.com:fastlane/fastlane.git", # The URL of the repository to import the Fastfile from.
-            branch: "HEAD", # The branch to checkout on the repository
-            path: "fastlane/Fastfile", # The path of the Fastfile in the repository
+            branch: "HEAD", # The branch to checkout on the repository.
+            path: "fastlane/Fastfile", # The path of the Fastfile in the repository.
             version: [">= 1.1.0", "< 2.0.0"], # The version to checkout on the repository. Multiple conditions can be used to select the latest version within constraints.
             cache_path: "~/.cache/fastlane/imported" # A directory in which the repository will be added, which means that it will not be cloned again on subsequent calls.
           )'
