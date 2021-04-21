78cc6d70b3d1e685eeb58be77d5005ddb6a3708e
Hiroki Nagasawa <hiroki@pixyzehn.com>
2021-04-21 09:37:46 +0900

    Use 'rebase' rather than 'merge' to not create a merge commit

--- fastlane/spec/actions_specs/import_from_git_spec.rb
+++ fastlane/spec/actions_specs/import_from_git_spec.rb
@@ -209,7 +209,7 @@ describe Fastlane do
           end").runner.execute(:test)
         end
 
-        # Meaning, `git fetch` and `git merge` are performed when caching is eligible
+        # Meaning, `git fetch` and `git rebase` are performed when caching is eligible
         # and the version isn't specified.
         it "works with updated branch" do
           Dir.chdir(source_directory_path) do
