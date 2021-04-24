2c831f2497f2285f51d5376892020846fda82dec
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-04-24 23:01:57 +0200

    [action][git_submodule_update] documentation improvement

--- fastlane/lib/fastlane/actions/git_submodule_update.rb
+++ fastlane/lib/fastlane/actions/git_submodule_update.rb
@@ -13,20 +13,27 @@ module Fastlane
       #####################################################
 
       def self.description
-        "Executes a git submodule command"
+        "Executes a git submodule update command"
       end
 
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(key: :recursive,
-           description: "Should the submodules be updated recursively",
-           type: Boolean,
-           default_value: false),
+                                       description: "Should the submodules be updated recursively?",
+                                       type: Boolean,
+                                       is_string: false,
+                                       default_value: false,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Please pass a valid value for recursive. Use one of the following: true, false") unless value.kind_of?(TrueClass) || value.kind_of?(FalseClass)
+                                       end),
           FastlaneCore::ConfigItem.new(key: :init,
-           description: "Should the submodules be initiated before update",
-           type: Boolean,
-           is_string: false,
-           default_value: false)
+                                       description: "Should the submodules be initiated before update?",
+                                       type: Boolean,
+                                       is_string: false,
+                                       default_value: false,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Please pass a valid value for init. Use one of the following: true, false") unless value.kind_of?(TrueClass) || value.kind_of?(FalseClass)
+                                       end)
         ]
       end
 
@@ -42,6 +49,15 @@ module Fastlane
 
       def self.is_supported?(platform)
         return true
+      end
+
+      def self.example_code
+        [
+          'git_submodule_update',
+          'git_submodule_update(recursive: true)',
+          'git_submodule_update(init: true)',
+          'git_submodule_update(recursive: true, init: true)'
+        ]
       end
 
       def self.category
