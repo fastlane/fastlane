0fa4c81e26ecd08863e001a91f4b2d096426b531
ainame <s.namai.09@gmail.com>
2021-04-28 06:10:30 +0100

    Use Gem::Version to compare semantic version

--- fastlane/lib/fastlane/commands_generator/terminal_compact.rb
+++ fastlane/lib/fastlane/commands_generator/terminal_compact.rb
@@ -2,7 +2,7 @@ module Fastlane
   class CommandsGenerator
     class TerminalCompact < ::Commander::HelpFormatter::TerminalCompact
       def template(name)
-        if RUBY_VERSION < '2.6'
+        if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.6')
           ERB.new(File.read(File.join(File.dirname(__FILE__), "#{name}.erb")), nil, '-')
         else
           ERB.new(File.read(File.join(File.dirname(__FILE__), "#{name}.erb")), trim_mode: '-')
