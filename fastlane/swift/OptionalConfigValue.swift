188b00d27bea910f28fb87dcafadfc340dba5631
Jorge Revuelta Herrero <minuscorp@gmail.com>
2021-05-14 12:01:48 +0200

    Fix for `Any?` with `ExpressibleByIntegerLiteral`

--- fastlane/swift/OptionalConfigValue.swift
+++ fastlane/swift/OptionalConfigValue.swift
@@ -23,14 +23,6 @@ public enum OptionalConfigValue<T> {
     }
 }
 
-extension Optional: ExpressibleByIntegerLiteral where Wrapped: ExpressibleByIntegerLiteral {
-    public typealias IntegerLiteralType = Wrapped.IntegerLiteralType
-
-    public init(integerLiteral value: Wrapped.IntegerLiteralType) {
-        self = .some(.init(integerLiteral: value))
-    }
-}
-
 extension Optional: ExpressibleByUnicodeScalarLiteral where Wrapped: ExpressibleByUnicodeScalarLiteral {
     public typealias UnicodeScalarLiteralType = Wrapped.UnicodeScalarLiteralType
 
