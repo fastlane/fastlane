d126a82be65ae45d98eade74b07b6a7d662c8355
Josh Holtz <josh@rokkincat.com>
2021-05-17 11:59:54 -0500

    [supplly] fix update_listing_for_language to work in Ruby 3.0 (#18699)

--- supply/lib/supply/client.rb
+++ supply/lib/supply/client.rb
@@ -279,7 +279,7 @@ module Supply
     def update_listing_for_language(language: nil, title: nil, short_description: nil, full_description: nil, video: nil)
       ensure_active_edit!
 
-      listing = AndroidPublisher::Listing.new({
+      listing = AndroidPublisher::Listing.new(**{
         language: language,
         title: title,
         full_description: full_description,
