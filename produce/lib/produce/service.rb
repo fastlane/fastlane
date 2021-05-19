23d25f426ed7817d05065793ee85d47141bc6b7b
shyamlad <slad@google.com>
2021-05-19 11:55:44 -0400

    Add new bundle capabilities

--- produce/lib/produce/service.rb
+++ produce/lib/produce/service.rb
@@ -36,10 +36,12 @@ module Produce
     end
 
     def valid_services_for(options)
-      allowed_keys = [:access_wifi, :app_group, :apple_pay, :associated_domains, :auto_fill_credential, :data_protection, :game_center, :healthkit, :homekit,
-                      :hotspot, :icloud, :in_app_purchase, :inter_app_audio, :multipath, :network_extension,
-                      :nfc_tag_reading, :personal_vpn, :passbook, :push_notification, :sirikit, :vpn_conf,
-                      :wallet, :wireless_conf]
+      allowed_keys = [:access_wifi, :app_attest, :app_group, :apple_pay, :associated_domains, :auto_fill_credential, :car_play_audio_app, :car_play_messaging_app, 
+                        :car_play_navigation_app, :car_play_voip_calling_app, :class_kit, :icloud, :critical_alerts, :custom_network_protocol, :data_protection, 
+                        :extended_virtual_address_space, :file_provider_testing_mode, :fonts, :game_center, :health_kit, :hls_interstitial_preview, :home_kit, :hotspot, 
+                        :hotspot_helper, :in_app_purchase, :inter_app_audio, :low_latency_hls, :managed_associated_domains, :maps, :multipath, :network_extension, 
+                        :nfc_tag_reading, :passbook, :personal_vpn, :push_notification, :sign_in_with_apple, :siri_kit, :system_extension, :user_management, :vpn_configuration, :wallet, 
+                        :wireless_accessory]
       options.__hash__.select { |key, value| allowed_keys.include?(key) }
     end
 
@@ -53,6 +55,15 @@ module Produce
           app.update_service(Spaceship.app_service.access_wifi.on)
         else
           app.update_service(Spaceship.app_service.access_wifi.off)
+        end
+      end
+
+      if options.access_wifi
+        UI.message("\tApp Attest")
+        if on
+          app.update_service(Spaceship.app_service.app_attest.on)
+        else
+          app.update_service(Spaceship.app_service.app_attest.off)
         end
       end
 
@@ -96,6 +107,76 @@ module Produce
         end
       end
 
+      if options.car_play_audio_app
+        UI.message("\tCarPlay Audio App")
+
+        if on
+          app.update_service(Spaceship.app_service.car_play_audio_app.on)
+        else
+          app.update_service(Spaceship.app_service.car_play_audio_app.off)
+        end
+      end
+
+      if options.car_play_messaging_app
+        UI.message("\tCarPlay Messaging App")
+
+        if on
+          app.update_service(Spaceship.app_service.car_play_messaging_app.on)
+        else
+          app.update_service(Spaceship.app_service.car_play_messaging_app.off)
+        end
+      end
+
+      if options.car_play_navigation_app
+        UI.message("\tCarPlay Navigation App")
+
+        if on
+          app.update_service(Spaceship.app_service.car_play_navigation_app.on)
+        else
+          app.update_service(Spaceship.app_service.car_play_navigation_app.off)
+        end
+      end
+
+      if options.car_play_voip_calling_app
+        UI.message("\tCarPlay Voip Calling App")
+
+        if on
+          app.update_service(Spaceship.app_service.car_play_voip_calling_app.on)
+        else
+          app.update_service(Spaceship.app_service.car_play_voip_calling_app.off)
+        end
+      end
+
+      if options.class_kit
+        UI.message("\tClassKit")
+
+        if on
+          app.update_service(Spaceship.app_service.class_kit.on)
+        else
+          app.update_service(Spaceship.app_service.class_kit.off)
+        end
+      end
+
+      if options.critical_alerts
+        UI.message("\tCritical Alerts")
+
+        if on
+          app.update_service(Spaceship.app_service.critical_alerts.on)
+        else
+          app.update_service(Spaceship.app_service.critical_alerts.off)
+        end
+      end
+
+      if options.custom_network_protocol
+        UI.message("\tCustom Network Protocol")
+
+        if on
+          app.update_service(Spaceship.app_service.custom_network_protocol.on)
+        else
+          app.update_service(Spaceship.app_service.custom_network_protocol.off)
+        end
+      end
+
       if options.data_protection
         UI.message("\tData Protection")
 
@@ -112,6 +193,36 @@ module Produce
           end
         else
           app.update_service(Spaceship.app_service.data_protection.off)
+        end
+      end
+
+      if options.extended_virtual_address_space
+        UI.message("\tExtended Virtual Address Space")
+
+        if on
+          app.update_service(Spaceship.app_service.extended_virtual_address_space.on)
+        else
+          app.update_service(Spaceship.app_service.extended_virtual_address_space.off)
+        end
+      end
+
+      if options.file_provider_testing_mode
+        UI.message("\tFile Provider Testing Mode")
+
+        if on
+          app.update_service(Spaceship.app_service.file_provider_testing_mode.on)
+        else
+          app.update_service(Spaceship.app_service.file_provider_testing_mode.off)
+        end
+      end
+
+      if options.fonts
+        UI.message("\tFonts")
+
+        if on
+          app.update_service(Spaceship.app_service.fonts.on)
+        else
+          app.update_service(Spaceship.app_service.fonts.off)
         end
       end
 
@@ -135,6 +246,16 @@ module Produce
         end
       end
 
+      if options.hls_interstitial_preview
+        UI.message("\tHLS Interstitial Preview")
+
+        if on
+          app.update_service(Spaceship.app_service.hls_interstitial_preview.on)
+        else
+          app.update_service(Spaceship.app_service.hls_interstitial_preview.off)
+        end
+      end
+
       if options.homekit
         UI.message("\tHomeKit")
 
@@ -145,23 +266,23 @@ module Produce
         end
       end
 
-      if options.wallet
-        UI.message("\tWallet")
+      if options.hotspot
+        UI.message("\tHotspot")
 
         if on
-          app.update_service(Spaceship.app_service.wallet.on)
+          app.update_service(Spaceship.app_service.hotspot.on)
         else
-          app.update_service(Spaceship.app_service.wallet.off)
+          app.update_service(Spaceship.app_service.hotspot.off)
         end
       end
 
-      if options.wireless_conf
-        UI.message("\tWireless Accessory Configuration")
+      if options.hotspot_helper
+        UI.message("\tHotspot Helper")
 
         if on
-          app.update_service(Spaceship.app_service.wireless_accessory.on)
+          app.update_service(Spaceship.app_service.hotspot_helper.on)
         else
-          app.update_service(Spaceship.app_service.wireless_accessory.off)
+          app.update_service(Spaceship.app_service.hotspot_helper.off)
         end
       end
 
@@ -170,14 +291,12 @@ module Produce
 
         if on
           case options.icloud
-          when "legacy"
-            app.update_service(Spaceship.app_service.cloud.on)
-            app.update_service(Spaceship.app_service.cloud_kit.xcode5_compatible)
-          when "cloudkit"
-            app.update_service(Spaceship.app_service.cloud.on)
-            app.update_service(Spaceship.app_service.cloud_kit.cloud_kit)
+          when "xcode6_compatible"
+            app.update_service(Spaceship.app_service.cloud.xcode6_compatible)
+          when "xcode5_compatible"
+            app.update_service(Spaceship.app_service.cloud.xcode5_compatible)
           else
-            UI.user_error!("Unknown service '#{options.icloud}'. Valid values: 'legacy', 'cloudkit'")
+            UI.user_error!("Unknown service '#{options.icloud}'. Valid values: 'xcode6_compatible', 'xcode5_compatible', 'off'")
           end
         else
           app.update_service(Spaceship.app_service.cloud.off)
@@ -204,13 +323,68 @@ module Produce
         end
       end
 
-      if options.personal_vpn
-        UI.message("\tPersonal VPN")
+      if options.low_latency_hls
+        UI.message("\tLow Latency HLS")
 
         if on
-          app.update_service(Spaceship.app_service.personal_vpn.on)
+          app.update_service(Spaceship.app_service.low_latency_hls.on)
         else
-          app.update_service(Spaceship.app_service.personal_vpn.off)
+          app.update_service(Spaceship.app_service.low_latency_hls.off)
+        end
+      end
+
+
+      if options.managed_associated_domains
+        UI.message("\tManaged Associated Domains")
+
+        if on
+          app.update_service(Spaceship.app_service.managed_associated_domains.on)
+        else
+          app.update_service(Spaceship.app_service.managed_associated_domains.off)
+        end
+      end
+
+
+      if options.maps
+        UI.message("\tMaps")
+
+        if on
+          app.update_service(Spaceship.app_service.maps.on)
+        else
+          app.update_service(Spaceship.app_service.maps.off)
+        end
+      end
+
+
+      if options.multipath
+        UI.message("\tMultipath")
+
+        if on
+          app.update_service(Spaceship.app_service.multipath.on)
+        else
+          app.update_service(Spaceship.app_service.multipath.off)
+        end
+      end
+
+
+      if options.network_extension
+        UI.message("\tNetwork Extension")
+
+        if on
+          app.update_service(Spaceship.app_service.network_extension.on)
+        else
+          app.update_service(Spaceship.app_service.network_extension.off)
+        end
+      end
+
+
+      if options.nfc_tag_reading
+        UI.message("\tNFC Tag Reading")
+
+        if on
+          app.update_service(Spaceship.app_service.nfc_tag_reading.on)
+        else
+          app.update_service(Spaceship.app_service.nfc_tag_reading.off)
         end
       end
 
@@ -222,6 +396,16 @@ module Produce
           app.update_service(Spaceship.app_service.passbook.on)
         else
           app.update_service(Spaceship.app_service.passbook.off)
+        end
+      end
+
+      if options.personal_vpn
+        UI.message("\tPersonal VPN")
+
+        if on
+          app.update_service(Spaceship.app_service.personal_vpn.on)
+        else
+          app.update_service(Spaceship.app_service.personal_vpn.off)
         end
       end
 
@@ -241,6 +425,16 @@ module Produce
         end
       end
 
+      if options.sign_in_with_apple
+        UI.message("\tSign In With Apple")
+
+        if on
+          app.update_service(Spaceship.app_service.sign_in_with_apple.on)
+        else
+          app.update_service(Spaceship.app_service.sign_in_with_apple.off)
+        end
+      end
+
       if options.sirikit
         UI.message("\tSiriKit")
 
@@ -248,6 +442,28 @@ module Produce
           app.update_service(Spaceship.app_service.siri_kit.on)
         else
           app.update_service(Spaceship.app_service.siri_kit.off)
+        end
+      end
+
+
+      if options.system_extension
+        UI.message("\tSystem Extension")
+
+        if on
+          app.update_service(Spaceship.app_service.system_extension.on)
+        else
+          app.update_service(Spaceship.app_service.system_extension.off)
+        end
+      end
+
+
+      if options.user_management
+        UI.message("\tUser Management")
+
+        if on
+          app.update_service(Spaceship.app_service.user_management.on)
+        else
+          app.update_service(Spaceship.app_service.user_management.off)
         end
       end
 
@@ -262,43 +478,23 @@ module Produce
         end
       end
 
-      if options.network_extension
-        UI.message("\tNetwork Extension")
+      if options.wallet
+        UI.message("\tWallet")
 
         if on
-          app.update_service(Spaceship.app_service.network_extension.on)
+          app.update_service(Spaceship.app_service.wallet.on)
         else
-          app.update_service(Spaceship.app_service.network_extension.off)
+          app.update_service(Spaceship.app_service.wallet.off)
         end
       end
 
-      if options.hotspot
-        UI.message("\tHotspot")
+      if options.wireless_conf
+        UI.message("\tWireless Accessory Configuration")
 
         if on
-          app.update_service(Spaceship.app_service.hotspot.on)
+          app.update_service(Spaceship.app_service.wireless_accessory.on)
         else
-          app.update_service(Spaceship.app_service.hotspot.off)
-        end
-      end
-
-      if options.multipath
-        UI.message("\tMultipath")
-
-        if on
-          app.update_service(Spaceship.app_service.multipath.on)
-        else
-          app.update_service(Spaceship.app_service.multipath.off)
-        end
-      end
-
-      if options.nfc_tag_reading
-        UI.message("\tNFC Tag Reading")
-
-        if on
-          app.update_service(Spaceship.app_service.nfc_tag_reading.on)
-        else
-          app.update_service(Spaceship.app_service.nfc_tag_reading.off)
+          app.update_service(Spaceship.app_service.wireless_accessory.off)
         end
       end
 
