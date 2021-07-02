928d411e17760838af9822e29bc5e112b3640104
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-07-02 02:55:03 +0200

    [action][version_get_podspec] fix Options formatting (#19031)

--- fastlane/lib/fastlane/actions/version_get_podspec.rb
+++ fastlane/lib/fastlane/actions/version_get_podspec.rb
@@ -28,7 +28,6 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :path,
                                        env_name: "FL_VERSION_PODSPEC_PATH",
                                        description: "You must specify the path to the podspec file",
-                                       is_string: true,
                                        code_gen_sensitive: true,
                                        default_value: Dir["*.podspec"].last,
                                        default_value_dynamic: true,
@@ -38,7 +37,7 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :require_variable_prefix,
                                        env_name: "FL_VERSION_BUMP_PODSPEC_VERSION_REQUIRE_VARIABLE_PREFIX",
                                        description: "true by default, this is used for non CocoaPods version bumps only",
-                                       is_string: false,
+                                       type: Boolean,
                                        default_value: true)
         ]
       end
