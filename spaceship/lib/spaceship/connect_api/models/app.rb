01fe542707117f1552d45a035775c3f3a5a70bd9
ainame <s.namai.09@gmail.com>
2021-07-03 03:12:03 +0900

    Add entrypoint to App

--- spaceship/lib/spaceship/connect_api/models/app.rb
+++ spaceship/lib/spaceship/connect_api/models/app.rb
@@ -431,6 +431,16 @@ module Spaceship
           client.delete_user_visible_apps(user_id: user_id, app_ids: [id])
         end
       end
+
+      #
+      # App Clips
+      #
+
+      def get_app_clips(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
+        client ||= Spaceship::ConnectAPI
+        resps = client.get_app_clips(app_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        resps.flat_map(&:to_models)
+      end
     end
   end
 end
