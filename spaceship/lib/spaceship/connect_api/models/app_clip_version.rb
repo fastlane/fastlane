4f9054d231c53bf6188520128f0f9701e04f71cc
ainame <s.namai.09@gmail.com>
2021-07-03 03:11:56 +0900

    Add models

---
+++ spaceship/lib/spaceship/connect_api/models/app_clip_version.rb
@@ -0,0 +1,37 @@
+require_relative '../model'
+
+module Spaceship
+  class ConnectAPI
+    class AppClipVersion
+      include Spaceship::ConnectAPI::Model
+
+      attr_accessor :invocation_verb
+
+      attr_mapping(
+        'invocationVerb' => 'invocation_verb',
+      )
+
+      def self.type
+        'appClipVersions'
+      end
+
+      def get_app_clip_version_localizations(client: nil, filter: {}, includes: Spaceship::ConnectAPI::AppClipVersionLocalization::ESSENTIAL_INCLUDES, limit: nil, sort: nil)
+        client ||= Spaceship::ConnectAPI
+        resps = client.get_app_clip_version_localizations(app_clip_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        resps.flat_map(&:to_models)
+      end
+
+      def get_release_with_app_store_version(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
+        client ||= Spaceship::ConnectAPI
+        resps = client.get_app_clip_version_release_with_app_store_version(app_clip_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        resps.flat_map(&:to_models)
+      end
+
+      def get_app_clip_app_store_review_detail(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
+        client ||= Spaceship::ConnectAPI
+        resps = client.get_app_clip_app_store_review_detail(app_clip_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        resps.flat_map(&:to_models)
+      end
+    end
+  end
+end
---
+++ spaceship/lib/spaceship/connect_api/models/app_clip_version_localization.rb
@@ -0,0 +1,38 @@
+require_relative '../model'
+
+module Spaceship
+  class ConnectAPI
+    class AppClipVersionLocalization
+      include Spaceship::ConnectAPI::Model
+
+      attr_accessor :locale
+      attr_accessor :subtitle
+
+      attr_mapping(
+        'locale' => 'locale',
+        'subtitle' => 'subtitle',
+        'appClipHeaderImage' => 'app_clip_header_image',
+      )
+
+      ESSENTIAL_INCLUDES = [
+        'appClipHeaderImage'
+      ].join(',')
+
+      def self.type
+        'appClipVersionLocalizations'
+      end
+
+      def get_app_clip_header_image(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
+        client ||= Spaceship::ConnectAPI
+        resps = client.get_app_clip_header_images(app_clip_version_localization_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
+        resps.flat_map(&:to_models).first
+      end
+
+      def update(client: nil, attributes: nil)
+        client ||= Spaceship::ConnectAPI
+        attributes = reverse_attr_mapping(attributes)
+        client.patch_app_clip_version_localization(app_clip_version_localization_id: id, attributes: attributes)
+      end
+    end
+  end
+end
