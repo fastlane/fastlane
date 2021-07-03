3eebfce80f56c565364fef0bab9b838215c50eeb
ainame <s.namai.09@gmail.com>
2021-07-04 01:24:42 +0900

    Remove unsupported ones

--- spaceship/lib/spaceship/connect_api/models/app_clip_version.rb
+++ spaceship/lib/spaceship/connect_api/models/app_clip_version.rb
@@ -28,18 +28,6 @@ module Spaceship
         resps.flat_map(&:to_models)
       end
 
-      def get_release_with_app_store_version(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
-        client ||= Spaceship::ConnectAPI
-        resps = client.get_app_clip_version_release_with_app_store_version(app_clip_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
-        resps.flat_map(&:to_models)
-      end
-
-      def get_app_clip_app_store_review_detail(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
-        client ||= Spaceship::ConnectAPI
-        resps = client.get_app_clip_app_store_review_detail(app_clip_version_id: id, filter: filter, includes: includes, limit: limit, sort: sort).all_pages
-        resps.flat_map(&:to_models)
-      end
-
       def create_app_store_version_localization(client: nil, attributes: nil)
         client ||= Spaceship::ConnectAPI
         client.post_app_clip_version_localization(app_clip_version_id: id, attributes: attributes).first
