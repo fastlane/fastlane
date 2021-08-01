fa344284c6b326c2d4b7865aa9f6818e0a3b5d66
atyamash <atyamash@yahoo-corp.jp>
2021-08-02 00:24:13 +0900

    add no-ansi option to clean_cocoapods_cache action

--- fastlane/lib/fastlane/actions/clean_cocoapods_cache.rb
+++ fastlane/lib/fastlane/actions/clean_cocoapods_cache.rb
@@ -7,6 +7,7 @@ module Fastlane
         cmd = ['pod cache clean']
 
         cmd << params[:name].to_s if params[:name]
+        cmd << '--no-ansi' if params[:no_ansi]
         cmd << '--all'
 
         Actions.sh(cmd.join(' '))
@@ -24,7 +25,12 @@ module Fastlane
                                        optional: true,
                                        verify_block: proc do |value|
                                          UI.user_error!("You must specify pod name which should be removed from cache") if value.to_s.empty?
-                                       end)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :no_ansi,
+                                       env_name: "FL_CLEAN_COCOAPODS_CACHE_NO_ANSI",
+                                       description: "Show output without ANSI codes",
+                                       type: Boolean,
+                                       default_value: false)
         ]
       end
 
