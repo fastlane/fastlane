5dc9f74769c0b9e3688e8befe24f3ef6de4c6187
Josh Holtz <me@joshholtz.com>
2021-11-15 21:43:45 -0600

    Version bump to 2.198.0 (#19611)

--- fastlane/swift/GymfileProtocol.swift
+++ fastlane/swift/GymfileProtocol.swift
@@ -196,4 +196,4 @@ public extension GymfileProtocol {
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.89]
+// FastlaneRunnerAPIVersion [0.9.90]
