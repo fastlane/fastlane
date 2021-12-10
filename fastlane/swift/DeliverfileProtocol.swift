b3799145dfd726cd43751d158e27d39bc522e920
Josh Holtz <me@joshholtz.com>
2021-12-10 13:36:16 -0600

    Version bump to 2.199.0

--- fastlane/swift/DeliverfileProtocol.swift
+++ fastlane/swift/DeliverfileProtocol.swift
@@ -260,4 +260,4 @@ public extension DeliverfileProtocol {
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.88]
+// FastlaneRunnerAPIVersion [0.9.89]
