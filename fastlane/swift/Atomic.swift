47ac7b7b68889a87a7f4e3f8749cfb183172f1ac
Jorge Revuelta Herrero <minuscorp@gmail.com>
2021-12-28 22:18:43 +0100

    Cleanup hacky code

--- fastlane/swift/Atomic.swift
+++ fastlane/swift/Atomic.swift
@@ -7,8 +7,6 @@ protocol DictionaryProtocol: class {
     associatedtype Key: Hashable
     associatedtype Value
 
-    init()
-
     subscript(_: Key) -> Value? { get set }
 
     @discardableResult
@@ -33,60 +31,62 @@ protocol LockProtocol: DictionaryProtocol {
     associatedtype Lock
 
     var _lock: Lock { get set }
-}
 
-extension LockProtocol {
-    func lock() {
-        if #available(macOS 10.12, *), let _lock = _lock as? UnsafeMutablePointer<os_unfair_lock> {
-            os_unfair_lock_lock(_lock)
-        } else if var _lock = _lock as? Int32 {
-            OSSpinLockLock(&_lock)
-        }
-    }
-
-    func unlock() {
-        if #available(macOS 10.12, *), let _lock = _lock as? UnsafeMutablePointer<os_unfair_lock> {
-            os_unfair_lock_unlock(_lock)
-        } else if var _lock = _lock as? Int32 {
-            OSSpinLockUnlock(&_lock)
-        }
-    }
+    func lock()
+    func unlock()
 }
 
 protocol AnyLock {}
 
-@available(macOS, deprecated: 10.12)
-extension OSSpinLock: AnyLock {
-    static func make() -> Self {
-        OS_SPINLOCK_INIT
+extension UnsafeMutablePointer: AnyLock {
+    @available(macOS, deprecated: 10.12)
+    static func make() -> Self where Pointee == OSSpinLock {
+        let spin = UnsafeMutablePointer<OSSpinLock>.allocate(capacity: 1)
+        spin.initialize(to: OS_SPINLOCK_INIT)
+        return spin
     }
-}
-
-@available(macOS, introduced: 10.12)
-extension UnsafeMutablePointer: AnyLock where Pointee == os_unfair_lock {
-    static func make() -> Self {
+    
+    @available(macOS, introduced: 10.12)
+    static func make() -> Self where Pointee == os_unfair_lock {
         let unfairLock = UnsafeMutablePointer<os_unfair_lock>.allocate(capacity: 1)
         unfairLock.initialize(to: os_unfair_lock())
         return unfairLock
+    }
+    
+    @available(macOS, deprecated: 10.12)
+    static func lock(_ lock: Self) where Pointee == OSSpinLock {
+        OSSpinLockLock(lock)
+    }
+    
+    @available(macOS, deprecated: 10.12)
+    static func unlock(_ lock: Self) where Pointee == OSSpinLock {
+        OSSpinLockUnlock(lock)
+    }
+    
+    @available(macOS, introduced: 10.12)
+    static func lock(_ lock: Self) where Pointee == os_unfair_lock {
+        os_unfair_lock_lock(lock)
+    }
+    
+    @available(macOS, introduced: 10.12)
+    static func unlock(_ lock: Self) where Pointee == os_unfair_lock {
+        os_unfair_lock_unlock(lock)
     }
 }
 
 // MARK: - Classes
 
 class AtomicDictionary<Key: Hashable, Value>: LockProtocol {
+    
     typealias Lock = AnyLock
 
-    var _lock: Lock = {
-        if #available(macOS 10.12, *) {
-            return UnsafeMutablePointer<os_unfair_lock>.make()
-        } else {
-            return OSSpinLock.make()
-        }
-    }()
+    var _lock: Lock
 
     private var storage: [Key: Value] = [:]
 
-    required init() {}
+    init(_ lock: Lock) {
+        self._lock = lock
+    }
 
     @discardableResult
     func removeValue(forKey key: Key) -> Value? {
@@ -106,15 +106,46 @@ class AtomicDictionary<Key: Hashable, Value>: LockProtocol {
         defer { unlock() }
         storage[key] = value
     }
+    
+    func lock() {
+        fatalError()
+    }
+    
+    func unlock() {
+        fatalError()
+    }
+}
+
+@available(macOS, introduced: 10.12)
+final class UnfairAtomicDictionary<Key: Hashable, Value>: AtomicDictionary<Key, Value> {
+    typealias Lock = UnsafeMutablePointer<os_unfair_lock>
+    
+    init() {
+        super.init(Lock.make())
+    }
+
+    override func lock() {
+        Lock.lock(_lock as! Lock)
+    }
+
+    override func unlock() {
+        Lock.unlock(_lock as! Lock)
+    }
 }
 
 @available(macOS, deprecated: 10.12)
-class OSSPinAtomicDictionary<Key: Hashable, Value>: AtomicDictionary<Key, Value> {}
+final class OSSPinAtomicDictionary<Key: Hashable, Value>: AtomicDictionary<Key, Value> {
+    typealias Lock = UnsafeMutablePointer<OSSpinLock>
+    
+    init() {
+        super.init(Lock.make())
+    }
 
-@available(macOS, introduced: 10.12)
-class UnfairAtomicDictionary<Key: Hashable, Value>: AtomicDictionary<Key, Value> {
+    override func lock() {
+        Lock.lock(_lock as! Lock)
+    }
 
-    deinit {
-        (_lock as? UnsafeMutablePointer<os_unfair_lock>)?.deallocate()
+    override func unlock() {
+        Lock.unlock(_lock as! Lock)
     }
 }
