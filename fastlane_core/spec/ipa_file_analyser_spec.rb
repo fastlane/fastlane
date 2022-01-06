7f8fef62e5fba9520d6dd2987fe810a7a7984099
Josh Holtz <me@joshholtz.com>
2022-01-05 21:38:50 -0600

    Skip unzip tests if windows

--- fastlane_core/spec/ipa_file_analyser_spec.rb
+++ fastlane_core/spec/ipa_file_analyser_spec.rb
@@ -28,26 +28,28 @@ describe FastlaneCore do
     end
 
     context 'with fetch_info_plist_with_unzip' do
-      before(:each) do
-        expect(FastlaneCore::IpaFileAnalyser).to receive(:fetch_info_plist_with_rubyzip).and_return(nil)
-        expect(FastlaneCore::IpaFileAnalyser).to receive(:fetch_info_plist_with_unzip).and_call_original
-      end
-
-      describe '::fetch_app_identifier' do
-        subject { described_class.fetch_app_identifier(path) }
-        it { is_expected.to eq('com.example.Sample') }
-        context 'when contains embedded app bundle' do
-          let('ipa') { 'ContainsWatchApp' }
-          it { is_expected.to eq('com.example.Sample') }
+      unless FastlaneCore::Helper.windows?
+        before(:each) do
+          expect(FastlaneCore::IpaFileAnalyser).to receive(:fetch_info_plist_with_rubyzip).and_return(nil)
+          expect(FastlaneCore::IpaFileAnalyser).to receive(:fetch_info_plist_with_unzip).and_call_original
         end
-      end
 
-      describe '::fetch_app_build' do
-        subject { described_class.fetch_app_build(path) }
-        it { is_expected.to eq('1') }
-        context 'when contains embedded app bundle' do
-          let('ipa') { 'ContainsWatchApp' }
+        describe '::fetch_app_identifier' do
+          subject { described_class.fetch_app_identifier(path) }
+          it { is_expected.to eq('com.example.Sample') }
+          context 'when contains embedded app bundle' do
+            let('ipa') { 'ContainsWatchApp' }
+            it { is_expected.to eq('com.example.Sample') }
+          end
+        end
+
+        describe '::fetch_app_build' do
+          subject { described_class.fetch_app_build(path) }
           it { is_expected.to eq('1') }
+          context 'when contains embedded app bundle' do
+            let('ipa') { 'ContainsWatchApp' }
+            it { is_expected.to eq('1') }
+          end
         end
       end
     end
