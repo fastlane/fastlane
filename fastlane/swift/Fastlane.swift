6569fbc46141c633ef577c75c2c249afd04fa4d5
Josh Holtz <me@joshholtz.com>
2022-01-25 21:49:48 -0600

    Version bump to 2.202.0 (#19863)

--- fastlane/swift/Fastlane.swift
+++ fastlane/swift/Fastlane.swift
@@ -13394,4 +13394,4 @@ public let snapshotfile: Snapshotfile = .init()
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.146]
+// FastlaneRunnerAPIVersion [0.9.147]
--- fastlane/swift/Precheckfile.swift
+++ fastlane/swift/Precheckfile.swift
@@ -17,4 +17,4 @@ public class Precheckfile: PrecheckfileProtocol {
     // during the `init` process, and you won't see this message
 }
 
-// Generated with fastlane 2.201.2
+// Generated with fastlane 2.202.0
