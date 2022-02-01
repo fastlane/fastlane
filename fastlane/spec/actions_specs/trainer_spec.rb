1359917910acf3f1dcebaa3f54119075246b57a1
David Cacenabes <david.cacenabes@glovoapp.com>
2022-02-01 15:35:25 +0100

    Flag tests to require Xcode

--- fastlane/spec/actions_specs/trainer_spec.rb
+++ fastlane/spec/actions_specs/trainer_spec.rb
@@ -2,7 +2,7 @@ describe Fastlane do
   describe Fastlane::FastFile do
     describe "Trainer Integration" do
       context ":fail_build" do
-        it "does not raise an error if tests fail and fail_build is false" do
+        it "does not raise an error if tests fail and fail_build is false", requires_xcode: true do
           expect do
             Fastlane::FastFile.new.parse("lane :parse_test_result do
                 trainer(
@@ -14,7 +14,7 @@ describe Fastlane do
           end.not_to(raise_error)
         end
 
-        it "raises an error if tests fail and fail_build is true" do
+        it "raises an error if tests fail and fail_build is true", requires_xcode: true do
           failing_xcresult_path = "../trainer/spec/fixtures/Test.test_result.xcresult"
           expect do
             Fastlane::FastFile.new.parse("lane :parse_test_result do
