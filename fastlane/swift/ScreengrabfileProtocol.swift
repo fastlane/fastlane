1ee89cc0f246fb7a0429dbcdecb0bb45bad9b213
Josh Holtz <me@joshholtz.com>
2022-02-03 15:31:49 -0600

    Version bump to 2.204.2

--- fastlane/swift/ScreengrabfileProtocol.swift
+++ fastlane/swift/ScreengrabfileProtocol.swift
@@ -96,4 +96,4 @@ public extension ScreengrabfileProtocol {
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.95]
+// FastlaneRunnerAPIVersion [0.9.96]
