40d46e8c713f8072f28f69f3a49ea3e69e93dd92
Josh Soref <jsoref@users.noreply.github.com>
2022-03-07 09:16:59 -0500

    spelling: attempts
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- scan/lib/scan/options.rb
+++ scan/lib/scan/options.rb
@@ -269,7 +269,7 @@ module Scan
                                      default_value: Fastlane::Helper::XcodebuildFormatterHelper.xcbeautify_installed? ? 'xcbeautify' : 'xcpretty',
                                      default_value_dynamic: true),
         FastlaneCore::ConfigItem.new(key: :output_remove_retry_attempts,
-                                     env_name: "SCAN_OUTPUT_REMOVE_RETRY_ATTEMPS",
+                                     env_name: "SCAN_OUTPUT_REMOVE_RETRY_ATTEMPTS",
                                      description: "Remove retry attempts from test results table and the JUnit report (if not using xcpretty)",
                                      type: Boolean,
                                      default_value: false),
