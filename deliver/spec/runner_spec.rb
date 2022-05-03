3b6cbd2002ed1fc5135d57ee367b6f5c758b473b
Pol Piella Abadia <hi@polpiella.dev>
2022-05-03 20:12:40 +0100

    Updates unit tests for `deliver`’s `runner`

--- deliver/spec/runner_spec.rb
+++ deliver/spec/runner_spec.rb
@@ -98,4 +98,51 @@ describe Deliver::Runner do
       end
     end
   end
+
+  describe :verify_binary do
+    let(:transporter) { MockTransporter.new }
+    before do
+      allow(FastlaneCore::ItunesTransporter).to receive(:new).and_return(transporter)
+    end
+
+    describe 'with an IPA file for iOS' do
+      it 'verifies the IPA for the iOS platform' do
+        expect_any_instance_of(FastlaneCore::IpaUploadPackageBuilder).to receive(:generate)
+          .with(app_id: 'YI8C2AS', ipa_path: 'ACME.ipa', package_path: '/tmp', platform: 'ios')
+          .and_return('path')
+        expect(transporter).to receive(:verify).with(package_path: 'path').and_return(true)
+        runner.verify_binary
+      end
+    end
+
+    describe 'with an IPA file for tvOS' do
+      before do
+        options[:platform] = 'appletvos'
+      end
+
+      it 'verifies the IPA for the tvOS platform' do
+        expect_any_instance_of(FastlaneCore::IpaUploadPackageBuilder).to receive(:generate)
+          .with(app_id: 'YI8C2AS', ipa_path: 'ACME.ipa', package_path: '/tmp', platform: 'appletvos')
+          .and_return('path')
+        expect(transporter).to receive(:verify).with(package_path: 'path').and_return(true)
+        runner.verify_binary
+      end
+    end
+
+    describe 'with a PKG file for macOS' do
+      before do
+        options[:platform] = 'osx'
+        options[:pkg] = 'ACME.pkg'
+        options[:ipa] = nil
+      end
+
+      it 'verifies the PKG for the macOS platform' do
+        expect_any_instance_of(FastlaneCore::PkgUploadPackageBuilder).to receive(:generate)
+          .with(app_id: 'YI8C2AS', pkg_path: 'ACME.pkg', package_path: '/tmp', platform: 'osx')
+          .and_return('path')
+        expect(transporter).to receive(:verify).with(package_path: 'path').and_return(true)
+        runner.verify_binary
+      end
+    end
+  end
 end
