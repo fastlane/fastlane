03cffaa2130442d336a79ec5b7e7ca926e5ff7f2
Matt Blessed <matt@askimpulse.com>
2022-05-18 13:41:33 -0400

    deliver: handle app clip metadata copy from live version if no options specified

--- spaceship/lib/spaceship/connect_api/tunes/tunes.rb
+++ spaceship/lib/spaceship/connect_api/tunes/tunes.rb
@@ -1298,7 +1298,7 @@ module Spaceship
         end
 
         # https://developer.apple.com/documentation/appstoreconnectapi/create_a_default_app_clip_experience
-        def post_app_clip_default_experience(app_clip_id: nil, app_store_version_id: nil, attributes: {})
+        def post_app_clip_default_experience(app_clip_id: nil, app_store_version_id: nil, attributes: {}, template_default_experience_id: nil)
           body = {
             data: {
               type: "appClipDefaultExperiences",
@@ -1319,6 +1319,15 @@ module Spaceship
               }
             }
           }
+
+          unless template_default_experience_id.nil?
+            body[:data][:relationships][:appClipDefaultExperienceTemplate] = {
+              data: {
+                id: template_default_experience_id,
+                type: 'appClipDefaultExperiences'
+              }
+            }
+          end
 
           tunes_request_client.post("appClipDefaultExperiences", body)
         end
