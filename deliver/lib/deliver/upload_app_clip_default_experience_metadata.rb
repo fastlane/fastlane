eb4e90168377d2cc5bde68eebe5566be7670bf25
Matt Blessed <matt@askimpulse.com>
2022-05-18 13:41:33 -0400

    deliver: handle app clip metadata copy from live version if no options specified

--- deliver/lib/deliver/upload_app_clip_default_experience_metadata.rb
+++ deliver/lib/deliver/upload_app_clip_default_experience_metadata.rb
@@ -12,11 +12,6 @@ module Deliver
       # app clip default experience metadata is not editable in a live version
       return if options[:edit_live]
 
-      # validate options
-      subtitle_localized = options[:app_clip_default_experience_subtitle]
-      action = options[:app_clip_default_experience_action]
-      UI.user_error!("You must provide at least the subtitle and action for a app clip default experience") if subtitle_localized.nil? || action.nil?
-
       app = Deliver.cache[:app]
       platform = Spaceship::ConnectAPI::Platform.map(options[:platform])
       version = fetch_edit_app_store_version(app, platform)
@@ -26,7 +21,24 @@ module Deliver
       # TODO: support handling more than one app clip target per app
       app_clips = app.get_app_clips
       app_clip = app_clips.first
-      UI.user_error!("A build with an app clip must be uploaded to App Store Connect before uploading the default experience metadata") if app_clip.nil?
+
+      # Validate options
+      subtitle_localized = options[:app_clip_default_experience_subtitle]
+      action = options[:app_clip_default_experience_action]
+      has_options_specified = !subtitle_localized.nil? or !action.nil?
+
+      if app_clip.nil?
+        UI.user_error!("A build with an app clip must be uploaded to App Store Connect before uploading the default experience metadata") if has_options_specified
+        # Nothing to do if the app clip is nil and no app clip options specified
+        return if app_clip.nil?
+      end
+
+      unless has_options_specified
+        # Handle the default case where there is an app clip, but no options specified.
+        return copy_live_version_app_clip_default_experience_metadata(app: app, platform: platform, edit_version: version, app_clip: app_clip)
+      end
+
+      UI.user_error!("You must provide at least the subtitle and action for a app clip default experience") if subtitle_localized.nil? || action.nil?
 
       # see if there's an existing experience for this version
       existing_default_experience = version.app_clip_default_experience
@@ -48,6 +60,12 @@ module Deliver
     def fetch_edit_app_store_version(app, platform, wait_time: 10)
       retry_if_nil("Cannot find edit app store version", wait_time: wait_time) do
         app.get_edit_app_store_version(platform: platform, includes: 'appClipDefaultExperience')
+      end
+    end
+
+    def fetch_live_app_store_version(app, platform, wait_time: 10)
+      retry_if_nil("Cannot find live app store version", wait_time: wait_time) do
+        app.get_live_app_store_version(platform: platform, includes: 'appClipDefaultExperience')
       end
     end
 
@@ -98,6 +116,50 @@ module Deliver
       end
       app_info_worker.batch_enqueue(localized_subtitle_attributes_by_locale.keys)
       app_info_worker.start
+    end
+
+    # Handle the case where an app clip exists, but the user did not specify any app clip default
+    # experience metadata options. We check if the current editable version already has the app clip
+    # default experience metadata. If not, copy over the live version's app clip default
+    # experience metadata. This mimics the default behavior of creating an App Store version from
+    # the ASC UI.
+    #
+    # This function will also produce warnings, but not outright fail, when it finds missing app
+    # clip metadata.
+    #
+    # As of 2022-05-18, App Store versions created by the ASC API do not "carry over" the live
+    # version's app clip default experience metadata, so we handle this for the user by default.
+    #
+    def copy_live_version_app_clip_default_experience_metadata(app:, platform:, edit_version:, app_clip:)
+      edit_version_default_experience = edit_version.app_clip_default_experience
+      if !edit_version_default_experience
+        live_version = fetch_live_app_info(app, platform)
+        # no live version to carry over metadata from
+        return if live_version.nil?
+
+        live_version_default_experience = live_version.app_clip_default_experience
+        # no live version default experience to carry over metadata from
+        return if live_version_default_experience.nil?
+
+        # create a default experience and use the live version default experience as a "template"
+        Spaceship::ConnectAPI::AppClipDefaultExperience.create(app_clip_id: app_clip.id, app_store_version_id: edit_version.id, template_default_experience_id: live_version_default_experience.id)
+
+        # TODO: if the above code using the "template" doesn't work, we can use the following to manually copy the values
+        #
+        # default_experience.update(attributes: { action: live_version_default_experience.action })
+        # UI.message("Added app clip default experience action from live version #{live_version.version}")
+
+        # # copy the live version localizations
+        # live_version_localizations = Spaceship::ConnectAPI::AppClipDefaultExperience.get(app_clip_default_experience_id: live_version_default_experience.id, includes: 'appClipDefaultExperienceLocalizations').app_clip_default_experience_localizations
+
+        # live_version_localizations.each do |live_localization|
+        #   attributes = { subtitle: live_localization.subtitle, locale: live_localization.locale }
+        #   Spaceship::ConnectAPI::AppClipDefaultExperienceLocalizations.create(default_experience_id: default_experience.id, attributes: attributes)
+        # end
+      else
+        # if the edit version app clip default experience already exists, just check it's values
+        UI.important("ASC requires the app clip default experience to contain a valid `action` value. To specify this value in fastlane use deliver's `app_clip_default_experience_action` option.") if edit_version_default_experience.action.nil?
+      end
     end
   end
 end
--- spaceship/lib/spaceship/connect_api/models/app_clip_default_experience.rb
+++ spaceship/lib/spaceship/connect_api/models/app_clip_default_experience.rb
@@ -20,9 +20,9 @@ module Spaceship
       # API
       #
 
-      def self.create(client: nil, app_clip_id:, app_store_version_id:, attributes:)
+      def self.create(client: nil, app_clip_id:, app_store_version_id:, attributes:, template_default_experience_id: nil)
         client ||= Spaceship::ConnectAPI
-        client.post_app_clip_default_experience(app_clip_id: app_clip.id, app_store_version_id: version.id, attributes: { action: action })
+        return client.post_app_clip_default_experience(app_clip_id: app_clip.id, app_store_version_id: version.id, attributes: { action: action }, template_default_experience_id: template_default_experience_id)
       end
 
       def self.get(client: nil, app_clip_default_experience_id:, filter: {}, includes: {}, limit: nil, sort: nil)
