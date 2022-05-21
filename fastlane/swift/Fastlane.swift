f2e48056f9d97a2d1a942e4ec41d6b0f752a56ee
Josh Holtz <me@joshholtz.com>
2022-05-21 14:25:29 -0500

    Version bump to 2.206.1 (#20308)

--- fastlane/swift/Fastlane.swift
+++ fastlane/swift/Fastlane.swift
@@ -13466,4 +13466,4 @@ public let snapshotfile: Snapshotfile = .init()
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.156]
+// FastlaneRunnerAPIVersion [0.9.157]
