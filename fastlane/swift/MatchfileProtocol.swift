3bbaf61a06cb0635c98161fd260beec31dcfa792
Josh Holtz <me@joshholtz.com>
2022-05-25 08:21:09 -0500

    Version bump to 2.206.2 (#20320)

--- fastlane/swift/MatchfileProtocol.swift
+++ fastlane/swift/MatchfileProtocol.swift
@@ -200,4 +200,4 @@ public extension MatchfileProtocol {
 
 // Please don't remove the lines below
 // They are used to detect outdated files
-// FastlaneRunnerAPIVersion [0.9.101]
+// FastlaneRunnerAPIVersion [0.9.102]
