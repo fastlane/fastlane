38a316ff6ffe0717d94f9c88a3dff100195f13d6
Paul Taykalo <ptaykalo@macpaw.com>
2022-06-30 05:09:05 +0300

    [match] add profile_type filtering when checking if profile exists (#20311)
    * [match] add profile_type filtering when checking if profile exists
    
    * Moved nuke spec to module spec
    
    Co-authored-by: Josh Holtz <me@joshholtz.com>

--- match/lib/match/nuke.rb
+++ match/lib/match/nuke.rb
@@ -150,9 +150,7 @@ module Match
       # Get all iOS and macOS profile
       self.profiles = []
       prov_types.each do |prov_type|
-        types = profile_types(prov_type)
-        # Filtering on 'profileType' seems to be undocumented as of 2020-07-30
-        # but works on both web session and official API
+        types = Match.profile_types(prov_type)
         self.profiles += Spaceship::ConnectAPI::Profile.all(filter: { profileType: types.join(",") }, includes: "certificates")
       end
 
@@ -403,55 +401,6 @@ module Match
         ]
       else
         raise "Unknown type '#{type}'"
-      end
-    end
-
-    # The kind of provisioning profile we're interested in
-    def profile_types(prov_type)
-      case prov_type.to_sym
-      when :appstore
-        return [
-          Spaceship::ConnectAPI::Profile::ProfileType::IOS_APP_STORE,
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_APP_STORE,
-          Spaceship::ConnectAPI::Profile::ProfileType::TVOS_APP_STORE,
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_CATALYST_APP_STORE
-        ]
-      when :development
-        return [
-          Spaceship::ConnectAPI::Profile::ProfileType::IOS_APP_DEVELOPMENT,
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_APP_DEVELOPMENT,
-          Spaceship::ConnectAPI::Profile::ProfileType::TVOS_APP_DEVELOPMENT,
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_CATALYST_APP_DEVELOPMENT
-        ]
-      when :enterprise
-        profiles = [
-          Spaceship::ConnectAPI::Profile::ProfileType::IOS_APP_INHOUSE,
-          Spaceship::ConnectAPI::Profile::ProfileType::TVOS_APP_INHOUSE
-        ]
-
-        # As of 2022-06-25, only available with Apple ID auth
-        if Spaceship::ConnectAPI.token
-          UI.important("Skipping #{Spaceship::ConnectAPI::Profile::ProfileType::MAC_APP_INHOUSE} and #{Spaceship::ConnectAPI::Profile::ProfileType::MAC_CATALYST_APP_INHOUSE}... only available with Apple ID auth")
-        else
-          profiles += [
-            Spaceship::ConnectAPI::Profile::ProfileType::MAC_APP_INHOUSE,
-            Spaceship::ConnectAPI::Profile::ProfileType::MAC_CATALYST_APP_INHOUSE
-          ]
-        end
-
-        return profiles
-      when :adhoc
-        return [
-          Spaceship::ConnectAPI::Profile::ProfileType::IOS_APP_ADHOC,
-          Spaceship::ConnectAPI::Profile::ProfileType::TVOS_APP_ADHOC
-        ]
-      when :developer_id
-        return [
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_APP_DIRECT,
-          Spaceship::ConnectAPI::Profile::ProfileType::MAC_CATALYST_APP_DIRECT
-        ]
-      else
-        raise "Unknown provisioning type '#{prov_type}'"
       end
     end
 
