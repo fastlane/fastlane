b0241cadd75893148951a224c42cef648ce70f81
Caruceru Cristian <caruceru.cristian@gmail.com>
2022-08-21 13:18:27 +0200

    add exception over existing

--- spaceship/lib/spaceship/connect_api/models/app_info_localization.rb
+++ spaceship/lib/spaceship/connect_api/models/app_info_localization.rb
@@ -33,14 +33,14 @@ module Spaceship
         attributes = reverse_attr_mapping(attributes)
         client.patch_app_info_localization(app_info_localization_id: id, attributes: attributes)
       rescue
-        raise $!, "#{$!} for language '#{@locale}'", $!.backtrace
+        raise "An exception occured for language '#{@locale}'"
       end
 
       def delete!(client: nil, filter: {}, includes: nil, limit: nil, sort: nil)
         client ||= Spaceship::ConnectAPI
         client.delete_app_info_localization(app_info_localization_id: id)
       rescue
-        raise $!, "#{$!} for language '#{@locale}'", $!.backtrace
+        raise "An exception occured for language '#{@locale}'"
       end
     end
   end
