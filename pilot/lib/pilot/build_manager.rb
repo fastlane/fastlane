f4cd02047284bcd0cf124b8c7552fd917341eabb
freddi <freddi@linecorp.com>
2022-09-13 18:16:34 +0900

    fix lint

--- pilot/lib/pilot/build_manager.rb
+++ pilot/lib/pilot/build_manager.rb
@@ -390,13 +390,13 @@ module Pilot
       # Use JWT auth
       api_token = Spaceship::ConnectAPI.token
       api_key = if options[:api_key].nil? && !options[:api_key_path].nil?
-          # Load api key info if user set api_key_path, not api_key
-          { key_id: api_token.key_id, issuer_id: api_token.issuer_id, key: api_token.key_raw, is_key_content_base64: api_token.is_key_content_base64 }
-        elsif !options[:api_key].nil?
-          options[:api_key]
-        else
-          nil
-        end
+        # Load api key info if user set api_key_path, not api_key
+        { key_id: api_token.key_id, issuer_id: api_token.issuer_id, key: api_token.key_raw, is_key_content_base64: api_token.is_key_content_base64 }
+      elsif !options[:api_key].nil?
+        options[:api_key]
+      else
+        nil
+      end
 
       unless api_token.nil?
         api_token.refresh! if api_token.expired?
