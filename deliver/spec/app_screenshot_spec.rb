de457a8cba5707756fbbe42dd4e292b180c9f33c
Evgeny Cherpak <cherpake@gmail.com>
2022-09-25 23:53:03 +0300

    upload 6.7inch screenshots

--- deliver/spec/app_screenshot_spec.rb
+++ deliver/spec/app_screenshot_spec.rb
@@ -66,6 +66,11 @@ describe Deliver::AppScreenshot do
     end
 
     describe "valid screen sizes" do
+        it "should calculate all 6.7 inch iPhone resolutions" do
+          expect_screen_size_from_file("iPhone14ProMax-Portrait{1290x2796}.jpg").to eq(ScreenSize::IOS_67)
+          expect_screen_size_from_file("iPhone14ProMax-Landscape{2796x1290}.jpg").to eq(ScreenSize::IOS_67)
+        end
+        
       it "should calculate all 6.5 inch iPhone resolutions" do
         expect_screen_size_from_file("iPhoneXSMax-Portrait{1242x2688}.jpg").to eq(ScreenSize::IOS_65)
         expect_screen_size_from_file("iPhoneXSMax-Landscape{2688x1242}.jpg").to eq(ScreenSize::IOS_65)
@@ -148,6 +153,11 @@ describe Deliver::AppScreenshot do
     end
 
     describe "valid iMessage app screen sizes" do
+      it "should calculate all 6.5 inch iPhone resolutions" do
+        expect_screen_size_from_file("iMessage/en-GB/iPhone14ProMax-Portrait{1290x2796}.jpg").to eq(ScreenSize::IOS_67_MESSAGES)
+        expect_screen_size_from_file("iMessage/en-GB/iPhone14ProMax-Landscape{2796x1290}.jpg").to eq(ScreenSize::IOS_67_MESSAGES)
+      end
+          
       it "should calculate all 6.5 inch iPhone resolutions" do
         expect_screen_size_from_file("iMessage/en-GB/iPhoneXSMax-Portrait{1242x2688}.jpg").to eq(ScreenSize::IOS_65_MESSAGES)
         expect_screen_size_from_file("iMessage/en-GB/iPhoneXSMax-Landscape{2688x1242}.jpg").to eq(ScreenSize::IOS_65_MESSAGES)
@@ -306,6 +316,11 @@ describe Deliver::AppScreenshot do
 
     it "should return nil for 6.1 inch displays (iPhone XR)" do
       expect(app_screenshot_with(ScreenSize::IOS_61).device_type).to be_nil
+    end
+
+    it "should return iphone65 for 6.7 inch displays" do
+      expect(app_screenshot_with(ScreenSize::IOS_67).device_type).to eq("APP_IPHONE_67")
+      expect(app_screenshot_with(ScreenSize::IOS_67_MESSAGES).device_type).to eq("IMESSAGE_APP_IPHONE_67")
     end
 
     it "should return iphone65 for 6.5 inch displays" do
