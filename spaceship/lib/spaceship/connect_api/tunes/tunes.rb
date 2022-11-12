22634038da055a8c103d5e8f928b63e921d9538c
Hiroto Nakamura <Hiroto-N@users.noreply.github.com>
2022-11-13 05:11:49 +1300

    [spaceship] implement resolution center API in Spaceship::ConnectAPI (#20726)
    * Get resolution center threads
    
    * Add models for resolution messages and rejections
    
    * Add resolution center calls to tunes client
    
    * fix rubocop findings
    
    * Add ability to get resolution center messages from ReviewSubmission (#1)
    
    * Fix list issues
    
    * Added comment about only available with Apple ID auth
    
    Co-authored-by: Addison Brickey <addbrick@gmail.com>
    Co-authored-by: Josh Holtz <me@joshholtz.com>

--- spaceship/lib/spaceship/connect_api/tunes/tunes.rb
+++ spaceship/lib/spaceship/connect_api/tunes/tunes.rb
@@ -1252,6 +1252,29 @@ module Spaceship
           params = tunes_request_client.build_params(filter: nil, includes: nil, limit: nil, sort: nil)
           tunes_request_client.get("territories", params)
         end
+
+        #
+        # resolutionCenter
+        #
+        # As of 2022-11-11:
+        # This is not official available throught the App Store Connect API using an API Key.
+        # This is only works with Apple ID auth.
+        #
+
+        def get_resolution_center_threads(filter: {}, includes: nil)
+          params = tunes_request_client.build_params(filter: filter, includes: includes)
+          tunes_request_client.get('resolutionCenterThreads', params)
+        end
+
+        def get_resolution_center_messages(thread_id:, filter: {}, includes: nil)
+          params = tunes_request_client.build_params(filter: filter, includes: includes)
+          tunes_request_client.get("resolutionCenterThreads/#{thread_id}/resolutionCenterMessages", params)
+        end
+
+        def get_review_rejection(filter: {}, includes: nil)
+          params = tunes_request_client.build_params(filter: filter, includes: includes)
+          tunes_request_client.get("reviewRejections", params)
+        end
       end
     end
   end
