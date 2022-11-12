6609e7a298aa87dcd6092c1634d5477bee0e6468
Josh Holtz <me@joshholtz.com>
2022-11-12 10:25:57 -0600

    Updated snapshot helper version

--- snapshot/lib/assets/SnapshotHelper.swift
+++ snapshot/lib/assets/SnapshotHelper.swift
@@ -306,4 +306,4 @@ private extension CGFloat {
 
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
-// SnapshotHelperVersion [1.28]
+// SnapshotHelperVersion [1.29]
