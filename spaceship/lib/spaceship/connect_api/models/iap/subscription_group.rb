81654ad3a335876bb90049bdb34caeeba9e7406e
Addison Brickey <addbrick@gmail.com>
2023-01-08 15:56:04 -0800

    Fix suvscription_group.create_subscription_group_localization

--- spaceship/lib/spaceship/connect_api/models/iap/subscription_group.rb
+++ spaceship/lib/spaceship/connect_api/models/iap/subscription_group.rb
@@ -42,7 +42,7 @@ module Spaceship
       def create_subscription_group_localization(client: nil, custom_app_name:, locale:, name:)
         client ||= Spaceship::ConnectAPI
         resps = client.create_subscription_group_localization(custom_app_name: custom_app_name, locale: locale, name: name, family_id: id)
-        return resps.flat_map(&:to_models).first
+        return resps.to_models.first
       end
     end
   end
