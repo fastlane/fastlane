907f72cea484a7b21b4f111ed1ee52fe8c3152bb
Stanislav Gribushin <stanislav@jigx.com>
2023-03-29 13:16:57 +0200

    fix http requests in tests

--- match/spec/spec_helper.rb
+++ match/spec/spec_helper.rb
@@ -8,3 +8,18 @@ def before_each_match
   ENV["DELIVER_USER"] = "flapple@krausefx.com"
   ENV["DELIVER_PASSWORD"] = "so_secret"
 end
+
+def stub_request(*args)
+  WebMock::API.stub_request(*args)
+end
+
+stub_request(:put, "http://169.254.169.254/latest/api/token").
+  with(
+    headers: {
+    'Accept' => '*/*',
+    'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent' => 'aws-sdk-ruby3/3.167.0',
+    'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
+    }
+).
+  to_return(status: 200, body: "", headers: {})
