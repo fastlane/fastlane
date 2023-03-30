bf741fda78fc9e3c53a6137452c2673b0d4f1bc3
Stanislav Gribushin <stanislav@jigx.com>
2023-03-30 19:55:35 +0200

    fix http requests in tests

--- match/spec/mock_servers.rb
+++ match/spec/mock_servers.rb
@@ -3,34 +3,10 @@ RSpec.configure do |config|
 
   config.before do
     stub_request(:put, %r(http://169.254.169.254/latest/.*)).
-      with(
-        headers: {
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent' => 'aws-sdk-ruby3/3.167.0',
-        'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
-        }
-    ).
       to_return(status: 200, body: "", headers: {})
     stub_request(:get, %r(http://169.254.169.254/latest/.*)).
-      with(
-        headers: {
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent' => 'aws-sdk-ruby3/3.167.0',
-        'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
-        }
-    ).
       to_return(status: 200, body: "", headers: {})
     stub_request(:post, %r(http://169.254.169.254/latest/.*)).
-      with(
-        headers: {
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent' => 'aws-sdk-ruby3/3.167.0',
-        'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
-        }
-    ).
       to_return(status: 200, body: "", headers: {})
   end
 end
