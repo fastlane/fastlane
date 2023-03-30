d729a059fdeb259437d1c2b24bcbda4d72712a9b
Stanislav Gribushin <stanislav@jigx.com>
2023-03-30 18:02:43 +0200

    fix http requests in tests

--- match/spec/mock_servers.rb
+++ match/spec/mock_servers.rb
@@ -10,10 +10,27 @@ RSpec.configure do |config|
         'User-Agent' => 'aws-sdk-ruby3/3.167.0',
         'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
         }
-).
+    ).
+      to_return(status: 200, body: "", headers: {})
+    stub_request(:get, %r(http://169.254.169.254/latest/.*)).
+      with(
+        headers: {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent' => 'aws-sdk-ruby3/3.167.0',
+        'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
+        }
+    ).
+      to_return(status: 200, body: "", headers: {})
+    stub_request(:post, %r(http://169.254.169.254/latest/.*)).
+      with(
+        headers: {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent' => 'aws-sdk-ruby3/3.167.0',
+        'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
+        }
+    ).
       to_return(status: 200, body: "", headers: {})
   end
-
-  #   config.after do
-  #   end
 end
