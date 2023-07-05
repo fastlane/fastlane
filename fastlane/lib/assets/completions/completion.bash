eb3f806bd9dce09f801f3975d6ef453d9c380319
Bartosz Kosiorek <gang65@poczta.onet.pl>
2023-07-05 15:23:42 +0200

    Add import support bash and zsh autocomplete
    In previous implementation the imports, like:
      import("../../../FastfileGlobal")
    or
      import('../../../FastfileGlobal')
    
    were ignored, and the lanes were not added for
    autocompletion. This patch is fixing it,
    and autocompletion is working correctly also
    for imported files.

--- fastlane/lib/assets/completions/completion.bash
+++ fastlane/lib/assets/completions/completion.bash
@@ -4,21 +4,26 @@ _fastlane_complete() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
   local completions=""
-  local file
 
   # look for Fastfile either in this directory or fastlane/ then grab the lane names
   if [[ -e "Fastfile" ]]; then
-    file="Fastfile"
+    dir="."
   elif [[ -e "fastlane/Fastfile" ]]; then
-    file="fastlane/Fastfile"
+    dir="fastlane"
   elif [[ -e ".fastlane/Fastfile" ]]; then
-    file=".fastlane/Fastfile"
+    dir=".fastlane"
   else
     return 1
   fi
-
-  # parse 'beta' out of 'lane :beta do', etc
-  completions="$(sed -En 's/^[ 	]*lane +:([^ 	]+).*$/\1/p' "$file")"
+  local imported_files=('Fastfile')
+  # parse imports and grab lane names
+  imported_files+=($(cd $dir && sed -En "s/^[ 	]*import\([\"']([^\"']+)[\"']\)[ 	]*$/\1/p" "Fastfile"))
+  for imported_file in "${imported_files[@]}"; do
+    if [[ -e "$dir/$imported_file" ]]; then
+      # parse 'beta' out of 'lane :beta do', etc
+      completions+=$(cd $dir && sed -En 's/^[ 	]*lane +:([^ 	]+).*$/\1/p' "$imported_file")
+    fi
+  done
   completions="$completions update_fastlane"
 
   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
