ba1f7f757e912e94e8c29c4fe1931b1888f1b2a8
Bob Voorneveld <info@bobvoorneveld.nl>
2023-07-26 12:44:20 +0200

    Make include_mac_in_profiles include all mac devices
    APPLE_SILICON_MAC is not longer supported in the App store connect API, so new silicon mac devices are just mac devices.
    We need to include them when creating a profile if include_mac_in_profiles is set to true.

--- sigh/lib/sigh/runner.rb
+++ sigh/lib/sigh/runner.rb
@@ -290,7 +290,10 @@ module Sigh
                          [Spaceship::ConnectAPI::Device::DeviceClass::MAC]
                        end
       if Sigh.config[:platform].to_s == 'ios' && Sigh.config[:include_mac_in_profiles]
-        device_classes += [Spaceship::ConnectAPI::Device::DeviceClass::APPLE_SILICON_MAC]
+        device_classes += [
+          Spaceship::ConnectAPI::Device::DeviceClass::APPLE_SILICON_MAC,
+          Spaceship::ConnectAPI::Device::DeviceClass::MAC
+        ]
       end
       if Spaceship::ConnectAPI.token
         return Spaceship::ConnectAPI::Device.all.select do |device|
