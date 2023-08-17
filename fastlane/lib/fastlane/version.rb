5d3cd329eebcf19b0daa7e6946c7b3db8ddff287
Roger Oba <rogerluan.oba@gmail.com>
2023-08-16 22:42:01 -0300

    Update Rubocop from 1.12.1 to 1.56.0 (latest) to support Ruby 3.1.

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -2,5 +2,5 @@ module Fastlane
   VERSION = '2.214.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
   MINIMUM_XCODE_RELEASE = "7.0".freeze
-  RUBOCOP_REQUIREMENT = '1.12.1'.freeze
+  RUBOCOP_REQUIREMENT = '1.56.0'.freeze # Minimum version that supports Ruby 3.1
 end
