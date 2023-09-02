18ee73d6e56dacfdea8722b7dcf9225183b57749
Roger Oba <rogerluan.oba@gmail.com>
2023-09-02 16:19:58 -0300

    Run rubocop.

--- fastlane/spec/helper/tool_name_formatting_helper_spec.rb
+++ fastlane/spec/helper/tool_name_formatting_helper_spec.rb
@@ -8,10 +8,11 @@ describe Fastlane::Helper::ToolNameFormattingHelper do
 
   describe "when parsing fixture file" do
     it "should match array of expected errors" do
-      expect(@helper.find_tool_name_formatting_errors).to match_array([
+      expected_errors = [
         "fastlane tools have to be formatted in lowercase: fastlane in 'CONTRIBUTING.md:19': _FastLane_ makes code signing management easy. ❌",
-        "fastlane tools have to be formatted in lowercase: fastlane in 'CONTRIBUTING.md:23': A sentence that contains a _FastLane_ keyword, but also an env var: `FASTLANE_SKIP_CHANGELOG`. ❌",
-      ])
+        "fastlane tools have to be formatted in lowercase: fastlane in 'CONTRIBUTING.md:23': A sentence that contains a _FastLane_ keyword, but also an env var: `FASTLANE_SKIP_CHANGELOG`. ❌"
+      ]
+      expect(@helper.find_tool_name_formatting_errors).to match_array(expected_errors)
     end
   end
 end
