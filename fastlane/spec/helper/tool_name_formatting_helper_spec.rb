207920600828af84552f5dd81b3743e189bb6046
Olivier Halligon <olivier@halligon.net>
2023-09-03 14:09:18 +0200

    Also detect formatting issues at start of line

--- fastlane/spec/helper/tool_name_formatting_helper_spec.rb
+++ fastlane/spec/helper/tool_name_formatting_helper_spec.rb
@@ -9,6 +9,8 @@ describe Fastlane::Helper::ToolNameFormattingHelper do
   describe 'when parsing fixture file' do
     it 'should match array of expected errors' do
       expected_errors = [
+        "fastlane tools have to be formatted in lowercase: fastlane in '#{fixture_path}:17': FastLane makes code signing management easy.",
+        "fastlane tools have to be formatted in lowercase: fastlane in '#{fixture_path}:18': Fastlane makes code signing management easy.",
         "fastlane tools have to be formatted in lowercase: fastlane in '#{fixture_path}:19': _FastLane_ makes code signing management easy.",
         "fastlane tools have to be formatted in lowercase: fastlane in '#{fixture_path}:23': A sentence that contains a _FastLane_ keyword, but also an env var: `FASTLANE_SKIP_CHANGELOG`."
       ]
