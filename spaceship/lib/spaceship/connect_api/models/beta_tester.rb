33a01029c840907cae5f156404e337ccd2bf87d3
Caesar Wirth <caesar@duolingo.com>
2023-09-08 16:35:04 +0200

    [spaceship] Add beta tester metric properties to beta tester object

--- spaceship/lib/spaceship/connect_api/models/beta_tester.rb
+++ spaceship/lib/spaceship/connect_api/models/beta_tester.rb
@@ -15,6 +15,15 @@ module Spaceship
       attr_accessor :beta_tester_metrics
       attr_accessor :builds
 
+      attr_accessor :install_count
+      attr_accessor :crash_count
+      attr_accessor :session_count
+      attr_accessor :beta_tester_state
+      attr_accessor :last_modified_date
+      attr_accessor :remove_after_date
+      attr_accessor :installed_cf_bundle_short_version_string
+      attr_accessor :installed_cf_bundle_version
+
       attr_mapping({
         "firstName" => "first_name",
         "lastName" => "last_name",
@@ -25,7 +34,16 @@ module Spaceship
         "apps" => "apps",
         "betaGroups" => "beta_groups",
         "betaTesterMetrics" => "beta_tester_metrics",
-        "builds" => "builds"
+        "builds" => "builds",
+
+        "installCount" => "install_count",
+        "crashCount" => "crash_count",
+        "sessionCount" => "session_count",
+        "betaTesterState" => "beta_tester_state",
+        "lastModifiedDate" => "last_modified_date",
+        "removeAfterDate" => "remove_after_date",
+        "installedCfBundleShortVersionString" => "installed_cf_bundle_short_version_string",
+        "installedCfBundleVersion" => "installed_cf_bundle_version"
       })
 
       def self.type
