b1c67d873f694578366f8137908b118bda37887f
Daniele Segato <daniele.segato@gmail.com>
2023-10-12 11:29:27 +0200

    Fix supply Client.track_version_codes always expecting releases
    when a track has been created in the Google Play console but still has
    no release the result.release is Nil

--- supply/lib/supply/client.rb
+++ supply/lib/supply/client.rb
@@ -481,7 +481,7 @@ module Supply
           current_edit.id,
           track
         )
-        return result.releases.flat_map(&:version_codes) || []
+        return result.releases&.flat_map(&:version_codes) || []
       rescue Google::Apis::ClientError => e
         return [] if e.status_code == 404 && (e.to_s.include?("trackEmpty") || e.to_s.include?("Track not found"))
         raise
