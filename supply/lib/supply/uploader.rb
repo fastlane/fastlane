fbad7d8567c63e34b2c590ba8f2296fd9ee95131
Mariano Miani <marianomiani@gmail.com>
2023-10-28 11:42:26 +1100

    [supply] Fix issue with releases containing retained versions (#20997) (#20998)
    Fix issue with releases containing retained versions failing to complete the staged rollout in the Google Play Store.

--- supply/lib/supply/uploader.rb
+++ supply/lib/supply/uploader.rb
@@ -128,7 +128,7 @@ module Supply
       UI.user_error!("Unable to find the requested track - '#{Supply.config[:track]}'") unless track
       UI.user_error!("Unable to find the requested release on track - '#{Supply.config[:track]}'") unless release
 
-      version_code = release.version_codes.first
+      version_code = release.version_codes.max
 
       UI.message("Updating #{version_code}'s rollout to '#{Supply.config[:rollout]}' on track '#{Supply.config[:track]}'...")
 
