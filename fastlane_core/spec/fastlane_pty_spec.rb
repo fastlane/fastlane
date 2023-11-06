e31a1d13ee0325cfb4a324b8ff6502934073cefe
Jerome Lacoste <jerome.lacoste@gmail.com>
2023-11-06 13:52:49 +0100

    Fix tests to work on all platforms, and add a test showing up an issue in the code

--- fastlane_core/spec/fastlane_pty_spec.rb
+++ fastlane_core/spec/fastlane_pty_spec.rb
@@ -6,14 +6,31 @@ describe FastlaneCore do
 
         exit_status = FastlaneCore::FastlanePty.spawn('echo foo') do |command_stdout, command_stdin, pid|
           command_stdout.each do |line|
-            @all_lines << line
+            @all_lines << line.chomp
           end
         end
         expect(exit_status).to eq(0)
-        expect(@all_lines).to eq(["foo\r\n"])
+        expect(@all_lines).to eq(["foo"])
       end
 
-      it 'doesn t return -1 if an exception was raised in the block' do
+      it 'doesn t return -1 if an exception was raised in the block in PTY.spawn' do
+        @all_lines = []
+
+        exception = StandardError.new
+        expect {
+          exit_status = FastlaneCore::FastlanePty.spawn('echo foo') do |command_stdout, command_stdin, pid|
+            raise exception
+          end
+        }.to raise_error(FastlaneCore::FastlanePtyError) { |error|
+          expect(error.exit_status).to eq(0) # command was success but output handling failed
+        }
+      end
+
+      it 'doesn t return -1 if an exception was raised in the block in Open3.popen2e' do
+        expect(FastlaneCore::FastlanePty).to receive(:require).with("pty").and_raise(LoadError)
+        allow(FastlaneCore::FastlanePty).to receive(:require).with("open3").and_call_original
+        allow(FastlaneCore::FastlanePty).to receive(:open3)
+
         @all_lines = []
 
         exception = StandardError.new
