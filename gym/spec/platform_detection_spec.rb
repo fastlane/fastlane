c47df881a268d648415370452076d0846cc82c0e
Philipp Resch <philipp.resch@sap.com>
2023-12-17 12:38:20 +0100

    use proper if condition for Xcode version in test

--- gym/spec/platform_detection_spec.rb
+++ gym/spec/platform_detection_spec.rb
@@ -25,21 +25,16 @@ describe Gym do
     expect(Gym.building_multiplatform_for_mac?).to eq(true)
   end
 
-  it "detects the correct platform for a visionOS project", requires_xcodebuild: true do
+  it "detects the correct platform for a visionOS project", requires_xcodebuild: true, if: FastlaneCore::Helper.xcode_at_least?('15.0') do
     options = { project: "./gym/examples/visionos/VisionExample.xcodeproj", sdk: "xros", skip_package_dependencies_resolution: true }
     Gym.config = FastlaneCore::Configuration.create(Gym::Options.available_options, options)
 
-    if FastlaneCore::Helper.xcode_at_least?('15.0')
-      expect(Gym.project.multiplatform?).to eq(false)
-      expect(Gym.project.visionos?).to eq(true)
-      expect(Gym.project.ios?).to eq(false)
+    expect(Gym.project.multiplatform?).to eq(false)
+    expect(Gym.project.visionos?).to eq(true)
+    expect(Gym.project.ios?).to eq(false)
 
-      expect(Gym.building_for_ios?).to eq(true)
-      expect(Gym.building_for_mac?).to eq(false)
-      expect(Gym.building_multiplatform_for_mac?).to eq(false)
-    else
-      UI.important("Skipping test for older xcode versions.")
-      expect(Gym.project.ios?).to eq(true)
-    end
+    expect(Gym.building_for_ios?).to eq(true)
+    expect(Gym.building_for_mac?).to eq(false)
+    expect(Gym.building_multiplatform_for_mac?).to eq(false)
   end
 end
