f0f42862e16b39ee8fa67118c6d6d8e88cd648a3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-27 05:02:37 -0500

    spelling: far, far
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fastlane_core/lib/fastlane_core/string_filters.rb
+++ fastlane_core/lib/fastlane_core/string_filters.rb
@@ -1,15 +1,15 @@
 class String
   # Truncates a given +text+ after a given <tt>length</tt> if +text+ is longer than <tt>length</tt>:
   #
-  #   'Once upon a time in a world far far away'.truncate(27)
+  #   'Once upon a time in a world far, far away'.truncate(27)
   #   # => "Once upon a time in a wo..."
   #
   # Pass a string or regexp <tt>:separator</tt> to truncate +text+ at a natural break:
   #
-  #   'Once upon a time in a world far far away'.truncate(27, separator: ' ')
+  #   'Once upon a time in a world far, far away'.truncate(27, separator: ' ')
   #   # => "Once upon a time in a..."
   #
-  #   'Once upon a time in a world far far away'.truncate(27, separator: /\s/)
+  #   'Once upon a time in a world far, far away'.truncate(27, separator: /\s/)
   #   # => "Once upon a time in a..."
   #
   # The last characters will be replaced with the <tt>:omission</tt> string (defaults to "...")
