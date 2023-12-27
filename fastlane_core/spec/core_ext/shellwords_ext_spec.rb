4588f6b57d75447c4b82f336b897bb9b808e0ab0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-27 05:48:59 -0500

    spelling: shellwords
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fastlane_core/spec/core_ext/shellwords_ext_spec.rb
+++ fastlane_core/spec/core_ext/shellwords_ext_spec.rb
@@ -385,7 +385,7 @@ def expect_correct_implementation_to_be_called(obj, method, os)
       expect(WindowsShellwords).to receive(:shellescape).with(obj).and_call_original
       expect(Shellwords).not_to(receive(:escape))
     else
-      # Shellswords.escape
+      # Shellwords.escape
       expect(Shellwords).to receive(:escape).with(obj).and_call_original
       expect(WindowsShellwords).not_to(receive(:shellescape))
     end
