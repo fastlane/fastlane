0b4ba160553cc27225a635e63579f6842e4d1134
Josh Holtz <me@joshholtz.com>
2024-01-04 09:55:28 -0600

    Version bump to 2.219.0

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
-  VERSION = '2.218.0'.freeze
+  VERSION = '2.219.0'.freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
   MINIMUM_XCODE_RELEASE = "7.0".freeze
   RUBOCOP_REQUIREMENT = '1.50.2'.freeze
