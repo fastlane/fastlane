a3a20d9da0e493cc04e884454ed09fce3a2cfc88
akramhussain4 <akramhussain0007@gmail.com>
2024-01-30 12:52:45 +0530

    remove new line

--- fastlane/lib/fastlane/actions/onesignal.rb
+++ fastlane/lib/fastlane/actions/onesignal.rb
@@ -53,7 +53,6 @@ module Fastlane
         payload["organization_id"] = organization_id unless organization_id.nil?
 
         # here's the actual lifting - POST or PUT to OneSignal
-
         json_headers = { 'Content-Type' => 'application/json', 'Authorization' => "Basic #{auth_token}" }
         url = +'https://api.onesignal.com/apps'
         url << '/' + app_id if is_update
