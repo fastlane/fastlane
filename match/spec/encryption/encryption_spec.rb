26b695f8ed697fa21ca30d57db97d060ce21e631
Skulflesh <84808160+Skulflesh@users.noreply.github.com>
2024-01-31 21:33:05 +0100

    Fix flaky encryption test (#21844) (#21845)
    * Fix flaky encryption test (#21844)
    
    * Adjust documentation

--- match/spec/encryption/encryption_spec.rb
+++ match/spec/encryption/encryption_spec.rb
@@ -16,12 +16,32 @@ describe Match do
       expect(e.decrypt(base64encoded_encrypted: encoded_encrypted_data, password: password)).to eq(data)
     end
 
-    it "decrypts V1 Encryption with SHA256 hash" do
+    it "decrypts V1 Encryption with SHA256 hash when the decryption fails when given the default hash" do
       encryption = v1.encrypt(data: data, password: password, salt: salt, hash_algorithm: "SHA256")
       encrypted_data = Match::Encryption::MatchDataEncryption::V1_PREFIX + salt + encryption[:encrypted_data]
       encoded_encrypted_data = Base64.encode64(encrypted_data)
 
+      eMock = double("EncryptionV1")
+      # make it fail with the default hash
+      allow(eMock).to receive(:decrypt).with(encrypted_data: encryption[:encrypted_data], password: password, salt: salt).and_raise(OpenSSL::Cipher::CipherError)
+      allow(eMock).to receive(:decrypt).with(encrypted_data: encryption[:encrypted_data], password: password, salt: salt, hash_algorithm: "SHA256").and_return(data)
+
+      allow(Match::Encryption::EncryptionV1).to receive(:new).and_return(eMock)
       expect(e.decrypt(base64encoded_encrypted: encoded_encrypted_data, password: password)).to eq(data)
+    end
+
+    it "fails to decrypt V1 Encryption with SHA256 hash when the decryption doesn't fail when given the default hash" do
+      encryption = v1.encrypt(data: data, password: password, salt: salt, hash_algorithm: "SHA256")
+      encrypted_data = Match::Encryption::MatchDataEncryption::V1_PREFIX + salt + encryption[:encrypted_data]
+      encoded_encrypted_data = Base64.encode64(encrypted_data)
+
+      eMock = double("EncryptionV1")
+      # make it return garbage
+      garbage = "garbage"
+      allow(eMock).to receive(:decrypt).with(encrypted_data: encryption[:encrypted_data], password: password, salt: salt).and_return(garbage)
+
+      allow(Match::Encryption::EncryptionV1).to receive(:new).and_return(eMock)
+      expect(e.decrypt(base64encoded_encrypted: encoded_encrypted_data, password: password)).to eq(garbage)
     end
   end
 end
