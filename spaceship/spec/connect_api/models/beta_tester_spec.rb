385a15f74cc617d2a483790575de17cba6d66665
mlch911 <mlch911@mluoc.tech>
2024-02-26 18:12:54 +0800

    [spaceship] Update Model: beta_tester (#21799)
    * Update Model: beta_tester
    
    * Update Model: beta_tester & spec test
    
    * Update beta_tester.rb - adding fields
    
    * Update beta_tester_spec.rb - adjusting tests
    
    ---------
    
    Co-authored-by: Jerome Lacoste <jerome.lacoste@gmail.com>

--- spaceship/spec/connect_api/models/beta_tester_spec.rb
+++ spaceship/spec/connect_api/models/beta_tester_spec.rb
@@ -17,7 +17,21 @@ describe Spaceship::ConnectAPI::BetaTester do
       expect(model.last_name).to eq("AreCute")
       expect(model.email).to eq("email@email.com")
       expect(model.invite_type).to eq("EMAIL")
-      expect(model.invitation).to eq(nil)
+      expect(model.beta_tester_state).to eq("INSTALLED")
+      expect(model.is_deleted).to eq(false)
+      expect(model.last_modified_date).to eq("2024-01-21T20:52:18.921-08:00")
+      expect(model.installed_cf_bundle_short_version_string).to eq("1.3.300")
+      expect(model.installed_cf_bundle_version).to eq("1113")
+      expect(model.remove_after_date).to eq("2024-04-20T00:00:00-07:00")
+      expect(model.installed_device).to eq("iPhone14_7")
+      expect(model.installed_os_version).to eq("17.2.1")
+      expect(model.number_of_installed_devices).to eq(1.0)
+      expect(model.latest_expiring_cf_bundle_short_version_string).to eq("1.3.300")
+      expect(model.latest_expiring_cf_bundle_version_string).to eq("1113")
+      expect(model.installed_device_platform).to eq("IOS")
+      expect(model.latest_installed_device).to eq("iPhone14_7")
+      expect(model.latest_installed_os_version).to eq("17.2.1")
+      expect(model.latest_installed_device_platform).to eq("IOS")
     end
   end
 end
