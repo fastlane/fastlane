38b7931cb94e1832d5c2c7d80de556286ba0ead3
demonguru18 <demonguru18@gmail.com>
2024-03-06 11:09:11 -0500

    Update upload_price_tier.rb

--- deliver/lib/deliver/upload_price_tier.rb
+++ deliver/lib/deliver/upload_price_tier.rb
@@ -21,6 +21,9 @@ module Deliver
       # Need to get prices from the app's relationships
       # Prices from app's relationship does not have price tier so need to fetch app price with price tier relationship
       app_prices = app.prices
+       if app_prices.nil? 
+         UI.message("App has no prices yet... No changes required.")
+         return
       if app_prices&.first
         app_price = Spaceship::ConnectAPI.get_app_price(app_price_id: app_prices.first.id, includes: "priceTier").first
         old_price = app_price.price_tier.id
