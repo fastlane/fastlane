1898d389b21e27f4b2a830db0551bec4f82db59f
Ahmediosxr <166955218+Ahmediosxr@users.noreply.github.com>
2024-04-15 10:09:37 +0300

    Delete credentials_manager directory

--- credentials_manager/lib/credentials_manager.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'credentials_manager/account_manager'
-require_relative 'credentials_manager/cli'
-require_relative 'credentials_manager/appfile_config'
-
-module CredentialsManager
-  ROOT = Pathname.new(File.expand_path('../..', __FILE__))
-end
