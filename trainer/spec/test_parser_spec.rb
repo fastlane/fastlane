fastlane/fastlane

commit dda4007c7a5bc8bd6be840c18ba9ebd6623abca7

[trainer] fix issues where number of failures would always be zero (#21432)
* Sanitize both identifier and test case

* Add unit tests

* Replace String extension with individual rules with simpler `proc` regex.

* Add `.gitattributes` and represent .xcresult as binary.

* Revert "Add `.gitattributes` and represent .xcresult as binary."

This reverts commit 0281dc5904257033341e1f3952d5d0ffc26bff79.

---------

Co-authored-by: Roger Oba <rogerluan.oba@gmail.com>

trainer/lib/trainer/xcresult.rb
@@ -173,6 +173,8 @@ module Trainer
       end
 
       def find_failure(failures)
+        sanitizer = proc { |name| name.gsub(/\W/, "_") }
+        sanitized_identifier = sanitizer.call(self.identifier)
         if self.test_status == "Failure"
           # Tries to match failure on test case name
           # Example TestFailureIssueSummary:
@@ -184,16 +186,10 @@ module Trainer
           #     or identifier: "TestThisDude/testFailureJosh2" (when Objective-C)
 
           found_failure = failures.find do |failure|
-            # Clean test_case_name to match identifier format
-            # Sanitize for Swift by replacing "." for "/"
-            # Sanitize for Objective-C by removing "-", "[", "]", and replacing " " for ?/
-            sanitized_test_case_name = failure.test_case_name
-                                              .tr(".", "/")
-                                              .tr("-", "")
-                                              .tr("[", "")
-                                              .tr("]", "")
-                                              .tr(" ", "/")
-            self.identifier == sanitized_test_case_name
+            # Sanitize both test case name and identifier in a consistent fashion, then replace all non-word
+            # chars with underscore, and compare them
+            sanitized_test_case_name = sanitizer.call(failure.test_case_name)
+            sanitized_identifier == sanitized_test_case_name
           end
           return found_failure
         else

trainer/spec/test_parser_spec.rb
@@ -207,6 +207,44 @@ describe Trainer do
         expect(failure_messages).to eq(["XCTAssertTrue failed", "XCTAssertTrue failed"])
         RSpec::Mocks.space.proxy_for(Trainer::XCResult::TestFailureIssueSummary).reset
       end
+
+      it "works as expected with xcresult with spaces", requires_xcode: true do
+        tp = Trainer::TestParser.new("./trainer/spec/fixtures/Test.with_spaces.xcresult")
+        expect(tp.data).to eq([
+                                {
+                                  project_path: "SpaceTests.xcodeproj",
+                                  target_name: "SpaceTestsTests",
+                                  test_name: "SpaceTestsTests",
+                                  configuration_name: "Test Scheme Action",
+                                  duration: 0.21180307865142822,
+                                  tests: [
+                                    {
+                                      identifier: "SpaceTestsSpec.a test with spaces, should always fail()",
+                                      name: "a test with spaces, should always fail()",
+                                      duration: 0.21180307865142822,
+                                      status: "Failure",
+                                      test_group: "SpaceTestsSpec",
+                                      guid: "",
+                                      failures: [
+                                        {
+                                          failure_message: "expected to equal <1>, got <2>\n (/Users/mahmood.tahir/Developer/SpaceTests/SpaceTestsTests/TestSpec.swift#CharacterRangeLen=0&EndingLineNumber=15&StartingLineNumber=15)",
+                                          file_name: "",
+                                          line_number: 0,
+                                          message: "",
+                                          performance_failure: {}
+                                        }
+                                      ]
+                                    }
+                                  ],
+                                  number_of_tests: 1,
+                                  number_of_failures: 1,
+                                  number_of_skipped: 0,
+                                  number_of_tests_excluding_retries: 1,
+                                  number_of_failures_excluding_retries: 1,
+                                  number_of_retries: 0
+                                }
+                              ])
+      end
     end
   end
 end

trainer/spec/fixtures/Test.with_spaces.xcresult/Info.plist
@@ -1,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>dateCreated</key>
+	<date>2023-07-31T19:01:10Z</date>
+	<key>externalLocations</key>
+	<array/>
+	<key>rootId</key>
+	<dict>
+		<key>hash</key>
+		<string>0~SsRyiCHDfQl2m19SwdRuZY_vhc88s24Xb6jgs7SXLFpWpNV6sGPwMav-Pin-3_-rheMqlGi_0gxrcwMBrUii1w==</string>
+	</dict>
+	<key>storage</key>
+	<dict>
+		<key>backend</key>
+		<string>fileBacked2</string>
+		<key>compression</key>
+		<string>standard</string>
+	</dict>
+	<key>version</key>
+	<dict>
+		<key>major</key>
+		<integer>3</integer>
+		<key>minor</key>
+		<integer>39</integer>
+	</dict>
+</dict>
+</plist>

trainer/spec/fixtures/Test.with_spaces.xcresult/Data/data.0~4VqMqsI5lOfxRppnud6-VDWcNsU8J7VgFCJfW2dXPwOcAkvU-I8Um5yp9n0Zv6nr3VmcxYggaVMDFfR0U_vjKw==
@@ -1,0 +1,1 @@
+[]

trainer/spec/fixtures/Test.with_spaces.xcresult/Data/data.0~96Gg-fMtc5VM-luuv1gvnNzTxyoeYQUgP4hhLCxgMi0c2wQPCGVwRe19qboVX1d3C9dnfZsZBpqv1B28isTPXA==
@@ -1,0 +1,1 @@
+[{"name":"testmanagerd.log","type":1}]

commit 0df4bea56e2a5396808554b3df65d1d4200b544f

Fix trainer failure result handling with objective C tests
When trainer is parsing an xcresult bundle which contains test
failures, it has an `ActionTestMetadata` object that is identified one
way (by "identifier") and it's trying to find the corresponding
`TestFailureIssueSummary` object that's identified another way (by
`testCaseName`). These values are not the same, so in order to locate
the correct `TestFailureIssueSummary` object it needs to transform the
`testCaseName` in the same way that Xcode does when generating the
`ActionTestMetadata` identifier. Exactly how to do this is
undocumented, so folks have attempted to figure it out empirically.

Most recently, dda4007c7a ([trainer] fix issues where number of
failures would always be zero (#21432), 2024-01-10) changed this
transformation code in a way that fixed test names that contain spaces,
but broke Objective-C test names.

This commit fixes the regression caused by dda4007c7a with ObjC tests
as follows:

1) It reverts the transformation behavior to how the code worked
previously. Instead of "sanitizing" both the identifier and the test
case name, it now keeps the identifier as is and "normalizes" the test
case name. The test case name normalization has been moved into its own
method which passes unit tests for ObjC, Swift, and tests with spaces.

2) It adds an ObjC xcresult bundle and unit test to prevent regressing
parsing ObjC test names.

3) It refactors summaries_to_data so that if the corresponding test
failure message cannot be located (but hopefully that won't happen
anymore), we at least properly report that the test failed, but use the
generic "unknown failure message" as the failure message.

See discussion on:

- https://github.com/fastlane/fastlane/pull/21565
- https://github.com/fastlane/fastlane/pull/21432

trainer/lib/trainer/xcresult.rb
@@ -172,29 +172,11 @@ module Trainer
         return [self]
       end
 
-      def find_failure(failures)
-        sanitizer = proc { |name| name.gsub(/\W/, "_") }
-        sanitized_identifier = sanitizer.call(self.identifier)
-        if self.test_status == "Failure"
-          # Tries to match failure on test case name
-          # Example TestFailureIssueSummary:
-          #   producingTarget: "TestThisDude"
-          #   test_case_name: "TestThisDude.testFailureJosh2()" (when Swift)
-          #     or "-[TestThisDudeTests testFailureJosh2]" (when Objective-C)
-          # Example ActionTestMetadata
-          #   identifier: "TestThisDude/testFailureJosh2()" (when Swift)
-          #     or identifier: "TestThisDude/testFailureJosh2" (when Objective-C)
-
-          found_failure = failures.find do |failure|
-            # Sanitize both test case name and identifier in a consistent fashion, then replace all non-word
-            # chars with underscore, and compare them
-            sanitized_test_case_name = sanitizer.call(failure.test_case_name)
-            sanitized_identifier == sanitized_test_case_name
-          end
-          return found_failure
-        else
-          return nil
+      def failure_message(failures)
+        found = failures.find do |failure|
+          self.identifier == failure.normalized_name
         end
+        return found ? found.failure_message : "unknown failure message"
       end
     end
 
@@ -383,6 +365,21 @@ module Trainer
       def initialize(data)
         self.test_case_name = fetch_value(data, "testCaseName")
         super
+      end
+
+      def normalized_name
+        # Return test_case_name normalized to match the identifier from ActionTestMetadata
+        # Example TestFailureIssueSummary:
+        #   producingTarget: "TestThisDude"
+        #   test_case_name: "TestThisDude.testFailureJosh2()" (when Swift)
+        #     or "-[TestThisDudeTests testFailureJosh2]" (when Objective-C)
+        # Example ActionTestMetadata
+        #   identifier: "TestThisDude/testFailureJosh2()" (when Swift)
+        #     or identifier: "TestThisDude/testFailureJosh2" (when Objective-C)
+        if self.test_case_name.start_with?("-[") && self.test_case_name.end_with?("]")
+          return self.test_case_name[2...-1].tr(" ", "/")
+        end
+        return self.test_case_name.tr(".", "/")
       end
 
       def failure_message

trainer/lib/trainer/test_parser.rb
@@ -267,19 +267,17 @@ module Trainer
           end
           info[:retry_count] = retry_count
 
-          # Set failure message if failure found
-          failure = test.find_failure(failures)
-          if failure
+          case test.test_status
+          when "Failure"
             test_row[:failures] = [{
               file_name: "",
               line_number: 0,
               message: "",
               performance_failure: {},
-              failure_message: failure.failure_message
+              failure_message: test.failure_message(failures)
             }]
-
             info[:failure_count] += 1
-          elsif test.test_status == "Skipped"
+          when "Skipped"
             test_row[:skipped] = true
             info[:skip_count] += 1
           else

trainer/spec/fixtures/Test.objc.xcresult/Info.plist
@@ -1,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>dateCreated</key>
+	<date>2024-04-23T23:38:03Z</date>
+	<key>externalLocations</key>
+	<array/>
+	<key>rootId</key>
+	<dict>
+		<key>hash</key>
+		<string>0~QcTkA55UPcxEP1nO1AY1osK3wJ6ccJbYiM6K7BJmWZ0MFG5UOgWiKPIxZQdQeP-BtGoBEwiEcoYSkz_QqfjjMA==</string>
+	</dict>
+	<key>storage</key>
+	<dict>
+		<key>backend</key>
+		<string>fileBacked2</string>
+		<key>compression</key>
+		<string>standard</string>
+	</dict>
+	<key>version</key>
+	<dict>
+		<key>major</key>
+		<integer>3</integer>
+		<key>minor</key>
+		<integer>39</integer>
+	</dict>
+</dict>
+</plist>

trainer/spec/test_parser_spec.rb
@@ -245,6 +245,53 @@ describe Trainer do
                                 }
                               ])
       end
+
+      it "works as expected with Objective C xcresult", requires_xcode: true do
+        tp = Trainer::TestParser.new("./trainer/spec/fixtures/Test.objc.xcresult")
+        expect(tp.data).to eq([
+                                {
+                                  project_path: "HelloWorldTest.xcodeproj",
+                                  target_name: "HelloWorldTestTests",
+                                  test_name: "HelloWorldTestTests",
+                                  configuration_name: "Test Scheme Action",
+                                  duration: 0.04512190818786621,
+                                  tests: [
+                                    {
+                                      identifier: "HelloWorldTestTests.testExample",
+                                      name: "testExample",
+                                      duration: 0.0011409521102905273,
+                                      status: "Success",
+                                      test_group: "HelloWorldTestTests",
+                                      guid: "",
+                                    },
+                                    {
+                                      identifier: "HelloWorldTestTests.testFailure",
+                                      name: "testFailure",
+                                      duration: 0.043980956077575684,
+                                      status: "Failure",
+                                      test_group: "HelloWorldTestTests",
+                                      guid: "",
+                                      failures: [
+                                        {
+                                          failure_message: "((NO) is true) failed - Fail (/Users/jsoffian/HelloWorldTest/HelloWorldTestTests/HelloWorldTestTests.m#CharacterRangeLen=0&EndingLineNumber=39&StartingLineNumber=39)",
+                                          file_name: "",
+                                          line_number: 0,
+                                          message: "",
+                                          performance_failure: {}
+                                        }
+                                      ]
+                                    }
+                                  ],
+                                  number_of_tests: 2,
+                                  number_of_failures: 1,
+                                  number_of_skipped: 0,
+                                  number_of_tests_excluding_retries: 2,
+                                  number_of_failures_excluding_retries: 1,
+                                  number_of_retries: 0
+                                }
+                              ])
+      end
+
     end
   end
 end