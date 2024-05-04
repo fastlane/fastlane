6da4002449854d2a1ddc8a5516f4366329319cfb
Lukasz Grabowski <luc.grabowski@gmail.com>
2024-05-04 14:08:47 +0200

    Merge fix

--- spaceship/spec/connect_api/tunes/tunes_stubbing.rb
+++ spaceship/spec/connect_api/tunes/tunes_stubbing.rb
@@ -22,6 +22,7 @@ class ConnectAPIStubbing
       def stub_get_app_availabilities_removed_from_sale
         stub_request(:get, "https://appstoreconnect.apple.com/iris/v2/appAvailabilities/123456789?include=territoryAvailabilities&limit%5BterritoryAvailabilities%5D=200").
           to_return(status: 200, body: read_fixture_file('app_availabilities_removed_app.json'), headers: { 'Content-Type' => 'application/json' })
+      end
 
       def stub_get_app_infos
         stub_request(:get, "https://appstoreconnect.apple.com/iris/v1/apps/123456789/appInfos").
