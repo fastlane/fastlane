6a57030f660081f9832f85f119c07123b703dca7
Jens Kuhr Jørgensen <jenskuhr4@gmail.com>
2024-07-09 07:28:18 +0200

    Replace show-current
    Replace `git branch --show-current` with `git rev-parse --abbrev-ref HEAD` to support git < 2.22 (https://github.com/git/git/blob/master/Documentation/RelNotes/2.22.0.txt)

--- fastlane/lib/fastlane/fast_file.rb
+++ fastlane/lib/fastlane/fast_file.rb
@@ -345,7 +345,7 @@ module Fastlane
               Helper.with_env_values('GIT_TERMINAL_PROMPT' => '0') do
                 Actions.sh("cd #{clone_folder.shellescape} && git fetch --all --quiet && git checkout #{checkout_param.shellescape} #{checkout_path} && git reset --hard")
                 # Check if checked out "branch" is actually a branch or a tag
-                current_branch = Actions.sh("cd #{clone_folder.shellescape} && git branch --show-current")
+                current_branch = Actions.sh("cd #{clone_folder.shellescape} && git rev-parse --abbrev-ref HEAD")
                 unless current_branch.empty?
                   Actions.sh("cd #{clone_folder.shellescape} && git rebase")
                 end
