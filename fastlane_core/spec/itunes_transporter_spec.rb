ccdac7204a20ee97e8f2e26c7598063449517da9
Jamie <jtlthe2@gmail.com>
2025-10-03 11:38:55 -0400

    Revert "Remove changes to tests to generate a patch"
    This reverts commit 66ace45c96e3e439d40507cbf16a6f9860f5c33e.

--- fastlane_core/spec/itunes_transporter_spec.rb
+++ fastlane_core/spec/itunes_transporter_spec.rb
@@ -13,8 +13,9 @@ describe FastlaneCore do
       allow(SecureRandom).to receive(:uuid).and_return(random_uuid)
     end
 
-    def shell_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false, username: email, input_pass: password, api_key: nil)
+    def shell_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false, use_asset_description: false, username: email, input_pass: password, api_key: nil)
       upload_part = use_asset_path ? "-assetFile /tmp/#{random_uuid}.ipa" : "-f /tmp/my.app.id.itmsp"
+      asset_description_part = use_asset_description ? "-assetDescription /tmp/AppStoreInfo.plist" : nil
 
       username = username if username != email
       escaped_password = input_pass
@@ -44,6 +45,7 @@ describe FastlaneCore do
            "-jwt #{jwt}"
          end),
         upload_part,
+        asset_description_part,
         (transporter.to_s if transporter),
         "-k 100000",
         ("-WONoPause true" if FastlaneCore::Helper.windows?),
@@ -142,8 +144,9 @@ describe FastlaneCore do
       ].compact.join(' ')
     end
 
-    def java_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, classpath: true, use_asset_path: false)
+    def java_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, classpath: true, use_asset_path: false, use_asset_description: false)
       upload_part = use_asset_path ? "-assetFile /tmp/#{random_uuid}.ipa" : "-f /tmp/my.app.id.itmsp"
+      asset_description_part = use_asset_description ? "-assetDescription /tmp/AppStoreInfo.plist" : nil
 
       [
         FastlaneCore::Helper.transporter_java_executable_path.shellescape,
@@ -161,6 +164,7 @@ describe FastlaneCore do
         ("-u #{email.shellescape} -p #{password.shellescape}" if jwt.nil?),
         ("-jwt #{jwt}" unless jwt.nil?),
         upload_part,
+        asset_description_part,
         (transporter.to_s if transporter),
         "-k 100000",
         ("-itc_provider #{provider_short_name}" if provider_short_name),
@@ -233,8 +237,9 @@ describe FastlaneCore do
       ].compact.join(' ')
     end
 
-    def java_upload_command_9(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false)
+    def java_upload_command_9(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false, use_asset_description: false)
       upload_part = use_asset_path ? "-assetFile /tmp/#{random_uuid}.ipa" : "-f /tmp/my.app.id.itmsp"
+      asset_description_part = use_asset_description ? "-assetDescription /tmp/AppStoreInfo.plist" : nil
 
       [
         FastlaneCore::Helper.transporter_java_executable_path.shellescape,
@@ -250,6 +255,7 @@ describe FastlaneCore do
         ("-u #{email.shellescape} -p #{password.shellescape}" if jwt.nil?),
         ("-jwt #{jwt}" unless jwt.nil?),
         upload_part,
+        asset_description_part,
         (transporter.to_s if transporter),
         "-k 100000",
         ("-itc_provider #{provider_short_name}" if provider_short_name),
@@ -299,8 +305,9 @@ describe FastlaneCore do
       ].compact.join(' ')
     end
 
-    def xcrun_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false)
+    def xcrun_upload_command(provider_short_name: nil, transporter: nil, jwt: nil, use_asset_path: false, use_asset_description: false)
       upload_part = use_asset_path ? "-assetFile /tmp/#{random_uuid}.ipa" : "-f /tmp/my.app.id.itmsp"
+      asset_description_part = use_asset_description ? "-assetDescription /tmp/AppStoreInfo.plist" : nil
 
       [
         ("ITMS_TRANSPORTER_PASSWORD=#{password.shellescape}" if jwt.nil?),
@@ -310,6 +317,7 @@ describe FastlaneCore do
         ("-p @env:ITMS_TRANSPORTER_PASSWORD" if jwt.nil?),
         ("-jwt #{jwt}" unless jwt.nil?),
         upload_part,
+        asset_description_part,
         (transporter.to_s if transporter),
         "-k 100000",
         ("-itc_provider #{provider_short_name}" if provider_short_name),
