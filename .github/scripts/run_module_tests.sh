b0687d1f41bb003c5abe15cddce876b76659b3aa
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-11-26 06:52:42 -0500

    chore: split module tests to own file

---
+++ .github/scripts/run_module_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# running tests
+TEST_MODULES=(
+  "cert"
+  "credentials_manager"
+  "deliver"
+  "fastlane"
+  "fastlane_core"
+  "frameit"
+  "gym"
+  "match"
+  "pem"
+  "pilot"
+  "precheck"
+  "produce"
+  "scan"
+  "screengrab"
+  "sigh"
+  "snapshot"
+  "spaceship"
+  "supply"
+)
+
+for TEST_MODULE in "${TEST_MODULES[@]}"; do
+  echo "Executing $TEST_MODULE module load up test"
+  if ruby -e "require '$TEST_MODULE'"; then
+    echo "Succeeded."
+  else
+    exit 1
+  fi
+done
+
+exit 0
