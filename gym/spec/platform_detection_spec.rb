fastlane/fastlane

commit 8d3270a7a0b2a52dedfabce9f5db185fc2cf3cf3

[gym] evaluate building for .ipa or .pkg beyond iOS and macOS (#22145)
* [gym] evaluate building for .ipa or .pkg

* Prefer unless

* Fix Mac project condition

* Keep former condition for Mac

* Fix Mac condition again

* Test new methods

gym/lib/gym/module.rb
@@ -25,6 +25,14 @@ module Gym
       require 'gym/xcodebuild_fixes/generic_archive_fix'
     end
 
+    def building_for_ipa?
+      return !building_for_pkg?
+    end
+
+    def building_for_pkg?
+      return building_for_mac?
+    end
+
     def building_for_ios?
       if Gym.project.mac?
         # Can be building for iOS if mac project and catalyst or multiplatform and set to iOS
@@ -43,10 +51,7 @@ module Gym
         # Can be a mac project and not build mac if catalyst
         return building_mac_catalyst_for_mac?
       else
-        # Can be mac project but multiplatform and building for iOS
-        return false if building_multiplatform_for_ios?
-
-        return Gym.project.mac?
+        return (!Gym.project.multiplatform? && Gym.project.mac?) || building_multiplatform_for_mac?
       end
     end
 

gym/spec/platform_detection_spec.rb
@@ -9,6 +9,8 @@ describe Gym do
 
     expect(Gym.building_for_mac?).to eq(false)
     expect(Gym.building_for_ios?).to eq(true)
+    expect(Gym.building_for_ipa?).to eq(true)
+    expect(Gym.building_for_pkg?).to eq(false)
     expect(Gym.building_multiplatform_for_ios?).to eq(true)
   end
 
@@ -22,6 +24,8 @@ describe Gym do
 
     expect(Gym.building_for_ios?).to eq(false)
     expect(Gym.building_for_mac?).to eq(true)
+    expect(Gym.building_for_ipa?).to eq(false)
+    expect(Gym.building_for_pkg?).to eq(true)
     expect(Gym.building_multiplatform_for_mac?).to eq(true)
   end
 
@@ -35,6 +39,8 @@ describe Gym do
 
     expect(Gym.building_for_ios?).to eq(true)
     expect(Gym.building_for_mac?).to eq(false)
+    expect(Gym.building_for_ipa?).to eq(true)
+    expect(Gym.building_for_pkg?).to eq(false)
     expect(Gym.building_multiplatform_for_mac?).to eq(false)
   end
 end

gym/lib/gym/runner.rb
@@ -24,7 +24,7 @@ module Gym
       FileUtils.mkdir_p(File.expand_path(Gym.config[:output_directory]))
 
       # Archive
-      if Gym.building_for_ios?
+      if Gym.building_for_ipa?
         fix_generic_archive unless Gym.project.watchos? # See https://github.com/fastlane/fastlane/pull/4325
         return BuildCommandGenerator.archive_path if Gym.config[:skip_package_ipa]
 
@@ -41,7 +41,7 @@ module Gym
           appstore_info_path = move_appstore_info
           generate_appstore_info(path) unless appstore_info_path
         end
-      elsif Gym.building_for_mac?
+      elsif Gym.building_for_pkg?
         path = File.expand_path(Gym.config[:output_directory])
         compress_and_move_dsym
         if Gym.project.mac_app? || Gym.building_mac_catalyst_for_mac?

commit 08f648b33bfd65c6fadc47e5bb5a8fe3958dfb8e

[gym] Fix Mac Catalyst package type detection when catalyst_platform is not set
This fixes a regression introduced in #22145 where Mac Catalyst apps were
incorrectly being validated as IPA instead of PKG when the catalyst_platform
option was not explicitly set.

The issue occurred because the building_mac_catalyst_for_mac? and
building_mac_catalyst_for_ios? methods only checked if catalyst_platform
was explicitly set to "macos" or "ios". When this option wasn't set (which
is common), the methods would return false, causing the build system to
incorrectly determine the package type.

This change updates both methods to fall back to SDK-based detection when
catalyst_platform is not explicitly set:
- SDK macosx -> build for Mac (PKG)
- SDK iphoneos/iphonesimulator -> build for iOS (IPA)

This maintains backward compatibility with projects that explicitly set
catalyst_platform while properly handling projects that rely on SDK-based
detection.

gym/lib/gym/module.rb
@@ -56,11 +56,29 @@ module Gym
     end
 
     def building_mac_catalyst_for_ios?
-      Gym.project.supports_mac_catalyst? && Gym.config[:catalyst_platform] == "ios"
+      return false unless Gym.project.supports_mac_catalyst?
+
+      # If catalyst_platform is explicitly set, use it
+      if Gym.config[:catalyst_platform]
+        return Gym.config[:catalyst_platform] == "ios"
+      end
+
+      # If catalyst_platform is not set, use SDK to determine
+      # Mac Catalyst apps with iOS SDK build for iOS (IPA), not macOS
+      return Gym.config[:sdk] == "iphoneos" || Gym.config[:sdk] == "iphonesimulator"
     end
 
     def building_mac_catalyst_for_mac?
-      Gym.project.supports_mac_catalyst? && Gym.config[:catalyst_platform] == "macos"
+      return false unless Gym.project.supports_mac_catalyst?
+
+      # If catalyst_platform is explicitly set, use it
+      if Gym.config[:catalyst_platform]
+        return Gym.config[:catalyst_platform] == "macos"
+      end
+
+      # If catalyst_platform is not set, use SDK to determine
+      # Mac Catalyst apps with SDK macosx build for Mac (PKG), otherwise for iOS (IPA)
+      return Gym.config[:sdk] == "macosx"
     end
 
     def building_multiplatform_for_ios?

gym/spec/platform_detection_spec.rb
@@ -43,4 +43,36 @@ describe Gym do
     expect(Gym.building_for_pkg?).to eq(false)
     expect(Gym.building_multiplatform_for_mac?).to eq(false)
   end
+
+  it "detects when a Mac Catalyst project with macosx SDK is building for macOS without explicit catalyst_platform" do
+    options = { project: "./gym/examples/standard/Example.xcodeproj", sdk: "macosx" }
+    Gym.config = FastlaneCore::Configuration.create(Gym::Options.available_options, options)
+
+    # Mock the project to support Mac Catalyst
+    allow(Gym.project).to receive(:supports_mac_catalyst?).and_return(true)
+    allow(Gym.project).to receive(:mac?).and_return(true)
+    allow(Gym.project).to receive(:ios?).and_return(true)
+
+    # When SDK is macosx and no explicit catalyst_platform is set, should build for Mac (PKG)
+    expect(Gym.building_for_mac?).to eq(true)
+    expect(Gym.building_for_ios?).to eq(false)
+    expect(Gym.building_for_ipa?).to eq(false)
+    expect(Gym.building_for_pkg?).to eq(true)
+  end
+
+  it "detects when a Mac Catalyst project with iphoneos SDK is building for iOS without explicit catalyst_platform" do
+    options = { project: "./gym/examples/standard/Example.xcodeproj", sdk: "iphoneos" }
+    Gym.config = FastlaneCore::Configuration.create(Gym::Options.available_options, options)
+
+    # Mock the project to support Mac Catalyst
+    allow(Gym.project).to receive(:supports_mac_catalyst?).and_return(true)
+    allow(Gym.project).to receive(:mac?).and_return(true)
+    allow(Gym.project).to receive(:ios?).and_return(true)
+
+    # When SDK is iphoneos and no explicit catalyst_platform is set, should build for iOS (IPA)
+    expect(Gym.building_for_mac?).to eq(false)
+    expect(Gym.building_for_ios?).to eq(true)
+    expect(Gym.building_for_ipa?).to eq(true)
+    expect(Gym.building_for_pkg?).to eq(false)
+  end
 end