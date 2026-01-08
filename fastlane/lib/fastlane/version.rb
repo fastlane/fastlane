b4413acd7a25399fc5411b130b692ba59cdc04a7
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2026-01-08 07:25:20 -0500

    [core] add warning to output on unsupported Ruby (< 3.2) version (#29840)

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -4,4 +4,5 @@ module Fastlane
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
   MINIMUM_XCODE_RELEASE = "7.0".freeze
   RUBOCOP_REQUIREMENT = '1.50.2'.freeze
+  SUGGESTED_RUBY_MIN_VERSION = '3.2.0'.freeze
 end
