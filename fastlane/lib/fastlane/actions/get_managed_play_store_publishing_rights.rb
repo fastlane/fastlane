92583b3f8a15f3177a85f7fc6527dffc154e7334
Tyler Renslow <tyler.renslow@medel.com>
2026-04-22 08:42:03 +0200

    [supply] Revert get_managed_play_store_publishing_rights descriptions to service account only

--- fastlane/lib/fastlane/actions/get_managed_play_store_publishing_rights.rb
+++ fastlane/lib/fastlane/actions/get_managed_play_store_publishing_rights.rb
@@ -78,7 +78,7 @@ module Fastlane
                                        short_option: "-j",
                                        conflicting_options: [:json_key_data],
                                        optional: true, # optional until it is possible specify either json_key OR json_key_data are required
-                                       description: "The path to a Google credentials JSON file (Application Default, Workload Identity, or Service Account) used to authenticate with Google",
+                                       description: "The path to a file containing service account JSON, used to authenticate with Google",
                                        code_gen_sensitive: true,
                                        default_value: CredentialsManager::AppfileConfig.try_fetch_value(:json_key_file),
                                        default_value_dynamic: true,
@@ -91,7 +91,7 @@ module Fastlane
                                        short_option: "-c",
                                        conflicting_options: [:json_key],
                                        optional: true,
-                                       description: "The raw content of a Google credentials JSON file (Application Default, Workload Identity, or Service Account) used to authenticate with Google",
+                                       description: "The raw service account JSON data used to authenticate with Google",
                                        code_gen_sensitive: true,
                                        default_value: CredentialsManager::AppfileConfig.try_fetch_value(:json_key_data_raw),
                                        default_value_dynamic: true,
