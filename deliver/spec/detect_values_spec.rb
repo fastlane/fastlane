648208e3d00ec2bd6f6a1ad6de74659e02802802
Connor Tumbleson <connor.tumbleson@gmail.com>
2026-04-24 06:08:14 -0400

    chore: create dir in tmp and clean up at end

--- deliver/spec/detect_values_spec.rb
+++ deliver/spec/detect_values_spec.rb
@@ -1,7 +1,14 @@
 require 'deliver/detect_values'
+require 'fileutils'
+require 'tmpdir'
 
 describe Deliver::DetectValues do
   let(:value_detector) { Deliver::DetectValues.new }
+  let(:tmpdir) { Dir.mktmpdir }
+
+  after do
+    FileUtils.remove_entry_secure(tmpdir)
+  end
 
   describe :find_folders do
     describe 'when folders are not specified in options' do
@@ -10,30 +17,36 @@ describe Deliver::DetectValues do
       describe 'running with fastlane' do
         before do
           allow(FastlaneCore::Helper).to receive(:fastlane_enabled?).and_return(true)
-          allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return('./fastlane')
+          allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(File.join(tmpdir, 'fastlane'))
         end
 
         it 'sets up screenshots folder in fastlane folder' do
           value_detector.find_folders(options)
-          expect(options[:screenshots_path]).to eq('./fastlane/screenshots')
+          expect(options[:screenshots_path]).to eq(File.join(tmpdir, 'fastlane', 'screenshots'))
         end
 
         it 'sets up metadata folder in fastlane folder' do
           value_detector.find_folders(options)
-          expect(options[:metadata_path]).to eq('./fastlane/metadata')
+          expect(options[:metadata_path]).to eq(File.join(tmpdir, 'fastlane', 'metadata'))
         end
 
         it 'does not automatically set up app previews folder in fastlane folder even if it exists' do
-          FileUtils.mkdir_p('./fastlane/app-previews')
+          path = File.join(tmpdir, 'fastlane', 'app-previews')
+          FileUtils.mkdir_p(path)
           value_detector.find_folders(options)
           expect(options[:app_previews_path]).to be_nil
-          FileUtils.rm_rf('./fastlane/app-previews')
         end
       end
 
       describe 'running without fastlane' do
         before do
           allow(FastlaneCore::Helper).to receive(:fastlane_enabled?).and_return(false)
+          @old_cwd = Dir.pwd
+          Dir.chdir(tmpdir)
+        end
+
+        after do
+          Dir.chdir(@old_cwd)
         end
 
         it 'sets up screenshots folder in current folder' do
@@ -50,7 +63,6 @@ describe Deliver::DetectValues do
           FileUtils.mkdir_p('./app-previews')
           value_detector.find_folders(options)
           expect(options[:app_previews_path]).to be_nil
-          FileUtils.rm_rf('./app-previews')
         end
       end
     end
