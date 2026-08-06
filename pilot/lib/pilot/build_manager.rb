c7d3498d10679d482730d6a9eb7a5726e779c1af
Alexander Chapliuk <2318002+krin-san@users.noreply.github.com>
2026-08-06 14:15:56 +0200

    [pilot] Match groups by either name or id in tester_manager

--- pilot/lib/pilot/build_manager.rb
+++ pilot/lib/pilot/build_manager.rb
@@ -459,7 +459,7 @@ module Pilot
       if options[:groups]
         app = uploaded_build.app
         beta_groups = app.get_beta_groups.select do |group|
-          options[:groups].include?(group.name) || options[:groups].include?(group.id)
+          group.matches_identifiers?(options[:groups])
         end
 
         unless beta_groups.empty?
