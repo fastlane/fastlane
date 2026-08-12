811f2a481bab9852e8a331515ea9c3282767f790
Connor Tumbleson <connor.tumbleson@gmail.com>
2026-08-12 06:27:06 -0400

    [deliver] prevent duplicate image uploads

--- deliver/lib/deliver/upload_screenshots.rb
+++ deliver/lib/deliver/upload_screenshots.rb
@@ -171,13 +171,17 @@ module Deliver
     def wait_for_complete(iterator, timeout_seconds)
       start_time = Time.now
       loop do
+        # App Store Connect publishes a screenshot's `sourceFileChecksum` asynchronously,
+        # shortly after its state becomes COMPLETE. (#30094)
+        number_of_screenshots_missing_checksum = 0
         states = iterator.each_app_screenshot.map { |_, _, app_screenshot| app_screenshot }.each_with_object({}) do |app_screenshot, hash|
           state = app_screenshot.asset_delivery_state['state']
           hash[state] ||= 0
           hash[state] += 1
+          number_of_screenshots_missing_checksum += 1 if state == 'COMPLETE' && app_screenshot.source_file_checksum.nil?
         end
 
-        is_processing = states.fetch('UPLOAD_COMPLETE', 0) > 0
+        is_processing = states.fetch('UPLOAD_COMPLETE', 0) > 0 || number_of_screenshots_missing_checksum > 0
         return states unless is_processing
 
         if Time.now - start_time > timeout_seconds
@@ -185,7 +189,11 @@ module Deliver
           return states
         end
 
-        UI.verbose("There are still incomplete screenshots - #{states}")
+        if number_of_screenshots_missing_checksum > 0
+          UI.verbose("There are still incomplete screenshots - #{states}, missing checksum: #{number_of_screenshots_missing_checksum}")
+        else
+          UI.verbose("There are still incomplete screenshots - #{states}")
+        end
         sleep(5)
       end
     end
@@ -205,9 +213,9 @@ module Deliver
         UI.user_error!("Failed verification of all screenshots uploaded... #{incomplete_screenshot_count} incomplete screenshot(s) still exist")
       else
         UI.error("Failed to upload all screenshots... Tries remaining: #{tries}")
-        # Delete bad entries before retry
+        # Delete bad entries before retry (not complete OR missing checksum)
         iterator.each_app_screenshot do |_, _, app_screenshot|
-          app_screenshot.delete! unless app_screenshot.complete?
+          app_screenshot.delete! unless app_screenshot.complete? && !app_screenshot.source_file_checksum.nil?
         end
         upload_screenshots(localizations, screenshots_per_language, timeout_seconds, tries: tries)
       end
